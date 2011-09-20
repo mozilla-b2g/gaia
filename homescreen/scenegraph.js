7e524a05bfde4c01351e7b65209fa69d60551f5c
Andreas Gal <andreas.gal@gmail.com>
2011-09-19 19:49:37 -0700

    fix a race condition, but GL is still not rendering

--- homescreen/scenegraph.js
+++ homescreen/scenegraph.js
@@ -138,10 +138,10 @@ function SceneGraph(canvas) {
 SceneGraph.prototype = {
   // add a sprite to the scene graph
   add: function(sprite) {
+    this.blitter.spriteAdded(sprite);
     var sprites = this.sprites;
     sprite.index = sprites.length;
     sprites.push(sprite);
-    this.blitter.spriteAdded(sprite);
   },
   // remove a sprite from the scene graph
   remove: function(sprite) {
@@ -337,7 +337,7 @@ SpriteBlitterGL.prototype = {
 
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
-                  sprite.canvas);
+                  canvas);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     // XXX mipmap if we start downscaling a lot
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
