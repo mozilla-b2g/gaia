4993ab52f81a39c7bd3d9f294e24637c39e56fd2
Andreas Gal <andreas.gal@gmail.com>
2011-09-26 22:05:38 -0700

    initial demo import

---
+++ homescreen.js
@@ -0,0 +1,390 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+function createPhysicsFor(iconGrid) {
+  return new DefaultPhysics(iconGrid);
+}
+
+function DefaultPhysics(iconGrid) {
+  this.iconGrid = iconGrid;
+  this.moved = false;
+  this.touchState = { active: false, startX: 0, startY: 0 };
+}
+
+DefaultPhysics.prototype = {
+  onTouchStart: function(e) {
+    var touchState = this.touchState;
+    this.moved = false;
+    touchState.active = true;
+    touchState.startX = e.pageX;
+    touchState.startY = e.pageY;
+    touchState.startTime = e.timeStamp;
+  },
+  onTouchMove: function(e) {
+    var iconGrid = this.iconGrid;
+    var touchState = this.touchState;
+    if (touchState.active) {
+      var dx = touchState.startX - e.pageX;
+      if (dx !== 0) {
+        iconGrid.sceneGraph.setViewportTopLeft(
+          iconGrid.currentPage * iconGrid.containerWidth + dx, 0, 0);
+        this.moved = true;
+      }
+    }
+  },
+  onTouchEnd: function(e) {
+    var touchState = this.touchState;
+    if (!touchState.active)
+      return;
+    touchState.active = false;
+
+    var startX = touchState.startX;
+    var endX = e.pageX;
+    var diffX = endX - startX;
+    var dir = (diffX > 0) ? -1 : 1;
+
+    var quick = (e.timeStamp - touchState.startTime < 200);
+    var small = Math.abs(diffX) < 10;
+
+    var flick = quick && !small;
+    var tap = !this.moved && small;
+    var drag = !quick;
+
+    var iconGrid = this.iconGrid;
+    var currentPage = iconGrid.currentPage
+    if (tap) {
+      iconGrid.tap(currentPage * iconGrid.containerWidth + startX,
+                   touchState.startY);
+    } else if (flick) {
+      iconGrid.setPage(currentPage + dir, 200);
+    } else {
+      if (Math.abs(diffX) < this.containerWidth/2)
+        iconGrid.setPage(currentPage, 200);
+      else
+        iconGrid.setPage(currentPage + dir, 200);
+    }
+  }
+};
+
+function Icon(iconGrid, index) {
+  this.iconGrid = iconGrid;
+  this.index = index;
+  this.label = '';
+  this.url = '';
+}
+
+Icon.prototype = {
+  update: function(img, label, url) {
+    this.label = label;
+    this.url = url;
+    var iconGrid = this.iconGrid;
+    var iconWidth = iconGrid.iconWidth;
+    var iconHeight = iconGrid.iconHeight;
+    var border = iconGrid.border;
+    var sprite = this.sprite;
+    var createSprite = !sprite;
+    if (createSprite) {
+      var sceneGraph = iconGrid.sceneGraph;
+      sprite = new Sprite(iconWidth, iconHeight);
+      sprite.icon = this;
+      this.sprite = sprite;
+    }
+    var ctx = sprite.getContext2D();
+    ctx.drawImage(img, iconWidth * border, iconHeight * border,
+                  iconWidth * (1 - border * 2),
+                  iconHeight * (1 - border * 2));
+    ctx.font = Math.floor(iconHeight * border * 0.6) + "pt Arial, sans-serif";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.textBaseline = "top";
+    ctx.fillText(label, iconWidth/2, iconHeight - iconHeight*border, iconWidth*0.9);
+    if (createSprite)
+      sceneGraph.add(sprite);
+    this.reflow();
+  },
+  // return the X coordinate of the top left corner of a slot
+  slotLeft: function() {
+    var iconGrid = this.iconGrid;
+    return iconGrid.itemBoxWidth * (this.index % iconGrid.columns);
+  },
+  // return the Y coordinate of the top left corner of a slot
+  slotTop: function() {
+    var iconGrid = this.iconGrid;
+    var slot = this.index % iconGrid.itemsPerPage;
+    return Math.floor(slot / iconGrid.columns) * iconGrid.itemBoxHeight;
+  },
+  reflow: function(duration) {
+    var sprite = this.sprite;
+    if (!sprite)
+      return;
+    var iconGrid = this.iconGrid;
+    var index = this.index;
+    var itemsPerPage = iconGrid.itemsPerPage;
+    var page = Math.floor(index / iconGrid.itemsPerPage);
+    sprite.setPosition(page * iconGrid.containerWidth + this.slotLeft(),
+                       this.slotTop(),
+                       duration);
+    sprite.setScale(1, duration);
+  }
+}
+
+function IconGrid(canvas, background, iconWidth, iconHeight, border) {
+  this.canvas = canvas;
+  canvas.mozOpaque = true;
+
+  this.iconWidth = iconWidth;
+  this.iconHeight = iconHeight;
+  this.sceneGraph = new SceneGraph(canvas);
+  this.border = border || 0.1;
+  this.icons = [];
+  this.currentPage = 0;
+  this.physics = createPhysicsFor(this);
+
+  // add the background image
+  this.sceneGraph.setBackground(background);
+
+  // update the layout state
+  this.reflow(canvas.width, canvas.height, 0);
+
+  // install event handlers
+  canvas.addEventListener("touchstart", this, true);
+  canvas.addEventListener("mousedown", this, true);
+  canvas.addEventListener("touchmove", this, true);
+  canvas.addEventListener("mousemove", this, true);
+  canvas.addEventListener("touchend", this, true);
+  canvas.addEventListener("mouseup", this, true);
+  canvas.addEventListener("mouseout", this, true);
+  window.addEventListener("resize", this, true);
+}
+
+IconGrid.prototype = {
+  add: function(src, label, url) {
+    // Create the icon in the icon grid
+    var icons = this.icons;
+    var icon = new Icon(this, this.icons.length);
+    icon.index = icons.length;
+    icons.push(icon);
+    // Load the image, sprite will be created when image load is complete
+    var img = new Image();
+    img.src = src;
+    img.label = label;
+    img.url = url;
+    img.icon = icon;
+    img.onload = function() {
+      // Update the icon (this will trigger a reflow and a repaint)
+      var icon = this.icon;
+      icon.update(this, this.label, this.url);
+    }
+    return icon;
+  },
+  remove: function(icon) {
+    this.icons.splice(icon.index);
+    if (icon.sprite)
+      sceneGraph.remove(icon.sprite);
+  },
+  // reflow the icon grid
+  reflow: function(width, height, duration) {
+    // first recalculate all the layout information
+    this.containerWidth = width;
+    this.containerHeight = height;
+    this.panelWidth = this.containerWidth;
+    this.pageIndicatorWidth = this.containerWidth;
+    this.pageIndicatorHeight = Math.min(Math.max(this.containerHeight * 0.7, 14), 20);
+    this.panelHeight = this.containerHeight - this.pageIndicatorHeight;
+    this.columns = Math.floor(this.panelWidth / this.iconWidth);
+    this.rows = Math.floor(this.panelHeight / this.iconHeight);
+    this.itemsPerPage = this.rows * this.columns;
+    this.itemBoxWidth = Math.floor(this.panelWidth / this.columns);
+    this.itemBoxHeight = Math.floor(this.panelHeight / this.rows);
+
+    // switch to the right page
+    this.setPage(this.currentPage, duration);
+
+    // now reflow all the icons
+    var icons = this.icons;
+    for (var n = 0; n < icons.length; ++n)
+      icons[n].reflow(duration);
+  },
+  // get last page with an icon
+  getLastPage: function() {
+    var itemsPerPage = this.itemsPerPage;
+    var lastPage = Math.floor((this.icons.length + (itemsPerPage - 1)) / itemsPerPage);
+    if (lastPage > 0)
+      --lastPage;
+    return lastPage;
+  },
+  // switch to a different page
+  setPage: function(page, duration) {
+    page = Math.max(0, page);
+    page = Math.min(page, this.getLastPage());
+    this.sceneGraph.setViewportTopLeft(this.containerWidth * page, 0, duration);
+    this.currentPage = page;
+  },
+  // process a computed tap at the given scene-graph coordinates
+  tap: function(x, y) {
+    this.sceneGraph.forHit(
+      x, y,
+      function(sprite) { openApplication(sprite.icon.url); });
+  },
+  handleEvent: function(e) {
+    var physics = this.physics;
+    switch (e.type) {
+    case 'touchstart':
+    case 'mousedown':
+      physics.onTouchStart(e.touches ? e.touches[0] : e);
+      break;
+    case 'touchmove':
+    case 'mousemove':
+      physics.onTouchMove(e.touches ? e.touches[0] : e);
+      break;
+    case 'touchend':
+    case 'mouseup':
+    case 'mouseout':
+      physics.onTouchEnd(e.touches ? e.touches[0] : e);
+      break;
+    case "resize":
+      var canvas = this.canvas;
+      var width = canvas.width = window.innerWidth;
+      // TODO Substract the height of the statusbar
+      var height = canvas.height = window.innerHeight - 24;
+      if (kUseGL) {
+        this.sceneGraph.blitter.viewportWidth = width;
+        this.sceneGraph.blitter.viewportHeight = height;
+      }
+      this.reflow(width, height, 0);
+      break;
+    }
+  }
+}
+
+function OnLoad() {
+  var fruits = [
+    { label: 'Phone', src: 'images/Phone.png',
+      url: 'dialer/dialer.html' },
+    { label: 'Messages', src: 'images/Messages.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Calendar', src: 'images/Calendar.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Gallery', src: 'images/Gallery.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Camera', src: 'images/Camera.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Maps', src: 'images/Maps.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'YouTube', src: 'images/YouTube.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Calculator', src: 'images/Calculator.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Books', src: 'images/Books.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Browser', src: 'images/Browser.png',
+      url: 'data:text/html,<font color="blue">Hello' },
+    { label: 'Music', src: 'images/Music.png',
+      url: 'data:text/html,<font color="blue">Hello' }
+  ];
+
+  var icons = [];
+  // XXX this add 5 times the same set of icons
+  for (var i = 0; i < 5; i++)
+    for (var n = 0; n < fruits.length; ++n)
+      icons.push(fruits[n]);
+
+  var iconGrid = new IconGrid(document.getElementById("homeCanvas"),
+                              "images/background.png",
+                              120, 120, 0.2);
+  for (var n = 0; n < icons.length; ++n)
+    iconGrid.add(icons[n].src, icons[n].label, icons[n].url);
+
+  // XXX In the long term this is probably bad for battery
+  window.setInterval(updateClock, 60000);
+  updateClock();
+
+  document.getElementById('statusPadding').innerHTML =
+    kUseGL ? '(WebGL)' : '(2D canvas)';
+
+  WindowManager.start();
+}
+
+var WindowManager = {
+  start: function wm_start() {
+    window.addEventListener("appclose", this, true);
+  },
+  stop: function wm_stop() {},
+  handleEvent: function wm_handleEvent(evt) {
+    switch (evt.type) {
+      case "appclose":
+        var windows = document.getElementById('windows');
+        if (windows.childElementCount <= 1)
+          return;
+
+        var loadScreen = document.getElementById('loadAnimationScreen');
+        loadScreen.style.display = 'block';
+        loadScreen.classList.toggle('animateClosing');
+
+        // TODO when existing window will be checked, this should be
+        // point to the real window
+        var topWindow = windows.lastElementChild;
+        windows.removeChild(topWindow);
+
+        window.addEventListener(
+          'animationend',
+          function listener() {
+            loadScreen.className = '';
+            loadScreen.style.display = 'none';
+            window.removeEventListener('animationend', listener, false);
+
+            if (windows.childElementCount <= 1)
+              windows.setAttribute("hidden", "true");
+          },
+          false);
+        break;
+      default:
+        throw new Error("Unhandled event in WindowManager");
+        break;
+    }
+  }
+};
+
+// open the application referred to by |url| into a new window, or
+// bring its window to front if already open.
+function openApplication(url) {
+  // TODO
+  //var existingWindow = document.querySelector('#windows > ...');
+
+  var newWindow = document.createElement('iframe');
+  newWindow.className = 'appWindow';
+  newWindow.style.display = 'none';
+  // XXX need to decide whether to try to load this during animation
+  newWindow.src = url;
+
+  var windows = document.getElementById('windows');
+  windows.removeAttribute("hidden");
+  windows.appendChild(newWindow);
+
+  var loadScreen = document.getElementById('loadAnimationScreen');
+  loadScreen.classList.toggle('animateOpening');
+  loadScreen.style.display = 'block';
+
+  window.addEventListener(
+    'animationend',
+    function listener() {
+      loadScreen.classList.toggle('animateOpening');
+      loadScreen.style.display = 'none';
+      newWindow.style.display = 'block';
+      window.removeEventListener('animationend', listener, false);
+    },
+    false);
+}
+
+function updateClock() {
+  var now = new Date();
+  var str = now.getHours();
+  str += ':';
+  var mins = now.getMinutes();
+  if (mins < 10)
+    str += "0";
+  str += mins;
+  document.getElementById('statusClock').innerHTML = str;
+}
---
+++ scenegraph.js
@@ -0,0 +1,433 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+/*const*/var kUseGL = 1;
+/*const*/var kSnapToWholePixels = !kUseGL;
+
+function abort(why) { alert(why); throw why; }
+function assert(cond, msg) { if (!cond) abort(msg); }
+
+// Return the current time of the "animation clock", which ticks on
+// each frame drawn.
+function GetAnimationClockTime() {
+  return window.mozAnimationStartTime ||
+         window.webkitAnimationStartTime ||
+         window.animationStartTime;
+}
+
+function RequestAnimationFrame() {
+  if (window.mozRequestAnimationFrame)
+    window.mozRequestAnimationFrame();
+  else if (window.webkitRequestAnimationFrame)
+    window.webkitRequestAnimationFrame();
+  else if (window.requestAnimationFrame)
+    window.requestAnimationFrame();
+}
+
+var Physics = {
+  Linear: function(elapsed, start, current, target) {
+    return start + (target - start) * elapsed;
+  },
+  Spring: function(elapsed, start, current, target) {
+    return current + (target - current) * elapsed;
+  }
+}
+
+function Sprite(width, height) {
+  var canvas = document.createElement('canvas');
+  this.width = canvas.width = width;
+  this.height = canvas.height = height;
+  this.canvas = canvas;
+  this.setPosition(0, 0);
+  this.setScale(1);
+}
+
+Sprite.prototype = {
+  getContext2D: function() {
+    var ctx = this.canvas.getContext('2d');
+    // XXX it appears that canvases aren't translated into GL
+    // coordinates before uploading, unlike <img>s :/.  So hack
+    // here.  Need to figure out if that's a FF bug or actually
+    // spec'd like that.
+    if (kUseGL) {
+      ctx.translate(0, this.height);
+      ctx.scale(1, -1);
+    }
+    return ctx;
+  },
+  setPosition: function(targetX, targetY, duration, fn) {
+    RequestAnimationFrame();
+    if (duration && (this.x != targetX || this.y != targetY)) {
+      this.startX = this.x;
+      this.startY = this.y;
+      this.targetX = targetX;
+      this.targetY = targetY;
+      this.moveStart = GetAnimationClockTime();
+      this.moveStop = this.moveStart + duration;
+      this.moveFunction = fn || Physics.Linear;
+      return;
+    }
+    this.x = targetX;
+    this.y = targetY;
+    this.moveFuncton = null;
+  },
+  setScale: function(targetScale, duration, fn) {
+    RequestAnimationFrame();
+    if (duration && this.scale != targetScale) {
+      this.startScale = this.scale;
+      this.targetScale = targetScale;
+      this.scaleStart = GetAnimationClockTime();
+      this.scaleStop = this.scaleStart + duration;
+      this.scaleFunction = fn || Physics.Linear;
+      return;
+    }
+    this.scale = targetScale;
+    this.scaleFunction = null;
+  },
+  animate: function(now) {
+    function GetElapsed(start, stop, now) {
+      return (now < start || now > stop) ? 1 : ((now - start) / (stop - start));
+    }
+    if (this.moveFunction) {
+      var elapsed = GetElapsed(this.moveStart, this.moveStop, now);
+      this.x = this.moveFunction(elapsed, this.startX, this.x, this.targetX);
+      this.y = this.moveFunction(elapsed, this.startY, this.y, this.targetY);
+      if (elapsed == 1)
+        this.moveFunction = null;
+    }
+    if (this.scaleFunction) {
+      var elapsed = GetElapsed(this.scaleStart, this.scaleStop, now);
+      this.scale = this.scaleFunction(elapsed, this.startScale, this.scale, this.targetScale);
+      if (elapsed == 1)
+        this.scaleFunction = null;
+    }
+    return this.moveFunction || this.scaleFunction;
+  }
+}
+
+function SceneGraph(canvas) {
+  this.blitter =
+    kUseGL ? new SpriteBlitterGL(canvas) : new SpriteBlitter2D(canvas);
+  this.canvas = canvas;
+  this.sprites = [];
+  this.background = null;
+  this.x = 0;
+  this.y = 0;
+
+  var self = this;
+  window.addEventListener("MozBeforePaint", function(event) {
+      var now = GetAnimationClockTime();
+      // continue painting until we are run out of animations
+      if (self.animate(now))
+        RequestAnimationFrame();
+      self.draw();
+    }, false);
+}
+
+SceneGraph.prototype = {
+  // add a sprite to the scene graph
+  add: function(sprite) {
+    this.blitter.spriteAdded(sprite);
+    var sprites = this.sprites;
+    sprite.index = sprites.length;
+    sprites.push(sprite);
+  },
+  // remove a sprite from the scene graph
+  remove: function(sprite) {
+    this.sprites.splice(sprite.index, 1);
+    this.blitter.spriteRemoved(sprite);
+  },
+  // animate the scene graph, returning false if the animation is done
+  animate: function(now) {
+    function GetElapsed(start, stop, now) {
+      return (now < start || now > stop) ? 1 : ((now - start) / (stop - start));
+    }
+    var more = false;
+    if (this.scrollFunction) {
+      var elapsed = GetElapsed(this.scrollStart, this.scrollStop, now);
+      this.x = this.scrollFunction(elapsed, this.startX, this.x, this.targetX);
+      this.y = this.scrollFunction(elapsed, this.startY, this.y, this.targetY);
+      if (elapsed == 1)
+        this.scrollFunction = null;
+      else
+        more = true;
+    }
+    var sprites = this.sprites;
+    for (var n = 0; n < sprites.length; ++n) {
+      var sprite = sprites[n];
+      if (sprite.animate(now))
+        more = true;
+    }
+    return more;
+  },
+  draw: function() {
+    var x = this.x, y = this.y;
+    if (kSnapToWholePixels) {
+      x |= 0;
+      y |= 0;
+    }
+    this.blitter.draw(x, y, this.sprites);
+  },
+  // walk over all sprites in the scene
+  forAll: function(callback) {
+    var sprites = this.sprites;
+    for (var n = 0; n < sprites.length; ++n)
+      callback(sprites[n]);
+  },
+  forHit: function(x, y, callback) {
+    var sprites = this.sprites;
+    for (var n = 0; n < sprites.length; ++n) {
+      var sprite = sprites[n];
+      if (x >= sprite.x && x < sprite.x + sprite.width &&
+          y >= sprite.y && y < sprite.y + sprite.height) {
+        callback(sprite);
+        return;
+      }
+    }
+  },
+  setBackground: function(imageUrl) {
+    var bgImg = document.createElement('img');
+    bgImg.src = imageUrl;
+    bgImg.sceneGraph = this;
+    bgImg.onload = function() {
+      var sceneGraph = this.sceneGraph;
+      sceneGraph.background = this;
+      sceneGraph.blitter.backgroundChanged(this);
+      RequestAnimationFrame();
+    };
+  },
+  setViewportTopLeft: function(targetX, targetY, duration, fn) {
+    RequestAnimationFrame();
+    if (duration && (this.x != targetX || this.y != targetY)) {
+      this.startX = this.x;
+      this.startY = this.y;
+      this.targetX = targetX;
+      this.targetY = targetY;
+      this.scrollStart = GetAnimationClockTime();
+      this.scrollStop = this.scrollStart + duration;
+      this.scrollFunction = fn || Physics.Linear;
+      return;
+    }
+    this.x = targetX;
+    this.y = targetY;
+    this.scrollFuncton = null;
+  }
+}
+
+// fallback 2D canvas backend
+function SpriteBlitter2D(canvas) {
+  this.background = 'black';
+  this.canvas = canvas;
+  this.ctx = canvas.getContext('2d');
+}
+SpriteBlitter2D.prototype = {
+draw: function(x, y, sprites, background) {
+    var canvas = this.canvas;
+    var ctx = this.ctx;
+    var width = canvas.width;
+    var height = canvas.height;
+    ctx.fillStyle = this.background;
+    ctx.fillRect(0, 0, width, height);
+    for (var n = 0; n < sprites.length; ++n) {
+      var sprite = sprites[n];
+      var canvas = sprite.canvas;
+      if (canvas) {
+        var scale = sprite.scale;
+        ctx.drawImage(canvas, sprite.x - x, sprite.y - y, canvas.width * scale, canvas.height * scale);
+      }
+    }    
+  },
+  backgroundChanged: function(background) {
+    this.background = this.ctx.createPattern(background, 'repeat');
+  },
+  // nothing to do here
+  spriteAdded: function(sprite) {},
+  spriteRemoved: function(sprite) {}
+};
+
+// blt using WebGL.  This should be as fast or faster than using a 2D
+// drawing context when the browser engine is drawing with OpenGL (and
+// so doesn't need to read back from the WebGL context).
+var kVertexShader = [
+  'attribute vec4 aPosAndTexCoord;',
+  'uniform mat4 uProjection;',
+  'varying vec2 vTexCoord;',
+  '',
+  'void main(void) {',
+  '  vTexCoord = aPosAndTexCoord.zw;',
+  '  vec4 transformedPos = vec4(aPosAndTexCoord.x, aPosAndTexCoord.y, 0.0, 1.0);',
+  '  gl_Position = uProjection * transformedPos;',
+  '}'
+].join("\n");
+
+var kFragmentShader = [
+  '#ifdef GL_ES',
+  'precision highp float;',
+  '#endif',
+  '',
+  'varying vec2 vTexCoord;',
+  'uniform sampler2D uTexture;',
+  '',
+  'void main(void) {',
+  '  vec4 texColor;',
+  '  texColor = texture2D(uTexture, vTexCoord);',
+  '  gl_FragColor = texColor;',
+  '}'
+].join("\n");
+
+var kMaxTextureSize = 0;
+
+// NB: sprites really need a depth value since they can overlap.  As
+// it stands we should assume that they're drawn in an undefined
+// z-order (GL may actually specify that, not sure).
+function SpriteBlitterGL(canvas) {
+  this.canvas = canvas;
+  var gl =
+    this.gl = canvas.getContext('experimental-webgl');
+  this.maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+  this.posAndTexCoordArray = new Float32Array(4/*vertices*/ * 4/*elements per vertex*/);
+  this.posAndTexCoordBuffer = gl.createBuffer();
+  var program =
+    this.program = compileGLProgram(gl, kVertexShader, kFragmentShader);
+  this.projectionMatrix = new Float32Array(
+    [ 2.0,     0.0,   0.0,   0.0,
+      0.0,    -2.0,   0.0,   0.0,
+      0.0,     0.0,   1.0,   0.0,
+     -1.0,     1.0,   0.0,   1.0 ]);
+  this.viewportWidth = canvas.width;
+  this.viewportHeight = canvas.height;
+
+  gl.useProgram(program);
+
+  program.aPosAndTexCoord = gl.getAttribLocation(program, 'aPosAndTexCoord');
+  gl.enableVertexAttribArray(program.aPosAndTexCoord);
+
+  program.uProjection = gl.getUniformLocation(program, 'uProjection');
+  program.uTexture = gl.getUniformLocation(program, 'uTexture');
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+}
+SpriteBlitterGL.prototype = {
+  draw: function(x, y, sprites) {
+    var gl = this.gl;
+    var posAndTexCoordArray = this.posAndTexCoordArray;
+    var posAndTexCoordBuffer = this.posAndTexCoordBuffer;
+    var program = this.program;
+    var viewportWidth = this.viewportWidth, viewportHeight = this.viewportHeight;
+
+    gl.viewport(0, 0, viewportWidth, viewportHeight);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // XXX we need to enable blending to deal with overlapping
+    // sprites, but doing so causes the appearance of fonts to worsen.
+    // Need to investigate that.
+    gl.enable(gl.BLEND);
+    // "over" operator
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    gl.uniformMatrix4fv(program.uProjection, false, this.projectionMatrix);
+    gl.uniform1i(program.uTexture, 0);
+
+    var translateX = x;
+    var translateY = y;
+
+    for (var n = 0; n < sprites.length; ++n) {
+      var sprite = sprites[n];
+      var canvas = sprite.canvas;
+      var scale = sprite.scale;
+      var x = (sprite.x - translateX) / viewportWidth;
+      var y = (sprite.y - translateY) / viewportHeight;
+      var width = canvas.width * scale;
+      var height = canvas.height * scale;
+      var xmost = x + width / viewportWidth;
+      var ymost = y + height / viewportHeight;
+
+      var i = 0;
+      // bottom left
+      posAndTexCoordArray[i++] = x;
+      posAndTexCoordArray[i++] = ymost;
+      posAndTexCoordArray[i++] = 0;
+      posAndTexCoordArray[i++] = 0;
+      // bottom right
+      posAndTexCoordArray[i++] = xmost;
+      posAndTexCoordArray[i++] = ymost;
+      posAndTexCoordArray[i++] = 1;
+      posAndTexCoordArray[i++] = 0;
+      // top left
+      posAndTexCoordArray[i++] = x;
+      posAndTexCoordArray[i++] = y;
+      posAndTexCoordArray[i++] = 0;
+      posAndTexCoordArray[i++] = 1;
+      // top right
+      posAndTexCoordArray[i++] = xmost;
+      posAndTexCoordArray[i++] = y;
+      posAndTexCoordArray[i++] = 1;
+      posAndTexCoordArray[i++] = 1;
+
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, sprite.texture);
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, posAndTexCoordBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, posAndTexCoordArray, gl.STREAM_DRAW);
+      gl.vertexAttribPointer(program.aPosition, 4, gl.FLOAT, false, 0, 0);
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    }
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  },
+  backgroundChanged: function(background) {
+    // TODO
+  },
+  spriteAdded: function(sprite) {
+    if (('texture' in sprite) && sprite.texture !== null)
+      return;
+
+    var canvas = sprite.canvas;
+    assert(canvas.width <= this.maxTextureSize
+           && canvas.height <= this.maxTextureSize,
+           "Sprite canvas must be smaller than max texture dimension");
+    var gl = this.gl;
+    var texture = sprite.texture = gl.createTexture();
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE,
+                  canvas);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    // XXX mipmap if we start downscaling a lot
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+  },
+  spriteRemoved: function(sprite) {
+    gl.deleteTexture(sprite.texture);
+    sprite.texture = null;
+  }
+};
+
+function compileGLProgram(gl, vxShader, pixShader) {
+  function compileShader(type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    assert(gl.getShaderParameter(shader, gl.COMPILE_STATUS),
+           "Compile error for "+ type +": "+ gl.getShaderInfoLog(shader));
+    return shader;
+  }
+
+  var p = gl.createProgram();
+  var vs = compileShader(gl.VERTEX_SHADER, vxShader);
+  var fs = compileShader(gl.FRAGMENT_SHADER, pixShader);
+  gl.attachShader(p, vs);  gl.deleteShader(vs);
+  gl.attachShader(p, fs);  gl.deleteShader(fs);
+  gl.linkProgram(p);
+
+  if (!gl.getProgramParameter(p, gl.LINK_STATUS))
+    // FIXME fall back on 2d
+    abort("Failed to compile shaders.");
+
+  return p;
+}
