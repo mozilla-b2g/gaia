16c233cad6a0f628f51baf81feb7452f89912e24
Ben Francis <ben@tola.me.uk>
2011-11-07 18:49:51 +0000

    Hacky first implementation of back button, only works with user-entered URLs due to same-origin policy and comes with yummy global variables

--- browser/browser.js
+++ browser/browser.js
@@ -1,14 +1,37 @@
 window.onload = function() {
-  var go = document.getElementById("browser-go-button");
-  go.addEventListener("click", Browser.navigate, false);
-
+  urlHistory = [];
+  currentURL = -1;
+  Browser.init();
 }
 
 var Browser = {
+  init: function() {
+    var go = document.getElementById("browser-go-button");
+    go.addEventListener("click", Browser.navigate, false);
+    var back = document.getElementById("browser-back-button");
+    back.addEventListener("click", Browser.back, false);
+    Browser.navigate();
+  },
+
   navigate: function(event) {
+    if(event) {event.preventDefault(); }
     var url = document.getElementById("browser-url").value;
     var iframe = document.getElementById("browser-iframe");
+    if(url && url !== urlHistory[currentURL]) {
+      urlHistory.push(url);
+      currentURL++;
+    }
     iframe.setAttribute("src", url);
-    event.preventDefault();
+  },
+
+  back: function() {
+    if (currentURL == 0) {
+      return;
+    } else {
+      currentURL--;
+      document.getElementById("browser-url").value = urlHistory[currentURL];
+      Browser.navigate();
+    }
   }
-}
+  
+};
