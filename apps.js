141472ed763377524b82364c647a4f0fbfbc165d
Vivien Nicolas <21@vingtetun.org>
2011-11-23 02:21:48 +0100

    Emulate touch events in the web pages

--- apps.js
+++ apps.js
@@ -1240,3 +1240,199 @@ var ContactsAPI = {
 
 Apps.init();
 
+
+(function touchEventHandler() {
+  var debugging = false;
+  function debug(str) {
+    if (debugging)
+      dump(str);
+  };
+  var gIgnoreEvents = false;
+
+  var contextMenuTimeout = 0;
+
+  // During a 'touchstart' and the first 'touchmove' mouse events can be
+  // prevented for the current touch sequence.
+  var canPreventMouseEvents = false;
+
+  // Used to track the first mousemove and to cancel click dispatc if it's not
+  // true.
+  var isNewTouchAction = false;
+
+  // If this is set to true all mouse events will be cancelled by calling
+  // both evt.preventDefault() and evt.stopPropagation().
+  // This will not prevent a contextmenu event to be fired.
+  // This can be turned on if canPreventMouseEvents is true and the consumer
+  // application call evt.preventDefault();
+  var preventMouseEvents = false;
+
+  var TouchEventHandler = {
+    events: ['mousedown', 'mousemove', 'mouseup', 'click', 'unload'],
+    start: function teh_start() {
+      this.events.forEach((function(evt) {
+        window.addEventListener(evt, this, true);
+      }).bind(this));
+    },
+    stop: function teh_stop() {
+      this.events.forEach((function(evt) {
+        window.removeEventListener(evt, this, true);
+      }).bind(this));
+    },
+    handleEvent: function teh_handleEvent(evt) {
+      // Handle left button only and prevents re-entering the event loop
+      // for self disptached events.
+      if (evt.button || gIgnoreEvents)
+        return;
+
+      var eventTarget = this.target;
+      var type = '';
+      switch (evt.type) {
+        case 'mousedown':
+          debug('mousedown:');
+
+          this.target = evt.target;
+          this.timestamp = evt.timeStamp;
+          evt.target.setCapture(false);
+
+          preventMouseEvents = false;
+          canPreventMouseEvents = true;
+          isNewTouchAction = true;
+
+          contextMenuTimeout =
+            this.sendContextMenu(evt.target, evt.pageX, evt.pageY, 2000);
+          this.startX = evt.pageX;
+          this.startY = evt.pageY;
+          type = 'touchstart';
+          break;
+
+        case 'mousemove':
+          if (!eventTarget)
+            return;
+
+          // On device a mousemove event if fired right after the mousedown
+          // because of the size of the finger, so var's ignore what happens
+          // below 5ms
+          if (evt.timeStamp - this.timestamp < 30)
+            break;
+
+          if (isNewTouchAction) {
+            canPreventMouseEvents = true;
+            isNewTouchAction = false;
+          }
+
+          if (Math.abs(this.startX - evt.pageX) > 15 ||
+              Math.abs(this.startY - evt.pageY) > 15)
+            window.clearTimeout(contextMenuTimeout);
+          type = 'touchmove';
+          break;
+
+        case 'mouseup':
+          if (!eventTarget)
+            return;
+          debug('mouseup:');
+
+          window.clearTimeout(contextMenuTimeout);
+          eventTarget.ownerDocument.releaseCapture();
+          this.target = null;
+          type = 'touchend';
+          break;
+
+        case 'unload':
+          window.clearTimeout(contextMenuTimeout);
+          eventTarget.ownerDocument.releaseCapture();
+          this.target = null;
+          TouchEventHandler.stop();
+          return;
+
+        case 'click':
+          if (!isNewTouchAction) {
+            debug('click: cancel\n');
+            evt.preventDefault();
+            evt.stopPropagation();
+          } else {
+            // Mouse events has been cancelled so dispatch a sequence
+            // of events to where touchend has been fired
+            if (preventMouseEvents) {
+            var target = evt.target;
+            try{
+              gIgnoreEvents = true;
+              this.fireMouseEvent('mousemove', target, evt.pageX, evt.pageY);
+              this.fireMouseEvent('mousedown', target, evt.pageX, evt.pageY);
+              this.fireMouseEvent('mouseup', target, evt.pageX, evt.pageY);
+              gIgnoreEvents = false;
+            } catch(e) {
+              alert(e);
+            }
+            }
+            debug('click: fire\n');
+          }
+          return;
+      }
+
+      var target = eventTarget || this.target;
+      if (target && type) {
+        var touchEvent = this.sendTouchEvent(evt, target, type);
+        if (touchEvent.getPreventDefault() && canPreventMouseEvents)
+          preventMouseEvents = true;
+      }
+
+      if (preventMouseEvents) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        if (type != 'touchmove')
+          debug('cancelled (fire ' + type + ')\n');
+      }
+    },
+    fireMouseEvent: function teh_fireMouseEvent(type, target, x, y) {
+      debug(type + ': fire\n');
+      var doc = target.ownerDocument;
+      var evt = doc.createEvent('MouseEvent');
+      evt.initMouseEvent(type, true, true, doc.defaultView,
+                         0, x, y, x, y, false, false, false, false,
+                         0, null);
+      target.dispatchEvent(evt);
+    },
+    sendContextMenu: function teh_sendContextMenu(target, x, y, delay) {
+      var doc = target.ownerDocument;
+      var evt = doc.createEvent('MouseEvent');
+      evt.initMouseEvent('contextmenu', true, true, doc.defaultView,
+                         0, x, y, x, y, false, false, false, false,
+                         0, null);
+
+      var timeout = window.setTimeout((function contextMenu() {
+        debug('fire context-menu');
+
+        target.dispatchEvent(evt);
+        if (!evt.getPreventDefault())
+          return;
+
+        doc.releaseCapture();
+        this.target = null;
+
+        isNewTouchAction = false;
+      }).bind(this), delay);
+      return timeout;
+    },
+    sendTouchEvent: function teh_sendTouchEvent(evt, target, name) {
+      var touchEvent = document.createEvent('touchevent');
+      var point = document.createTouch(window, target, 0,
+                                     evt.pageX, evt.pageY,
+                                     evt.screenX, evt.screenY,
+                                     evt.clientX, evt.clientY,
+                                     1, 1, 0, 0);
+      var touches = document.createTouchList(point);
+      var targetTouches = touches;
+      var changedTouches = touches;
+      touchEvent.initTouchEvent(name, true, true, window, 0,
+                                false, false, false, false,
+                                touches, targetTouches, changedTouches);
+      target.dispatchEvent(touchEvent);
+      return touchEvent;
+    }
+  };
+
+  if (!window.matchMedia('(-moz-touch-enabled)'))
+    TouchEventHandler.start();
+})();
+
