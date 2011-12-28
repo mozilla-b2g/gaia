047ac475591c7d2bb71186a683924e0725afc336
Vivien Nicolas <21@vingtetun.org>
2011-12-28 17:44:17 +0100

    ServerGaia.js code clean up

--- tools/debug/server/components/ServerGaia.js
+++ tools/debug/server/components/ServerGaia.js
@@ -8,13 +8,19 @@ const Cu = Components.utils;
 const CC = Components.Constructor;
 const CID = Components.ID;
 
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/FileUtils.jsm");
-Cu.import("resource://gre/modules/NetUtil.jsm");
+const ServerSocket = CC('@mozilla.org/network/server-socket;1',
+                        'nsIServerSocket',
+                        'init');
 
-const SERVER_CONTRACTID = "@mozilla.org/server/gaia;1";
-const SERVER_CID = CID("{cff21d8a-3140-11e1-9a20-af1d63323c56}");
+const BinaryInputStream = CC('@mozilla.org/binaryinputstream;1',
+                             'nsIBinaryInputStream',
+                             'setInputStream');
+
+Cu.import('resource://gre/modules/XPCOMUtils.jsm');
+Cu.import('resource://gre/modules/Services.jsm');
+
+const SERVER_CONTRACTID = '@mozilla.org/server/gaia;1';
+const SERVER_CID = CID('{cff21d8a-3140-11e1-9a20-af1d63323c56}');
 
 const ServerFactory = {
   _instance: null,
@@ -26,39 +32,40 @@ const ServerFactory = {
 };
 
 
+var debug = false;
+function log(str) {
+  if (!debug)
+    return;
+
+  dump('ServerGaia: ' + str + '\n');
+}
+
 // ServerGaia Impl
-const ServerSocket = CC("@mozilla.org/network/server-socket;1",
-                        "nsIServerSocket",
-                        "init");
 const SOCKET_PORT = 6789;
 
 function ServerGaia() {
+  log('new server');
 }
 
 ServerGaia.prototype = {
   start: function sg_start() {
-    dump('server: starting socket: ');
     let socket = this._socket = new ServerSocket(SOCKET_PORT, false, -1);
     socket.asyncListen(this);
-    dump('started\n');
   },
 
   stop: function sg_stop() {
-    dump('server: closing clients connections: ');
-    for each (let client in this._connections)
+    this._connections.forEach(function(client) {
       client.close();
-    dump('closed\n');
+    });
 
-    dump('server: closing socket: ');
     this._socket.close();
-    dump('closed\n');
-
   },
 
   // nsIServerSocketListener impl
   _connections: [],
   onSocketAccepted: function sg_onSocketAccepted(sock, transport) {
-    dump('server: got client connection\n');
+    log('new client connection');
+
     let input = transport.openInputStream(0, 0, 0);
     let output = transport.openOutputStream(0, 0, 0);
     let client = new SocksClient(this, input, output);
@@ -72,11 +79,17 @@ ServerGaia.prototype = {
   observe: function sg_observe(subject, topic, data) {
     switch (topic) {
       case 'profile-after-change':
+        log('starting socket...');
         this.start();
+        log('started');
+
         Services.obs.addObserver(this, 'quit-application', false);
         break;
       case 'quit-application':
+        log('stopping server...');
         this.stop();
+        log('stopped');
+
         Services.obs.removeObserver(this, 'quit-application');
         break;
       default:
@@ -88,35 +101,50 @@ ServerGaia.prototype = {
   _xpcom_factory: ServerFactory,
   QueryInterface: XPCOMUtils.generateQI([
     Ci.nsIObserver, Ci.nsIServerSocketListener
-  ]),
+  ])
 };
 
 
 // SocksClient Impl
 function stringToSha1(str) {
-  let hasher = Cc["@mozilla.org/security/hash;1"]
+  let hasher = Cc['@mozilla.org/security/hash;1']
                  .createInstance(Ci.nsICryptoHash);
   hasher.init(hasher.SHA1);
 
-  let converter = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
+  let converter = Cc['@mozilla.org/intl/scriptableunicodeconverter']
                     .createInstance(Ci.nsIScriptableUnicodeConverter);
-  converter.charset = "UTF-8";
+  converter.charset = 'UTF-8';
 
   let data = converter.convertToByteArray(str, {});
   hasher.update(data, data.length);
   let bytes = hasher.finish(false);
 
-  let hex = "";
+  let hex = '';
   for (let i = 0; i < bytes.length; i++)
-    hex += ("0" + bytes[i].charCodeAt().toString(16)).slice(-2);
+    hex += ('0' + bytes[i].charCodeAt().toString(16)).slice(-2);
   return hex;
 }
 
 const STATE_CONNECTING = 0;
 const STATE_CONNECTED = 1;
 
-const currentThread = Cc["@mozilla.org/thread-manager;1"]
-                        .getService().currentThread;
+const kControlFrameMask = 0x8;
+const kMaskBit = 0x80;
+const kFinalFragBit = 0x80;
+
+// Controls frames
+const kClose = 0x8;
+const kPing = 0x9;
+const kPong = 0xA;
+
+// Non-control frames
+const kContinuation = 0x0;
+const kText = 0x1;
+const kBinary = 0x2;
+
+const currentThread = Cc['@mozilla.org/thread-manager;1']
+                        .getService()
+                        .currentThread;
 
 function SocksClient(server, input, output) {
   this._input = input;
@@ -126,110 +154,110 @@ function SocksClient(server, input, output) {
   this.waitRead();
 }
 
-const BinaryInputStream = CC("@mozilla.org/binaryinputstream;1",
-                             "nsIBinaryInputStream",
-                             "setInputStream");
-
 SocksClient.prototype = {
+  _parseHTTPHeaders: function sc_parseHTTPHeaders(str) {
+    let headers = {};
+    let fields = str.split('\r\n');
+    for each(let field in fields) {
+      let name = field.split(': ')[0];
+      let value = field.split(': ')[1];
+      headers[name] = value;
+    }
+    return headers;
+  },
+
+  _acceptConnection: function sc_acceptConnection(securityKey) {
+    let reply = securityKey + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+    let sha1 = stringToSha1(reply);
+
+    let str = '';
+    for (var i = 0; i < sha1.length; i += 2)
+      str += String.fromCharCode(parseInt(sha1.substr(i, 2), 16));
+
+    log('server: key:  ' + securityKey + '\n' +
+        '        sha1: ' + sha1 + '\n' +
+        '        b64:  ' + btoa(str) + '\n');
+
+    let response = 'HTTP/1.1 101 Switching Protocols\r\n' +
+                   'Upgrade: websocket\r\n' +
+                   'Connection: Upgrade\r\n' +
+                   'Sec-WebSocket-Accept: ' + btoa(str) + '\r\n' +
+                   '\r\n';
+    this.write(response);
+  },
+
+  _parseFrame: function sc_parseFrame(bytes) {
+    let type = bytes[0];
+
+    if (type & kControlFrameMask) {
+      log('receive a control frame. Aborting.');
+      return;
+    }
+
+    if (type & kBinary) {
+      log('receive a binary frame. Aborting.');
+      return;
+    } else if (type & kContinuation) {
+      log('receive a continuation frame. Aborting.');
+      return;
+    }
+
+    if (type & kText) {
+      let headerSize = 0;
+
+      let length = bytes[1] & ~kMaskBit;
+      if (length < 126) {
+        headerSize = 6;
+      } else if (lenght == 126) {
+        log('!!frame length = 126. Length calculation need to be implemented');
+        headerSize = 8;
+      } else if (length == 127) {
+        log('!!frame length = 127. Length calculation need to be implemented');
+        headerSize = 14;
+      }
+
+      let masks = bytes.slice(headerSize - 4, headerSize);
+      log('masks: ' + masks);
+
+      let decoded = [];
+      for (let i = headerSize, j = 0; i < bytes.length; i++, j++)
+        decoded[j] = String.fromCharCode(bytes[i] ^ masks[j % 4]);
+
+      log('server: text: ' + decoded + '(' + length + ')\n');
+    }
+  },
+
   onInputStreamReady: function sc_onInputStreamReady(input) {
     let len = input.available();
     if (len == 0) {
-      dump('server: client close\n');
+      log('client closed');
       return;
     }
-    var bin = new BinaryInputStream(input);
-    data = bin.readByteArray(len);
-    dump('bytes: \n');
-    dump(data);
-    dump('\n');
+
+    let binaryOutputStream = new BinaryInputStream(input);
+    let bytes = binaryOutputStream.readByteArray(len);
 
     switch (this.state) {
       case STATE_CONNECTING:
-        let headers = {};
+        try {
+          let data = String.fromCharCode.apply(null, bytes);
+          let headers = this._parseHTTPHeaders(data);
 
-        data = String.fromCharCode.apply(null, data);
-        let fields = data.split('\r\n');
-        for each (let field in fields) {
-          let [name, value] = field.split(': ');
-          headers[name] = value;
+          let securityKey = headers['Sec-WebSocket-Key'];
+          this._acceptConnection(securityKey);
+
+          this.state = STATE_CONNECTED;
+        } catch (e) {
+          dump('Error during client connection: ' + e + '\n');
         }
-
-        let magicKey = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-        let key = headers['Sec-WebSocket-Key'];
-        let reply = key + magicKey;
-        let sha1 = stringToSha1(reply);
-
-        let str = '';
-        for (var i = 0; i < sha1.length; i+=2)
-          str += String.fromCharCode(parseInt(sha1.substr(i, 2), 16));
-
-        dump('server: key: ' + key + '\n' +
-             '        sha1: ' + sha1 + '\n' +
-             '        b64:  ' + btoa(str) + '\n');
-
-        let response = 'HTTP/1.1 101 Switching Protocols\r\n' +
-                       'Upgrade: websocket\r\n' +
-                       'Connection: Upgrade\r\n' +
-                       'Sec-WebSocket-Accept: ' + btoa(str) + '\r\n' +
-                       '\r\n';
-        this.write(response);
-        this.state = STATE_CONNECTED;
         break;
-      default:
-        let frameType = data[0];
 
-        const kControlFrameMask = 0x8;
-        const kMaskBit = 0x80;
-        const kFinalFragBit = 0x80;
-
-        if (frameType & kControlFrameMask) {
-          // Controls frames
-          const kClose = 0x8;
-          const kPing = 0x9;
-          const kPong = 0xA;
-
-          dump('server: Got a control frame....\n');
-          return;
+      case STATE_CONNECTED:
+        try {
+          this._parseFrame(bytes);
+        } catch (e) {
+          dump('Error while parsing frame: ' + e + '\n');
         }
-
-        dump('server: Got a regular frame....\n');
-
-        // Non-control frames
-        const kContinuation = 0x0;
-        const kText = 0x1;
-        const kBinary = 0x2;
-
-        if (frameType & kContinuation) {
-          dump('continuation\n');
-        } else if (frameType & kText) {
-          let length = data[1] & ~kMaskBit;
-          let headerSize = 0;
-          let mask = '';
-          if (length < 126) {
-            headerSize = 6;
-            let decoded = [];
-            let masks = data.slice(2, 6);
-            dump('server: ' + masks + '\n');
-            for (let i = headerSize, j = 0; i < data.length; i++, j++)
-              decoded[j] = String.fromCharCode(data[i] ^ masks[j % 4]);
-
-            dump('server: text: ' + decoded + '(' + length + ')\n');
-
-            //data = String.fromCharCode.apply(null, data);
-            this.write('toto');
-          } else if (lenght == 126) {
-            headerSize = 8;
-
-            // XXX not supported yet
-          } else if (length == 127) {
-            headerSize = 14;
-
-            // XXX not supported yet
-          }
-        } else if (frameType & kBinary) {
-          dump('binary\n');
-        }
-  
         break;
     }
 
@@ -237,8 +265,6 @@ SocksClient.prototype = {
   },
 
   onOutputStreamReady: function sc_onOutputStreamReady(output) {
-    dump('server: output stream ready!\n');
-
     let buffer = this._outputBuffer;
     this._outputBuffer = '';
 
@@ -262,13 +288,16 @@ SocksClient.prototype = {
 
   _outputBuffer: '',
   write: function(str) {
+    // If the socket is connected, the server needs to reply to the client
+    // using the websocket framing protocol.
+    // The following add a the minimum required.
     if (this.state == STATE_CONNECTED) {
-      str = String.fromCharCode(129) + 
+      str = String.fromCharCode(kFinalFragBit | kText) +
             String.fromCharCode(str.length) +
             str;
-    } 
+    }
 
-    dump('sending....' + str + '\n');
+    log('sending: ' + str);
     this._outputBuffer += str;
     this.waitWrite();
   },
