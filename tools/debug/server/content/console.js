c1b6426a46c133c4f6de49abfc18cc46c88989fb
Vivien Nicolas <21@vingtetun.org>
2011-12-30 14:44:59 +0100

    Create a hook to the input/output of jsterm

--- tools/debug/server/content/console.js
+++ tools/debug/server/content/console.js
@@ -1,30 +1,173 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
-const Gaia = {
-  startup: function () {
+const Cu = Components.utils;
+
+const RemoteConsole = {
+  init: function rc_init() {
     try {
-      Components.utils.import("resource:///modules/HUDService.jsm");
-      var xulWindow = HUDService.currentContext();
+      Cu.import('resource:///modules/HUDService.jsm');
+      HUDService.wakeup();
 
-      // These objects are what and where to attach the web console
-      var browser = xulWindow.gBrowser;
-      var linkedBrowser = browser.selectedTab.linkedBrowser;
-      var ownerDocument = browser.selectedTab.ownerDocument;
+      // XXX should use a properties
+      let topWindow = HUDService.currentContext();
+      topWindow.document.title = 'Remote Console';
 
-      // XXX In the future linkedBrowser should be a proxy to be
-      // able to autocomplete from the content of the remote page
-      let context = {
-        linkedBrowser: linkedBrowser,
-        ownerDocument: ownerDocument,
-      };
-      HUDService.activateHUDForContext(context);
+      let browser = topWindow.gBrowser.selectedBrowser;
+      let notificationBoxId = browser.parentNode.parentNode.id;
+      HUDService.registerActiveContext(notificationBoxId);
+      HUDService.windowInitializer(window);
+
+      let hudId = 'hud_' + notificationBoxId;
+      HUDService.disableAnimation(hudId);
 
       // Ensure the hud takes all the window available height
-      let tabId = browser.getNotificationBox(linkedBrowser).id;
-      let hud = ownerDocument.getElementById('hud_' + tabId);
-      hud.style.height = xulWindow.innerHeight + 'px';
+      let hud = browser.ownerDocument.getElementById(hudId);
+      topWindow.addEventListener('resize', function hudResizeHandler() {
+        hud.style.height = topWindow.innerHeight + 'px';
+      });
+
+      let hudRef = HUDService.getHudReferenceById(hudId);
+      let jsterm = hudRef.jsterm;
+
+      // XXX implement the communication over websocket
+      // rewire the input autocompletion
+      jsterm.propertyProvider = function(scope, inputValue) {
+        return {
+          matchProp: inputValue,
+          matches: ['argh']
+        };
+      };
+
+      // XXX implement the communication over websocket
+      // rewire the output evaluation
+      jsterm.evalInSandbox = function(str) {
+        if (str.trim() === 'help' || str.trim() === '?')
+          str = 'help()';
+      
+        let result = 'urgh!';
+        return result;
+      };
     } catch (e) {
       dump(e);
     }
   }
 };
+
+function handlerMaker(obj) {
+  return {
+    // Fundamental traps
+    getOwnPropertyDescriptor: function(name) {
+      dump('getOwnPropertyDescriptor: ' + name + '\n');
+
+      let desc = Object.getOwnPropertyDescriptor(obj, name);
+
+      // a trapping proxy's properties must always be configurable
+      if (desc !== undefined)
+        desc.configurable = true;
+      return desc;
+    },
+
+    getPropertyDescriptor: function(name) {
+      dump('getPropertyDescriptor: ' + name + '\n');
+
+      let index = ~~name;
+      if (index == name && index > 1) {
+        return {
+          get: function() {
+            let val = this[index - 1] + this[index - 2];
+            Object.defineProperty(this, index, {
+              value: val,
+              enumerable: true,
+              writable: true,
+              configurable: true
+            });
+            return val;
+          }
+        }
+      }
+      return Object.getOwnPropertyDescriptor(Object.prototype, name);
+    },
+
+    getOwnPropertyNames: function() {
+      dump('getOwnPropertyNames\n');
+
+      return Object.getOwnPropertyNames(obj);
+    },
+
+    getPropertyNames: function() {
+      dump('getPropertyNames\n');
+
+      return Object.getPropertyNames(obj); // not in ES5
+    },
+
+    defineProperty: function(name, desc) {
+      dump('defineProperty: ' + name + ' = ' + desc + '\n');
+
+      Object.defineProperty(obj, name, desc);
+    },
+
+    delete: function(name) {
+      dump('delete ' + name + '\n');
+
+      return delete obj[name];
+    },
+
+    fix: function() {
+      dump('fix\n');
+
+      if (Object.isFrozen(obj)) {
+        return Object.getOwnPropertyNames(obj).map(function(name) {
+          return Object.getOwnPropertyDescriptor(obj, name);
+        });
+      }
+
+      // As long as obj is not frozen, the proxy won't allow itself to be fixed
+      return undefined; // will cause a TypeError to be thrown
+    },
+
+    // derived traps
+    has: function(name) {
+      dump('has: ' + name + '\n');
+      return name in obj;
+    },
+
+    hasOwn: function(name) {
+      dump('hasOwn: ' + name + '\n');
+
+      return Object.prototype.hasOwnProperty.call(obj, name);
+    },
+
+    get: function(receiver, name) {
+      dump('get: ' + name + '\n');
+
+      let object = obj[name];
+      if (object)
+        return Proxy.create(handlerMaker(object));
+      return object;
+    },
+
+    set: function(receiver, name, val) {
+      dump('set: ' + name + ' = ' + val + '\n');
+
+      obj[name] = val;
+      return true;
+    }, // bad behavior when set fails in non-strict mode
+
+    enumerate: function() {
+      dump('enumerate\n');
+
+      let result = [];
+      for (name in obj)
+        result.push(name);
+      return result;
+    },
+
+    keys: function() {
+      dump('keys\n');
+
+      return Object.keys(obj);
+    }
+  };
+}
 
