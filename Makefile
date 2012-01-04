238b5b463991f6b10bc14148235c8f1d207b4b50
Vivien Nicolas <21@vingtetun.org>
2012-01-04 20:32:56 +0100

    Make some changes that let us runs browser-chrome test

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-INJECTED_GAIA = "$(MOZ_OBJDIR)/_tests/testing/mochitest/tests/gaia"
+
+B2G_HOMESCREEN=file://${GAIA}/homescreen.html
+INJECTED_GAIA = "$(MOZ_OBJDIR)/_tests/testing/mochitest/browser/gaia"
+
 mochitest:
 	test -L $(INJECTED_GAIA) || ln -s $(GAIA) $(INJECTED_GAIA)
-	TEST_PATH=gaia/tests/ make -C $(MOZ_OBJDIR) mochitest-plain
+	TEST_PATH=gaia/tests/ make -C $(MOZ_OBJDIR) mochitest-browser-chrome EXTRA_TEST_ARGS=--browser-arg=""
---
+++ tests/dialer/browser_contact_intent.js
@@ -0,0 +1,36 @@
+    
+const kDefaultWait = 2000;
+// Wait for a condition and call a supplied callback if condition is met within
+// alloted time. If condition is not met, cause a hard failure, stopping the test.
+function waitFor(callback, test, timeout) {
+  if (test()) {
+    callback();
+    return;
+  }
+
+  timeout = timeout || Date.now();
+  if (Date.now() - timeout > kDefaultWait)
+    throw "waitFor timeout";
+  setTimeout(waitFor, 50, callback, test, timeout);
+};
+
+function test() {
+  waitForExplicitFinish();
+
+  function testDialerAndFinish() {
+    let contentWindow = shell.home.contentWindow.wrappedJSObject;
+    var AppManager = contentWindow.Gaia.AppManager;
+    
+    // TODO Do a test here!
+    todo(false);
+    finish();
+  }
+
+  waitFor(testDialerAndFinish, function () {
+    let contentWindow = shell.home.contentWindow.wrappedJSObject;
+    return 'Gaia' in contentWindow;
+  });
+}
+ 
+test();
+
