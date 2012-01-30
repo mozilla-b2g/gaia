bf2d9b35014f19fc91d0cb535ca21e4ffab204bf
Etienne Segonzac <etienne@segonzac.info>
2012-01-30 20:32:08 +0100

    Hardening the test (some issue where raised with the hacky setTimeout removed)

--- tests/browser_sms_new.js
+++ tests/browser_sms_new.js
@@ -1,39 +1,49 @@
 
 function test() {
+  waitForExplicitFinish();
+
   appTest(function(appManager) {
     var smsFrame = appManager.launch('../sms/sms.html');
     waitFor(function() {
       let document = smsFrame.contentWindow.document;
 
-      var convCount = document.getElementById('messages').children.length;
-
-      var newButton = document.querySelector(".message[data-num='*']");
-      EventUtils.sendMouseEvent({type: 'click'}, newButton);
-
-      var conversationView = document.getElementById('conversationView');
-      var contactField = document.getElementById('contact');
-      var textField = document.getElementById('text');
-      var sendButton = document.getElementById('send');
-
-      ok(!conversationView.hidden, 'Conversation displayed');
-      ok(contactField.value.length == 0, 'To: field empty');
-
-      contactField.value = '123';
-      textField.value = 'Hello world.';
-      EventUtils.sendMouseEvent({type: 'click'}, sendButton);
-
-      var throbber = document.getElementById('throbber');
-      ok(!throbber.hidden, 'Throbber displayed');
-
       waitFor(function() {
-        ok(throbber.hidden, 'Throbber hidden');
-        ok(conversationView.hidden, 'Conversation hidden');
-        finish();
-      }, function() {
-        return (document.getElementById('messages').children.length ==
-                (convCount + 1));
-      });
+        var convCount = document.getElementById('messages').children.length;
 
+        var conversationView = document.getElementById('conversationView');
+        var contactField = document.getElementById('contact');
+        var textField = document.getElementById('text');
+        var sendButton = document.getElementById('send');
+        var newButton = document.querySelector(".message[data-num='*']");
+        EventUtils.sendMouseEvent({type: 'click'}, newButton);
+
+
+        conversationView.addEventListener('transitionend', function trWait() {
+          conversationView.removeEventListener('transitionend', trWait);
+          ok(!conversationView.hidden, 'Conversation displayed');
+          ok(contactField.value.length == 0, 'To: field empty');
+
+          contactField.value = '123';
+          textField.value = 'Hello world.';
+          EventUtils.sendMouseEvent({type: 'click'}, sendButton);
+
+          var throbber = document.getElementById('throbber');
+          ok(!throbber.hidden, 'Throbber displayed');
+
+          waitFor(function() {
+            ok(throbber.hidden, 'Throbber hidden');
+            ok(conversationView.hidden, 'Conversation hidden');
+
+            finish();
+          }, function() {
+            return (document.getElementById('messages').children.length ==
+                    (convCount + 1));
+          });
+        });
+      }, function() {
+        let newButton = document.querySelector(".message[data-num='*']");
+        return (newButton != null);
+      });
     }, function() {
       let smsWindow = smsFrame.contentWindow;
       return 'MessageView' in smsWindow;
