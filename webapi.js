6c2462757a0fca54e7960aa3d84a42bc38656f02
Andreas Gal <andreas.gal@gmail.com>
2012-02-15 02:03:36 -0800

    add onerror support for MozSettings emulation and remove hack from keyboard.js

--- webapi.js
+++ webapi.js
@@ -247,20 +247,34 @@
   navigator.mozSettings = {
     get: function(key) {
       var onsuccess = [];
+      var onerror = [];
       var request = {
         addEventListener: function(name, fn) {
-          if (name === "success")
+          if (name === 'success')
             onsuccess.push(fn);
+          if (name === 'error')
+            onerror.push(fn);
         },
         set onsuccess(fn) {
           onsuccess.push(fn);
         },
+        set onerror(fn) {
+          onerror.push(fn);
+        }
       };
       setImmediate(function() {
-        request.result = {
-          key: key,
-          value: localStorage.getItem(prefix + key)
-        };
+        try {
+          request.result = {
+            key: key,
+            value: localStorage.getItem(prefix + key)
+          };
+        } catch (e) {
+          while (onerror.length > 0) {
+            var fn = onerror.shift();
+            fn();
+          }
+          return;
+        }
         while (onsuccess.length > 0) {
           var fn = onsuccess.shift();
           fn();
