70887c0b2a33682897443c6a1d2ac09b3d4b8cc9
David Flanagan <dflanagan@mozilla.com>
2012-02-22 22:52:09 -0800

    add an intro CSS animation to Penguin Pop to hide the blackscreen while loading

---
+++ apps/penguinpop/js/penguinpop.js
@@ -0,0 +1,31 @@
+// When we get an onload event for this main document we set the src
+// property of the iframe to start loading the game.  Then we also
+// set some CSS styles that will begin CSS transitions that will
+// fill the blackscreen time while the game loads
+
+window.onload = function() {
+  var gameframe = document.getElementById("gameframe");
+  var splash = document.getElementById("splash");
+
+  // Start a CSS animation
+  splash.style.left = "180px";
+  splash.addEventListener("transitionend", function(e) {
+    if (e.propertyName === "left") {
+      splash.style.MozTransform = "scale(4)";
+      splash.style.opacity = 0;
+    }
+    else if (e.propertyName === "-moz-transform") {
+      splash.parentNode.removeChild(splash);
+    }
+  });
+
+  // Load the game.
+  gameframe.src = 'http://goosypets.com/html5games/whac/';
+
+  gameframe.onload = function() { 
+    // Make it opaque when loaded, hiding the splash icon
+    gameframe.style.opacity = 1;
+  };
+
+};
+
