a26193c43b62590736c8af4d50343ba9aac0f83e
David Flanagan <dflanagan@mozilla.com>
2012-02-24 00:16:49 -0800

    add an actual clock to the clock app

--- apps/clock/js/clock.js
+++ apps/clock/js/clock.js
@@ -24,8 +24,28 @@ function choiceChanged(target) {
   view.hidden = false;
 }
 
+function updateTime() { // Update the SVG clock graphic to show current time
+    var now = new Date();                       // Current time
+    var min = now.getMinutes();                 // Minutes
+    var hour = (now.getHours() % 12) + min/60;  // Fractional hours
+    var minangle = min*6;                       // 6 degrees per minute
+    var hourangle = hour*30;                    // 30 degrees per hour
+
+    // Get SVG elements for the hands of the clock
+    var minhand = document.getElementById("minutehand");
+    var hourhand = document.getElementById("hourhand");
+
+    // Set an SVG attribute on them to move them around the clock face
+    minhand.setAttribute("transform", "rotate(" + minangle + ",50,50)");
+    hourhand.setAttribute("transform", "rotate(" + hourangle + ",50,50)");
+
+    // Update the clock again in 1 minute
+    setTimeout(updateTime, 60000);
+}
+
+
 window.addEventListener('load', function clockLoad(evt) {
   window.removeEventListener('load', clockLoad);
-
+  updateTime();
   window.parent.postMessage('appready', '*');
 });
