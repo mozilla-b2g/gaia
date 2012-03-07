c178243a36af5910e29f0c78d1e9fad9f8fe6747
Fabien Cazenave <fabien@cazenave.cc>
2012-03-07 17:18:30 +0100

    96px lines + markup cleanup

--- apps/settings/js/wifi.js
+++ apps/settings/js/wifi.js
@@ -4,52 +4,78 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
-  var minSignal, maxSignal;
-  var p = document.querySelector('#status span');
+  var gStatus = document.querySelector('#status small');
+  var gCurrentSSID = '';
 
-  function newListItem(ssid, network) { // .ssid, .bssid, .flags, .signal
+  // current state
+  var gCurrentNetwork = navigator.mozWifiManager.connected;
+  var enabledBox = document.querySelector('input[name="wifi.enabled"]');
+  if (gCurrentNetwork) {
+    gCurrentSSID = gCurrentNetwork.ssid;
+    gStatus.textContent = 'Connected to ' + gCurrentSSID;
+    enabledBox.checked = true;
+  } else {
+    gStatus.textContent = 'Disconnected.';
+    enabledBox.checked = false;
+  }
+
+  function newListItem(ssid, network) { // .ssid, .bssid, .keyManagement, .signal
     var li = document.createElement('li');
     var br = document.createElement('br');
     var span = document.createElement('span');
     var small = document.createElement('small');
-    var wifi = document.createElement('span');
+    var label = document.createElement('label');
     var signal = document.createElement('span');
 
     // ssid
     span.textContent = ssid;
-    small.textContent = (network.flags) ? network.flags : 'open';
+    small.textContent = (network.keyManagement) ? network.keyManagement : 'open';
 
-    // signal
-    // level should be between 0 and 4
-    var level = Math.ceil(4 * (network.signal - 100) / maxSignal);
-    wifi.className = 'wifi';
-    signal.className = 'wifi-signal' + Math.max(0, level);
-    wifi.appendChild(signal);
-    if (network.flags !== undefined) {
+    // signal is between 0 and 100, level should be between 0 and 4
+    var level = Math.min(Math.floor(network.signal / 20), 4);
+    label.className = 'wifi';
+    signal.className = 'wifi-signal' + level;
+    label.appendChild(signal);
+    if (network.keyManagement.length) {
       var secure = document.createElement('span');
       secure.className = 'wifi-secure';
-      wifi.appendChild(secure);
+      label.appendChild(secure);
     }
 
     // create list item
     li.appendChild(span);
     li.appendChild(br);
     li.appendChild(small);
-    li.appendChild(wifi);
+    li.appendChild(label);
 
     // bind connection callback
     li.onclick = function() {
-      p.textContent = 'Connecting to ' + ssid + '…';
+      //gStatus.textContent = 'Connecting to ' + ssid + '…';
       var connection = navigator.mozWifiManager.select(network);
       connection.onsuccess = function() {
-        p.textContent = ssid + ' connected!';
+        //gStatus.textContent = ssid + ' connected!';
+        gStatus.textContent = 'Connecting to ' + ssid + '…';
+        gCurrentSSID = ssid;
       };
       connection.onerror = function() {
-        p.textContent = connection.error.message;
+        gStatus.textContent = connection.error.name;
       };
     };
     return li;
   }
+
+  // mozWifiManager events / callbacks
+  navigator.mozWifiManager.onconnect = function(networkEvent) {
+    //gStatus.textContent = networkEvent.ssid + ' connected.';
+    gStatus.textContent = gCurrentSSID + ' connected.';
+  };
+  navigator.mozWifiManager.onassociate = function(networkEvent) {
+    //gStatus.textContent = networkEvent.ssid + ' associated.';
+    gStatus.textContent = gCurrentSSID + ' associated.';
+  };
+  navigator.mozWifiManager.ondisconnect = function() {
+    gStatus.textContent = 'offline';
+  };
 
   // scan wifi networks
   var networks;
@@ -64,8 +90,6 @@ window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
     ssids.sort(function(a, b) {
       return networks[b].signal - networks[a].signal;
     });
-    maxSignal = -100 + networks[ssids[0]].signal;
-    minSignal = -100 + networks[ssids[ssids.length - 1]].signal;
     for (var i = 0; i < ssids.length; i++) {
       var key = ssids[i];
       var li = newListItem(key, networks[key]);
@@ -73,6 +97,6 @@ window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
     }
   };
   req.onerror = function(error) {
-    p.textContent = 'error: '+ req.error.name;
+    gStatus.textContent = 'error: '+ req.error.name;
   };
 });
