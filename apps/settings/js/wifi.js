a3e12624be57cf7890719a180358ea840aab5b00
Fabien Cazenave <fabien@cazenave.cc>
2012-03-08 12:10:56 +0100

    super dirty hack to test the wifi EAP auth

--- apps/settings/js/wifi.js
+++ apps/settings/js/wifi.js
@@ -1,25 +1,52 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
-
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
+  var gToggle = document.querySelector('#status input');
   var gStatus = document.querySelector('#status small');
+  var gList = document.querySelector('#wifi-networks');
   var gCurrentSSID = '';
 
   // current state
   var gCurrentNetwork = navigator.mozWifiManager.connected;
-  var enabledBox = document.querySelector('input[name="wifi.enabled"]');
-  if (gCurrentNetwork) {
-    gCurrentSSID = gCurrentNetwork.ssid;
-    gStatus.textContent = 'Connected to ' + gCurrentSSID;
-    enabledBox.checked = true;
-  } else {
-    gStatus.textContent = 'Disconnected.';
-    enabledBox.checked = false;
+  function updateState() {
+    //var enabledBox = document.querySelector('input[name="wifi.enabled"]');
+    var enabledBox = document.querySelector('#wifi input[type=checkbox]');
+    if (gCurrentNetwork) {
+      gCurrentSSID = gCurrentNetwork.ssid;
+      gStatus.textContent = 'connected to ' + gCurrentSSID + '.';
+      enabledBox.checked = true;
+    } else if (navigator.mozWifiManager.enabled) {
+      gStatus.textContent = 'offline';
+      enabledBox.checked = true;
+    } else {
+      gStatus.textContent = 'disabled';
+      enabledBox.checked = false;
+    }
+    dump('### ' + enabledBox.outerHTML + ' - ' + enabledBox.checked);
   }
 
-  function newListItem(ssid, network) { // .ssid, .bssid, .keyManagement, .signal
+  gToggle.onclick = function() {
+    if (navigator.mozWifiManager.enabled) {
+      navigator.mozWifiManager.setEnabled(false);
+      while (gList.hasChildNodes())
+        gList.removeChild(gList.lastChild);
+      /*
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      var label = document.createElement('label');
+      var signal = document.createElement('signal');
+      var secure = document.createElement('secure');
+      */
+    } else {
+      navigator.mozWifiManager.setEnabled(true);
+      req = navigator.mozWifiManager.getNetworks();
+    }
+  }
+
+  function newListItem(ssid, network) {
+    // network = { ssid, bssid, keyManagement, signal }
     var li = document.createElement('li');
     var br = document.createElement('br');
     var span = document.createElement('span');
@@ -29,7 +56,8 @@ window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
 
     // ssid
     span.textContent = ssid;
-    small.textContent = (network.keyManagement) ? network.keyManagement : 'open';
+    small.textContent = (network.keyManagement.length) ?
+      'secured by ' + network.keyManagement.join(', ') : 'open';
 
     // signal is between 0 and 100, level should be between 0 and 4
     var level = Math.min(Math.floor(network.signal / 20), 4);
@@ -49,11 +77,15 @@ window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
     li.appendChild(label);
 
     // bind connection callback
-    li.onclick = function() {
-      //gStatus.textContent = 'Connecting to ' + ssid + '…';
+    if (1) li.onclick = function() {
+      if (/EAP$/.test(network.keyManagement)) {
+        network.identity = '********';
+        network.password = '********';
+        network.keyManagement = 'WPA-EAP';
+        dump('### trying EAP auth…\n');
+      }
       var connection = navigator.mozWifiManager.select(network);
       connection.onsuccess = function() {
-        //gStatus.textContent = ssid + ' connected!';
         gStatus.textContent = 'Connecting to ' + ssid + '…';
         gCurrentSSID = ssid;
       };
@@ -61,31 +93,36 @@ window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
         gStatus.textContent = connection.error.name;
       };
     };
+    else li.onclick = function() {
+      showNetwork(network);
+    };
     return li;
   }
 
   // mozWifiManager events / callbacks
-  navigator.mozWifiManager.onconnect = function(networkEvent) {
-    //gStatus.textContent = networkEvent.ssid + ' connected.';
-    gStatus.textContent = gCurrentSSID + ' connected.';
-  };
   navigator.mozWifiManager.onassociate = function(networkEvent) {
-    //gStatus.textContent = networkEvent.ssid + ' associated.';
-    gStatus.textContent = gCurrentSSID + ' associated.';
+    gStatus.textContent = 'obtaining an IP address from ' + gCurrentSSID + ' …';
+    dump('### ' + gCurrentSSID + ' associated.\n');
+  };
+  navigator.mozWifiManager.onconnect = function(networkEvent) {
+    gCurrentSSID = navigator.mozWifiManager.connected.ssid;
+    gStatus.textContent = 'connected to ' + gCurrentSSID + '.';
+    dump('### ' + gCurrentSSID + ' connected.\n');
   };
   navigator.mozWifiManager.ondisconnect = function() {
     gStatus.textContent = 'offline';
+    dump('### ' + gCurrentSSID + ' disconnected.\n');
   };
 
   // scan wifi networks
   var networks;
-  var ul = document.querySelector('#wifi-networks');
   var req = navigator.mozWifiManager.getNetworks();
+  updateState();
 
   req.onsuccess = function() {
     networks = req.result;
-    while (ul.hasChildNodes())
-      ul.removeChild(ul.lastChild);
+    while (gList.hasChildNodes())
+      gList.removeChild(gList.lastChild);
     var ssids = Object.getOwnPropertyNames(networks);
     ssids.sort(function(a, b) {
       return networks[b].signal - networks[a].signal;
@@ -93,10 +130,29 @@ window.addEventListener('DOMContentLoaded', function scanWifiNetworks(evt) {
     for (var i = 0; i < ssids.length; i++) {
       var key = ssids[i];
       var li = newListItem(key, networks[key]);
-      ul.appendChild(li);
+      gList.appendChild(li);
     }
   };
   req.onerror = function(error) {
     gStatus.textContent = 'error: '+ req.error.name;
   };
+
+  function showNetwork(network) {
+    var dialog = document.querySelector('#wifi-eap');
+
+    var header = dialog.querySelector('header');
+    header.textContent = network.ssid;
+    dump('### network = ' + network.ssid + '\n');
+
+    var footer = dialog.querySelector('footer');
+    var action = document.createElement('button');
+    var cancel = document.createElement('button');
+    action.textContent = 'disconnect';
+    cancel.textContent = 'cancel';
+    footer.appendChild(action);
+    footer.appendChild(cancel);
+    dump('### network = ' + network.ssid + '\n');
+
+    body.className = 'dialog';
+  }
 });
