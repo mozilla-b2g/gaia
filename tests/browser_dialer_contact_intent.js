ca1759563b7a773d4c366bc6f4701edd2822fafe
David Flanagan <dflanagan@mozilla.com>
2012-03-16 09:39:29 -0700

    test conversion

--- tests/browser_dialer_contact_intent.js
+++ tests/browser_dialer_contact_intent.js
@@ -1,52 +1,35 @@
+function generatorTest() {
+  yield testApp('../dialer/dialer.html', testContactIntent);
+}
 
-function test() {
-  waitForExplicitFinish();
-  let url = '../dialer/dialer.html';
+function testContactIntent(window, document, nextStep) {
 
-  getWindowManager(function(windowManager) {
-    var testContact;
-    function onReady(dialerFrame) {
-      let dialerWindow = dialerFrame.contentWindow;
-
-      // creating a contact
-      testContact = new mozContact();
-      testContact.init({
-        givenName: 'Andreas',
-        familyName: 'Gal',
-        name: 'Andreas Gal',
-      });
-
-      var req = navigator.mozContacts.save(testContact);
-      req.onsuccess = function() {
-        var fakeEvt = {
-          data: {
-            hidden: false
-          }
-        };
-        dialerWindow.visibilityChanged('../dialer/dialer.html?choice=contact',
-                                       fakeEvt);
-
-        ok(!dialerWindow.document.getElementById('contacts-view').hidden,
-           'Contact view displayed');
-
-        waitFor(function() {
-          var containerId = 'contacts-container';
-          var container = dialerWindow.document.getElementById(containerId);
-          ok(container.children.length > 0, 'Contacts displayed');
-          windowManager.closeForegroundWindow();
-        }, function() {
-          return (('Contacts' in dialerWindow) && dialerWindow.Contacts._loaded);
-        });
-      };
-    }
-
-    function onClose() {
-      navigator.mozContacts.remove(testContact);
-      windowManager.kill(url);
-      finish();
-    }
-
-    let appFrame = windowManager.launch(url).element;
-    ApplicationObserver(appFrame, onReady, onClose);
+  // creating a contact
+  var testContact = new mozContact();
+  testContact.init({
+    givenName: 'Andreas',
+    familyName: 'Gal',
+    name: 'Andreas Gal'
   });
+
+  yield navigator.mozContacts.save(testContact).onsuccess = nextStep;
+
+
+  var fakeEvt = {
+    data: {
+      hidden: false
+    }
+  };
+  window.visibilityChanged('../dialer/dialer.html?choice=contact', fakeEvt);
+
+  ok(!document.getElementById('contacts-view').hidden,
+     'Contact view displayed');
+
+  yield until(function() window.Contacts && window.Contacts._loaded, nextStep);
+
+  var containerId = 'contacts-container';
+  var container = document.getElementById(containerId);
+  ok(container.children.length > 0, 'Contacts displayed');
+
+  navigator.mozContacts.remove(testContact);
 }
