5b0d5e05e848b5ece351809dbbe217c1fdca23fd
David Flanagan <dflanagan@mozilla.com>
2012-03-16 00:43:01 -0700

    converted another test

--- tests/browser_dialer_call_from_contacts.js
+++ tests/browser_dialer_call_from_contacts.js
@@ -1,70 +1,48 @@
-
-function test() {
-  waitForExplicitFinish();
-  let url = '../dialer/dialer.html';
-
-  getWindowManager(function(windowManager) {
-    var testContact;
-    function onReady(dialerFrame) {
-      let dialerWindow = dialerFrame.contentWindow;
-
-      // creating a contact
-      testContact = new mozContact();
-      testContact.init({
-        givenName: 'Tom',
-        familyName: 'Testing',
-        name: 'Tom Testing',
-        tel: '123-456-789'
-      });
-
-      var req = navigator.mozContacts.save(testContact);
-      req.onsuccess = function() {
-        var keyboardTab = dialerWindow.document.getElementById('keyboard-label');
-        EventUtils.sendMouseEvent({type: 'click'}, keyboardTab);
-
-        var contactTab = dialerWindow.document.getElementById('contacts-label');
-        EventUtils.sendMouseEvent({type: 'click'}, contactTab);
-
-        ok(!dialerWindow.document.getElementById('contacts-view').hidden,
-           'Contact view displayed');
-
-        waitFor(function() {
-          var listId = 'contacts-container';
-          var contactsList = dialerWindow.document.getElementById(listId);
-          var aContact = contactsList.querySelector('.contact');
-          EventUtils.sendMouseEvent({type: 'click'}, aContact);
-
-          var overlay = dialerWindow.ContactDetails.overlay;
-
-          overlay.addEventListener('transitionend', function trWait() {
-            overlay.removeEventListener('transitionend', trWait);
-
-            ok(overlay.classList.contains('displayed'), 'Overlay view displayed');
-
-            var number = dialerWindow.ContactDetails.contactPhone;
-            var callScreen = dialerWindow.CallHandler.callScreen;
-
-            EventUtils.sendMouseEvent({type: 'click'}, number);
-            callScreen.addEventListener('transitionend', function trWait() {
-              callScreen.removeEventListener('transitionend', trWait);
-              ok(callScreen.classList.contains('oncall'), 'CallScreen displayed');
-
-              windowManager.closeForegroundWindow();
-            });
-          });
-        }, function() {
-          return (('Contacts' in dialerWindow) && dialerWindow.Contacts._loaded);
-        });
-      };
-    }
-
-    function onClose() {
-      navigator.mozContacts.remove(testContact);
-      windowManager.kill(url);
-      finish();
-    }
-
-    let appFrame = windowManager.launch(url).element;
-    ApplicationObserver(appFrame, onReady, onClose);
+function generatorTest() {
+  // Create a test contact
+  var testContact = new mozContact();
+  testContact.init({
+    givenName: 'Tom',
+    familyName: 'Testing',
+    name: 'Tom Testing',
+    tel: '123-456-789'
   });
+
+  yield navigator.mozContacts.save(testContact).onsuccess = nextStep;
+  yield testApp('../dialer/dialer.html', testCallFromContacts);
+  yield navigator.mozContacts.remove(testContact).onsuccess = nextStep;
+}
+
+function testCallFromContacts(window, document, nextStep) {
+  // Wait for the API we need
+/*
+  yield until(
+    function() window.Contacts && window.Contacts._loaded && window.CallHandler,
+    nextStep);
+*/
+
+  var keyboardTab = document.getElementById('keyboard-label');
+  EventUtils.sendMouseEvent({type: 'click'}, keyboardTab);
+
+  var contactTab = document.getElementById('contacts-label');
+  EventUtils.sendMouseEvent({type: 'click'}, contactTab);
+
+  ok(!document.getElementById('contacts-view').hidden,
+     'Contact view displayed');
+
+  // Wait for at least one contact to be displayed
+  var contactsList = document.getElementById('contacts-container');
+  yield until(function() contactsList.querySelector('.contact'), nextStep);
+
+  var aContact = contactsList.querySelector('.contact');
+  var overlay = window.ContactDetails.overlay;
+  EventUtils.sendMouseEvent({type: 'click'}, aContact);
+  yield until(function() overlay.classList.contains('displayed'), nextStep);
+  ok(overlay.classList.contains('displayed'), 'Overlay view displayed');
+
+  var number = window.ContactDetails.contactPhone;
+  var callScreen = window.CallHandler.callScreen;
+  EventUtils.sendMouseEvent({type: 'click'}, number);
+  yield until(function() callScreen.classList.contains('oncall'), nextStep);
+  ok(callScreen.classList.contains('oncall'), 'CallScreen displayed');
 }
