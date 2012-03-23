3abf5138af8d248007d91596a62f1924dfef73ff
David Flanagan <dflanagan@mozilla.com>
2012-03-23 13:11:43 -0700

    use mozApps launch method and convert manifests to launch apps from the web

--- apps/homescreen/js/app_manager.js
+++ apps/homescreen/js/app_manager.js
@@ -1,79 +1,58 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
-
 'use strict';
 
-if (!window['Gaia'])
-  var Gaia = {};
+// TODO:
+// This separation of AppManager from the homescreen code that is the
+// visual representation of the apps is unnatural.  This code ought to
+// be part of homescreen.js, and much of the code in homescreen.js ought
+// to be spun off into separate files like lockscreen.js and sleepmenu.js
+var AppManager = (function() {
 
-Gaia.AppManager = {
-  _makeUrlObject: function GAM_makeUrlObject(u) {
-    var a = document.createElement('a');
-    a.href = u;
-    return a;
-  },
+  var installedApps = null;
 
-  // Return true iff the strings u1 and u2 are equal up the first
-  // occurrence of '?', which is assumed to be the start of a URL
-  // query string and ignored.
-  _urlsEqualToQueryString: function GAM_urlsEqualToQueryString(u1, u2) {
-    var uo1 = this._makeUrlObject(u1);
-    var uo2 = this._makeUrlObject(u2);
-    return (uo1.protocol === uo2.protocol &&
-            uo1.host === uo2.host &&
-            uo1.hostname === uo2.hostname &&
-            uo1.port === uo2.port &&
-            uo1.pathname === uo2.pathname &&
-            /* ignore .search.  Should we also ignore .hash? */
-            uo1.hash === uo2.hash);
-  },
-
-  installedApps: null,
-
-  loadInstalledApps: function(callback) {
-    if (this.installedApps) {
-      callback(this.installedApps);
+  function getInstalledApps(callback) {
+    if (installedApps) {
+      callback(installedApps);
       return;
     }
 
-    this.installedApps = [];
+    installedApps = {};
 
-    var self = this;
-
-    var lang = document.mozL10n.language.code;
 
     navigator.mozApps.mgmt.getAll().onsuccess = function(e) {
       var apps = e.target.result;
-      var cache = [];
       apps.forEach(function(app) {
-        var manifest = app.manifest;
+        installedApps[app.origin] = app;
+      });
+      callback(installedApps);
+    };
 
-        if (!manifest) {
+/*
+    // The current language for localizing the manifest
+    var lang = document.mozL10n.language.code;
+
+
+        if (!app.manifest) {
           console.warn('malformed manifest for ' + app.origin);
           return;
         }
 
-        // localized manifest?
+        var origin = app.origin;
+        var manifest = app.manifest;
+        var name = manifest.name;
+
+        // localize the manifest
         if (manifest.locales && manifest.locales[lang]) {
           var loc = manifest.locales[lang];
           for (var k in loc)
             manifest[k] = loc[k];
         }
 
-        var icon = manifest.icons ? app.origin + manifest.icons['120'] : '';
 
-        // Even if the icon is stored by the offline cache, trying to load it
-        // will fail because the cache is used only when the application is
-        // opened.
-        // So when an application is installed its icon is inserted into
-        // the offline storage database - and retrieved later when the
-        // homescreen is used offline. (TODO)
-        // So we try to look inside the database for the icon and if it's
-        // empty an icon is loaded by the homescreen - this is the case
-        // of pre-installed apps that does not have any icons defined
-        // in offline storage.
-        if (icon && !window.localStorage.getItem(icon))
-          icon = '.' + manifest.icons['120'];
+        // FIXME: need to be smarter about using whatever icon size
+        // is the best.  And actually, that logic ought to in the homescreen
+        // UI, not here.  Maybe just store the entire manifest and let
+        // the homescreen choose it own icons
+        var icon = manifest.icons ? app.origin + manifest.icons['120'] : '';
 
         var orientation = "";
         // We only allow those values for orientation in manifest.
@@ -86,8 +65,9 @@ Gaia.AppManager = {
 
         var url = app.origin + manifest.launch_path;
 
-        cache.push({
+        installedApps.push({
           name: manifest.name,
+          app: app,
           url: url,
           icon: icon,
           fullscreen: manifest.fullscreen || false,
@@ -95,28 +75,29 @@ Gaia.AppManager = {
           hackKillMe: manifest.hackKillMe || false
         });
       });
+*/
 
-      self.installedApps = cache;
-      callback(cache);
-    };
-  },
+  }
 
-  getInstalledAppForURL: function(url) {
-    var installedApps = this.installedApps;
-    for (var i = 0; i < installedApps.length; i++) {
-      var installedApp = installedApps[i];
-      if (this._urlsEqualToQueryString(installedApp.url, url))
-        return installedApp;
-    }
+  function getAppByOrigin(origin) {
+    return installedApps[origin];
+  }
 
-    return null;
-  },
+  return {
+    getInstalledApps: getInstalledApps,
+    getAppByOrigin: getAppByOrigin
+  };
+}());
 
-  // TODO
-  // This is currently used in src/b2g/chrome/content/webapi.js file
-  // There isn't any reason for any other code to use it, and we should
-  // get rid of it when we can.
-  // See bug 736628: https://bugzilla.mozilla.org/show_bug.cgi?id=736628
+// TODO
+// Gaia.AppManager.foregroundWindow is currently used by the keyboard
+// There isn't any reason for any other code to use it, and we should
+// get rid of it when we can.
+// See bug 736628: https://bugzilla.mozilla.org/show_bug.cgi?id=736628
+if (!window['Gaia'])
+  var Gaia = {};
+
+Gaia.AppManager = {
   get foregroundWindow() {
     return WindowManager.getAppFrame(WindowManager.getDisplayedApp());
   }
