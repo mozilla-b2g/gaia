78dfd9bc76ea547c0be0ba0e23218fab6b922b29
David Flanagan <dflanagan@mozilla.com>
2012-03-24 02:22:46 -0700

    app installation and uninstallation

--- apps/homescreen/js/app_manager.js
+++ apps/homescreen/js/app_manager.js
@@ -1,41 +1,5 @@
 'use strict';
 
-// TODO:
-// This separation of AppManager from the homescreen code that is the
-// visual representation of the apps is unnatural.  This code ought to
-// be part of homescreen.js, and much of the code in homescreen.js ought
-// to be spun off into separate files like lockscreen.js and sleepmenu.js
-var AppManager = (function() {
-
-  var installedApps = null;
-
-  function getInstalledApps(callback) {
-    if (installedApps) {
-      callback(installedApps);
-      return;
-    }
-
-    installedApps = {};
-
-    navigator.mozApps.mgmt.getAll().onsuccess = function(e) {
-      var apps = e.target.result;
-      apps.forEach(function(app) {
-        installedApps[app.origin] = app;
-      });
-      callback(installedApps);
-    };
-  }
-
-  function getAppByOrigin(origin) {
-    return installedApps[origin];
-  }
-
-  return {
-    getInstalledApps: getInstalledApps,
-    getAppByOrigin: getAppByOrigin
-  };
-}());
-
 // TODO
 // Gaia.AppManager.foregroundWindow is currently used by the keyboard
 // There isn't any reason for any other code to use it, and we should
