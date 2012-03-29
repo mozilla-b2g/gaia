7d1193765a4ded27a94d3e17bf8415d0d80ba229
Andreas Gal <andreas.gal@gmail.com>
2012-03-29 07:13:30 -0700

    add simple python web server

---
+++ tools/httpd.py
@@ -0,0 +1,18 @@
+import SimpleHTTPServer;
+import SocketServer;
+
+class CustomHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        host = self.headers['Host']
+        if ":" in host:
+            host = host.split(':')[0]
+        if "." in host:
+            host = host.split('.')[0]
+        self.path = 'apps/' + host + self.path
+        print(self.path)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = SocketServer.TCPServer(("", 8000), CustomHTTPRequestHandler)
+
+httpd.serve_forever()
+
