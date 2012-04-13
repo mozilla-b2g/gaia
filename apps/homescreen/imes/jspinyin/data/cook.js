456093eea0287ba5baddd79b9b7d9149d2916aee
Yuan Xulei <xyuan@mozilla.com>
2012-04-14 06:12:55 +0800

    add the tool of creating pinyin database JSON files.

---
+++ apps/homescreen/imes/jspinyin/data/cook.js
@@ -0,0 +1,44 @@
+var result = {};
+
+if (!stringsAreUTF8()) {
+  throw "You need UTF-8 enabled SpiderMonkey to do cook the data.";
+  quit();
+}
+
+var line;
+while (line = readline()) {
+
+  var fields = line.split(' ');
+  if (fields.length < 4) 
+    continue;
+  var pinyin = fields.slice(3).join("'");
+  var chinese = fields[0];
+  var freq = parseFloat(fields[1]);
+  switch (arguments[0]) {
+    case 'words':
+    default:
+      if (chinese.length !== 1) continue;
+    break;
+    case 'phrases':
+      if (chinese.length === 1) continue;
+    break;
+  }
+
+  if (!result[pinyin]) result[pinyin] = [];
+
+  result[pinyin].push([chinese, freq]);
+}
+
+for (syllables in result) {
+  result[syllables] = result[syllables].sort(
+    function (a, b) {
+      return (b[1] - a[1]);
+    }
+  );
+}
+
+var jsonStr = JSON.stringify(result).replace(/\],/g, '],\n');
+
+print(jsonStr);
+
+quit(0);
