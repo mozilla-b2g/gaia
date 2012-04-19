73b9573f50b538cea789c48444e8e19a8443ce8d
James Lal <james@lightsofapollo.com>
2012-04-18 20:05:55 -0700

    Vastly improved install process by just leverging a local private npm package

--- tools/test-agent/start-server.sh
+++ tools/test-agent/start-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-START_CMD="js-test-agent server -c $DIR/test-agent-server.js --http-path $DIR/../../"
+START_CMD="$DIR/node_modules/test-agent/bin/js-test-agent server -c $DIR/test-agent-server.js --http-path $DIR/../../"
 
 type node > /dev/null 2> /dev/null
 
@@ -23,14 +23,9 @@ else
   exit;
 fi
 
-type js-test-agent > /dev/null 2> /dev/null
 
-if [ $? == 0 ]
-then
-  echo "Check - js-test-agent is present -- starting:"
-  $START_CMD
-else
-  echo "Warn: js-test-agent is missing -- installing"
-  npm install -g test-agent
-  $START_CMD
-fi
+
+cd $DIR && npm install $DIR
+
+echo "Check - js-test-agent is present -- starting:"
+$START_CMD
