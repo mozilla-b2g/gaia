90a36642be50f385b5a61ef16ba4e8af9e073307
Vivien Nicolas <21@vingtetun.org>
2012-04-29 05:21:58 +0200

    Move the real node instead of creating a new one

--- apps/homescreen/js/dragdrop.js
+++ apps/homescreen/js/dragdrop.js
@@ -1,9 +1,81 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
-'use strict';
-
 (function dragdropHandler() {
+  'use strict';
+
+  var DragDropHandler = {
+    onDragStart: function ddh_onDragStart(target) {
+      target.classList.add('float');
+      target.style.MozTransform = 'scale(1.1) translate(0px, 0px)';
+      button.classList.add('visible');
+    },
+
+    onDragMove: function ddh_onDragMove(target, deltaX, deltaY) {
+      target.style.MozTransform =
+        'scale(1.1) translate(' + deltaX + 'px, ' + deltaY + 'px)';
+
+      overlap(target, button) ? button.classList.add('active')
+                              : button.classList.remove('active');
+    },
+
+    onDragStop: function ddh_onDragStop(target) {
+      var shouldUninstall = overlap(target, button);
+      target.classList.add('hidden');
+
+      target.classList.remove('float');
+      target.style.MozTransform = '';
+      button.classList.remove('visible');
+
+      if (shouldUninstall) {
+        var app = appscreen.getAppByOrigin(target.dataset.url);
+        var msg = 'Do you want to uninstall ' + app.manifest.name + '?';
+        requestPermission(msg, function() { app.uninstall() },
+                          function() { target.classList.remove('hidden') });
+        return;
+      }
+
+      target.classList.remove('hidden');
+    },
+
+    target: null,
+    startX: 0,
+    startY: 0,
+    handleEvent: function ddh_handleEvent(e) {
+      var target = e.target;
+
+      switch (e.type) {
+        case 'contextmenu':
+          if (!('url' in target.dataset))
+            return;
+
+          this.target = target;
+          this.startX = e.pageX;
+          this.startY = e.pageY;
+          this.onDragStart(target);
+          break;
+
+        case 'mousemove':
+          if (!this.target)
+            return;
+
+          this.onDragMove(target, e.pageX - this.startX, e.pageY - this.startY);
+          break;
+
+        case 'mouseup':
+          if (!this.target)
+            return;
+
+          this.onDragStop(target);
+          this.target = null;
+          break;
+      }
+    }
+  };
+
+  window.addEventListener('contextmenu', DragDropHandler, true);
+  window.addEventListener('mousemove', DragDropHandler);
+  window.addEventListener('mouseup', DragDropHandler);
 
   function overlap(element1, element2) {
     var r1 = element1.getBoundingClientRect();
@@ -13,116 +85,8 @@
             r1.top < r2.top + r2.height && r1.top + r1.height > r2.top);
   }
 
-
-  var floatingClone = null;
-  function createFloatingClone(target) {
-    floatingClone = target.cloneNode(true);
-    floatingClone.id = 'clone';
-
-    var rect = target.getBoundingClientRect();
-    floatingClone.style.left = rect.left + 'px';
-    floatingClone.style.top = rect.top + 'px';
-
-    floatingClone.style.backgroundImage = target.style.backgroundImage;
-    document.body.appendChild(floatingClone);
-  }
-
-  function updateFloatingClone(deltaX, deltaY) {
-    var transform = 'scale(1.1) translate(' + deltaX + 'px, ' + deltaY + 'px)';
-    floatingClone.style.MozTransform = transform;
-  }
-
-  function destroyFloatingClone() {
-    document.body.removeChild(floatingClone);
-  }
-
-
-  var uninstallButton = null;
-  function createUninstallButton() {
-    uninstallButton = document.createElement('button');
-    uninstallButton.id = 'uninstall';
-    document.body.appendChild(uninstallButton);
-  }
-
-  function updateUninstallButton(active) {
-    active ? uninstallButton.classList.add('active')
-           : uninstallButton.classList.remove('active');
-  }
-
-  function destroyUninstallButton() {
-    document.body.removeChild(uninstallButton);
-  }
-
-
-  var DragDropHandler = {
-    onDragStart: function ddh_onDragStart(evt) {
-      createFloatingClone(this.target);
-      this.target.style.opacity = 0.2;
-
-      createUninstallButton();
-    },
-
-    onDragStop: function ddh_onDragStop(evt) {
-      var target = this.target;
-
-      if (overlap(floatingClone, uninstallButton)) {
-        var app = appscreen.getAppByOrigin(target.dataset.url);
-
-        // FIXME: localize this message
-        // FIXME: This could be a simple confirm() (see bug 741587)
-        requestPermission(
-          'Do you want to uninstall ' + app.manifest.name + '?',
-          function() { app.uninstall(); },
-          function() { target.style.opacity = 1.0 }
-        );
-      } else {
-        target.style.opacity = 1.0;
-      }
-
-      destroyFloatingClone();
-      destroyUninstallButton();
-    },
-
-    onDragMove: function ddh_onDragMove(evt) {
-      updateFloatingClone(evt.pageX - this.startX, evt.pageY - this.startY);
-      updateUninstallButton(overlap(floatingClone, uninstallButton));
-    },
-
-    target: null,
-    startX: 0,
-    startY: 0,
-    handleEvent: function ddh_handleEvent(evt) {
-      var target = evt.target;
-
-      switch (evt.type) {
-        case 'contextmenu':
-          if (!('url' in target.dataset))
-            return;
-          this.target = target;
-          this.startX = evt.pageX;
-          this.startY = evt.pageY;
-
-          this.onDragStart(evt);
-          break;
-        case 'mouseup':
-          if (!this.target)
-            return;
-
-          this.onDragStop(evt);
-          this.target = null;
-          break;
-        case 'mousemove':
-          if (!this.target)
-            return;
-
-          this.onDragMove(evt);
-          break;
-      }
-    }
-  };
-
-  window.addEventListener('contextmenu', DragDropHandler, true);
-  window.addEventListener('mousemove', DragDropHandler);
-  window.addEventListener('mouseup', DragDropHandler);
+  var button = document.createElement('button');
+  button.id = 'uninstall';
+  document.body.appendChild(button);
 })();
 
