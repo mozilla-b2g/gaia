f6d7da194a5773cd7de0b81d9995a94303e0e60c
Alfredos-Panagiotis Damkalis <fredy@freemail.gr>
2012-04-30 22:41:18 +0300

    Corecting Greek Keyboard Layout
    Correction on greek keyboard layout.

--- apps/system/js/keyboard/layout.js
+++ apps/system/js/keyboard/layout.js
@@ -353,14 +353,15 @@ const Keyboards = {
       ε: "έ€",
       ω: "ώ",
       ο: "ό",
-      Y: "ΎΫ",
+      Υ: "ΎΫ",
       υ: "ύϋΰ",
+      Ι: "ΊΪ",
       ι: "ίϊΐ",
       η: "ή",
       σ: "ς"
     },
     keys: [
-      [ { value: ";" }, { value: "ς" }, { value: "ε" } , { value: "ρ" }, { value: "τ" } , { value: "υ" }, { value: "θ" } , { value: "ι" }, { value: "ο" }, { value: "π" } ],
+      [ { value: ";", keyCode: KeyEvent.DOM_VK_PERIOD }, { value: "ς" }, { value: "ε" } , { value: "ρ" }, { value: "τ" } , { value: "υ" }, { value: "θ" } , { value: "ι" }, { value: "ο" }, { value: "π" } ],
       [ { value: "α" }, { value: "σ" }, { value: "δ" }, { value: "φ" }, { value: "γ" } , { value: "η" }, { value: "ξ" }, { value: "κ" }, { value: "λ" }, { value: "'", keyCode: 39 } ],
       [ { value: "⇪", ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK }, { value: "ζ" }, { value: "χ" }, { value: "ψ" }, { value: "ω" }, { value: "β" }, { value:"ν" }, { value: "μ" }, { value: "⌫", ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE } ],
       [ { value: " ", ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE }, { value: "↵", ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN } ]
