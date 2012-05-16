570c64bee61dd7d1da778dac64a78c9b684674bb
Alberto Pastor <albert.pastor@gmail.com>
2012-05-16 13:44:39 +0100

    Removing unneeded files

--- apps/system/js/keyboard/imes/jspinyin/tests/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get a copy of SpiderMonkey Javascript Shell to run this file
-# http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/
-
-JSSHELL=js
-
-$JSSHELL -U `dirname $0`/test*.js 
