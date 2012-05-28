0081a30e6656900dce5979f43b03cc478861680e
ian-liu <iliu@mozilla.com>
2012-05-28 11:23:20 +0800

    Improve user experience of sleep menu control. Modulize the sleep menu widget.

---
+++ apps/system/js/sleepmenu/sleepmenu.js
@@ -0,0 +1,77 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+var SleepMenu = {
+  get element() {
+    delete this.element;
+    return this.element = document.getElementById('sleep');
+  },
+
+  get visible() {
+    return this.element.classList.contains('visible');
+  },
+
+  init: function sm_init() {
+    window.addEventListener('click', SleepMenu, true);
+    window.addEventListener('keyup', SleepMenu, true);
+  },
+
+  show: function sm_show() {
+    this.element.classList.add('visible');
+  },
+
+  hide: function sm_hide() {
+    this.element.classList.remove('visible');
+  },
+
+  handleEvent: function sm_handleEvent(evt) {
+    if (!this.visible)
+      return;
+    
+    switch (evt.type) {
+      case 'click':
+        var action = evt.target.dataset.value;
+        switch (action) {
+          case 'airplane':
+            // XXX There is no API for that yet
+            break;
+          case 'silent':
+            var settings = window.navigator.mozSettings;
+            if (settings)
+              settings.getLock().set({ 'phone.ring.incoming': false});
+
+            document.getElementById('silent').hidden = true;
+            document.getElementById('normal').hidden = false;
+            break;
+          case 'normal':
+            var settings = window.navigator.mozSettings;
+            if (settings)
+              settings.getLock().set({'phone.ring.incoming': true});
+
+            document.getElementById('silent').hidden = false;
+            document.getElementById('normal').hidden = true;
+            break;
+          case 'restart':
+            navigator.mozPower.reboot();
+            break;
+          case 'power':
+            navigator.mozPower.powerOff();
+            break;
+        }
+        this.hide();
+        break;
+
+      case 'keyup':
+        if (evt.keyCode == evt.DOM_VK_ESCAPE ||
+            evt.keyCode == evt.DOM_VK_HOME) {
+              
+            this.hide();
+            evt.preventDefault();
+            evt.stopPropagation();
+         }
+        break;
+    }
+  }
+};
