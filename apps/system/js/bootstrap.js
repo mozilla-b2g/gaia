a6a076b0f1fd64c512d554056a95869b780e5fbc
Timothy Guan-tin Chien <timdream@gmail.com>
2012-05-31 17:33:47 +0800

    Move SourceView out of bootstrap.js, rename KeyHandler to ReloadManager

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -8,9 +8,10 @@ function startup() {
   LockScreen.init();
   PinLock.init();
   StatusBar.init();
-  KeyHandler.init();
   SoundManager.init();
   SleepMenu.init();
+  SourceView.init();
+  ReloadManager.init();
 
   Applications.rebuild(function start(apps) {
     // FIXME Loop over all the registered activities from the applications
@@ -120,96 +121,18 @@ var SleepMenu = {
   }
 };
 
-/* === Source View === */
-var SourceView = {
-  get viewer() {
-    return document.getElementById('appViewsource');
-  },
 
-  get active() {
-    return !this.viewer ? false : this.viewer.style.visibility === 'visible';
-  },
-
-  show: function sv_show() {
-    var viewsource = this.viewer;
-    if (!viewsource) {
-      var style = '#appViewsource { ' +
-                  '  position: absolute;' +
-                  '  top: -moz-calc(10%);' +
-                  '  left: -moz-calc(10%);' +
-                  '  width: -moz-calc(80% - 2 * 15px);' +
-                  '  height: -moz-calc(80% - 2 * 15px);' +
-                  '  visibility: hidden;' +
-                  '  margin: 15px;' +
-                  '  background-color: white;' +
-                  '  opacity: 0.92;' +
-                  '  color: black;' +
-                  '  z-index: 9999;' +
-                  '}';
-      document.styleSheets[0].insertRule(style, 0);
-
-      viewsource = document.createElement('iframe');
-      viewsource.id = 'appViewsource';
-      document.body.appendChild(viewsource);
-
-      window.addEventListener('locked', this);
-    }
-
-    var url = WindowManager.getDisplayedApp();
-    if (!url)
-      // Assume the home screen is the visible app.
-      url = window.location.toString();
-    viewsource.src = 'view-source: ' + url;
-    viewsource.style.visibility = 'visible';
-  },
-
-  hide: function sv_hide() {
-    var viewsource = this.viewer;
-    if (viewsource) {
-      viewsource.style.visibility = 'hidden';
-      viewsource.src = 'about:blank';
-    }
-  },
-
-  toggle: function sv_toggle() {
-    this.active ? this.hide() : this.show();
-  },
-
-  handleEvent: function sv_handleEvent(evt) {
-    switch (evt.type) {
-      case 'locked':
-        this.hide();
-        break;
-    }
-  }
-};
-
-/* === KeyHandler === */
-var KeyHandler = {
-  init: function kh_init() {
-    window.addEventListener('keydown', this);
+/* === ReloadManager === */
+var ReloadManager = {
+  init: function rm_init() {
     window.addEventListener('keyup', this);
   },
 
-  handleEvent: function kh_handleEvent(evt) {
-    if (!navigator.mozPower.screenEnabled)
+  handleEvent: function rm_handleEvent(evt) {
+    if (!ScreenManager.screenEnabled || evt.keyCode !== evt.DOM_VK_F6)
       return;
 
-    switch (evt.type) {
-      case 'keydown':
-        break;
-      case 'keyup':
-        switch (evt.keyCode) {
-          case evt.DOM_VK_CONTEXT_MENU:
-            SourceView.toggle();
-            break;
-
-          case evt.DOM_VK_F6:
-            document.location.reload();
-            break;
-        }
-        break;
-    }
+    document.location.reload();
   }
 };
 
