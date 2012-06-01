a560ea5d1e92a26ef5238b19aef76af969eba809
Dominic Kuo <dkuo@mozilla.com>
2012-06-01 15:46:57 +0800

    New arch & code design of the Music app, rewrite code. Also the three views of Music app are usable now (But certainly not completed yet)

--- apps/music/js/music.js
+++ apps/music/js/music.js
@@ -1,3 +1,10 @@
+'use strict';
+
+/*
+ * This is Music Application of Gaia
+ */
+
+// Hard-coded ogg files as testing songs
 var songs = [
   {
     file: 'audio/jonobacon-freesoftwaresong2.ogg',
@@ -21,90 +28,305 @@ var songs = [
   }
 ];
 
+// This App. has three modes, TILES, LIST and PLAYER
 var MODE_TILES = 1;
 var MODE_LIST = 2;
 var MODE_PLAYER = 3;
+var currentMode;
 
-window.addEventListener('DOMContentLoaded', function() {
-  var body = document.body;
-  var songList = document.getElementById('views-list');
-  var player = document.getElementById('player');
-  var audio = document.getElementById('player-audio');
-  
-  var titleText = document.getElementById('title-text');
-  
-  var playerCoverTitle = document.getElementById('player-cover-title');
-  var playerCoverArtist = document.getElementById('player-cover-artist');
-  var playerControlsPrevious = document.getElementById('player-controls-previous');
-  var playerControlsPlay = document.getElementById('player-controls-play');
-  var playerControlsNext = document.getElementById('player-controls-next');
-  
-  changeMode(MODE_LIST);
-  
-  var content = '';
-  songs.forEach(function(song) {
-    content += '<li class="song">' +
-               '  <a href="#" id="' + escapeHTML(song.file, true) + '" ' + 
-               '   data-title="'+ escapeHTML(song.title, true) + '" ' + 
-               '   data-artist="'+ escapeHTML(song.artist, true) + '" ' + '>' +
-               '    ' + escapeHTML(song.title) + ' - ' + escapeHTML(song.artist) +
-               '  </a>' +
-               '</li>';
-  });
-  songList.innerHTML = content;
+function changeMode(mode) {
+  currentMode = mode;
 
-  songList.addEventListener('click', function(evt) {
-    var target = evt.target;
-    if (!target)
+  document.body.classList.remove('tiles-mode');
+  document.body.classList.remove('list-mode');
+  document.body.classList.remove('player-mode');
+
+  switch (mode) {
+    case MODE_TILES:
+      document.body.classList.add('tiles-mode');
+      break;
+    case MODE_LIST:
+      document.body.classList.add('list-mode');
+      break;
+    case MODE_PLAYER:
+      document.body.classList.add('player-mode');
+      break;
+  }
+}
+
+// Title Bar
+var TitleBar = {
+  get view() {
+    delete this._view;
+    return this._view = document.getElementById('title');
+  },
+
+  get titleText() {
+    delete this._titleText;
+    return this._titleText = document.getElementById('title-text');
+  },
+
+  init: function tb_init() {
+    this.view.addEventListener('click', this);
+  },
+
+  changeTitleText: function tb_changeTitleText(content) {
+    this.titleText.textContent = content;
+  },
+
+  handleEvent: function tb_handleEvent(evt) {
+    switch (evt.type) {
+      case 'click':
+        var target = evt.target;
+        if (!target)
+          return;
+
+        changeMode(MODE_PLAYER);
+
+        break;
+
+      default:
+        return;
+    }
+  }
+};
+
+// View of Tiles
+var TilesView = {
+  get view() {
+    delete this._view;
+    return this._view = document.getElementById('views-tiles');
+  },
+
+  init: function tv_init() {
+    this.view.addEventListener('click', this);
+  },
+
+  handleEvent: function tv_handleEvent(evt) {
+    switch (evt.type) {
+      case 'click':
+        var target = evt.target;
+        if (!target)
+          return;
+
+        changeMode(MODE_LIST);
+
+        break;
+
+      default:
+        return;
+    }
+  }
+};
+
+// View of List
+var ListView = {
+  get view() {
+    delete this._view;
+    return this._view = document.getElementById('views-list');
+  },
+
+  get dataSource() {
+    return this._dataSource;
+  },
+
+  set dataSource(source) {
+    this._dataSource = source;
+  },
+
+  init: function lv_init() {
+    this.dataSource = [];
+
+    this.view.addEventListener('click', this);
+
+    this.updateList(songs);
+  },
+
+  updateList: function lv_updateList(source) {
+    var content = '';
+    var index = 0;
+
+    source.forEach(function(song) {
+      content += '<li class="song">' +
+                 '  <a href="#" id="' + escapeHTML(song.file, true) + '" ' +
+                 '   data-title="' + escapeHTML(song.title, true) + '" ' +
+                 '   data-artist="' + escapeHTML(song.artist, true) + '" ' +
+                 '   data-index="' + index + '" ' + '>' +
+                 '    ' + escapeHTML(song.title) + ' - ' +
+                          escapeHTML(song.artist) +
+                 '  </a>' +
+                 '</li>';
+      index++;
+    });
+    this.view.innerHTML = content;
+
+    this.dataSource = source;
+  },
+
+  handleEvent: function lv_handleEvent(evt) {
+    switch (evt.type) {
+      case 'click':
+        var target = evt.target;
+        if (!target)
+          return;
+
+        changeMode(MODE_PLAYER);
+
+        PlayerView.dataSource = this.dataSource;
+        PlayerView.play(target);
+
+        break;
+
+      default:
+        return;
+    }
+  }
+};
+
+// View of Player
+var PlayerView = {
+  get view() {
+    delete this._view;
+    return this._view = document.getElementById('views-player');
+  },
+
+  get audio() {
+    delete this._audio;
+    return this._audio = document.getElementById('player-audio');
+  },
+
+  get isPlaying() {
+    return this._isPlaying;
+  },
+
+  set isPlaying(val) {
+    this._isPlaying = val;
+  },
+
+  get dataSource() {
+    return this._dataSource;
+  },
+
+  set dataSource(source) {
+    this._dataSource = source;
+  },
+
+  init: function pv_init() {
+    this.title = document.getElementById('player-cover-title');
+    this.artist = document.getElementById('player-cover-artist');
+    this.playControl = document.getElementById('player-controls-play');
+
+    this.isPlaying = false;
+    this.dataSource = [];
+    this.currentIndex = 0;
+
+    this.view.addEventListener('click', this);
+  },
+
+  play: function pv_play(target) {
+    this.isPlaying = true;
+
+    if (target) {
+      this.title.textContent = target.dataset.title;
+      this.artist.textContent = target.dataset.artist;
+      this.audio.src = target.id;
+      this.currentIndex = target.dataset.index.valueOf();
+    } else {
+      this.audio.play();
+    }
+
+    this.playControl.textContent = 'Pause';
+  },
+
+  pause: function pv_pause() {
+    this.isPlaying = false;
+
+    this.audio.pause();
+
+    this.playControl.textContent = 'Play';
+  },
+
+  next: function pv_next() {
+    var songElements = ListView.view.children;
+
+    if (this.currentIndex >= this.dataSource.length - 1)
       return;
-    playSong(target.id); // song url is anchor id
-    playerCoverTitle.textContent = target.dataset.title;
-    playerCoverArtist.textContent = target.dataset.artist;
-  });
-  
+
+    this.currentIndex++;
+
+    this.play(songElements[this.currentIndex].firstElementChild);
+  },
+
+  previous: function pv_previous() {
+    var songElements = ListView.view.children;
+
+    if (this.currentIndex <= 0)
+      return;
+
+    this.currentIndex--;
+
+    this.play(songElements[this.currentIndex].firstElementChild);
+  },
+
+  handleEvent: function pv_handleEvent(evt) {
+    switch (evt.type) {
+      case 'click':
+        var target = evt.target;
+        if (!target)
+          return;
+
+        switch (target.id) {
+          case 'player-controls-previous':
+            this.previous();
+
+            break;
+
+          case 'player-controls-play':
+            if (this.isPlaying) {
+              this.pause();
+            } else {
+              this.play();
+            }
+
+            break;
+
+          case 'player-controls-next':
+            this.next();
+
+            break;
+        }
+
+        break;
+
+      default:
+        return;
+    }
+  }
+};
+
+// App. start from here after 'DOMContentLoaded' event is fired
+window.addEventListener('DOMContentLoaded', function() {
+  TitleBar.init();
+  TilesView.init();
+  ListView.init();
+  PlayerView.init();
+
+  changeMode(MODE_TILES);
+
   window.addEventListener('keyup', function keyPressHandler(evt) {
-    if (playing && evt.keyCode == evt.DOM_VK_ESCAPE) {
-      stopSong();
-      showSongList();
-      evt.preventDefault();
+    if (evt.keyCode == evt.DOM_VK_ESCAPE) {
+      switch (currentMode) {
+        case MODE_TILES:
+          break;
+        case MODE_LIST:
+          changeMode(MODE_TILES);
+          evt.preventDefault();
+          break;
+        case MODE_PLAYER:
+          changeMode(MODE_LIST);
+          evt.preventDefault();
+          break;
+      }
     }
   });
-
-  var playing = false;
-  
-  function showSongList(songs) {
-    changeMode(MODE_LIST);
-    
-    playing = false;
-  }
-
-  function playSong(url) {
-    changeMode(MODE_PLAYER);
-    
-    audio.src = url;
-    playing = true;
-  }
-
-  function stopSong(song) {
-    audio.pause();
-    playing = false;
-  }
-  
-  function changeMode(mode) {
-    body.classList.remove('tiles-mode');
-    body.classList.remove('list-mode');
-    body.classList.remove('player-mode');
-    
-    switch (mode) {
-      case MODE_TILES:
-        body.classList.add('tiles-mode');
-        break;
-      case MODE_LIST:
-        body.classList.add('list-mode');
-        break;
-      case MODE_PLAYER:
-        body.classList.add('player-mode');
-        break;
-    }
-  }
 });
