8da697bd313e9e0f8c1c615d4721c3f54937e808
Vivien Nicolas <21@vingtetun.org>
2012-06-07 21:18:48 +0200

    Fix some errors with a redefined variable and add some line break

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -195,7 +195,7 @@ var Browser = {
         break;
 
       case 'keyup':
-        if (!this.currentTab.session.backLength() ||
+        if (!this.currentTab || !this.currentTab.session.backLength() ||
           evt.keyCode != evt.DOM_VK_ESCAPE)
           break;
 
@@ -227,8 +227,9 @@ var Browser = {
   },
 
   go: function browser_go(e) {
-    if (e)
+    if (e) {
       e.preventDefault();
+    }
 
     if (this.urlButtonMode == this.REFRESH) {
       this.navigate(this.currentTab.url);
@@ -238,12 +239,15 @@ var Browser = {
     var url = this.urlInput.value.trim();
     var protocolRegexp = /^([a-z]+:)(\/\/)?/i;
     var protocol = protocolRegexp.exec(url);
-    if (!protocol)
+    if (!protocol) {
       url = 'http://' + url;
+    }
+
     if (url != this.currentTab.url) {
       this.urlInput.value = url;
       this.currentTab.url = url;
     }
+
     this.navigate(url);
     this.urlInput.blur();
   },
@@ -379,6 +383,7 @@ var Browser = {
     if (this.currentTab !== null && this.currentTab.id !== id) {
       this.hideCurrentTab();
     }
+
     this.currentTab = this.tabs[id];
     this.currentTab.dom.style.display = 'block';
     this.currentTab.dom.style.top = '0px';
