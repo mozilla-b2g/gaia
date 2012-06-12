cb056d7ed7dea9378273a878e24448ae7fce3feb
Timothy Guan-tin Chien <timdream@gmail.com>
2012-06-13 01:33:52 +0800

    Create PermissionManager and move webapp install request to it

--- apps/system/js/applications.js
+++ apps/system/js/applications.js
@@ -9,10 +9,6 @@
 var Applications = {
   installedApps: {},
   init: function a_init() {
-    // XXX: handle webapps-ask-install on behalf of permission manager
-    // need relocate.
-    window.addEventListener('mozChromeEvent', this);
-
     var self = this;
     var apps = navigator.mozApps;
 
@@ -42,42 +38,6 @@ var Applications = {
 
   getByOrigin: function a_getByOrigin(origin) {
     return this.installedApps[origin];
-  },
-
-  handleEvent: function a_handleEvent(evt) {
-    var detail = evt.detail;
-    if (detail.type !== 'webapps-ask-install')
-      return;
-
-    // This is how we say yes or no to the request after the user decides
-    var self = this;
-    function sendResponse(id, allow) {
-
-      var event = document.createEvent('CustomEvent');
-      event.initCustomEvent('mozContentEvent', true, true, {
-        id: id,
-        type: allow ? 'webapps-install-granted' : 'webapps-install-denied'
-      });
-      window.dispatchEvent(event);
-    }
-
-    var app = detail.app;
-    if (document.location.toString().indexOf(app.installOrigin) == 0) {
-      sendResponse(detail.id, true);
-      return;
-    }
-
-    var name = app.manifest.name;
-    var locales = app.manifest.locales;
-    var lang = navigator.language;
-    if (locales && locales[lang] && locales[lang].name)
-      name = locales[lang].name;
-
-    var str = document.mozL10n.get('install', {
-      'name': name, 'origin': app.origin
-    });
-    requestPermission(str, function() { sendResponse(detail.id, true); },
-                           function() { sendResponse(detail.id, false); });
   },
 
   fireApplicationReadyEvent: function a_fireAppReadyEvent() {
--- apps/system/js/permission_manager.js
+++ apps/system/js/permission_manager.js
@@ -3,6 +3,44 @@
 
 'use strict';
 
+(function permissionRequestListener() {
+  window.addEventListener('mozChromeEvent', function handleWebappInstall(evt) {
+    var detail = evt.detail;
+    if (detail.type !== 'webapps-ask-install')
+      return;
+
+    // This is how we say yes or no to the request after the user decides
+    var self = this;
+    function sendResponse(id, allow) {
+
+      var event = document.createEvent('CustomEvent');
+      event.initCustomEvent('mozContentEvent', true, true, {
+        id: id,
+        type: allow ? 'webapps-install-granted' : 'webapps-install-denied'
+      });
+      window.dispatchEvent(event);
+    }
+
+    var app = detail.app;
+    if (document.location.toString().indexOf(app.installOrigin) == 0) {
+      sendResponse(detail.id, true);
+      return;
+    }
+
+    var name = app.manifest.name;
+    var locales = app.manifest.locales;
+    var lang = navigator.language;
+    if (locales && locales[lang] && locales[lang].name)
+      name = locales[lang].name;
+
+    var str = document.mozL10n.get('install', {
+      'name': name, 'origin': app.origin
+    });
+    requestPermission(str, function() { sendResponse(detail.id, true); },
+                           function() { sendResponse(detail.id, false); });
+  });
+})();
+
 var requestPermission = (function() {
   // A queue of pending requests.
   // Callers must be careful not to create an infinite loop!
