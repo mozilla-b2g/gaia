8b489f98ed05158f47d9b091f02b91987937b098
Ben Francis <ben@tola.me.uk>
2012-06-15 16:28:12 +0100

    Addressing review comments

--- apps/browser/test/unit/date_helper_test.js
+++ apps/browser/test/unit/date_helper_test.js
@@ -2,6 +2,13 @@ requireApp('browser/js/date_helper.js');
 
 suite('Date Helper', function() {
 
+  function assertMidnight(date) {
+    assert.equal(date.getHours(), 0);
+    assert.equal(date.getMinutes(), 0);
+    assert.equal(date.getSeconds(), 0);
+    assert.equal(date.getMilliseconds(), 0);
+  };
+
   suite('started', function() {
 
     test('todayStarted', function() {
@@ -11,11 +18,7 @@ suite('Date Helper', function() {
       assert.equal(todayStarted.getFullYear(), now.getFullYear());
       assert.equal(todayStarted.getMonth(), now.getMonth());
       assert.equal(todayStarted.getDate(), now.getDate());
-      assert.equal(todayStarted.getHours(), 0);
-      assert.equal(todayStarted.getMinutes(), 0);
-      assert.equal(todayStarted.getSeconds(), 0);
-      assert.equal(todayStarted.getMilliseconds(), 0);
-
+      assertMidnight(todayStarted);
     });
 
     test('yesterdayStarted', function() {
@@ -23,10 +26,7 @@ suite('Date Helper', function() {
       var yesterdayStartedTimestamp = DateHelper.yesterdayStarted();
       var yesterdayStarted = new Date(yesterdayStartedTimestamp);
       assert.equal(true, yesterdayStartedTimestamp <= now.valueOf());
-      assert.equal(yesterdayStarted.getHours(), 0);
-      assert.equal(yesterdayStarted.getMinutes(), 0);
-      assert.equal(yesterdayStarted.getSeconds(), 0);
-      assert.equal(yesterdayStarted.getMilliseconds(), 0);
+      assertMidnight(yesterdayStarted);
 
     });
 
@@ -36,10 +36,7 @@ suite('Date Helper', function() {
       var weekStarted = new Date(weekStartedTimestamp);
       assert.equal(true, weekStartedTimestamp <= now.valueOf());
       assert.equal(weekStarted.getDay(), 1);
-      assert.equal(weekStarted.getHours(), 0);
-      assert.equal(weekStarted.getMinutes(), 0);
-      assert.equal(weekStarted.getSeconds(), 0);
-      assert.equal(weekStarted.getMilliseconds(), 0);
+      assertMidnight(weekStarted);
     });
 
     test('thisMonthStarted', function() {
@@ -50,10 +47,7 @@ suite('Date Helper', function() {
       assert.equal(monthStarted.getFullYear(), now.getFullYear());
       assert.equal(monthStarted.getMonth(), now.getMonth());
       assert.equal(monthStarted.getDate(), 1);
-      assert.equal(monthStarted.getHours(), 0);
-      assert.equal(monthStarted.getMinutes(), 0);
-      assert.equal(monthStarted.getSeconds(), 0);
-      assert.equal(monthStarted.getMilliseconds(), 0);
+      assertMidnight(monthStarted);
     });
 
     test('lastSixMonthsStarted', function() {
@@ -72,10 +66,7 @@ suite('Date Helper', function() {
       assert.equal(yearStarted.getFullYear(), now.getFullYear());
       assert.equal(yearStarted.getMonth(), 0);
       assert.equal(yearStarted.getDate(), 1);
-      assert.equal(yearStarted.getHours(), 0);
-      assert.equal(yearStarted.getMinutes(), 0);
-      assert.equal(yearStarted.getSeconds(), 0);
-      assert.equal(yearStarted.getMilliseconds(), 0);
+      assertMidnight(yearStarted);
     });
 
   });
