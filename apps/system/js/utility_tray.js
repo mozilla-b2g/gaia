9ac7d16c43c199ac719e63779a213f89c7c890d4
Timothy Guan-tin Chien <timdream@gmail.com>
2012-06-18 13:39:15 +0800

    Split Utility Tray from notifications.js

---
+++ apps/system/js/utility_tray.js
@@ -0,0 +1,112 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+var UtilityTray = {
+  locked: false,
+
+  active: false,
+
+  overlay: document.getElementById('utility-tray'),
+
+  statusbar: document.getElementById('statusbar'),
+
+  screen: document.getElementById('screen'),
+
+  init: function ut_init() {
+    var events = ['touchstart', 'touchmove', 'touchend'];
+
+    events.forEach(function bindEvents(name) {
+      window.addEventListener(name, this);
+    }, this);
+
+    this.overlay.addEventListener('transitionend', this);
+  },
+
+  handleEvent: function ut_handleEvent(evt) {
+    switch (evt.type) {
+      case 'touchstart':
+        if (LockScreen.locked)
+          return;
+        if (this.locked && evt.target !== this.overlay)
+          return;
+        if (!this.locked && evt.target !== this.statusbar)
+          return;
+
+        this.active = true;
+
+        this.onTouchStart(evt.touches[0]);
+        break;
+
+      case 'touchmove':
+      if (!this.active)
+        return;
+
+        this.onTouchMove(evt.touches[0]);
+        break;
+
+      case 'touchend':
+        if (!this.active)
+          return;
+          this.active = false;
+
+        this.onTouchEnd(evt.changedTouches[0]);
+        break;
+
+      case 'transitionend':
+        if (!this.locked)
+          this.screen.classList.remove('utility-tray');
+        break;
+    }
+  },
+
+  onTouchStart: function ut_onTouchStart(touch) {
+    this.startX = touch.pageX;
+    this.startY = touch.pageY;
+    this.screen.classList.add('utility-tray');
+    this.onTouchMove({ pageY: touch.pageY + 32 });
+  },
+
+  onTouchMove: function ut_onTouchMove(touch) {
+    var screenHeight = this.overlay.getBoundingClientRect().height;
+    var dy = -(this.startY - touch.pageY);
+    if (this.locked)
+      dy += screenHeight;
+    dy = Math.min(screenHeight, dy);
+
+    var style = this.overlay.style;
+    style.MozTransition = '';
+    style.MozTransform = 'translateY(' + dy + 'px)';
+  },
+
+  onTouchEnd: function ut_onTouchEnd(touch) {
+    var screenHeight = this.overlay.getBoundingClientRect().height;
+    var dy = -(this.startY - touch.pageY);
+    var offset = Math.abs(dy);
+    if ((!this.locked && offset > screenHeight / 4) ||
+        (this.locked && offset < 10))
+      this.lock();
+    else
+      this.unlock();
+  },
+
+  unlock: function ut_unlock(instant) {
+    var style = this.overlay.style;
+    style.MozTransition = instant ? '' : '-moz-transform 0.2s linear';
+    style.MozTransform = 'translateY(0)';
+    this.locked = false;
+    if (instant)
+      this.screen.classList.remove('utility-tray');
+  },
+
+  lock: function ut_lock(dy) {
+    var style = this.overlay.style;
+    style.MozTransition = '-moz-transform 0.2s linear';
+    style.MozTransform = 'translateY(100%)';
+    this.locked = true;
+    this.screen.classList.add('utility-tray');
+  }
+};
+
+UtilityTray.init();
