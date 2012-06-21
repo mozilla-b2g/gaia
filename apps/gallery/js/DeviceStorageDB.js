84080bed35d60299f544e6dc9c5f6e041aec580f
David Flanagan <dflanagan@mozilla.com>
2012-06-21 09:43:52 -0700

    fix lint

--- apps/gallery/js/DeviceStorageDB.js
+++ apps/gallery/js/DeviceStorageDB.js
@@ -1,14 +1,14 @@
 /*
  * DeviceStorageDB.js
- * 
+ *
  * This library uses the DeviceStorage API and the IndexedDB API to present
  * a simple view of media files stored on a device.  If given a function
  * for parsing metadata, it will automatically extract media metadata from
  * each file and store it in the database along with the filename.
- * 
- * The idea is that a media app should just be able to create a 
+ *
+ * The idea is that a media app should just be able to create a
  * DeviceStorageDB object and then list the files on it, passing in
- * filtering and sorting criteria.  When files are added or removed, 
+ * filtering and sorting criteria.  When files are added or removed,
  * DSDB will be notified by the DeviceStorage API and will pass those
  * notifications on to media apps.  DSDB should generally handle any
  * rescanning that needs to be done, so that media apps don't have to do it
@@ -17,17 +17,17 @@ var createDeviceStorageDB = (function() {
 
   // The callback will be passed a DeviceStorageDB object when it is ready.
   // The options argument is an object that specifies various options:
-  // 
+  //
   //   mediaType: the device storage type: 'pictures', 'movies', 'music'.
   //   onchange: the callback function that gets change notifications
   //     1st callback argument is a string: 'created', 'deleted',
   //           'mounted' or 'umounted'
   //     2nd argument is an array of file info objects for add and remove.
-  // 
+  //
   //   These first two options are required. The rest are optional:
-  // 
+  //
   //   directory: a path beneath the device storage root
-  //   mimeTypes: an array of mime types (or filename extensions) 
+  //   mimeTypes: an array of mime types (or filename extensions)
   //      If this is specified, files that don't match will be ignored
   //   metadataParser: a function that takes a File object from DeviceStorage
   //      and returns an object of its metadata to be stored in the db
@@ -43,7 +43,7 @@ var createDeviceStorageDB = (function() {
     try {
       storage = navigator.getDeviceStorage(mediaType)[0];
     }
-    catch(e) {
+    catch (e) {
       console.error("createDeviceStorageDB: can't get DeviceStorage object");
       return;
     }
@@ -55,9 +55,9 @@ var createDeviceStorageDB = (function() {
       callback = options;
       options = undefined;
     }
-    if (!options) 
+    if (!options)
       options = {};
-    
+
     var indexes = options.indexes || [];
     var version = options.version || 0;
     var directory = options.directory || '';
@@ -82,7 +82,7 @@ var createDeviceStorageDB = (function() {
       // If there are already existing object stores, delete them all
       // If the version number changes we just want to start over.
       var existingStoreNames = db.objectStoreNames;
-      for(var i = 0; i < existingStoreNames.length; i++) {
+      for (var i = 0; i < existingStoreNames.length; i++) {
         db.deleteObjectStore(existingStoreNames);
       }
 
@@ -113,7 +113,7 @@ var createDeviceStorageDB = (function() {
   function DeviceStorageDB(storage, db, options) {
     this.storage = storage;
     this.db = db;
-    this.mediaType = options.mediaType
+    this.mediaType = options.mediaType;
     this.onchange = options.onchange;
     this.directory = options.directory;
     this.mimeTypes = options.mimeTypes || [];
@@ -126,7 +126,7 @@ var createDeviceStorageDB = (function() {
   }
 
   DeviceStorageDB.prototype = {
-    
+
     // Look up the specified filename in DeviceStorage and pass the
     // resulting File object to the specified callback.
     // XXX If the file does not exist, what happens? I think the
@@ -137,7 +137,7 @@ var createDeviceStorageDB = (function() {
         callback(getRequest.result);
       };
       getRequest.onerror = function() {
-        var errmsg = getRequest.error && getRequest.error.name
+        var errmsg = getRequest.error && getRequest.error.name;
         if (errback)
           errback(errmsg);
         else
@@ -149,9 +149,9 @@ var createDeviceStorageDB = (function() {
     // property (which must be one of the indexes, or null for the filename).
     // Direction is ascending or descending. Use whatever string
     // constant IndexedDB uses.  f is the function to pass each record to.
-    // 
+    //
     // Each record is an object like this:
-    // 
+    //
     // {
     //    // The basic fields are all from the File object
     //    name: // the filename
@@ -160,17 +160,17 @@ var createDeviceStorageDB = (function() {
     //    date: // file mod time
     //    metadata: // whatever object the metadata parser returns
     // }
-    // 
-    // 
+    //
+    //
     enumerate: function(key, range, direction, callback) {
       // The first three arguments are optional, but the callback
       // is required, and we don't want to have to pass three nulls
-      if (arguments.length > 0 && arguments.length < 4) 
-        callback = arguments[arguments.length-1];
+      if (arguments.length > 0 && arguments.length < 4)
+        callback = arguments[arguments.length - 1];
 
       var store = this.db.transaction('files').objectStore('files');
       var index;
-      
+
       // If a key is specified, look up the index for that key.
       // Otherwise, just use the basic object store with filename keys.
       if (key)
@@ -199,7 +199,7 @@ var createDeviceStorageDB = (function() {
     // when to rescan rather than doing it automatically. Until we have
     // change event notifications, gaia apps might want to do a scan
     // every time they are made visible.
-    // 
+    //
     // Filesystem changes discovered by a scan are generally
     // batched. If a scan discovers 10 new files, the information
     // about those files will generally be passed as an array to a the
@@ -207,7 +207,7 @@ var createDeviceStorageDB = (function() {
     // newly discovered file.  Apps can decide whether to handle
     // batches by processing each element individually or by just starting
     // fresh with a new call to enumerate().
-    // 
+    //
     // Scan details are not tightly specified, but the goal is to be
     // as efficient as possible.  We'll try to do a quick date-based
     // scan to look for new files and report those first. Following
@@ -220,13 +220,13 @@ var createDeviceStorageDB = (function() {
       // First, scan for new files since the last scan, if there was one
       // When the quickScan is done it will begin a full scan.  If we don't
       // have a last scan date, then we just begin a full scan immediately
-      if (dsdb.lastScanDate) 
+      if (dsdb.lastScanDate)
         quickScan(dsdb.lastScanDate);
       else {
         dsdb.lastScanDate = new Date();
         fullScan();
       }
-      
+
       // Do a quick scan and then follow with a full scan
       function quickScan(date) {
         var newfiles = [];
@@ -256,10 +256,10 @@ var createDeviceStorageDB = (function() {
               name: file.name,
               type: file.type,
               size: file.size,
-              date: file.lastModifiedDate,
+              date: file.lastModifiedDate
             };
             newfiles.push(fileinfo);
-            
+
             if (dsdb.metadataParser) {
               dsdb.metadataParser(file, function(metadata) {
                 fileinfo.metadata = metadata;
@@ -274,7 +274,7 @@ var createDeviceStorageDB = (function() {
               callback();
             }
           }
-          catch(e) {
+          catch (e) {
             console.error(e);
             cursor.continue();
           }
@@ -287,7 +287,7 @@ var createDeviceStorageDB = (function() {
           var store = transaction.objectStore('files');
 
           // Save the new files
-          for(var i = 0; i < newfiles.length; i++) {
+          for (var i = 0; i < newfiles.length; i++) {
             var fileinfo = newfiles[i];
 
             // When an existing file is overwritten, we should report
@@ -298,7 +298,7 @@ var createDeviceStorageDB = (function() {
 
             addRequest.onerror = function() {
               // It probably failed because a file by that name is
-              // already in the db. Don't save or report it now. We'll 
+              // already in the db. Don't save or report it now. We'll
               // handle it when we do a full scan.
               newfiles.splice(i, 1);  // remove the file
               i--;
@@ -317,7 +317,7 @@ var createDeviceStorageDB = (function() {
       // Pay attention to files whose size or date has changed and
       // treat those as deletions followed by insertions.
       // Sync up the database while stepping through the lists and
-      // then call the onchange handler to report deleted files and 
+      // then call the onchange handler to report deleted files and
       // created files.  (Report deleted files first because we model
       // file changes as deletions followed by creations)
       function fullScan() {
@@ -349,11 +349,11 @@ var createDeviceStorageDB = (function() {
 
         function compareLists(dbfiles, dsfiles) {
           // The dbfiles are sorted when we get them from the db.
-          // But the ds files are not sorted 
-          dsfiles.sort(function(a,b) {
-            if (a.name < b.name) 
+          // But the ds files are not sorted
+          dsfiles.sort(function(a, b) {
+            if (a.name < b.name)
               return -1;
-            else 
+            else
               return 1;
           });
 
@@ -362,21 +362,21 @@ var createDeviceStorageDB = (function() {
 
           // Loop through both the dsfiles and dbfiles lists
           var dsindex = 0, dbindex = 0;
-          while(true) {
+          while (true) {
             // Get the next DeviceStorage file or null
-            var dsfile
+            var dsfile;
             if (dsindex < dsfiles.length)
-              dsfile =  dsfiles.dsfiles[dsindex];
+              dsfile = dsfiles.dsfiles[dsindex];
             else
               dsfile = null;
 
             // Get the next DB file or null
             var dbfile;
             if (dbindex < dbfiles.length)
-              dbfile =  dbfiles.dbfiles[dbindex];
+              dbfile = dbfiles.dbfiles[dbindex];
             else
               dbfile = null;
-            
+
             // Case 1: both files are null.  If so, we're done.
             if (dsfile === null && dbfile === null)
               break;
@@ -396,7 +396,7 @@ var createDeviceStorageDB = (function() {
               dbindex++;
               continue;
             }
-            
+
             // Case 4: two files with the same name.
             // 4a: date and size are the same for both: do nothing
             // 4b: file has changed: it is both a deletion and a creation
@@ -433,7 +433,7 @@ var createDeviceStorageDB = (function() {
 
           // Deal with the deleted files first
           if (deletedFiles.length > 0) {
-            var transaction = this.db.transaction('files', 'readwrite')
+            var transaction = this.db.transaction('files', 'readwrite');
             var store = transaction.objectStore('files');
             deletedFiles.forEach(function(fileinfo) {
               store.delete(fileinfo.name);
@@ -442,8 +442,8 @@ var createDeviceStorageDB = (function() {
             // And then deal with the new files
             transaction.oncomplete = function() {
               dsdb.onchange('deleted', deletedFiles);
-              
-              if (createdFiles.length > 1) 
+
+              if (createdFiles.length > 1)
                 handleCreatedFiles();
             };
           }
@@ -464,7 +464,7 @@ var createDeviceStorageDB = (function() {
           // and then calls the callback. We
           function getMetadataForFile(n, callback) {
             var fileinfo = createdFiles[n];
-            var fileRequest = storage.get(fileinfo.name); 
+            var fileRequest = storage.get(fileinfo.name);
             fileRequest.onsuccess = function() {
               var file = fileRequest.result;
               dsdb.metadataParser(file, function(metadata) {
@@ -479,14 +479,14 @@ var createDeviceStorageDB = (function() {
           }
 
           function storeCreatedFiles() {
-            var transaction = this.db.transaction('files', 'readwrite')
+            var transaction = this.db.transaction('files', 'readwrite');
             var store = transaction.objectStore('files');
-            for(var i = 0; i < createdFiles.length; i++) {
+            for (var i = 0; i < createdFiles.length; i++) {
               store.add(createdFiles[i]);
             }
 
             // Now once we're done storing the files deliver a notification
-            dsdb.onchange("created", createdFiles);
+            dsdb.onchange('created', createdFiles);
           }
         }
       }
