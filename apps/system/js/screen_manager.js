79038f76ffe8289e923b66a5980c3d1f88422099
Timothy Guan-tin Chien <timdream@gmail.com>
2012-06-23 12:41:15 +0800

    Actually pass the idleObserver into the the Idle API

--- apps/system/js/screen_manager.js
+++ apps/system/js/screen_manager.js
@@ -170,7 +170,7 @@ var ScreenManager = {
       return;
 
     // Remove the original observer.
-    navigator.removeIdleObserver();
+    navigator.removeIdleObserver(this.idleObserver);
 
     // If time = 0, then there is no idle timeout to set.
     if (!time)
@@ -179,7 +179,7 @@ var ScreenManager = {
     var self = this;
     this.idleObserver.time = time;
 
-    navigator.addIdleObserver();
+    navigator.addIdleObserver(this.idleObserver);
   },
 
   fireScreenChangeEvent: function scm_fireScreenChangeEvent() {
