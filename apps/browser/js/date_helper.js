178beed5b72944d9320e50093d812fdffd348427
James Lal <james@lightsofapollo.com>
2012-06-24 05:03:37 +0200

    fix first day of the week

--- apps/browser/js/date_helper.js
+++ apps/browser/js/date_helper.js
@@ -29,8 +29,16 @@ DateHelper = {
   thisWeekStarted: function dh_thisWeekStarted() {
     var now = new Date();
     var dayOfTheWeek = now.getDay();
-    var offset = 1 - dayOfTheWeek;
-    var firstDay = now.valueOf() + offset * 86400000;
+    var firstDay = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      //getDay is zero based so if today
+      //is the start of the week it will not
+      //change the date. Also if we get
+      //into negative days the date object
+      //handles that too...
+      now.getDate() - dayOfTheWeek
+    );
     return this.getMidnight(firstDay);
   },
 
