6701966a24a2304214f20fef1530f01365415b75
Francisco Jordano <francisco.jordano@o2.com>
2012-06-26 15:00:42 +0200

    Hack to allow open the sms app from the contacts with the phone number as a parameter

---
+++ apps/contacts/js/sms_integration.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var SmsIntegration = (function() {
+  
+  var smsApp;
+  
+  //Walk the apps and look for the sms one
+  navigator.mozApps.mgmt.getAll().onsuccess = function onSuccess(e) {
+    var apps = e.target.result;
+    apps.forEach(function parseApp(app) {
+      if(app.manifest.permissions) {
+        
+        var keys = Object.keys(app.manifest.permissions);
+        var permissions = keys.map(function map_perm(key) {
+          return app.manifest.permissions[key];
+        });
+        
+        if(permissions.indexOf('sms') != -1) {
+          smsApp = app;
+          return;
+        }
+      }
+    });
+  };
+  
+  return {
+    sendSms: function(phoneNumber) {
+      smsApp.launch('#' + phoneNumber);
+    }
+  };
+  
+})();
