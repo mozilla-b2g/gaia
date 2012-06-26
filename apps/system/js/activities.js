f4dbde0fb4287f8067660642897b97345a981177
Etienne Segonzac <etienne@segonzac.info>
2012-06-26 18:32:20 +0200

    Updating the WebActivities system part to make use of the ListMenu component and to let the System Message Handler handle the window opening.

--- apps/system/js/activities.js
+++ apps/system/js/activities.js
@@ -4,49 +4,20 @@
 'use strict';
 
 var Activities = {
-  get element() {
-    delete this.element;
-    return this.element = document.getElementById('activities');
-  },
-
-  get visible() {
-    return this.element.classList.contains('visible');
-  },
-
-  get list() {
-    delete this.list;
-    return this.list = this.element.querySelector('.overlay-menu');
-  },
-
   init: function act_init() {
     window.addEventListener('mozChromeEvent', this);
-    this.element.addEventListener('click', this);
   },
 
   handleEvent: function act_handleEvent(evt) {
-    switch (evt.type) {
-      case 'click':
-        var choice = evt.target.dataset.choice || null;
-        this.choose(choice);
-        break;
-      case 'mozChromeEvent':
-        var detail = evt.detail;
-        if (detail.type !== 'activity-choice')
-          return;
+    if (evt.type !== 'mozChromeEvent')
+      return;
 
-        this.list.innerHTML = this._listFragment(detail.choices);
-        this._id = detail.id;
-        this.show();
-        break;
-    }
-  },
+    var detail = evt.detail;
+    if (detail.type !== 'activity-choice')
+      return;
 
-  show: function act_show() {
-    this.element.classList.add('visible');
-  },
-
-  hide: function act_hide() {
-    this.element.classList.remove('visible');
+    ListMenu.request(this._listItems(detail.choices), this.choose.bind(this));
+    this._id = detail.id;
   },
 
   choose: function act_choose(choice) {
@@ -59,17 +30,16 @@ var Activities = {
     window.dispatchEvent(event);
 
     delete this._id;
-    this.hide();
   },
 
-  _listFragment: function act_listFragment(choices) {
-    var result = '';
-    choices.forEach(function(choice, index) {
-      result += '<div data-choice="' + index + '">' + choice + '</div>';
-    });
-    result += '<div>Cancel</div>';
+  _listItems: function act_listItems(choices) {
+    var items = [];
 
-    return result;
+    choices.forEach(function(choice, index) {
+      items.push({label: choice, value: index});
+    });
+
+    return items;
   }
 };
 
