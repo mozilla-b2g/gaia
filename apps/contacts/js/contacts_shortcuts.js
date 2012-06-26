eb0f3f136716a3ea29ec5e77571cda8c513b77e3
crdlc <crdlc@tid.es>
2012-06-26 16:25:48 +0200

    bug fixed in time to hide current letter

--- apps/contacts/js/contacts_shortcuts.js
+++ apps/contacts/js/contacts_shortcuts.js
@@ -32,7 +32,7 @@
   function scrollEnd(evt) {
     evt.preventDefault();
     evt.stopPropagation();
-    overlayStyle.MozTransitionDelay = '3s';
+    overlayStyle.MozTransitionDelay = '0.3s';
     overlayStyle.MozTransitionDuration = '0.2s';
     overlayStyle.opacity = '0';
   }
