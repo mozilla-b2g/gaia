9a0f26db9a0bd19344f9eefd8f467322f32e6492
crdlc <crdlc@tid.es>
2012-06-27 19:58:53 +0200

    bug fixed showing photos in contacts

--- apps/contacts/js/contacts.js
+++ apps/contacts/js/contacts.js
@@ -349,6 +349,7 @@ var Contacts = (function() {
     var request = navigator.mozContacts.save(contact);
     request.onsuccess = function onsuccess() {
       myContact.id = contact.id;
+      myContact.photo = contact.photo;
       contactsList.refresh(myContact);
       reloadContactDetails(myContact);
       navigation.back();
