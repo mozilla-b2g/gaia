5c5e86b15f9a0ac650c6e91bfd1b72ba2eb7e091
fcampo <fcampo@novanotio.es>
2012-06-28 15:14:36 +0200

    Quick fix for timestamp ordering when painting the messages

--- apps/sms/js/sms.js
+++ apps/sms/js/sms.js
@@ -763,6 +763,14 @@ var ConversationView = {
     });
 
     MessageManager.getMessages(function mm_getMessages(messages) {
+      /** QUICK and dirty fix for the timestamp issues, 
+       * it seems that API call does not give the messages ordered 
+       * so we need to sort the array
+       */
+      messages.sort(function(a,b){
+        return a.timestamp - b.timestamp;
+      });
+      
       var lastMessage = messages[messages.length - 1];
       if (pendingMsg &&
           (!lastMessage || lastMessage.id !== pendingMsg.id))
@@ -773,7 +781,6 @@ var ConversationView = {
 
       for (var i = 0; i < messages.length; i++) {
         var msg = messages[i];
-
         if (!msg.read)
           unreadList.push(msg.id);
 
