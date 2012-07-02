2e171f0e7058e56d39201005685fdb985442c131
Gregor Wagner <anygregor@gmail.com>
2012-07-02 13:11:26 -0700

    rename mozIndexedDB to indexedDB

--- apps/homescreen/js/state.js
+++ apps/homescreen/js/state.js
@@ -11,7 +11,7 @@ const HomeState = (function() {
   function openDB(success, error) {
     try {
       var indexedDB = window.indexedDB || window.webkitIndexedDB ||
-                      window.mozIndexedDB || window.msIndexedDB;
+                      window.msIndexedDB;
     } catch (e) {
       error(e);
       return;
