f5d83865e8d89aede7451854ff33f591c8e5ca5d
crdlc <crdlc@tid.es>
2012-07-04 19:24:50 +0200

    bug fixed when users move an application to previous page and it is full

--- apps/homescreen/js/grid.js
+++ apps/homescreen/js/grid.js
@@ -619,32 +619,42 @@ const GridManager = (function() {
       var x = status.cCoords.x;
       if (dirCtrl.limitNext(x)) {
         this.isDisabledDrop = true;
-        if (!this.isDisabledCheckingLimits) {
-          var curPageObj = pageHelper.getCurrent();
-          if (pages.current < pages.total - 1) {
-            curPageObj.remove(draggableIcon);
-            pageHelper.getNext().prependIcon(draggableIcon);
-            this.setDisabledCheckingLimits(true);
-            this.transitioning = true;
-            goNext(this.onNavigationEnd);
-          } else if (curPageObj.getNumApps() > 1) {
-            // New page if there are two or more icons
-            curPageObj.remove(draggableIcon);
-            pageHelper.push([draggableIcon]);
-            this.setDisabledCheckingLimits(true);
-            this.transitioning = true;
-            goNext(this.onNavigationEnd);
-          }
+        if (this.isDisabledCheckingLimits) {
+          return;
+        }
+
+        var curPageObj = pageHelper.getCurrent();
+        if (pages.current < pages.total - 1) {
+          curPageObj.remove(draggableIcon);
+          pageHelper.getNext().prependIcon(draggableIcon);
+          this.setDisabledCheckingLimits(true);
+          this.transitioning = true;
+          goNext(this.onNavigationEnd);
+        } else if (curPageObj.getNumApps() > 1) {
+          // New page if there are two or more icons
+          curPageObj.remove(draggableIcon);
+          pageHelper.push([draggableIcon]);
+          this.setDisabledCheckingLimits(true);
+          this.transitioning = true;
+          goNext(this.onNavigationEnd);
         }
       } else if (dirCtrl.limitPrev(x)) {
         this.isDisabledDrop = true;
-        if (pages.current > 0 && !this.isDisabledCheckingLimits) {
-          pageHelper.getCurrent().remove(draggableIcon);
-          pageHelper.getPrevious().append(draggableIcon);
-          this.setDisabledCheckingLimits(true);
-          this.transitioning = true;
-          goPrev(this.onNavigationEnd);
+        if (pages.current === 0 || this.isDisabledCheckingLimits) {
+          return;
         }
+
+        var curPageObj = pageHelper.getCurrent();
+        curPageObj.remove(draggableIcon);
+        var prevPageObj = pageHelper.getPrevious();
+        if (prevPageObj.getNumApps() === pageHelper.getMaxPerPage()) {
+          var propagateIco = prevPageObj.popIcon();
+          curPageObj.prependIcon(propagateIco);
+        }
+        prevPageObj.append(draggableIcon);
+        this.setDisabledCheckingLimits(true);
+        this.transitioning = true;
+        goPrev(this.onNavigationEnd);
       } else if (this.transitioning) {
         this.isDisabledDrop = true;
       } else {
