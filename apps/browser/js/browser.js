9151b35a057174152ba61792736a108d32a60089
Dale Harvey <dale@arandomurl.com>
2012-07-04 01:03:30 +0100

    Refactor session history to use browser api (fixes #1118)

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -227,17 +227,14 @@ var Browser = {
     var urlInput = this.urlInput;
     switch (evt.type) {
       case 'submit':
-          this.go(evt);
+        this.go(evt);
         break;
 
       case 'keyup':
-        if (!this.currentTab || !this.currentTab.session.backLength() ||
-          evt.keyCode != evt.DOM_VK_ESCAPE)
-          break;
-
-        this.goBack();
-        evt.preventDefault();
-        break;
+        if (evt.keyCode === evt.DOM_VK_ESCAPE) {
+          this.showPageScreen();
+          this.urlInput.blur();
+        }
     }
   },
 
@@ -289,22 +286,25 @@ var Browser = {
   },
 
   goBack: function browser_goBack() {
-    this.currentTab.session.back();
+    this.currentTab.dom.goBack();
     this.refreshButtons();
   },
 
   goForward: function browser_goForward() {
-    this.currentTab.session.forward();
+    this.currentTab.dom.goForward();
     this.refreshButtons();
   },
 
   refreshButtons: function browser_refreshButtons() {
-    this.backButton.disabled = !this.currentTab.session.backLength();
-    this.forwardButton.disabled = !this.currentTab.session.forwardLength();
+    this.currentTab.dom.getCanGoBack().onsuccess = (function(e) {
+      this.backButton.disabled = !e.target.result;
+    }).bind(this);
+    this.currentTab.dom.getCanGoForward().onsuccess = (function(e) {
+      this.forwardButton.disabled = !e.target.result;
+    }).bind(this);
   },
 
   updateHistory: function browser_updateHistory(url) {
-    this.currentTab.session.pushState(null, '', url);
     GlobalHistory.addVisit(url);
     this.refreshButtons();
   },
@@ -566,7 +566,6 @@ var Browser = {
       url: url || null,
       title: null,
       loading: false,
-      session: new SessionHistory(),
       screenshot: null,
       security: null
     };
