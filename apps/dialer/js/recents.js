63c46675ed4ce5e8d7f46fdccab0272fde91aa47
Germán Toro del Valle <gtorodelvalle@gmail.com>
2012-07-05 15:21:13 +0200

    A couple of comments more by Etienne ;-)

--- apps/dialer/js/recents.js
+++ apps/dialer/js/recents.js
@@ -151,10 +151,9 @@ var Recents = {
     var length = commLogItemPhoneNumbers.length;
     for (var i = 0; i < length; i++) {
       Contacts.findByNumber(commLogItemPhoneNumbers[i].textContent.trim(),
-        function re_contactCallBack() {
-          var contact = arguments[1];
+        function re_contactCallBack(phoneNumberEl, contact) {
           if (contact && contact.name) {
-            arguments[0].textContent = contact.name;
+            phoneNumberEl.textContent = contact.name;
           }
         }.bind(this, commLogItemPhoneNumbers[i]));
     }
