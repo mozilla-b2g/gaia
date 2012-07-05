e74f71ce11c0ed3cb7d629832c640164fa314cc5
Vivien Nicolas <21@vingtetun.org>
2012-07-05 13:25:28 +0200

    Fix some lint warnings

--- apps/homescreen/js/grid.js
+++ apps/homescreen/js/grid.js
@@ -714,25 +714,28 @@ const GridManager = (function() {
      */
     move: function dg_move(overlapElem) {
       draggableIcon.onDragMove(status.cCoords.x, status.cCoords.y);
+
       var currentPage = pageHelper.getCurrent();
-      if (currentPage.ready) {
-        this.checkLimits();
-        if (this.isDisabledDrop) {
-          return;
-        }
-        var classList = overlapElem.classList;
-        if (classList.contains('icon') || classList.contains('options')) {
-          var overlapElemOrigin = overlapElem.dataset.origin;
-          currentPage.drop(draggableIconOrigin, overlapElemOrigin);
-        } else if (classList.contains('page')) {
-          var lastIcon = currentPage.getLastIcon();
-          if (lastIcon && status.cCoords.y > lastIcon.getTop()
-              && overlapElem !== lastIcon) {
-            currentPage.drop(draggableIconOrigin, lastIcon.getOrigin());
-          }
-        }
-      } else {
+      if (!currentPage.ready) {
         currentPage.onReArranged = dispatchMouseMoveEvent;
+        return;
+      }
+
+      this.checkLimits();
+      if (this.isDisabledDrop) {
+        return;
+      }
+
+      var classList = overlapElem.classList;
+      if (classList.contains('icon') || classList.contains('options')) {
+        var overlapElemOrigin = overlapElem.dataset.origin;
+        currentPage.drop(draggableIconOrigin, overlapElemOrigin);
+      } else if (classList.contains('page')) {
+        var lastIcon = currentPage.getLastIcon();
+        if (lastIcon && status.cCoords.y > lastIcon.getTop() &&
+            overlapElem !== lastIcon) {
+          currentPage.drop(draggableIconOrigin, lastIcon.getOrigin());
+        }
       }
     }
   };
