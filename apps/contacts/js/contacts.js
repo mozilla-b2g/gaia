a1257e05d54a4ce81f7a929b4d406e27cbe4b78c
Alberto Pastor <albert.pastor@gmail.com>
2012-07-10 17:29:52 +0100

    Avoiding repeated ids and encapsulating comments on 1 li

--- apps/contacts/js/contacts.js
+++ apps/contacts/js/contacts.js
@@ -258,30 +258,33 @@ var Contacts = (function() {
     var listContainer = document.getElementById('details-list');
     listContainer.innerHTML = '';
 
-    var phonesTemplate = document.getElementById('phone-details-template');
+    var phonesTemplate = document.getElementById('phone-details-template-#i#');
     for (var tel in contact.tel) {
       var currentTel = contact.tel[tel];
       var telField = {
         number: currentTel.number || '',
         type: currentTel.type || TAG_OPTIONS['phone-type'][0].value,
-        notes: ''
+        notes: '',
+        i: tel
       };
       var template = utils.templates.render(phonesTemplate, telField);
       listContainer.appendChild(template);
     }
 
-    var emailsTemplate = document.getElementById('email-details-template');
+    var emailsTemplate = document.getElementById('email-details-template-#i#');
     for (var email in contact.email) {
       var currentEmail = contact.email[email];
       var emailField = {
         address: currentEmail['address'] || '',
-        type: currentEmail['type'] || ''
+        type: currentEmail['type'] || '',
+        i: email
       };
       var template = utils.templates.render(emailsTemplate, emailField);
       listContainer.appendChild(template);
     }
 
-    var addressesTemplate = document.getElementById('address-details-template');
+    var selector = document.getElementById('address-details-template-#i#');
+    var addressesTemplate = selector;
     for (var i in contact.adr) {
       var currentAddress = contact.adr[i];
       var addressField = {
@@ -289,24 +292,28 @@ var Contacts = (function() {
         postalCode: currentAddress['postalCode'] || '',
         locality: currentAddress['locality'] || '',
         countryName: currentAddress['countryName'] || '',
-        type: currentAddress['type'] || TAG_OPTIONS['address-type'][0].value
+        type: currentAddress['type'] || TAG_OPTIONS['address-type'][0].value,
+        i: i
       };
       var template = utils.templates.render(addressesTemplate, addressField);
       listContainer.appendChild(template);
     }
 
     if (contact.note && contact.note.length > 0) {
+      var container = document.createElement('li');
       var title = document.createElement('h2');
       title.textContent = 'Comments';
-      listContainer.appendChild(title);
-      var notesTemplate = document.getElementById('note-details-template');
+      container.appendChild(title);
+      var notesTemplate = document.getElementById('note-details-template-#i#');
       for (var i in contact.note) {
         var currentNote = contact.note[i];
         var noteField = {
-          note: currentNote || ''
+          note: currentNote || '',
+          i: i
         };
         var template = utils.templates.render(notesTemplate, noteField);
-        listContainer.appendChild(template);
+        container.appendChild(template);
+        listContainer.appendChild(container);
       }
     }
 
