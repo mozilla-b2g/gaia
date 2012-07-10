97108e8edddbd1010c7322b57c40d584ca61004f
lodr <salva@unoyunodiez.com>
2012-07-10 13:21:03 +0200

    Code improvement and avoiding showing keyboard for special input types

--- apps/keyboard/js/keyboard.js
+++ apps/keyboard/js/keyboard.js
@@ -132,10 +132,14 @@ const IMEManager = {
     }
 
     window.navigator.mozKeyboard.onfocuschange = function(e) {
+      var exclusionList = [
+        'button', 'checkbox', 'file',
+        'image', 'reset', 'submit'
+      ];
       if (e.detail.type === 'blur') {
         IMEController.hideIME();
       } else {
-        if (e.detail.type != 'submit')
+        if (exclusionList.indexOf(e.detail.type) === -1)
           IMEController.showIME(e.detail.type);
       }
     };
