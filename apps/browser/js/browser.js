2484c212a780448728a511b4ad762b5df7efe320
Etienne Segonzac <etienne@segonzac.info>
2012-07-12 16:44:00 +0200

    Removing old and outdated WebActivity support for the browser.

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -87,19 +87,7 @@ var Browser = {
     Places.init((function() {
       this.selectTab(this.createTab());
       this.showPageScreen();
-
-      // Activity handler ready from there
-      navigator.setActivityHandler((function(pendingActivity) {
-        var url = pendingActivity.activity.data.value;
-        this.selectTab(this.createTab());
-        this.navigate(url);
-      }).bind(this));
     }).bind(this));
-
-    // Registering for viewing URLs
-    navigator.registerActivityHandler('view',
-                                      { type: 'url' },
-                                      'Browser');
   },
 
   // Clicking the page preview on the left gutter of the tab page opens
