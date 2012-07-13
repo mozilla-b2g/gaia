5c3ba6c0a5001cac9b9eb2316589814b9638cb27
Yuan Xulei <xyuan@mozilla.com>
2012-07-13 15:25:01 +0800

    fix the pending symbol panel bug that simplified Chinese will be shown in the traditional Chinese mode.

--- apps/keyboard/js/imes/jspinyin/jspinyin.js
+++ apps/keyboard/js/imes/jspinyin/jspinyin.js
@@ -401,12 +401,14 @@ IMEngine.prototype = {
    * @return {void}  No return value.
    */
   _sendCandidates: function engine_sendCandidates(candidates) {
-    this._firstCandidate = (candidates.length > 0) ? candidates[0][0] : '';
     var list = [];
     var len = candidates.length;
     for (var id = 0; id < len; id++) {
       var strs = candidates[id];
       var cand = this._inputTraditionalChinese ? strs[1] : strs[0];
+      if (id == 0) {
+        this._firstCandidate = cand;
+      }
       var data = new IMEngine.CandidateData(id, strs);
       list.push([cand, data.serialize()]);
     }
