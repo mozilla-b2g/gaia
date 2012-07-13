32666ad3f301756c9a4dbb314541b789d36bfc4c
Vivien Nicolas <21@vingtetun.org>
2012-07-13 16:57:57 +0200

    Remove a forgotten console.log

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -40,7 +40,6 @@ function startup() {
   } else {
     var host = document.location.host;
     var domain = host.replace(/(^[\w\d]+\.)?([\w\d]+\.[a-z]+)/, '$2');
-console.log(document.location.protocol);
     var src = document.location.protocol + '//homescreen.' + domain;
   }
   document.getElementById('homescreen').src = src;
