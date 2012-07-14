f58d4032e7f3a05626385a0c7c3c563354688faf
David Flanagan <dflanagan@mozilla.com>
2012-07-13 22:26:51 -0700

    implement single photo delete

--- apps/gallery/js/gallery.js
+++ apps/gallery/js/gallery.js
@@ -292,20 +292,32 @@ function deleteImage(n) {
   // Remove the image from the array
   var deletedImageData = images.splice(n, 1)[0];
 
+  // Delete the file from the MediaDB. This removes the db entry and
+  // deletes the file in device storage. This method returns immediately
+  // and the deletion happens asynchronously.
+  photodb.deleteFile(deletedImageData.name);
+
   // Remove the corresponding thumbanail
   var thumbnailElts = thumbnails.querySelectorAll('.thumbnail');
   thumbnails.removeChild(thumbnailElts[n]);
 
-  if (n < currentPhotoIndex) 
+  // Change the index associated with all the thumbnails after the deleted one
+  // This keeps the data-index attribute of each thumbnail element in sync
+  // with the images[] array.
+  for (var i = n + 1; i < thumbnailElts.length; i++) {
+    thumbnailElts[i].dataset.index = i - 1;
+  }
+
+  // Adjust currentPhotoIndex, too, if we have to.
+  if (n < currentPhotoIndex)
     currentPhotoIndex--;
 
-  // If we're displaying a single photo, redisplay because it just changed.
+  // If we're in single photo display mode, then the only way this function,
+  // gets called is when we delete the currently displayed photo.  This means
+  // that we need to redisplay.
   if (currentView === photoView) {
     showPhoto(currentPhotoIndex);
   }
-
-  // XXX
-  // Note that we still have to actually delete the photo from device storage!
 }
 
 //
