ef6cbb6c053c4a88bbad252d78a84ba21ab4c8cf
Vivien Nicolas <21@vingtetun.org>
2012-07-16 21:42:19 +0200

    Move some code in homescreen.js

--- apps/homescreen/js/homescreen.js
+++ apps/homescreen/js/homescreen.js
@@ -2,19 +2,27 @@
 'use strict';
 
 const Homescreen = (function() {
-  var host = document.location.host;
-  var domain = host.replace(/(^[\w\d]+\.)?([\w\d]+\.[a-z]+)/, '$2');
-  Search.init(domain);
-
-  PaginationBar.init('.paginationScroller');
-
   function initUI() {
     setLocale();
-    DockManager.init(document.querySelector('#footer'));
     GridManager.init('.apps', function gm_init() {
-      GridManager.goToPage(1);
+      // Initialize the search page
+      var host = document.location.host;
+      var domain = host.replace(/(^[\w\d]+\.)?([\w\d]+\.[a-z]+)/, '$2');
+      Search.init(domain);
+
+      // Initialize the pagination scroller
+      PaginationBar.init('.paginationScroller');
       PaginationBar.show();
+
+      // Initialize the dock
+      DockManager.init(document.querySelector('#footer'));
+
+      // Initialize drag&drop
       DragDropManager.init();
+
+      // Go to the first page
+      GridManager.goToPage(1);
+
       window.addEventListener('localized', function localize() {
         setLocale();
         GridManager.localize();
@@ -22,35 +30,6 @@ const Homescreen = (function() {
       });
     });
   }
-
-  function setLocale() {
-    // set the 'lang' and 'dir' attributes to <html> when the page is translated
-    document.documentElement.lang = navigator.mozL10n.language.code;
-    document.documentElement.dir = navigator.mozL10n.language.direction;
-  }
-
-  function start() {
-    if (Applications.isReady()) {
-      initUI();
-    } else {
-      Applications.addEventListener('ready', initUI);
-    }
-  }
-
-  HomeState.init(function success(onUpgradeNeeded) {
-    if (!onUpgradeNeeded) {
-      start();
-      return;
-    }
-
-    // First time the database is empty -> Dock by default
-    var appsInDockByDef = ['browser', 'dialer', 'music', 'gallery'];
-    var protocol = window.location.protocol;
-    appsInDockByDef = appsInDockByDef.map(function mapApp(name) {
-      return protocol + '//' + name + '.' + domain;
-    });
-    HomeState.saveShortcuts(appsInDockByDef, start, start);
-  }, start);
 
   // XXX Currently the home button communicate only with the
   // system application. It should be an activity that will
@@ -77,6 +56,35 @@ const Homescreen = (function() {
         break;
     }
   });
+
+  function setLocale() {
+    // set the 'lang' and 'dir' attributes to <html> when the page is translated
+    document.documentElement.lang = navigator.mozL10n.language.code;
+    document.documentElement.dir = navigator.mozL10n.language.direction;
+  }
+
+  function start() {
+    if (Applications.isReady()) {
+      initUI();
+      return;
+    }
+    Applications.addEventListener('ready', initUI);
+  }
+
+  HomeState.init(function success(onUpgradeNeeded) {
+    if (!onUpgradeNeeded) {
+      start();
+      return;
+    }
+
+    // First time the database is empty -> Dock by default
+    var appsInDockByDef = ['browser', 'dialer', 'music', 'gallery'];
+    var protocol = window.location.protocol;
+    appsInDockByDef = appsInDockByDef.map(function mapApp(name) {
+      return protocol + '//' + name + '.' + domain;
+    });
+    HomeState.saveShortcuts(appsInDockByDef, start, start);
+  }, start);
 
   // Listening for installed apps
   Applications.addEventListener('install', function oninstall(app) {
@@ -106,10 +114,7 @@ const Homescreen = (function() {
       Permissions.show(title, body,
                        function onAccept() { app.uninstall() },
                        function onCancel() {});
-    },
-
-    isIcongridInViewport: function h_isIcongridInViewport() {
-      return GridManager.pageHelper.getCurrentPageNumber() >= 1;
     }
   };
 })();
+
