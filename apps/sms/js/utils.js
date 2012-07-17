506ae42374d879e4abbbf627f7f2378b0e8ecc46
Borja Salguero <borja.salguero@gmail.com>
2012-07-17 12:37:32 +0200

    Added H2 styles and automatic update of headers

--- apps/sms/js/utils.js
+++ apps/sms/js/utils.js
@@ -7,7 +7,28 @@
 var _ = navigator.mozL10n.get;
 
 var Utils = {
-
+  updateHeaders: function ut_updateHeaders() {
+    if (!Utils.updating) {
+      Utils.updating = true;
+      Utils.updateTimer = setInterval(function() {
+        var elementsToUpdate =
+        document.querySelectorAll('h2[data-time-update]');
+        if (elementsToUpdate.length > 0) {
+          for (var i = 0; i < elementsToUpdate.length; i++) {
+            var ts = elementsToUpdate[i].getAttribute('data-time');
+            var tmpHeaderDate = Utils.getHeaderDate(ts);
+            var currentHeader = elementsToUpdate[i].innerHTML;
+            if (tmpHeaderDate != currentHeader) {
+              elementsToUpdate[i].innerHTML = tmpHeaderDate;
+            }
+          }
+        } else {
+          clearInterval(Utils.updateTimer);
+          Utils.updating = false;
+        }
+      },60000);
+    }
+  },
   escapeHTML: function ut_escapeHTML(str, escapeQuotes) {
     var span = document.createElement('span');
     span.textContent = str;
