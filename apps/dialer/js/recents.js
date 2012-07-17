085d670a57f6488d8949965ccd0c04d7efe8cc9d
Germán Toro del Valle <gtorodelvalle@gmail.com>
2012-07-17 19:22:09 +0200

    Dates in HH:MM A/PM format and gjslint fixing.

--- apps/dialer/js/recents.js
+++ apps/dialer/js/recents.js
@@ -230,7 +230,8 @@ var Recents = {
     var daysElementsLength = daysElements.length;
     var itemSelector = '.log-item';
     var callLogItems, length, phoneNumber, callType, callCount, callDate,
-      sameTypeCallSelector, sameTypeCall, sameTypeCallSelectorAux, sameTypeCallAux,
+      sameTypeCallSelector, sameTypeCall, sameTypeCallSelectorAux,
+      sameTypeCallAux,
       sameTypeCallCount;
     for (var dayElementsCounter = 0; dayElementsCounter < daysElementsLength;
       dayElementsCounter++) {
@@ -240,19 +241,28 @@ var Recents = {
       for (var i = 0; i < length; i++) {
         phoneNumber = callLogItems[i].dataset.num.trim();
         callType = callLogItems[i].dataset.type;
-        callCount = (callLogItems[i].dataset.count ? parseInt(callLogItems[i].dataset.count) : 1);
+        callCount = (callLogItems[i].dataset.count ?
+          parseInt(callLogItems[i].dataset.count) : 1);
         callDate = callLogItems[i].dataset.date;
         if (callType.indexOf('dialing') != -1) {
-          sameTypeCallSelector = '[data-num^="' + phoneNumber + '"][data-type^="dialing"][data-count]:not(.hide)';
-          sameTypeCall = daysElements[dayElementsCounter].querySelector(sameTypeCallSelector);
-        } else if(callType.indexOf('incoming-connected') != -1) {
-          sameTypeCallSelector = '[data-num^="' + phoneNumber + '"][data-type="incoming-connected"][data-count]:not(.hide)';
-          sameTypeCall = daysElements[dayElementsCounter].querySelector(sameTypeCallSelector);
+          sameTypeCallSelector = '[data-num^="' + phoneNumber +
+            '"][data-type="dialing-refused"][data-count]:not(.hide)';
+          sameTypeCall = daysElements[dayElementsCounter].
+            querySelector(sameTypeCallSelector);
+        } else if (callType.indexOf('incoming-connected') != -1) {
+          sameTypeCallSelector = '[data-num^="' + phoneNumber +
+            '"][data-type="incoming-connected"][data-count]:not(.hide)';
+          sameTypeCall = daysElements[dayElementsCounter].
+            querySelector(sameTypeCallSelector);
         } else {
-          sameTypeCallSelector = '[data-num^="' + phoneNumber + '"][data-type="incoming"][data-count]:not(.hide)';
-          sameTypeCall = daysElements[dayElementsCounter].querySelector(sameTypeCallSelector);
-          sameTypeCallSelectorAux = '[data-num^="' + phoneNumber + '"][data-type="incoming-refused"][data-count]:not(.hide)';
-          sameTypeCallAux = daysElements[dayElementsCounter].querySelector(sameTypeCallSelectorAux);
+          sameTypeCallSelector = '[data-num^="' + phoneNumber +
+            '"][data-type="incoming"][data-count]:not(.hide)';
+          sameTypeCall = daysElements[dayElementsCounter].
+            querySelector(sameTypeCallSelector);
+          sameTypeCallSelectorAux = '[data-num^="' + phoneNumber +
+            '"][data-type="incoming-refused"][data-count]:not(.hide)';
+          sameTypeCallAux = daysElements[dayElementsCounter].
+            querySelector(sameTypeCallSelectorAux);
           if (sameTypeCallAux) {
             if (sameTypeCall) {
               if (sameTypeCall.dataset.date < sameTypeCallAux.dataset.date) {
@@ -267,24 +277,27 @@ var Recents = {
         if (sameTypeCall) {
           sameTypeCallCount = parseInt(sameTypeCall.dataset.count);
           if (sameTypeCall.dataset.date > callDate) {
-            this.groupCalls(callLogItems[i], sameTypeCall, sameTypeCallCount, 1);
+            this.groupCalls(callLogItems[i], sameTypeCall,
+              sameTypeCallCount, 1);
           } else {
-            this.groupCalls(sameTypeCall, callLogItems[i], callCount, sameTypeCallCount);
+            this.groupCalls(sameTypeCall, callLogItems[i],
+              callCount, sameTypeCallCount);
           }
         }
       }
     }
   },
 
-  groupCalls: function re_groupCalls(olderCallEl, newerCallEl, count, increment) {
+  groupCalls: function re_groupCalls(olderCallEl, newerCallEl, count, inc) {
     olderCallEl.classList.add('hide');
     olderCallEl.classList.add('collapsed');
     var primaryInfo = newerCallEl.querySelector('.primary-info');
     var callDetails = primaryInfo.textContent.trim();
     var countIndex = callDetails.indexOf('(' + count + ')');
-    count += increment;
+    count += inc;
     if (countIndex != -1) {
-      primaryInfo.textContent = callDetails.substr(0, countIndex) + '(' + count + ')';
+      primaryInfo.textContent = callDetails.substr(0, countIndex) +
+        '(' + count + ')';
     } else {
       primaryInfo.textContent = callDetails + ' (' + count + ')';
     }
