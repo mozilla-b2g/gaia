b9fbc232ead826dd054922aa3b82892ce4a092dd
James Lal <james@lightsofapollo.com>
2012-07-18 18:00:12 -0700

    Calendar model

---
+++ apps/calendar/js/models/calendar.js
@@ -0,0 +1,96 @@
+(function(window) {
+
+  function Cal(options) {
+    var key;
+    if (typeof(options) === 'undefined') {
+      options = {};
+    }
+
+    for (key in options) {
+      if (options.hasOwnProperty(key)) {
+        this[key] = options[key];
+      }
+    }
+
+    if (this.provider && !options.hasOwnProperty('remote')) {
+      this.updateRemote(this.provider);
+      delete this.provider;
+    }
+  }
+
+  Cal.prototype = {
+
+    /**
+     * Local copy of calendars remote state.
+     * Taken from a calendar providers .toJSON method.
+     *
+     * @type {Object}
+     */
+    remote: null,
+
+    /**
+     * Last sync token used in previous
+     * event synchronization.
+     *
+     * @type {String}
+     */
+    lastEventSyncToken: null,
+
+    /**
+     * Last date of event synchronization.
+     *
+     * @type {Date}
+     */
+    lastEventSyncDate: null,
+
+    /**
+     * Updates remote with data from a calendar provider.
+     *
+     * @param {Calendar.Provider.Calendar.Abstract} provider remote.
+     */
+    updateRemote: function(provider) {
+      this.remote = provider.toJSON();
+    },
+
+    /**
+     * Checks if local and remote state differ
+     * via sync tokens. Returns true when
+     * local sync token and remote do not match.
+     * Does not account for local changes only
+     * when the server state has changed
+     * and we have not yet synchronized.
+     *
+     * @return {Boolean} true when sync needed.
+     */
+    eventSyncNeeded: function() {
+      var local = this.lastEventSyncToken;
+      var remote = this.remote.syncToken;
+
+      return local != remote;
+    },
+
+    get name() {
+      return this.remote.name;
+    },
+
+    get color() {
+      return this.remote.color;
+    },
+
+    get description() {
+      return this.remote.description;
+    },
+
+    toJSON: function() {
+      return {
+        remote: this.remote,
+        lastEventSyncDate: this.lastEventSyncDate,
+        lastEventSyncToken: this.lastEventSyncToken
+      };
+    }
+
+  };
+
+  Calendar.ns('Models').Calendar = Cal;
+
+}(this));
