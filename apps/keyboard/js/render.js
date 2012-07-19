4d307431a025817c6b8a2a95b029da3432d57ebd
lodr <salva@unoyunodiez.com>
2012-07-19 23:10:08 +0200

    Alternative key reduced to keep the keyboard fitting inside screen

--- apps/keyboard/js/render.js
+++ apps/keyboard/js/render.js
@@ -304,7 +304,7 @@ const IMERender = (function() {
       var dataset = [{'key': 'keycode', 'value': keyCode}];
       var label = keyChar.label || keyChar;
       var cssWidth =
-        key.scrollWidth * (1 + 0.5 * (label.length - original.length));
+        key.offsetWidth * (0.9 + 0.5 * (label.length - original.length));
       if (label.length > 1)
         dataset.push({'key': 'compositekey', 'value': label});
       content += buildKey(label, '', cssWidth + 'px', dataset);
