785962eb0a5dd5c54994f6e4923de1ffbbb56022
riadh <riadh@riadh-K70ID>
2012-07-20 21:41:22 +0200

    Renaming data Adding test

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -983,7 +983,7 @@ var Browser = {
   handleActivity: function browser_handleActivity(activity) {
     switch (activity.type) {
       case 'view':
-        var url = activity.source.data.string;
+        var url = activity.source.data.url;
         this.createTab(url);
         break;
     }
