3fe28ec0e6eb3845214916241d2bdeb90aeb8b90
Evelyn Hung <jj.evelyn@gmail.com>
2012-07-25 05:07:47 +0800

    let bluetooth in Setting app only set bluetooth.enable flag, not call bluetooth.setEnabled directly. (System app will do it.)

--- apps/settings/js/bluetooth.js
+++ apps/settings/js/bluetooth.js
@@ -3,79 +3,41 @@
 
 'use strict';
 
-// create a fake mozBluetooth if required (e.g. desktop browser)
-var gBluetooth = (function(window) {
-  var navigator = window.navigator;
-  if (('mozBluetooth' in navigator) && navigator.mozBluetooth)
-    return navigator.mozBluetooth;
-
-  var enabled = false;
-  return {
-    get enabled() {
-      return enabled;
-    },
-    setEnabled: function(value) {
-      enabled = value;
-      return { // fake DOM request
-        set onsuccess(callback) { setTimeout(callback, 500); },
-        set onerror(callback) {}
-      };
-    }
-  };
-})(this);
-
 // handle BlueTooth settings
 window.addEventListener('localized', function bluetoothSettings(evt) {
   var _ = navigator.mozL10n.get;
   var settings = window.navigator.mozSettings;
-  var infoBlock = document.querySelector('#bluetooth-status small');
+  var gBluetoothInfoBlock = document.querySelector('#bluetooth-status small');
+  var gBluetoothCheckBox = document.querySelector('#bluetooth-status input');
 
   // display Bluetooth power state
-  function updatePowerState(error) {
-    if (error) {
-      infoBlock.textContent = _('error');
-    } else {
-      infoBlock.textContent = gBluetooth.enabled ? _('enabled') : _('disabled');
-    }
+  function updatePowerState(value) {
+    gBluetoothInfoBlock.textContent = value ? _('enabled') : _('disabled');
+    gBluetoothCheckBox.checked = value;
   }
 
+  // activate main button
+  gBluetoothCheckBox.onchange = function changeBT() {
+    if (settings) {
+      settings.getLock().set({'bluetooth.enabled': this.checked});
+    }
+  };
+
   // enable Bluetooth if the related settings says so
-  // XXX this should rather be done in `System' than in `Settings'...
   if (settings) {
+    // register an observer to monitor bluetooth.enabled changes
+    settings.addObserver('bluetooth.enabled', function(event) {
+      updatePowerState(event.settingValue);
+    });
+
+    // startup, update status
     var req = settings.getLock().get('bluetooth.enabled');
 
     req.onsuccess = function bt_EnabledSuccess() {
       var enabled = req.result['bluetooth.enabled'];
-      gBluetooth.setEnabled(enabled);
-      document.querySelector('#bluetooth-status input').checked = enabled;
-    };
-
-    req.onerror = function bt_EnabledError() {
-      console.log('Settings error when reading bluetooth setting!');
+      updatePowerState(enabled);
     };
   }
 
-  // update BT description and setting
-  function changeBT() {
-    infoBlock.textContent = '';
-    var req = gBluetooth.setEnabled(this.checked);
-
-    req.onsuccess = function bt_enabledSuccess() {
-      updatePowerState();
-      if (settings) {
-        settings.getLock().set({
-          'bluetooth.enabled': gBluetooth.enabled
-        });
-      }
-    };
-
-    req.onerror = function bt_enabledError() {
-      updatePowerState(true);
-    };
-  };
-
-  // activate main button
-  document.querySelector('#bluetooth-status input').onchange = changeBT;
-  updatePowerState();
 });
 
