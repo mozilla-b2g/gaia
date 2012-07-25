a91addfdad8f8aad29ff56de85f9e35cf2907013
Evelyn Hung <jj.evelyn@gmail.com>
2012-07-26 03:53:47 +0800

    add fake bluetooth back

--- apps/settings/js/bluetooth.js
+++ apps/settings/js/bluetooth.js
@@ -3,6 +3,27 @@
 
 'use strict';
 
+// create a fake mozBluetooth if required (e.g. desktop browser)
+var gBluetooth = (function(window) {
+  var navigator = window.navigator;
+  if (('mozBluetooth' in navigator) && navigator.mozBluetooth)
+    return navigator.mozBluetooth;
+
+  var enabled = false;
+  return {
+    get enabled() {
+      return enabled;
+    },
+    setEnabled: function(value) {
+      enabled = value;
+      return { // fake DOM request
+        set onsuccess(callback) { setTimeout(callback, 500); },
+        set onerror(callback) {}
+      };
+    }
+  };
+})(this);
+
 // handle BlueTooth settings
 window.addEventListener('localized', function bluetoothSettings(evt) {
   var _ = navigator.mozL10n.get;
