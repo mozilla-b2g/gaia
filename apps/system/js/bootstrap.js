382558701f1d84bb29f991446c72750dc23a5924
Evelyn Hung <jj.evelyn@gmail.com>
2012-07-26 05:23:58 +0800

    remove Bluetooth.init in bootstrap

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -8,7 +8,6 @@ function startup() {
   SleepMenu.init();
   SourceView.init();
   Shortcuts.init();
-  Bluetooth.init();
 
   // We need to be sure to get the focus in order to wake up the screen
   // if the phone goes to sleep before any user interaction.
