c5d693da1875ba19162e2d3b9738364cf7a917a0
Fernando Jiménez <ferjmoreno@gmail.com>
2012-07-26 17:36:08 +0200

    Add mock payment provider

--- apps/system/js/payment.js
+++ apps/system/js/payment.js
@@ -24,7 +24,7 @@ var Payment = (function() {
       button.setAttribute('type', 'button');
       button.setAttribute('value', providers[i].typ);
       button.onclick = function selectProvider(evt) {
-        // Gaia send the selected payment provider back to Chrome
+        // Gaia sends the selected payment provider back to Chrome
         var event = document.createEvent('CustomEvent');
         event.initCustomEvent('mozContentEvent', true, true,
                               {id: eventId,
@@ -40,11 +40,11 @@ var Payment = (function() {
   };
 
   window.addEventListener('mozChromeEvent', function(e) {
+    eventId = e.detail.id;
     switch (e.detail.type) {
       // Chrome asks Gaia to show the payment provider selection dialog
       case 'open-payment-selection-dialog':
         providers = e.detail.paymentProviders;
-        eventId = e.detail.id;
         if (!providers || !eventId)
           return;
         // Once the trusted dialog is opened and the payment provider
@@ -60,6 +60,9 @@ var Payment = (function() {
         if (!e.detail.url)
           return;
         providers = null;
+        // TODO: For now, known payment providers (BlueVia and Mozilla Market)
+        //       only accepts the JWT by GET, so we just add it to the URL.
+        e.detail.url += e.detail.jwt;
         var frame = TrustedDialog.open(e.detail.url);
         // After creating the new frame containing the payment provider buy
         // flow, we send it back to chrome so payment callbacks can be
