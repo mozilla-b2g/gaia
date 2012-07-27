921d1149fc0cc05702b04af3523e7610ac00d810
Alive Kuo <alegnadise@gmail.com>
2012-07-27 13:14:15 -0700

    [R] rm dbg msg

--- apps/keyboard/js/controller.js
+++ apps/keyboard/js/controller.js
@@ -442,8 +442,6 @@ const IMEController = (function() {
       hidden: !!IMERender.ime.dataset.hidden
     };
 
-
-    console.log('=====',height);
     parent.postMessage(JSON.stringify(message), '*');
   }
 
