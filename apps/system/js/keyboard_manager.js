9df4fa0166a92b1c05f0cfa3ab828e4a36ffbb05
lodr <salva@unoyunodiez.com>
2012-07-30 13:03:18 +0200

    Added code to check if a ModalDialog is being shown. If so, as system has no "iframe", the currentApp is the modal dialog container and the height should be calculated separately.

--- apps/system/js/keyboard_manager.js
+++ apps/system/js/keyboard_manager.js
@@ -29,40 +29,45 @@ var KeyboardManager = (function() {
       return;
 
     var app = WindowManager.getDisplayedApp();
-    if (!app && !TrustedDialog.trustedDialogIsShown())
+    if (!app &&
+        !TrustedDialog.trustedDialogIsShown() &&
+        !ModalDialog.modalDialogIsShown())
       return;
 
-    var currentApp;
+    var currentApp, height;
     if (TrustedDialog.trustedDialogIsShown()) {
       currentApp = TrustedDialog.getFrame();
+      height = currentApp.getBoundingClientRect().height -
+               message.keyboardHeight;
+
+    // XXX: As system has no iframe, we calc the height separately
+    } else if (ModalDialog.modalDialogIsShown()) {
+      currentApp = document.getElementById('dialog-overlay');
+      height = window.innerHeight -
+               message.keyboardHeight;
     } else {
       WindowManager.setAppSize(app);
       currentApp = WindowManager.getAppFrame(app);
+      height = currentApp.getBoundingClientRect().height -
+               message.keyboardHeight;
     }
 
-    var dialogOverlay = document.getElementById('dialog-overlay');
-
-    var height = currentApp.getBoundingClientRect().height -
-                  message.keyboardHeight;
     keyboardOverlay.hidden = true;
 
     if (message.hidden) {
       keyboardFrame.classList.add('hide');
       keyboardFrame.classList.remove('visible');
-      dialogOverlay.style.height = (height + 20) + 'px';
       return;
     }
 
     if (!keyboardFrame.classList.contains('hide')) {
       currentApp.style.height = height + 'px';
-      dialogOverlay.style.height = (height + 20) + 'px';
       keyboardOverlay.style.height = (height + 20) + 'px';
       keyboardOverlay.hidden = false;
     } else {
       keyboardFrame.classList.remove('hide');
       keyboardFrame.addEventListener('transitionend', function keyboardShown() {
         keyboardFrame.removeEventListener('transitionend', keyboardShown);
-        dialogOverlay.style.height = (height + 20) + 'px';
         currentApp.style.height = height + 'px';
         keyboardOverlay.style.height = (height + 20) + 'px';
         keyboardOverlay.hidden = false;
