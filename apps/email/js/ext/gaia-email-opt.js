30d4e3585081ef9f078eb6d20fac4df8a3389a36
Jim Porter <jporter@mozilla.com>
2012-08-02 20:08:03 -0500

    Email/ActiveSync: Listen for changes to message state, improve folder sync performance

--- apps/email/js/ext/gaia-email-opt.js
+++ apps/email/js/ext/gaia-email-opt.js
@@ -26830,9 +26830,16 @@ function ActiveSyncFolderStorage(account, folderInfo, dbConn) {
   this._headers = [];
   this._bodiesBySuid = {};
 
-  this._loaded = 0;
+  this._onLoadListeners = [];
+
+  let loading = 0;
   function onLoaded(type, block) {
-    this._loaded++;
+    if (++loading == 2) {
+      this._loaded = true;
+      for (let [,listener] in Iterator(this._onLoadListeners))
+        listener();
+      this._onLoadListeners = [];
+    }
     if (!block)
       return;
 
@@ -26890,21 +26897,28 @@ ActiveSyncFolderStorage.prototype = {
   },
 
   _loadMessages: function(callback, deferred) {
+    let folderStorage = this;
+    let account = this.account;
+
+    if (!account.conn.connected) {
+      account.conn.autodiscover(function(config) {
+        // TODO: handle errors
+        folderStorage._loadMessages(callback, deferred);
+      });
+      return;
+    }
     if (this.folderMeta.syncKey === '0' && !deferred) {
       this._getSyncKey(this._loadMessages.bind(this, callback, true));
       return;
     }
 
-    let folderStorage = this;
-    let account = this.account;
     let as = $ascp.AirSync.Tags;
     let asb = $ascp.AirSyncBase.Tags;
-    let em = $ascp.Email.Tags;
 
     let w = new $wbxml.Writer('1.3', 1, 'UTF-8');
     w.stag(as.Sync)
        .stag(as.Collections)
-         .stag(as.Collection)
+         .stag(as.Collection);
 
     if (account.conn.currentVersionInt < $activesync.VersionInt('12.1'))
           w.tag(as.Class, 'Email');
@@ -26927,148 +26941,230 @@ ActiveSyncFolderStorage.prototype = {
      .etag();
 
     account.conn.doCommand(w, function(aError, aResponse) {
+      let added   = { headers: [], bodies: {} };
+      let changed = { headers: [], bodies: {} };
+      let deleted = [];
+      let status;
+
       if (aError)
         return;
       if (!aResponse) {
-        callback([], {});
+        callback(added, changed, deleted);
         return;
       }
 
       let e = new $wbxml.EventParser();
-      let headers = [];
-      let bodies = {};
-
       const base = [as.Sync, as.Collections, as.Collection];
+
+      e.addEventListener(base.concat(as.Status), function(node) {
+        status = node.children[0].textContent;
+      });
+
       e.addEventListener(base.concat(as.SyncKey), function(node) {
         folderStorage.folderMeta.syncKey = node.children[0].textContent;
       });
 
-      e.addEventListener(base.concat(as.Commands, as.Add, as.ApplicationData),
-                         function(node)
-      {
-        let guid = Date.now() + Math.random().toString(16).substr(1) +
-                   '@mozgaia';
-        let header = {
-          subject: null,
-          author: null,
-          date: null,
-          flags: [],
-          id: null,
-          suid: folderStorage.folderId + '/' + guid,
-          guid: guid,
-          hasAttachments: false,
-          snippet: null,
-        };
-        var body = {
-          to: null,
-          cc: null,
-          bcc: null,
-          replyTo: null,
-          attachments: null,
-          references: null,
-          bodyRep: null,
-        };
+      e.addEventListener(base.concat(as.Commands, [[as.Add, as.Change]]),
+                         function(node) {
+        let guid;
+        let msg;
 
         for (let [,child] in Iterator(node.children)) {
-          let childText = child.children.length &&
-            child.children[0].textContent;
-
           switch (child.tag) {
-          case em.Subject:
-            header.subject = childText;
+          case as.ServerId:
+            guid = child.children[0].textContent;
             break;
-          case em.From:
-            header.author = $mimelib.parseAddresses(childText)[0];
-            break;
-          case em.To:
-            body.to = $mimelib.parseAddresses(childText);
-            break;
-          case em.Cc:
-            nody.cc = $mimelib.parseAddresses(childText);
-            break;
-          case em.ReplyTo:
-            body.replyTo = $mimelib.parseAddresses(childText);
-            break;
-          case em.DateReceived:
-            header.date = new Date(childText).valueOf();
-            break;
-          case em.Read:
-            if (childText == '1')
-              header.flags.push('\\Seen');
-            break;
-          case em.Flag:
-            for (let [,grandchild] in Iterator(child.children)) {
-              if (grandchild.tag === em.Status &&
-                  grandchild.children[0].textContent !== '0')
-                header.flags.push('\\Flagged');
-            }
-            break;
-          case asb.Body: // ActiveSync 12.0+
-            for (let [,grandchild] in Iterator(child.children)) {
-              if (grandchild.tag === asb.Data) {
-                body.bodyRep = $quotechew.quoteProcessTextBody(
-                  grandchild.children[0].textContent);
-                header.snippet = $quotechew.generateSnippet(body.bodyRep);
-              }
-            }
-            break;
-          case em.Body: // pre-ActiveSync 12.0
-            body.bodyRep = $quotechew.quoteProcessTextBody(childText);
-            header.snippet = $quotechew.generateSnippet(body.bodyRep);
-            break;
-          case asb.Attachments: // ActiveSync 12.0+
-          case em.Attachments:  // pre-ActiveSync 12.0
-            header.hasAttachments = true;
-            body.attachments = [];
-            for (let [,attachmentNode] in Iterator(child.children)) {
-              if (attachmentNode.tag !== asb.Attachment &&
-                  attachmentNode.tag !== em.Attachment)
-                continue; // XXX: throw an error here??
-
-              let attachment = { type: 'text/plain' }; // XXX: this is lies
-              for (let [,attachData] in Iterator(attachmentNode.children)) {
-                switch (attachData.tag) {
-                case asb.DisplayName:
-                case em.DisplayName:
-                  attachment.name = attachData.children[0].textContent;
-                  break;
-                case asb.EstimatedDataSize:
-                case em.AttSize:
-                  attachment.sizeEstimate = attachData.children[0].textContent;
-                  break;
-                }
-              }
-              body.attachments.push(attachment);
-            }
+          case as.ApplicationData:
+            msg = folderStorage._processMessage(child);
             break;
           }
         }
 
-        headers.push(header);
-        bodies[header.suid] = body;
+        msg.headers.guid = guid;
+        msg.headers.suid = folderStorage.folderId + '/' + guid;
+
+        let collection = node.tag === as.Add ? added : changed;
+        collection.headers.push(msg.headers);
+        collection.bodies[msg.headers.suid] = msg.body;
+      });
+
+      e.addEventListener(base.concat(as.Commands, as.Delete), function(node) {
+        let guid;
+
+        for (let [,child] in Iterator(node.children)) {
+          switch (child.tag) {
+          case as.ServerId:
+            guid = child.children[0].textContent;
+            break;
+          }
+        }
+
+        deleted.push(guid);
       });
 
       e.run(aResponse);
 
-      headers.sort(function(a, b) a.date < b.date);
-      callback(headers, bodies);
+      if (status === '1') { // Success
+        added.headers.sort(function(a, b) a.date < b.date);
+        callback(added, changed, deleted);
+      }
+      else if (status === '3') { // Bad sync key
+        console.log('ActiveSync had a bad sync key');
+        // This should already be set to 0, but let's just be safe.
+        folderStorage.folderMeta.syncKey = '0';
+        folderStorage._needsPurge = true;
+        folderStorage._loadMessages(callback);
+      }
+      else {
+        console.error('Something went wrong during ActiveSync syncing and we ' +
+                      'got a status of ' + status);
+      }
     });
   },
 
+  _processMessage: function(node) {
+    let asb = $ascp.AirSyncBase.Tags;
+    let em = $ascp.Email.Tags;
+
+    let headers = {
+      get flags() {
+        let f = [];
+        if (this.read)
+          f.push('\\Seen');
+        if (this.flagged)
+          f.push('\\Flagged');
+        return f;
+      },
+    };
+    var body = {};
+
+    for (let [,child] in Iterator(node.children)) {
+      let childText = child.children.length &&
+                      child.children[0].textContent;
+
+      switch (child.tag) {
+      case em.Subject:
+        headers.subject = childText;
+        break;
+      case em.From:
+        headers.author = $mimelib.parseAddresses(childText)[0];
+        break;
+      case em.To:
+        body.to = $mimelib.parseAddresses(childText);
+        break;
+      case em.Cc:
+        body.cc = $mimelib.parseAddresses(childText);
+        break;
+      case em.ReplyTo:
+        body.replyTo = $mimelib.parseAddresses(childText);
+        break;
+      case em.DateReceived:
+        headers.date = new Date(childText).valueOf();
+        break;
+      case em.Read:
+        headers.read = (childText === '1');
+        break;
+      case em.Flag:
+        for (let [,grandchild] in Iterator(child.children)) {
+          if (grandchild.tag === em.Status)
+            headers.flagged = (grandchild.children[0].textContent !== '0');
+        }
+        break;
+      case asb.Body: // ActiveSync 12.0+
+        for (let [,grandchild] in Iterator(child.children)) {
+          if (grandchild.tag === asb.Data) {
+            body.bodyRep = $quotechew.quoteProcessTextBody(
+              grandchild.children[0].textContent);
+            headers.snippet = $quotechew.generateSnippet(body.bodyRep);
+          }
+        }
+        break;
+      case em.Body: // pre-ActiveSync 12.0
+        body.bodyRep = $quotechew.quoteProcessTextBody(childText);
+        headers.snippet = $quotechew.generateSnippet(body.bodyRep);
+        break;
+      case asb.Attachments: // ActiveSync 12.0+
+      case em.Attachments:  // pre-ActiveSync 12.0
+        headers.hasAttachments = true;
+        body.attachments = [];
+        for (let [,attachmentNode] in Iterator(child.children)) {
+          if (attachmentNode.tag !== asb.Attachment &&
+              attachmentNode.tag !== em.Attachment)
+            continue; // XXX: throw an error here??
+
+          let attachment = { type: 'text/plain' }; // XXX: this is lies
+          for (let [,attachData] in Iterator(attachmentNode.children)) {
+            switch (attachData.tag) {
+            case asb.DisplayName:
+            case em.DisplayName:
+              attachment.name = attachData.children[0].textContent;
+              break;
+            case asb.EstimatedDataSize:
+            case em.AttSize:
+              attachment.sizeEstimate = attachData.children[0].textContent;
+              break;
+            }
+          }
+          body.attachments.push(attachment);
+        }
+        break;
+      }
+    }
+
+    return { headers: headers, body: body };
+  },
+
   _sliceFolderMessages: function ffs__sliceFolderMessages(bridgeHandle) {
-    // XXX: this is a very silly way of handling this error case. Fix it.
-    if (this._loaded !== 2) {
-      bridgeHandle.sendSplice(0, 0, [], true, false);
+    if (!this._loaded) {
+      this._onLoadListeners.push(this._sliceFolderMessages
+                                     .bind(this, bridgeHandle));
       return;
     }
 
+    bridgeHandle.sendSplice(0, 0, this._headers, true, true);
+
     var folderStorage = this;
-    this._loadMessages(function(headers, bodies) {
-      folderStorage._headers = folderStorage._headers.concat(headers);
+    this._loadMessages(function(added, changed, deleted) {
+      if (folderStorage._needsPurge) {
+        bridgeHandle.sendSplice(0, folderStorage._headers.length, [], false,
+                                true);
+        folderStorage._headers = [];
+        folderStorage._bodiesBySuid = {};
+        folderStorage._needsPurge = false;
+      }
+
+      // Handle messages that have been deleted
+      for (let [,guid] in Iterator(deleted)) {
+        for (let [i, header] in Iterator(folderStorage._headers)) {
+          if (header.guid === guid) {
+            delete folderStorage._bodiesBySuid[header.suid];
+            folderStorage._headers.splice(i, 1);
+            bridgeHandle.sendSplice(i, 1, [], true, true);
+            break;
+          }
+        }
+      }
+
+      // Handle messages that have been changed
+      for (let [,newHeader] in Iterator(changed.headers)) {
+        for (let [i, oldHeader] in Iterator(folderStorage._headers)) {
+          if (oldHeader.guid === newHeader.guid) {
+            // TODO: update bodies, improve merging
+            for (let [k, v] in Iterator(newHeader))
+              oldHeader[k] = v;
+            bridgeHandle.sendSplice(i, 1, [oldHeader], true, true);
+            break;
+          }
+        }
+      }
+
+      // Handle messages that have been added; TODO: sort them properly
+      folderStorage._headers = folderStorage._headers.concat(added.headers);
       folderStorage._headers.sort(function(a, b) a.date < b.date);
-      for (let [k,v] in Iterator(bodies))
+      for (let [k,v] in Iterator(added.bodies))
         folderStorage._bodiesBySuid[k] = v;
-      bridgeHandle.sendSplice(0, 0, folderStorage._headers, true, false);
+
+      bridgeHandle.sendSplice(0, 0, added.headers, true, false);
       folderStorage.account.saveAccountState();
     });
   },
@@ -27255,8 +27351,8 @@ ActiveSyncAccount.prototype = {
         }
 
         if (node.tag == fh.Add)
-          account._addedFolder(folder.ServerId, folder.DisplayName,
-                               folder.Type);
+          account._addedFolder(folder.ServerId, folder.ParentId,
+                               folder.DisplayName, folder.Type);
         else
           account._deletedFolder(folder.ServerId);
       });
@@ -27279,19 +27375,28 @@ ActiveSyncAccount.prototype = {
     12: 'normal', // User-created mail folder
   },
 
-  _addedFolder: function as__addedFolder(serverId, displayName, typeNum) {
+  _addedFolder: function as__addedFolder(serverId, parentId, displayName,
+                                         typeNum) {
     if (!(typeNum in this._folderTypes))
       return; // Not a folder type we care about.
 
-    var folderId = this.id + '/' + serverId;
-    var folderInfo = this._folderInfos[folderId] = {
+    let folderId = this.id + '/' + serverId;
+    let path = displayName;
+    let depth = 0;
+    if (parentId !== '0') {
+      let parent = this._folderInfos[this.id + '/' + parentId];
+      path = parent.$meta.path + '/' + path;
+      depth = parent.$meta.depth + 1;
+    }
+
+    let folderInfo = this._folderInfos[folderId] = {
       $meta: {
         id: folderId,
         serverId: serverId,
         name: displayName,
-        path: displayName,
+        path: path,
         type: this._folderTypes[typeNum],
-        depth: 0,
+        depth: depth,
       },
       $impl: {
         nextHeaderBlock: 0,
@@ -27826,10 +27931,10 @@ Configurators['activesync'] = {
       },
       $mutations: [],
     };
-    universe.saveAccountDef(accountDef, folderInfo);
     var account = universe._loadAccount(accountDef, folderInfo, null);
     account.syncFolderList(function() {
       accountDef.identities[0].name = account.conn.config.user.name;
+      universe.saveAccountDef(accountDef, folderInfo);
       callback(true, account);
     });
   },
