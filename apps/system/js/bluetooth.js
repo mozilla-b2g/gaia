ee0820a8678f879e5cf3ad72b758c45e6f513f71
Timothy Guan-tin Chien <timdream@gmail.com>
2012-08-09 20:47:32 +0800

    Handle situation between device found and paired

--- apps/system/js/bluetooth.js
+++ apps/system/js/bluetooth.js
@@ -59,17 +59,32 @@ var Bluetooth = {
           self.defaultAdapter = evt.result;
 
         /* If we get adapter.ondeviceconnected and
-        adapter.ondevicedisconnected, we can end here. But we don't..
+        adapter.ondevicedisconnected, we can end here. But we don't...
         https://bugzilla.mozilla.org/show_bug.cgi?id=778640
         */
 
-        adapter.ondevicefound = self.updateDeviceList.bind(self);
+        self.updateDeviceList.call(self);
+
+        /* We neither have adapter.ondevicepaired nor
+        adapter.ondeviceunpaired, that leaves us with only
+        adapter.ondevicefound to tackle */
+
+        // New device is being found. Update the paired device list only
+        // if it is being paired.
+        adapter.ondevicefound = function bt_deviceFound(evt) {
+          var device = evt.device;
+          device.onpropertychanged = function bt_devicePropertychanged() {
+            if (device.paired)
+              self.updateDeviceList.bind(self);
+          };
+        };
 
         // We might have this callback or |device.ondisappeared|
         // put it here anyway.
-        adapter.ondevicedisappeared = self.updateDeviceList.bind(self);
-
-        self.updateDeviceList.call(self);
+        adapter.ondevicedisappeared = function bt_deviceDisappeared(evt) {
+          var device = evt.device;
+          device.onpropertychanged = null;
+        };
       };
     }
   },
@@ -88,17 +103,26 @@ var Bluetooth = {
       var devices =
         self.pairedDevices = evt.result;
 
+      self.updateConnected.call(self);
+
       devices.forEach(function(device) {
-        device.onpropertychanged =
-          self.updateConnected.bind(self);
+        device.onpropertychanged = function bt_devicePropertychanged() {
+          // Device is connected, update connected status
+          if (device.connected)
+            self.updateConnected.bind(self);
+
+          // Device is unpaired, update the paired device list
+          if (!device.paired)
+            self.updateDeviceList.call(self);
+        }
 
         // We might have this callback or |adapter.ondevicedisappeared|
         // put it here anyway.
-        device.ondisappeared =
-          self.updateDeviceList.bind(self);
+        device.ondisappeared = function bt_deviceDisappeared() {
+          device.onpropertychanged = null;
+        };
       });
 
-      self.updateConnected.call(self);
     }
   },
 
