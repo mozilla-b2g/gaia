5fe8f684bb17ea64e785ed8ece927ab65d64a780
Timothy Guan-tin Chien <timdream@gmail.com>
2012-08-09 14:47:47 +0800

    Talk to mozBluetooth to get a global connected status

--- apps/system/js/bluetooth.js
+++ apps/system/js/bluetooth.js
@@ -4,12 +4,24 @@
 'use strict';
 
 var Bluetooth = {
+
+  /* this property store a reference of the default adapter */
+  defaultAdapter: null,
+
+  /* this property store the array of paired devices we got from the
+   * adapter */
+  pairedDevices: [],
+
+  /* keep a global connected property here */
+  connected: false,
+
   init: function bt_init() {
     var settings = window.navigator.mozSettings;
     if (!settings)
       return;
 
     var bluetooth = window.navigator.mozBluetooth;
+    var self = this;
 
     // Sync the bluetooth.enabled mozSettings value with real API
     // These code should be removed once this bug is fixed
@@ -38,6 +50,61 @@ var Bluetooth = {
           'bluetooth.enabled': !value
         });
       };
+    });
+
+    if (bluetooth && bluetooth.getDefaultAdapter) {
+      var req = bluetooth.getDefaultAdapter();
+      req.onsuccess = function bt_gotDefaultAdapter(evt) {
+        var adapter =
+          self.defaultAdapter = evt.result;
+
+        /* If we get adapter.ondeviceconnected and
+        adapter.ondevicedisconnected, we can end here. But we don't..
+        https://bugzilla.mozilla.org/show_bug.cgi?id=778640
+        */
+
+        adapter.ondevicefound = self.updateDeviceList.bind(self);
+
+        // We might have this callback or |device.ondisappeared|
+        // put it here anyway.
+        adapter.ondevicedisappeared = self.updateDeviceList.bind(self);
+
+        self.updateDeviceList.call(self);
+      };
+    }
+  },
+
+  updateDeviceList: function bt_updateDeviceList() {
+    var adapter = this.defaultAdapter;
+
+    // adapter.getPairedDevices will be implemented in
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=777671
+    if (!adapter.getPairedDevices)
+      return;
+
+    var self = this;
+    var req = adapter.getPairedDevices();
+    req.onsuccess = function bt_gotPairedDevices(evt) {
+      var devices =
+        self.pairedDevices = evt.result;
+
+      devices.forEach(function(device) {
+        device.onpropertychanged =
+          self.updateConnected.bind(self);
+
+        // We might have this callback or |adapter.ondevicedisappeared|
+        // put it here anyway.
+        device.ondisappeared =
+          self.updateDeviceList.bind(self);
+      });
+
+      self.updateConnected.call(self);
+    }
+  },
+
+  updateConnected: function bt_updateConnected() {
+    this.connected = this.pairedDevices.some(function(device) {
+      return device.connected;
     });
   }
 };
