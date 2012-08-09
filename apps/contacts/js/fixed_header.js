93b8cce439e6eeff5ca5ce99526e3c6ee2c15b63
Alberto Pastor <albert.pastor@gmail.com>
2012-08-09 11:37:59 +0100

    Adding documentation

--- apps/contacts/js/fixed_header.js
+++ apps/contacts/js/fixed_header.js
@@ -7,6 +7,13 @@ var FixedHeader = (function FixedHeader() {
   var fixedContainer;
   var currentlyFixed;
 
+  /**
+   * Start listening scroll event, and applying the fixed header effect
+   * @param  {String} scrollView HTML element that is going to be scrolled.
+   * @param  {String} container  HTML element that is
+   *                             going to contain the top header.
+   * @param  {String} select     Selector of the headers to be checked.
+   */
   var init = function init(scrollView, container, select) {
     selector = select;
     view = document.querySelector(scrollView);
