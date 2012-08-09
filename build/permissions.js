cce1536e5409261e98887c95622075e4e0316df9
Gregor Wagner <anygregor@gmail.com>
2012-08-09 12:06:18 -0700

    fix permissionlist

--- build/permissions.js
+++ build/permissions.js
@@ -10,11 +10,13 @@ let permissionList = ["power", "sms", "contacts", "telephony",
                       "background", "settings", "offline-app",
                       "indexedDB-unlimited", "alarm", "camera",
                       "fmradio", "devicestorage", "voicemail",
-                      "pin-app"];
+                      "pin-app", "wifi-manage", "geolocation",
+                      "webapps-manage", "desktop-notification"];
 
 let commonPermissionList = ['offline-app', 'indexedDB-unlimited',
                             'webapps-manage', 'pin-app',
                             'desktop-notification'];
+
 
 let appSrcDirs = GAIA_APP_SRCDIRS.split(' ');
 
