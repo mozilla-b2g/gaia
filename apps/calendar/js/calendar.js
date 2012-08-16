48d3c3435b7796a37ac61f2ff47f05503bb04c80
James Lal <james@lightsofapollo.com>
2012-08-16 12:09:12 -0700

    remove binsearch.range

--- apps/calendar/js/calendar.js
+++ apps/calendar/js/calendar.js
@@ -65,32 +65,6 @@
         return null;
       },
 
-      range: function (list, seekVal, cmpfunc) {
-        if (!list.length)
-          return 0;
-
-        var low = 0, high = list.length - 1,
-            mid, cmpval;
-
-        while (low <= high) {
-          mid = low + Math.floor((high - low) / 2);
-          cmpval = cmpfunc(seekVal, list[mid]);
-
-          if (cmpval < 0)
-            high = mid - 1;
-          else if (cmpval > 0)
-            low = mid + 1;
-          else
-            break;
-        }
-
-        if (cmpval < 0) {
-          return mid - 1;
-        } else {
-          return mid;
-        }
-      },
-
       insert: function bsearchForInsert(list, seekVal, cmpfunc) {
         if (!list.length)
           return 0;
