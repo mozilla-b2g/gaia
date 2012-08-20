c591cdaa188b1fbe2ac182bc484017b048903d49
Timothy Guan-tin Chien <timdream@gmail.com>
2012-08-20 11:52:09 +0800

    Introduce wifi wake lock (turn off wifi if screen off and no lock)

--- apps/system/js/wifi.js
+++ apps/system/js/wifi.js
@@ -4,7 +4,14 @@
 'use strict';
 
 var Wifi = {
+  wifiWakeLocked: false,
+
+  wifiEnabled: true,
+
   init: function wf_init() {
+    window.addEventListener('screenchange', this);
+
+    var self = this;
     var settings = window.navigator.mozSettings;
     if (!settings)
       return;
@@ -12,10 +19,12 @@ var Wifi = {
     var wifiManager = window.navigator.mozWifiManager;
 
     // Sync the wifi.enabled mozSettings value with real API
-    // These code should be removed once this bug is fixed
+    // These code should be rewritten once this bug is fixed
     // https://bugzilla.mozilla.org/show_bug.cgi?id=729877
     SettingsListener.observe('wifi.enabled', true, function(value) {
       if (!wifiManager) {
+        self.wifiEnabled = false;
+
         // roll back the setting value to notify the UIs
         // that wifi interface is not available
         if (value) {
@@ -23,9 +32,10 @@ var Wifi = {
             'wifi.enabled': false
           });
         }
-
         return;
       }
+
+      self.wifiEnabled = value;
 
       if (wifiManager.enabled == value)
         return;
@@ -39,6 +49,42 @@ var Wifi = {
         });
       };
     });
+
+    var power = navigator.mozPower;
+    power.addWakeLockListener(function wifi_handleWakeLock(topic, state) {
+      if (topic !== 'wifi')
+        return;
+
+      self.wifiWakeLocked = (state == 'locked-foreground' ||
+                               state == 'locked-background');
+
+      // Let's quietly turn off wifi if there is no wake lock and
+      // the screen is off.
+      if (!ScreenManager.screenEnabled && !self.wifiWakeLocked)
+        wifiManager.setEnabled(false);
+    });
+  },
+
+  handleEvent: function wifi_handleEvent(evt) {
+    var wifiManager = window.navigator.mozWifiManager;
+    if (!wifiManager)
+      return;
+
+    switch (evt.type) {
+      case 'screenchange':
+        var screenEnabled = evt.detial.screenEnabled;
+
+        // Let's quietly turn off wifi if there is no wake lock and
+        // the screen is off.
+        if (!screenEnabled && !this.wifiWakeLocked)
+          wifiManager.setEnabled(false);
+
+        // ... and quietly turn it back on when the screen is on
+        if (screenEnabled && this.wifiEnabled)
+          wifiManager.setEnabled(true);
+
+        break;
+    }
   }
 };
 
