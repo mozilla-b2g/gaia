d7d51e404ecf9d37e3421d1a6ab7f3331dcb1eec
Borja Salguero <borja.salguero@gmail.com>
2012-08-27 16:43:01 -0300

    Merge complete

--- apps/dialer/js/dialer.js
+++ apps/dialer/js/dialer.js
@@ -2,7 +2,10 @@
 
 document.addEventListener('mozvisibilitychange', function visibility(e) {
   if (!document.mozHidden) {
-    Recents.render();
+      RecentsDBManager.get(function(recents){
+        Recents.render(recents);
+      });
+    
   }
 });
 
