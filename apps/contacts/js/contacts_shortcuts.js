bd49c31819e8e8eaa61f1ffea8916cb6a3c53239
Jose M. Cantera <jmcanterafonseca@gmail.com>
2012-08-28 16:24:48 +0200

    Alphascroll is now a component

--- apps/contacts/js/contacts_shortcuts.js
+++ apps/contacts/js/contacts_shortcuts.js
@@ -1,71 +1,86 @@
 
 'use strict';
 
-(function initScrollbar(doc) {
-  var jumper = doc.querySelector('.view-jumper-inner');
-  jumper.addEventListener('mousedown', scrollStart);
-  jumper.addEventListener('mousemove', scrollTo);
-  jumper.addEventListener('mouseleave', scrollEnd);
-  jumper.addEventListener('mouseup', scrollEnd);
+var utils = window.utils || {};
 
-  var scrollable = doc.querySelector('#groups-container');
-  var overlay = doc.querySelector('.view-jumper-current');
-  var overlayContent = doc.querySelector('#current-jumper');
-  overlayContent.textContent = '';
-  var overlayStyle = overlay.style;
+if (!utils.alphaScroll) {
+  (function initScrollbar(doc) {
+    var alphaScroll = utils.alphaScroll = {};
 
-  var isScrolling = false;
+    var scrollToCallback, jumper, overlay, overlayContent, overlayStyle;
 
-  var alphabet = [];
-  for (var i = 65; i <= 90; i++) {
-    alphabet.push({ letter: String.fromCharCode(i) });
-  }
-  utils.templates.append(jumper, alphabet);
+    var isScrolling = false;
 
-  var overlayTimeout = 0, scrollToTimeout = 0;
-  var previous = null;
+    var overlayTimeout = 0, scrollToTimeout = 0;
+    var previous = null;
 
-  function scrollStart(evt) {
-    overlayStyle.MozTransitionDelay = '0s';
-    overlayStyle.MozTransitionDuration = '0s';
-    overlayStyle.opacity = '1';
-    isScrolling = true;
-    scrollTo(evt);
-  }
+    var P_SCROLLTO_CB = 'scrollToCb';
+    var P_JUMPER = 'jumper';
+    var P_OVERLAY = 'overlay';
+    var P_OVERLAY_CONTENT = 'overlayContent';
 
-  function scrollEnd(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    overlayStyle.MozTransitionDelay = '0.3s';
-    overlayStyle.MozTransitionDuration = '0.2s';
-    overlayStyle.opacity = '0';
-    overlayContent.textContent = previous = null;
-    isScrolling = false;
-  }
+    alphaScroll.init = function(params) {
+      scrollToCallback = params[P_SCROLLTO_CB];
+      jumper = params[P_JUMPER];
+      overlay = params[P_OVERLAY];
+      overlayContent = params[P_OVERLAY_CONTENT];
 
-  function scrollTo(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
+      overlayContent.textContent = '';
+      overlayStyle = overlay.style;
 
-    if (!isScrolling) {
-      return;
+      jumper.addEventListener('mousedown', scrollStart);
+      jumper.addEventListener('mousemove', scrollTo);
+      jumper.addEventListener('mouseleave', scrollEnd);
+      jumper.addEventListener('mouseup', scrollEnd);
+
+      var alphabet = [];
+      for (var i = 65; i <= 90; i++) {
+        alphabet.push({ letter: String.fromCharCode(i) });
+      }
+      utils.templates.append(jumper, alphabet);
     }
 
-    var current = evt.target.dataset.letter;
-    overlayContent.textContent = current || null;
-
-    if (previous === current) {
-      return;
+    function scrollStart(evt) {
+      overlayStyle.MozTransitionDelay = '0s';
+      overlayStyle.MozTransitionDuration = '0s';
+      overlayStyle.opacity = '1';
+      isScrolling = true;
+      scrollTo(evt);
     }
 
-    var groupContainer = doc.querySelector('#group-' + current);
-    if (!groupContainer || groupContainer.clientHeight <= 0)
-      return;
+    function scrollEnd(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      overlayStyle.MozTransitionDelay = '0.3s';
+      overlayStyle.MozTransitionDuration = '0.2s';
+      overlayStyle.opacity = '0';
+      overlayContent.textContent = previous = null;
+      isScrolling = false;
+    }
 
-    previous = current;
+    function scrollTo(evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
 
-    scrollable.scrollTop = groupContainer.offsetTop;
+      if (!isScrolling) {
+        return;
+      }
 
-  }
+      var current = evt.target.dataset.letter;
+      overlayContent.textContent = current || null;
 
-})(document);
+      if (previous === current) {
+        return;
+      }
+
+      var groupContainer = doc.querySelector('#group-' + current);
+      if (!groupContainer || groupContainer.clientHeight <= 0)
+        return;
+
+      previous = current;
+
+      scrollToCallback(groupContainer);
+    }
+
+  })(document);
+}
