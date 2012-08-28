bfaffb23c0e94d8c3026684ecbaa1b76e636d871
Alberto Pastor <albert.pastor@gmail.com>
2012-08-28 21:08:29 +0100

    Allowing multiple entry points to the dock

--- build/applications-data.js
+++ build/applications-data.js
@@ -2,34 +2,36 @@
 'use strict';
 
 // Homescreen
-let init = getFile(GAIA_DIR, "apps", "homescreen", "js", "init.json");
+let init = getFile(GAIA_DIR, 'apps', 'homescreen', 'js', 'init.json');
 
-function makeURL(name) {
-  return GAIA_SCHEME + name + "." + GAIA_DOMAIN + (GAIA_PORT ? GAIA_PORT : "");
+function makeURL(name, ep) {
+  var url = GAIA_SCHEME + name + '.' + GAIA_DOMAIN + (GAIA_PORT ? GAIA_PORT : '');
+  url += ep? ('/' + ep) : '';
+  return url;
 }
 
 let content = {
   grid: [
     [ // page 1
-      makeURL("email"),
-      makeURL("video"),
-      makeURL("camera"),
-      makeURL("gallery"),
-      makeURL("music"),
-      makeURL("calendar"),
-      makeURL("calculator"),
-      makeURL("settings"),
-      makeURL("clock"),
-      makeURL("fm"),
-      makeURL("pdfjs"),
-      "https://marketplace.mozilla.org/telefonica/"
+      makeURL('email'),
+      makeURL('video'),
+      makeURL('camera'),
+      makeURL('gallery'),
+      makeURL('music'),
+      makeURL('calendar'),
+      makeURL('calculator'),
+      makeURL('settings'),
+      makeURL('clock'),
+      makeURL('fm'),
+      makeURL('pdfjs'),
+      'https://marketplace.mozilla.org/telefonica/'
     ]
   ],
   dock: [
-    makeURL("dialer"),
-    makeURL("sms"),
-    makeURL("contacts"),
-    makeURL("browser")
+    makeURL('comms', 'dialer'),
+    makeURL('sms'),
+    makeURL('comms', 'contacts'),
+    makeURL('browser')
   ]
 }
 
