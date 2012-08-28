04df4b00721d4dc4202e5cbf164aa161b1354b90
James Lal <james@lightsofapollo.com>
2012-08-28 01:08:47 -0700

    remove console log in test

--- apps/calendar/test/unit/calc_test.js
+++ apps/calendar/test/unit/calc_test.js
@@ -122,7 +122,6 @@ suite('calendar/calc', function() {
 
     setup(function() {
       start = new Date(2012, 0, 1, 2);
-      console.log(subject, '<--');
       ms = subject.utcMs(start);
     });
 
