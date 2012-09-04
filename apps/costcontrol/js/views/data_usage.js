4d45a2d76cab22121d5c86c56192aaf66720aa6c
lodr <salva@unoyunodiez.com>
2012-09-04 23:51:06 +0200

    Graphics in progress

--- apps/costcontrol/js/views/data_usage.js
+++ apps/costcontrol/js/views/data_usage.js
@@ -227,7 +227,249 @@ appVManager.tabs[TAB_DATA_USAGE] = (function cc_setUpDataUsage() {
 
   }
 
+  function _drawBackgroundLayer(options) {
+    var canvas = document.getElementById('background-layer');
+    var height = canvas.height = options.height;
+    var width = canvas.width = options.width;
+    var ctx = canvas.getContext('2d');
+
+    // A linear gradient from transparent to opaque white
+    var bgGradient = ctx.createLinearGradient(0, 0, 0, height);
+    bgGradient.addColorStop(0, 'rgba(255,255,255,0)');
+    bgGradient.addColorStop(1, 'rgba(255,255,255,1)');
+    ctx.fillStyle = bgGradient;
+    ctx.fillRect(0, 0, width, options.originY );
+
+    // A line just after the end of the gradient
+    var lineY = options.originY - 1.5;
+    ctx.beginPath();
+    ctx.strokeStyle = '#d5d5d5';
+    ctx.moveTo(0, lineY);
+    ctx.lineTo(width, lineY);
+    ctx.stroke();
+
+    // Lines every step
+    var step =  options.axis.Y.step;
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    for (var y = options.originY - step; y > 0; y -= step) {
+      ctx.moveTo(0, y);
+      ctx.lineTo(width, y);
+      ctx.stroke();
+    }
+  }
+
+  function _drawAxisLayer(options) {
+    function pad(value) {
+      if (value === 0)
+        return '0MB';
+
+      if (value < 1000) {
+        var str = (10 * Math.ceil(value/10)) + 'M';
+        switch(str.length) {
+          case 2:
+            return '00' + str;
+          case 3:
+            return '0' + str;
+          default:
+            return str;
+        }
+      }
+
+      return (value / 1000).toFixed(1) + 'G';
+    }
+
+    var canvas = document.getElementById('axis-layer');
+    var height = canvas.height = options.height;
+    var width = canvas.width = options.width;
+    var ctx = canvas.getContext('2d');
+
+    // Start drawing Y axis
+    var step = options.axis.Y.step;
+    var dataStep = options.axis.Y.upper - options.axis.Y.maxValue;
+    var marginLeft = 4, marginBottom = 4;
+    var fontsize = 12;
+    ctx.font = '500 ' + fontsize + 'px Arial';
+    ctx.fillStyle = '#6a6a6a';
+    ctx.textBaseline = 'bottom';
+    ctx.textAlign = 'start';
+    for (var y = options.originY, value = 0;
+         y > 0; y -= step, value += dataStep) {
+      ctx.fillText(pad(value), marginLeft, y - marginBottom);
+    }
+
+    // Now the X axis
+    var marginTop = 10;
+    var leftTag = 
+      options.axis.X.lower.toLocaleFormat('%b %d').toUpperCase();
+    ctx.font = '600 ' + fontsize + 'px Arial';
+    ctx.textBaseline = 'top';
+    ctx.textAlign = 'start';
+    ctx.fillText(leftTag, marginLeft, options.originY + marginTop);
+
+    var rightTag = 
+      options.axis.X.upper.toLocaleFormat('%b %d').toUpperCase();
+    ctx.textAlign = 'end';
+    ctx.fillText(rightTag, width - marginLeft, options.originY + marginTop);
+  }
+
+  function _drawTodayLayer(options) {
+    var canvas = document.getElementById('today-layer');
+    var height = canvas.height = options.height;
+    var width = canvas.width = options.width;
+    var ctx = canvas.getContext('2d');
+
+    // Compute the X offset
+    var lower = options.axis.X.lower.getTime();
+    var upper = options.axis.X.upper.getTime() - lower;
+    var today = options.axis.X.today.getTime() - lower;
+    var offsetX = Math.round(today / upper * width);
+
+    // Draw the vertical line
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.moveTo(offsetX - 0.5, options.originY);
+    ctx.lineTo(offsetX - 0.5, 0.5);
+    ctx.lineTo(offsetX + 6.5, 0.5);
+    ctx.stroke();
+
+    // The shadow as a gradient
+    var shadowLength = 10;
+    var shadow = ctx.createLinearGradient(
+      offsetX + 0.5, 0,
+      offsetX + 0.5 + shadowLength, 0
+    );
+    shadow.addColorStop(0, 'rgba(128, 128, 128, 0.3)');
+    shadow.addColorStop(1, 'rgba(128, 128, 128, 0.0)');
+    ctx.fillStyle = shadow;
+    ctx.fillRect(
+      offsetX + 0.5, 1.5,
+      offsetX + 0.5 + shadowLength, options.originY - 3.5
+    );
+
+    // Centered today text
+    var fontsize = 12;
+    var marginTop = 10;
+    var tagTop = options.axis.X.today.toLocaleFormat('%b %d').toUpperCase();
+    var tagBottom = _('today').toUpperCase();
+    ctx.font = '600 ' + fontsize + 'px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+    ctx.fillStyle = 'black';
+    ctx.fillText(tagTop, offsetX, options.originY + marginTop);
+    ctx.fillText(tagBottom, offsetX, options.originY + marginTop + fontsize);
+
+    // The box around the text
+    var widthTop = ctx.measureText(tagTop).width;
+    var widthBottom = ctx.measureText(tagBottom).width;
+    var maxWidth = widthTop.width > widthBottom ? widthTop : widthBottom;
+    var marginSide = 10;
+    var radiusTop = 3;
+    var radiusBottom = 5;
+
+    var topRight = {
+      x: offsetX + (maxWidth / 2) + marginSide,
+      y: options.originY + (marginTop / 2)
+    };
+    var bottomRight = {
+      x: topRight.x,
+      y: topRight.y + (fontsize * 2) + marginTop
+    };
+    var bottomLeft = {
+      x: offsetX - (maxWidth / 2) - marginSide,
+      y: bottomRight.y
+    };
+    var topLeft = {
+      x: bottomLeft.x,
+      y: topRight.y
+    };
+
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+
+    ctx.moveTo(offsetX, options.originY);
+    ctx.lineTo(topRight.x - radiusTop, topRight.y);
+    ctx.arcTo(
+      topRight.x, topRight.y,
+      topRight.x, topRight.y + radiusTop,
+      radiusTop
+    );
+
+    ctx.lineTo(bottomRight.x, bottomRight.y - radiusBottom);
+    ctx.arcTo(
+      bottomRight.x, bottomRight.y,
+      bottomRight.x - radiusBottom, bottomRight.y,
+      radiusBottom
+    );
+
+    ctx.lineTo(bottomLeft.x + radiusBottom, bottomLeft.y);
+    ctx.arcTo(
+      bottomLeft.x, bottomLeft.y,
+      bottomLeft.x, bottomLeft.y - radiusBottom,
+      radiusBottom
+    );
+
+    ctx.lineTo(topLeft.x, topLeft.y + radiusTop);
+
+    // Here a trick to simplify this:
+    // it is the same as the beginnign but to the other direction
+    ctx.moveTo(offsetX, options.originY);
+    ctx.lineTo(topLeft.x + radiusTop, topLeft.y);
+    ctx.arcTo(
+      topLeft.x, topLeft.y,
+      topLeft.x, topLeft.y + radiusTop,
+      radiusTop
+    );
+
+    ctx.stroke();
+  }
+
   function _updateUI() {
+    var graphicArea = document.getElementById('graphic-area');
+
+    var options = {
+      height: graphicArea.clientHeight,
+      width: graphicArea.clientWidth,
+      get originY() {
+        delete this.originY;
+        return (this.originY = Math.floor(this.height * 5/6));
+      },
+      originX: 0,
+      axis: {
+        X: {
+          lower: new Date(2012, 0, 1),
+          upper: new Date(2012, 0, 31),
+          today: new Date(2012, 0, 21)
+        },
+        Y: {
+          lower: 0,
+          margin: 0.20,
+          maxValue: 2356,
+          get range() {
+            delete this.range;
+            return (this.range = this.upper - this.lower);
+          },
+          get upper() {
+            delete this.upper;
+            return (this.upper = (1 + this.margin) * this.maxValue);
+          },
+          get step() {
+            delete this.step;
+            return (this.step = this.get(this.maxValue));
+          },
+          get: function cc_dataToYPx(value) {
+            return options.originY * (1 - value / this.upper);
+          }
+        }
+      }
+    };
+
+    _drawBackgroundLayer(options);
+    _drawAxisLayer(options);
+    _drawTodayLayer(options);
+  }
+
+/*  function _updateUI() {
 
     for (var type in _enabledNetworkTypes) if (_enabledNetworkTypes[type]) {
 
@@ -265,7 +507,7 @@ appVManager.tabs[TAB_DATA_USAGE] = (function cc_setUpDataUsage() {
         };
       }
     }
-  }
+  }*/
 
   return {
     init: _init,
