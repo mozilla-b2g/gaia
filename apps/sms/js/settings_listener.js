9cc4c65a9c486b2273bc8f38d4793a773e753bb8
Timothy Guan-tin Chien <timdream@gmail.com>
2012-09-06 06:32:33 +0800

    mozSettings: save the lock created for reuse, fixes #4365

--- apps/sms/js/settings_listener.js
+++ apps/sms/js/settings_listener.js
@@ -4,6 +4,41 @@
 'use strict';
 
 var SettingsListener = {
+  /* Timer to remove the lock. */
+  _timer: null,
+
+  /* lock stores here */
+  _lock: null,
+
+  /* getSettingsLock: create a lock or retrive one that we saved.
+  *  mozSettings.createLock() is expensive and lock should be reused
+  *  whenever possible.
+  */
+  getSettingsLock: function sl_getSettingsLock() {
+    // Each time there is a getSettingsLock call, we pospone the removal
+    clearTimeout(this._timer);
+    this._timer = setTimeout((function removeLock() {
+      this._lock = null;
+    }).bind(this), 0);
+
+    // If there is a lock present we return that
+    if (this._lock) {
+      return this._lock;
+    }
+
+    // If there isn't we return one.
+    var settings = window.navigator.mozSettings;
+
+    // Once
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=788561
+    // lands, we should get rid of `getLock()` call below.
+    if (settings.createLock) {
+      this._lock = settings.createLock();
+    } else {
+      this._lock = settings.getLock();
+    }
+    return this._lock;
+  },
   observe: function sl_observe(name, defaultValue, callback) {
     var settings = window.navigator.mozSettings;
     if (!settings) {
@@ -11,7 +46,7 @@ var SettingsListener = {
       return;
     }
 
-    var req = settings.getLock().get(name);
+    var req = this.getSettingsLock().get(name);
     req.addEventListener('success', (function onsuccess() {
       callback(typeof(req.result[name]) != 'undefined' ?
         req.result[name] : defaultValue);
