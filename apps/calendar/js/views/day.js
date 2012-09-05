d14cf26e50c1d2d75939661e0dd06f16182f93f5
James Lal <james@lightsofapollo.com>
2012-09-04 23:52:18 -0700

    day view js implementation still looks ugly

---
+++ apps/calendar/js/views/day.js
@@ -0,0 +1,107 @@
+Calendar.ns('Views').Day = (function() {
+
+  var Parent = Calendar.Views.TimeParent;
+
+  function Day() {
+    Parent.apply(this, arguments);
+
+    this._initEvents();
+  }
+
+  Day.prototype = {
+    __proto__: Parent.prototype,
+    panThreshold: 50,
+
+    scale: 'day',
+
+    selectors: {
+      element: '#day-view'
+    },
+
+    _initEvents: function() {
+      Parent.prototype._initEvents.call(this);
+    },
+
+    handleEvent: function(e) {
+      Parent.prototype.handleEvent.apply(
+        this, arguments
+      );
+
+      switch (e.type) {
+        case 'selectedDayChange':
+        case 'dayChange':
+          this._activateTime(e.data[0]);
+          break;
+      }
+    },
+
+    _getId: function(date) {
+      return date.valueOf();
+    },
+
+    _nextTime: function(time) {
+      return new Date(
+        time.getFullYear(),
+        time.getMonth(),
+        time.getDate() + 1
+      );
+    },
+
+    _previousTime: function(time) {
+      return new Date(
+        time.getFullYear(),
+        time.getMonth(),
+        time.getDate() - 1
+      );
+    },
+
+    /**
+     * Creates child day view.
+     *
+     * @param {Date} time date.
+     */
+    _createChild: function(time) {
+      return new Calendar.Views.DayChild({
+        date: time,
+        app: this.app
+      });
+    },
+
+    render: function() {
+      this._activateTime(
+        this.app.timeController.day
+      );
+    },
+
+    oninactive: function() {
+      Parent.prototype.oninactive.apply(this, arguments);
+
+      /**
+       * We disable events here because this view
+       * does not need to be updated while hidden
+       * notice this will not effect the day children
+       * at all...
+       */
+      var controller = this.app.timeController;
+      controller.removeEventListener('dayChange', this);
+      controller.removeEventListener('selectedDayChange', this);
+    },
+
+    onactive: function() {
+      Parent.prototype.onactive.apply(this, arguments);
+      /**
+       * We only want to listen to views when
+       * this view is actually active...
+       */
+      var controller = this.app.timeController;
+      controller.on('dayChange', this);
+      controller.on('selectedDayChange', this);
+      controller.moveToMostRecentDay();
+    }
+  };
+
+  Day.prototype.onfirstseen = Day.prototype.render;
+
+  return Day;
+
+}());
