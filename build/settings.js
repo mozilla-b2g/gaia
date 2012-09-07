36c67db4154bf02529677c95e4dcf56675743c3b
Timothy Guan-tin Chien <timdream@gmail.com>
2012-09-07 16:24:22 +0800

    Correct filename in build/settings.js, follow up of #4431

--- build/settings.js
+++ build/settings.js
@@ -35,7 +35,7 @@ var settings = [
  new Setting("devtools.debugger.remote-enabled", false),
  new Setting("devtools.debugger.remote-port", 6000),
  new Setting("geolocation.enabled", true),
- new Setting("homescreen.ring", 'classic.wav'),
+ new Setting("homescreen.ring", "classic.ogg"),
  new Setting("homescreen.wallpaper", "default.png"),
  new Setting("keyboard.layouts.english", true),
  new Setting("keyboard.layouts.dvorak", false),
