2c4306591cbd9cce44c0c9bd8c96aa4d6fd1ba99
arcturus <arcturus@ardeenelinfierno.com>
2012-09-18 22:08:57 +0100

    Adding unit test to check that a social contact is not editable

--- apps/communications/contacts/test/unit/mock_fb.js
+++ apps/communications/contacts/test/unit/mock_fb.js
@@ -1,15 +1,45 @@
 'use strict';
 
-var MockFb = {};
+var MockFb = {
+  fbContact: false,
+  fbLinked: false
+};
+
+MockFb.setIsFbContact = function (isFB) {
+  this.fbContact = isFB;
+};
+
+MockFb.setIsFbLinked = function (isLinked) {
+  this.fbLinked = isLinked;
+};
+
 MockFb.Contact = function(deviceContact, cid) {
   this.deviceContact = deviceContact;
   this.cid = cid;
+
+  var getData = function getData() {
+    return {
+      set onsuccess (callback) {
+        // Fetch FB data, that is returning a contact info
+        this.result = new MockContactAllFields();
+        this.result.org[0] = 'FB';
+        callback.call(this);
+      },
+      set onerror (callback) {
+
+      }
+    };
+  };
+
+  return {
+    'getData': getData
+  };
 };
 
 MockFb.isFbContact = function(contact) {
-    return false;
+  return this.fbContact;
 };
 
 MockFb.isFbLinked = function(contact) {
-    return false;
+  return this.fbLinked;
 };
