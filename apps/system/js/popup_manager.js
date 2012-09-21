84c03d5f6b0c8848bc2f204595cca964f903556d
Alive Kuo <alegnadise@gmail.com>
2012-09-21 20:06:02 +0800

    [Issue #4615] [System: Popup] Update Visual to Entry Sheet; add URL bar

--- apps/system/js/popup_manager.js
+++ apps/system/js/popup_manager.js
@@ -3,26 +3,37 @@
 'use strict';
 
 var PopupManager = {
-  _currentPopup: null,
+  _currentPopup: {},
+  _currentOrigin: '',
   _lastDisplayedApp: null,
   _endTimes: 0,
   _startTimes: 0,
 
+  throbber: document.getElementById('popup-throbber'),
+
   overlay: document.getElementById('dialog-overlay'),
 
-  container: document.getElementById('popup-container'),
+  popupContainer: document.getElementById('popup-container'),
+
+  container: document.getElementById('frame-container'),
 
   screen: document.getElementById('screen'),
+
+  closeButton: document.getElementById('popup-close'),
 
   loadingIcon: document.getElementById('statusbar-loading'),
 
   init: function pm_init() {
+    this.origin = document.getElementById('popup-origin');
     window.addEventListener('mozbrowseropenwindow', this);
     window.addEventListener('mozbrowserclose', this);
     window.addEventListener('appwillclose', this);
+    window.addEventListener('appopen', this);
+    window.addEventListener('appterminated', this);
     window.addEventListener('home', this);
     window.addEventListener('keyboardhide', this);
     window.addEventListener('keyboardchange', this);
+    this.closeButton.addEventListener('click', this);
   },
 
   _showWait: function pm_showWait() {
@@ -34,10 +45,11 @@ var PopupManager = {
   },
 
   open: function pm_open(name, frame, origin, trusted) {
-    // Only one popup at a time. If the popup is being shown, we swap frames.
-    if (this._currentPopup) {
-      this.container.removeChild(this._currentPopup);
-      this._currentPopup = null;
+    // Only one popup per origin at a time.
+    // If the popup is being shown, we swap frames.
+    if (this._currentPopup[origin]) {
+      this.container.removeChild(this._currentPopup[origin]);
+      delete this._currentPopup[origin];
     } else if (trusted) {
       // Save the current displayed app in order to show it after closing the
       // popup.
@@ -51,12 +63,14 @@ var PopupManager = {
       WindowManager.setDisplayedApp(null);
     }
 
+    this.popupContainer.dataset.trusty = trusted;
+
     // Reset overlay height
     this.setHeight(window.innerHeight - StatusBar.height);
 
-    this._currentPopup = frame;
+    this._currentPopup[origin] = frame;
 
-    var popup = this._currentPopup;
+    var popup = this._currentPopup[origin];
     var dataset = popup.dataset;
     dataset.frameType = 'popup';
     dataset.frameName = name;
@@ -75,13 +89,14 @@ var PopupManager = {
         evt.target.dataset.frameType !== 'popup'))
       return;
 
-    this.screen.classList.remove('popup');
 
     var self = this;
-    this.container.addEventListener('transitionend', function trWait() {
-      self.container.removeEventListener('transitionend', trWait);
-      self.container.removeChild(self._currentPopup);
-      self._currentPopup = null;
+    this.popupContainer.addEventListener('transitionend', function wait(event) {
+      self.popupContainer.removeEventListener('transitionend', wait);
+      self.screen.classList.remove('popup');
+      self.popupContainer.classList.remove('disappearing');
+      self.container.removeChild(self._currentPopup[self._currentOrigin]);
+      delete self._currentPopup[self._currentOrigin];
 
       // If the popup was opened as a trusted UI on top of the homescreen
       // we show the last displayed application.
@@ -93,6 +108,8 @@ var PopupManager = {
       if (callback)
         callback();
     });
+
+    this.popupContainer.classList.add('disappearing');
 
     // We just removed the focused window leaving the system
     // without any focused window, let's fix this.
@@ -119,15 +136,15 @@ var PopupManager = {
       }
   },
 
-  backHandling: function pm_backHandling(evt) {
-    if (!this._currentPopup)
+  backHandling: function pm_backHandling() {
+    if (!this._currentPopup[this._currentOrigin])
       return;
 
     this.close();
   },
 
   isVisible: function pm_isVisible() {
-    return (this._currentPopup != null);
+    return (this._currentPopup[this._currentOrigin] != null);
   },
 
   setHeight: function pm_setHeight(height) {
@@ -137,10 +154,15 @@ var PopupManager = {
 
   handleEvent: function pm_handleEvent(evt) {
     switch (evt.type) {
+      case 'click':
+        this.backHandling();
+        break;
       case 'mozbrowserloadstart':
+        this.throbber.classList.add('loading');
         this.handleLoadStart(evt);
         break;
       case 'mozbrowserloadend':
+        this.throbber.classList.remove('loading');
         this.handleLoadEnd(evt);
         break;
       case 'mozbrowseropenwindow':
@@ -157,6 +179,11 @@ var PopupManager = {
             }
           });
         } else {
+          this.throbber.classList.remove('loading');
+          var origin = this.getOrigin(detail.url);
+          this.origin.textContent = origin;
+          detail.frameElement.dataset.origin = origin;
+
           this.open(detail.name, detail.frameElement,
                     evt.target.dataset.frameOrigin, false);
         }
@@ -167,13 +194,22 @@ var PopupManager = {
       case 'home':
         // Reset overlay height before hiding
         this.setHeight(window.innerHeight - StatusBar.height);
-        this.backHandling(evt);
+        this.hide(this._currentOrigin);
         break;
       case 'appwillclose':
-        if (!this._currentPopup)
+        if (!this._currentPopup[evt.detail.origin])
           return;
 
-        this.close();
+        this.hide(evt.detail.origin);
+        break;
+      case 'appopen':
+        this._currentOrigin = evt.detail.origin;
+        this.show();
+        break;
+      case 'appterminated':
+        if (!this._currentPopup[evt.detail.origin])
+          return;
+        this.close(evt.detail.origin);
         break;
       case 'keyboardchange':
         this.setHeight(window.innerHeight -
@@ -183,6 +219,30 @@ var PopupManager = {
         this.setHeight(window.innerHeight - StatusBar.height);
         break;
     }
+  },
+
+  getOrigin: function pm_getOrigin(url) {
+    return url.split('//')[0] + '//' + url.split('//')[1].split('/')[0];
+  },
+
+  getPopupFromOrigin: function pm_getPopupFromOrigin(origin) {
+    return this._currentPopup[origin];
+  },
+
+  show: function pm_show() {
+    if (!this._currentPopup[this._currentOrigin])
+      return;
+
+    this.screen.classList.add('popup');
+    this._currentPopup[this._currentOrigin].style.display = 'block';
+  },
+
+  hide: function pm_hide(origin) {
+    if (!this._currentPopup[origin])
+      return;
+
+    this._currentPopup[origin].style.display = 'none';
+    this.screen.classList.remove('popup');
   }
 };
 
