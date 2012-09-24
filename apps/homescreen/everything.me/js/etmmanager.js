2ca11fe08be79297b39d04c4569610484aff2428
crdlc <crdlc@tid.es>
2012-09-24 14:39:50 +0200

    Do not pick wallpaper from evme

--- apps/homescreen/everything.me/js/etmmanager.js
+++ apps/homescreen/everything.me/js/etmmanager.js
@@ -40,6 +40,12 @@ var EvmeManager = (function() {
   var footerStyle = document.querySelector('#footer').style;
   footerStyle.MozTransition = '-moz-transform .3s ease';
 
+  document.querySelector('#etmPage').addEventListener('contextmenu',
+    function longPress(evt) {
+      evt.stopImmediatePropagation();
+    }
+  );
+
   return {
     openApp: openApp,
 
