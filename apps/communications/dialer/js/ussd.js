2180e237e3773228e1436eb942cbd8668b0c4b68
Germán Toro del Valle <gtorodelvalle@gmail.com>
2012-09-25 16:08:48 +0200

    [Dialer] Application of the UX guidelines to the USSD feature.

--- apps/communications/dialer/js/ussd.js
+++ apps/communications/dialer/js/ussd.js
@@ -3,6 +3,8 @@
 var UssdManager = {
   _conn: window.navigator.mozMobileConnection,
   _popup: null,
+  _origin: document.location.protocol + '//' +
+    document.location.host,
 
   init: function um_init() {
     if (this._conn) {
@@ -11,28 +13,50 @@ var UssdManager = {
     }
   },
 
-  send: function um_send(number) {
+  send: function um_send(message) {
     if (this._conn) {
-      this._conn.sendUSSD(number);
-      this._popup = window.open('./ussd.html', 'ussd');
+      this.clearTimeout();
+      this._conn.sendUSSD(message);
+      this.setTimeout();
+      if (!this._popup) {
+        var urlBase = this._origin + '/dialer/ussd.html';
+        this._popup = window.open(urlBase, 'ussd', 'attention');
+      }
+    }
+  },
+
+  setTimeout: function um_setTimeout() {
+    this._timeout = setTimeout(this.notifyNoResponse.bind(this), 10000);
+  },
+
+  clearTimeout: function um_clearTimeout() {
+    if (this._timeout) {
+      clearTimeout(this._timeout);
+      delete this._timeout;
+    }
+  },
+
+  notifyNoResponse: function um_notifyNoResponse() {
+    if (this._popup) {
+      this._popup.postMessage('noresponse', this._origin);
     }
   },
 
   handleEvent: function ph_handleEvent(evt) {
     if (evt.type == 'ussdreceived') {
+      this.clearTimeout();
       if (this._popup) {
-        var origin = document.location.protocol + '//' +
-          document.location.host;
-        this._popup.postMessage(evt.message, origin);
+        this._popup.postMessage(evt.message, this._origin);
       }
       return;
     }
 
     switch (evt.data.type) {
       case 'reply':
-        this.send(evt.data.number);
+        this.send(evt.data.message);
         break;
       case 'close':
+        this.clearTimeout();
         this._conn.cancelUSSD();
         this._popup = null;
         break;
@@ -40,4 +64,6 @@ var UssdManager = {
   }
 };
 
-UssdManager.init();
+window.addEventListener('localized', function us_startup(evt) {
+  UssdManager.init();
+});
