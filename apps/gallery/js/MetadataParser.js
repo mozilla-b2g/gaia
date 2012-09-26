424e2342bfce37916e2e0a4e15c089c06914afb1
Aditya Bhatt <aditya@adityabhatt.org>
2012-09-26 19:06:18 +0530

    fix crash by avoiding using the same image as thumbnail if it is smaller

--- apps/gallery/js/MetadataParser.js
+++ apps/gallery/js/MetadataParser.js
@@ -130,14 +130,13 @@ var metadataParser = (function() {
 
       // Take the larger of the two scales: we crop the image to the thumbnail
       var scale = Math.max(scalex, scaley);
-
       // If the image was already thumbnail size, it is its own thumbnail
-      if (scale >= 1) {
+      /*if (scale >= 1) {
         offscreenImage.src = null;
         metadata.thumbnail = file;
         callback(metadata);
         return;
-      }
+      }*/
 
       // Calculate the region of the image that will be copied to the
       // canvas to create the thumbnail
