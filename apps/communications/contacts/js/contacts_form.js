e7ec6c904f09ab48386d1dbd5fb5f6be38c01ecd
Alberto Pastor <albert.pastor@gmail.com>
2012-10-01 20:39:17 +0100

    [5553] Avoiding activity reset the photo

--- apps/communications/contacts/js/contacts_form.js
+++ apps/communications/contacts/js/contacts_form.js
@@ -26,7 +26,8 @@ contacts.Form = (function() {
       _,
       formView,
       nonEditableValues,
-      deviceContact;
+      deviceContact,
+      currentPhoto;
 
   var REMOVED_CLASS = 'removed';
   var FB_CLASS = 'facebook';
@@ -277,7 +278,7 @@ contacts.Form = (function() {
     var photo = [];
     var isRemoved = thumbAction.classList.contains(REMOVED_CLASS);
     if (!isRemoved) {
-      photo = currentContact.photo;
+      photo = currentPhoto;
     }
     return photo;
   }
@@ -315,7 +316,8 @@ contacts.Form = (function() {
       myContact['category'] = currentContact['category'];
     }
 
-    myContact['photo'] = getCurrentPhoto();
+    myContact['photo'] = currentContact['photo'] || [];
+    myContact['photo'][0] = getCurrentPhoto();
 
     if (myContact.givenName || myContact.familyName) {
       var name = myContact.givenName || '';
@@ -501,11 +503,12 @@ contacts.Form = (function() {
   }
 
   var resetForm = function resetForm() {
+    currentPhoto = null;
     thumbAction.querySelector('p').classList.remove('hide');
     saveButton.removeAttribute('disabled');
     resetRemoved();
     currentContactId.value = '';
-    currentContact = null;
+    currentContact = {};
     givenName.value = '';
     familyName.value = '';
     company.value = '';
@@ -606,8 +609,7 @@ contacts.Form = (function() {
       var dataurl = this.result.url;  // A data URL for a 320x320 JPEG image
       dataURLToBlob(dataurl, function(blob) {
         Contacts.updatePhoto(blob, thumb);
-        currentContact.photo = currentContact.photo || [];
-        currentContact.photo[0] = blob;
+        currentPhoto = blob;
       });
 
       function dataURLToBlob(dataurl, callback) {
