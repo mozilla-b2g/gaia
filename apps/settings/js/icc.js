62cd1b812b8e908992c9316c09e0cfcda110cf0e
Fernando Rodriguez Sela <frsela@tid.es>
2012-10-08 12:50:28 +0200

    use strict declaration moved inside the closure - Fixes: https://github.com/mozilla-b2g/gaia/pull/5139#discussion_r1721842

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -1,9 +1,9 @@
 /* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil -*- */
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
-'use strict';
-
 (function () {
+  'use strict';
+
   /**
    * Debug method
    */
