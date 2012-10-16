043f72ee58873e620777f5844af403da76392633
Fabien Cazenave <fabien@cazenave.cc>
2012-10-16 12:12:04 +0200

    fix lint errors

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -1010,7 +1010,7 @@ var Browser = {
             icon: item.icon,
             label: item.label,
             callback: function() {
-              evt.detail.contextMenuItemSelected(item.id)
+              evt.detail.contextMenuItemSelected(item.id);
             }
           });
         } else if (item.type === 'menu') {
