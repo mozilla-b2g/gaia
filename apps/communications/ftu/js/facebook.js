9c45e22b368246c568897c61f3afc7130749a0bd
arcturus <arcturus@ardeenelinfierno.com>
2012-10-17 12:10:47 +0100

    Enabling/Disabling facebook import based on configuration
    Linting

--- apps/communications/ftu/js/facebook.js
+++ apps/communications/ftu/js/facebook.js
@@ -1,9 +1,10 @@
 // FB constants
 var fb = window.fb || {};
+// fb.Category: contact category
 fb.CATEGORY = 'facebook';
 
 var FacebookIntegration = {
-	get fbExtensions() {
+  get fbExtensions() {
     delete this.fbExtensions;
     return this.fbExtensions = document.getElementById('fb-extensions');
   },
@@ -11,39 +12,39 @@ var FacebookIntegration = {
     delete this.fbImport;
     return this.fbImport = document.getElementById('fb_import');
   },
-	init: function fb_init(){
-		this.fbImport.addEventListener('click', this);
-		document.addEventListener('fb_imported', this);
-	},
-	handleEvent: function fb_he(event) {
-		switch(event.type) {
-			case 'click':
-				this.fbExtensions.classList.remove('hidden');
-				Contacts.extFb.importFBFromUrl('/contacts/fb_import.html');
-				break;
+  init: function fb_init() {
+    this.fbImport.addEventListener('click', this);
+    document.addEventListener('fb_imported', this);
+  },
+  handleEvent: function fb_he(event) {
+    switch (event.type) {
+      case 'click':
+        this.fbExtensions.classList.remove('hidden');
+        Contacts.extFb.importFBFromUrl('/contacts/fb_import.html');
+        break;
 
-			case 'fb_imported':
+      case 'fb_imported':
         this.fbExtensions.classList.add('hidden');
-				this.updateContactsNumber();
-				break;
-		}
-	},
-	updateContactsNumber: function fb_ucn() {
-		var fbMessage = document.querySelector('#fb_import > small');
-		fbMessage.textContent = '... checking';
+        this.updateContactsNumber();
+        break;
+    }
+  },
+  updateContactsNumber: function fb_ucn() {
+    var fbMessage = document.querySelector('#fb_import > small');
+    fbMessage.textContent = '... checking';
 
-		var fbUpdateTotals = function fbUpdateTotals(imported, total) {
-        if (total == null) {
-          fbMessage.textContent = 'Contacts not imported';
-        } else {
-  		    fbMessage.textContent = imported + ' imported of ' + total;
-        }
-		  };
+    var fbUpdateTotals = function fbUpdateTotals(imported, total) {
+      if (total == null) {
+        fbMessage.textContent = 'Contacts not imported';
+      } else {
+        fbMessage.textContent = imported + ' imported of ' + total;
+      }
+    };
 
-		var req = fb.utils.getNumFbContacts();
+    var req = fb.utils.getNumFbContacts();
 
     req.onsuccess = function() {
-    	var friendsOnDevice = req.result;
+      var friendsOnDevice = req.result;
 
       var callbackListener = {
         'local': function localContacts(number) {
@@ -60,7 +61,27 @@ var FacebookIntegration = {
     req.onerror = function() {
       console.error('Could not get number of local contacts');
     }
-	}
+  }
+};
 
+var FacebookConfiguration = function FacebookConfiguration() {
+  var disableFacebook = function disableFacebook() {
+    FacebookIntegration.fbImport.classList.add('hidden');
+  };
 
-}
+  var enableFacebook = function enableFacebook() {
+    FacebookIntegration.fbImport.classList.remove('hidden');
+  };
+
+  var req = utilities.config.load('/contacts/config.json');
+  req.onload = function(configData) {
+    if (configData.facebookEnabled === true) {
+      enableFacebook();
+    } else {
+      disableFacebook();
+    }
+  };
+  req.onerror = function(code) {
+    disableFacebook();
+  };
+}();
