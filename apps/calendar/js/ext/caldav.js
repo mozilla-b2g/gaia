7ac768b46d2c85c67239e0125b51ce68a45c3cc8
Michal Budzynski <michal@virtualdesign.pl>
2012-10-26 19:39:25 +0200

    test

--- apps/calendar/js/ext/caldav.js
+++ apps/calendar/js/ext/caldav.js
@@ -2032,6 +2032,9 @@ function write (chunk) {
     data: null,
 
     _seralize: function _seralize() {
+      //DEBUG
+      console.log('odpowiedzzz', this.data);
+      
       return this.data;
     },
 
@@ -2465,6 +2468,7 @@ function write (chunk) {
       'DAV:/resourcetype': ArrayHandler,
       'DAV:/current-user-privilege-set': PrivilegeSet,
       'DAV:/principal-URL': HrefHandler,
+      //'DAV:/unauthenticated': TextHandler,
       'DAV:/current-user-principal': HrefHandler,
       'urn:ietf:params:xml:ns:caldav/calendar-data': CalendarDataHandler,
       'DAV:/value': TextHandler,
@@ -2981,13 +2985,15 @@ function write (chunk) {
   function findProperty(name, data, single) {
     var url, results = [], prop;
 
+    console.log('whujdatat', name, data);
+    
     for (url in data) {
       if (Object.hasOwnProperty.call(data, url)) {
         if (name in data[url]) {
           prop = data[url][name];
-          if (prop.status === '200') {
+          //if (prop.status === '200') {
             results.push(data[url][name].value);
-          }
+          //}
         }
       }
     }
@@ -3012,11 +3018,14 @@ function write (chunk) {
       });
 
       find.prop('current-user-principal');
-      find.prop('principal-URL');
+      find.prop('principal-URL');   
+      //find.prop('unauthenticated');
 
       find.send(function(err, data) {
         var principal;
 
+        //console.log('propertyz', findProperty('unauthenticated', data, true));
+        
         if (err) {
           callback(err);
           return;
