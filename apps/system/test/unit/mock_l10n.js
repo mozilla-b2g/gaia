bcc7f6fbf2fadd96f90821f032b799987de91ca6
Julien Wajsberg <felash@gmail.com>
2012-11-06 16:42:24 -0800

    Bug 809275 - [statusbar] add logic to show an icon if there are active downloads Also makes the statusbar.js testable

---
+++ apps/system/test/unit/mock_l10n.js
@@ -0,0 +1,13 @@
+
+var MockL10n = {
+  get: function get(key, params) {
+    return key;
+  },
+  DateTimeFormat: function() {}
+};
+
+MockL10n.DateTimeFormat.prototype = {
+  localeFormat: function mockLocaleFormat(time, strFormat) {
+    return "" + time;
+  }
+};
