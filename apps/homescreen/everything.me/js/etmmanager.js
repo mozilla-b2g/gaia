4c5ca05c0df1664a1f061011dd304baa9bbeaa7b
crdlc <crdlc@tid.es>
2012-11-13 15:25:17 +0100

    Bug 811277 - [Homescreen] Evme - Launching the same app from search should not be loaded again [r=vingtetun]

--- apps/homescreen/everything.me/js/etmmanager.js
+++ apps/homescreen/everything.me/js/etmmanager.js
@@ -3,6 +3,7 @@
 
 var EvmeManager = (function() {
     var currentWindow = null;
+    var currentURL = null;
 
     function openApp(params) {
         var evmeApp = new EvmeApp({
@@ -11,10 +12,11 @@ var EvmeManager = (function() {
             icon: params.icon
         });
 
-        if (currentWindow) {
+        if (currentWindow && currentURL !== params.url) {
             currentWindow.close();
         }
         currentWindow = evmeApp.launch(params.url, params.urlTitle);
+        currentURL = params.url;
     }
 
     function addBookmark(params) {
@@ -44,26 +46,26 @@ var EvmeManager = (function() {
             }
         });
     }
-    
+
     function menuShow() {
         footerStyle.MozTransform = "translateY(0)";
     }
-    
+
     function menuHide() {
         footerStyle.MozTransform = "translateY(75px)";
     }
 
     var footerStyle = document.getElementById("footer").style;
     footerStyle.MozTransition = "-moz-transform .3s ease";
-    
+
     function getMenuHeight() {
         return document.getElementById("footer").offsetHeight;
     }
-    
+
     function getApps() {
         return Applications.getAll();
     }
-    
+
     function getAppIcon(app) {
         return Applications.getIcon(app.origin);
     }
@@ -84,7 +86,7 @@ var EvmeManager = (function() {
         getAppName: getAppName,
 
         openUrl: openUrl,
-        
+
         menuShow: menuShow,
         menuHide: menuHide,
         getMenuHeight: getMenuHeight
