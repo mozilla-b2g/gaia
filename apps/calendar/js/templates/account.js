64e6316ed1d8f6231349d7e4090c2fe764030bd3
Kevin Grandon <kevingrandon@yahoo.com>
2012-11-30 11:51:09 -0800

    Array.join -> String concatenation
    r=lightsofapollo

--- apps/calendar/js/templates/account.js
+++ apps/calendar/js/templates/account.js
@@ -2,24 +2,20 @@
 
   var Account = Calendar.Template.create({
     provider: function() {
-      return [
-        '<li class="', this.h('name'), '">',
-          '<a data-provider="', this.h('name'), '" href="/create-account/', this.h('name'), '">',
-            this.l10n('name', 'preset-'),
-          '</a>',
-        '</li>'
-      ].join('');
+      return '<li class="' + this.h('name') + '">' +
+          '<a data-provider="' + this.h('name') + '" href="/create-account/' + this.h('name') + '">' +
+            this.l10n('name', 'preset-') +
+          '</a>' +
+        '</li>';
     },
 
     account: function() {
-      return [
-        '<li id="account-', this.h('id'), '">',
-          '<a href="/update-account/', this.h('id'), '">',
-            '<span class="preset">', this.l10n('preset', 'preset-'), '</span>',
-            '<span class="user">', this.h('user'), '</span>',
-          '</a>',
-        '</li>'
-      ].join('');
+      return '<li id="account-' + this.h('id') + '">' +
+          '<a href="/update-account/' + this.h('id') + '">' +
+            '<span class="preset">' + this.l10n('preset', 'preset-') + '</span>' +
+            '<span class="user">' + this.h('user') + '</span>' +
+          '</a>' +
+        '</li>';
     }
   });
 
