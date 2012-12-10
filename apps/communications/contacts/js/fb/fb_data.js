3a2e3416901c94e24d9a2c0ca7d697b3349fd00b
Alberto Pastor <albert.pastor@gmail.com>
2012-12-10 14:55:28 -0800

    Bug 816563 - Getting FB contacts in Dialer
    Bug 816563 - Getting FB contacts in Dialer
    
    Adding index to indexedDB
    
    Applying JMC patch
    
    Migrating current db

--- apps/communications/contacts/js/fb/fb_data.js
+++ apps/communications/contacts/js/fb/fb_data.js
@@ -10,7 +10,11 @@ if (!window.fb.contacts) {
       window.indexedDB;
 
     var database;
-    var STORE_NAME = 'FBFriends';
+    var STORE_NAME = 'FBFriendsV2';
+    var OLD_STORE_NAME = 'FBFriends';
+    var INDEX_NAME = 'byTelephone';
+    var isInitialized = false;
+    var migrationNeeded = false;
 
 
     /**
@@ -18,9 +22,74 @@ if (!window.fb.contacts) {
      *
      */
     function createStore(e) {
-      e.target.result.createObjectStore(STORE_NAME, { keyPath: 'uid' });
+      return e.target.result.createObjectStore(STORE_NAME, { keyPath: 'uid' });
     }
 
+    function createStoreAndIndex(e) {
+      var store = createStore(e);
+      store.createIndex(INDEX_NAME,'telephone', {
+        unique: true,
+        multiEntry: true
+      });
+    }
+
+    function upgradeDB(e) {
+      if(e.oldVersion === 1.0 && e.newVersion === 2.0) {
+        window.console.warn('Upgrading Facebook Cache!!!!!');
+        migrationNeeded = true;
+        createStoreAndIndex(e);
+      }
+      else if(e.newVersion === 1.0) {
+        createStore(e);
+      }
+      else if(e.newVersion === 2.0) {
+        createStoreAndIndex(e);
+      }
+    }
+
+    function migrateData() {
+      migrationNeeded = false;
+      var transaction = database.transaction([OLD_STORE_NAME], 'readonly');
+      var objectStore = transaction.objectStore(OLD_STORE_NAME);
+
+      var req = objectStore.mozGetAll();
+
+      req.onsuccess = function(e) {
+        var data = e.target.result;
+        var out = {};
+        data.forEach(function(contact) {
+          var req = fb.contacts.save(contact);
+          req.onsuccess = function saveSuccess() {
+            console.log('Success migrating ', contact.uid);
+          }
+          req.onerror = function saveError() {
+            console.error('Error migrating ', contact.uid);
+          }
+        });
+      };
+
+      req.onerror = function(e) {
+        outRequest.failed(e.target.error);
+      }
+    }
+
+    function initError(outRequest, error) {
+      outRequest.failed(e);
+    }
+
+    function doGet(uid, outRequest) {
+      var transaction = database.transaction([STORE_NAME], 'readonly');
+      var objectStore = transaction.objectStore(STORE_NAME);
+      var areq = objectStore.get(uid);
+
+      areq.onsuccess = function(e) {
+        outRequest.done(e.target.result);
+      };
+
+      areq.onerror = function(e) {
+        outRequest.failed(e.target.error);
+      }
+    }
 
     /**
      *  Allows to obtain the FB contact information by UID
@@ -30,22 +99,73 @@ if (!window.fb.contacts) {
     contacts.get = function(uid) {
       var retRequest = new fb.utils.Request();
 
-      window.setTimeout(function() {
-        var transaction = database.transaction([STORE_NAME], 'readonly');
-        var objectStore = transaction.objectStore(STORE_NAME);
-        var areq = objectStore.get(uid);
-
-        areq.onsuccess = function(e) {
-          retRequest.done(e.target.result);
-        };
-
-        areq.onerror = function(e) {
-          reqRequest.failed(e.target.error);
-        }
-
+      window.setTimeout(function get() {
+        contacts.init(function() {
+          doGet(uid, retRequest);
+        }, function() {
+          initError(retRequest);
+        });
       },0);
 
       return retRequest;
+    }
+
+    function doGetByPhone(tel, outRequest) {
+      var transaction = database.transaction([STORE_NAME], 'readonly');
+      var objectStore = transaction.objectStore(STORE_NAME);
+
+      var index = objectStore.index(INDEX_NAME);
+
+      var areq = index.get(tel);
+
+      areq.onsuccess = function(e) {
+        outRequest.done(e.target.result);
+      }
+
+      areq.onerror = function(e) {
+        outRequest.failed(e.target.error);
+      }
+    }
+
+    contacts.getByPhone = function(tel) {
+      var outRequest = new fb.utils.Request();
+
+      window.setTimeout(function get_by_phone() {
+        contacts.init(function get_by_phone() {
+          doGetByPhone(tel,outRequest);
+        },
+        function() {
+          initError(outRequest);
+        });
+      }, 0);
+
+      return outRequest;
+    }
+
+    function doSave(obj,outRequest) {
+      var transaction = database.transaction([STORE_NAME], 'readwrite');
+
+      transaction.onerror = function(e) {
+        outRequest.failed(e.target.error);
+      }
+
+      var objectStore = transaction.objectStore(STORE_NAME);
+
+      if(Array.isArray(obj.tel) && obj.tel.length > 0) {
+        obj.telephone = [];
+        obj.tel.forEach(function(atel) {
+          obj.telephone.push(atel.value);
+        });
+      }
+
+      var req = objectStore.put(obj);
+      req.onsuccess = function(e) {
+        outRequest.done(e.target.result);
+      }
+
+      req.onerror = function(e) {
+        outRequest.failed(e.target.error);
+      }
     }
 
     /**
@@ -56,52 +176,66 @@ if (!window.fb.contacts) {
     contacts.save = function(obj) {
       var retRequest = new fb.utils.Request();
 
-      window.setTimeout(function() {
-        var transaction = database.transaction([STORE_NAME], 'readwrite');
-
-        transaction.onerror = function(e) {
-          retRequest.failed(e.target.error);
-        }
-
-        var objectStore = transaction.objectStore(STORE_NAME);
-
-        var req = objectStore.put(obj);
-        req.onsuccess = function(e) {
-          retRequest.done(e.target.result);
-        }
-
-        req.onerror = function(e) {
-          retRequest.failed(e.target.error);
-        }
+      window.setTimeout(function save() {
+        contacts.init(function() {
+          doSave(obj, retRequest);
+        },
+        function() {
+          initError(retRequest);
+        });
       },0);
 
-        return retRequest;
+      return retRequest;
+    }
+
+    function doGetAll(outRequest) {
+      var transaction = database.transaction([STORE_NAME], 'readonly');
+      var objectStore = transaction.objectStore(STORE_NAME);
+
+      var req = objectStore.mozGetAll();
+
+      req.onsuccess = function(e) {
+        var data = e.target.result;
+        var out = {};
+        data.forEach(function(contact) {
+          out[contact.uid] = contact;
+        });
+        outRequest.done(out);
+      };
+
+      req.onerror = function(e) {
+        outRequest.failed(e.target.error);
+      }
+    }
+
+    contacts.getAll = function() {
+      var retRequest = new fb.utils.Request();
+
+      window.setTimeout(function() {
+        contacts.init(function get_all() {
+          doGetAll(retRequest);
+        },
+        function() {
+          initError(retRequest);
+        });
+      },0);
+
+      return retRequest;
+    }
+
+    function doRemove(uid,outRequest) {
+      var transaction = database.transaction([STORE_NAME], 'readwrite');
+      transaction.oncomplete = function(e) {
+        outRequest.done(e.target.result);
       }
 
-      contacts.getAll = function() {
-        var retRequest = new fb.utils.Request();
-        window.setTimeout(function() {
-          var transaction = database.transaction([STORE_NAME], 'readonly');
-          var objectStore = transaction.objectStore(STORE_NAME);
-
-          var req = objectStore.mozGetAll();
-
-          req.onsuccess = function(e) {
-            var data = e.target.result;
-            var out = {};
-            data.forEach(function(contact) {
-              out[contact.uid] = contact;
-            });
-            retRequest.done(out);
-          };
-
-          req.onerror = function(e) {
-            retRequest.failed(e.target.error);
-          }
-        }, 0);
-
-        return retRequest;
+      transaction.onerror = function(e) {
+        outRequest.failed(e.target.error);
       }
+      var objectStore = transaction.objectStore(STORE_NAME);
+
+      objectStore.delete(uid);
+    }
 
     /**
      *  Allows to remove FB contact from the DB
@@ -111,27 +245,32 @@ if (!window.fb.contacts) {
     contacts.remove = function(uid) {
       var retRequest = new fb.utils.Request();
 
-      window.setTimeout(function() {
-        var transaction = database.transaction([STORE_NAME], 'readwrite');
-        transaction.oncomplete = function(e) {
-          retRequest.done(e.target.result);
-        }
-
-        transaction.onerror = function(e) {
-          retRequest.failed(e.target.error);
-        }
-        var objectStore = transaction.objectStore(STORE_NAME);
-        objectStore.delete(uid);
+      window.setTimeout(function remove() {
+        contacts.init(function() {
+          doRemove(uid, retRequest);
+        },
+        function() {
+           initError(retRequest);
+        });
       },0);
 
       return retRequest;
     }
 
-    contacts.init = function(cb) {
-      var req = indexedDB.open('Gaia_Facebook_Friends', 1.0);
+    contacts.init = function(cb, errorCb) {
+      if(isInitialized === true) {
+        cb();
+        return;
+      }
+
+      var req = indexedDB.open('Gaia_Facebook_Friends', 2.0);
 
       req.onsuccess = function(e) {
         database = e.target.result;
+        if (migrationNeeded) {
+          migrateData();
+        }
+        isInitialized = true;
         if (typeof cb === 'function') {
           cb();
         }
@@ -140,12 +279,12 @@ if (!window.fb.contacts) {
       req.onerror = function(e) {
         window.console.error('FB: Error while opening the DB: ',
                                                         e.target.error.name);
-        if (typeof cb === 'function') {
-          cb();
+        if (typeof errorCb === 'function') {
+          errorCb();
         }
       };
 
-      req.onupgradeneeded = createStore;
+      req.onupgradeneeded = upgradeDB;
     }
 
   }) (document);
