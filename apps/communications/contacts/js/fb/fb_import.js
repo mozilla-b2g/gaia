eb829a42c8619b619069b8b47d372ea4ebe86228
Jose M. Cantera <jmcanterafonseca@gmail.com>
2012-12-13 19:23:02 +0100

    Bug 821287 - [Facebook] Unlinking all has performance problems with high number of friends

--- apps/communications/contacts/js/fb/fb_import.js
+++ apps/communications/contacts/js/fb/fb_import.js
@@ -70,8 +70,6 @@ if (typeof fb.importer === 'undefined') {
     var scrollableElement = document.querySelector('#mainContent');
     var imgLoader;
 
-    var BLOCK_SIZE = 5;
-
     UI.init = function() {
       var overlay;
 
@@ -584,23 +582,53 @@ if (typeof fb.importer === 'undefined') {
       }
     }
 
+    function getCleaner(mode, contacts, cb) {
+      if (mode === 'update') {
+        var cleaner = new fb.utils.FbContactsCleaner(contacts, mode);
+        window.setTimeout(cleaner.start, 0);
+        cb(cleaner);
+      }
+      else {
+        // Mode === clear
+        var req = fb.utils.clearFbData();
+        req.onsuccess = function() {
+          cb(req.result);
+        }
+
+        req.onerror = function(e) {
+          window.console.error('Error while starting cleaning: ',
+                               e.target.error.name);
+          cb(null);
+        }
+      }
+    }
+
     function cleanContacts(onsuccess, progress) {
       var contacts = [];
+      var unSelectedKeys = Object.keys(unSelectedContacts);
       // FbContactsCleaner expects an Array object
-      Object.keys(unSelectedContacts).forEach(function iterator(uid) {
+      unSelectedKeys.forEach(function iterator(uid) {
         var deviceContacts = unSelectedContacts[uid];
         for (var i = 0; i < deviceContacts.length; i++) {
           contacts.push(deviceContacts[i]);
         }
       });
 
-      var cleaner = new fb.utils.FbContactsCleaner(contacts);
+      // To optimize if the user wishes to unselect all
+      var mode = 'update';
+      if (unSelectedKeys.length === existingFbContacts.length) {
+        mode = 'clear';
+      }
 
-      cleaner.oncleaned = progress.update;
-      cleaner.onsuccess = onsuccess;
-
-      // The cleaning activity should be starting immediately
-      window.setTimeout(cleaner.start, 0);
+      getCleaner(mode, contacts, function got_cleaner(cleaner) {
+        if (cleaner) {
+          cleaner.oncleaned = progress.update;
+          cleaner.onsuccess = onsuccess;
+        }
+        else {
+          Importer.errorHandler();
+        }
+      });
     }
 
     function getTotalUnselected() {
