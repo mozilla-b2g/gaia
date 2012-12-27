53dd83d77b85cc787f30b8af0669de93e357ab47
Vivien Nicolas <21@vingtetun.org>
2012-12-27 13:44:58 +0100

    Bug 822565 - Remove the calculator app

--- apps/calculator/test/integration/atoms/hold.js
+++
@@ -1,6 +0,0 @@
-(function hold(element, length) {
-  SyntheticGestures.touchSupported = false;
-  SyntheticGestures.hold(element, length, 0, 0, 0, 0, 0, function() {
-    marionetteScriptFinished(true);
-  });
-}.apply(this, arguments));
