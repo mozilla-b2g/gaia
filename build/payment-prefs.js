39810a38f9632bc12e5e5b8212cd0742f451c8ce
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-01-07 15:08:43 -0600

    Configure the prod payment provider (bug 824841)
    This also removes the mock payment provider
    that was in use only for testing.

--- build/payment-prefs.js
+++ build/payment-prefs.js
@@ -1,5 +1,5 @@
-pref("dom.payment.provider.0.name", "mockpayprovider");
-pref("dom.payment.provider.0.description", "Mock Payment Provider");
-pref("dom.payment.provider.0.type", "mock/payments/inapp/v1");
-pref("dom.payment.provider.0.uri", "https://mockpayprovider.phpfogapp.com/?req=");
+pref("dom.payment.provider.0.name", "firefoxmarket");
+pref("dom.payment.provider.0.description", "marketplace.firefox.com");
+pref("dom.payment.provider.0.uri", "https://marketplace.firefox.com/mozpay/?req=");
+pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
 pref("dom.payment.provider.0.requestMethod", "GET");
