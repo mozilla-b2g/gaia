788d9517bc325f317e8a140fd75582729765a59d
Fernando Rodriguez Sela <frsela@tid.es>
2013-01-08 12:21:39 +0100

    Remove STK cached data on startup

--- apps/system/js/icc_cache.js
+++ apps/system/js/icc_cache.js
@@ -27,43 +27,51 @@
   }
 
   var icc = window.navigator.mozMobileConnection.icc;
-  window.navigator.mozSetMessageHandler('icc-stkcommand',
-    function handleSTKCommand(command) {
-      debug('STK Proactive Command:', command);
-      if (command.typeOfCommand == icc.STK_CMD_SET_UP_MENU) {
-        debug('STK_CMD_SET_UP_MENU:', command.options);
-        var reqApplications = window.navigator.mozSettings.createLock().set({
-          'icc.applications': JSON.stringify(command.options)
-        });
-        reqApplications.onsuccess = function icc_getApplications() {
-          debug('Cached');
-          icc.sendStkResponse(command, {
-            resultCode: icc.STK_RESULT_OK
+  // Remove previous menu
+  var resetApplications = window.navigator.mozSettings.createLock().set({
+    'icc.applications': {}
+  });
+  resetApplications.onsuccess = function icc_resetApplications() {
+    debug('STK Cache Reseted');
+    // Register to receive STK commands
+    window.navigator.mozSetMessageHandler('icc-stkcommand',
+      function handleSTKCommand(command) {
+        debug('STK Proactive Command:', command);
+        if (command.typeOfCommand == icc.STK_CMD_SET_UP_MENU) {
+          debug('STK_CMD_SET_UP_MENU:', command.options);
+          var reqApplications = window.navigator.mozSettings.createLock().set({
+            'icc.applications': JSON.stringify(command.options)
           });
-        }
-      } else {
-        // Unsolicited command? -> Open settings
-        debug('CMD: ', command);
-        var application = document.location.protocol + '//' +
-          document.location.host.replace('system', 'settings');
-        debug('application: ', application);
-        if (WindowManager.getRunningApps()[application]) {
-          return;   // If settings is opened, we don't manage it
-        }
-        navigator.mozApps.mgmt.getAll().onsuccess = function gotApps(evt) {
-          var apps = evt.target.result;
-          apps.forEach(function appIterator(app) {
-            if (app.origin == application) {
-              var reqIccData = window.navigator.mozSettings.createLock().set({
-                'icc.data': JSON.stringify(command)
-              });
-              reqIccData.onsuccess = function icc_getIccData() {
-                debug('Launching ', app.origin);
-                app.launch();
+          reqApplications.onsuccess = function icc_getApplications() {
+            debug('Cached');
+            icc.sendStkResponse(command, {
+              resultCode: icc.STK_RESULT_OK
+            });
+          }
+        } else {
+          // Unsolicited command? -> Open settings
+          debug('CMD: ', command);
+          var application = document.location.protocol + '//' +
+            document.location.host.replace('system', 'settings');
+          debug('application: ', application);
+          if (WindowManager.getRunningApps()[application]) {
+            return;   // If settings is opened, we don't manage it
+          }
+          navigator.mozApps.mgmt.getAll().onsuccess = function gotApps(evt) {
+            var apps = evt.target.result;
+            apps.forEach(function appIterator(app) {
+              if (app.origin == application) {
+                var reqIccData = window.navigator.mozSettings.createLock().set({
+                  'icc.data': JSON.stringify(command)
+                });
+                reqIccData.onsuccess = function icc_getIccData() {
+                  debug('Launching ', app.origin);
+                  app.launch();
+                }
               }
-            }
-          }, this);
+            }, this);
+          }
         }
-      }
-    });
+      });
+  }
 })();
