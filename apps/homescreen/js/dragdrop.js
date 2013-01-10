3424f9711b49a0160ebf08d3ad8e77d8ff4d134e
crdlc <crdlc@tid.es>
2013-01-10 18:48:05 +0100

    Bug 829050 - Add a config preference to allow disabling of e.me altogether [r=vingtetun]

--- apps/homescreen/js/dragdrop.js
+++ apps/homescreen/js/dragdrop.js
@@ -120,8 +120,8 @@ const DragDropManager = (function() {
           GridManager.goToNextPage(onNavigationEnd);
           transitioning = true;
         }
-      } else if (pageHelper.getCurrentPageNumber() !== 2 &&
-          dirCtrl.limitPrev(currentX)) {
+      } else if (pageHelper.getCurrentPageNumber() > GridManager.landingPage + 1
+                 && dirCtrl.limitPrev(currentX)) {
         isDisabledDrop = true;
 
         pageHelper.getPrevious().appendIconVisible(draggableIcon);
