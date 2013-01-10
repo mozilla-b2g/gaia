d185ed8095a575a95752e91b72413aac46f23463
Fernando Rodriguez Sela <frsela@tid.es>
2013-01-10 12:11:54 +0100

    Hide alert box is only needed when it's open

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -46,7 +46,10 @@
     };
 
     document.getElementById('icc-stk-app-back').onclick = stkResGoBack;
-    document.getElementById('icc-stk-alert-btn_back').onclick = stkResGoBack;
+    document.getElementById('icc-stk-alert-btn_back').onclick = function() {
+      alertbox.classList.add('hidden');
+      stkResGoBack();
+    }
     document.getElementById('icc-stk-help-exit').onclick = updateMenu;
 
     window.onunload = function() {
@@ -109,7 +112,6 @@
   }
 
   function stkResGoBack() {
-    alertbox.classList.add('hidden');
     iccLastCommandProcessed = true;
     responseSTKCommand({
       resultCode: icc.STK_RESULT_BACKWARD_MOVE_BY_USER
