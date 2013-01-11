24f2544d2a824e9d6fdc3d9d76c2bfca47dc88dc
Siddartha Pothapragada <Siddartha.Pothapragada@telekom.com>
2013-01-11 03:18:21 -0800

    Bug 826508 - STK app no option to terminate SIM session. r=ochameau.

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -108,6 +108,16 @@
   }
 
   /**
+   * Send Terminal Response : UICC SESSION TERMINATED BY USER
+   */
+  function sendSessionEndTROnFocusLose() {
+    if (document.mozHidden)
+      responseSTKCommand({
+        resultCode: icc.STK_RESULT_UICC_SESSION_TERM_BY_USER
+      });
+  }
+
+  /**
    * Response ICC Command
    */
   function responseSTKCommand(response, force) {
@@ -552,6 +562,7 @@
 
     debug('STK Input title: ' + options.text);
 
+    document.addEventListener('mozvisibilitychange', sendSessionEndTROnFocusLose, true);
     var li = document.createElement('li');
     var p = document.createElement('p');
     p.id = 'stk-item-title';
