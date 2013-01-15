580b049ca1063d32e634b6f76fc4a240aff90790
Fernando Rodriguez Sela <frsela@tid.es>
2013-01-15 13:02:33 +0100

    Showed optional message on STK_CMD_SEND_* commands

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -241,6 +241,11 @@
         });
         // TODO: Show a spinner instead the message (UX decission).
         // Stop it on any other command
+        if(options.text) {
+          debug("display text" + options.text)
+          command.options.userClear = true;
+          displayText(command);
+        }
         break;
 
       case icc.STK_CMD_SET_UP_CALL:
