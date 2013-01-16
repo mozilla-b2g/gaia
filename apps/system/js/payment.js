782fc34d1e52034322e0e8fe24ea4fbb5e50feaa
Fernando Jiménez <ferjmoreno@gmail.com>
2013-01-16 12:25:14 +0100

    Bug 813811 - Trusted UI for payments needs a localized title; r=jparson

--- apps/system/js/payment.js
+++ apps/system/js/payment.js
@@ -8,16 +8,21 @@
 
 const kPaymentConfirmationScreen = '../payment.html';
 
-var Payment = (function Payment() {
-  var chromeEventId = null;
-  var trustedUILayerID = null;
+var Payment = {
+  chromeEventId: null,
+  trustedUILayerID: null,
 
-  window.addEventListener('mozChromeEvent', function onMozChromeEvent(e) {
+  init: function init() {
+    window.addEventListener('mozChromeEvent', this);
+  },
+
+  handleEvent: function onMozChromeEvent(e) {
     // We save the mozChromeEvent identifiers to send replies back from content
     // with this exact value.
-    chromeEventId = e.detail.id;
-    if (!chromeEventId)
+    this.chromeEventId = e.detail.id;
+    if (!this.chromeEventId)
       return;
+
     switch (e.detail.type) {
       // Chrome asks Gaia to show the payment request confirmation dialog.
       case 'open-payment-confirmation-dialog':
@@ -25,17 +30,15 @@ var Payment = (function Payment() {
         if (!requests)
           return;
 
-        var returnSelection = function returnSelection(selection) {
+        var returnSelection = (function returnSelection(selection) {
           if (!selection)
             return;
 
-          var event = document.createEvent('CustomEvent');
-          event.initCustomEvent('mozContentEvent', true, true, {
-            id: chromeEventId,
+          this._dispatchEvent({
+            id: this.chromeEventId,
             userSelection: selection
           });
-          window.dispatchEvent(event);
-        };
+        }).bind(this);
 
         // If there is only one request, we skip the confirmation dialog and
         // send the request type back to the chrome as a user selection, so
@@ -58,7 +61,7 @@ var Payment = (function Payment() {
           // TODO: Temp layout until issue #2692 is solved.
           var frameDocument = frame.contentWindow.document;
           var requestsList = frameDocument.getElementById('requests')
-                             .getElementsByTagName('ul')[0];
+                                          .getElementsByTagName('ul')[0];
           for (var i in requests) {
             var requestElement = frameDocument.createElement('li');
             var button = frameDocument.createElement('button');
@@ -79,9 +82,7 @@ var Payment = (function Payment() {
           }
         });
 
-        // The payment request confirmation screen is shown within the trusted
-        // UI.
-        TrustedUIManager.open('payment-confirmation', frame, chromeEventId);
+        this._openTrustedUI(frame);
         break;
 
       // Chrome asks Gaia to show the payment flow according to the
@@ -89,40 +90,58 @@ var Payment = (function Payment() {
       case 'open-payment-flow-dialog':
         if (!e.detail.uri)
           return;
+
         // TODO: For now, known payment providers (BlueVia and Mozilla Market)
         //       only accepts the JWT by GET, so we just add it to the URI.
         e.detail.uri += e.detail.jwt;
 
-        trustedUILayerID = chromeEventId;
+        this.trustedUILayerID = this.chromeEventId;
 
         var frame = document.createElement('iframe');
         frame.setAttribute('mozbrowser', 'true');
         frame.classList.add('screen');
         frame.src = e.detail.uri;
-        frame.addEventListener('mozbrowserloadstart', function loadStart(evt) {
+        frame.addEventListener('mozbrowserloadstart', (function loadStart(evt) {
           // After creating the new frame containing the payment provider buy
           // flow, we send it back to chrome so the payment callbacks can be
           // injected.
-          var event = document.createEvent('CustomEvent');
-          event.initCustomEvent('mozContentEvent', true, true, {
-            id: chromeEventId,
+          this._dispatchEvent({
+            id: this.chromeEventId,
             frame: evt.target
           });
-          window.dispatchEvent(event);
-        });
+        }).bind(this));
 
-        // The payment flow is shown within the trusted UI.
-        TrustedUIManager.open('PaymentFlow', frame, trustedUILayerID);
+        // The payment flow is shown within the trusted UI
+        this._openTrustedUI(frame);
         break;
 
       case 'close-payment-flow-dialog':
-        TrustedUIManager.close(trustedUILayerID, function dialogClosed() {
-          var event = document.createEvent('customEvent');
-          event.initCustomEvent('mozContentEvent', true, true,
-                                { id: chromeEventId });
-          window.dispatchEvent(event);
-        });
+        TrustedUIManager.close(this.trustedUILayerID, (function dialogClosed() {
+          this._dispatchEvent({ id: this.chromeEventId });
+        }).bind(this));
         break;
     }
-  });
-})();
+  },
+
+  _openTrustedUI: function _openTrustedUI(frame) {
+    // The payment flow is shown within the trusted UI with the name of
+    // the mozPay caller application as title.
+    var title = WindowManager.getCurrentDisplayedApp().name;
+    title = title ? title : navigator.mozL10n.get('payment-flow');
+    TrustedUIManager.open(title, frame, this.chromeEventId);
+  },
+
+  _dispatchEvent: function _dispatchEvent(obj) {
+    var event = document.createEvent('CustomEvent');
+    event.initCustomEvent('mozContentEvent', true, true, obj);
+    window.dispatchEvent(event);
+  }
+};
+
+// Make sure L10n is ready before init
+if (navigator.mozL10n.readyState == 'complete' ||
+    navigator.mozL10n.readyState == 'interactive') {
+  Payment.init();
+} else {
+  window.addEventListener('localized', Payment.init.bind(Payment));
+}
