d99c338821eda359ea2620a5db3226a9e596535b
Evelyn Hung <jj.evelyn@gmail.com>
2013-01-17 17:36:06 +0800

    bug 823783 - change wifi.js in both System and Settings app
    System app:
    - keep the hardware and the setting in sync even when the settings app isn't running
    Settings app:
    - initial wifi.enabled so that it mirrors the state of the hardware.
    - won't set wifi.enabled when wifi is really on enabled, it's moved to
    System app.

--- apps/settings/js/connectivity.js
+++ apps/settings/js/connectivity.js
@@ -215,6 +215,16 @@ var Connectivity = (function(window, document, undefined) {
   var wifiStatusChangeListeners = [updateWifi];
   var settings = Settings.mozSettings;
 
+  //
+  // Set wifi.enabled so that it mirrors the state of the hardware.
+  // wifi.enabled is not an ordinary user setting because the system
+  // turns it on and off when wifi goes up and down.
+  //
+  settings.createLock().set({'wifi.enabled': gWifiManager.enabled});
+
+  //
+  // Now register callbacks to track the state of the wifi hardware
+  //
   gWifiManager.onenabled = wifiEnabled;
   gWifiManager.ondisabled = wifiDisabled;
   gWifiManager.onstatuschange = wifiStatusChange;
