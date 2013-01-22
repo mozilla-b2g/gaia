6cb3c0bafe56b34883efcd69de6034ba730de9ca
Fernando Rodriguez Sela <frsela@tid.es>
2013-01-22 13:09:25 +0100

    Add a CB to show an alert with the complete message

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -807,7 +807,9 @@
    */
   function displayNotification(command) {
     var options = command.options;
-    NotificationHelper.send('STK', options.text);
+    NotificationHelper.send('STK', options.text, '',function() {
+      alert(options.text);
+    });
   }
 
   /**
