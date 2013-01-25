4a1438a4a4622099602a83212b42b2c364fc5ef0
Fernando Rodriguez Sela <frsela@tid.es>
2013-01-25 08:36:19 +0100

    Move settings to foreground on any STK command. Bug 829827

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -206,6 +206,8 @@
     // By default a generic screen
     setSTKScreenType(STK_SCREEN_DEFAULT);
 
+    reopenSettings();
+
     switch (command.typeOfCommand) {
       case icc.STK_CMD_SELECT_ITEM:
         updateSelection(command);
