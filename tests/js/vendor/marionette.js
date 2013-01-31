1e1c8c0ff2bc7f252fbe95016f108e38ece691a9
James Lal <james@lightsofapollo.com>
2013-01-31 14:49:15 -0800

    Bug 836929 - Fix integration tests switchToFrame call (cherry picked from commit 41a483fbf9c91cf1f6b253367800a280b513fa4f)
    a=npotb
    
    Conflicts:
    	tools/test-agent/package.json

--- tests/js/vendor/marionette.js
+++ tests/js/vendor/marionette.js
@@ -301,6 +301,11 @@
   Client.prototype.start = function start() {
     var i, event, fn;
 
+    if (this.socket && this.socket.readyState < 2) {
+      // don't open a socket is one is already open.
+      return;
+    }
+
     if (this.retry && this.retries >= this.retryLimit) {
       throw new Client.RetryError(
         'Retry limit has been reach retried ' + String(this.retries) + ' times'
@@ -1473,6 +1478,12 @@
 
       if (id instanceof this.Element) {
         cmd.element = id.id;
+      } else if (
+        id !== null &&
+        typeof(id) === 'object' &&
+        id.ELEMENT
+      ) {
+        cmd.element = id.ELEMENT;
       } else if (id) {
         cmd.value = id;
       }
@@ -2238,7 +2249,11 @@
 ));
 (function(module, ns) {
 
-  if (!window.navigator.mozTCPSocket) {
+  try {
+    if (!window.navigator.mozTCPSocket) {
+      return;
+    }
+  } catch(e) {
     return;
   }
 
