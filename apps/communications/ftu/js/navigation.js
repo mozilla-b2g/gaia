04e3e923bdae0773b0133ffa4958831b7124bf7a
Fernando Campo <fernando.campo@o2.com>
2013-02-01 05:43:40 -0800

    Merge pull request #7846 from fcampo/jumpStepIndicator Bug 826623 - Step indicator shows incorrect steps (r=francisco.jordano, a=akeybl)

--- apps/communications/ftu/js/navigation.js
+++ apps/communications/ftu/js/navigation.js
@@ -152,20 +152,15 @@ var Navigation = {
 
     switch (actualHash) {
       case '#languages':
-        UIManager.progressBar.className = 'step-state step-1';
         UIManager.mainTitle.innerHTML = _('language');
         // Hide refresh button in case we end up here coming back from wifi
-        UIManager.activationScreen.classList.add('no-options');
         break;
       case '#data_3g':
-        UIManager.progressBar.className = 'step-state step-2';
         UIManager.mainTitle.innerHTML = _('3g');
         DataMobile.
           getStatus(UIManager.updateDataConnectionStatus.bind(UIManager));
-        UIManager.activationScreen.classList.add('no-options');
         break;
       case '#wifi':
-        UIManager.progressBar.className = 'step-state step-3';
         UIManager.mainTitle.innerHTML = _('wifi');
         UIManager.activationScreen.classList.remove('no-options');
         if (UIManager.navBar.classList.contains('secondary-menu')) {
@@ -176,12 +171,9 @@ var Navigation = {
         WifiManager.scan(UIManager.renderNetworks);
         break;
       case '#date_and_time':
-        UIManager.progressBar.className = 'step-state step-4';
         UIManager.mainTitle.innerHTML = _('dateAndTime');
-        UIManager.activationScreen.classList.add('no-options');
         break;
       case '#import_contacts':
-        UIManager.progressBar.className = 'step-state step-5';
         UIManager.mainTitle.innerHTML = _('importContacts3');
         var fbOption = document.getElementById('fb_import');
         // Enabling or disabling SIM import depending on card status
@@ -201,7 +193,6 @@ var Navigation = {
         }
         break;
       case '#welcome_browser':
-        UIManager.progressBar.className = 'step-state step-6';
         UIManager.mainTitle.innerHTML = _('browserPrivacyChoices');
         break;
       case '#about-your-rights':
@@ -213,6 +204,21 @@ var Navigation = {
         UIManager.navBar.classList.add('back-only');
         break;
     }
+    // Manage step state (dinamically change)
+    var className = 'step-state step-';
+    if (this.skipped && this.currentStep > 2) {
+      className += (this.currentStep - 1) + ' less-steps';
+    } else {
+      className += this.currentStep;
+    }
+    UIManager.progressBar.className = className;
+
+    // Managing options button
+    if (this.currentStep != 3) { //wifi
+      UIManager.activationScreen.classList.add('no-options');
+    }
+
+    // Managing nav buttons when coming back from out-of-steps (privacy)
     if (this.currentStep <= numSteps &&
         steps[this.currentStep].hash === actualHash) {
       UIManager.navBar.classList.remove('back-only');
@@ -220,13 +226,15 @@ var Navigation = {
   },
 
   skipStep: function n_skipStep() {
-    this.currentStep = this.currentStep + (this.currentStep - this.previousStep);
-    if (this.currentStep < 1){
+    this.currentStep = this.currentStep +
+                      (this.currentStep - this.previousStep);
+    if (this.currentStep < 1) {
       this.previousStep = this.currentStep = 1;
     }
     if (this.currentStep > numSteps) {
       this.previousStep = this.currentStep = numSteps;
     }
+    this.skipped = true;
     this.manageStep();
   },
 
@@ -242,7 +250,8 @@ var Navigation = {
     window.location.hash = steps[self.currentStep].hash;
     // SIM card management
     if (steps[this.currentStep].requireSIM) {
-      SimManager.handleCardState(function (response) {
+      SimManager.handleCardState(function check_cardState(response) {
+        self.skipped = false;
         if (!response) {
           self.skipStep();
         }
