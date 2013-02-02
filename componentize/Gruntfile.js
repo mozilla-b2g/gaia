272a2dd99ad605095442488bc95c67c202f84c50
mcp <Admin@clu.(none)>
2013-02-02 17:43:19 +0100

    Added Push to repo

--- componentize/Gruntfile.js
+++ componentize/Gruntfile.js
@@ -3,6 +3,7 @@ var wrench = require('wrench');
 var source = '../shared/style';
 var target = '/tmp/build';
 var targetCheckout = '/gaia-bower';
+var exec = require('child_process').exec;
 
 module.exports = function(grunt) {
   grunt.initConfig({
@@ -15,16 +16,22 @@ module.exports = function(grunt) {
   });
   grunt.registerTask('checkout', function() {
     var done = this.async();
-    var sys = require('sys');
-    var exec = require('child_process').exec;
     child = exec("git clone git@github.com:sebs/gaia-bower.git "+target+targetCheckout, function (error, stdout, stderr) {
-      console.log('repo cloned');
+      console.log(stdout);
       done();
     });
   });
   grunt.registerTask('copy', function() {
     wrench.copyDirSyncRecursive(source, target+targetCheckout+'/style');
   });
-  grunt.registerTask('default', ['clean', 'checkout', 'copy']);
+  grunt.registerTask('commitandpush', function() {
+    var done = this.async();
+    child = exec('cd /tmp/build/gaia-bower; git commit . -m "added files"; git push origin master', function(error, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      done();
+    });    
+  });
+  grunt.registerTask('default', ['clean', 'checkout', 'copy', 'commitandpush']);
 };
 
