61a465039d88db28634a7a99aad10c9743d12839
mcp <Admin@clu.(none)>
2013-02-02 17:22:32 +0100

    Copy etc via tmp to avoid git issues

--- componentize/Gruntfile.js
+++ componentize/Gruntfile.js
@@ -1,22 +1,30 @@
+var fs = require('fs');
+var wrench = require('wrench');      
+var source = '../shared/style';
+var target = '/tmp/build';
+var targetCheckout = '/gaia-bower';
+
 module.exports = function(grunt) {
   grunt.initConfig({
   });
-  grunt.registerTask('copy', function() {
-    var fs = require('fs');
-    var wrench = require('wrench'),
+  grunt.registerTask('clean', function() {
       util = require('util');
-      wrench.rmdirSyncRecursive('gaia-bower/style', true);
-      wrench.mkdirSyncRecursive('gaia-bower/style', 0777);      
-      wrench.copyDirSyncRecursive('../shared/style', 'gaia-bower/style');
-      console.log('files copied');
+      wrench.rmdirSyncRecursive(target, true);
+      wrench.mkdirSyncRecursive(target);
+      console.log('cleaned');
   });
   grunt.registerTask('checkout', function() {
+    var done = this.async();
     var sys = require('sys');
     var exec = require('child_process').exec;
-    child = exec("git clone git@github.com:sebs/gaia-bower.git", function (error, stdout, stderr) {
+    child = exec("git clone git@github.com:sebs/gaia-bower.git "+target+targetCheckout, function (error, stdout, stderr) {
+      console.log('repo cloned');
+      done();
     });
-    console.log('repo cloned');
   });
-  grunt.registerTask('default', ['checkout', 'copy']);
+  grunt.registerTask('copy', function() {
+    wrench.copyDirSyncRecursive(source, target+targetCheckout+'/style');
+  });
+  grunt.registerTask('default', ['clean', 'checkout', 'copy']);
 };
 
