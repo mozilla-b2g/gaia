0fe84a961a86fbc4ef08270b3cd1ca7e9f02918e
vingtetun <21@vingtetun.org>
2013-02-06 05:41:52 -0800

    Bug 835881 - Remove some unused code in the clock app r=vivien

--- apps/clock/js/clock.js
+++ apps/clock/js/clock.js
@@ -1,41 +1,11 @@
 'use strict';
 
-function onTabClick(event) {
-  var target = event.target;
-  var choice = target.dataset.choice;
-  if (!choice)
-    return;
-
-  var view = document.getElementById(choice + '-view');
-  if (!view)
-    return;
-
-  var tabs = document.getElementById('tabs').querySelector('fieldset');
-  var tabsCount = tabs.childElementCount;
-  for (var i = 0; i < tabsCount; i++) {
-    var tab = tabs.children[i];
-    delete tab.dataset.active;
-
-    var tabView = document.getElementById(tab.dataset.choice + '-view');
-    if (tabView)
-      tabView.hidden = true;
-  }
-
-  target.dataset.active = true;
-  view.hidden = false;
-}
-
 // Set the 'lang' and 'dir' attributes to <html> when the page is translated
-window.addEventListener('localized', function showBody() {
+window.addEventListener('localized', function localized() {
   document.documentElement.lang = navigator.mozL10n.language.code;
   document.documentElement.dir = navigator.mozL10n.language.direction;
-  // <body> children are hidden until the UI is translated
-  document.body.classList.remove('hidden');
-});
-
-window.addEventListener('load', function onLoad() {
-  window.removeEventListener('load', onLoad);
-
-  document.getElementById('tabs').onclick = onTabClick;
+  ClockView.init();
+  AlarmList.init();
+  ActiveAlarmController.init();
 });
 
