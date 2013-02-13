11752b8575250a8842691216c03dd800cb7db110
Etienne Segonzac <etienne@segonzac.info>
2013-02-13 09:43:51 -0800

    Merge pull request #8063 from etiennesegonzac/dialer-lazy-telephony-connection
    Bug 840538 - Make sure nothing causes the RIL to get instanciated in the dialer critical path.(cherry picked from commit 4cfe0507e51e962ee6911c477c589e47ffdd6506)

--- apps/communications/dialer/js/telephony_helper.js
+++ apps/communications/dialer/js/telephony_helper.js
@@ -2,8 +2,6 @@
 
 var TelephonyHelper = (function() {
 
-  var telephony = navigator.mozTelephony;
-
   var call = function(number, oncall, onconnected, ondisconnected, onerror) {
     var settings = window.navigator.mozSettings, req;
     if (settings) {
@@ -30,8 +28,7 @@ var TelephonyHelper = (function() {
   };
 
   var startDial = function(number, oncall, connected, disconnected, onerror) {
-    var sanitizedNumber = number.replace(/-/g, '');
-
+    var telephony = navigator.mozTelephony;
     if (telephony) {
       var conn = window.navigator.mozMobileConnection;
       var call;
