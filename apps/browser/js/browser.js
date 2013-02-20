ac811e3add7f2179e02255b2ff6ca43d5dd62c96
Dale  Harvey <dale@arandomurl.com>
2013-02-20 08:38:13 -0800

    Merge pull request #8198 from daleharvey/default-background-tabs
    Bug 836647 - Set newly created tabs to background by default(cherry picked from commit b4beb05b81ad2fd3d6851a0e87757e7d18e7e72c)

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -1207,11 +1207,26 @@ var Browser = {
       }
     }
 
-    if (tab.dom.setVisible)
-      tab.dom.setVisible(visible);
-
+    this.setVisibleWrapper(tab, visible);
     tab.dom.style.display = visible ? 'block' : 'none';
     tab.dom.style.top = '0px';
+  },
+
+  // dom.setVisible is loaded asynchronously from BrowserElementChildPreload
+  // and may require a yield before we call it, we want to make sure to
+  // clear any previous call
+  setVisibleWrapper: function(tab, visible) {
+    if (tab.setVisibleTimeout) {
+      clearTimeout(tab.setVisibleTimeout);
+    }
+    if (tab.dom.setVisible) {
+      tab.dom.setVisible(visible);
+      return;
+    }
+    tab.setVisibleTimeout = setTimeout(function() {
+      if (tab.dom.setVisible)
+        tab.dom.setVisible(visible);
+    });
   },
 
   bindBrowserEvents: function browser_bindBrowserEvents(iframe, tab) {
@@ -1258,8 +1273,9 @@ var Browser = {
       };
     }
 
+    // Default newly created frames to the background
+    this.setVisibleWrapper(tab, false);
     this.bindBrowserEvents(iframe, tab);
-
     this.tabs[tab.id] = tab;
     this.frames.appendChild(iframe);
     return tab.id;
@@ -1748,7 +1764,8 @@ var Browser = {
     switch (activity.source.data.type) {
       case 'url':
         var url = this.getUrlFromInput(activity.source.data.url);
-        this.hideCurrentTab();
+        if (this.currentTab)
+          this.hideCurrentTab();
         this.selectTab(this.createTab(url));
         this.showPageScreen();
         break;
