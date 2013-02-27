386a1ab5e6aaf42025dbb12d3cea433b2e6f26d6
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-02-27 12:22:07 +0100

    Bug 843505 - Define a generic component for importing contacts from different sources

--- apps/communications/ftu/js/fb_launcher.js
+++ apps/communications/ftu/js/fb_launcher.js
@@ -6,8 +6,8 @@ var FbLauncher = (function(document) {
 
     var URIs = {
       facebook: '/contacts/fb_import.html?ftu=1',
-      live: '/contacts/live_import.html'
-    }
+      live: '/contacts/import.html?ftu=1&service=live'
+    };
 
     var access_token, currentURI;
 
@@ -45,12 +45,13 @@ var FbLauncher = (function(document) {
       extensionFrame.className = 'closing';
       window.removeEventListener('message', messageHandler);
 
-       // Notify observers that a change from FB could have happened
-      var event = new CustomEvent('fb_imported', {
-        'detail' : true
-      });
-
-      document.dispatchEvent(event);
+      if (currentURI === URIs.facebook) {
+        // Notify observers that a change from FB could have happened
+        var event = new CustomEvent('fb_imported', {
+          'detail' : true
+        });
+        document.dispatchEvent(event);
+      }
     }
 
 
