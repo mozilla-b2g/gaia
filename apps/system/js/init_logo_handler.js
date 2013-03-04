7b0aa5e218152e2a3d32353c88fb5660422fbfcb
KM Lee <rexboy@mozilla.com>
2013-03-04 16:40:20 +0800

    Bug 847264 - Allow customizing poweroff animation.

--- apps/system/js/init_logo_handler.js
+++ apps/system/js/init_logo_handler.js
@@ -1,14 +1,22 @@
 'use strict';
 
+var CustomLogoPath = {
+  poweron: {
+    video: '/resources/power/carrier_power_on.mp4',
+    image: '/resources/power/carrier_power_on.png'
+  },
+  poweroff: {
+    video: '/resources/power/carrier_power_off.mp4',
+    image: '/resources/power/carrier_power_off.png'
+  }
+};
+
 // Function to animate init starting logo
 var InitLogoHandler = {
-  carrierVideoPath: '/resources/power/carrier_power_on.mp4',
-  carrierImagePath: '/resources/power/carrier_power_on.png',
-  noCarrierVideo: false,
-  noCarrierImage: false,
   ready: false,
+  animated: false,
   readyCallBack: null,
-  carrierPowerOnElement: null,
+  logoLoader: null,
 
   get carrierLogo() {
     delete this.carrierLogo;
@@ -20,46 +28,10 @@ var InitLogoHandler = {
     return (this.osLogo = document.getElementById('os-logo'));
   },
 
-  init: function ilh_init() {
-    var self = this;
-
-    //Load carrier animation / logo if exist.
-    //Only one of them will be success so try loading them at the same time.
-    this._initCarrierVideo();
-    this._initCarrierImage();
-  },
-
-  _initCarrierVideo: function ilh__initCarrierVideo() {
-    var video = document.createElement('video');
-    video.src = this.carrierVideoPath;
-    video.setAttribute('autoplay', null);
-    var self = this;
-    video.oncanplay = function() {
-      self.carrierPowerOnElement = video;
-      self._appendCarrierPowerOn();
-    };
-
-    video.onerror = function() {
-      self.noCarrierVideo = true;
-      if (self.noCarrierImage)
-        self._removeCarrierPowerOn();
-    };
-  },
-
-  _initCarrierImage: function ilh_initCarrierImage() {
-    var self = this;
-    var img = new Image();
-    img.src = this.carrierImagePath;
-    img.onload = function() {
-      self.carrierPowerOnElement = img;
-      self._appendCarrierPowerOn();
-    };
-
-    img.onerror = function() {
-      self.noCarrierImage = true;
-      if (self.noCarrierVideo)
-        self._removeCarrierPowerOn();
-    };
+  init: function ilh_init(logoLoader) {
+    this.logoLoader = logoLoader;
+    logoLoader.onnotfound = this._removeCarrierPowerOn.bind(this);
+    logoLoader.onload = this._appendCarrierPowerOn.bind(this);
   },
 
   _removeCarrierPowerOn: function ilh_removeCarrierPowerOn() {
@@ -78,12 +50,12 @@ var InitLogoHandler = {
 
   _appendCarrierPowerOn: function ilh_appendCarrierPowerOn() {
     if (this.carrierLogo) {
-      this.carrierLogo.appendChild(this.carrierPowerOnElement);
+      this.carrierLogo.appendChild(this.logoLoader.element);
       this._setReady();
     } else {
       var self = this;
       document.addEventListener('DOMContentLoaded', function() {
-        self.carrierLogo.appendChild(self.carrierPowerOnElement);
+        self.carrierLogo.appendChild(self.logoLoader.element);
         self._setReady();
       });
     }
@@ -109,8 +81,13 @@ var InitLogoHandler = {
       return;
     }
 
+    if (this.animated)
+      return;
+
+    this.animated = true;
+
     // No carrier logo - Just animate OS logo.
-    if (self.noCarrierVideo && self.noCarrierImage) {
+    if (!self.logoLoader.found) {
       self.osLogo.classList.add('hide');
 
     // Has carrier logo - Animate carrier logo, then OS logo.
@@ -119,7 +96,7 @@ var InitLogoHandler = {
       // to prevent flashing.
       self.carrierLogo.className = 'transparent';
 
-      var elem = self.carrierPowerOnElement;
+      var elem = self.logoLoader.element;
       if (elem.tagName == 'VIDEO' && !elem.ended) {
         elem.onended = function() {
           elem.classList.add('hide');
@@ -148,4 +125,4 @@ var InitLogoHandler = {
   }
 };
 
-InitLogoHandler.init();
+InitLogoHandler.init(new LogoLoader(CustomLogoPath.poweron));
