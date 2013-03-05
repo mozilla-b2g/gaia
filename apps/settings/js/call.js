d5c25ace466dd56a941846bfd1b778ea8b7c376f
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2013-03-05 08:02:36 -0800

    Merge pull request #7703 from jaoo/827282
    Bug 827282 - [Call Forwarding] Settings app shows out of sync info about...

--- apps/settings/js/call.js
+++ apps/settings/js/call.js
@@ -246,6 +246,8 @@ var Calls = (function(window, document, undefined) {
     });
   }
 
+  // Call subpanel navigation control.
+  var oldHash = document.location.hash || '#root';
   function initCallForwarding() {
     var settings = window.navigator.mozSettings;
     if (!settings) {
@@ -306,6 +308,16 @@ var Calls = (function(window, document, undefined) {
             'ril.cf.' + settingKey + '.enabled', false);
         }
       });
+    });
+
+    window.addEventListener('hashchange', function() {
+      // If navigation is from #root to #call panels then update UI always.
+      if (document.location.hash === '#call' && !oldHash.startsWith('#call-cf-')) {
+        if (!updateCFEntryLock) {
+          updateCallForwardingEntry();
+        }
+      }
+      oldHash = document.location.hash;
     });
   }
 
