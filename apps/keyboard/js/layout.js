ec2b2794c7bbbfe726f9e42b11153da165e486e3
Rudy Lu <left.lu@gmail.com>
2013-03-12 14:39:32 +0800

    Bug 849226 - Polish should have its own keyboard layout. r=djf, a=shira+

--- apps/keyboard/js/layout.js
+++ apps/keyboard/js/layout.js
@@ -1009,6 +1009,84 @@ const Keyboards = {
       ]
     ]
   },
+  pl: {
+    label: 'Polish',
+    imEngine: 'latin',
+    menuLabel: 'polski',
+    alt: {
+      a: 'ąáàâäåãāæ',
+      c: 'çćč',
+      e: 'éèêëēę€ɛ',
+      i: 'ïíìîīį',
+      o: 'öóòôōœøɵ',
+      u: 'üúùûū',
+      s: 'ßśš$',
+      S: 'ŚŠŞ',
+      n: 'ñń',
+      l: 'ł£',
+      y: 'ÿ¥',
+      z: 'žźż',
+      r: 'R$ ',
+      '.': ',?!;:'
+    },
+    keys: [
+      [
+        { value: 'q' }, { value: 'w' }, { value: 'e' } , { value: 'r' },
+        { value: 't' } , { value: 'y' }, { value: 'u' } , { value: 'i' },
+        { value: 'o' }, { value: 'p' }
+      ], [
+        { value: 'a' }, { value: 's' }, { value: 'd' }, { value: 'f' },
+        { value: 'g' } , { value: 'h' }, { value: 'j' }, { value: 'k' },
+        { value: 'l' }, { value: "'", keyCode: 39, hidden: ['email', 'url'] },
+        { value: ':', visible: ['url']}, { value: '_', visible: ['email']}
+      ], [
+        { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+        { value: 'z' }, { value: 'x' }, { value: 'c' }, { value: 'v' },
+        { value: 'b' }, { value: 'n' }, { value: 'm' },
+        { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+      ], [
+        { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+        { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+      ]
+    ],
+    alternateLayout: {
+      alt: {
+        '0': 'º',
+        '1': '1st ',
+        '2': '2nd ',
+        '3': '3rd ',
+        '4': '4th ',
+        '5': '5th ',
+        '6': '6th ',
+        '7': '7th ',
+        '8': '8th ',
+        '9': '9th ',
+        '$': '€ £ ¥ R$',
+        '?': '¿',
+        '!': '¡'
+      },
+      keys: [
+        [
+          { value: '1' }, { value: '2' }, { value: '3' } , { value: '4' },
+          { value: '5' } , { value: '6' }, { value: '7' } , { value: '8' },
+          { value: '9' }, { value: '0' }
+        ], [
+          { value: '@', hidden: ['email'] }, { value: '#' }, { value: '$' },
+          { value: '%' }, { value: '&' } , { value: '*' }, { value: '-' },
+          { value: '+' }, { value: '(' }, { value: ')' },
+          { value: '_', visible: ['email'] }
+        ], [
+          { value: 'ALT', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
+          { value: '!' }, { value: '\"' }, { value: "'" }, { value: ':' },
+          { value: ';' }, { value: '/' }, { value: '?' },
+          { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+        ], [
+          { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+          { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+        ]
+      ]
+    }
+  },
   'jp-kanji': {
     label: 'Japanese - Kanji',
     menuLabel: 'Japanese - Kanji',
