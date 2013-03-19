79710fcbe37ab6ce488539b38799ad534b6f6ac6
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2013-03-19 04:08:17 -0700

    Merge pull request #8601 from ferjm/bug841521
    Bug 841521 - The Trusted UI never opens on first launch (purchase button...(cherry picked from commit 07be1290ffca3c1c6387e19273ec8b05fbce468d)
    
    Conflicts:
    
    	apps/system/js/window_manager.js

--- apps/system/js/window_manager.js
+++ apps/system/js/window_manager.js
@@ -953,47 +953,6 @@ var WindowManager = (function() {
     });
   }
 
-  // Hide current app
-  function hideCurrentApp(callback) {
-    if (displayedApp == null || displayedApp == homescreen)
-      return;
-
-    toggleHomescreen(true);
-    var frame = getAppFrame(displayedApp);
-    frame.classList.add('back');
-    frame.classList.remove('restored');
-    if (callback) {
-      frame.addEventListener('transitionend', function execCallback() {
-        frame.style.visibility = 'hidden';
-        frame.removeEventListener('transitionend', execCallback);
-        callback();
-      });
-    }
-  }
-
-  // If app parameter is passed,
-  // it means there's a specific app needs to be restored
-  // instead of current app
-  function restoreCurrentApp(app) {
-    if (app) {
-      // Restore app visibility immediately but don't open it.
-      var frame = getAppFrame(app);
-      frame.style.visibility = 'visible';
-      frame.classList.remove('back');
-    } else {
-      app = displayedApp;
-      toggleHomescreen(false);
-      var frame = getAppFrame(app);
-      frame.style.visibility = 'visible';
-      frame.classList.remove('back');
-      frame.classList.add('restored');
-      frame.addEventListener('transitionend', function removeRestored() {
-        frame.removeEventListener('transitionend', removeRestored);
-        frame.classList.remove('restored');
-      });
-    }
-  }
-
   function toggleHomescreen(visible) {
     var homescreenFrame = ensureHomescreen();
     if (homescreenFrame && 'setVisible' in homescreenFrame.firstChild)
@@ -2130,8 +2089,7 @@ var WindowManager = (function() {
     getCurrentDisplayedApp: function() {
       return runningApps[displayedApp];
     },
-    hideCurrentApp: hideCurrentApp,
-    restoreCurrentApp: restoreCurrentApp,
+    toggleHomescreen: toggleHomescreen,
     retrieveHomescreen: retrieveHomescreen,
     retrieveFTU: retrieveFTU,
     screenshots: screenshots
