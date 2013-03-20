0efb8d329b61bdd708afd9347271717b5a505e04
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2013-03-20 11:53:49 +0100

    Bug 834103 - [OPEN_][Call setting]call forwarding icon in status bar does not disappear when remove SIM card.(617001982823). r=arthurcc

--- apps/system/js/call_forwarding.js
+++ apps/system/js/call_forwarding.js
@@ -24,12 +24,46 @@
   if (!settings) {
     return;
   }
-  var mobileconnection = window.navigator.mozMobileConnection;
-  if (!mobileconnection) {
+  var mobileConnection = window.navigator.mozMobileConnection;
+  if (!mobileConnection) {
     return;
   }
 
-  mobileconnection.addEventListener('cfstatechange', function(event) {
+  // Initialize the icon based on the card state and whether
+  // it is in airplane mode
+  var _cfIconStateInitialized = false;
+  function initCallForwardingIconState() {
+    var cardState = mobileConnection.cardState;
+    if (_cfIconStateInitialized || cardState !== 'ready')
+      return;
+
+    if (!mobileConnection.iccInfo)
+      return;
+
+    var iccid = mobileConnection.iccInfo.iccid;
+    if (!iccid)
+      return;
+
+    asyncStorage.getItem('ril.cf.enabled.' + iccid, function(value) {
+      if (value === null) {
+        value = false;
+      }
+      settings.createLock().set({'ril.cf.enabled': value});
+      _cfIconStateInitialized = true;
+    });
+  }
+
+  settings.createLock().set({'ril.cf.enabled': false});
+
+  initCallForwardingIconState();
+  mobileConnection.addEventListener('cardstatechange', function() {
+    initCallForwardingIconState();
+  });
+  mobileConnection.addEventListener('iccinfochange', function() {
+    initCallForwardingIconState();
+  });
+
+  mobileConnection.addEventListener('cfstatechange', function(event) {
     if (event &&
         event.reason == _cfReason.CALL_FORWARD_REASON_UNCONDITIONAL) {
       var enabled = false;
@@ -39,7 +73,15 @@
         enabled = true;
       }
       settings.createLock().set({'ril.cf.enabled': enabled});
+      asyncStorage.setItem('ril.cf.enabled.' + mobileConnection.iccInfo.iccid,
+        enabled);
     }
   });
 
+  settings.addObserver('ril.cf.carrier.enabled', function(event) {
+    var showIcon = event.settingValue;
+    settings.createLock().set({'ril.cf.enabled': showIcon});
+    asyncStorage.setItem('ril.cf.enabled.' + mobileConnection.iccInfo.iccid,
+    showIcon);
+  });
 })();
