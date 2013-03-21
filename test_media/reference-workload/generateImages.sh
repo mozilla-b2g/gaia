66affd0ad3db61af2b6ae435ccd3d57e3c4ff94f
Jon Hylands <jhylands@mozilla.com>
2013-03-21 13:41:19 -0400

    Bug 852541 - fixed reference-workload dialer history to determine appID correctly

--- test_media/reference-workload/generateImages.sh
+++ test_media/reference-workload/generateImages.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 if [ -z "$1" ]; then 
   echo Must provide number of iterations
+  exit
 fi
 
 adb push ${SCRIPT_DIR}/MasterGalleryImage.jpg /mnt/sdcard/DCIM/100MZLLA/IMG_0001.jpg
