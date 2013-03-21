ec4ac7098d35714f80897349b1be9edea7ba0eaa
ch.lee <ch.lee@lge.com>
2013-03-21 15:36:20 +0900

    When update button pressed, 'No contacts' screen will not appear.

--- apps/communications/contacts/js/contacts_list.js
+++ apps/communications/contacts/js/contacts_list.js
@@ -779,7 +779,6 @@ contacts.List = (function() {
 
   // Perform contact refresh by id
   var refresh = function refresh(id, callback, op) {
-    remove(id);
     if (typeof(id) == 'string') {
       getContactById(id, function(contact, fbData) {
         var enrichedContact = null;
@@ -787,6 +786,7 @@ contacts.List = (function() {
           var fbContact = new fb.Contact(contact);
           enrichedContact = fbContact.merge(fbData);
         }
+        remove(id);
         addToList(contact, enrichedContact);
         if(callback) {
           callback(id);
