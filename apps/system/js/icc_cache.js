47d0b4249b0c624c904e7f42f7fbe854b7f68dd8
Fabien Cazenave <fabien@cazenave.cc>
2013-04-01 15:24:29 -0700

    Merge pull request #8864 from frsela/STK/Bug854792
    Bug 854792 - [Buri][Settings]it will always go into SATK menu when open settings, r=kaze(cherry picked from commit 35c12720bfe8b9aa35bb5abfb39cc6c8bb146226)
    (cherry picked from commit a29c1b3aed248e42c911bc2d60c64488c2678edf)

--- apps/system/js/icc_cache.js
+++ apps/system/js/icc_cache.js
@@ -65,19 +65,14 @@
 
             function launchSettings() {
               debug('Locating settings . . .');
-              navigator.mozApps.mgmt.getAll().onsuccess = function gotApps(evt) {
+              navigator.mozApps.mgmt.getAll().onsuccess =
+              function gotApps(evt) {
                 var apps = evt.target.result;
                 apps.forEach(function appIterator(app) {
                   if (app.origin != application)
                     return;
-
-                  var reqIccData = window.navigator.mozSettings.createLock().set({
-                    'icc.data': JSON.stringify(command)
-                  });
-                  reqIccData.onsuccess = function icc_getIccData() {
-                    debug('Launching ', app.origin);
-                    app.launch();
-                  };
+                  debug('Launching ', app.origin);
+                  app.launch();
                 }, this);
               };
             }
