3b8ec55740a387aa79cd590a83051d423227d22d
Rick Waldron <waldron.rick@gmail.com>
2013-04-03 14:16:55 -0400

    Bug 853788 - [SMS] support the "full name search" use case r=julienw
    - Bug 853788 - [SMS] support the "full name search" use case …  05c0217
    - Make multi-term searches match on both fields. Fixes display of match… …  db02b1c
    - Add clarification of contact match validation. Set handlers to null (… …  870b7b6
    - Change contact lookup and filter strategy …   037bd66
    - Finalize Validation and Lookup strategies …   6a6b441
    - Fix early bailouts on missing inbound filterValues. Update Lookup Str… …  578cfba
    - Clarifies bailout strategy for avoiding known errors …  e1d70b7
    - Failing tests: multi-term lookups where predominate term is the first… …  142c5cf
    - Fixes: multi-term lookups where predominate term is the first in the … …  49c3938
    - gjslint line length nits …  5ed2251
    - Devolve the isMatch implementation …  99bf4eb
    - Prevent duplicates appearing as a result of fast-typing. Reduces list… …  4ba9a8a
    - Modify MockContact to accept explicit givenName, familyName; Modify M… …  41abe0a
    - Update MockMozContacts.find to support multi-term search result mocks …   d0cba06
    - Update multi-term search validation and matching tests …  d35624e
    - Always remove the previously existing results. …  d3804b0
    - Don't expose isMatch on the Contacts object
    
    Bailout Strategy
    1. A _missing_ filter.filterValue will results in an
       error when passed to mozContacts.find, bailing out
       early avoids the additional overhead of making a
       request that we know will fail.
    2. Missing navigator.mozContacts API or empty but
       present filter.filterValue make the app useless,
       so prevent unpredictable state by making the
       result a no-op.
    
    Validation Strategy
    1. Let _found_ be a register of confirmed terms
    2. Let _contact_ be a contact record.
    3. For each _term_ [...input list], with the label _outer_
     - For each _field_ of [givenName, familyName]
       - For each _value_ in _contact_[ _field_ ]
         - Let _found_[ _term_ ] be the result of calling
             _value_.startsWith( _term_ )
           - If _found_[ _term_ ] is **true**, continue to
             loop labelled _outer_
    4. If every value of _key_ in _found_ is **true** return **true**,
      else return **false**
    
    Lookup Strategy
    1. If only 1 term, set filterValue to that term.
    2. If more than 1 term, find the length predominate term.
    3. If filterValue.length is < 3, set the filterLimit to 10
    4. Remove the length predominate term from the list of validation terms
    5. Make a mozContact.find request with the length predominate term
       as the filter.filterValue.
    6. In the mozContacts.find request success handler, filter the
       results by evaluating each contact record against
       the list of validation terms.
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    (cherry picked from commit e58c98b9eafd24046577c80242f157c31b8c43e0)
    
    Conflicts:
    	apps/sms/js/thread_ui.js

--- apps/sms/test/unit/contacts_test.js
+++ apps/sms/test/unit/contacts_test.js
@@ -23,7 +23,10 @@ suite('Contacts', function(done) {
         var onsuccess, onerror;
 
         navigator.mozContacts.mHistory.push({
-          filter: filter,
+          // make a "copy" of the filter object
+          filter: Object.keys(filter).reduce(function(copy, key) {
+            return (copy[key] = filter[key]) && copy;
+          }, {}),
           request: this
         });
 
@@ -39,40 +42,76 @@ suite('Contacts', function(done) {
             // This will behave like a _REALLY_ fast DB query
             set: function(callback) {
               onsuccess = callback;
-              // Implement a mock that gives results that appear to
-              // match the real behaviour of filtered contacts results
-              this.result = (function() {
-                // Supports the error case
-                if (filter == null ||
-                      (!filter.filterOp || !filter.filterValue)) {
-                  return null;
-                }
+              if (callback !== null) {
+                // Implement a mock that gives results that appear to
+                // match the real behaviour of filtered contacts results
+                this.result = (function() {
+                  // Supports the error case
+                  if (filter == null ||
+                        (!filter.filterOp || !filter.filterValue)) {
+                    return null;
+                  }
 
-                // Supports two "no match" cases
-                if (filter.filterValue === '911' ||
-                    filter.filterValue === 'wontmatch') {
-                  return [];
-                }
+                  // Supports two "no match" cases
+                  if (filter.filterValue === '911' ||
+                      filter.filterValue === 'wontmatch') {
+                    return [];
+                  }
 
-                // All other cases
-                return MockContact.list();
-              }());
+                  if (filter.filterValue === 'jane') {
+                    return MockContact.list([
+                      // true
+                      { givenName: ['Jane'], familyName: ['D'] },
+                      // false
+                      { givenName: ['jane'], familyName: ['austen'] },
+                      // true
+                      { givenName: ['jane'], familyName: ['doe'] },
+                      // false
+                      { givenName: ['jane'], familyName: ['fonda'] },
+                      // true
+                      { givenName: ['jane'], familyName: ['dow'] },
+                      // false
+                      { givenName: ['janet'], familyName: [''] }
+                    ]);
+                  }
 
-              if (this.result === null) {
-                this.error = {
-                  name: 'Mock missing filter params'
-                };
-                if (onerror) {
+                  if (filter.filterValue === 'do') {
+                    return MockContact.list([
+                      // true
+                      { givenName: ['Jane'], familyName: ['Doozer'] },
+                      // false
+                      { givenName: ['doug'], familyName: ['dooley'] },
+                      // true
+                      { givenName: ['jane'], familyName: ['doe'] },
+                      // false
+                      { givenName: ['jerry'], familyName: ['doe'] },
+                      // true
+                      { givenName: ['j'], familyName: ['dow'] },
+                      // true
+                      { givenName: ['john'], familyName: ['doland'] }
+                    ]);
+                  }
+
+                  // All other cases
+                  return MockContact.list();
+                }());
+
+                if (this.result === null) {
+                  this.error = {
+                    name: 'Mock missing filter params'
+                  };
+                  if (onerror) {
+                    setTimeout(function() {
+                      onerror.call(this);
+                      onerror = null;
+                    }.bind(this), 0);
+                  }
+                } else {
                   setTimeout(function() {
-                    onerror.call(this);
-                    onerror = null;
+                    onsuccess.call(this);
+                    onsuccess = null;
                   }.bind(this), 0);
                 }
-              } else {
-                setTimeout(function() {
-                  onsuccess.call(this);
-                  onsuccess = null;
-                }.bind(this), 0);
               }
             }
           },
@@ -80,11 +119,13 @@ suite('Contacts', function(done) {
           onerror: {
             set: function(callback) {
               onerror = callback;
-              if (this.result === null && this.error !== null) {
-                setTimeout(function() {
-                  onerror.call(this);
-                  onerror = null;
-                }.bind(this), 0);
+              if (callback !== null) {
+                if (this.result === null && this.error !== null) {
+                  setTimeout(function() {
+                    onerror.call(this);
+                    onerror = null;
+                  }.bind(this), 0);
+                }
               }
             }
           }
@@ -101,7 +142,7 @@ suite('Contacts', function(done) {
     navigator.mozContacts = nativeMozContacts;
   });
 
-  suite('Contacts.findByString (success)', function() {
+  suite('Contacts.findByString, single-term', function() {
 
     test('(string[tel,givenName,familyName], ...) Match', function(done) {
       var mozContacts = navigator.mozContacts;
@@ -160,7 +201,6 @@ suite('Contacts', function(done) {
       });
     });
 
-
     test('(string[tel], ...) No Match', function(done) {
       var mozContacts = navigator.mozContacts;
 
@@ -181,12 +221,175 @@ suite('Contacts', function(done) {
     });
   });
 
+  suite('Contacts.findByString, multi-term', function() {
+
+    test('no predominate', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('Pepito Grillo', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+        // contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 1);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('no predominate, reversed', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('Grillo Pepito', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 1);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('predominate first, upper', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('Pepi G', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 1);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('predominate last, upper', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('G Pepi', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 1);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+
+    test('predominate first, lower', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('pepi g', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 1);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('predominate last, lower', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('g pepi', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 1);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('no matches, predominate first, upper', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('Pepito S', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 0);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('no matches, predominate last, upper', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('S Pepito', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 0);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('no matches, predominate first, lower', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('pepi s', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 0);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+
+    test('no matches, predominate last, lower', function(done) {
+      var mozContacts = navigator.mozContacts;
+
+      Contacts.findByString('s pepi', function(contacts) {
+        var mHistory = mozContacts.mHistory;
+
+        // No contacts were found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 0);
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mHistory.length, 1);
+        done();
+      });
+    });
+  });
+
   suite('Contacts.findBy (success)', function() {
 
     test('(object, ...), Match', function(done) {
       var mozContacts = navigator.mozContacts;
       var filter = {
-        findBy: ['tel'],
+        filterBy: ['tel'],
         filterOp: 'contains',
         filterValue: '12125559999'
       };
@@ -210,7 +413,7 @@ suite('Contacts', function(done) {
     test('(object, ...), No Match', function(done) {
       var mozContacts = navigator.mozContacts;
       var filter = {
-        findBy: ['tel'],
+        filterBy: ['tel'],
         filterOp: 'contains',
         filterValue: '911'
       };
@@ -242,10 +445,59 @@ suite('Contacts', function(done) {
       Contacts.findBy({}, function(contacts) {
         var mHistory = mozContacts.mHistory;
         assert.equal(contacts, null);
+        done();
+      });
+    });
+  });
+
+  suite('Contacts validation', function() {
+    test('Contact validation, predominate first', function(done) {
+      Contacts.findByString('jane d', function(contacts) {
+        var mozContacts = navigator.mozContacts;
+
+        // contacts were not found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 3);
+
+        /**
+         * The three matching contacts are:
+         *
+         * { givenName: ['Jane'], familyName: ['D'] }
+         * { givenName: ['jane'], familyName: ['doe'] }
+         * { givenName: ['jane'], familyName: ['dow'] }
+         *
+         */
 
         // navigator.mozContacts.find was called?
-        assert.equal(mHistory.length, 1);
-        assert.isNotNull(mHistory[0].request.error);
+        assert.equal(mozContacts.mHistory.length, 1);
+        assert.deepEqual(mozContacts.mHistory[0].filter.filterValue, 'jane');
+
+        done();
+      });
+    });
+
+    test('Contact validation, predominate last', function(done) {
+      Contacts.findByString('j do', function(contacts) {
+        var mozContacts = navigator.mozContacts;
+
+        // contacts were not found
+        assert.ok(Array.isArray(contacts));
+        assert.equal(contacts.length, 5);
+
+        /**
+         * The five matching contacts are:
+         *
+         * { givenName: ['Jane'], familyName: ['Doozer'] }
+         * { givenName: ['jane'], familyName: ['doe'] }
+         * { givenName: ['jerry'], familyName: ['doe'] }
+         * { givenName: ['j'], familyName: ['dow'] }
+         * { givenName: ['john'], familyName: ['doland'] }
+         *
+         */
+
+        // navigator.mozContacts.find was called?
+        assert.equal(mozContacts.mHistory.length, 1);
+        assert.deepEqual(mozContacts.mHistory[0].filter.filterValue, 'do');
 
         done();
       });
