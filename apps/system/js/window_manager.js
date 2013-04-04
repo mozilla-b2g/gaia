9422feaece74c8bbbd8109efc11fd845752c722b
Etienne Segonzac <etienne@segonzac.info>
2013-04-04 10:02:17 -0700

    Merge pull request #8967 from mcirlanaru/bug_835972
    Bug 835972 - Display SIM PIN dialog (if SIM card locked) for apps that ...(cherry picked from commit b126eeb39ed9bfa1e5662386f74611d5d6527b18)
    
    Conflicts:
    
    	apps/system/js/sim_lock.js

--- apps/system/js/window_manager.js
+++ apps/system/js/window_manager.js
@@ -1003,7 +1003,7 @@ var WindowManager = (function() {
 
       var app = runningApps[newApp];
       // Allows listeners to cancel app opening and so stay on homescreen
-      if (!app.frame.dispatchEvent(evt)) {
+      if (!app.iframe.dispatchEvent(evt)) {
         if (typeof(callback) == 'function')
           callback();
         return;
