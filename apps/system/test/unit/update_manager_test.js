057f326d66934a0942f47774513f68295e41a8de
Julien Wajsberg <felash@gmail.com>
2013-04-04 19:26:55 +0200

    Bug 858118 - [system] fix and reenable unit tests for update manager r=etienne
    - fix updatable tests and use the new mocks helper
    - reenable update tests without any more change as I couldn't make them fail.
    Feel free to disable again if this intermittently fails after all.
    - fix lockscreen test for various firefox versions
    (cherry picked from commit 3f9979fe26842d90c01e040f57edb9ff59580766)
    
    Conflicts:
    	apps/system/test/unit/lockscreen_test.js

--- apps/system/test/unit/update_manager_test.js
+++ apps/system/test/unit/update_manager_test.js
@@ -38,13 +38,6 @@ mocksForUpdateManager.forEach(function(mockName) {
   }
 });
 
-/*
-// These tests are currently failing and have been temporarily disabled as per
-// Bug 838993. They should be fixed and re-enabled as soon as possible as per
-// Bug 840500.
-// Please also note: the outcome of this test suite is non-deterministic.
-// Failures occur inconsistently, so potential fixes should be thoroughly
-// vetted.
 suite('system/UpdateManager', function() {
   var realL10n;
   var realWifiManager;
@@ -579,7 +572,6 @@ suite('system/UpdateManager', function() {
       suite('notification behavior after addToDownloadsQueue', function() {
         setup(function() {
           var css = UpdateManager.container.classList;
-          assert.isFalse(css.contains('displayed'));
           UpdateManager.addToDownloadsQueue(uAppWithDownloadAvailable);
         });
 
@@ -1463,4 +1455,3 @@ suite('system/UpdateManager', function() {
     });
   });
 });
-*/
