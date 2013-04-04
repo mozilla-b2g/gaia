90eb5a3e3228e7a05777c5dfe526de044b530e72
Etienne Segonzac <etienne@segonzac.info>
2013-04-04 06:57:25 -0700

    Merge pull request #8928 from aajain/emergency_call_helper
    Bug 839363 - Dialer should allow calls to be placed when in airplane mod...(cherry picked from commit ae542dafb8339a2562060181857ddbe5193cc949)

--- apps/communications/dialer/js/telephony_helper.js
+++ apps/communications/dialer/js/telephony_helper.js
@@ -8,29 +8,13 @@ var TelephonyHelper = (function() {
       displayMessage('BadNumber');
       return;
     }
-    var settings = window.navigator.mozSettings, req;
-    if (settings) {
-      var settingsLock = settings.createLock();
-      req = settingsLock.get('ril.radio.disabled');
-      req.addEventListener('success', function onsuccess() {
-        var status = req.result['ril.radio.disabled'];
-        if (!status) {
-          var conn = window.navigator.mozMobileConnection;
-          if (!conn || !conn.voice.network) {
-            // No voice connection, the call won't make it
-            displayMessage('NoNetwork');
-            return;
-          }
-
-          startDial(sanitizedNumber, oncall, onconnected, ondisconnected,
-            onerror);
-        } else {
-          displayMessage('FlightMode');
-        }
-      });
-    } else {
-      startDial(sanitizedNumber, oncall, onconnected, ondisconnected, onerror);
+    var conn = window.navigator.mozMobileConnection;
+    if (!conn || !conn.voice || !conn.voice.network) {
+      // No voice connection, the call won't make it
+      displayMessage('NoNetwork');
+      return;
     }
+    startDial(sanitizedNumber, oncall, onconnected, ondisconnected, onerror);
   };
 
   function startDial(sanitizedNumber, oncall, connected, disconnected, error) {
@@ -69,6 +53,8 @@ var TelephonyHelper = (function() {
             displayMessage(emergencyOnly ? 'NoNetwork' : 'BadNumber');
           } else if (errorName === 'DeviceNotAcceptedError') {
             displayMessage('DeviceNotAccepted');
+          } else if (errorName === 'RadioNotAvailable') {
+            displayMessage('FlightMode');
           } else {
             // If the call failed for some other reason we should still
             // display something to the user. See bug 846403.
