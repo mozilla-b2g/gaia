7a3fbf0eca5db4161172778a730fdc0894ec37f2
Michal Budzynski <michal@virtualdesign.pl>
2013-04-04 14:00:00 +0100

    Bug 856543 - scaling gallery image in wallpaper app r=davidflanagan

--- apps/wallpaper/js/share.js
+++ apps/wallpaper/js/share.js
@@ -14,13 +14,59 @@ window.onload = function() {
   var url;
 
   function startShare(request) {
+    cancelButton.addEventListener('click', cancelShare);
+
     activity = request;
     blob = activity.source.data.blobs[0];
-    url = URL.createObjectURL(blob);
+    scaleImage(blob, function(resizedBlob) {
+      blob = resizedBlob;
+      url = URL.createObjectURL(blob);
+      preview.style.backgroundImage = 'url(' + url + ')';
+      setButton.addEventListener('click', setWallpaper);
+    });
+  }
 
-    preview.style.backgroundImage = 'url(' + url + ')';
-    setButton.addEventListener('click', setWallpaper);
-    cancelButton.addEventListener('click', cancelShare);
+  var tmpImage;
+  function scaleImage(blobToResize, callback) {
+    tmpImage = new Image();
+
+    tmpImage.onload = function resizeWallpaper() {
+      // The image is corrupted
+      if (tmpImage.naturalWidth == 0 || tmpImage.naturalHeight == 0) {
+        setButton.disabled = true;
+        return;
+      }
+
+      var documentElement = document.documentElement;
+      var canvas = document.createElement('canvas');
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      var ctx = canvas.getContext('2d');
+
+      var origWidth = tmpImage.width;
+      var origHeight = tmpImage.height;
+      var scalex = canvas.width / origWidth;
+      var scaley = canvas.height / origHeight;
+
+      // Take the larger of the two scales: we crop the image to the thumbnail
+      var scale = Math.max(scalex, scaley);
+
+      // Calculate the region of the image that will be copied to the
+      // canvas to create the thumbnail
+      var w = Math.round(canvas.width / scale);
+      var h = Math.round(canvas.height / scale);
+      var x = Math.round((origWidth - w) / 2);
+      var y = Math.round((origHeight - h) / 2);
+
+      ctx.drawImage(
+        tmpImage,
+        x, y, w, h,
+        0, 0, canvas.width, canvas.height
+      );
+
+      canvas.toBlob(callback, 'image/jpeg');
+    }
+    tmpImage.src = window.URL.createObjectURL(blobToResize);
   }
 
   function setWallpaper() {
@@ -54,7 +100,8 @@ window.onload = function() {
 
   function endShare() {
     activity = null;
-    URL.revokeObjectURL(url);
+    window.URL.revokeObjectURL(url);
+    window.URL.revokeObjectURL(tmpImage.src);
     setButton.removeEventListener('click', setWallpaper);
     cancelButton.removeEventListener('click', cancelShare);
   }
