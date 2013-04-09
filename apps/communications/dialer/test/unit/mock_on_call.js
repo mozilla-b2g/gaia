694e0773bee9479f27f1713f0296456c7c73484b
Etienne Segonzac <etienne@segonzac.info>
2013-04-09 15:38:10 +0200

    Bug 857951 - DuT doesn't show 'busy line' screen or play 'busy line' tone.

--- apps/communications/dialer/test/unit/mock_on_call.js
+++ apps/communications/dialer/test/unit/mock_on_call.js
@@ -1,11 +1,17 @@
 var MockOnCallHandler = {
   mLastEntryAdded: null,
+  mNotifyBusyLineCalled: false,
 
   addRecentEntry: function(entry) {
     this.mLastEntryAdded = entry;
   },
 
+  notifyBusyLine: function notifyBusyLine() {
+    this.mNotifyBusyLineCalled = true;
+  },
+
   mTeardown: function() {
     this.mLastEntryAdded = null;
+    this.mNotifyBusyLineCalled = false;
   }
 };
