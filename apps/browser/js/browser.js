63890fe9845c13613e9de6017f742a28ea4ee24d
Ben Francis <ben@tola.me.uk>
2013-04-11 04:48:08 -0700

    Merge pull request #9035 from crh0716/855524
    Bug 855524 - Localize lazy loaded contents(cherry picked from commit 2d48fb6a0d209e39907419762d51b4f4eece7113)

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -106,7 +106,7 @@ var Browser = {
       return;
 
     console.log('----------loadink!');
-    var filesToLoad = [
+    var elementsToLoad = [
       // DOM Nodes with commented content to load
       this.awesomescreen,
       this.crashscreen,
@@ -118,8 +118,10 @@ var Browser = {
       document.getElementById('modal-dialog-prompt'),
       document.getElementById('modal-dialog-confirm'),
       document.getElementById('modal-dialog-custom-prompt'),
-      document.getElementById('http-authentication-dialog'),
+      document.getElementById('http-authentication-dialog')
+    ];
 
+    var filesToLoad = [
       // css files
       'shared/style/headers.css',
       'shared/style/buttons.css',
@@ -155,6 +157,13 @@ var Browser = {
 
     var loadBrowserFiles = function() {
       LazyLoader.load(jsFiles, function() {
+        var mozL10n = navigator.mozL10n;
+        mozL10n.ready(function browser_localizeElements() {
+          elementsToLoad.forEach(function l10nElement(element) {
+            mozL10n.translate(element);
+          });
+        });
+
         domElements.forEach(function createElementRef(name) {
           this[this.toCamelCase(name)] = document.getElementById(name);
         }, this);
@@ -167,7 +176,8 @@ var Browser = {
       }.bind(this));
     }
 
-    LazyLoader.load(filesToLoad, loadBrowserFiles.bind(this));
+    LazyLoader.load(elementsToLoad.concat(filesToLoad),
+      loadBrowserFiles.bind(this));
   },
 
   initRemainingListeners: function browser_initRemainingListeners() {
