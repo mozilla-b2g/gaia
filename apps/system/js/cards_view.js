adad88f6c039fbb065925f44f6e29986b9f1d6f4
Cristian Rodriguez <crdlc@tid.es>
2013-04-12 04:54:53 -0700

    Merge pull request #9117 from crdlc/859259
    Bug 859259 - [Transitions. UX] Implement task switching transitions as s...(cherry picked from commit fa79f2f1101dd904dbda67478ce4e0c336bcd73d)

--- apps/system/js/cards_view.js
+++ apps/system/js/cards_view.js
@@ -12,12 +12,6 @@ var CardsView = (function() {
   //display icon of an app on top of app's card
   var DISPLAY_APP_ICON = false;
   var USER_DEFINED_ORDERING = false;
-  // If 'true', scrolling moves the list one card
-  // at time, and snaps the list so the current card
-  // is centered in the view
-  // If 'false', use free, physics-based scrolling
-  // (Gaia default)
-  var SNAPPING_SCROLLING = true;
   // if 'true' user can close the app
   // by dragging it upwards
   var MANUAL_CLOSING = true;
@@ -39,8 +33,6 @@ var CardsView = (function() {
   // before the timer ticks while in reordering
   // mode
   var allowScrollingWhileSorting = false;
-  // Initial margin of the reordered card
-  var dragMargin = 0;
   // Are we reordering or removing the card now?
   var draggingCardUp = false;
   // Are we moving card left or right?
@@ -49,6 +41,8 @@ var CardsView = (function() {
   var userSortedApps = [];
   var HVGA = document.documentElement.clientWidth < 480;
   var cardsViewShown = false;
+
+  var windowWidth = window.innerWidth;
 
   // init events
   var gd = new GestureDetector(cardsView);
@@ -173,11 +167,7 @@ var CardsView = (function() {
 
     }
 
-    if (SNAPPING_SCROLLING) {
-      cardsView.style.overflow = 'hidden'; //disabling native scrolling
-    }
-
-    if (SNAPPING_SCROLLING || MANUAL_CLOSING) {
+    if (MANUAL_CLOSING) {
       cardsView.addEventListener('mousedown', CardsView);
     }
 
@@ -187,6 +177,8 @@ var CardsView = (function() {
     // If there is a displayed app, take keyboard focus away
     if (displayedApp)
       runningApps[displayedApp].frame.blur();
+
+    placeCards();
 
     function addCard(origin, app, displayedAppCallback) {
       // Display card switcher background first to make user focus on the
@@ -259,12 +251,23 @@ var CardsView = (function() {
 
       cardsList.appendChild(card);
 
-      // If we have a cached screenshot, use that first
-      // We then 'res-in' the correctly sized version
-      var cachedLayer = WindowManager.screenshots[origin];
-      if (cachedLayer) {
-        card.style.backgroundImage = 'url(' + cachedLayer + ')';
-      }
+      card.addEventListener('outviewport', function outviewport() {
+        card.style.display = 'none';
+      });
+
+      card.addEventListener('onviewport', function onviewport() {
+        card.style.display = 'block';
+
+        if (card.style.backgroundImage) {
+          return;
+        }
+
+        // If we have a cached screenshot, use that first
+        // We then 'res-in' the correctly sized version
+        var cachedLayer = WindowManager.screenshots[origin];
+        if (cachedLayer) {
+          card.style.backgroundImage = 'url(' + cachedLayer + ')';
+        }
 
         // And then switch it with screenshots when one will be ready
         // (instead of -moz-element backgrounds)
@@ -295,6 +298,7 @@ var CardsView = (function() {
               }
             };
         }
+      });
 
       // Set up event handling
       // A click elsewhere in the card switches to that task
@@ -386,10 +390,9 @@ var CardsView = (function() {
     function removeCards() {
       cardsView.removeEventListener('transitionend', removeCards);
       screenElement.classList.remove('cards-view');
-
-      while (cardsList.firstElementChild) {
-        cardsList.removeChild(cardsList.firstElementChild);
-      }
+      cardsList.innerHTML = '';
+      prevCardStyle = currentCardStyle = nextCardStyle = currentCard =
+      prevCard = nextCard = deltaX = null;
     }
     if (removeImmediately) {
       removeCards();
@@ -402,9 +405,8 @@ var CardsView = (function() {
     return cardsViewShown;
   }
 
-  //scrolling cards
-  var initialCardViewPosition;
-  var initialTouchPosition = {};
+  //scrolling cards (Positon 0 is x-coord and position 1 is y-coord)
+  var initialTouchPosition = [0, 0];
   // If the pointer down event starts outside of a card, then there's
   // no ambiguity between tap/pan, so we don't need a transition
   // threshold.
@@ -414,38 +416,150 @@ var CardsView = (function() {
   // switching the card.  It doesn't make sense for users to start
   // swiping because they want to stay on the same card.
   var threshold = 1;
+  var thresholdOrdering = 100;
   // Distance after which dragged card starts moving
   var moveCardThreshold = window.innerHeight / 6;
   // Arbitrarily chosen to be 4x larger than the gecko18 drag
   // threshold.  This constant should be a truemm/mozmm value, but
   // it's hard for us to evaluate that here.
   var removeCardThreshold = 100;
+  var switchingCardThreshold = 30;
+
+  var prevCardStyle, currentCardStyle, nextCardStyle, deltaX;
+  var currentCard, prevCard, nextCard;
+
+  // With this object we avoid several if statements
+  var pseudoCard = {
+    style: {
+      // Empty object
+    },
+    dispatchEvent: function() {
+      // Do nothing
+    }
+  };
+
+  var onViewPortEvent = new CustomEvent('onviewport');
+  var outViewPortEvent = new CustomEvent('outviewport');
+
+  // Scale for current card
+  var CC_SCALE = 0.8;
+  // Scale for current card's siblings
+  var SC_SCALE = 0.6;
+  // Opacity value for current card's siblings
+  var SC_OPA = 0.4;
+  var CARD_TRANSITION = '-moz-transform .3s, opacity .3s';
+
+  function placeCards() {
+    currentCard = cardsList.children[currentDisplayed];
+
+    if (!currentCard) {
+      // Fake objects -> index out of bound exception
+      currentCard = nextCard = prevCard = pseudoCard;
+      currentCardStyle = nextCardStyle = prevCardStyle = pseudoCard.style;
+      return;
+    }
+
+    currentCard.dispatchEvent(onViewPortEvent);
+    // Link to the style objects of the cards
+    currentCardStyle = reorderedCard !== currentCard ? currentCard.style :
+                                                       noCard;
+
+    prevCard = currentCard.previousElementSibling || pseudoCard;
+    if (prevCard !== reorderedCard) {
+      prevCard.dispatchEvent(onViewPortEvent);
+      prevCardStyle = prevCard.style;
+    } else {
+      prevCardStyle = pseudoCard.style;
+    }
+
+    nextCard = currentCard.nextElementSibling || pseudoCard;
+    if (nextCard !== reorderedCard) {
+      nextCard.dispatchEvent(onViewPortEvent);
+      nextCardStyle = nextCard.style;
+    } else {
+      nextCardStyle = pseudoCard.style;
+    }
+
+    // Scaling and translating cards to reach target positions
+    prevCardStyle.MozTransform = 'scale(' + SC_SCALE + ') translateX(-100%)';
+    currentCardStyle.MozTransform = 'scale(' + CC_SCALE + ') translateX(0)';
+    nextCardStyle.MozTransform = 'scale(' + SC_SCALE + ') translateX(100%)';
+
+    // Current card sets the z-index to level 2 and opacity to 1
+    currentCardStyle.zIndex = 2;
+    currentCardStyle.opacity = 1;
+    currentCardStyle.pointerEvents = 'auto';
+
+    // Previous and next cards set the z-indez to level 1 and opacity to 0.4
+    prevCardStyle.zIndex = nextCardStyle.zIndex = 1;
+    prevCardStyle.opacity = nextCardStyle.opacity = SC_OPA;
+  }
 
   function alignCurrentCard() {
-    var number = currentDisplayed;
-    if (!cardsList.children[number]) {
-      return;
-    }
+    // We're going to release memory hiding card out of screen
+    deltaX < 0 ? prevCard.dispatchEvent(outViewPortEvent) :
+                 nextCard.dispatchEvent(outViewPortEvent);
 
-    var target = cardsList.children[number];
-    var scrollLeft = cardsView.scrollLeft;
-    var targetScrollLeft = target.offsetLeft;
+    // Disable previous current card
+    currentCardStyle.pointerEvents = 'none';
 
-    var scrollDiff = scrollLeft - targetScrollLeft;
-    if (Math.abs(scrollDiff) < 4) {
-      if (scrollDiff) {
-        // don't assign if this is already the same value
-        cardsView.scrollLeft = targetScrollLeft;
-      }
-      return;
-    }
+    placeCards();
 
-    cardsView.scrollLeft = scrollLeft + (targetScrollLeft - scrollLeft) / 2;
-    target.transform = '';
+    prevCardStyle.MozTransition = nextCardStyle.MozTransition =
+                              currentCardStyle.MozTransition = CARD_TRANSITION;
 
-    window.mozRequestAnimationFrame(function newFrameCallback() {
-      alignCurrentCard();
+    currentCard.addEventListener('transitionend', function transitionend() {
+      currentCard.removeEventListener('transitionend', transitionend);
+      prevCardStyle.MozTransition = currentCardStyle.MozTransition =
+      nextCardStyle.MozTransition = '';
     });
+  }
+
+  function moveCards() {
+    var scaleFactor = Math.abs((deltaX / windowWidth) * (CC_SCALE - SC_SCALE));
+
+    var card = prevCardStyle;
+    var oppositeCard = nextCardStyle;
+    var translateSign = -100;
+    if (deltaX > 0) {
+      var card = nextCardStyle;
+      var oppositeCard = prevCardStyle;
+      var translateSign = 100;
+    }
+
+    var movementFactor = Math.abs(deltaX) / windowWidth;
+
+    // Scaling and translating next or previous sibling
+    card.MozTransform = 'scale(' + (SC_SCALE + scaleFactor) +
+                ') translateX(' + (translateSign * (1 - movementFactor)) + '%)';
+
+    // Fading in new card
+    card.opacity = SC_OPA + (movementFactor * (1 - SC_OPA));
+    // Hiding the opposite sibling card progressively
+    oppositeCard.opacity = SC_OPA - movementFactor;
+    // Fading out current card
+    currentCardStyle.opacity = 1 - (movementFactor * (1 - SC_OPA));
+
+    // Scaling and translating current card
+    currentCardStyle.MozTransform = 'scale(' + (CC_SCALE - scaleFactor) +
+                                    ') translateX(' + -deltaX + 'px)';
+  }
+
+  function isReorderingMode() {
+    return (USER_DEFINED_ORDERING && reorderedCard !== null);
+  }
+
+  function onMoveEventForScrolling(evt) {
+    deltaX = initialTouchPosition[0] - (evt.touches ? evt.touches[0].pageX :
+                                        evt.pageX);
+    moveCards();
+  }
+
+  function onMoveEventForDeleting(evt, deltaY) {
+    var dy = deltaY | initialTouchPosition[1] -
+                              (evt.touches ? evt.touches[0].pageY : evt.pageY);
+    evt.target.style.MozTransform = 'scale(' + CC_SCALE +
+                                                  ') translateY(-' + dy + 'px)';
   }
 
   function onStartEvent(evt) {
@@ -455,54 +569,50 @@ var CardsView = (function() {
     cardsView.addEventListener('mouseup', CardsView);
     cardsView.addEventListener('swipe', CardsView);
 
-    initialCardViewPosition = cardsView.scrollLeft;
-    initialTouchPosition = {
-        x: evt.touches ? evt.touches[0].pageX : evt.pageX,
-        y: evt.touches ? evt.touches[0].pageY : evt.pageY
-    };
+    if (evt.touches) {
+      initialTouchPosition = [evt.touches[0].pageX, evt.touches[0].pageY];
+    } else {
+      initialTouchPosition = [evt.pageX, evt.pageY];
+    }
   }
 
   function onMoveEvent(evt) {
     evt.stopPropagation();
-    var touchPosition = {
-        x: evt.touches ? evt.touches[0].pageX : evt.pageX,
-        y: evt.touches ? evt.touches[0].pageY : evt.pageY
-    };
+    var touchPosition = evt.touches ? [evt.touches[0].pageX,
+                                       evt.touches[0].pageY] :
+                                      [evt.pageX, evt.pageY];
 
-    if (evt.target.classList.contains('card') && MANUAL_CLOSING) {
-      var differenceY = initialTouchPosition.y - touchPosition.y;
-      if (differenceY > moveCardThreshold) {
-        // We don't want user to scroll the CardsView when one of the card is
-        // already dragger upwards
-        draggingCardUp = true;
-        evt.target.style.MozTransform = 'scale(0.6) translate(0, -' +
-                                        differenceY + 'px)';
+    deltaX = initialTouchPosition[0] - touchPosition[0];
+    var deltaY = initialTouchPosition[1] - touchPosition[1];
+
+    if (!isReorderingMode()) {
+      if (MANUAL_CLOSING && deltaY > moveCardThreshold &&
+          evt.target.classList.contains('card')) {
+          // We don't want user to scroll the CardsView when one of the card is
+          // already dragger upwards
+          draggingCardUp = true;
+          cardsView.removeEventListener('mousemove', CardsView);
+          document.addEventListener('mousemove', onMoveEventForDeleting);
+          onMoveEventForDeleting(evt, deltaY);
+      } else {
+        // If we are not reordering or removing Cards now and Snapping
+        // Scrolling is enabled, we want to scroll the CardList
+        if (Math.abs(deltaX) > switchingCardThreshold) {
+          cardsView.removeEventListener('mousemove', CardsView);
+          document.addEventListener('mousemove', onMoveEventForScrolling);
+        }
+
+        moveCards();
       }
-    }
-
-    // If we are not reordering or removing Cards now
-    // and Snapping Scrolling is enabled, we want to scroll
-    // the CardList
-    if (SNAPPING_SCROLLING && reorderedCard === null && !draggingCardUp) {
-      var differenceX = initialTouchPosition.x - touchPosition.x;
-      cardsView.scrollLeft = initialCardViewPosition + differenceX;
-    }
-
     // If re are in reordering mode (there is a DOM element in)
     // reorderedCard variable) we are able to put this element somewere
     // among the others
-    if (USER_DEFINED_ORDERING && reorderedCard !== null) {
-      var differenceX = touchPosition.x - initialTouchPosition.x;
-      // Probably there is more clever solution for calculating
-      // position of transformed DOM element, but this was my
-      // first thought and it seems to work
-      var moveOffset = (cardsList.children[currentDisplayed].offsetLeft / 0.6) +
-                       differenceX - (dragMargin / 0.6);
+    } else {
+      var differenceX = -deltaX;
+      reorderedCard.style.MozTransform = 'scale(0.9) translateX(' +
+                                          differenceX + 'px)';
 
-      reorderedCard.style.MozTransform =
-        'scale(0.6) translate(' + moveOffset + 'px, 0)';
-
-      if (Math.abs(differenceX) > threshold) {
+      if (Math.abs(differenceX) > thresholdOrdering) {
         // We don't want to jump to the next page immediately,
         // We are waiting half a second for user to decide if
         // he wants to leave the Card here or scroll further
@@ -514,7 +624,7 @@ var CardsView = (function() {
           }, 500);
 
           if (differenceX > 0 &&
-              currentDisplayed <= cardsList.children.length) {
+              currentDisplayed < cardsList.children.length - 1) {
             currentDisplayed++;
             sortingDirection = 'right';
             alignCurrentCard();
@@ -535,6 +645,8 @@ var CardsView = (function() {
 
     document.releaseCapture();
     cardsView.removeEventListener('mousemove', CardsView);
+    document.removeEventListener('mousemove', onMoveEventForDeleting);
+    document.removeEventListener('mousemove', onMoveEventForScrolling);
     cardsView.removeEventListener('mouseup', CardsView);
     cardsView.removeEventListener('swipe', CardsView);
 
@@ -546,29 +658,32 @@ var CardsView = (function() {
       dy = eventDetail.dy;
       direction = eventDetail.direction;
     } else {
-      var touchPosition = {
-        x: evt.touches ? evt.touches[0].pageX : evt.pageX,
-        y: evt.touches ? evt.touches[0].pageY : evt.pageY
-      };
-      dx = touchPosition.x - initialTouchPosition.x;
-      dy = touchPosition.y - initialTouchPosition.y;
+      if (evt.touches) {
+        dx = evt.touches[0].pageX - initialTouchPosition[0];
+        dy = evt.touches[0].pageY - initialTouchPosition[1];
+      } else {
+        dx = evt.pageX - initialTouchPosition[0];
+        dy = evt.pageY - initialTouchPosition[1];
+      }
       direction = dx > 0 ? 'right' : 'left';
     }
 
-    if (SNAPPING_SCROLLING && !draggingCardUp && reorderedCard === null) {
+    if (!draggingCardUp && reorderedCard === null) {
       if (Math.abs(dx) > threshold) {
+        deltaX = dx;
         direction = dx > 0 ? 'right' : 'left';
         if (direction === 'left' &&
             currentDisplayed < cardsList.children.length - 1) {
           currentDisplayed++;
-          alignCurrentCard();
         } else if (direction === 'right' && currentDisplayed > 0) {
           currentDisplayed--;
-          alignCurrentCard();
         }
+        alignCurrentCard();
       } else {
         alignCurrentCard();
       }
+
+      return;
     }
 
     // if the element we start dragging on
@@ -611,9 +726,18 @@ var CardsView = (function() {
         element.style.MozTransform = '';
         alignCurrentCard();
       }
+
+      return;
     }
 
-    if (USER_DEFINED_ORDERING && reorderedCard !== null) {
+    if (isReorderingMode()) {
+      if (reorderedCard === cardsList.children[currentDisplayed]) {
+        reorderedCard = null;
+        alignCurrentCard();
+
+        return;
+      }
+
       // Position of the card depends on direction of scrolling
       if (sortingDirection === 'right') {
         if (currentDisplayed <= cardsList.children.length) {
@@ -630,10 +754,8 @@ var CardsView = (function() {
           cardsList.children[currentDisplayed]
         );
       }
-      reorderedCard.style.MozTransform = '';
-      reorderedCard.dataset['edit'] = 'false';
-      reorderedCard = null;
 
+      reorderedCard = null;
       alignCurrentCard();
 
       // remove the app origin from ordering array
@@ -646,13 +768,19 @@ var CardsView = (function() {
     }
   }
 
+  function setEditMode(card) {
+    var style = card.style;
+    style.zIndex = 3;
+    style.opacity = 0.8;
+    style.MozTransform = 'scale(0.9) translateX(0)';
+  }
+
   function manualOrderStart(evt) {
     evt.preventDefault();
     reorderedCard = evt.target;
     allowScrollingWhileSorting = true;
     if (reorderedCard.classList.contains('card')) {
-      dragMargin = reorderedCard.offsetLeft;
-      reorderedCard.dataset['edit'] = true;
+      setEditMode(reorderedCard, true);
       sortingDirection = 'left';
     }
   }
@@ -732,4 +860,3 @@ window.addEventListener('attentionscreenhide', CardsView);
 window.addEventListener('holdhome', CardsView);
 window.addEventListener('home', CardsView);
 window.addEventListener('appwillopen', CardsView);
-
