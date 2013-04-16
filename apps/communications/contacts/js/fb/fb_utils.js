cb2424a694fd7a3b44ef3909769a24549890a5ef
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-04-16 02:43:03 -0700

    Bug 861843 - Intermittent issue: Contact Settings screen does not appear...

--- apps/communications/contacts/js/fb/fb_utils.js
+++ apps/communications/contacts/js/fb/fb_utils.js
@@ -2,9 +2,9 @@
 
 var fb = window.fb || {};
 
-if (!fb.utils) {
   (function(document) {
-    var Utils = fb.utils = {};
+    var Utils = fb.utils || {};
+    fb.utils = Utils;
 
     var TIMEOUT_QUERY = fb.operationsTimeout;
     var FRIEND_COUNT_QUERY = 'select friend_count from user where uid=me()';
@@ -449,5 +449,3 @@ if (!fb.utils) {
     } // FbContactsCleaner
 
   })(document);
-
-}
