3f96a6d72cc6a31a4b5bcede29b00f0b9656fac6
Alive.Kuo <alegnadise@gmail.com>
2013-04-17 02:34:36 -0700

    Merge pull request #9222 from askeing/Bug-862664
    Bug 862664 - [CustomizeUI] Cannot launch FTU when use customize power on image, r=alive(cherry picked from commit 99006f507a53b3824f125a5a719099011cc74684)

--- apps/system/js/init_logo_handler.js
+++ apps/system/js/init_logo_handler.js
@@ -97,7 +97,7 @@ var InitLogoHandler = {
       self.carrierLogo.className = 'transparent';
 
       var elem = self.logoLoader.element;
-      if (elem.tagName == 'VIDEO' && !elem.ended) {
+      if (elem.tagName.toLowerCase() == 'video' && !elem.ended) {
         elem.onended = function() {
           elem.classList.add('hide');
           // XXX workaround of bug 831747
@@ -108,11 +108,13 @@ var InitLogoHandler = {
         };
       } else {
         elem.classList.add('hide');
-        // XXX workaround of bug 831747
-        // Unload the video. This releases the video decoding hardware
-        // so other apps can use it.
-        elem.removeAttribute('src');
-        elem.load();
+        if (elem.tagName.toLowerCase() == 'video') {
+            // XXX workaround of bug 831747
+            // Unload the video. This releases the video decoding hardware
+            // so other apps can use it.
+            elem.removeAttribute('src');
+            elem.load();
+        }
       }
 
       self.carrierLogo.addEventListener('transitionend',
