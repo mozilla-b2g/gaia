84d2d5f36e44262dfe8f4b89c58311975fa4f0a9
crdlc <crdlc@tid.es>
2013-04-17 15:55:15 +0200

    Bug 861846 - Landscape orientation locked apps render portrait in the task manager, which breaks the view of the app

--- apps/system/js/window_manager.js
+++ apps/system/js/window_manager.js
@@ -2139,6 +2139,14 @@ var WindowManager = (function() {
     getCurrentDisplayedApp: function() {
       return runningApps[displayedApp];
     },
+    getOrientationForApp: function(origin) {
+      var app = runningApps[origin];
+
+      if (!app || !app.manifest)
+        return;
+
+      return app.manifest.orientation;
+    },
     toggleHomescreen: toggleHomescreen,
     retrieveHomescreen: retrieveHomescreen,
     screenshots: screenshots
