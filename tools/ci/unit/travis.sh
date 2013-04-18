a0b3ed81e1234b56ae99216aaeb5355ac892a606
Yuren Ju <yurenju@gmail.com>
2013-04-18 17:16:38 +0200

    bug 863684 - made travis log readable

--- tools/ci/unit/travis.sh
+++ tools/ci/unit/travis.sh
@@ -22,36 +22,42 @@ function waiting_port {
 
 function section_echo {
   echo ${GREEN_COLOR}$1${NORMAL_COLOR}
+  echo ${GREEN_COLOR}`seq -s= $(expr ${#1} + 1)|tr -d '[:digit:]'`${NORMAL_COLOR}
 }
 
-section_echo 'Download and install closure linter'
-sudo easy_install $GJSLINT_PACKAGE_URL
 echo
+section_echo 'Preparing test environment'
 
+echo 'Downloading and installing closure linter'
+sudo easy_install $GJSLINT_PACKAGE_URL &> /dev/null
+
+echo 'Downloading Firefox'
+curl -s "$FIREFOX_URL" | tar jx &> /dev/null
+
+echo 'Downloading & installing node dependencies'
+make common-install &> /dev/null
+
+# Make gaia for test-agent environment
+echo 'Downloading xulrunner-sdk and making profile for testing (more than 5 minutes)'
+DEBUG=1 WGET_OPTS=-nv make &> /dev/null
+
+echo 'Starting test-agent-server and waiting for server to start on port 8789'
+make test-agent-server &> /dev/null &
+waiting_port 8789
+
+echo 'Starting Firefox'
+firefox/firefox -profile `pwd`/profile "$TESTAGENT_URL" &> /dev/null &
+waiting_port 8080
+sleep 5
+
+echo
 section_echo 'make lint'
 make lint
 LINT_RESULT_STATUS=$?
 echo
 
-section_echo 'Download Firefox and install all test-agent-server dependencies'
-curl "$FIREFOX_URL" | tar jx
-make common-install
-echo
-
-# Make gaia for test-agent environment
-DEBUG=1 WGET_OPTS=-nv make
-
-section_echo 'Start test-agent-server and waiting for server to start on port 8789'
-make test-agent-server &
-waiting_port 8789
-
-section_echo 'Start Firefox'
-firefox/firefox -profile `pwd`/profile "$TESTAGENT_URL" &
-waiting_port 8080
-sleep 5
-
 section_echo 'make test-agent-test'
-make test-agent-test
+make test-agent-test REPORTER=Min
 TEST_RESULT_STATUS=$?
 echo
 
