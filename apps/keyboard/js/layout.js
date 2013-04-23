140a9f7a6ade455763aca5e65b2572a51c731746
Rudy Lu <left.lu@gmail.com>
2013-04-23 03:51:23 -0700

    Merge pull request #9169 from RudyLu/keyboard/Bug849828-add_Serbian_latin
    Bug 849828 - Add Serbian (latin) keyboard layout. r=stas, a=leo+ (cherry picked from commit d092d010e28609478bc3d16ffc92f6c86dd26425)

--- apps/keyboard/js/layout.js
+++ apps/keyboard/js/layout.js
@@ -594,6 +594,65 @@ const Keyboards = {
       ]
     ]
   },
+  'sr-Latn': {
+    label: 'Serbian (Latin)',
+    menuLabel: 'srpski',
+    alt: {
+      c: 'čć',
+      d: 'đ',
+      s: 'š',
+      z: 'ž',
+      '.': ',?!;:'
+    },
+    keys: [
+      [
+        { value: 'q' }, { value: 'w' }, { value: 'e' } , { value: 'r' },
+        { value: 't' } , { value: 'y' }, { value: 'u' } , { value: 'i' },
+        { value: 'o' }, { value: 'p' }
+      ], [
+        { value: 'a' }, { value: 's' }, { value: 'd' }, { value: 'f' },
+        { value: 'g' } , { value: 'h' }, { value: 'j' }, { value: 'k' },
+        { value: 'l' }, { value: '"', hidden: ['email', 'url'] },
+        { value: ':', visible: ['url']}, { value: '_', visible: ['email']}
+      ], [
+        { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+        { value: 'z' }, { value: 'x' }, { value: 'c' }, { value: 'v' },
+        { value: 'b' }, { value: 'n' }, { value: 'm' },
+        { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+      ], [
+        { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+        { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+      ]
+    ],
+    alternateLayout: {
+      alt: {
+        '0': 'º',
+        '$': '€£¥',
+        '?': '¿',
+        '!': '¡'
+      },
+      keys: [
+        [
+          { value: '1' }, { value: '2' }, { value: '3' } , { value: '4' },
+          { value: '5' } , { value: '6' }, { value: '7' } , { value: '8' },
+          { value: '9' }, { value: '0' }
+        ], [
+          { value: '@', hidden: ['email'] }, { value: '#' }, { value: '$' },
+          { value: '%' }, { value: '&' } , { value: '*' }, { value: '-' },
+          { value: '+' }, { value: '(' }, { value: ')' },
+          { value: '_', visible: ['email'] }
+        ], [
+          { value: 'ALT', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
+          { value: '!' }, { value: '\"' }, { value: "'" }, { value: ':' },
+          { value: ';' }, { value: '/' }, { value: '?' },
+          { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+        ], [
+          { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+          { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+        ]
+      ]
+    }
+  },
   sk: {
     label: 'Slovak',
     menuLabel: 'Slovenčina',
