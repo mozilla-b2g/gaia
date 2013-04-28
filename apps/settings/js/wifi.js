f89ee29d007c5bd9385cacca1b25a0dc62282b54
Masashi Honma <masashi.honma@gmail.com>
2013-04-28 13:03:23 +0900

    Bug 866484 - [WiFi] Use shared code for settings

--- apps/settings/js/wifi.js
+++ apps/settings/js/wifi.js
@@ -11,7 +11,7 @@ navigator.mozL10n.ready(function wifiSettings() {
   if (!settings)
     return;
 
-  var gWifiManager = getWifiManager();
+  var gWifiManager = WifiHelper.getWifiManager();
   var gWifi = document.querySelector('#wifi');
   var gWifiCheckBox = document.querySelector('#wifi-enabled input');
   var gWifiInfoBlock = document.querySelector('#wifi-desc');
@@ -511,7 +511,7 @@ navigator.mozL10n.ready(function wifiSettings() {
       wifiConnect();
     } else {
       // offline, unknown network: propose to connect
-      var key = getKeyManagement();
+      var key = WifiHelper.getKeyManagement();
       switch (key) {
         case 'WEP':
         case 'WPA-PSK':
@@ -539,19 +539,8 @@ navigator.mozL10n.ready(function wifiSettings() {
       gCurrentNetwork = null;
     }
 
-    function getKeyManagement() {
-      var key = network.capabilities[0];
-      if (/WEP$/.test(key))
-        return 'WEP';
-      if (/PSK$/.test(key))
-        return 'WPA-PSK';
-      if (/EAP$/.test(key))
-        return 'WPA-EAP';
-      return '';
-    }
-
     function setPassword(password, identity) {
-      var key = getKeyManagement();
+      var key = WifiHelper.getKeyManagement();
       if (key == 'WEP') {
         network.wep = password;
       } else if (key == 'WPA-PSK') {
