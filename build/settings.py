77ea18320fdae6520dbbfb759064968f610a9065
Corey Frang <gnarf@gnarf.net>
2013-04-29 14:56:13 -0700

    Bug 845173 - [MMS][Utility] smil document generate and attchment extract utility for MMS message - r=julienw
    Implements a parser and generator for SMIL XML format, the format used by gecko for MMS, which is converted to/from arrays for consuming in the sms application.
    
    Co-Authored by @steveck-chung and @gnarf37

--- build/settings.py
+++ build/settings.py
@@ -101,6 +101,7 @@ settings = {
  "ril.mms.mmsproxy": "",
  "ril.mms.passwd": "",
  "ril.mms.user": "",
+ "ril.mms.retrieval_mode": "automatic",
  "ril.radio.preferredNetworkType": "",
  "ril.radio.disabled": False,
  "ril.supl.apn": "",
