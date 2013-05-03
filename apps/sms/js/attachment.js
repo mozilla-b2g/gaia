513b37597c56a68c3182792f6dbde74214731bbb
Greg <greg@bocoup.com>
2013-05-03 17:16:04 -0400

    Bug 840069 - [MMS][User Story] Message preview including size - r=borjasalguero
    Pull Request #9385
    (cherry picked from commit 081391ea5338351cc9ad5f03786ebdadd5246f67)
    
    Conflicts:
    	apps/sms/style/sms.css

---
+++ apps/sms/js/attachment.js
@@ -0,0 +1,45 @@
+/** Creates an Attachment object
+ * @param {String} type of attachment (image, video, etc).
+ * @param {String} uri Location or datauri of image to show.
+ * @param {Number} size Size of attachment in bytes.
+ * @return {Attachment} new attachment object.
+ *
+ * The render method creates an iframe to represent the
+ * attachment in the message composition area. An iframe
+ * is used because Gecko will still try to put the
+ * cursor into elements with [contentEditable=false].
+ * Instead of a bunch of JavaScript to manage where the
+ * caret is and what to delete on backspace, the
+ * contentEditable region treats the iframe as a simple
+ * block. Win.
+ *
+ * It uses the main sms.css stylesheet so that styles
+ * can be defined in that.
+ */
+
+'use strict';
+
+function Attachment(type, uri, size) {
+  this.type = type;
+  this.uri = uri;
+  this.size = size;
+}
+
+Attachment.prototype = {
+  render: function() {
+    var _ = navigator.mozL10n.get;
+    var el = document.createElement('iframe');
+    el.setAttribute('sandbox', '');
+    var src = 'data:text/html,';
+    // We want kilobytes so we divide by 1024, with one fractional digit
+    var size = Math.floor(this.size / 102.4) / 10;
+    var sizeString = _('attachmentSize', {n: size});
+    src += Utils.Template('attachment-tmpl').interpolate({
+      uri: this.uri,
+      size: sizeString
+    });
+    el.src = src;
+    el.className = 'attachment';
+    return el;
+  }
+};
--- apps/sms/test/unit/setup.js
+++ apps/sms/test/unit/setup.js
@@ -2,5 +2,3 @@
 
 require('/shared/test/unit/load_body_html_helper.js');
 requireApp('sms/test/unit/mocks_helper.js');
-require('/shared/test/unit/load_body_html_helper.js');
-
