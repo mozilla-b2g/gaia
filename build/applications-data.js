75dd1de2b22fb22a37bf84a90a0193253e5e3b79
Kevin Grandon <kevingrandon@yahoo.com>
2013-05-06 18:32:04 -0700

    Bug 867781 - Move calendar configuration into distribution file.

--- build/applications-data.js
+++ build/applications-data.js
@@ -103,7 +103,7 @@ function getDistributionFileContent(name, defaultContent) {
       return getFileContent(distributionFile);
     }
   }
-  return JSON.stringify(defaultContent);
+  return JSON.stringify(defaultContent, null, '  ');
 }
 
 // zeroth grid page is the dock
@@ -287,3 +287,65 @@ content = {
 }
 
 writeContent(init, getDistributionFileContent('icc', content));
+
+// Calendar Config
+init = getFile(GAIA_DIR, 'apps', 'calendar', 'js', 'presets.js');
+content = {
+  'google': {
+    providerType: 'Caldav',
+    group: 'remote',
+    authenticationType: 'oauth2',
+    apiCredentials: {
+      tokenUrl: 'https://accounts.google.com/o/oauth2/token',
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
+      user_info: {
+        url: 'https://www.googleapis.com/oauth2/v3/userinfo',
+        field: 'email'
+      },
+      client_secret: 'jQTKlOhF-RclGaGJot3HIcVf',
+      client_id: '605300196874-1ki833poa7uqabmh3hq' +
+                 '6u1onlqlsi54h.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/calendar ' +
+             'https://www.googleapis.com/auth/userinfo.email',
+      redirect_uri: 'https://oauth.gaiamobile.org/authenticated'
+    },
+    options: {
+      domain: 'https://apidata.googleusercontent.com',
+      entrypoint: '/caldav/v2/',
+      providerType: 'Caldav'
+    }
+  },
+
+  'yahoo': {
+    providerType: 'Caldav',
+    group: 'remote',
+    options: {
+      domain: 'https://caldav.calendar.yahoo.com',
+      entrypoint: '/',
+      providerType: 'Caldav',
+      user: '@yahoo.com',
+      usernameType: 'email'
+    }
+  },
+
+  'caldav': {
+    providerType: 'Caldav',
+    group: 'remote',
+    options: {
+      domain: '',
+      entrypoint: '',
+      providerType: 'Caldav'
+    }
+  },
+
+  'local': {
+    singleUse: true,
+    providerType: 'Local',
+    group: 'local',
+    options: {
+      providerType: 'Local'
+    }
+  }
+};
+
+writeContent(init, 'Calendar.Presets = ' + getDistributionFileContent('calendar', content) + ';');
