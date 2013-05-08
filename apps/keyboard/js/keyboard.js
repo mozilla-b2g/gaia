4932f97b4df0863480e579e0f4bab5dfd432a70b
Alive Kuo <alegnadise@gmail.com>
2013-05-08 10:56:27 +0800

    Bug 867912 - Silent notification channel when silent incoming calls

--- apps/keyboard/js/keyboard.js
+++ apps/keyboard/js/keyboard.js
@@ -287,7 +287,7 @@ function getKeyboardSettings() {
     'keyboard.wordsuggestion': true,
     'keyboard.vibration': false,
     'keyboard.clicksound': false,
-    'ring.enabled': true
+    'audio.volume.notification': 7
   };
 
   // Add the keyboard group settings to our query, too.
@@ -302,7 +302,7 @@ function getKeyboardSettings() {
     suggestionsEnabled = values['keyboard.wordsuggestion'];
     vibrationEnabled = values['keyboard.vibration'];
     clickEnabled = values['keyboard.clicksound'];
-    isSoundEnabled = values['ring.enabled'];
+    isSoundEnabled = !!values['audio.volume.notification'];
 
     handleKeyboardSound();
 
@@ -342,8 +342,8 @@ function initKeyboard() {
     vibrationEnabled = e.settingValue;
   });
 
-  navigator.mozSettings.addObserver('ring.enabled', function(e) {
-    isSoundEnabled = e.settingValue;
+  navigator.mozSettings.addObserver('audio.volume.notification', function(e) {
+    isSoundEnabled = !!e.settingValue;
     handleKeyboardSound();
   });
 
