5dfaa1b3316d55f47a5bbd02d9e2e74a862531f5
Hubert Figuière <hub@figuiere.net>
2013-05-10 17:38:13 -0400

    Bug 867829 - Part 2: have a dismiss callback for the system banner so we can know the user didn't want to send crash report. r=margaret

--- apps/system/js/system_banner.js
+++ apps/system/js/system_banner.js
@@ -3,9 +3,15 @@
 var SystemBanner = {
   banner: document.getElementById('system-banner'),
 
+  // clicked callback called. Bypass dismiss.
+  _clicked: false,
+  _clickCallback: null,
+
   // Shows a banner with a given message.
-  // Optionally shows a button with a given label/callback.
-  // buttonParams = { label: ..., callback: ... }
+  // Optionally shows a button with a given label/callback/dismiss.
+  // 'dismiss' is called when the banner is dismissed and button
+  // has not been clicked. It is optional.
+  // buttonParams = { label: ..., callback: ..., dismiss: ... }
   show: function sb_show(message, buttonParams) {
     var banner = this.banner;
     banner.firstElementChild.textContent = message;
@@ -14,7 +20,12 @@ var SystemBanner = {
     if (buttonParams) {
       banner.dataset.button = true;
       button.textContent = buttonParams.label;
-      button.addEventListener('click', buttonParams.callback);
+      var self = this;
+      self._clickCallback = function() {
+        self._clicked = true;
+        buttonParams.callback();
+      };
+      button.addEventListener('click', self._clickCallback);
     }
 
     banner.addEventListener('animationend', function animationend() {
@@ -22,8 +33,11 @@ var SystemBanner = {
       banner.classList.remove('visible');
 
       if (buttonParams) {
+        if (buttonParams.dismiss && !this._clicked) {
+          buttonParams.dismiss();
+        }
         banner.dataset.button = false;
-        button.removeEventListener('click', buttonParams.callback);
+        button.removeEventListener('click', this._clickCallback);
         button.classList.remove('visible');
       }
     });
