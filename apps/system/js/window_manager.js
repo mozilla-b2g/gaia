9eae4099c55802b6398c7dcb8949c10270c5e43b
Alive Kuo <alegnadise@gmail.com>
2013-05-13 14:45:43 +0800

    Bug 871374 - contentWindow check for OOP apps

--- apps/system/js/window_manager.js
+++ apps/system/js/window_manager.js
@@ -1467,7 +1467,8 @@ var WindowManager = (function() {
           // Instantly blur the frame in order to ensure hiding the keyboard
           var app = runningApps[displayedApp];
           if (app) {
-            if ('contentWindow' in app.iframe) {
+            if ('contentWindow' in app.iframe &&
+                app.iframe.contentWindow != null) {
               // Bug 845661 - Attention screen does not appears when
               // the url bar input is focused.
               // Calling app.iframe.blur() on an in-process window
