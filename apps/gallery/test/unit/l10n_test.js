a817b6f341cb309dbbce9b6f4cab6ec9de6c9ce9
Anthony Ricaud <anthony@ricaud.me>
2013-05-14 17:32:44 +0200

    Bug 862785 - no error message is shown when the update.xml url leads to a 404 error r=kaze

---
+++ apps/gallery/test/unit/l10n_test.js
@@ -0,0 +1,32 @@
+/**
+ * Tests for the shared l10n date code
+ * TODO: Shared code unit tests should not be in gallery
+ * Bug #841422 has been filed to move these tests
+ */
+require('/shared/js/l10n.js');
+
+suite('L10n', function() {
+
+  var _;
+
+  suiteSetup(function(done) {
+    _ = navigator.mozL10n.get;
+
+    navigator.mozL10n.ready(function () {
+      done();
+    });
+
+  });
+
+  suite('get', function() {
+    test('existing key', function() {
+      assert.strictEqual(_('cropimage'), 'Crop');
+    });
+
+    test('inexisting key', function() {
+      assert.strictEqual(_('bla'), '');
+    });
+
+  });
+
+});
