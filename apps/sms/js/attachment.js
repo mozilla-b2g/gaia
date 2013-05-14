c1250d945c421284122a9c0bf37a18bcbfe28787
Greg <greg@bocoup.com>
2013-05-14 17:50:41 -0400

    Bug 868227 - [MMS][User Story] Attachment (pre-send) options - r=julienw r=gnarf
    Closes gh-9822

--- apps/sms/js/attachment.js
+++ apps/sms/js/attachment.js
@@ -31,29 +31,57 @@ Attachment.prototype = {
   get type() {
     return Utils.typeFromMimeType(this.blob.type);
   },
+  handleLoad: function(objectURL, event) {
+    // Signal Gecko to release the reference to the Blob
+    URL.revokeObjectURL(objectURL);
+
+    // Bubble click events from inside the iframe
+    event.target.contentDocument.addEventListener('click',
+      event.target.click.bind(event.target));
+
+    // Bubble the contextmenu(longpress) as a click
+    event.target.contentDocument.addEventListener('contextmenu',
+      event.target.click.bind(event.target));
+  },
   render: function() {
-    var _ = navigator.mozL10n.get;
     var el = document.createElement('iframe');
+
     // The attachment's iFrame requires access to the parent document's context
     // so that URIs for Blobs created in the parent may resolve as expected.
     el.setAttribute('sandbox', 'allow-same-origin');
+    el.className = 'attachment';
+    var objectURL = window.URL.createObjectURL(this.blob);
+
+    // When rendering is complete
+    el.addEventListener('load', this.handleLoad.bind(this, objectURL));
+
+    var _ = navigator.mozL10n.get;
     var src = 'data:text/html,';
     // We want kilobytes so we divide by 1024, with one fractional digit
     var size = Math.floor(this.size / 102.4) / 10;
     var sizeString = _('attachmentSize', {n: size});
-    var objectURL = window.URL.createObjectURL(this.blob);
     src += Utils.Template('attachment-tmpl').interpolate({
       uri: objectURL,
       size: sizeString
     });
     el.src = src;
-    el.className = 'attachment';
-
-    // When rendering is complete, signal Gecko to release the reference to the
-    // Blob
-    el.addEventListener('load',
-      window.URL.revokeObjectURL.bind(window.URL, objectURL));
 
     return el;
+  },
+
+  view: function() {
+    var activity = new MozActivity({
+      name: 'open',
+      data: {
+        type: this.blob.type,
+        filename: this.name,
+        blob: this.blob
+      }
+    });
+    activity.onerror = function() {
+      var _ = navigator.mozL10n.get;
+      console.error('error with open activity', this.error.name);
+      alert(_('attachmentOpenError'));
+    };
   }
 };
