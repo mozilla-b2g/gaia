b42a6dd5f7f49764e00dec455e89083b6cb9379e
Greg <greg@bocoup.com>
2013-05-16 17:02:49 -0400

    first stab at tests for attachment.js

---
+++ apps/sms/test/unit/attachment_test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+requireApp('sms/js/attachment.js');
+
+suite('attachment_test.js', function() {
+
+  setup(function() {
+    loadBodyHTML('/index.html');
+    this.attachment = new Attachment(
+      new Blob(['This is an image message'],
+               { type: 'text/plain' },
+      'Test Attachment'));
+  });
+  teardown(function() {
+    
+  });
+
+  test('render', function() {
+    var el = this.attachment.render();
+    assert.ok(el.src, 'src set');
+  });
+
+  test('openOptionsMenu', function() {
+    assert.ok(!document.querySelector('#attachment-options'));
+    this.attachment.openOptionsMenu();
+    assert.ok(document.querySelector('#attachment-options'));
+  });
+
+  test('closeOptionsMenu', function() {
+    this.attachment.openOptionsMenu();
+    assert.ok(document.querySelector('#attachment-options'));
+    this.attachment.closeOptionsMenu();
+    assert.ok(!document.querySelector('#attachment-options'));
+  });
+
+  test('view', function() {
+    // TODO
+  });
+
+  test('remove', function() {
+    var el = this.attachment.render();
+    var parent = document.createElement('div');
+    // TODO
+    //parent.appendChild(el);
+    //assert.ok(this.attachment.el);
+    //this.attachment.remove();
+    //assert.ok(!this.attachment.el);
+  });
+
+  test('replace', function() {
+    // TODO 
+  });
+
+});
