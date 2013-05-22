f91dd7b1a72ce9de9cf04e144c4dff770a5bb2d8
Corey Frang <gnarf@gnarf.net>
2013-05-22 13:36:55 -0500

    adding tests to ensure localization

--- apps/sms/test/unit/attachment_menu_test.js
+++ apps/sms/test/unit/attachment_menu_test.js
@@ -24,19 +24,45 @@ suite('attachment_menu_test.js', function() {
     AttachmentMenu.init('attachment-options-menu');
 
     this.blob = new Blob(['This is an image message'],
-      {type: 'text/plain'});
+      {type: 'image/jpeg'});
     this.attachment = new Attachment(
       this.blob,
-      'Test Attachment');
+      'Test.jpg');
 
   });
 
-  test('open', function() {
-    assert.equal(document.querySelector('#attachment-options-menu').className,
-      'hide');
-    AttachmentMenu.open(this.attachment);
-    assert.equal(document.querySelector('#attachment-options-menu').className,
-      '');
+  suite('open', function() {
+    setup(function() {
+      document.querySelector('#attachment-options-menu').className = 'hide';
+      // clear out a bunch of fields to make sure open uses localization
+      AttachmentMenu.viewButton.textContent = '';
+      AttachmentMenu.replaceButton.textContent = '';
+      AttachmentMenu.removeButton.textContent = '';
+      AttachmentMenu.cancelButton.textContent = '';
+      AttachmentMenu.header.textContent = '';
+
+      AttachmentMenu.open(this.attachment);
+    });
+    test('removes hide class', function() {
+      assert.isFalse(AttachmentMenu.el.classList.contains('hide'));
+    });
+    test('sets header text', function() {
+      assert.equal(AttachmentMenu.header.textContent, this.attachment.name);
+    });
+    test('sets view text', function() {
+      assert.equal(AttachmentMenu.viewButton.textContent, 'view-attachment');
+    });
+    test('sets remove text', function() {
+      assert.equal(AttachmentMenu.removeButton.textContent,
+        'remove-attachment{"type":"attachment-type-image"}');
+    });
+    test('sets replace text', function() {
+      assert.equal(AttachmentMenu.replaceButton.textContent,
+        'replace-attachment{"type":"attachment-type-image"}');
+    });
+    test('sets cancel text', function() {
+      assert.equal(AttachmentMenu.cancelButton.textContent, 'cancel');
+    });
   });
 
   test('close', function() {
