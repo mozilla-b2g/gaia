b6935f59bea22501ec654b2ae1b7e2d636ef8abb
Mihai Cirlanaru <mihai@cirlanaru.com>
2013-05-26 23:54:51 +0200

    Bug 872825 - [Keyboard] Add keyboard layout group for Serbian. r=rudyl

--- apps/keyboard/js/layout.js
+++ apps/keyboard/js/layout.js
@@ -587,7 +587,7 @@ const Keyboards = {
   },
   'sr-Cyrl': {
     label: 'Serbian (Cyrillic)',
-    menuLabel: 'Cрпска ћирилица',
+    menuLabel: 'Српски (ћирилица)',
     alt: {
       // incomplete
     },
@@ -614,7 +614,7 @@ const Keyboards = {
   },
   'sr-Latn': {
     label: 'Serbian (Latin)',
-    menuLabel: 'srpski',
+    menuLabel: 'Srpski',
     alt: {
       c: 'čć',
       d: 'đ',
