d5a8ac4da65f56df1edca41bacbd7b200a0f5853
Dominic Kuo <dkuo@mozilla.com>
2013-05-29 14:16:35 -0700

    Bug 877313 - [music] Remove Math.floor() on the audio duration and refine formatTime()

--- apps/music/js/utils.js
+++ apps/music/js/utils.js
@@ -13,17 +13,18 @@ function formatTime(secs) {
   if (isNaN(secs))
     return;
 
+  secs = Math.floor(secs);
+
   var formatedTime;
-  var seconds = parseInt(secs % 60, 10);
-  var minutes = parseInt(secs / 60) % 60;
-  var hours = parseInt(secs / 3600) % 24;
+  var seconds = secs % 60;
+  var minutes = Math.floor(secs / 60) % 60;
+  var hours = Math.floor(secs / 3600);
 
   if (hours === 0) {
     formatedTime =
       (minutes < 10 ? '0' + minutes : minutes) + ':' +
       (seconds < 10 ? '0' + seconds : seconds);
   } else {
-
     formatedTime =
       (hours < 10 ? '0' + hours : hours) + ':' +
       (minutes < 10 ? '0' + minutes : minutes) + ':' +
