140148d15455b1ac16cdb28abbe8570bc64e77cd
Michal Budzynski <michal@virtualdesign.pl>
2013-05-29 23:34:12 +0200

    Bug 874043 - airplane mode allert when sending sms
    fixes
    
    cleaning
    
    cleaning
    
    lint fix
    
    test fix
    
    lint error fix

--- apps/sms/test/unit/settings_test.js
+++ apps/sms/test/unit/settings_test.js
@@ -3,13 +3,12 @@
 */
 'use strict';
 
-requireApp('sms/test/unit/mock_moz_settings.js');
+requireApp('sms/shared/test/unit/mocks/mock_navigator_moz_settings.js');
 requireApp('sms/js/settings.js');
 
 
 suite('Message App settings Unit-Test', function() {
   var nativeSettings = navigator.mozSettings;
-
   suite('Fetch mms messaage size limitation', function() {
     test('Query size limitation without settings', function(done) {
       Settings.getMmsSizeLimitation(function callback(size) {
@@ -19,7 +18,7 @@ suite('Message App settings Unit-Test', function() {
     });
 
     test('Query size limitation with settings exist(300KB)', function(done) {
-      navigator.mozSettings = MockMozSettings;
+      navigator.mozSettings = MockNavigatorSettings;
       Settings.getMmsSizeLimitation(function callback(size) {
         assert.equal(size, 300 * 1024);
         navigator.mozSettings = nativeSettings;
