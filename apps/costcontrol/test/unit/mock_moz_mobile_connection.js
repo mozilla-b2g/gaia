c4eaf0e512e4175f579d8240e47af1bc2bffad6d
lodr <salva@unoyunodiez.com>
2013-05-31 14:30:32 +0200

    Bug 877990 - Adding minimum delay between automatic requests to respect performance limits

--- apps/costcontrol/test/unit/mock_moz_mobile_connection.js
+++ apps/costcontrol/test/unit/mock_moz_mobile_connection.js
@@ -1,10 +1,27 @@
 'use strict';
 
 var MockMozMobileConnection = function(state) {
+  state = state || {};
+
   if (!state.iccInfo) {
     state.iccInfo = {
       iccid: 'TEST_ICCID'
     };
   }
+
+  if (!state.voice) {
+    state.voice = {
+    };
+  }
+
+  if (!state.data) {
+    state.data = {
+      network: {
+        shortName: 'TestNetName',
+        longName: 'Testing Network Name'
+      }
+    };
+  }
+
   return state;
 };
