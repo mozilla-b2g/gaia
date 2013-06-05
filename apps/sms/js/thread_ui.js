5441f53c04cf570efc722f1e9df2f91c4d5204a2
Corey Frang <gnarf@gnarf.net>
2013-06-05 14:53:19 -0400

    Revert "Merge pull request #10018 from huchengtw-moz/sms/Bug_875564_after_attaching_image_sometimes_send_button_is_still_disabled"
    Bug 875564 - [MMS] after attaching image, sometimes send button is still disabled. r=steveck-chung
    
    This reverts commit e19c6980de1089af7a120be4b49d49a6bb5d28e4, reversing
    changes made to b3dd91ed197f96c698f03e1f67de3a7602d46880.

--- apps/sms/js/thread_ui.js
+++ apps/sms/js/thread_ui.js
@@ -163,9 +163,9 @@ var ThreadUI = global.ThreadUI = {
       'click', this.activateContact.bind(this)
     );
 
-    // When 'blur' we have to remove 'edit-mode' in the recipient
-    this.toField.addEventListener(
-      'blur', this.recipientWrappingHandler.bind(this), true
+    // When 'focus' we have to remove 'edit-mode' in the recipient
+    this.input.addEventListener(
+      'focus', this.messageComposerFocusHandler.bind(this)
     );
 
     this.container.addEventListener(
@@ -287,7 +287,7 @@ var ThreadUI = global.ThreadUI = {
     this.enableSend();
   },
 
-  recipientWrappingHandler: function thui_recipientWrappingHandler(event) {
+  messageComposerFocusHandler: function thui_messageInputHandler(event) {
     var node = this.recipientsList.lastChild;
     var typed;
 
@@ -1505,8 +1505,7 @@ var ThreadUI = global.ThreadUI = {
       // TODO Modify in Bug 861227 in order to create a standalone element
       var contactsUl = document.createElement('ul');
       contactsUl.classList.add('contactList');
-      // Using mousedown event can execute before blur event.
-      contactsUl.addEventListener('mousedown', function uiHandler(event) {
+      contactsUl.addEventListener('click', function contactsUlHandler(event) {
         // Since the "dataset" DOMStringMap property is essentially
         // just an object of properties that exactly match the properties
         // used for recipients, push the whole dataset object into
@@ -1516,7 +1515,7 @@ var ThreadUI = global.ThreadUI = {
         ).focus();
 
         // Clean up the event listener
-        contactsUl.removeEventListener('mousedown', contactsUlHandler);
+        contactsUl.removeEventListener('click', contactsUlHandler);
 
         event.stopPropagation();
         event.preventDefault();
