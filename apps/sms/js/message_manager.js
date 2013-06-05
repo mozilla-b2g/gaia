0930b6d924d98bde66601005813cf54102b50fa0
Gabriele Svelto <gabriele.svelto@gmail.com>
2013-06-05 21:58:23 +0200

    Bug 879291 - [SMS] Properly exit edit mode before transitioning to another panel - r=gnarf
    Closes gh-10208
    (cherry picked from commit 48b2ec2b6a2f40730485159f75bca60ea5423c9d)

--- apps/sms/js/message_manager.js
+++ apps/sms/js/message_manager.js
@@ -204,11 +204,15 @@ var MessageManager = {
     // Group Participants should never persist any hash changes
     ThreadUI.groupView.reset();
 
+    // Leave the edit mode before transitioning to another panel. This is safe
+    // to do even if we're not in edit mode as it's essentially a no-op then.
+    ThreadUI.cancelEdit();
+    ThreadListUI.cancelEdit();
+
     switch (window.location.hash) {
       case '#new':
 
         ThreadUI.cleanFields(true);
-        mainWrapper.classList.remove('edit');
         threadMessages.classList.add('new');
 
         MessageManager.activity.recipients = null;
@@ -274,9 +278,7 @@ var MessageManager = {
           filter = new MozSmsFilter();
           filter.threadId = threadId;
 
-          if (mainWrapper.classList.contains('edit')) {
-            mainWrapper.classList.remove('edit');
-          } else if (threadMessages.classList.contains('new')) {
+          if (threadMessages.classList.contains('new')) {
             // After a message is sent...
             //
             threadMessages.classList.remove('new');
