b8d656d6b9a6ab30ceb2adb1bf47240d16a28901
Dale Harvey <dale@arandomurl.com>
2013-06-06 01:30:38 +0100

    Bug 874796 - Add setting to disable / enable cell broadcast alerts.
    The setting does not currently effect channel 50 cell broadcast
    messages, which will be displayed regardless of this setting.

--- apps/system/js/cell_broadcast_system.js
+++ apps/system/js/cell_broadcast_system.js
@@ -2,12 +2,28 @@
 
 var CellBroadcastSystem = {
 
+  _settingsDisabled: null,
+  _settingsKey: 'ril.cellbroadcast.disabled',
   _sound: 'style/notifications/ringtones/notifier_exclamation.ogg',
 
   init: function cbs_init() {
+    var self = this;
     if (navigator && navigator.mozCellBroadcast) {
       navigator.mozCellBroadcast.onreceived = this.show.bind(this);
     }
+
+    var settings = window.navigator.mozSettings;
+    var req = settings.createLock().get(this._settingsKey);
+    req.onsuccess = function() {
+      self._settingsDisabled = req.result[self._settingsKey];
+    };
+
+    settings.addObserver(this._settingsKey,
+                         this.settingsChangedHandler.bind(this));
+  },
+
+  settingsChangedHandler: function cbs_settingsChangedHandler(event) {
+    this._settingsDisabled = event.settingValue;
   },
 
   show: function cbs_show(event) {
@@ -18,6 +34,10 @@ var CellBroadcastSystem = {
         conn.voice.network.mcc === MobileOperator.BRAZIL_MCC &&
         msg.messageId === MobileOperator.BRAZIL_CELLBROADCAST_CHANNEL) {
       LockScreen.setCellbroadcastLabel(msg.body);
+      return;
+    }
+
+    if (this._settingsDisabled) {
       return;
     }
 
