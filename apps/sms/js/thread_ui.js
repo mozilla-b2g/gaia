15a244b62e1757283a1775077da6689c81d31017
Rick Waldron <waldron.rick@gmail.com>
2013-06-12 15:05:22 -0400

    Bug 882369 - [SMS][MMS] Regression. contactList => contact-list for search results and group view r=borjasalguero
    https://bugzilla.mozilla.org/show_bug.cgi?id=882369
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>
    (cherry picked from commit 3dee169168113a8b166ef2700ad0671d9022abfb)

--- apps/sms/js/thread_ui.js
+++ apps/sms/js/thread_ui.js
@@ -1607,7 +1607,7 @@ var ThreadUI = global.ThreadUI = {
       }
       // TODO Modify in Bug 861227 in order to create a standalone element
       var ul = document.createElement('ul');
-      ul.classList.add('contactList');
+      ul.classList.add('contact-list');
       ul.addEventListener('click', function ulHandler(event) {
         event.stopPropagation();
         event.preventDefault();
