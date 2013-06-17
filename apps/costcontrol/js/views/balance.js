74d772358979c7aaab543df0f4328b3f7d89774e
Salvador de la Puente González <salva@unoyunodiez.com>
2013-06-17 06:45:05 -0700

    Merge pull request #10434 from lodr/bug-882656-topup
    Bug 882656 - Restoring topup functionality
    r=arcturus(cherry picked from commit c2a74edb8871e126bf0edd6f911262fba717ad9d)

--- apps/costcontrol/js/views/balance.js
+++ apps/costcontrol/js/views/balance.js
@@ -17,6 +17,10 @@ var BalanceTab = (function() {
   var topUpLayoutController;
   var costcontrol, initialized;
 
+  function showTopUpWithCode(evt) {
+    topUpWithCode();
+  }
+
   function setupTab() {
     if (initialized) {
       return;
@@ -56,7 +60,7 @@ var BalanceTab = (function() {
 
       // Configure top up
       topUpUSSD.addEventListener('click', topUpWithUSSD);
-      topUp.addEventListener('click', topUpWithCode);
+      topUp.addEventListener('click', showTopUpWithCode);
       topUpCodeInput.addEventListener('input', toogleSend);
       sendCode.addEventListener('click', requestTopUp);
       ConfigManager.observe('waitingForTopUp', onConfirmation, true);
@@ -87,7 +91,7 @@ var BalanceTab = (function() {
     ConfigManager.removeObserver('errors', onBalanceTimeout);
 
     topUpUSSD.removeEventListener('click', topUpWithUSSD);
-    topUp.removeEventListener('click', topUpWithCode);
+    topUp.removeEventListener('click', showTopUpWithCode);
     topUpCodeInput.removeEventListener('input', toogleSend);
     sendCode.removeEventListener('click', requestTopUp);
     ConfigManager.removeObserver('waitingForTopUp', onConfirmation);
