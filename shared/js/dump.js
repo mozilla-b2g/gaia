432aa7bdea60b2bd233ec36359b3738d9cc0669e
Fernando Rodriguez Sela <frsela@tid.es>
2013-06-18 08:42:11 +0200

    console.log method also includes appName so only dump method will use it

--- shared/js/dump.js
+++ shared/js/dump.js
@@ -7,15 +7,16 @@
 
   function dump_off(msg, optionalObject) {};
   function dump_on(msg, optionalObject) {
-    var appName = document.location.hostname.replace(/\..*$/, '');
-    var output = '[' + appName + '] ' + msg;
+    var output = msg;
     if (optionalObject) {
       output += JSON.stringify(optionalObject);
     }
     if (dump) {
-      return dump(output + '\n');
+      var appName = document.location.hostname.replace(/\..*$/, '');
+      dump('[' + appName + '] ' + output + '\n');
+    } else {
+      console.log(output);
     }
-    console.log(output);
   }
 
   window.DUMP = dump_off;   // no traces by default
@@ -24,7 +25,8 @@
   var settings = window.navigator.mozSettings;
   var reqGaiaDebug = settings.createLock().get('debug.gaia.enabled');
   reqGaiaDebug.onsuccess = function gaiaDebug() {
-    window.DUMP = reqGaiaDebug.result['debug.gaia.enabled'] ? dump_on : dump_off;
+    window.DUMP =
+      reqGaiaDebug.result['debug.gaia.enabled'] ? dump_on : dump_off;
   };
   settings.addObserver('debug.gaia.enabled', function(event) {
     window.DUMP = event.settingValue ? dump_on : dump_off;
