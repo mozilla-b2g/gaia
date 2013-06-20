591a2e5fe47b81e431fe222dba699667a636cdb5
Fernando Rodríguez Sela <frsela@tid.es>
2013-06-20 10:45:53 +0200

    STK_CMD_GET_INPUT moved to system

--- apps/settings/js/icc.js
+++ apps/settings/js/icc.js
@@ -27,7 +27,6 @@
   var iccLastCommandProcessed = false;
   var stkOpenAppName = null;
   var stkLastSelectedTest = null;
-  var inputTimeout = 40000;
   var goBackTimer = {
     timer: null,
     timeout: 1000
@@ -79,16 +78,6 @@
 
     // Load STK apps
     updateMenu();
-
-    // XXX https://bugzilla.mozilla.org/show_bug.cgi?id=844727
-    // We should use Settings.settingsCache first
-    var settings = Settings.mozSettings;
-    var lock = settings.createLock();
-    // Update inputTimeout with settings parameter
-    var reqInputTimeout = lock.get('icc.inputTextTimeout');
-    reqInputTimeout.onsuccess = function icc_getInputTimeout() {
-      inputTimeout = reqInputTimeout.result['icc.inputTextTimeout'];
-    };
   }
 
   function stkResTerminate() {
@@ -145,16 +134,6 @@
   }
 
   /**
-   * Send Terminal Response : UICC SESSION TERMINATED BY USER
-   */
-  function sendSessionEndTROnFocusLose() {
-    if (document.mozHidden)
-      responseSTKCommand({
-        resultCode: icc.STK_RESULT_UICC_SESSION_TERM_BY_USER
-      });
-  }
-
-  /**
    * Response ICC Command
    */
   function responseSTKCommand(response, force) {
@@ -190,12 +169,6 @@
       case icc.STK_CMD_SELECT_ITEM:
         updateSelection(command);
         openSTKApplication();
-        iccLastCommandProcessed = true;
-        break;
-
-      case icc.STK_CMD_GET_INKEY:
-      case icc.STK_CMD_GET_INPUT:
-        updateInput(command);
         iccLastCommandProcessed = true;
         break;
 
@@ -339,180 +312,6 @@
       itemIdentifier: identifier
     });
     stkLastSelectedTest = event.target.textContent;
-  }
-
-  function calculateDurationInMS(timeUnit, timeInterval) {
-    var timeout = timeInterval;
-    switch (timeUnit) {
-      case icc.STK_TIME_UNIT_MINUTE:
-        timeout *= 3600000;
-        break;
-      case icc.STK_TIME_UNIT_SECOND:
-        timeout *= 1000;
-        break;
-      case icc.STK_TIME_UNIT_TENTH_SECOND:
-        timeout *= 100;
-        break;
-    }
-    return timeout;
-  }
-
-  /**
-   * Show an INPUT box requiring data
-   * Command options like:
-   * { 'options': {
-   *   'text':'Caption String','minLength':3,'maxLength':15,'isAlphabet':true}}
-   */
-  function updateInput(command) {
-    var options = command.options;
-
-    DUMP('Showing STK input box');
-    clearList();
-    showTitle(stkLastSelectedTest);
-
-    DUMP('STK Input title: ' + options.text);
-
-    document.addEventListener('mozvisibilitychange',
-        sendSessionEndTROnFocusLose, true);
-
-    // AutoClose
-    var timeoutInUse = options.duration;
-    var inputTimeOutID = setTimeout(function() {
-      DUMP('No response from user (Timeout)');
-      responseSTKCommand({
-        resultCode: icc.STK_RESULT_NO_RESPONSE_FROM_USER
-      });
-    }, timeoutInUse ? calculateDurationInMS(options.duration) : inputTimeout);
-
-    // Common updateInput methodd
-    function stopSTKInputTimer() {
-      if (inputTimeOutID) {
-        clearTimeout(inputTimeOutID);
-        inputTimeOutID = null;
-      }
-    }
-    function inputSTKResponse(value) {
-      stopSTKInputTimer();
-      responseSTKCommand({
-        resultCode: icc.STK_RESULT_OK,
-        input: value
-      });
-    }
-
-    // Showing input screen
-    var li = document.createElement('li');
-    var p = document.createElement('p');
-    p.id = 'stk-item-title';
-    p.classList.add('multiline_title');
-    p.textContent = options.text;
-    li.appendChild(p);
-
-    if (!options.isYesNoRequired && !options.isYesNoRequested) {
-      var input = document.createElement('input');
-      input.id = 'stk-item-input';
-      input.maxLength = options.maxLength;
-      input.placeholder = options.text;
-      if (options.isAlphabet) {
-        input.type = 'text';
-      } else {
-        input.type = 'tel';
-      }
-      if (options.defaultText) {
-        input.value = options.defaultText;
-      }
-      if (options.hideInput) {
-        input.type = 'password';
-      }
-      if (options.hidden) {
-        input.type = 'hidden';
-      }
-      li.appendChild(input);
-      iccStkList.appendChild(li);
-
-      li = document.createElement('li');
-      var label = document.createElement('label');
-      var button = document.createElement('button');
-      button.id = 'stk-item-ok';
-      button.textContent = _('ok');
-      button.disabled = !checkInputLengthValid(input.value.length,
-                                                options.minLength,
-                                                options.maxLength);
-      button.onclick = function(event) {
-        inputSTKResponse(document.getElementById('stk-item-input').value);
-      };
-
-      input.onkeyup = function(event) {
-        stopSTKInputTimer();
-        if (input.type === 'tel') {
-          // Removing unauthorized characters
-          input.value = input.value.replace(/[()-]/g, '');
-        }
-        button.disabled = !checkInputLengthValid(input.value.length,
-                                                options.minLength,
-                                                options.maxLength);
-      };
-
-      label.appendChild(button);
-      li.appendChild(label);
-      iccStkList.appendChild(li);
-      input.focus();
-    } else {
-      // Include default title
-      iccStkList.appendChild(li);
-
-      li = document.createElement('li');
-      var label = document.createElement('label');
-      var buttonYes = document.createElement('button');
-      buttonYes.id = 'stk-item-yes';
-      buttonYes.textContent = _('yes');
-      buttonYes.onclick = function(event) {
-        inputSTKResponse(1);
-      };
-      label.appendChild(buttonYes);
-      li.appendChild(label);
-      iccStkList.appendChild(li);
-
-      li = document.createElement('li');
-      var label = document.createElement('label');
-      var buttonNo = document.createElement('button');
-      buttonNo.id = 'stk-item-no';
-      buttonNo.textContent = _('no');
-      buttonNo.onclick = function(event) {
-        inputSTKResponse(0);
-      };
-      label.appendChild(buttonNo);
-      li.appendChild(label);
-      iccStkList.appendChild(li);
-    }
-
-    // Help
-    if (options.isHelpAvailable) {
-      li = document.createElement('li');
-      label = document.createElement('label');
-      var buttonHelp = document.createElement('button');
-      buttonHelp.id = 'stk-item-help';
-      buttonHelp.textContent = _('operatorServices-help');
-      buttonHelp.dataset.l10nId = 'operatorServices-help';
-      buttonHelp.onclick = function(event) {
-        responseSTKCommand({
-          resultCode: icc.STK_RESULT_HELP_INFO_REQUIRED
-        });
-      };
-      label.appendChild(buttonHelp);
-      li.appendChild(label);
-      iccStkList.appendChild(li);
-    }
-  }
-
-  /**
-   * Check if the length of the input is valid.
-   *
-   * @param {Integer} inputLen    The length of the input.
-   * @param {Integer} minLen      Minimum length required of the input.
-   * @param {Integer} maxLen      Maximum length required of the input.
-   */
-  function checkInputLengthValid(inputLen, minLen, maxLen) {
-    return (inputLen >= minLen) && (inputLen <= maxLen);
   }
 
   /**
