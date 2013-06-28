0b484d106f5bf6bcd353cc1f63fd38d6ecc74f9d
Mark Shiao <mshiao@mozilla.com>
2013-06-28 17:26:07 +0800

    Bug 881140 - hdpi/xhdpi resolution assets and layout for Contacts app

--- apps/communications/contacts/js/contacts.js
+++ apps/communications/contacts/js/contacts.js
@@ -5,6 +5,9 @@ var TAG_OPTIONS;
 var COMMS_APP_ORIGIN = document.location.protocol + '//' +
   document.location.host;
 
+// Scale ratio for different devices
+var SCALE_RATIO = window.innerWidth / 320;
+
 var Contacts = (function() {
   var navigation = new navigationStack('view-contacts-list');
 
@@ -752,7 +755,6 @@ var Contacts = (function() {
     var lazyLoadFiles = [
       '/contacts/js/utilities/templates.js',
       '/contacts/js/contacts_shortcuts.js',
-      '/contacts/js/utilities/responsive.js',
       '/contacts/js/confirm_dialog.js',
       '/contacts/js/contacts_settings.js',
       '/contacts/js/contacts_details.js',
