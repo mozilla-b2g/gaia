f053424f88e89bdab874d074482d0eb0c2be9d39
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2013-06-30 19:45:43 +0800

    Bug 879680 - (mmi-result-cf) [MMI] Use MMIResult for Call Forwarding related functionality. r=ferjm (cherry picked from commit 7f3e1fa0204afc369e91fe3f619388781a2279fb)

--- apps/communications/dialer/js/mmi.js
+++ apps/communications/dialer/js/mmi.js
@@ -108,6 +108,7 @@ var MmiManager = {
     }).bind(this);
 
     var mmiResult = evt.target.result;
+    var message = {};
 
     // We always expect an MMIResult object even for USSD requests.
     if (!mmiResult) {
@@ -120,9 +121,7 @@ var MmiManager = {
       return;
     }
 
-    var message = {
-      type: 'mmi-success'
-    };
+    message.type = 'mmi-success';
 
     if (mmiResult.serviceCode) {
       message.title = this._(mmiResult.serviceCode);
@@ -159,12 +158,18 @@ var MmiManager = {
         }
         break;
       case 'scCallForwarding':
-        // Call forwarding requests via MMI codes might return an array of
-        // nsIDOMMozMobileCFInfo objects. In that case we serialize that array
-        // into a single string that can be shown on the screen.
-
-        // TODO: Bug 884343. Use MMIResult for Call Forwarding related
-        //       functionality.
+        if (mmiResult.statusMessage) {
+          message.result = this._(mmiResult.statusMessage);
+          // Call forwarding requests via MMI codes might return an array of
+          // nsIDOMMozMobileCFInfo objects. In that case we serialize that array
+          // into a single string that can be shown on the screen.
+          if (mmiResult.additionalInformation) {
+            message.result = processCf(mmiResult.additionalInformation);
+          }
+        } else {
+          message.type = 'mmi-error';
+          message.error = this._('GenericFailure');
+        }
         break;
       case 'scCallBarring':
       case 'scCallWaiting':
