164998075e869b6cebe22a0f07b8847f4d1edd76
John Hu <johu@mozilla.com>
2013-07-01 17:44:41 +0800

    split files to multiple single files

--- apps/communications/dialer/js/voiceclip_player.js
+++ apps/communications/dialer/js/voiceclip_player.js
@@ -5,78 +5,26 @@ var VoiceClipPlayer = {
 
   init: function vc_init() {
     this.player = document.getElementById('voiceclip');
-    this.player.src = 'style/audio/one-to-ten.ogg';
-    // play first to force loading the voice clip
-    this.player.play();
-    this.player.pause();
   },
 
   AUDIO_MAP: {
-    d0: {
-      start: 9.52,
-      duration: 0.53
-    },
-    d1: {
-      start: 0.45,
-      duration: 0.5
-    },
-    d2: {
-      start: 1.43,
-      duration: 0.5
-    },
-    d3: {
-      start: 2.65,
-      duration: 0.5
-    },
-    d4: {
-      start: 3.55,
-      duration: 0.5
-    },
-    d5: {
-      start: 4.9,
-      duration: 0.6
-    },
-    d6: {
-      start: 5.9,
-      duration: 0.6
-    },
-    d7: {
-      start: 6.7,
-      duration: 0.55
-    },
-    d8: {
-      start: 7.75,
-      duration: 0.5
-    },
-    d9: {
-      start: 8.77,
-      duration: 0.53
-    },
-    d10: {
-      start: 9.52,
-      duration: 0.53
-    },
-    thank: {
-      start: 10.73,
-      duration: 1.55
-    }
+    'd0': '10.ogg',
+    'd1': '1.ogg',
+    'd2': '2.ogg',
+    'd3': '3.ogg',
+    'd4': '4.ogg',
+    'd5': '5.ogg',
+    'd6': '6.ogg',
+    'd7': '7.ogg',
+    'd8': '8.ogg',
+    'd9': '9.ogg',
+    'd*': 'thanks.ogg',
+    'd#': 'sit.ogg'
   },
 
   start: function(partialIndex, rate) {
-      var duration, partial,
-        self = this;
-      if (rate == null) {
-        rate = 1.0;
-      }
-      partial = this.AUDIO_MAP['d' + partialIndex];
-      // able to interrupt while playing
-      this.player.pause();
-      // play
-      this.player.currentTime = partial.start;
-      this.player.play();
-      duration = partial.duration / rate * 1000;
-      return setTimeout(function() {
-        return self.player.pause();
-      }, duration);
-    }
+    this.player.pause();
+    this.player.src = 'style/audio/' + this.AUDIO_MAP['d' + partialIndex];
+    this.player.play();
+  }
 };
