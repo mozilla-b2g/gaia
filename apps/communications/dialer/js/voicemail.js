5956004a08d05fc43a36abacc83321005da33660
Anthony Ricaud <anthony@ricaud.me>
2013-07-04 11:22:30 +0200

    Bug 889245 - [Call log] When user make a voicemail call, call log shows only number
    Implement some caching into Voicemail.check now that we use it for every number in the call log.

--- apps/communications/dialer/js/voicemail.js
+++ apps/communications/dialer/js/voicemail.js
@@ -1,33 +1,66 @@
 'use strict';
 
 var Voicemail = {
-  check: function vm_check(number, callback) {
-    // check the voicemail number if the number is in the sim card
-    var voicemail = navigator.mozVoicemail;
-    if (voicemail && voicemail.number) {
-      if (voicemail.number == number) {
-        callback(true);
-        return;
+  _simNumber: undefined,
+
+  _reset: function vm_reset() {
+    this._simNumber = undefined;
+    this._settingsNumber = undefined;
+  },
+
+  // Returns the voicemail number from the SIM card if present
+  get simNumber() {
+    if (this._simNumber === undefined) {
+      if (!navigator.mozVoicemail) {
+        this._simNumber = null;
       }
+      this._simNumber = navigator.mozVoicemail.number;
     }
 
-    // check the voicemail number with  with the mozSetting value
-    // based on /shared/resources/apn.json
+    return this._simNumber;
+  },
+
+  _settingsNumber: undefined,
+
+  // Returns the voicemail number with the mozSetting value
+  // based on /shared/resources/apn.json
+  _getSettingsNumber: function vm_getSettingsNumber(callback) {
+    if (this._settingsNumber !== undefined) {
+      setTimeout(callback, 0, this._settingsNumber);
+      return;
+    }
+
     var settings = navigator.mozSettings;
+    var self = this;
+    settings.addObserver('ril.iccInfo.mbdn', function(event) {
+      self._settingsNumber = event.settingValue;
+    });
+
     var req = settings.createLock().get('ril.iccInfo.mbdn');
-
     req.onsuccess = function getVoicemailNumber() {
-      var isVoicemailNumber = false;
-      var voicemailNumber = req.result['ril.iccInfo.mbdn'];
-
-      if (voicemailNumber === number) {
-        isVoicemailNumber = true;
-      }
-      callback(isVoicemailNumber);
+      self._settingsNumber = req.result['ril.iccInfo.mbdn'];
+      setTimeout(callback, 0, self._settingsNumber);
     };
 
     req.onerror = function getVoicemailNumberError() {
-      callback(false);
+      self._settingsNumber = null;
+      setTimeout(callback, 0, self._settingsNumber);
     };
+  },
+
+  check: function vm_check(number, callback) {
+    // check the voicemail number if the number is in the sim card
+    if (this.simNumber == number) {
+      callback(true);
+      return;
+    }
+
+    this._getSettingsNumber(function(settingsNumber) {
+      var isVoicemailNumber = false;
+      if (settingsNumber == number) {
+        isVoicemailNumber = true;
+      }
+      callback(isVoicemailNumber);
+    });
   }
 };
