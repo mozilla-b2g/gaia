fe889efc8e2783eaa5ca744e5fc854b9b80c8b3f
Rick Waldron <waldron.rick@gmail.com>
2013-07-12 14:00:41 -0400

    Bug 887712 - [SMS/MMS] Make editable cursor element "zero width" until any user interaction r=gnarf
    https://bugzilla.mozilla.org/show_bug.cgi?id=887712
    
    Includes: https://github.com/gnarf/gaia/commit/4880acb5ecf10b274dd8f6cf5e75b7d9ab5f8ab3
    
    - Empty, out of focus placeholders contract to zero offset width
    - Empty, in focus placeholders expand to rem-based width (margin: r & l, padding: r & l)
    - When all recipients are deleted, don't leave empty editable in the to-field.
    - When the editable placeholder is contracted to a zero offset width, it's possible to accidentally tap a recipient when the intention is to tap the to-field area around the recipient list, which will correctly prompt the user to remove the recipient. Since there is no way to unambiguously detect the user's intention, always handle "Remove" and "Cancel" in an intuitive way.
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- apps/sms/test/unit/mock_dialog.js
+++ apps/sms/test/unit/mock_dialog.js
@@ -3,7 +3,17 @@
 function MockDialog(params) {
   MockDialog.calls.push(params);
   MockDialog.instances.push(this);
+
+  Object.keys(params.options).forEach(function(option) {
+    MockDialog.triggers[option] = function() {
+      MockDialog.triggers[option].called = true;
+      params.options[option].method();
+    };
+  });
+
 }
+
+MockDialog.triggers = {};
 
 MockDialog.prototype = {
   show: function() {
@@ -19,4 +29,8 @@ MockDialog.mSetup = function() {
 MockDialog.mTeardown = function() {
   delete MockDialog.calls;
   delete MockDialog.instances;
+
+  Object.keys(MockDialog.triggers).forEach(function(trigger) {
+    MockDialog.triggers[trigger].called = false;
+  });
 };
