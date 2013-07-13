477cb7cf3e903cb2edf019186351c6226e8ba870
Kevin Grandon <kevingrandon@yahoo.com>
2013-07-12 17:21:29 -0700

    Bug 893188 - Update system message manager

--- tools/extensions/activities/components/SystemMessageManager.js
+++ tools/extensions/activities/components/SystemMessageManager.js
@@ -27,9 +27,15 @@ function debug(aMsg) {
 // Implementation of the DOM API for system messages
 
 function SystemMessageManager() {
-  // Message handlers for this page.
-  // We can have only one handler per message type.
-  this._handlers = {};
+  // If we have a system message handler registered for messages of type
+  // |type|, this._dispatchers[type] equals {handler, messages, isHandling},
+  // where
+  //  - |handler| is the message handler that the page registered,
+  //  - |messages| is a list of messages which we've received while
+  //    dispatching messages to the handler, but haven't yet sent, and
+  //  - |isHandling| indicates whether we're currently dispatching messages
+  //    to this handler.
+  this._dispatchers = {};
 
   // Pending messages for this page, keyed by message type.
   this._pendings = {};
@@ -52,7 +58,21 @@ function SystemMessageManager() {
 SystemMessageManager.prototype = {
   __proto__: DOMRequestIpcHelper.prototype,
 
-  _dispatchMessage: function sysMessMgr_dispatchMessage(aType, aHandler, aMessage) {
+  _dispatchMessage: function sysMessMgr_dispatchMessage(aType, aDispatcher, aMessage) {
+    if (aDispatcher.isHandling) {
+      // Queue up the incomming message if we're currently dispatching a
+      // message; we'll send the message once we finish with the current one.
+      //
+      // _dispatchMethod is reentrant because a page can spin up a nested
+      // event loop from within a system message handler (e.g. via alert()),
+      // and we can then try to send the page another message while it's
+      // inside this nested event loop.
+      aDispatcher.messages.push(aMessage);
+      return;
+    }
+
+    aDispatcher.isHandling = true;
+
     // We get a json blob, but in some cases we want another kind of object
     // to be dispatched.
     // To do so, we check if we have a with a contract ID of
@@ -72,12 +92,22 @@ SystemMessageManager.prototype = {
       }
     }
 
-    aHandler.handleMessage(wrapped ? aMessage
-                                   : ObjectWrapper.wrap(aMessage, this._window));
+    aDispatcher.handler
+      .handleMessage(wrapped ? aMessage
+                             : ObjectWrapper.wrap(aMessage, this._window));
 
-    Services.obs.notifyObservers(/* aSubject */ null,
-                                 "SystemMessageManager:HandleMessageDone",
-                                 /* aData */ null);
+    // We need to notify the parent one of the system messages has been handled,
+    // so the parent can release the CPU wake lock it took on our behalf.
+    cpmm.sendAsyncMessage("SystemMessageManager:HandleMessagesDone",
+                          { type: aType,
+                            manifest: this._manifest,
+                            uri: this._uri,
+                            handledCount: 1 });
+
+    aDispatcher.isHandling = false;
+    if (aDispatcher.messages.length > 0) {
+      this._dispatchMessage(aType, aDispatcher, aDispatcher.messages.shift());
+    }
   },
 
   mozSetMessageHandler: function sysMessMgr_setMessageHandler(aType, aHandler) {
@@ -94,16 +124,16 @@ SystemMessageManager.prototype = {
       return;
     }
 
-    let handlers = this._handlers;
+    let dispatchers = this._dispatchers;
     if (!aHandler) {
-      // Setting the handler to null means we don't want to receive messages
-      // of this type anymore.
-      delete handlers[aType];
+      // Setting the dispatcher to null means we don't want to handle messages
+      // for this type anymore.
+      delete dispatchers[aType];
       return;
     }
 
     // Last registered handler wins.
-    handlers[aType] = aHandler;
+    dispatchers[aType] = { handler: aHandler, messages: [], isHandling: false };
 
     // Ask for the list of currently pending messages.
     cpmm.sendAsyncMessage("SystemMessageManager:GetPendingMessages",
@@ -122,7 +152,7 @@ SystemMessageManager.prototype = {
     }
 
     // If we have a handler for this type, we can't have any pending message.
-    if (aType in this._handlers) {
+    if (aType in this._dispatchers) {
       return false;
     }
 
@@ -133,7 +163,7 @@ SystemMessageManager.prototype = {
   },
 
   uninit: function sysMessMgr_uninit()  {
-    this._handlers = null;
+    this._dispatchers = null;
     this._pendings = null;
 
     if (this._isParentProcess) {
@@ -148,50 +178,77 @@ SystemMessageManager.prototype = {
 
     cpmm.sendAsyncMessage("SystemMessageManager:Unregister",
                           { manifest: this._manifest,
-                            innerWindowID: this.innerWindowID
-                          });
+                            uri: this._uri,
+                            innerWindowID: this.innerWindowID });
   },
 
+  // Possible messages:
+  //
+  //   - SystemMessageManager:Message
+  //     This one will only be received when the child process is alive when
+  //     the message is initially sent.
+  //
+  //   - SystemMessageManager:GetPendingMessages:Return
+  //     This one will be received when the starting child process wants to
+  //     retrieve the pending system messages from the parent (i.e. after
+  //     sending SystemMessageManager:GetPendingMessages).
   receiveMessage: function sysMessMgr_receiveMessage(aMessage) {
-    debug("receiveMessage " + aMessage.name + " for [" + aMessage.data.type + "] " +
-          "with manifest = " + aMessage.data.manifest + " (" + this._manifest + ") " +
-          "and uri = " + aMessage.data.uri + " (" + this._uri + ")");
-
     let msg = aMessage.data;
-    if (msg.manifest != this._manifest || msg.uri != this._uri) {
+    debug("receiveMessage " + aMessage.name + " for [" + msg.type + "] " +
+          "with manifest = " + msg.manifest + " and uri = " + msg.uri);
+
+    // Multiple windows can share the same target (process), the content
+    // window needs to check if the manifest/page URL is matched. Only
+    // *one* window should handle the system message.
+    if (msg.manifest !== this._manifest || msg.uri !== this._uri) {
+      debug("This page shouldn't handle the messages because its " +
+            "manifest = " + this._manifest + " and uri = " + this._uri);
       return;
     }
 
     if (aMessage.name == "SystemMessageManager:Message") {
       // Send an acknowledgement to parent to clean up the pending message,
       // so a re-launched app won't handle it again, which is redundant.
-      cpmm.sendAsyncMessage(
-        "SystemMessageManager:Message:Return:OK",
-        { type: msg.type,
-          manifest: msg.manifest,
-          uri: msg.uri,
-          msgID: msg.msgID });
-    }
-
-    // Bail out if we have no handlers registered for this type.
-    if (!(msg.type in this._handlers)) {
-      debug("No handler for this type");
-      return;
+      cpmm.sendAsyncMessage("SystemMessageManager:Message:Return:OK",
+                            { type: msg.type,
+                              manifest: this._manifest,
+                              uri: this._uri,
+                              msgID: msg.msgID });
     }
 
     let messages = (aMessage.name == "SystemMessageManager:Message")
                    ? [msg.msg]
                    : msg.msgQueue;
 
-    messages.forEach(function(aMsg) {
-      this._dispatchMessage(msg.type, this._handlers[msg.type], aMsg);
-    }, this);
+    // We only dispatch messages when a handler is registered.
+    let dispatcher = this._dispatchers[msg.type];
+    if (dispatcher) {
+      messages.forEach(function(aMsg) {
+        this._dispatchMessage(msg.type, dispatcher, aMsg);
+      }, this);
+    } else {
+      // We need to notify the parent that all the queued system messages have
+      // been handled (notice |handledCount: messages.length|), so the parent
+      // can release the CPU wake lock it took on our behalf.
+      cpmm.sendAsyncMessage("SystemMessageManager:HandleMessagesDone",
+                            { type: msg.type,
+                              manifest: this._manifest,
+                              uri: this._uri,
+                              handledCount: messages.length });
+    }
+
+    if (!dispatcher || !dispatcher.isHandling) {
+      // TODO: Bug 874353 - Remove SystemMessageHandledListener in ContentParent
+      Services.obs.notifyObservers(/* aSubject */ null,
+                                   "handle-system-messages-done",
+                                   /* aData */ null);
+    }
   },
 
   // nsIDOMGlobalPropertyInitializer implementation.
   init: function sysMessMgr_init(aWindow) {
     debug("init");
-    this.initHelper(aWindow, ["SystemMessageManager:Message",
+    this.initDOMRequestHelper(aWindow, ["SystemMessageManager:Message",
                               "SystemMessageManager:GetPendingMessages:Return"]);
 
     let principal = aWindow.document.nodePrincipal;
@@ -239,8 +296,9 @@ SystemMessageManager.prototype = {
     if (!this._registerManifestReady) {
       cpmm.sendAsyncMessage("SystemMessageManager:Register",
                             { manifest: this._manifest,
-                              innerWindowID: this.innerWindowID
-                            });
+                              uri: this._uri,
+                              innerWindowID: this.innerWindowID });
+
       this._registerManifestReady = true;
     }
   },
@@ -249,13 +307,15 @@ SystemMessageManager.prototype = {
 
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMNavigatorSystemMessages,
                                          Ci.nsIDOMGlobalPropertyInitializer,
-                                         Ci.nsIObserver]),
+                                         Ci.nsIObserver,
+                                         Ci.nsISupportsWeakReference]),
 
-  classInfo: XPCOMUtils.generateCI({classID: Components.ID("{bc076ea0-609b-4d8f-83d7-5af7cbdc3bb2}"),
-                                    contractID: "@mozilla.org/system-message-manager;1",
-                                    interfaces: [Ci.nsIDOMNavigatorSystemMessages],
-                                    flags: Ci.nsIClassInfo.DOM_OBJECT,
-                                    classDescription: "System Messages"})
+  classInfo: XPCOMUtils.generateCI({
+    classID: Components.ID("{bc076ea0-609b-4d8f-83d7-5af7cbdc3bb2}"),
+    contractID: "@mozilla.org/system-message-manager;1",
+    interfaces: [Ci.nsIDOMNavigatorSystemMessages],
+    flags: Ci.nsIClassInfo.DOM_OBJECT,
+    classDescription: "System Messages"})
 }
 
 this.NSGetFactory = XPCOMUtils.generateNSGetFactory([SystemMessageManager]);
