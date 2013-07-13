b7e9bf154388b0b3237f04e08aa7705f86a49811
Rudy Lu <left.lu@gmail.com>
2013-07-13 17:01:15 +0800

    Bug 805586 - Support "Swipe down" gesture to hide the keyboard.
    1. To fix the issue that keyboard may ignore all touch event after swipe
    down.
    2. Also change from asyncStorage to mozSettings to keep ftu.enabled, so
    that we could disable it when doing automated testing of keyboard.

--- apps/keyboard/js/keyboard.js
+++ apps/keyboard/js/keyboard.js
@@ -287,6 +287,11 @@ var eventHandlers = {
   'mousemove': onMouseMove
 };
 
+// For "swipe down to hide" feature
+var touchStartCoordinate;
+var toShowKeyboardFTU = false;
+const SWIPE_VELOCICTY_THRESHOLD = 0.4;
+
 // The first thing we do when the keyboard app loads is query all the
 // keyboard-related settings. Only once we have the current settings values
 // do we initialize the rest of the keyboard
@@ -302,6 +307,7 @@ function getKeyboardSettings() {
     'keyboard.autocorrect': true,
     'keyboard.vibration': false,
     'keyboard.clicksound': false,
+    'keyboard.ftu.enabled': false,
     'audio.volume.notification': 7
   };
 
@@ -319,6 +325,9 @@ function getKeyboardSettings() {
     vibrationEnabled = values['keyboard.vibration'];
     clickEnabled = values['keyboard.clicksound'];
     isSoundEnabled = !!values['audio.volume.notification'];
+
+    // To see if this is first time the user launches the keyboard
+    toShowKeyboardFTU = values['keyboard.ftu.enabled'];
 
     handleKeyboardSound();
 
@@ -396,6 +405,33 @@ function initKeyboard() {
   for (var event in eventHandlers) {
     IMERender.ime.addEventListener(event, eventHandlers[event]);
   }
+
+  // Prevent focus being taken away by tip window
+  var tipWindow = document.getElementById('confirm-dialog');
+  function tipFocusHandler(evt) {
+    evt.preventDefault();
+  }
+
+  tipWindow.addEventListener('mousedown', tipFocusHandler);
+
+  var tipButton = document.getElementById('ftu-ok');
+  tipButton.addEventListener('click', function(evt) {
+    // Need to preventDefault or it will make the input lose the focus
+    evt.preventDefault();
+    tipWindow.hidden = true;
+  });
+
+  /* To simulate :active effect for button */
+  tipButton.addEventListener('mousedown', function mouseDownHandler(evt) {
+    tipButton.classList.add('active');
+  });
+
+  var inActiveHandlers = ['mouseup', 'mouseleave'];
+  inActiveHandlers.forEach(function addInActiveHandler(evtName) {
+    tipButton.addEventListener(evtName, function inActiveHandler(evt) {
+      tipButton.classList.remove('active');
+    });
+  });
 
   dimensionsObserver = new MutationObserver(function() {
     updateTargetWindowHeight();
@@ -1106,6 +1142,11 @@ function onTouchStart(evt) {
 
     touchedKeys[touchId] = { target: target, x: touch.pageX, y: touch.pageY };
     startPress(target, touch, touchId);
+
+    touchStartCoordinate = { touchId: touchId,
+                             pageX: touch.pageX,
+                             pageY: touch.pageY,
+                             timeStamp: evt.timeStamp };
   });
 }
 
@@ -1138,6 +1179,35 @@ function onTouchEnd(evt) {
   touchCount = evt.touches.length;
 
   handleTouches(evt, function handleTouchEnd(touch, touchId) {
+
+    // Swipe down can trigger hiding the keyboard
+    if (touchStartCoordinate && touchStartCoordinate.touchId == touchId) {
+      var dx = touch.pageX - touchStartCoordinate.pageX;
+      var dy = touch.pageY - touchStartCoordinate.pageY;
+      var dt = evt.timeStamp - touchStartCoordinate.timeStamp;
+      var vy = dy / dt;
+
+      var keyboardHeight = IMERender.ime.scrollHeight;
+
+      // hide the keyboard if:
+      // 1. swipe down
+      // 2. the distance is longer than half of the keyboard
+      if ((dy > keyboardHeight / 2 && dy > dx) &&
+          vy > SWIPE_VELOCICTY_THRESHOLD) {
+
+        // de-activate the highlighted effect
+        if (touchedKeys[touchId] && touchedKeys[touchId].target)
+          IMERender.unHighlightKey(touchedKeys[touchId].target);
+
+        clearTimeout(deleteTimeout);
+        clearInterval(deleteInterval);
+        clearTimeout(menuTimeout);
+
+        window.navigator.mozKeyboard.removeFocus();
+        return;
+      }
+    }
+
     // Because of bug 822558, we sometimes get two touchend events,
     // so we should bail if we've already handled one touchend.
     if (!touchedKeys[touchId])
@@ -1582,6 +1652,16 @@ function showKeyboard(state) {
     inputMethod.activate(Keyboards[keyboardName].autoCorrectLanguage, state, {
       suggest: suggestionsEnabled,
       correct: correctionsEnabled
+    });
+  }
+
+  if (toShowKeyboardFTU) {
+    var dialog = document.getElementById('confirm-dialog');
+    dialog.hidden = false;
+    toShowKeyboardFTU = false;
+
+    navigator.mozSettings.createLock().set({
+      'keyboard.ftu.enabled': false
     });
   }
 
