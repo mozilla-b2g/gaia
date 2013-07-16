10b39b03beca0b64a7ce85830f89d687dcb08930
Mike Pennisi <mike@mikepennisi.com>
2013-07-16 16:33:35 -0400

    Add unit tests for ClockView#updateDigitalClock
    This commit also fixes a bug in the tested method's implementation,
    where the minute boundary was being calculated incorrectly. This bug
    would induce the method to asynchronously recurse during the second of
    time before the minute rolled over.

--- apps/clock/test/unit/clock_view_test.js
+++ apps/clock/test/unit/clock_view_test.js
@@ -1,4 +1,5 @@
 requireApp('clock/js/clock_view.js');
+requireApp('clock/js/utils.js');
 
 describe('ClockView', function() {
 
@@ -41,6 +42,59 @@ describe('ClockView', function() {
       this.clock.tick(18 * 60 * 60 * 1000);
       assert.equal(this.dayDateElem.innerHTML,
         'Wed Jul <b>17</b> 2013 00:00:00 GMT-0400 (EDT)');
+    });
+
+  });
+
+  suite('updateDigitalClock', function() {
+
+    suiteSetup(function() {
+      this.timeElem = document.createElement('div');
+      this.timeElem.id = 'clock-time';
+      document.body.appendChild(this.timeElem);
+      this.hourStateElem = document.createElement('div');
+      this.hourStateElem.id = 'clock-hour24-state';
+      document.body.appendChild(this.hourStateElem);
+    });
+
+    setup(function() {
+      this.clock = sinon.useFakeTimers(1373968801000);
+    });
+
+    teardown(function() {
+      this.timeElem.innerHTML = '';
+      this.hourStateElem.innerHTML = '';
+      this.clock.restore();
+    });
+
+    suiteTeardown(function() {
+      this.timeElem.parentNode.removeChild(this.timeElem);
+      this.hourStateElem.parentNode.removeChild(this.hourStateElem);
+    });
+
+    test('time and hourState elements are updated immediately',
+      function() {
+      ClockView.updateDigitalClock();
+      assert.equal(this.timeElem.innerHTML,
+        'Tue Jul 16 2013 06:00:01 GMT-0400 (EDT)');
+      assert.equal(this.hourStateElem.innerHTML, '&nbsp;&nbsp;');
+    });
+
+    test('time and hourState elements are not updated twice in the same ' +
+      'minute', function() {
+      ClockView.updateDigitalClock();
+      this.clock.tick(59 * 1000 - 1);
+      assert.equal(this.timeElem.innerHTML,
+        'Tue Jul 16 2013 06:00:01 GMT-0400 (EDT)');
+      assert.equal(this.hourStateElem.innerHTML, '&nbsp;&nbsp;');
+    });
+
+    test('time and hourState elements are updated each minute', function() {
+      ClockView.updateDigitalClock();
+      this.clock.tick(59 * 1000);
+      assert.equal(this.timeElem.innerHTML,
+        'Tue Jul 16 2013 06:01:00 GMT-0400 (EDT)');
+      assert.equal(this.hourStateElem.innerHTML, '&nbsp;&nbsp;');
     });
 
   });
