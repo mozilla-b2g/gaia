3724c21c68f88169a443e2a5636b65b4a4a58f59
Alive.Kuo <alegnadise@gmail.com>
2013-07-15 20:02:27 -0700

    Merge pull request #10991 from jlebar/gaia-event-listener-leak
    Bug 894081 - (Try to) fix a leak in Gaia by registering its mozbrowserer...r=alive
    (cherry picked from commit d85dbb240e000903c9cb0a7a1f8aff45b3592e28)

--- apps/system/js/window_manager.js
+++ apps/system/js/window_manager.js
@@ -1047,10 +1047,16 @@ var WindowManager = (function() {
         type = 'appopen';
       }
 
-      app.frame.addEventListener(type, function apploaded(e) {
+      // Be careful about what you reference from within the closure below (or
+      // any other closures in this function), or you might leak
+      // homescreenFrame.  For example, referencing |document| causes this
+      // leak; that's why we pull the document off e.target.  See bug 894135,
+      // and if in doubt, ask one of the people referenced in that bug.
+      app.iframe.addEventListener(type, function apploaded(e) {
+        var doc = e.target.ownerDocument;
         e.target.removeEventListener(e.type, apploaded, true);
 
-        var evt = document.createEvent('CustomEvent');
+        var evt = doc.createEvent('CustomEvent');
         evt.initCustomEvent('apploadtime', true, false, {
           time: parseInt(Date.now() - iframe.dataset.start),
           type: (e.type == 'appopen') ? 'w' : 'c'
