638562a329fa7d708413715f409867107db09999
Evan Shapiro <evan.a.shapiro@gmail.com>
2013-07-30 13:04:06 -0700

    music2: ux redesign
    Conflicts:
    	test_apps/music2/style/css/style.css

--- test_apps/music2/js/UI/ViewEvents.js
+++ test_apps/music2/js/UI/ViewEvents.js
@@ -1,43 +1,29 @@
 var ViewEvents = function(){
+
   Utils.loadDomIds(this, [
       'selectPageContentOverlay',
       'currentMusicContentOverlay',
 
-      'gotoCurrentMusicPage',
       'gotoSelectMusicPage',
-
-      'toggleSettingsDrawer',
-      'togglePlaylistDrawer',
 
       'toggleCurrentMusicPageView',
 
       'nowPlayingInfo'
   ]);
 
-  Utils.setupPassParent(this, 'gotoCurrentMusicPage');
-  Utils.setupPassParent(this, 'gotoSelectMusicPage');
-
-  Utils.setupPassParent(this, 'toggleSettingsDrawer');
-  Utils.setupPassParent(this, 'togglePlaylistDrawer');
-
-  Utils.setupPassParent(this, 'toggleCurrentMusicPageView');
-
-  Utils.onButtonTap(this.dom.gotoCurrentMusicPage, this.gotoCurrentMusicPage);
-  Utils.onButtonTap(this.dom.gotoSelectMusicPage, this.gotoSelectMusicPage);
-
-  Utils.onButtonTap(this.dom.toggleSettingsDrawer, this.toggleSettingsDrawer);
-  Utils.onButtonTap(this.dom.togglePlaylistDrawer, this.togglePlaylistDrawer);
+  Router.route(this, [
+    'gotoCurrentMusicPage',
+    'gotoSelectMusicPage',
+    'toggleCurrentMusicPageView'
+  ]);
 
   Utils.onButtonTap(this.dom.toggleCurrentMusicPageView, this.toggleCurrentMusicPageView);
-
   Utils.onButtonTap(this.dom.nowPlayingInfo, this.gotoCurrentMusicPage);
-
-
-  Utils.onButtonTap(this.dom.selectPageContentOverlay, this.toggleSettingsDrawer.bind(this));
-  Utils.onButtonTap(this.dom.currentMusicContentOverlay, this.togglePlaylistDrawer.bind(this));
+  Utils.onButtonTap(this.dom.gotoSelectMusicPage, this.gotoSelectMusicPage);
 
 }
 
 ViewEvents.prototype = {
+  name: "ViewEvents",
 
 }
