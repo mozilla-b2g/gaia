00e3a7f4628015395ae30c49b619dda1a606e5f9
Agustín Álvarez Martínez <aalvarezmartinez@danielcastelao.org>
2013-08-02 14:27:38 +0200

    bug 823385 - Show developer provided description of a permission inside the permission prompt. r=vingtetun.

--- apps/system/js/permission_manager.js
+++ apps/system/js/permission_manager.js
@@ -53,15 +53,15 @@ var PermissionManager = (function() {
     var permissionID = 'perm-' + detail.permission.replace(':', '-');
     // The app is certified o priviledged
     if ((detail.appType == Ci.nsIPrincipal.APP_STATUS_PRIVILEGED) ||
-       (detail.appType ==  Ci.nsIPrincipal.APP_STATUS_CERTIFIED)) { 
-      str = _(permissionID + '-appRequest', { 'app': detail.appName })
-            + detail.description;//Added to show permissions description
+       (detail.appType == Ci.nsIPrincipal.APP_STATUS_CERTIFIED)) {
+      str = _(permissionID + '-appRequest', { 'app': detail.appName }) +
+            detail.description;//Added to show permissions description
     // The app is webapp
     } else if (detail.appType == Ci.nsIPrincipal.APP_STATUS_INSTALLED) {
       //Added to show permissions description with a warning about its origin
       //Comment next two lines if don't want to show description for webapps
-      str = _(permissionID + '-appRequest', { 'app': detail.appName })
-            + "Provided by developer: " + detail.description;
+      str = _(permissionID + '-appRequest', { 'app': detail.appName }) +
+            'Provided by developer: ' + detail.description;
     } else { // Web content
       str = _(permissionID + '-webRequest', { 'site': detail.origin });
     }
