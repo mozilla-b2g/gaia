dce27932836dfdf9645eae565bda084692ed1a1d
Francisco Jordano <arcturus@ardeenelinfierno.com>
2013-08-06 05:08:33 -0700

    Merge pull request #11351 from jmcanterafonseca/fix_link_img
    Bug 901512 - [Contacts] Unable to finish the linking process when there ...(cherry picked from commit a356920154f9bdcde0624092fe0ec5d7a3f40dfe)
    
    Conflicts:
    	apps/communications/facebook/js/facebook_connector.js

--- apps/communications/facebook/js/facebook_connector.js
+++ apps/communications/facebook/js/facebook_connector.js
@@ -116,10 +116,6 @@ if (!window.FacebookConnector) {
             });
           }).bind(this); // successCb
 
-          photoCbs.timeout = function noop() {
-            // Do nothing...
-          };
-
           self.downloadContactPicture(friend, acc_tk, photoCbs);
         } // if friend
         else {
@@ -260,15 +256,8 @@ if (!window.FacebookConnector) {
       },
 
       downloadContactPicture: function(contact, access_token, callbacks) {
-        return fb.utils.getFriendPicture(contact.uid,
-          function(blobPicture) {
-            if (blobPicture) {
-              callbacks.success(blobPicture);
-            }
-            else {
-              callbacks.timeout();
-            }
-          }, access_token);
+        return fb.utils.getFriendPicture(contact.uid, callbacks.success,
+                        access_token, importUtils.getPreferredPictureDetail());
       },
 
       oncontactsloaded: function(lfriends) {
