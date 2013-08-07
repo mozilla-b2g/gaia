28ba36edf64c22aae7480c5e2d9897350a5d5d1d
Eric O'Connor <eric@bocoup.com>
2013-08-06 23:58:38 -0400

    Change alarm repeat data representation from a bit-string to an object.
    '0000011' -> {saturday: true, sunday: true}

---
+++ apps/clock/js/constants.js
@@ -0,0 +1,25 @@
+(function(exports) {
+
+  'use strict';
+
+  // ---------------------------------------------------------
+  // Constants
+
+  var DAYS = exports.DAYS = [
+    'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+    'saturday', 'sunday'
+  ];
+
+  var RDAYS = exports.RDAYS = DAYS.map(function(_, n) {
+    return n;
+  });
+
+  var WEEKDAYS = exports.WEEKDAYS = [0, 1, 2, 3, 4].map(function(x) {
+    return DAYS[x];
+  });
+
+  var WEEKENDS = exports.WEEKENDS = [5, 6].map(function(x) {
+    return DAYS[x];
+  });
+
+})(this);
