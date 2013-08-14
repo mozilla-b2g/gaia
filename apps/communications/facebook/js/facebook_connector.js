519ca3efb565e9850b024d41856b090712eb4334
John Ford <john@johnford.info>
2013-08-14 13:19:27 -0700

    bug 905287 was caused by this commit.  Reverting "Merge pull request #11351 from jmcanterafonseca/fix_link_img"
    This reverts commit dce27932836dfdf9645eae565bda084692ed1a1d.

--- apps/communications/facebook/js/facebook_connector.js
+++ apps/communications/facebook/js/facebook_connector.js
@@ -116,6 +116,10 @@ if (!window.FacebookConnector) {
             });
           }).bind(this); // successCb
 
+          photoCbs.timeout = function noop() {
+            // Do nothing...
+          };
+
           self.downloadContactPicture(friend, acc_tk, photoCbs);
         } // if friend
         else {
@@ -256,8 +260,15 @@ if (!window.FacebookConnector) {
       },
 
       downloadContactPicture: function(contact, access_token, callbacks) {
-        return fb.utils.getFriendPicture(contact.uid, callbacks.success,
-                        access_token, importUtils.getPreferredPictureDetail());
+        return fb.utils.getFriendPicture(contact.uid,
+          function(blobPicture) {
+            if (blobPicture) {
+              callbacks.success(blobPicture);
+            }
+            else {
+              callbacks.timeout();
+            }
+          }, access_token);
       },
 
       oncontactsloaded: function(lfriends) {
