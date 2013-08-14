55539f5eb09387352fd361c0f531069182cfb70c
Arthur Chen <crh0716@gmail.com>
2013-08-14 17:02:37 +0800

    Bug 856910, Bug 893557 - 1. Add Observable, ObservableArray, and ListView to Utils. 2. Add unit testing. 3. Implement the keyboard panel based on the UX spec. r=evelyn

--- apps/settings/js/settings.js
+++ apps/settings/js/settings.js
@@ -304,7 +304,7 @@ var Settings = {
         setTimeout(this.updateLanguagePanel);
         break;
       case 'keyboard':
-        Settings.updateKeyboardPanel();
+        //Settings.updateKeyboardPanel();
         break;
       case 'battery':             // full battery status
         Battery.update();
@@ -783,50 +783,50 @@ var Settings = {
   },
 
   updateKeyboardPanel: function settings_updateKeyboardPanel() {
-    var panel = document.getElementById('keyboard');
+    // var panel = document.getElementById('keyboard');
     // Update the keyboard layouts list from the Keyboard panel
-    if (panel) {
-      this.getSupportedKbLayouts(function updateKbList(keyboards) {
-        var kbLayoutsList = document.getElementById('keyboard-layouts');
-        // Get pointers to the top list entry and its labels which are used to
-        // pin the language associated keyboard at the top of the keyboards list
-        var pinnedKb = document.getElementById('language-keyboard');
-        var pinnedKbLabel = pinnedKb.querySelector('a');
-        var pinnedKbSubLabel = pinnedKb.querySelector('small');
-        pinnedKbSubLabel.textContent = '';
+    // if (panel) {
+    //   this.getSupportedKbLayouts(function updateKbList(keyboards) {
+    //     var kbLayoutsList = document.getElementById('keyboard-layouts');
+    //   // Get pointers to the top list entry and its labels which are used to
+    // // pin the language associated keyboard at the top of the keyboards list
+    //     var pinnedKb = document.getElementById('language-keyboard');
+    //     var pinnedKbLabel = pinnedKb.querySelector('a');
+    //     var pinnedKbSubLabel = pinnedKb.querySelector('small');
+    //     pinnedKbSubLabel.textContent = '';
 
-        // Get the current language and its associate keyboard layout
-        var currentLang = document.documentElement.lang;
-        var langKeyboard = keyboards.layout[currentLang];
+    //     // Get the current language and its associate keyboard layout
+    //     var currentLang = document.documentElement.lang;
+    //     var langKeyboard = keyboards.layout[currentLang];
 
-        var kbSelector = 'input[name="keyboard.layouts.' + langKeyboard + '"]';
-        var kbListQuery = kbLayoutsList.querySelector(kbSelector);
+    //  var kbSelector = 'input[name="keyboard.layouts.' + langKeyboard + '"]';
+    //     var kbListQuery = kbLayoutsList.querySelector(kbSelector);
 
-        if (kbListQuery) {
-          // Remove the entry from the list since it will be pinned on top
-          // of the Keyboard Layouts list
-          var kbListEntry = kbListQuery.parentNode.parentNode;
-          kbListEntry.hidden = true;
+    //     if (kbListQuery) {
+    //       // Remove the entry from the list since it will be pinned on top
+    //       // of the Keyboard Layouts list
+    //       var kbListEntry = kbListQuery.parentNode.parentNode;
+    //       kbListEntry.hidden = true;
 
-          var label = kbListEntry.querySelector('a');
-          var sub = kbListEntry.querySelector('small');
-          pinnedKbLabel.dataset.l10nId = label.dataset.l10nId;
-          pinnedKbLabel.textContent = label.textContent;
-          if (sub) {
-            pinnedKbSubLabel.dataset.l10nId = sub.dataset.l10nId;
-            pinnedKbSubLabel.textContent = sub.textContent;
-          }
-        } else {
-          // If the current language does not have an associated keyboard,
-          // fallback to the default keyboard: 'en'
-          // XXX update this if the list order in index.html changes
-          var englishEntry = kbLayoutsList.children[1];
-          englishEntry.hidden = true;
-          pinnedKbLabel.dataset.l10nId = 'english';
-          pinnedKbSubLabel.textContent = '';
-        }
-      });
-    }
+    //       var label = kbListEntry.querySelector('a');
+    //       var sub = kbListEntry.querySelector('small');
+    //       pinnedKbLabel.dataset.l10nId = label.dataset.l10nId;
+    //       pinnedKbLabel.textContent = label.textContent;
+    //       if (sub) {
+    //         pinnedKbSubLabel.dataset.l10nId = sub.dataset.l10nId;
+    //         pinnedKbSubLabel.textContent = sub.textContent;
+    //       }
+    //     } else {
+    //       // If the current language does not have an associated keyboard,
+    //       // fallback to the default keyboard: 'en'
+    //       // XXX update this if the list order in index.html changes
+    //       var englishEntry = kbLayoutsList.children[1];
+    //       englishEntry.hidden = true;
+    //       pinnedKbLabel.dataset.l10nId = 'english';
+    //       pinnedKbSubLabel.textContent = '';
+    //     }
+    //   });
+    // }
   }
 };
 
@@ -843,8 +843,10 @@ window.addEventListener('load', function loadSettings() {
   Settings.init();
   handleRadioAndCardState();
 
-  LazyLoader.load(['js/utils.js'], startupLocale);
+  LazyLoader.load(['js/utils.js', 'js/mvvm/models.js', 'js/mvvm/views.js'],
+    startupLocale);
   LazyLoader.load([
+      'shared/js/keyboard_helper.js',
       'js/airplane_mode.js',
       'js/battery.js',
       'shared/js/async_storage.js',
@@ -993,13 +995,13 @@ function initLocale() {
 
   // update the keyboard layouts list by resetting the top pinned element,
   // since it displays the previous language setting
-  var kbLayoutsList = document.getElementById('keyboard-layouts');
-  if (kbLayoutsList) {
-    var prevKbLayout = kbLayoutsList.querySelector('li[hidden]');
-    prevKbLayout.hidden = false;
+  // var kbLayoutsList = document.getElementById('keyboard-layouts');
+  // if (kbLayoutsList) {
+  //   var prevKbLayout = kbLayoutsList.querySelector('li[hidden]');
+  //   prevKbLayout.hidden = false;
 
-    Settings.updateKeyboardPanel();
-  }
+  //   Settings.updateKeyboardPanel();
+  // }
 }
 
 // Do initialization work that doesn't depend on the DOM, as early as
