749fbf5b9d1da7ee4c809c391a8eec65e424beba
Jim Porter <jporter@mozilla.com>
2013-08-15 18:03:44 -0500

    Bug 902974 - [Music] [User Story] Utility tray should display information about the song when music is still playing

--- apps/music/js/utils.js
+++ apps/music/js/utils.js
@@ -77,3 +77,47 @@ function displayAlbumArt(img, fileinfo) {
     });
   });
 }
+
+// Fetch the cover art for a given file and return it as a Blob. If there's no
+// embedded cover art, grab the appropriate placeholder image instead. XXX: This
+// function is a bit convoluted, and we could stand to simplify things here,
+// e.g. by merging placeholder art with embedded art and/or storing cover art
+// on the sdcard.
+function getAlbumArtBlob(fileinfo, backgroundIndex, callback) {
+  var getBlob = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+
+    xhr.onload = function() {
+      callback(null, xhr.response);
+    };
+    // I don't think onerror usually gets called, but let's play it safe.
+    xhr.onerror = function() {
+      callback('error');
+    };
+
+    // Bad local URLs throw in send() for some reason.
+    try {
+      xhr.send();
+    } catch (e) {
+      callback(e);
+    }
+  };
+
+  if ('picture' in fileinfo.metadata) {
+    getThumbnailURL(fileinfo, function(url) {
+      if (!url)
+        return callback(null);
+      getBlob(url, callback);
+    });
+  }
+  else if (backgroundIndex || backgroundIndex === 0) {
+    var realIndex = backgroundIndex % 10 + 1;
+    var url = '/style/images/AlbumArt' + realIndex + '_small.png';
+    getBlob(url, callback);
+  }
+  else {
+    callback(null, null);
+  }
+}
