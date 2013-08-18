e80bfdedb78da526a46c1d5d6a542b68b9b44aae
Caio Lima <ticaiolima@gmail.com>
2013-08-18 13:23:45 -0300

    Bug #838657 - Created a BaseMozReporter that makes the base work of the a PerfReporter and implemented the current reporters using it

---
+++ tests/reporters/basemozreporter.js
@@ -0,0 +1,88 @@
+/*
+ * This function is the Base of all performance reporters. The reporter callback is the function 
+ * that will be called when the runner end run the test 
+ */  
+
+function BasePerfReporter(runner, reporterCallback) {
+  global.Mocha.reporters.Base.call(this, runner);
+
+  // "mocha" is the Mocha instance
+  // by default mocha report if any test leaks a variable in the global scope.
+  // We don't need this here because we're really running tests on the device,
+  // so this ignores leaks in our tests, and make it easier to use a global
+  // variable to save our test resuls.
+  global.mocha.options.ignoreLeaks = true;
+
+  var failures = [];
+  var passes = [];
+
+  runner.on('test', function(test) {
+    global.mozPerfDurations = null;
+  });
+
+  runner.on('pass', function(test) {
+    if (global.mozPerfDurations === null) {
+      test.err = new Error('No perf data was reported');
+      failures.push(test);
+      return;
+    }
+
+    for (var title in global.mozPerfDurations) {
+      // we can have several measurements for one test, that's why we're
+      // rewriting the title (each measurement has a title)
+      passes.push({
+        title: test.title + ' ' + title,
+        fullTitle: test.fullTitle() + ' ' + title,
+        duration: test.duration,
+        mozPerfDurations: global.mozPerfDurations[title],
+        mozPerfDurationsAverage: average(global.mozPerfDurations[title])
+      });
+    }
+  });
+
+  runner.on('fail', function(test, err) {
+    failures.push(test);
+  });
+
+  var self = this;
+  runner.on('end', function() {
+    self.stats.application = window.mozTestInfo.appPath;
+    var obj = {
+      stats: self.stats,
+      failures: failures.map(cleanErr),
+      passes: passes
+    };
+
+    reporterCallback(obj);
+  });
+}
+
+function cleanErr(test) {
+  var err = test.err;
+  var message = err.message || '';
+  var stack = window.xpcError.format(err);
+  var index = stack.indexOf(message) + message.length;
+  var msg = stack.slice(0, index);
+  var actual = err.actual;
+  var expected = err.expected;
+
+  return {
+    title: test.title,
+    fullTitle: test.fullTitle(),
+    duration: test.duration,
+    stack: stack,
+    index: index,
+    msg: msg,
+    actual: actual,
+    expected: expected
+  }
+};
+
+function average(arr) {
+  var sum = arr.reduce(function(i, j) {
+    return i + j;
+  });
+
+  return sum / arr.length;
+};
+
--- tests/reporters/consolemozperf.js
+++ tests/reporters/consolemozperf.js
@@ -9,87 +9,12 @@
 (function(global) {
 
 function ConsoleMozPerfReporter(runner) {
-  global.Mocha.reporters.Base.call(this, runner);
-
-  // "mocha" is the Mocha instance
-  // by default mocha report if any test leaks a variable in the global scope.
-  // We don't need this here because we're really running tests on the device,
-  // so this ignores leaks in our tests, and make it easier to use a global
-  // variable to save our test resuls.
-  global.mocha.options.ignoreLeaks = true;
-
-  var failures = [];
-  var passes = [];
-
-  runner.on('test', function(test) {
-    global.mozPerfDurations = null;
-  });
-
-  runner.on('pass', function(test) {
-    if (global.mozPerfDurations === null) {
-      test.err = new Error('No perf data was reported');
-      failures.push(test);
-      return;
-    }
-
-    for (var title in global.mozPerfDurations) {
-      // we can have several measurements for one test, that's why we're
-      // rewriting the title (each measurement has a title)
-      passes.push({
-        title: test.title + ' ' + title,
-        fullTitle: test.fullTitle() + ' ' + title,
-        duration: test.duration,
-        mozPerfDurations: global.mozPerfDurations[title],
-        mozPerfDurationsAverage: average(global.mozPerfDurations[title])
-      });
-    }
-  });
-
-  runner.on('fail', function(test, err) {
-    failures.push(test);
-  });
-
-  var self = this;
-  runner.on('end', function() {
-    self.stats.application = window.mozTestInfo.appPath;
-    var obj = {
-      stats: self.stats,
-      failures: failures.map(cleanErr),
-      passes: passes
-    };
-
+  importScripts("../reporters/basemozreporter.js");
+  
+  BasePerfReporter(runner, function(obj){
     process.stdout.write(generateConsoleOut(obj));
-  });
+  });  
 }
-
-function cleanErr(test) {
-  var err = test.err;
-  var message = err.message || '';
-  var stack = window.xpcError.format(err);
-  var index = stack.indexOf(message) + message.length;
-  var msg = stack.slice(0, index);
-  var actual = err.actual;
-  var expected = err.expected;
-
-  return {
-    title: test.title,
-    fullTitle: test.fullTitle(),
-    duration: test.duration,
-    stack: stack,
-    index: index,
-    msg: msg,
-    actual: actual,
-    expected: expected
-  };
-};
-
-function average(arr) {
-  var sum = arr.reduce(function(i, j) {
-    return i + j;
-  });
-
-  return sum / arr.length;
-};
 
 function generateConsoleOut(objOut) {
   var consoleOut = 'Application: ' + objOut.stats.application + '\n\n';
@@ -134,8 +59,6 @@ function generateConsoleOut(objOut) {
 
   return consoleOut;
 }
-
-
 
 global.Mocha.reporters.ConsoleMozPerf = ConsoleMozPerfReporter;
 })(this);
