a3ba85fb2178bfa1c2ddfd8c4708affe53cd3f5b
Corey Frang <gnarf37@gmail.com>
2013-08-20 12:58:35 -0400

    Bug 886715 - [MMS] [Regression] After changing language all strings should be in new language - r=julienw
    This is a complete overhaul of all localization inside the SMS app.
    
    - Avoid using `navigator.mozL10n.get()` to populate `innerText` ourselves
      - Exceptions to this rule: when the final target is something like `alert()` or `navigator.mozNotification`, we can't really pass HTML, so do our best with the current translated string
    - Use `navigator.mozL10n.localize(element, id, args)` where possible
    - Use `navigator.mozL10n.translate(fragment)` and stuff the proper data-* attribs where we render templates.
    
    Closes gh-11491

--- apps/sms/js/startup.js
+++ apps/sms/js/startup.js
@@ -40,10 +40,42 @@ var lazyLoadFiles = [
   'style/notification.css'
 ];
 
-window.addEventListener('localized', function showBody() {
+window.addEventListener('localized', function localized() {
+  // This will be called during startup, and any time the languange is changed
+
   // Set the 'lang' and 'dir' attributes to <html> when the page is translated
   document.documentElement.lang = navigator.mozL10n.language.code;
   document.documentElement.dir = navigator.mozL10n.language.direction;
+
+  // Look for any iframes and localize them - mozL10n doesn't do this
+  Array.prototype.forEach.call(document.querySelectorAll('iframe'),
+    function forEachIframe(iframe) {
+      var doc = iframe.contentDocument;
+      doc.documentElement.lang = navigator.mozL10n.language.code;
+      doc.documentElement.dir = navigator.mozL10n.language.direction;
+      navigator.mozL10n.translate(doc.body);
+    }
+  );
+
+  // Also look for not-downloaded-message and re-translate the date message.
+  // More complex because the argument to the l10n string is itself a formatted
+  // date using l10n.
+  Array.prototype.forEach.call(
+    document.getElementsByClassName('not-downloaded-message'),
+    function(element) {
+      if (!(element.dataset.l10nArgs && element.dataset.l10nId &&
+            element.dataset.l10nDate)) {
+        return;
+      }
+      var args = JSON.parse(element.dataset.l10nArgs);
+      var format = navigator.mozL10n.get(element.dataset.l10nDateFormat);
+      var date = new Date(element.dataset.l10nDate);
+      args.date = Utils.date.format.localeFormat(date, format);
+
+      navigator.mozL10n.localize(element, element.dataset.l10nId, args);
+    }
+  );
+
 });
 
 window.addEventListener('load', function() {
