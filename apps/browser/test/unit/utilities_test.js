b41002e2de49fefb8d7a856178275c4d23cd8547
cctuan <georgeiscoming@gmail.com>
2013-08-22 14:30:50 +0800

    Bug 904731 - [browser] text entered into search bar containing dot (.) should not be treated as url address if it has whitespaces

--- apps/browser/test/unit/utilities_test.js
+++ apps/browser/test/unit/utilities_test.js
@@ -8,6 +8,8 @@ suite('URL Helper', function() {
       'a . b ?',
       'what is mozilla',
       'what is mozilla?',
+      'www.blah.com foo',
+      'a?some b',
       'docshell site:mozilla.org',
       '?mozilla',
       '?site:mozilla.org docshell'
@@ -18,16 +20,15 @@ suite('URL Helper', function() {
 
   test('isNotURL => false', function() {
     [
+      'http://foo',
       'blerg.co.uk',
       'blach.com',
       'www.blah.com',
-      'www.blah.com foo',
       'a:80',
       'a?',
       'a?b',
-      'a?some b',
-      'data:foo',
-      'http://foo.com'
+      'http://foo.com',
+      'data:about'
     ].forEach(function(input) {
       assert.ok(!UrlHelper.isNotURL(input));
     });
