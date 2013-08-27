6879d212d8f0030d64e30441dd4cdfc16415478e
Evelyn Eastmond <evelyn@bocoup.com>
2013-08-27 17:37:09 -0400

    Bug 909386 - [Clock] Refactor Stopwatch API
    r=jugglinmike

--- apps/clock/js/stopwatch.js
+++ apps/clock/js/stopwatch.js
@@ -1,79 +1,112 @@
-'use strict';
+(function(exports) {
 
-var StopWatch = {
-  _elapsed: 0,
+  'use strict';
 
-  get actionButton() {
-    delete this.actionButton;
-    var id = 'stopwatch-action-button';
-    return this.actionButton = document.getElementById(id);
-  },
+  var wm = new WeakMap();
 
-  get resetButton() {
-    delete this.resetButton;
-    return this.actionButton = document.getElementById('reset-button');
-  },
+  function Defaults() {
+    this.startTime = 0;
+    this.totalElapsed = 0;
+    this.isStarted = false;
+    this.laps = [];
+  }
 
-  get tickerView() {
-    delete this.tickerView;
-    return this.tickerView = document.getElementById('stopwatch-ticker-view');
-  },
+  function Stopwatch() {
+    this.reset();
+  };
 
-  get chronoView() {
-    delete this.chronoView;
-    return this.chronoView = document.getElementById('stopwatch-chrono-view');
-  },
+  Stopwatch.prototype = {
 
-  execute: function sw_execute(action) {
-    if (!this[action]) {
-      return;
+    constructor: Stopwatch,
+
+    /**
+    * start Starts the stopwatch, either from a reset or paused state
+    */
+    start: function sw_start() {
+      var sw = wm.get(this);
+      if (sw.isStarted) {
+        return;
+      }
+      sw.startTime = Date.now();
+      sw.isStarted = true;
+    },
+
+    /**
+    * getElapsedTime Calculates the total elapsed duration since the
+    *                stopwatch was started
+    * @return {Date} return total elapsed duration
+    */
+    getElapsedTime: function sw_getElapsedTime() {
+      var sw = wm.get(this);
+      var elapsed = 0;
+      if (sw.isStarted) {
+        elapsed = Date.now() - sw.startTime;
+      }
+      elapsed += sw.totalElapsed;
+
+      return new Date(elapsed);
+    },
+
+    /**
+    * pause Pauses the stopwatch
+    */
+    pause: function sw_pause() {
+      var sw = wm.get(this);
+      if (!sw.isStarted) {
+        return;
+      }
+      sw.isStarted = false;
+      var elapsed = Date.now() - sw.startTime;
+      sw.totalElapsed += elapsed;
+    },
+
+    /**
+    * lap Calculates a new lap duration since the last lap time
+    *     If the stopwatch isn't currently running, returns 0
+    * @return {Date} return the lap duration
+    */
+    lap: function sw_lap() {
+      var sw = wm.get(this);
+      if (!sw.isStarted) {
+        return new Date(0);
+      }
+
+      var lastLapTime;
+      var newLap = {};
+
+      if (sw.laps.length > 0) {
+        lastLapTime = sw.laps[sw.laps.length - 1].time;
+      } else {
+        lastLapTime = sw.startTime;
+      }
+
+      newLap.duration = Date.now() - lastLapTime;
+      newLap.time = Date.now();
+      sw.laps.push(newLap);
+
+      return new Date(newLap.duration);
+    },
+
+    /**
+    * getLapDurations Returns an array of lap durations, sorted by oldest first
+    * @return {Array} return an array of lap durations
+    */
+    getLapDurations: function sw_getLapDurations() {
+      var sw = wm.get(this);
+      return sw.laps.map(function(lap) {
+        return lap.duration;
+      });
+    },
+
+    /**
+    * reset Resets the stopwatch back to 0, clears laps
+    */
+    reset: function sw_reset() {
+      wm.set(this, new Defaults());
     }
 
-    this[action]();
-  },
+  };
 
-  start: function sw_start() {
-    this.actionButton.dataset.action = 'stop';
+  exports.Stopwatch = Stopwatch;
 
-    this.updateChrono(this._elapsed);
-    this.tickerView.classList.add('running');
-
-    this._startTime = Date.now();
-    this._ticker = setInterval(function sw_updateChrono(self) {
-      var elapsed = Date.now() - self._startTime + self._elapsed;
-      self.updateChrono(elapsed);
-    }, 500, this);
-  },
-
-  stop: function sw_stop() {
-    this.actionButton.dataset.action = 'start';
-    this.tickerView.classList.remove('running');
-
-    this._elapsed += Date.now() - this._startTime;
-    clearInterval(this._ticker);
-    delete this._ticker;
-    delete this._startTime;
-  },
-
-  reset: function sw_reset() {
-    this.stop();
-    this._elapsed = 0;
-    this.updateChrono(this._elapsed);
-  },
-
-  updateChrono: function sw_updateChrono(elapsed) {
-    var f = new navigator.mozL10n.DateTimeFormat();
-    var currentValue = this.chronoView.innerHTML;
-    var newValue = f.localeFormat(elapsed, '%M:%S');
-    if (currentValue != newValue)
-      this.chronoView.innerHTML = newValue;
-  }
-};
-
-window.addEventListener('load', function onLoad() {
-  window.removeEventListener('load', onLoad);
-
-  StopWatch.actionButton.onclick = StopWatch.start.bind(StopWatch);
-  StopWatch.resetButton.onclick = StopWatch.reset.bind(StopWatch);
-});
-
+})(this);
