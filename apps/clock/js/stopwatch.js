9f0758dcbe3988dd878c65f856918fba942ceba4
Evelyn Eastmond <evelyn@bocoup.com>
2013-08-29 12:20:09 -0400

    reimplemented stopwatch API according to @jugglinmike's feedback

--- apps/clock/js/stopwatch.js
+++ apps/clock/js/stopwatch.js
@@ -3,63 +3,87 @@
   'use strict';
 
   function Stopwatch() {
-    this.startTime = 0;
-    this.totalElapsed = 0;
-    this.started = false;
-    this.lapTimes = [];
+    this._startTime = 0;
+    this._totalElapsed = 0;
+    this._started = false;
+    this._laps = [];
   };
 
   Stopwatch.prototype = {
 
     constructor: Stopwatch,
 
+   /**
+   * start Starts the stopwatch timer, either from a reset or paused state
+   */
     start: function sw_start() {
-      if (!this.started) {
-        this.startTime = new Date().getTime();
-        this.started = true;
+      if (!this._started) {
+        this._startTime = new Date().getTime();
+        this._started = true;
       }
     },
 
+    /**
+    * pause Pauses the stopwatch timer
+    */
     pause: function sw_pause() {
-      if (this.started) {
-        this.started = false;
-        var elapsed = new Date().getTime() - this.startTime;
-        this.totalElapsed += elapsed;
+      if (this._started) {
+        this._started = false;
+        var elapsed = new Date().getTime() - this._startTime;
+        this._totalElapsed += elapsed;
       }
     },
 
+    /**
+    * reset Resets the stopwatch timer back to 00:00, clears laps
+    */
     reset: function sw_reset() {
-      this.startTime = new Date().getTime();
-      this.totalElapsed = 0;
-      this.started = false;
-      this.lapTimes = [];
+      this._startTime = new Date().getTime();
+      this._totalElapsed = 0;
+      this._started = false;
+      this._laps = [];
     },
 
+    /**
+    * lap Calculates a new lap duration since the last lap time
+    *     If the stopwatch isn't currently running, returns 00:00
+    *
+    * @return {Date} return the lap duration
+    */
     lap: function sw_lap() {
-      if (this.started) {
-        var lastLapTime = {};
-        var newLapTime = {};
-        if (this.lapTimes.length > 0) {
-          lastLapTime = this.lapTimes[this.lapTimes.length - 1];
-          newLapTime.duration = new Date().getTime() - lastLapTime.time;
+      if (this._started) {
+        var lastLapTime;
+        var newLap = {};
+
+        if (this._laps.length > 0) {
+          lastLapTime = this._laps[this._laps.length - 1].time;
         } else {
-          newLapTime.duration = new Date().getTime() - this.startTime;
+          lastLapTime = this._startTime;
         }
-        newLapTime.time = new Date().getTime();
-        this.lapTimes.push(newLapTime);
-        return newLapTime.duration;
+
+        newLap.duration = new Date().getTime() - lastLapTime;
+        newLap.time = new Date().getTime();
+        this._laps.push(newLap);
+
+        return new Date(newLap.duration);
       } else {
-        return 0;
+        return new Date(0);
       }
     },
 
+    /**
+    * getElapsedTime Calculates the total elapsed duration since the
+    *                stopwatch was started
+    *
+    * @return {Date} return total elapsed duration
+    */
     getElapsedTime: function sw_elapsed() {
       var elapsed = 0;
-      if (this.started)
-        elapsed = new Date().getTime() - this.startTime;
-      elapsed += this.totalElapsed;
+      if (this._started)
+        elapsed = new Date().getTime() - this._startTime;
+      elapsed += this._totalElapsed;
 
-      return elapsed;
+      return new Date(elapsed);
     }
 
   };
@@ -67,84 +91,3 @@
   exports.Stopwatch = Stopwatch;
 
 })(this);
-
-/*'use strict';
-
-var StopWatch = {
-  _elapsed: 0,
-
-  get actionButton() {
-    delete this.actionButton;
-    var id = 'stopwatch-action-button';
-    return this.actionButton = document.getElementById(id);
-  },
-
-  get resetButton() {
-    delete this.resetButton;
-    return this.actionButton = document.getElementById('reset-button');
-  },
-
-  get tickerView() {
-    delete this.tickerView;
-    return this.tickerView = document.getElementById('stopwatch-ticker-view');
-  },
-
-  get chronoView() {
-    delete this.chronoView;
-    return this.chronoView = document.getElementById('stopwatch-chrono-view');
-  },
-
-  execute: function sw_execute(action) {
-    if (!this[action]) {
-      return;
-    }
-
-    this[action]();
-  },
-
-  start: function sw_start() {
-    this.actionButton.dataset.action = 'stop';
-
-    this.updateChrono(this._elapsed);
-    this.tickerView.classList.add('running');
-
-    this._startTime = Date.now();
-    this._ticker = setInterval(function sw_updateChrono(self) {
-      var elapsed = Date.now() - self._startTime + self._elapsed;
-      self.updateChrono(elapsed);
-    }, 500, this);
-  },
-
-  stop: function sw_stop() {
-    this.actionButton.dataset.action = 'start';
-    this.tickerView.classList.remove('running');
-
-    this._elapsed += Date.now() - this._startTime;
-    clearInterval(this._ticker);
-    delete this._ticker;
-    delete this._startTime;
-  },
-
-  reset: function sw_reset() {
-    this.stop();
-    this._elapsed = 0;
-    this.updateChrono(this._elapsed);
-  },
-
-  updateChrono: function sw_updateChrono(elapsed) {
-    var f = new navigator.mozL10n.DateTimeFormat();
-    var currentValue = this.chronoView.innerHTML;
-    var newValue = f.localeFormat(elapsed, '%M:%S');
-    if (currentValue != newValue)
-      this.chronoView.innerHTML = newValue;
-  }
-};
-
-window.addEventListener('load', function onLoad() {
-  window.removeEventListener('load', onLoad);
-
-  StopWatch.actionButton.onclick = StopWatch.start.bind(StopWatch);
-  StopWatch.resetButton.onclick = StopWatch.reset.bind(StopWatch);
-});
-*/
-
--- apps/clock/test/unit/stopwatch_test.js
+++ apps/clock/test/unit/stopwatch_test.js
@@ -1,20 +1,12 @@
 requireApp('clock/js/stopwatch.js');
 requireApp('clock/js/utils.js');
 
-requireApp('clock/test/unit/mocks/mock_navigator_mozl10n.js');
-
 // http://www.reddit.com/r/mildlyinteresting/comments/1l4224/i_started_the_stopwatch_when_i_got_my_iphone_3/
 
 suite('Stopwatch', function() {
-  var nml;
   var sw;
 
   suiteSetup(function() {
-    nml = navigator.mozL10n;
-    navigator.mozL10n = MockmozL10n;
-
-    loadBodyHTML('/index.html');
-
     // The timestamp for "Tue Jul 16 2013 06:00:00" according to the local
     // system's time zone
     this.sixAm = 1373954400000 + (new Date()).getTimezoneOffset() * 60 * 1000;
@@ -22,7 +14,7 @@ suite('Stopwatch', function() {
   });
 
   suiteTeardown(function() {
-    navigator.mozL10n = nml;
+    // Nothing to teardown?
   });
 
   setup(function() {
@@ -34,15 +26,10 @@ suite('Stopwatch', function() {
     this.clock.restore();
   });
 
-  test('start', function() {
-    sw.start();
-    assert.equal(sw.startTime, this.sixAm);
-  });
-
   test('start & elapse 1 hr', function() {
     sw.start();
     this.clock.tick(this.oneHour);
-    assert.equal(sw.getElapsedTime(), this.oneHour);
+    assert.equal(sw.getElapsedTime().getTime(), this.oneHour);
   });
 
   test('start & elapse 1 hr & pause', function() {
@@ -50,14 +37,14 @@ suite('Stopwatch', function() {
     this.clock.tick(this.oneHour);
     sw.pause();
     this.clock.tick(this.oneHour); // do not elapse this hour
-    assert.equal(sw.getElapsedTime(), this.oneHour);
+    assert.equal(sw.getElapsedTime().getTime(), this.oneHour);
   });
 
   test('start & elapse 1hr & lap', function() {
     sw.start();
     this.clock.tick(this.oneHour);
     var l = sw.lap();
-    assert.equal(l, this.oneHour);
+    assert.equal(l.getTime(), this.oneHour);
   });
 
   test('start & lap multiple times', function() {
@@ -69,9 +56,9 @@ suite('Stopwatch', function() {
     this.clock.tick(this.oneHour);
     var l3 = sw.lap();
     console.log(sw.lapTimes);
-    assert.equal(l1, this.oneHour);
-    assert.equal(l2, this.oneHour);
-    assert.equal(l3, this.oneHour);
+    assert.equal(l1.getTime(), this.oneHour);
+    assert.equal(l2.getTime(), this.oneHour);
+    assert.equal(l3.getTime(), this.oneHour);
   });
 
   test('pause & elapse 1 hr & resume', function() {
@@ -81,7 +68,7 @@ suite('Stopwatch', function() {
     this.clock.tick(this.oneHour); // do not elapse this hour
     sw.start();
     this.clock.tick(this.oneHour);
-    assert.equal(sw.getElapsedTime(), this.oneHour + this.oneHour);
+    assert.equal(sw.getElapsedTime().getTime(), this.oneHour + this.oneHour);
   });
 
   test('pause & elapse 1hr & reset', function() {
@@ -91,6 +78,6 @@ suite('Stopwatch', function() {
     this.clock.tick(this.oneHour);
     sw.reset();
     this.clock.tick(this.oneHour);
-    assert.equal(sw.getElapsedTime(), 0);
+    assert.equal(sw.getElapsedTime().getTime(), 0);
   });
 });
