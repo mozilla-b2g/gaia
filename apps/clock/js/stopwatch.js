8e2cec3ed3eda033b018effb0759dd9a57c2503d
Evelyn Eastmond <evelyn@bocoup.com>
2013-08-30 13:35:06 -0400

    changes to stopwatch api according to Mike's feedback

--- apps/clock/js/stopwatch.js
+++ apps/clock/js/stopwatch.js
@@ -3,10 +3,7 @@
   'use strict';
 
   function Stopwatch() {
-    this._startTime = 0;
-    this._totalElapsed = 0;
-    this._started = false;
-    this._laps = [];
+    this.reset();
   };
 
   Stopwatch.prototype = {
@@ -14,13 +11,14 @@
     constructor: Stopwatch,
 
    /**
-   * start Starts the stopwatch timer, either from a reset or paused state
+   * start Starts the stopwatch, either from a reset or paused state
    */
     start: function sw_start() {
-      if (!this._started) {
-        this._startTime = new Date().getTime();
-        this._started = true;
+      if (this._started) {
+        return;
       }
+      this._startTime = new Date().getTime();
+      this._started = true;
     },
 
     /**
@@ -31,56 +29,58 @@
     */
     getElapsedTime: function sw_elapsed() {
       var elapsed = 0;
-      if (this._started)
+      if (this._started) {
         elapsed = new Date().getTime() - this._startTime;
+      }
       elapsed += this._totalElapsed;
 
       return new Date(elapsed);
     },
 
     /**
-    * pause Pauses the stopwatch timer
+    * pause Pauses the stopwatch
     */
     pause: function sw_pause() {
-      if (this._started) {
-        this._started = false;
-        var elapsed = new Date().getTime() - this._startTime;
-        this._totalElapsed += elapsed;
+      if (!this._started) {
+        return;
       }
+      this._started = false;
+      var elapsed = new Date().getTime() - this._startTime;
+      this._totalElapsed += elapsed;
     },
 
     /**
     * lap Calculates a new lap duration since the last lap time
-    *     If the stopwatch isn't currently running, returns 00:00
+    *     If the stopwatch isn't currently running, returns 0
     *
     * @return {Date} return the lap duration
     */
     lap: function sw_lap() {
-      if (this._started) {
-        var lastLapTime;
-        var newLap = {};
-
-        if (this._laps.length > 0) {
-          lastLapTime = this._laps[this._laps.length - 1].time;
-        } else {
-          lastLapTime = this._startTime;
-        }
-
-        newLap.duration = new Date().getTime() - lastLapTime;
-        newLap.time = new Date().getTime();
-        this._laps.push(newLap);
-
-        return new Date(newLap.duration);
-      } else {
+      if (!this._started) {
         return new Date(0);
       }
+
+      var lastLapTime;
+      var newLap = {};
+
+      if (this._laps.length > 0) {
+        lastLapTime = this._laps[this._laps.length - 1].time;
+      } else {
+        lastLapTime = this._startTime;
+      }
+
+      newLap.duration = new Date().getTime() - lastLapTime;
+      newLap.time = new Date().getTime();
+      this._laps.push(newLap);
+
+      return new Date(newLap.duration);
     },
 
     /**
-    * reset Resets the stopwatch timer back to 00:00, clears laps
+    * reset Resets the stopwatch back to 0, clears laps
     */
     reset: function sw_reset() {
-      this._startTime = new Date().getTime();
+      this._startTime = 0;
       this._totalElapsed = 0;
       this._started = false;
       this._laps = [];
