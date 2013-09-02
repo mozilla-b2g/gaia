66bae2930b1263898da4e8ee5ef49ac2fabdbfd9
Rudy Lu <left.lu@gmail.com>
2013-09-02 16:06:47 +0800

    Bug 911661 - sync up Gaia element naming with Gaia-ui-tests for keyboard frames. r=me
    This reverts commit 2acb6405c5b986a64f4386a18719b2799f5bff6e.

--- apps/system/js/keyboard_manager.js
+++ apps/system/js/keyboard_manager.js
@@ -40,7 +40,7 @@ const TYPE_GROUP_MAPPING = {
 const FOCUS_CHANGE_DELAY = 20;
 
 var KeyboardManager = {
-  keyboardFrameContainer: document.getElementById('keyboard-frame'),
+  keyboardFrameContainer: document.getElementById('keyboards'),
 
   // The set of installed keyboard layouts grouped by type_group.
   // This is a map from type_group to an object arrays.
