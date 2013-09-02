48a328aee49d38325096cae179c756585b8ccdf2
MBRSL <mabolosilugia@gmail.com>
2013-09-02 10:32:23 +0800

    Vibration now can be customize

--- test_apps/uitest/js/vibrator.js
+++ test_apps/uitest/js/vibrator.js
@@ -1,15 +1,32 @@
+durationDisplay = document.getElementById('duration-display');
+timesDisplay = document.getElementById('times-display');
 var clickHandlers = {
   'off': function () {
     window.navigator.vibrate(0);
   },
-  'pattern1': function () {
+  'one-short': function () {
     window.navigator.vibrate(200);
   },
-  'pattern2': function () {
+  'two-short': function () {
     window.navigator.vibrate([200,100,200]);
   },
-  'longTime': function () {
+  'one-long': function () {
     window.navigator.vibrate(10000);
+  },
+  'custom-pattern': function () {
+    var timesNum = parseInt(times.value);
+    var pattern = [];
+    for(i = 0; i < timesNum*2; i++)
+    {
+	pattern.push(parseInt(duration.value));
+    }
+    window.navigator.vibrate(pattern);
+  },
+  'duration': function () {
+    durationDisplay.innerHTML = duration.value;
+  },
+  'times': function () {
+    timesDisplay.innerHTML = times.value;
   }
 };
 
