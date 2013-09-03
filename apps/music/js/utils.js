2a631084ec471c6c8c378718e90ebcb050b42c2c
Archaeopteryx <archaeopteryx@coole-files.de>
2013-09-03 14:14:14 +0200

    Remove unused function escapeHTML.js from music's utils.js

--- apps/music/js/utils.js
+++ apps/music/js/utils.js
@@ -1,14 +1,5 @@
 'use strict';
 
-function escapeHTML(str, escapeQuotes) {
-  var span = document.createElement('span');
-  span.textContent = str;
-
-  if (escapeQuotes)
-    return span.innerHTML.replace(/"/g, '&quot;').replace(/'/g, '&#x27;');
-  return span.innerHTML;
-}
-
 function formatTime(secs) {
   if (isNaN(secs))
     return;
