c5cb222ff13b70ba4a9ffeb832e6c9b3a648866b
Tom Jao <tjao@mozilla.com>
2013-09-03 15:40:01 +0800

    Add 'use strict' to Ringer test

--- test_apps/uitest/js/ringer.js
+++ test_apps/uitest/js/ringer.js
@@ -1,17 +1,18 @@
-var ringer = function callsHandler() {
-  var phoneSoundURL = new SettingsURL();
-  SettingsListener.observe('dialer.ringtone', '', function(value) {
-    ringtonePlayer.pause();
-    ringtonePlayer.src = phoneSoundURL.set(value);
-  });
-  var ringtonePlayer = ringtonePlayer;
-  ringtonePlayer = new Audio();
-  ringtonePlayer.mozAudioChannelType = 'ringer';
-  ringtonePlayer.src = phoneSoundURL.get();
-  ringtonePlayer.loop = false;
+'use strict'
+var phoneSoundURL = new SettingsURL();
+SettingsListener.observe('dialer.ringtone', '', function(value) {
+  ringtonePlayer.pause();
+  ringtonePlayer.src = phoneSoundURL.set(value);
+});
 
-  document.body.addEventListener('click', function (evt) {
-    if (evt.target.id == 'ring')
-      ringtonePlayer.play();
-  });
-}();
+var ringtonePlayer = ringtonePlayer;
+ringtonePlayer = new Audio();
+ringtonePlayer.mozAudioChannelType = 'ringer';
+ringtonePlayer.src = phoneSoundURL.get();
+ringtonePlayer.loop = false;
+
+document.body.addEventListener('click', function (evt) {
+  if (evt.target.id == 'ring')
+    ringtonePlayer.play();
+});
+
