111717c84796934bf2dd3cfa900fb47dce30b54d
Reuben Morais <reuben.morais@gmail.com>
2013-09-07 21:28:44 -0300

    Bug 850430 - Create Date objects when creating a mozContact from JSON.

--- test_apps/uitest/js/contacts_saver.js
+++ test_apps/uitest/js/contacts_saver.js
@@ -10,6 +10,12 @@ function ContactsSaver(data) {
   }
 
   function saveContact(cdata) {
+    if (cdata.bday) {
+      cdata.bday = new Date(cdata.bday);
+    }
+    if (cdata.anniversary) {
+      cdata.anniversary = new Date(cdata.anniversary);
+    }
     var contact = new mozContact(cdata);
     var req = navigator.mozContacts.save(contact);
     req.onsuccess = function(e) {
