f09d7ccc03bd218e391bf57521944a1f9407eb7e
Yuren Ju <yurenju@gmail.com>
2013-09-09 17:18:57 +0200

    Bug 904105- part 2: create firefox extension
    create a firefox extension for building gaia. you can follow instructions to use this extension:
    
    1. cd <GAIA_DIR>/tools/extensions/gaia-build@gaiamobile.org
    2. FIREFOX="<FIREFOX_BIN_PATH>" make run
    3. go to tools -> addons and enable gaia-build extension (default is disable)
    4. go to url: about:gaia
    5. select your gaia dir and click launch gaia
    
    another firefox window will be opened and running gaia.

--- build/xpcshell-commonjs.js
+++ build/xpcshell-commonjs.js
@@ -2,38 +2,7 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 const CC = Components.Constructor;
-
-const systemPrincipal = CC('@mozilla.org/systemprincipal;1', 'nsIPrincipal')();
-
-function loadSandbox(uri) {
-  let proto = {
-    sandboxPrototype: {
-      loadSandbox: loadSandbox,
-      ChromeWorker: ChromeWorker
-    }
-  };
-  let sandbox = Cu.Sandbox(systemPrincipal, proto);
-  // Create a fake commonjs environnement just to enable loading loader.js
-  // correctly
-  sandbox.exports = {};
-  sandbox.module = { uri: uri, exports: sandbox.exports };
-  sandbox.require = function(id) {
-    if (id !== 'chrome')
-      throw new Error('Bootstrap sandbox `require` method isn\'t implemented.');
-
-    return Object.freeze({ Cc: Cc, Ci: Ci, Cu: Cu, Cr: Cr, Cm: Cm,
-      CC: bind(CC, Components), components: Components,
-      ChromeWorker: ChromeWorker });
-  };
-  scriptLoader.loadSubScript(uri, sandbox, 'UTF-8');
-  return sandbox;
-}
-
-const scriptLoader = Cc['@mozilla.org/moz/jssubscript-loader;1'].
-                     getService(Ci.mozIJSSubScriptLoader);
 const loaderURI = 'resource://gre/modules/commonjs/toolkit/loader.js';
-const loaderSandbox = loadSandbox(loaderURI);
-const loaderModule = loaderSandbox.exports;
 
 var { Loader } = Components.utils.import(loaderURI, {});
 var loader = Loader.Loader({
@@ -43,13 +12,7 @@ var loader = Loader.Loader({
     '': GAIA_BUILD_DIR
   },
   modules: {
-    'toolkit/loader': loaderModule
-  },
-  resolve: function(id, base) {
-    if (id == 'chrome' || id.startsWith('@')) {
-      return id;
-    }
-    return Loader.resolve(id, base);
+    'toolkit/loader': Loader
   }
 });
 
