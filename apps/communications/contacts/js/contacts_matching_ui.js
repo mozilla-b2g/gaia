6492214e0044633db949a5f748b3c7b99c333ed0
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-09-13 12:41:55 +0200

    Bug 902399 In the Contacts duplicates screen allow to show a summary of the matching info

--- apps/communications/contacts/js/contacts_matching_ui.js
+++ apps/communications/contacts/js/contacts_matching_ui.js
@@ -16,6 +16,8 @@ if (!contacts.MatchingUI) {
     var checkedContacts = {};
 
     var mergeButton, contactsList, duplicateMessage, title;
+    var matchingResults;
+    var matchingDetails, matchingDetailList, matchingImg, matchingTitle;
 
     function init() {
       mergeButton = document.getElementById('merge-action');
@@ -30,11 +32,22 @@ if (!contacts.MatchingUI) {
       document.getElementById('merge-close').addEventListener('click', onClose);
       contactsList.addEventListener('click', onClick);
       mergeButton.addEventListener('click', onMerge);
+
+      matchingDetails = document.querySelector('#matching-details');
+      matchingDetailList = matchingDetails.querySelector('#matching-list');
+      matchingImg = matchingDetails.querySelector('img');
+      matchingTitle = matchingDetails.querySelector('h1');
+
+      matchingDetails.querySelector('button').onclick = function() {
+        hideMatchingDetails();
+      };
     }
 
     function load(type, contact, results, cb) {
+      matchingResults = results;
+
       document.body.dataset.mode = type;
-      var params = { name: getCompleteName(getDisplayName(contact)) };
+      var params = { name: getDisplayName(contact) };
 
       if (type === 'matching') {
         // "Suggested duplicate contacts for xxx"
@@ -93,7 +106,7 @@ if (!contacts.MatchingUI) {
       populate(contact, out,
                     Object.getOwnPropertyNames(Object.getPrototypeOf(contact)));
 
-      out.displayName = getCompleteName(getDisplayName(contact));
+      out.displayName = getDisplayName(contact);
       out.mainReason = selectMainReason(reasons);
       if (Array.isArray(out.photo) && out.photo[0]) {
         out.thumb = window.URL.createObjectURL(out.photo[0]);
@@ -118,23 +131,25 @@ if (!contacts.MatchingUI) {
 
     // Fills the contact data to display if no givenName and familyName
     function getDisplayName(contact) {
-      if (hasName(contact))
-        return { givenName: contact.givenName, familyName: contact.familyName };
-
-      var givenName = [];
-      if (Array.isArray(contact.name) && contact.name.length > 0) {
-        givenName.push(contact.name[0]);
-      } else if (contact.org && contact.org.length > 0) {
-        givenName.push(contact.org[0]);
-      } else if (contact.tel && contact.tel.length > 0) {
-        givenName.push(contact.tel[0].value);
-      } else if (contact.email && contact.email.length > 0) {
-        givenName.push(contact.email[0].value);
-      } else {
-        givenName.push(_('noName'));
+      if (hasName(contact)) {
+        return getCompleteName(contact);
       }
 
-      return { givenName: givenName, modified: true };
+      var name = [];
+      if (Array.isArray(contact.name) && contact.name[0] &&
+          contact.name[0].trim()) {
+        name.push(contact.name[0]);
+      } else if (contact.org && contact.org[0] && contact.org[0].trim()) {
+        name.push(contact.org[0]);
+      } else if (contact.tel && contact.tel[0]) {
+        name.push(contact.tel[0].value);
+      } else if (contact.email && contact.email[0]) {
+        name.push(contact.email[0].value);
+      } else {
+        name.push(_('noName'));
+      }
+
+      return name[0];
     };
 
     function hasName(contact) {
@@ -173,15 +188,134 @@ if (!contacts.MatchingUI) {
       }, CONTACTS_APP_ORIGIN);
     }
 
+    // Obtains the action from the contacts list from the click coordinates
+    // The action can be: 'check' or 'detail'
+    // If it is 'check' the input check will be toggled
+    // If it is 'detail' the matching contact details overlay will be show
+    function getActionOverList(event) {
+      // 40% percent of the horizontal width will be consider 'check' area
+      var CHECKING_AREA_WIDTH = 0.4;
+
+      var out = 'detail';
+      if (event.clientX <= window.innerWidth * CHECKING_AREA_WIDTH) {
+        out = 'check';
+      }
+
+      return out;
+    }
+
     function onClick(e) {
       var target = e.target;
 
+      var uuid;
       if (target && target.dataset.uuid) {
-        var uuid = target.dataset.uuid;
+        uuid = target.dataset.uuid;
+      }
+
+      var targetAction = getActionOverList(e);
+      if (targetAction === 'check') {
         var checkbox = target.querySelector('input[type="checkbox"]');
         setChecked(target, checkbox, !checkbox.checked, uuid);
         checkMergeButton();
       }
+      else if (uuid) {
+        showMatchingDetails();
+        renderMatchingDetails(uuid);
+      }
+    }
+
+    function resetContentDetails() {
+      if (matchingImg.src) {
+        window.URL.revokeObjectURL(matchingImg.src);
+      }
+      matchingImg.src = '';
+      matchingImg.alt = '';
+      matchingTitle.textContent = '';
+      matchingDetailList.innerHTML = '';
+    }
+
+    function hideMatchingDetails() {
+      matchingDetails.classList.remove('fade-in');
+      matchingDetails.classList.add('fade-out');
+
+      matchingDetails.addEventListener('animationend', function cd_fadeOut(ev) {
+        matchingDetails.removeEventListener('animationend', cd_fadeOut);
+        matchingDetails.classList.add('no-opacity');
+        matchingDetails.classList.add('hide');
+
+        resetContentDetails();
+      });
+    }
+
+    function showMatchingDetails() {
+      matchingDetails.classList.remove('hide');
+      matchingDetails.classList.remove('fade-out');
+      matchingDetails.classList.add('fade-in');
+
+      matchingDetails.addEventListener('animationend', function cd_fadeIn(ev) {
+        matchingDetails.removeEventListener('animationend', cd_fadeIn);
+        matchingDetails.classList.remove('no-opacity');
+      });
+    }
+
+    function renderMatchingDetails(uuid) {
+      var fields = ['org', 'name', 'tel', 'email', 'adr', 'photo'];
+
+      var theContact = matchingResults[uuid].matchingContact;
+      var matchings = matchingResults[uuid].matchings;
+      if (!hasName(theContact)) {
+        theContact.name = [getDisplayName(theContact)];
+      }
+      fields.forEach(function(aField) {
+        if (!Array.isArray(theContact[aField]) || !theContact[aField][0]) {
+          return;
+        }
+
+        theContact[aField].forEach(function(fieldValue) {
+          if (!fieldValue) {
+            return;
+          }
+          var item = document.createElement('li');
+
+          if (matchings[aField]) {
+            matchings[aField].forEach(function(obj) {
+              var val = fieldValue.value || fieldValue;
+              if (obj.matchedValue === val) {
+                item.setAttribute('aria-selected', 'true');
+              }
+            });
+          }
+          switch (aField) {
+            case 'photo':
+              matchingImg.src = window.URL.createObjectURL(fieldValue);
+              matchingImg.alt = getDisplayName(theContact);
+            break;
+            case 'name':
+              matchingTitle.textContent = fieldValue;
+              if (hasName(theContact)) {
+                item.textContent = fieldValue;
+              }
+            break;
+            case 'tel':
+              item.textContent = fieldValue.type + ', ' + fieldValue.value;
+            break;
+            case 'adr':
+              var adrFields = ['streetAddress', 'locality',
+                               'region', 'countryName'];
+              adrFields.forEach(function(addrField) {
+                if (fieldValue[addrField]) {
+                  var p = document.createElement('p');
+                  p.textContent = fieldValue[addrField];
+                  item.appendChild(p);
+                }
+              });
+            break;
+            default:
+               item.textContent = fieldValue.value || fieldValue || '';
+          }
+          matchingDetailList.appendChild(item);
+        });
+      });
     }
 
     function checkMergeButton() {
