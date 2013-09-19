925c91dfd92859a3701fb5cc6e38053aaabeb79d
Albert <acperez@tid.es>
2013-09-19 19:01:10 +0200

    Bug 917740 - Update file format of customization files based on MCC/MNC

--- apps/communications/ftu/test/unit/variant_test.js
+++ apps/communications/ftu/test/unit/variant_test.js
@@ -1,56 +1,97 @@
 'use strict';
 
-requireApp('communications/ftu/test/unit/mock_icc_helper.js');
-requireApp('communications/ftu/test/unit/mock_navigator_moz_settings.js');
-requireApp('communications/ftu/test/unit/mock_xml_http_request.js');
 requireApp('communications/ftu/js/variant.js');
 
-var mocksHelperForVariant = new MocksHelper(['IccHelper', 'XMLHttpRequest']);
-mocksHelperForVariant.init();
+var resourcesDir = '/ftu/test/unit';
+var customizationFilePath = '/resources/customization.json';
+var reference_MNC_MCC = '214-007';
+var sample_reference_params, customizationFullPath;
 
 suite('variant >', function() {
-  var realSettings;
-  var mocksHelper = mocksHelperForVariant;
 
-  suiteSetup(function() {
-    realSettings = navigator.mozSettings;
-    navigator.mozSettings = MockNavigatorSettings;
-    mocksHelper.suiteSetup();
-    VariantManager._variantCustomization = null;
+  suiteSetup(function(done) {
+    customizationFullPath = resourcesDir + customizationFilePath;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', customizationFullPath, true);
+    xhr.overrideMimeType('application/json');
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        sample_reference_params = xhr.response[reference_MNC_MCC];
+        done();
+      } else {
+        console.error('Failed to fetch file: ' +
+          customizationFullPath, xhr.statusText);
+      }
+    };
+    try {
+      xhr.send();
+    } catch (e) {
+      console.error('Failed to fetch file: ' +
+        customizationFullPath);
+    }
   });
 
-  suiteTeardown(function() {
-    navigator.mozSettings = realSettings;
-    realSettings = null;
-    mocksHelper.suiteTeardown();
-  });
-
-   test('getVariantSettings no json', function() {
-    VariantManager.mcc_mnc = '220-033';
-    VariantManager.getVariantSettings();
-    assert.isNull(VariantManager._variantCustomization);
-  });
-
-  test('loads variant customizations (no Data)', function(done) {
-    var data = {};
-    VariantManager.mcc_mnc = '214-007';
-    VariantManager.getVariantSettings(function() {
-      assert.isNotNull(VariantManager._variantCustomization);
-      assert.isUndefined(VariantManager._variantCustomization['wallpaper']);
-      done();
+  suite(' customization.json >', function() {
+    setup(function() {
+      VariantManager.mcc_mnc = reference_MNC_MCC;
     });
-    MockXMLHttpRequest.mSendOnLoad({ response: data });
-  });
 
-  test('loads variant customizations from file OK', function(done) {
-    var data = {'wallpaper' : 'data:image/jpeg;base64,/9j/4AAQSkZJ'};
-    VariantManager.mcc_mnc = '214-007';
-    VariantManager.getVariantSettings(function() {
-      assert.isNotNull(VariantManager._variantCustomization);
-      assert.equal(VariantManager._variantCustomization['wallpaper'],
-        'data:image/jpeg;base64,/9j/4AAQSkZJ');
-      done();
+    teardown(function() {
+      VariantManager.mcc_mnc = null;
     });
-    MockXMLHttpRequest.mSendOnLoad({ response: data });
+
+    test(' load customization.json file & Check structure', function(done) {
+      VariantManager.readJSONFile(customizationFullPath,
+        function(customizations) {
+        var customization = customizations[reference_MNC_MCC];
+        // Check that the customization exists
+        assert.isFalse(!customization);
+        // Check that all params are reference ones
+        for (var param in customization) {
+          assert.isTrue(sample_reference_params.hasOwnProperty(param));
+        }
+        done();
+      });
+    });
+
+    test(' load customization.json file error.', function(done) {
+      var wrongFullPath = resourcesDir + 'wrong_path.json';
+      var onsuccess = function() {};
+      VariantManager.readJSONFile(wrongFullPath, onsuccess,
+        function onerror() {
+        done();
+      });
+    });
+
+    test(' check values in the structure.', function(done) {
+      VariantManager.readJSONFile(customizationFullPath,
+        function(customizations) {
+        var customization = customizations[reference_MNC_MCC];
+        // Check that all params are reference ones
+        for (var param in customization) {
+          assert.equal(sample_reference_params[param], customization[param]);
+        }
+        done();
+      });
+    });
+
+    test(' check customization events', function() {
+      window.addEventListener('customization',
+        function customizationListener(event) {
+        window.removeEventListener('customization', customizationListener);
+        assert.equal(event.detail.setting,
+          Object.keys(sample_reference_params)[0]);
+        assert.equal(event.detail.valye,
+          sample_reference_params[event.detail.setting]);
+        done();
+      });
+      VariantManager.readJSONFile(customizationFullPath,
+        function(customizations) {
+        var customization = customizations[reference_MNC_MCC];
+        // Dispatch events
+        VariantManager.dispatchCustomizationEvents(customization);
+      });
+    });
   });
 });
