db10777ecbf2849a998382b0689a74bc0f9ba8b9
Eitan Isaacson <eitan@monotonous.org>
2013-09-24 21:07:52 -0700

    Bug 987389 - [Clock] Make time picker for timer accessible.

--- apps/clock/js/picker/spinner.js
+++ apps/clock/js/picker/spinner.js
@@ -75,6 +75,9 @@ define(function(require) {
       },
       length: {
         value: setup.values.length
+      },
+      textValues: {
+        value: setup.textValues || setup.values
       }
     });
 
@@ -103,6 +106,11 @@ define(function(require) {
     this.container.addEventListener('touchstart', this, false);
     this.container.addEventListener('pan', this, false);
     this.container.addEventListener('swipe', this, false);
+    this.container.addEventListener('keypress', this, false);
+    this.container.setAttribute('aria-valuemax', this.upper);
+    this.container.setAttribute('aria-valuemin', this.lower);
+    this.container.setAttribute('aria-valuenow', this.index);
+    this.container.setAttribute('aria-valuetext', this.textValues[this.index]);
 
     this.reset();
 
@@ -118,6 +126,8 @@ define(function(require) {
   };
 
   Spinner.prototype.update = function() {
+    this.container.setAttribute('aria-valuenow', this.index);
+    this.container.setAttribute('aria-valuetext', this.textValues[this.index]);
     this.element.style.transform = 'translateY(' + this.top + 'px)';
   };
 
@@ -221,6 +231,15 @@ define(function(require) {
 
     this.stopInteraction();
 
+  };
+
+  Spinner.prototype.onkeypress = function(event) {
+    this.element.classList.add('animation-on');
+    if (event.keyCode == KeyEvent.DOM_VK_DOWN) {
+      this.select(this.index - 1);
+    } else {
+      this.select(this.index + 1);
+    }
   };
 
   return Spinner;
