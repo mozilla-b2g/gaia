fb012f8a7201d6f6dc5095f910d8c01672fff1d0
John Hu <johu@mozilla.com>
2013-09-30 11:34:31 +0800

    Bug 921421 - [Video] pause the video and back to beginning when a video plays to end

--- apps/video/js/video.js
+++ apps/video/js/video.js
@@ -723,7 +723,7 @@ function showPlayer(video, autoPlay) {
   });
 }
 
-function hidePlayer(updateMetadata) {
+function hidePlayer(updateVideoMetadata) {
   if (!playerShowing)
     return;
 
@@ -754,7 +754,7 @@ function hidePlayer(updateMetadata) {
     startParsingMetadata();
   }
 
-  if (!('metadata' in currentVideo) || !updateMetadata || pendingPick) {
+  if (!('metadata' in currentVideo) || !updateVideoMetadata || pendingPick) {
     completeHidingPlayer();
     return;
   }
@@ -816,11 +816,7 @@ function playerEnded() {
 
   dom.player.currentTime = 0;
 
-  if (pendingPick) {
-    pause();
-  } else {
-    hidePlayer(true);
-  }
+  pause();
 }
 
 function play() {
