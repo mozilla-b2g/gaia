703376260162dbc8173cee2ab5e72405661fb84e
Teodosia Pop <teodosia.pop@gmail.com>
2013-09-30 19:12:11 +0300

    Bug 922139 - Update lockscreen tests and app object to address to the new design on aurora

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/lockscreen/test_lockscreen_unlock_to_homescreen_with_passcode.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/lockscreen/test_lockscreen_unlock_to_homescreen_with_passcode.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette.by import By
 from gaiatest import GaiaTestCase
 from gaiatest.apps.lockscreen.app import LockScreen
 
@@ -10,10 +9,6 @@ from gaiatest.apps.lockscreen.app import LockScreen
 class TestLockScreen(GaiaTestCase):
 
     _input_passcode = '7931'
-
-    # Homescreen locators
-    _homescreen_frame_locator = (By.CSS_SELECTOR, 'div.homescreen iframe')
-    _homescreen_landing_locator = (By.ID, 'landing-page')
 
     def setUp(self):
         GaiaTestCase.setUp(self)
@@ -32,16 +27,9 @@ class TestLockScreen(GaiaTestCase):
 
         https://github.com/mozilla/gaia-ui-tests/issues/478
         """
-        self.lock_screen.swipe_to_unlock()
-        self.lock_screen.tap_unlock_button()
+        homescreen = self.lock_screen.unlock()
         self.lock_screen.passcode_pad.type_passcode(self._input_passcode)
-
         self.lock_screen.wait_for_lockscreen_not_visible()
 
-        hs_frame = self.marionette.find_element(*self._homescreen_frame_locator)
-        # TODO I would prefer to check visibility of the the frame at this point but bug 813583
-        self.marionette.switch_to_frame(hs_frame)
-
-        # Instead, check the main element of the landing screen
-        landing_element = self.marionette.find_element(*self._homescreen_landing_locator)
-        self.assertTrue(landing_element.is_displayed(), 'Landing element not displayed after unlocking')
+        homescreen.switch_to_homescreen_frame()
+        homescreen.wait_for_landing_page_visible()
