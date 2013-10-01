c1758342b21f68ba467b796f0bfb64695a7eb6eb
Albert <acperez@tid.es>
2013-10-01 12:12:18 +0200

    Bug 922560 - Fix typo error. r=yurenju

--- build/webapp-zip.js
+++ build/webapp-zip.js
@@ -303,7 +303,7 @@ function execute(options) {
       if (conf.exists()) {
         let resources = getSingleVariantResources(conf);
 
-        addEntryStringWithTime(zip, 'resources/costumization.json', JSON.stringify(resources.conf), DEFAULT_TIME);
+        addEntryStringWithTime(zip, 'resources/customization.json', JSON.stringify(resources.conf), DEFAULT_TIME);
 
         resources.files.forEach(function(file) {
           let filename = 'resources/' + file.leafName;
