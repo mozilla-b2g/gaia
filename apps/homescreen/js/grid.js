1c2a4942f39c056868d4247c21ddae8e53d0d660
cctuan <georgeiscoming@gmail.com>
2013-10-02 17:52:26 +0800

    Revert "Bug 922462 - Define various size of icons in manifest instead of use buildscript"
    This reverts commit 32153515a3eedad26aec89322e02934a0dc06a0f.

--- apps/homescreen/js/grid.js
+++ apps/homescreen/js/grid.js
@@ -4,11 +4,7 @@ var GridManager = (function() {
   // Be aware that the current manifest icon description syntax does
   // not distinguish between 60@1.5x and 90@1x, so we would have to use
   // the latter as the former.
-
-  // use 90px icons for tablet
-  var notTinyLayout = !ScreenLayout.getCurrentLayout('tiny');
-  var PREFERRED_ICON_SIZE =
-      (notTinyLayout ? 90 : 60) * (window.devicePixelRatio || 1);
+  var PREFERRED_ICON_SIZE = 60 * (window.devicePixelRatio || 1);
 
   var SAVE_STATE_TIMEOUT = 100;
   var BASE_HEIGHT = 460; // 480 - 20 (status bar height)
@@ -60,7 +56,7 @@ var GridManager = (function() {
   }
 
   // tablet+ devices are stricted to 5 x 3 grid
-  if (notTinyLayout) {
+  if (ScreenLayout.getCurrentLayout() !== 'tiny') {
     MAX_ICONS_PER_PAGE = 5 * 3;
   }
 
