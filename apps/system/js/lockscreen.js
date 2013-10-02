7663d744789f343766f505474444b669a7bba98c
Greg Weng <snowmantw@gmail.com>
2013-10-02 01:36:50 -0700

    Merge pull request #12600 from snowmantw/issue921335
    [LockScreen] Fix the overlay blue area of the track(cherry picked from commit 272c7f1c60e3a4ef77279fc0c2c68c29ee2cbaa5)

--- apps/system/js/lockscreen.js
+++ apps/system/js/lockscreen.js
@@ -556,6 +556,13 @@ var LockScreen = {
     // Shrink the boundaries.
     var slideBoundaryOffset = this.leftIcon.clientWidth;
 
+    // They're different because the center element of the circle
+    // occurs 1px width toward right.
+    var boundaryBorderRight = 5;
+    var boundaryBorderLeft = 4;
+    var boundaryBorder = 'left' === dir ? boundaryBorderLeft :
+      boundaryBorderRight;
+
     // Boundaries will shorter than the track length,
     // to make unlocking with one hand easier.
     var maxLength = natureBoundary -
@@ -563,7 +570,7 @@ var LockScreen = {
                     Math.floor(this.leftIcon.clientWidth / 2);
 
     var offset = utx;
-    var slideLength = offset + this.slideLeft.clientWidth;
+    var slideLength = offset + this.slideLeft.clientWidth + boundaryBorder;
 
     // Start to paint the slide.
     this.slideLeft.classList.add('pulling');
@@ -574,7 +581,7 @@ var LockScreen = {
     // and only the handle and the blue occurred area should be adjusted.
     if (slideLength > maxLength) {
       this._slideReachEnd = true;
-      offset = natureBoundary - this.slideLeft.clientWidth;
+      offset = natureBoundary - this.slideLeft.clientWidth - boundaryBorder;
       this._slideTo(offset, dir, true);
     } else {
       this._slideTo(offset, dir);
