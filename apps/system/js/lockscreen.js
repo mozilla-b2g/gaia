13c77362cfa7c137fe981de75836f52dd3e8c062
Greg Weng <snowmantw@gmail.com>
2013-10-02 20:37:34 -0700

    Merge pull request #12610 from snowmantw/issue921392
    Bug 921392 - [LockScreen] Need to dark the icons of locksceen(cherry picked from commit 529a4d741df4867eaa2adf0aaeef0fb5015752b3)

--- apps/system/js/lockscreen.js
+++ apps/system/js/lockscreen.js
@@ -314,7 +314,7 @@ var LockScreen = {
     this.leftIcon.classList.remove('dark');
   },
 
-  darkIcon: function() {
+  darkIcons: function() {
     this.rightIcon.classList.add('dark');
     this.leftIcon.classList.add('dark');
   },
@@ -601,11 +601,10 @@ var LockScreen = {
       this.slideCenter.classList.add('touched');
       this.slideRight.classList.add('touched');
     }
-    this.darkIcon();
     this._slideCount = 0;
   },
 
-  // Restore all slide elements.
+  // Restore all elements in the slide.
   //
   // easing {Boolean} true|undefined to bounce back slowly.
   restoreSlide: function(easing) {
@@ -658,7 +657,7 @@ var LockScreen = {
       this.handleIconClick('left' === this._slidingToward ?
         this.leftIcon : this.rightIcon);
     }
-    this.darkIcon();
+    this.darkIcons();
     this._slideCount = 0;
     this._sliding = false;
   },
