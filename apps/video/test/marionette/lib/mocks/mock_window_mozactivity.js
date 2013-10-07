9146a2b02a081bf3f491cd1d4d366cbcf7906b1a
Punam Dahiya <punamdahiya@yahoo.com>
2013-10-07 11:32:30 -0700

    Bug924170 - [Video] - Add Marionette tests for Video App r=jugglinmike

---
+++ apps/video/test/marionette/lib/mocks/mock_window_mozactivity.js
@@ -0,0 +1,21 @@
+Components.utils.import('resource://gre/modules/Services.jsm');
+
+Services.obs.addObserver(function(document) {
+  if (!document || !document.location)
+    return;
+
+  var window = document.defaultView.wrappedJSObject;
+  var mozActivityData = {};
+
+  window.__defineGetter__('MozActivity', function() {
+    return function fakeMozActivity(data) {
+      mozActivityData = data;
+    }
+  });
+
+  window.__defineGetter__('getMozActivityData', function() {
+    return function getMozActivityData() {
+      return mozActivityData;
+    }
+  });
+}, 'document-element-inserted', false);
