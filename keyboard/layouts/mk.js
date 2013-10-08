27b7d62b1a6b5012ab74fa006f1935aa203603cd
Damjan Georgievski <gdamjan@gmail.com>
2013-10-08 18:49:03 +0200

    Adding Macedonian keyboard layout
    backport of 86306ac2fc from master

---
+++ keyboard/layouts/mk.js
@@ -0,0 +1,30 @@
+Keyboards.mk = {
+  label: 'Macedonian',
+  menuLabel: 'Македонски',
+  imEngine: 'latin',
+  autoCorrectLanguage: 'mk',
+  alt: {
+    'е': 'ѐ',
+    'и': 'ѝ'
+  },
+  width: 11,
+  keys: [
+    [
+      { value: 'љ' }, { value: 'њ' }, { value: 'е' }, { value: 'р' },
+      { value: 'т' }, { value: 'ѕ' }, { value: 'у' }, { value: 'и' },
+      { value: 'о' }, { value: 'п' }, { value: 'ш' }
+    ], [
+      { value: 'а' }, { value: 'с' }, { value: 'д' }, { value: 'ф' },
+      { value: 'г' }, { value: 'х' }, { value: 'ј' }, { value: 'к' },
+      { value: 'л' }, { value: 'ч' }, { value: 'ќ' }
+    ], [
+      { value: '⇪', keyCode: KeyEvent.DOM_VK_CAPS_LOCK }, { value: 'з' },
+      { value: 'џ' }, { value: 'ц' }, { value: 'в' }, { value: 'б' },
+      { value: 'н' }, { value: 'м' }, { value: 'ѓ' }, { value: 'ж' },
+      { value: '⌫', keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+    ], [
+      { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+    ]
+  ]
+};
