d15061047a4035e3fef2f6a6b6e66c7d373ea7d1
Germán Toro del Valle <gtorodelvalle@gmail.com>
2013-10-10 07:58:44 -0700

    Merge pull request #12767 from jmcanterafonseca/fix_924400
    Bug 924400 - A no name contact imported from the SIM card does not allow...(cherry picked from commit e2ca5da33ff6ac022fd7628c3870ed6ae8c481ce)

--- apps/communications/contacts/js/contacts_matcher.js
+++ apps/communications/contacts/js/contacts_matcher.js
@@ -363,7 +363,7 @@ contacts.Matcher = (function() {
 
     var finalResult = {};
 
-    var resultsByName = null;
+    var resultsByName = [];
     if (!isEmptyStr(aContact.name)) {
       var targetName = aContact.name[0].trim();
       // Filter by familyName using startsWith. Gecko 'startsWith' operation
@@ -379,27 +379,22 @@ contacts.Matcher = (function() {
           return filterFacebook(aResult, options);
         });
         notifyFindNameReady();
-        if (isEmptyStr(aContact.familyName)) {
-          endOfMatchByName(finalResult, targetName, resultsByName, callbacks);
-        }
       };
 
       reqName.onerror = function(e) {
         window.console.warn('Error while trying to find by name: ',
                                 e.target.error.name);
-        resultsByName = [];
         notifyFindNameReady();
-        if (isEmptyStr(aContact.familyName)) {
-          endOfMatchByName(finalResult, targetName, resultsByName, callbacks);
-        }
       };
     }
     else {
-      resultsByName = [];
       notifyFindNameReady();
     }
 
-    if (!isEmptyStr(aContact.familyName)) {
+    if (isEmptyStr(aContact.familyName)) {
+      endOfMatchByName(finalResult, targetName, resultsByName, callbacks);
+    }
+    else {
       var targetFamilyName = aContact.familyName[0].trim();
       // Filter by familyName using startsWith. Gecko 'startsWith' operation
       // acts as 'equal' but does not match case.
