30d9ee1af2a5c9ca08b51ba60569f05465c01ddb
Mike Pennisi <mike@mikepennisi.com>
2013-10-16 17:59:09 -0400

    Increase timeout aggressively

--- apps/clock/test/unit/alarm_list_test.js
+++ apps/clock/test/unit/alarm_list_test.js
@@ -4,7 +4,7 @@ suite('AlarmList', function() {
 
   suiteSetup(function(done) {
     // Account for potentially-slow file loading operations
-    this.timeout(7000);
+    this.timeout(60000);
 
     testRequire([
         'panels/alarm/main',
