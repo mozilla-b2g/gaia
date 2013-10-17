a3fe468827119a8b836d3686f5089f323ee7bbd9
Mike Pennisi <mike@mikepennisi.com>
2013-10-17 19:54:20 -0400

    Attempt to isolate test failure

---
+++ apps/clock/test/unit/debug_test.js
@@ -0,0 +1,21 @@
+suite.only('DEBUG', function() {
+  suiteSetup(function(done) {
+    // Account for potentially-slow file loading operations
+    this.timeout(10000);
+
+    testRequire([
+        'panels/alarm/main',
+        'panels/alarm/alarm_list',
+        'alarm',
+        'mocks/mock_moz_alarm',
+        'mocks/mock_navigator_mozl10n'
+      ], {
+      },
+      function(AlarmPanel, alarmList, alarm, mockMozAlarms, mockL10n) {
+        done();
+      }
+    );
+  });
+
+  test('ensure that the suiteSetup is invoked', function() {});
+});
