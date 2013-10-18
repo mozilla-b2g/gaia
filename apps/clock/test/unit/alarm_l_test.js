84db8e02b218eb14d6c2543a23f5158aa4bc3556
Mike Pennisi <mike@mikepennisi.com>
2013-10-18 10:00:13 -0400

    Force error synchronously
    Determine if Mocha is capable of catching the error raised by this
    method invocation.

--- apps/clock/test/unit/alarm_l_test.js
+++ apps/clock/test/unit/alarm_l_test.js
@@ -2,6 +2,14 @@ suite.only('AlarmList debug', function() {
   var AlarmList;
 
   suiteSetup(function(done) {
+    navigator.mozSetMessageHandler('alarm', function() {});
+
+    // Throwing errors is the only consistent way to print information to the
+    // screen during a test run on TravisCI, so throw one here to ensure that
+    // the previous statement did indeed throw an error, even if Mocha does not
+    // report it.
+    throw new Error('Method invocation did not raise an exception.');
+
     // Account for potentially-slow file loading operations
     this.timeout(10000);
 
