c584b9ae7436c7cd134b568e17322fad0812f028
Mike Pennisi <mike@mikepennisi.com>
2013-10-18 18:37:13 -0400

    Revert "Bug 907177 - [Clock] Add integration tests"
    This reverts commit cd0e6bba0a258ab9a9215d274bcc339d07ca9ab3. It appears
    that a platform bug is causing a failure in this test suite.

--- shared/test/integration/setup.js
+++ shared/test/integration/setup.js
@@ -2,4 +2,3 @@
 marionette.plugin('apps', require('marionette-apps'));
 marionette.plugin('helper', require('marionette-helper'));
 marionette.plugin('contentScript', require('marionette-content-script'));
-marionette.plugin('forms', require('marionette-plugin-forms'));
