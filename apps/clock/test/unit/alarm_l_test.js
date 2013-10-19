46cce8e284424ff47f0a2681c087bb18977e06a3
Mike Pennisi <mike@mikepennisi.com>
2013-10-19 19:14:26 -0400

    Simplify codepath to asynchronous error

--- apps/clock/test/unit/alarm_l_test.js
+++ apps/clock/test/unit/alarm_l_test.js
@@ -1,6 +1,6 @@
 suite.only('AlarmList debug', function() {
   suiteSetup(function(done) {
-    requirejs([],
+    setTimeout(
       function() {
         // Throwing errors is the only consistent way to print information to
         // the screen during a test run on TravisCI, so throw one here to
@@ -8,7 +8,8 @@ suite.only('AlarmList debug', function() {
         // if Mocha does not report it.
         throw new Error('Method invocation did not raise an exception.');
         done();
-      }
+      },
+      3000
     );
 
     // Account for potentially-slow file loading operations
