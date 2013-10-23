88ed55227a0c0a312a96f22b18bb4e9ccafdda5f
Viorela Ioia <viorelaioia@gmail.com>
2013-10-23 10:05:29 +0300

    Bug 928414 - [v1.2] Write a test to use the 'Number' keyboard on aurora

---
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/keyboard/test_number_keyboard.py
@@ -0,0 +1,25 @@
+# -*- coding: iso-8859-15 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest import GaiaTestCase
+from gaiatest.apps.ui_tests.app import UiTests
+
+
+class TestNumberKeyboard(GaiaTestCase):
+
+    def test_number_keyboard(self):
+        self.ui_tests = UiTests(self.marionette)
+        self.ui_tests.launch()
+        self.ui_tests.tap_keyboard_option()
+        keyboard_page = self.ui_tests.switch_to_keyboard_page_frame()
+        keyboard = keyboard_page.tap_number_input()
+        keyboard.switch_to_keyboard()
+        self.assertEqual(str(keyboard.current_keyboard), 'number')
+        keyboard._tap('1')
+        self.marionette.switch_to_frame()
+        self.marionette.switch_to_frame(self.ui_tests.app.frame)
+        keyboard_page = self.ui_tests.switch_to_keyboard_page_frame()
+        typed_number = keyboard_page.number_input
+        self.assertEqual(typed_number, u'1')
