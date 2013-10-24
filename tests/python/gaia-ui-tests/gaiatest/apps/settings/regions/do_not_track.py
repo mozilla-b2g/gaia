72e243cc4f1a485fd846b9a4f0776f971d925d8c
Timothy Guan-tin Chien <timdream@gmail.com>
2013-10-24 03:07:58 -0700

    Merge pull request #13054 from timdream/reverse-dnt-value
    Bug 914407 - Correct the Do Not Track header values, r=arthurcc(cherry picked from commit ff127af9470ac575432404e8e70da8ac5858e4d8)

--- tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/do_not_track.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/do_not_track.py
@@ -9,7 +9,8 @@ from gaiatest.apps.base import Base
 class DoNotTrack(Base):
 
     _allow_tracking_checkbox_locator = (By.XPATH, '//li[p[@data-l10n-id="allowTracking"]]/label')
-    _do_not_track_checkbox_locator = (By.XPATH, '//li[p[@data-l10n-id="doNotHavePrefOnTracking"]]/label')
+    _disallow_tracking_checkbox_locator = (By.XPATH, '//li[p[@data-l10n-id="doNotTrackActions"]]/label')
+    _do_not_have_pref_on_tracking_checkbox_locator = (By.XPATH, '//li[p[@data-l10n-id="doNotHavePrefOnTracking"]]/label')
 
     def tap_allow_tracking(self):
         el = self.marionette.find_element(*self._allow_tracking_checkbox_locator)
@@ -17,8 +18,14 @@ class DoNotTrack(Base):
         el.tap()
         self.wait_for_condition(lambda m: el.get_attribute('checked') is not checked)
 
-    def tap_do_not_track(self):
-        el = self.marionette.find_element(*self._do_not_track_checkbox_locator)
+    def tap_disallow_tracking(self):
+        el = self.marionette.find_element(*self._disallow_tracking_checkbox_locator)
+        checked = el.get_attribute('checked')
+        el.tap()
+        self.wait_for_condition(lambda m: el.get_attribute('checked') is not checked)
+
+    def tap_do_not_have_pref_on_tracking(self):
+        el = self.marionette.find_element(*self._do_not_have_pref_on_tracking_checkbox_locator)
         checked = el.get_attribute('checked')
         el.tap()
         self.wait_for_condition(lambda m: el.get_attribute('checked') is not checked)
