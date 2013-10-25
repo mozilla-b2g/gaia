672963cda418369a54d39e12ad34c6ac3bb71e33
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2013-10-25 03:36:12 -0700

    Merge pull request #13087 from jaoo/926455
    Bug 926455 - Cannot enable data connection when updating from 1.1 to 1.2 via FOTA. r=timdream(cherry picked from commit 15c1089aea0490b9b73bc915b1495b3faa1b023c)

--- apps/system/test/unit/operator_variant_test.js
+++ apps/system/test/unit/operator_variant_test.js
@@ -9,36 +9,18 @@ var mocksForOperatorVariant = new MocksHelper([
   'IccHelper'
 ]).init();
 
-suite('operator variant', function() {
+suite('Operator variant', function() {
   const TEST_NETWORK_MCC = '001';
 
-  const EXPECTED_DATA_MNC = 1;
-  const EXPECTED_DATA_ICC_INFO = {
+  const EXPECTED_MNC = '01';
+  const EXPECTED_ICC_INFO = {
     mcc: TEST_NETWORK_MCC,
-    mnc: EXPECTED_DATA_MNC
+    mnc: EXPECTED_MNC
   };
 
-  const EXPECTED_SUPL_MNC = 4;
-  const EXPECTED_SUPL_ICC_INFO = {
-    mcc: TEST_NETWORK_MCC,
-    mnc: EXPECTED_SUPL_MNC
-  };
-
-  const EXPECTED_MMS_MNC = 2;
-  const EXPECTED_MMS_ICC_INFO = {
-    mcc: TEST_NETWORK_MCC,
-    mnc: EXPECTED_MMS_MNC
-  };
-
-  const EXPECTED_OV_MNC = 3;
-  const EXPECTED_OV_ICC_INFO = {
-    mcc: TEST_NETWORK_MCC,
-    mnc: EXPECTED_OV_MNC
-  };
-
-  const T_MOBILE_US_MCC = 310;
-  const T_MOBILE_160_US_MNC = 160;
-  const T_MOBILE_200_US_MNC = 200;
+  const T_MOBILE_US_MCC = '310';
+  const T_MOBILE_160_US_MNC = '160';
+  const T_MOBILE_200_US_MNC = '200';
 
   const T_MOBILE_160_US_ICC_INFO = {
     mcc: T_MOBILE_US_MCC,
@@ -50,40 +32,29 @@ suite('operator variant', function() {
     mnc: T_MOBILE_200_US_MNC
   };
 
-  const NULL_ICC_INFO = { mcc: 0, mnc: 0 };
-  const PERSIST_KEY = 'operator_variant_test.customize';
+  const NULL_ICC_INFO = { mcc: '000', mnc: '00' };
 
-  const DATA_KEYS_VALUES = [
+  const KEYS_VALUES = [
     { key: 'ril.data.carrier', value: 'Test Network' },
     { key: 'ril.data.apn', value: 'internet' },
     { key: 'ril.data.user', value: 'user' },
     { key: 'ril.data.passwd', value: 'password' },
     { key: 'ril.data.httpProxyHost', value: '127.0.0.1' },
     { key: 'ril.data.httpProxyPort', value: '8080' },
-    { key: 'ril.data.authtype', value: 'none' }
-  ];
-
-  const SUPL_KEYS_VALUES = [
-    { key: 'ril.supl.carrier', value: 'Test Network with SUPL' },
-    { key: 'ril.supl.apn', value: 'supl.internet' },
+    { key: 'ril.data.authtype', value: 'none' },
+    { key: 'ril.supl.carrier', value: 'Test Network' },
+    { key: 'ril.supl.apn', value: 'internet' },
     { key: 'ril.supl.user', value: 'user' },
     { key: 'ril.supl.passwd', value: 'password' },
     { key: 'ril.supl.httpProxyHost', value: '127.0.0.1' },
     { key: 'ril.supl.httpProxyPort', value: '8080' },
-    { key: 'ril.supl.authtype', value: 'none' }
-  ];
-
-  const MMS_KEYS_VALUES = [
-    { key: 'ril.mms.carrier', value: 'Test Network with MMS' },
-    { key: 'ril.mms.apn', value: 'mms.internet' },
+    { key: 'ril.supl.authtype', value: 'none' },
+    { key: 'ril.mms.carrier', value: 'Test Network' },
+    { key: 'ril.mms.apn', value: 'internet' },
     { key: 'ril.mms.mmsc', value: 'http://127.0.0.1' },
     { key: 'ril.mms.mmsproxy', value: '127.0.0.1' },
-    { key: 'ril.mms.mmsport', value: '8080' }
-  ];
-
-  const OV_KEYS_VALUES = [
-    { key: 'ril.data.carrier',
-      value: 'Test Network with Operator Variant Settings' },
+    { key: 'ril.mms.mmsport', value: '8080' },
+    { key: 'ril.data.carrier', value: 'Test Network' },
     { key: 'ril.iccInfo.mbdn', value: '999999' },
     { key: 'ril.cellbroadcast.searchlist', value: '0,1,2,3' }
   ];
@@ -137,13 +108,13 @@ suite('operator variant', function() {
     }
   }
 
-  test('operator variant data apn', function(done) {
+  test('Apply operator variant settings', function(done) {
     var observer = {
       bound: null,
-      expected: DATA_KEYS_VALUES.length,
+      expected: KEYS_VALUES.length,
       seen: 0,
       func: function(event) {
-        DATA_KEYS_VALUES.forEach(function(data) {
+        KEYS_VALUES.forEach(function(data) {
           if (data.key == event.settingName) {
             assert.equal(
               event.settingValue,
@@ -155,108 +126,15 @@ suite('operator variant', function() {
         }, this);
 
         if (this.seen == this.expected) {
-          setObservers(DATA_KEYS_VALUES, this, true);
+          setObservers(KEYS_VALUES, this, true);
           done();
         }
       }
     };
 
-    setObservers(DATA_KEYS_VALUES, observer);
+    setObservers(KEYS_VALUES, observer);
 
-    MockIccHelper.mProps.iccInfo = EXPECTED_DATA_ICC_INFO;
-    MockIccHelper.mTriggerEventListeners('iccinfochange', {});
-  });
-
-  test('operator variant agps / supl apn', function(done) {
-    var observer = {
-      bound: null,
-      expected: SUPL_KEYS_VALUES.length,
-      seen: 0,
-      func: function(event) {
-        SUPL_KEYS_VALUES.forEach(function(data) {
-
-          if (data.key == event.settingName) {
-            assert.equal(
-              event.settingValue,
-              data.value,
-              'Wrong AGPS/SUPL setting value'
-            );
-            ++this.seen;
-          }
-        }, this);
-
-        if (this.seen == this.expected) {
-          setObservers(SUPL_KEYS_VALUES, this, true);
-          done();
-        }
-      }
-    };
-
-    setObservers(SUPL_KEYS_VALUES, observer);
-
-    MockIccHelper.mProps.iccInfo = EXPECTED_SUPL_ICC_INFO;
-    MockIccHelper.mTriggerEventListeners('iccinfochange', {});
-  });
-
-  test('operator variant mms apn', function(done) {
-    var observer = {
-      bound: null,
-      expected: MMS_KEYS_VALUES.length,
-      seen: 0,
-      func: function(event) {
-        MMS_KEYS_VALUES.forEach(function(data) {
-
-          if (data.key == event.settingName) {
-            assert.equal(
-              event.settingValue,
-              data.value,
-              'Wrong MMS setting value'
-            );
-            ++this.seen;
-          }
-        }, this);
-
-        if (this.seen == this.expected) {
-          setObservers(MMS_KEYS_VALUES, this, true);
-          done();
-        }
-      }
-    };
-
-    setObservers(MMS_KEYS_VALUES, observer);
-
-    MockIccHelper.mProps.iccInfo = EXPECTED_MMS_ICC_INFO;
-    MockIccHelper.mTriggerEventListeners('iccinfochange', {});
-  });
-
-  test('operator variant ov apn', function(done) {
-    var observer = {
-      bound: null,
-      expected: OV_KEYS_VALUES.length,
-      seen: 0,
-      func: function(event) {
-        OV_KEYS_VALUES.forEach(function(data) {
-
-          if (data.key == event.settingName) {
-            assert.equal(
-              event.settingValue,
-              data.value,
-              'Wrong OV setting value'
-            );
-            ++this.seen;
-          }
-        }, this);
-
-        if (this.seen == this.expected) {
-          setObservers(OV_KEYS_VALUES, this, true);
-          done();
-        }
-      }
-    };
-
-    setObservers(OV_KEYS_VALUES, observer);
-
-    MockIccHelper.mProps.iccInfo = EXPECTED_OV_ICC_INFO;
+    MockIccHelper.mProps.iccInfo = EXPECTED_ICC_INFO;
     MockIccHelper.mTriggerEventListeners('iccinfochange', {});
   });
 
