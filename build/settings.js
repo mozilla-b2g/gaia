52b78993fa97ae12596bb831d4ddd6ade794178c
Rudy Lu <left.lu@gmail.com>
2013-10-26 22:44:52 +0800

    Bug 928281 - Add a setting entry to enable keyboard app run as Out-of-Process (OOP)

--- build/settings.js
+++ build/settings.js
@@ -141,6 +141,7 @@ function execute(options) {
    'debug.console.enabled': false,
    'debug.grid.enabled': false,
    'debug.oop.disabled': false,
+   'debug.keyboard-oop.enabled': false,
    'debug.fps.enabled': false,
    'debug.ttl.enabled': false,
    'debug.log-animations.enabled': false,
