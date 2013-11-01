f3408a4b7dac33901f05451463cca8154410278c
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2013-11-01 12:31:03 +0200

    Bug 932250 - Write a test for context menu activity picker
    Conflicts:
    	tests/python/gaia-ui-tests/gaiatest/apps/ui_tests/app.py

--- tests/python/gaia-ui-tests/gaiatest/apps/ui_tests/app.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/ui_tests/app.py
@@ -1,6 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marionette.marionette import Actions
+
 from marionette.by import By
 from gaiatest.apps.base import Base
 
@@ -23,6 +26,7 @@ class UiTests(Base):
     _app_logout_event = (By.CSS_SELECTOR, 'li.logout')
     _app_login_assertion_text = (By.CSS_SELECTOR, 'li.login div.assertion')
     _keyboard_locator = (By.LINK_TEXT, 'Keyboard')
+    _contextmenu_locator = (By.LINK_TEXT, 'Contextmenu')
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
@@ -90,16 +94,25 @@ class UiTests(Base):
         self.marionette.execute_script('arguments[0].scrollIntoView(false);', [keyboard_button])
         keyboard_button.tap()
 
+    def tap_contextmenu_option(self):
+        self.wait_for_element_displayed(*self._contextmenu_locator, timeout=120)
+        self.marionette.find_element(*self._contextmenu_locator).tap()
+        self.switch_to_contextmenu_page_frame()
+        return ContextmenuPage(self.marionette)
+
     def switch_to_keyboard_page_frame(self):
         keyboard_page_iframe = self.marionette.find_element(By.CSS_SELECTOR, "#test-iframe[src*='keyboard']")
         self.marionette.switch_to_frame(keyboard_page_iframe)
         return KeyboardPage(self.marionette)
 
+    def switch_to_contextmenu_page_frame(self):
+        contextmenu_page_iframe = self.marionette.find_element(By.CSS_SELECTOR, "#test-iframe[src*='contextmenu']")
+        self.marionette.switch_to_frame(contextmenu_page_iframe)
+
 
 class KeyboardPage(Base):
 
-    _keyboard_iframe_locator = (By.CSS_SELECTOR, "#test-iframe[src*='keyboard']")
-    _number_input_locator =(By.CSS_SELECTOR, 'input[type="number"]')
+    _number_input_locator = (By.CSS_SELECTOR, 'input[type="number"]')
     _text_input_locator = (By.CSS_SELECTOR, 'input[type="text"]')
 
     def tap_number_input(self):
@@ -119,3 +132,16 @@ class KeyboardPage(Base):
     @property
     def text_input(self):
         return self.marionette.find_element(*self._text_input_locator).get_attribute('value')
+
+
+class ContextmenuPage(Base):
+
+    _contextmenu_body_locator = (By.CSS_SELECTOR, "body > section")
+
+    def long_press_contextmenu_body(self):
+        self.wait_for_condition(lambda m: m.find_element(*self._contextmenu_body_locator).is_displayed())
+        contextmenu_body = self.marionette.find_element(*self._contextmenu_body_locator)
+        Actions(self.marionette).press(contextmenu_body).wait(1).release().perform()
+
+        from gaiatest.apps.system.regions.activities import Activities
+        return Activities(self.marionette)
