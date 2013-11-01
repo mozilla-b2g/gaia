f18e209885b79707331b3d3bb11c5195dde112ff
Alive.Kuo <alegnadise@gmail.com>
2013-11-01 01:32:11 -0700

    Merge pull request #13163 from dwi2/bug907625
    Bug 907625 - Prevent keyboard blocks incoming callscreen when keyboard i...,r=alive(cherry picked from commit 6f91e524f231d32c77c3045b6491a7513f396dc8)

--- apps/system/js/keyboard_manager.js
+++ apps/system/js/keyboard_manager.js
@@ -131,6 +131,7 @@ var KeyboardManager = {
     // when an inline activity goes away.
     window.addEventListener('appwillclose', this);
     window.addEventListener('activitywillclose', this);
+    window.addEventListener('attentionscreenshow', this);
 
     // To handle keyboard layout switching
     window.addEventListener('mozChromeEvent', function(evt) {
@@ -397,9 +398,17 @@ var KeyboardManager = {
   },
 
   handleEvent: function km_handleEvent(evt) {
+    var self = this;
     switch (evt.type) {
       case 'mozbrowserresize':
         this.resizeKeyboard(evt);
+        break;
+      case 'attentionscreenshow':
+        // If we call hideKeyboardImmediately synchronously,
+        // attention screen will not show up.
+        setTimeout(function hideKeyboardAsync() {
+          self.hideKeyboardImmediately();
+        }, 0);
         break;
       case 'activitywillclose':
       case 'appwillclose':
