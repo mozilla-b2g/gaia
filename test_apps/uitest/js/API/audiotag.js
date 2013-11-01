cb3cc0564bc04698f5c865b23d22743e5dfeaeeb
Tom Jao <tjao@mozilla.com>
2013-11-01 10:10:25 +0800

    bug - 931727 [HW test] update paths in UItest
    Modify paths according to the change of file structure
    Move audio files to data folder

--- test_apps/uitest/js/API/audiotag.js
+++ test_apps/uitest/js/API/audiotag.js
@@ -5,7 +5,7 @@ var clickHandlers = {
     var src = this.dataset.src;
     var ringtonePlayer = new Audio();
     ringtonePlayer.loop = true;
-    var selectedSound = '../style/ringtones/' + src;
+    var selectedSound = '../../data/ringtones/' + src;
     ringtonePlayer.src = selectedSound;
     ringtonePlayer.play();
     window.setTimeout(function _pauseRingtone() {
