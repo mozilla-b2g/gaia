ffa9943b1debb831e896782d5dc044aac03b8278
Rudy Lu <me@rudy.lu>
2013-10-31 23:50:27 -0700

    Merge pull request #13253 from RudyLu/Bug931200-add_croation_keyboard_master
    Bug931200 - Add Croation keyboard layout +shepherd.
    r=djf(cherry picked from commit 4d51e2a5a98415a5788eed2831f2f0d02da23cbb)

---
+++ keyboard/layouts/hr.js
@@ -0,0 +1,37 @@
+Keyboards.hr = {
+  label: 'Croatian',
+  menuLabel: 'Hrvatski',
+  imEngine: 'latin',
+  autoCorrectLanguage: 'hr',
+  alt: {
+    c: 'čćç',
+    d: 'đ',
+    s: 'šśß$',
+    n: 'ñń',
+    l: '£',
+    y: '¥',
+    z: 'žźż',
+    r: 'R$ ',
+    '.': ',?!;:'
+  },
+  keys: [ // qwertz
+    [
+      { value: 'q' }, { value: 'w' }, { value: 'e' } , { value: 'r' },
+      { value: 't' } , { value: 'z' }, { value: 'u' } , { value: 'i' },
+      { value: 'o' }, { value: 'p' }
+    ], [
+      { value: 'a' }, { value: 's' }, { value: 'd' }, { value: 'f' },
+      { value: 'g' } , { value: 'h' }, { value: 'j' }, { value: 'k' },
+      { value: 'l' }, { value: "'", keyCode: 39, hidden: ['email', 'url'] },
+      { value: ':', visible: ['url']}, { value: '_', visible: ['email']}
+    ], [
+      { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+      { value: 'y' }, { value: 'x' }, { value: 'c' }, { value: 'v' },
+      { value: 'b' }, { value: 'n' }, { value: 'm' },
+      { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+    ], [
+      { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+    ]
+  ]
+};
