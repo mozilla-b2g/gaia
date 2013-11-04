3980f02517b416c9bd7a33330285d0bb13f2d2a4
Alexandre Lissy <lissyx+github@lissyx.dyndns.org>
2013-11-05 00:19:50 +0100

    Bug 932271 - Reset context menu state at the correct moment
    Displaying the context menu in the browser app is controlled by a
    barrier. We have to make sure that this barrier is correctly reset at
    the right time to ensure that the context menu will work more than once:
    setting the state before doing the callback is safer than relying on the
    callback to handle it by itself.

--- apps/browser/test/marionette/contextmenu_test.js
+++ apps/browser/test/marionette/contextmenu_test.js
@@ -39,19 +39,65 @@ marionette('search', function() {
       client.switchToFrame(frame);
     });
 
-    test('long press link and open context menu', function() {
+    function longTap() {
       client.helper.waitForElement('#link');
       var link = client.findElement('#link');
 
       actions.longPress(link, 1.5).perform();
 
       subject.backToApp();
+    }
 
+    function clickOpenNewTab() {
       var openItem = client.findElement('#open-in-new-tab');
       openItem.click();
+    }
 
+    function openTab() {
+      longTap();
+      clickOpenNewTab();
+    }
+
+    function openNewTab() {
+      frame = subject.currentTabFrame();
+      client.switchToFrame(frame);
+
+      longTap();
+
+      clickOpenNewTab();
+    }
+
+    function assertTabsBadge(text) {
       var tabsBadge = client.findElement('#tabs-badge');
-      assert.equal(tabsBadge.text(), '2›');
+      assert.equal(tabsBadge.text(), text);
+    }
+
+    test('long press link and open context menu', function() {
+      openTab();
+      assertTabsBadge('2›');
+    });
+
+    test('context menu opens after a context menu', function() {
+      openTab();
+      openNewTab();
+      assertTabsBadge('3›');
+    });
+
+    test('context menu opens after a cancel', function() {
+      openTab();
+
+      frame = subject.currentTabFrame();
+      client.switchToFrame(frame);
+
+      longTap();
+
+      var cancelItem = client.findElement('#cancel');
+      cancelItem.click();
+
+      assertTabsBadge('2›');
+
+      openNewTab();
+      assertTabsBadge('3›');
     });
   });
 });
