fa0050bbd86296be373a89852ab5aa80ad08891b
Ran Ben Aharon <ran@ranbena.com>
2013-11-04 17:30:05 -0800

    Bug 934768 - [e.me] API key change for v1.2 [r=crdlc]

--- apps/homescreen/everything.me/config/config.js
+++ apps/homescreen/everything.me/config/config.js
@@ -1,7 +1,7 @@
 Evme.Config = Evme.__config = {
   'appVersion': '2.0.145',
   'apiHost': 'api.everything.me',
-  'apiKey': '65106dbdb655b25f2defa18ab7d1ecd8',
+  'apiKey': '1518c0001ff736528322f306f41f027d',
   'authCookieName': 'prod-credentials',
   'apps': {
     'appsPerRow': 4,
