399d84886a68a7145c42a471a30726e9a149b968
Sergey Tupchiy <tupchii.sergii@gmail.com>
2013-11-06 23:33:22 +0100

    Bug 935440 - Fix my mistake in test_geolocation_prompt.py

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/system/test_geolocation_prompt.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/system/test_geolocation_prompt.py
@@ -2,30 +2,33 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from marionette import By
+
 from gaiatest import GaiaTestCase
-from gaiatest.apps.camera.app import Camera
 from gaiatest.apps.homescreen.regions.permission_dialog import PermissionDialog
 
 
 class TestGeolocationPrompt(GaiaTestCase):
 
+    _geoloc_start_button_locator = (By.ID, 'btnStart')
+
     def setUp(self):
         GaiaTestCase.setUp(self)
-        self.apps.set_permission('Camera', 'geolocation', 'prompt')
+        self.apps.set_permission('Geoloc', 'geolocation', 'prompt')
 
-    def test_camera_geolocation_prompt(self):
+    def test_geolocation_prompt(self):
 
-        camera = Camera(self.marionette)
-        camera.launch()
+        self.app = self.apps.launch('Geoloc')
+        self.marionette.find_element(*self._geoloc_start_button_locator).tap()
 
         permission = PermissionDialog(self.marionette)
         self.marionette.switch_to_default_content()
         permission.wait_for_permission_dialog_displayed()
 
         self.assertEqual(permission.permission_dialog_message,
-                         '%s would like to know your location.' % camera.name)
+                         'Geoloc would like to know your location.')
 
         permission.tap_to_confirm_permission()
 
-        current_permission = self.apps.get_permission('Camera', 'geolocation')
+        current_permission = self.apps.get_permission('Geoloc', 'geolocation')
         self.assertEqual(current_permission, 'allow')
