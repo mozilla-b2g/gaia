7c1f2dcdcf98198c3d0f9311bd1f0ad74ec9c127
Timothy Guan-tin Chien <timdream@gmail.com>
2013-11-06 11:17:21 +0800

    Bug 935350 - Rename 'keyboard' role in homescreen, +shepherd

--- apps/homescreen/js/grid.js
+++ apps/homescreen/js/grid.js
@@ -14,7 +14,7 @@ var GridManager = (function() {
   var BASE_HEIGHT = 460; // 480 - 20 (status bar height)
   var DEVICE_HEIGHT = window.innerHeight;
 
-  var HIDDEN_ROLES = ['system', 'keyboard', 'homescreen'];
+  var HIDDEN_ROLES = ['system', 'input', 'homescreen'];
 
   function isHiddenApp(role) {
     if (!role) {
