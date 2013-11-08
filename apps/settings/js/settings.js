b24a12bccdffb35dbc41ad7dca446614724e2932
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2013-11-08 01:28:02 -0800

    (v1.2) Bug 927486 - [Settings] Modify Message settings category. r=arthurcc

--- apps/settings/js/settings.js
+++ apps/settings/js/settings.js
@@ -195,7 +195,9 @@ var Settings = {
 
     // hide telephony related entries if not supportted
     if (!navigator.mozTelephony) {
-      var elements = ['call-settings', 'data-connectivity',
+      var elements = ['call-settings',
+                      'messaging-settings',
+                      'data-connectivity',
                       'simSecurity-settings'];
       elements.forEach(function(el) {
         document.getElementById(el).hidden = true;
@@ -811,6 +813,7 @@ window.addEventListener('load', function loadSettings() {
   function handleRadioAndCardState() {
     function disableSIMRelatedSubpanels(disable) {
       var itemIds = ['call-settings',
+                     'messaging-settings',
                      'data-connectivity'];
 
       // Disable SIM security item only in case of SIM absent.
