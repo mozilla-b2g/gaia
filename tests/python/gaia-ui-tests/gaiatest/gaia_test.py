1cb5024ba27f26e737535cda8ec42a42f41a7927
Sergey Tupchiy <tupchii.sergii@gmail.com>
2013-11-10 15:58:18 +0100

    Bug 934402 - Write an atom to send SMS using 'MozSMS' API

--- tests/python/gaia-ui-tests/gaiatest/gaia_test.py
+++ tests/python/gaia-ui-tests/gaiatest/gaia_test.py
@@ -337,6 +337,13 @@ class GaiaData(object):
             return [filename for filename in files if filename.endswith(extension)]
         return files
 
+    def send_sms(self, number, message):
+        import json
+        number = json.dumps(number)
+        message = json.dumps(message)
+        result = self.marionette.execute_async_script('return GaiaDataLayer.sendSMS(%s, %s)' % (number, message), special_powers=True)
+        assert result, 'Unable to send SMS to recipient %s with text %s' % (number, message)
+
 
 class GaiaDevice(object):
 
@@ -516,7 +523,6 @@ class GaiaTestCase(MarionetteTestCase):
             # Set do not track pref back to the default
             self.data_layer.set_setting('privacy.donottrackheader.value', '-1')
 
-
             if self.data_layer.get_setting('ril.radio.disabled'):
                 # enable the device radio, disable Airplane mode
                 self.data_layer.set_setting('ril.radio.disabled', False)
@@ -543,7 +549,6 @@ class GaiaTestCase(MarionetteTestCase):
 
         # disable sound completely
         self.data_layer.set_volume(0)
-
 
     def install_marketplace(self):
         _yes_button_locator = (By.ID, 'app-install-install-button')
@@ -806,8 +811,8 @@ class GaiaEnduranceTestCase(GaiaTestCase):
         # Calculate the average b2g_rss
         total = 0
         for b2g_mem_value in b2g_rss_list:
-            total+=int(b2g_mem_value)
-        avg_rss = total/len(b2g_rss_list)
+            total += int(b2g_mem_value)
+        avg_rss = total / len(b2g_rss_list)
 
         # Create a summary text file
         summary_name = self.log_name.replace('.log', '_summary.log')
