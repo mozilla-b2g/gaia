3c73873c8e8ddc8cea9665db0317785e926b286b
Ben Kelly <bkelly@mozilla.com>
2013-11-12 13:51:49 -0800

    Merge pull request #13593 from wanderview/contacts-group-overflow
    Bug 937291:  Limit overflow of each group list.  r=jmcf

--- apps/communications/contacts/js/views/list.js
+++ apps/communications/contacts/js/views/list.js
@@ -351,6 +351,7 @@ contacts.List = (function() {
     // Create the DOM for the group list
     var letteredSection = document.createElement('section');
     letteredSection.id = 'section-group-' + group;
+    letteredSection.className = 'group-section';
     var title = document.createElement('header');
     title.id = 'group-' + group;
     title.className = 'hide';
@@ -362,6 +363,7 @@ contacts.List = (function() {
 
     var contactsContainer = document.createElement('ol');
     contactsContainer.id = 'contacts-list-' + group;
+    contactsContainer.className = 'group-list';
     contactsContainer.dataset.group = group;
     letteredSection.appendChild(title);
     letteredSection.appendChild(contactsContainer);
