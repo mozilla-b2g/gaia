b383347b2dd71e184e9b91d3b41098a5f8b301e7
EragonJ <eragonj@eragonj.me>
2013-11-15 14:02:05 +0800

    update simcard_manager_simcard_helper.js to use original _
    dont want to pollute this for too much unrelated stuffs, I try to use
    original way to include _ and update test for this use case

--- apps/settings/test/unit/sm_simcard_helper_test.js
+++ apps/settings/test/unit/sm_simcard_helper_test.js
@@ -1,23 +1,30 @@
 'use strict';
 
 requireApp('settings/test/unit/mock_l10n.js');
-requireApp('settings/js/simcard_manager_simcard_helper.js');
+
+mocha.globals(['SimCard']);
 
 suite('Simcard >', function() {
   var realL10n;
   var fakeSimcard;
   var fakeSimcardIndex = 0;
 
-  suiteSetup(function() {
+  suiteSetup(function(done) {
     realL10n = window.navigator.mozL10n;
     window.navigator.mozL10n = MockL10n;
 
-    // create a fakeSimcard instance
-    fakeSimcard = new SimCard(fakeSimcardIndex);
+    // because we use _, we have to make sure
+    // this script is executed after requiring
+    requireApp('settings/js/simcard_manager_simcard_helper.js', done);
   });
 
   suiteTeardown(function() {
     window.navigator.mozL10n = realL10n;
+  });
+
+  setup(function() {
+    // create a fakeSimcard instance
+    fakeSimcard = new SimCard(fakeSimcardIndex);
   });
 
   suite('SimCard.getInfo > ', function() {
