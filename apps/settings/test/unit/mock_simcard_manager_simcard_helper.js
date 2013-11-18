4346e9f1df7fc77b68e3a9393bd0a25cc57a9d23
EragonJ <eragonj@eragonj.me>
2013-11-18 16:29:49 +0800

    add more tests for SimCardManager

--- apps/settings/test/unit/mock_simcard_manager_simcard_helper.js
+++ apps/settings/test/unit/mock_simcard_manager_simcard_helper.js
@@ -4,17 +4,24 @@ function MockSimCard(cardIndex) {
   this.cardIndex = cardIndex;
 
   // state list
-  this.enabled = false;
+  this.enabled = true;
   this.absent = false;
   this.locked = false;
-  this.name = '';
-  this.number = '';
-  this.operator = '';
+  this.name = 'card';
+  this.number = '0123456789';
+  this.operator = 'Taiwan telecom';
 }
 
 MockSimCard.prototype = {
   getInfo: function() {
-    return {};
+    return {
+      enabled: this.enabled,
+      absent: this.absent,
+      locked: this.locked,
+      name: this.name,
+      number: this.number,
+      operator: this.operator
+    };
   },
   setState: function(key, options) {
     // do nothing
