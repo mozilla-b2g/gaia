28a0b3bf449e591b68be7a46929e8c1c843f6dc0
Jose Antonio Olivera Ortega <josea.olivera@gmail.com>
2013-11-19 15:28:53 +0100

    Bug 928297 - [DSDS][Gaia] Settings app should support cellular & data settings of multiple sim cards

--- apps/settings/js/settings.js
+++ apps/settings/js/settings.js
@@ -255,8 +255,8 @@ var Settings = {
     // hide telephony related entries if not supportted
     if (!navigator.mozTelephony) {
       var elements = ['call-settings',
-                      'messaging-settings',
                       'data-connectivity',
+                      'messaging-settings',
                       'simSecurity-settings'];
       elements.forEach(function(el) {
         document.getElementById(el).hidden = true;
@@ -802,7 +802,8 @@ window.addEventListener('load', function loadSettings() {
       'js/connectivity.js',
       'js/security_privacy.js',
       'js/icc_menu.js',
-      'js/nfc.js'
+      'js/nfc.js',
+      'js/dsds_settings.js'
     ], handleRadioAndCardState);
   });
 
@@ -823,15 +824,46 @@ window.addEventListener('load', function loadSettings() {
     }
   }
 
+  /**
+   * Enable or disable the menu items related to the ICC card relying on the
+   * card and radio state.
+   */
   function handleRadioAndCardState() {
+    var iccId;
+
+    // we hide all entry points by default,
+    // so we have to detect and show them up
+    if (navigator.mozMobileConnections) {
+      if (navigator.mozMobileConnections.length == 1) {
+        // single sim
+        document.getElementById('simSecurity-settings').hidden = false;
+      } else {
+        // dsds
+        document.getElementById('simCardManager-settings').hidden = false;
+      }
+    }
+
+    var mobileConnections = window.navigator.mozMobileConnections;
+    var iccManager = window.navigator.mozIccManager;
+    if (!mobileConnections || !iccManager) {
+      disableSIMRelatedSubpanels(true);
+      return;
+    }
+
     function disableSIMRelatedSubpanels(disable) {
       var itemIds = ['call-settings',
-                     'messaging-settings',
-                     'data-connectivity'];
+                     'messaging-settings'];
 
-      // Disable SIM security item only in case of SIM absent.
-      var cardState = IccHelper && IccHelper.cardState;
-      if (!disable || !cardState) {
+      if (mobileConnections.length === 1) {
+        itemIds.push('data-connectivity');
+      }
+
+      // Disable SIM security item in case of SIM absent or airplane mode.
+      // Note: mobileConnections[0].iccId being null could mean there is no ICC
+      // card or the ICC card is locked. If locked we would need to figure out
+      // how to check the current card state.
+      if (!mobileConnections[0].iccId ||
+          (mobileConnections[0].radioState === 'disabled')) {
         itemIds.push('simSecurity-settings');
       }
 
@@ -849,28 +881,63 @@ window.addEventListener('load', function loadSettings() {
       }
     }
 
-    // we hide all entry points by default,
-    // so we have to detect and show them up
-    if (navigator.mozMobileConnections) {
-      if (navigator.mozMobileConnections.length == 1) {
-        // single sim
-        document.getElementById('simSecurity-settings').hidden = false;
-      } else {
-        // dsds
-        document.getElementById('simCardManager-settings').hidden = false;
+    function cardStateAndRadioStateHandler() {
+      if (!mobileConnections[0].iccId) {
+        // This could mean there is no ICC card or the ICC card is locked.
+        disableSIMRelatedSubpanels(true);
+        return;
       }
-    }
 
-    if (!IccHelper) {
-      return disableSIMRelatedSubpanels(true);
-    }
+      if (mobileConnections[0].radioState !== 'enabled') {
+        // Airplane is enabled. Well, radioState property could be changing but
+        // let's disable the items during the transitions also.
+        disableSIMRelatedSubpanels(true);
+        return;
+      }
+      if (mobileConnections[0].radioState === 'enabled') {
+        disableSIMRelatedSubpanels(false);
+      }
 
-    var cardState = IccHelper.cardState;
-    disableSIMRelatedSubpanels(cardState !== 'ready');
-
-    IccHelper.addEventListener('cardstatechange', function() {
-      var cardState = IccHelper.cardState;
+      var iccCard = iccManager.getIccById(mobileConnections[0].iccId);
+      if (!iccCard) {
+        disableSIMRelatedSubpanels(true);
+        return;
+      }
+      var cardState = iccCard.cardState;
       disableSIMRelatedSubpanels(cardState !== 'ready');
+    }
+
+    function addListeners() {
+      iccId = mobileConnections[0].iccId;
+      var iccCard = iccManager.getIccById(iccId);
+      if (!iccCard) {
+        return;
+      }
+      iccCard.addEventListener('cardstatechange',
+        cardStateAndRadioStateHandler);
+      mobileConnections[0].addEventListener('radiostatechange',
+        cardStateAndRadioStateHandler);
+    }
+
+    cardStateAndRadioStateHandler();
+    addListeners();
+
+    iccManager.addEventListener('iccdetected',
+      function iccDetectedHandler(evt) {
+        if (mobileConnections[0].iccId &&
+           (mobileConnections[0].iccId === evt.iccId)) {
+          cardStateAndRadioStateHandler();
+          addListeners();
+        }
+    });
+
+    iccManager.addEventListener('iccundetected',
+      function iccUndetectedHandler(evt) {
+        if (iccId === evt.iccId) {
+          disableSIMRelatedSubpanels(true);
+          mobileConnections[0].removeEventListener('radiostatechange',
+            cardStateAndRadioStateHandler);
+        }
     });
   }
 
