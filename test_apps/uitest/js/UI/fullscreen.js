321f6bb75693445873456a9cf6df4696432272c5
Tom Jao <tjao@mozilla.com>
2013-11-20 16:07:19 +0800

    Bug 938952 - [UITest] provide a way to exit fullscreen mode

--- test_apps/uitest/js/UI/fullscreen.js
+++ test_apps/uitest/js/UI/fullscreen.js
@@ -1,15 +1,37 @@
 'use strict';
 
-var clickHandlers = {
-  'fullscreen': function fullscreen() {
-    document.getElementById('fullscreen-div').mozRequestFullScreen();
-  },
-  'fullscreenFrame': function fullscreenFrame() {
-    window.parent.document.getElementById('test-iframe').mozRequestFullScreen();
-  }
-};
+function fullscreenTest() {
+  var fullscreenDiv = document.getElementById('fullscreen-div');
+  var cancelButtonDiv = document.getElementById('cancleFullscreen-div');
+  var cancelButton = document.getElementById('cancleFullscreen');
 
-document.body.addEventListener('click', function(evt) {
-  if (clickHandlers[evt.target.id || evt.target.dataset.fn])
-    clickHandlers[evt.target.id || evt.target.dataset.fn].call(this, evt);
-});
+  function clickHandlers(evt) {
+    switch (evt.target.id) {
+      case 'fullscreen':
+        fullscreenDiv.mozRequestFullScreen();
+        cancelButtonDiv.hidden = false;
+      break;
+      case 'fullscreenFrame':
+        window.parent.document.getElementById('test-iframe')
+                                              .mozRequestFullScreen();
+        cancelButton.hidden = false;
+      break;
+      case 'cancleFullscreen-div':
+        document.mozCancelFullScreen();
+        cancelButtonDiv.hidden = true;
+        cancelButton.hidden = true;
+      break;
+      case 'cancleFullscreen':
+        window.parent.document.mozCancelFullScreen();
+        cancelButton.hidden = true;
+        break;
+    }
+  };
+
+  cancelButtonDiv.hidden = true;
+  cancelButton.hidden = true;
+
+  document.body.addEventListener('click', clickHandlers.bind(this));
+}
+
+window.addEventListener('load', fullscreenTest);
