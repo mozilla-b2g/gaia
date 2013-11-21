3c89aab6777f0ece3b30545b9b9bc7139c70d9b2
Cristian Rodriguez <crdlc@tid.es>
2013-11-21 05:30:06 -0800

    Merge pull request #13881 from AntonioMA/bug936018
    Bug 936018 - Change the way the icons are loaded to allow reentrant calls
    (cherry picked from commit cdab4981044ddcf0ca32cab6e66a8141556701dc)

--- apps/homescreen/js/page.js
+++ apps/homescreen/js/page.js
@@ -193,7 +193,7 @@ Icon.prototype = {
       icon: this,
       success: function(blob) {
         this.loadImageData(blob);
-      },
+      }.bind(this),
       error: function() {
         if (this.icon && !this.downloading &&
             this.icon.classList.contains('loading')) {
@@ -201,7 +201,7 @@ Icon.prototype = {
           this.img.src = null;
         }
         this.loadCachedIcon();
-      }
+      }.bind(this)
     });
   },
 
