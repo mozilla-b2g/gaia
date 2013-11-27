bcf635b7ea3d2745af4457b896ecccec2f750526
Michael Henretty <michael.henretty@gmail.com>
2013-11-26 22:59:16 -0800

    Bug 926347 - updating SIM unlocking in FTU to include dual-sim and SIM info screen

--- tools/extensions/desktop-helper/content/data/lib/icc_manager.js
+++ tools/extensions/desktop-helper/content/data/lib/icc_manager.js
@@ -4,24 +4,80 @@
     //dump('mozIccManager: ' + str + '\n');
   }
 
+  var iccs = {
+    111: {
+      _retryCount: 3,
+      cardState: 'pinRequired',
+      iccInfo: {
+        iccid: true,
+        msisdn: '5555555555'
+      },
+      setCardLock: function() {
+        debug('setCardLock');
+      },
+      getCardLock: FFOS_RUNTIME.domRequest({ enabled: false }),
+      getCardLockRetryCount: function(type, callback) {
+        callback(this._retryCount);
+      },
+      unlockCardLock: function() {
+        // simulate invalid input
+        if (this._retryCount > 1) {
+          var request = {};
+          setTimeout(function() {
+            request.error = {
+              retryCount: --this._retryCount,
+              lockType: 'pin'
+            };
+            request.onerror && request.onerror();
+          }.bind(this), 200);
+          return request;
+        }
+        this.cardState = 'ready';
+        return FFOS_RUNTIME.domRequest()();
+      },
+      addEventListener: function() {
+        debug('addEventListener');
+      },
+      removeEventListener: function() {
+        debug('removeEventListener');
+      },
+    },
+    222: {
+      _retryCount: 3,
+      cardState: 'pinRequired',
+      iccInfo: {
+        iccid: true
+      },
+      setCardLock: function() {
+        debug('setCardLock');
+      },
+      getCardLock: FFOS_RUNTIME.domRequest({ enabled: false }),
+      getCardLockRetryCount: function(type, callback) {
+        callback(this._retryCount);
+      },
+      unlockCardLock: function() {
+        this.cardState = 'ready';
+        return FFOS_RUNTIME.domRequest()();
+      },
+      addEventListener: function() {
+        debug('addEventListener');
+      },
+      removeEventListener: function() {
+        debug('removeEventListener');
+      },
+    }
+  };
+
   FFOS_RUNTIME.makeNavigatorShim('mozIccManager', {
-    iccIds: [],
-    iccInfo: {
-      iccid: true
-    },
-    cardState: 'ready',
-    setCardLock: function() {
-      debug('setCardLock');
-    },
-    getCardLock: FFOS_RUNTIME.domRequest({ enabled: false }),
-    unlockCardLock: function() {
-      debug('unlockCardLock');
-    },
     addEventListener: function() {
       debug('addEventListener');
     },
     removeEventListener: function() {
       debug('removeEventListener');
-    }
+    },
+    getIccById: function(iccId) {
+      return iccs[iccId];
+    },
+    iccIds: [111, 222]
   }, true);
 }();
--- tools/extensions/desktop-helper/content/data/lib/mobile_connection.js
+++ tools/extensions/desktop-helper/content/data/lib/mobile_connection.js
@@ -4,9 +4,18 @@
     //dump('mozMobileConnection: ' + str + '\n');
   }
 
-  var initialized = false;
+  var initialized = true;
   var fakeNetwork = { shortName: 'Fake Orange F', mcc: '208', mnc: '1' };
   var fakeVoice = {
+    network: fakeNetwork,
+    state: 'notSearching',
+    roaming: true,
+    connected: true,
+    emergencyCallsOnly: false
+  };
+  var fakeNetwork2 = { shortName: 'Verizon Fake', mcc: '208', mnc: '1' };
+  var fakeVoice2 = {
+    network: fakeNetwork2,
     state: 'notSearching',
     roaming: true,
     connected: true,
@@ -21,12 +30,13 @@
     setTimeout(function fakeCallback() {
       initialized = true;
       if (typeof callback === 'function') {
-        callback();
+        callback({ iddId: this.iccId });
       }
-    }, 5000);
+    }.bind(this), 5000);
   }
 
   FFOS_RUNTIME.makeNavigatorShim('mozMobileConnections', [{
+    iccId: 111,
     addEventListener: fakeEventListener,
     removeEventListener: function() {},
     getCallWaitingOption: FFOS_RUNTIME.domRequest(true),
@@ -38,6 +48,20 @@
     },
     get voice() {
       return initialized ? fakeVoice : null;
+    }
+  },{
+    iccId: 222,
+    addEventListener: fakeEventListener,
+    removeEventListener: function() {},
+    getCallWaitingOption: FFOS_RUNTIME.domRequest(true),
+    getCallForwardingOption: FFOS_RUNTIME.domRequest(1),
+    setCallWaitingOption: FFOS_RUNTIME.domRequest(),
+    getCallingLineIdRestriction: FFOS_RUNTIME.domRequest(),
+    get data() {
+      return initialized ? { network: fakeNetwork2 } : null;
+    },
+    get voice() {
+      return initialized ? fakeVoice2 : null;
     }
   }], true);
 
