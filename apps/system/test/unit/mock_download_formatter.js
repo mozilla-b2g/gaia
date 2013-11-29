6a59916c6de77fe94a67222f5a20bada6b99e3ac
crdlc <crdlc@tid.es>
2013-11-29 13:16:41 +0100

    Bug 935094 - [Download Manager] Download notifications

---
+++ apps/system/test/unit/mock_download_formatter.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+var MockDownloadFormatter = {
+  getUUID: function mdf_getUUID(download) {
+    return download.id;
+  },
+
+  getFileName: function mdf_getFileName(download) {
+    return download.path.split('/').pop();
+  },
+
+  getPercentage: function mdf_getDownloadedPercentage() {
+    return 40;
+  }
+};
