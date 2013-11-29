e6b3fe2fcc7f12e97f81a09eafec7e19e400dfce
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-11-29 04:55:40 +0100

    Bug 944575 - When exporting a local contact to SIM save the iccContactId in order to align with bug 943234

--- apps/communications/contacts/test/unit/export/sim_test.js
+++ apps/communications/contacts/test/unit/export/sim_test.js
@@ -1,13 +1,26 @@
 requireApp('communications/contacts/js/export/sim.js');
-
+requireApp('communications/contacts/test/unit/mock_mozContacts.js');
 
 suite('Sim export', function() {
 
   var subject;
   var realIcc = null;
+  var realMozContacts = null;
   var c1 = {}, c2 = {};
   var updateSpy = null;
   var progressMock = function dummy() {};
+
+  var iccContactId = '1234567';
+  var iccid = '999999999';
+
+  function getIccContactUrl(iccid, iccContactId) {
+    return 'urn:uuid:' + iccid + '-' + iccContactId;
+  }
+
+  var iccContactIdUrl = {
+    type: ['source', 'sim'],
+    value: getIccContactUrl(iccid, iccContactId)
+  };
 
   suiteSetup(function() {
     realIcc = navigator.mozIccManager;
@@ -20,6 +33,9 @@ suite('Sim export', function() {
       'updateContact': function(type, contact) {
         var self = this;
         return {
+          result: {
+            id: iccContactId
+          },
           set onsuccess(cb) {
             self.calledCount++;
             if (!self.faulty || self.calledCount % 2 != 0) {
@@ -32,15 +48,20 @@ suite('Sim export', function() {
             }
           }
         };
+      },
+      'iccInfo': {
+        iccid: iccid
       }
     };
+    realMozContacts = navigator.mozContacts;
+    navigator.mozContacts = MockMozContacts;
 
     updateSpy = sinon.spy(navigator.mozIccManager, 'updateContact');
-
   });
 
   suiteTeardown(function() {
     navigator.mozIccManager = realIcc;
+    navigator.mozContacts = realMozContacts;
   });
 
   setup(function() {
@@ -119,4 +140,53 @@ suite('Sim export', function() {
       done();
     });
   });
+
+  test('Updating a Contact that has not a previous iccContactId. URL created',
+       function(done) {
+    subject.setContactsToExport([c1]);
+
+    subject.doExport(function onFinish(error, exported, msg) {
+      assert.deepEqual(c1.url[0], iccContactIdUrl);
+      done();
+    });
+  });
+
+  test('Updating a Contact that has a previous iccContactId. URL kept as it is',
+       function(done) {
+
+    var contactToExport = {
+      url: [
+        iccContactIdUrl
+      ]
+    };
+    subject.setContactsToExport([contactToExport]);
+
+    subject.doExport(function onFinish(error, exported, msg) {
+      assert.equal(contactToExport.url.length, 1);
+      assert.deepEqual(contactToExport.url[0], iccContactIdUrl);
+      done();
+    });
+  });
+
+  test(
+    'Updating a Contact that has a previous iccContactId from a different SIM',
+       function(done) {
+    var contactToExport = {
+      url: [{
+        type: ['source', 'sim']
+      }]
+    };
+    contactToExport.url[0].value = getIccContactUrl('98765', '77777');
+    var contactToExportOriginalUrl = contactToExport.url[0];
+
+    subject.setContactsToExport([contactToExport]);
+
+    subject.doExport(function onFinish(error, exported, msg) {
+      assert.equal(contactToExport.url.length, 2);
+      assert.deepEqual(contactToExport.url[0], contactToExportOriginalUrl);
+      assert.deepEqual(contactToExport.url[1], iccContactIdUrl);
+      done();
+    });
+  });
+
 });
