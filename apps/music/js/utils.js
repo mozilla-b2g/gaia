0659f16b9790b1cf9eba4d80743fcc774d2ffe3a
Dominic Kuo <dkuo@mozilla.com>
2013-12-04 03:41:54 -0800

    Merge pull request #14251 from dominickuo/bug-943797
    Bug 943797 - [music] Use hash to decide the default album art, r=jimporter(cherry picked from commit e01827ec7b9bc0426e0f5d216123b9247413db4e)

--- apps/music/js/utils.js
+++ apps/music/js/utils.js
@@ -1,14 +1,5 @@
 'use strict';
 
-function escapeHTML(str, escapeQuotes) {
-  var span = document.createElement('span');
-  span.textContent = str;
-
-  if (escapeQuotes)
-    return span.innerHTML.replace(/"/g, '&quot;').replace(/'/g, '&#x27;');
-  return span.innerHTML;
-}
-
 function formatTime(secs) {
   if (isNaN(secs))
     return;
@@ -34,48 +25,25 @@ function formatTime(secs) {
   return formatedTime;
 }
 
-// This function is for cropping the onloaded IMG.
-// If a DIV(parent) contains a IMG(child),
-// the IMG will be cropped and fitted to vertical or horizontal center.
-// DIV(parent) - overflow: hidden;
-// IMG(child) - position: absolute;
-function cropImage(evt) {
-  var image = evt.target;
-  var style = image.style;
-
-  var parentWidth = image.parentElement.clientWidth;
-  var parentHeight = image.parentElement.clientHeight;
-
-  var childRatio = image.naturalWidth / image.naturalHeight;
-  var parentRatio = parentWidth / parentHeight;
-
-  if (childRatio > parentRatio) {
-    style.width = 'auto';
-    style.height = parentHeight + 'px';
-
-    style.left = -(parentHeight * childRatio - parentWidth) / 2 + 'px';
-  } else {
-    style.width = parentWidth + 'px';
-    style.height = 'auto';
-
-    style.top = -(parentWidth / childRatio - parentHeight) / 2 + 'px';
+// The Javascript implementation of Java’s String.hashCode() method.
+function hash(str) {
+  var hash = 0;
+  if (str.length === 0) return hash;
+  for (var i = 0; i < str.length; i++) {
+    var c = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + c;
+    hash = hash & hash; // Convert to 32-bit integer
   }
+  return hash;
 }
 
-// If the metadata music file includes embedded cover art,
-// use the thumbnail cache or extract it from the file,
-// create a blob: url for it, and set it on the specified img element.
-function displayAlbumArt(img, fileinfo) {
-  getThumbnailURL(fileinfo, function(url) {
-    if (!url)
-      return;
-    img.src = url;
-    // When the image loads make sure it is positioned correctly
-    img.addEventListener('load', function revoke(event) {
-      img.removeEventListener('load', revoke);
-      cropImage(event);
-    });
-  });
+function generateDefaultThumbnailURL(metadata) {
+  // If metadata does not contain both album and artist, then use title instead.
+  var infoForHash = (!metadata.album && !metadata.artist) ?
+    metadata.title : metadata.album + metadata.artist;
+  var hashedNumber = (Math.abs(hash(infoForHash)) % 10) + 1;
+
+  return '/style/images/AlbumArt' + hashedNumber + '_small.png';
 }
 
 // Fetch the cover art for a given file and return it as a Blob. If there's no
@@ -83,7 +51,7 @@ function displayAlbumArt(img, fileinfo) {
 // function is a bit convoluted, and we could stand to simplify things here,
 // e.g. by merging placeholder art with embedded art and/or storing cover art
 // on the sdcard.
-function getAlbumArtBlob(fileinfo, backgroundIndex, callback) {
+function getAlbumArtBlob(fileinfo, callback) {
   var getBlob = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
@@ -112,12 +80,8 @@ function getAlbumArtBlob(fileinfo, backgroundIndex, callback) {
       getBlob(url, callback);
     });
   }
-  else if (backgroundIndex || backgroundIndex === 0) {
-    var realIndex = backgroundIndex % 10 + 1;
-    var url = '/style/images/AlbumArt' + realIndex + '_small.png';
-    getBlob(url, callback);
-  }
   else {
-    callback(null, null);
+    var url = generateDefaultThumbnailURL(fileinfo.metadata);
+    getBlob(url, callback);
   }
 }
