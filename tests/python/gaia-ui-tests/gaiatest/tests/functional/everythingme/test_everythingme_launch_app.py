facc293247a99ba31f898bd38ab8e753eb986ae5
AlinT <trifandreialin@gmail.com>
2013-12-09 18:58:32 +0200

    Bug 937336 - Remove explicit frame switching from Homescreen/everything me app objects and tests

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/everythingme/test_everythingme_launch_app.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/everythingme/test_everythingme_launch_app.py
@@ -20,11 +20,11 @@ class TestEverythingMeLaunchApp(GaiaTestCase):
         homescreen = Homescreen(self.marionette)
         homescreen.launch()
 
+        self.apps.switch_to_displayed_app()
+
         search_panel = homescreen.tap_search_bar()
         search_panel.wait_for_keyboard_visible()
         search_panel.type_into_search_box(app_name)
-
-        homescreen.switch_to_homescreen_frame()
 
         search_panel.wait_for_everything_me_results_to_load()
 
