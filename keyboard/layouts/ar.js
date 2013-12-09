6f3231009074a6a24edba20035e1e18e7368061e
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2013-12-09 14:29:38 +0100

    Update ar.js

--- keyboard/layouts/ar.js
+++ keyboard/layouts/ar.js
@@ -1,7 +1,7 @@
 Keyboards.ar = {
   label: 'Arabic',
   menuLabel: 'العربية',
-  secondLayout:	true,
+  secondLayout: true,
   types: ['text', 'url', 'email'],
   alternateLayoutKey: '123',
   basicLayoutKey: 'أ ب ج',
