08756b1221dec770aeda527a751e60c8fed663ee
Rudy Lu <me@rudy.lu>
2013-12-11 07:41:35 -0800

    Merge pull request #14488 from RudyLu/keyboard/Bug888253-keyboard_settings_migration
     Bug 888253 - Settings data migration for keyboard layouts.
    r=gnarf, gary, yuren(cherry picked from commit 51ed5c6b6bda46284e797648b4b44dc9f804b14b)
    a=koi+

--- build/settings.js
+++ build/settings.js
@@ -170,29 +170,6 @@ function execute(options) {
    'icc.toneDefaultTimeout': 5000,
    'icc.goBackTimeout': 1000,
    'icc.selectTimeout': 150000,
-   'keyboard.layouts.english': true,
-   'keyboard.layouts.dvorak': false,
-   'keyboard.layouts.czech': false,
-   'keyboard.layouts.french': false,
-   'keyboard.layouts.german': false,
-   'keyboard.layouts.hungarian': false,
-   'keyboard.layouts.norwegian': false,
-   'keyboard.layouts.myanmar': false,
-   'keyboard.layouts.slovak': false,
-   'keyboard.layouts.turkish': false,
-   'keyboard.layouts.romanian': false,
-   'keyboard.layouts.russian': false,
-   'keyboard.layouts.arabic': false,
-   'keyboard.layouts.hebrew': false,
-   'keyboard.layouts.zhuyin': false,
-   'keyboard.layouts.pinyin': false,
-   'keyboard.layouts.greek': false,
-   'keyboard.layouts.japanese': false,
-   'keyboard.layouts.polish': false,
-   'keyboard.layouts.portuguese': false,
-   'keyboard.layouts.serbian': false,
-   'keyboard.layouts.spanish': false,
-   'keyboard.layouts.catalan': false,
    'keyboard.vibration': true,
    'keyboard.clicksound': true,
    'keyboard.autocorrect': true,
@@ -340,21 +317,6 @@ function execute(options) {
   }
 
   settings['language.current'] = config.GAIA_DEFAULT_LOCALE;
-  let file = utils.getGaia(config).sharedFolder.clone();
-  file.append('resources');
-  file.append('keyboard_layouts.json');
-  let keyboard_layouts_res = utils.getJSON(file);
-  let keyboard_layouts = keyboard_layouts_res['layout'];
-  let keyboard_nonLatins = keyboard_layouts_res['nonLatin'];
-  let default_layout;
-  if (config.GAIA_DEFAULT_LOCALE in keyboard_layouts) {
-    default_layout = keyboard_layouts[config.GAIA_DEFAULT_LOCALE];
-    if (!(config.GAIA_DEFAULT_LOCALE in keyboard_nonLatins)) {
-      settings['keyboard.layouts.english'] = false;
-    }
-    settings['keyboard.layouts.' + default_layout] = true;
-  }
-
   settings['devtools.debugger.remote-enabled'] = config.REMOTE_DEBUGGER == true;
 
   if (config.DEVICE_DEBUG) {
