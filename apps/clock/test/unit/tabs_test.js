1bdcdc2fb0e78f97420a6f6a94a2ef040360a197
Corey Frang <gnarf37@gmail.com>
2013-12-12 16:06:08 -0600

    Bug 921317 – [Gaia] Transform Animation in the clock app is wrong
    - Allow hashchange to initiate navigation instead of emitting 'selected' event from tabs
    - Remove emitter from tabs since it doesn't need to emit anything anymore

--- apps/clock/test/unit/tabs_test.js
+++ apps/clock/test/unit/tabs_test.js
@@ -17,38 +17,20 @@ suite('Tabs', function() {
       '<li><a href="#link-3"></a></li>';
     this.links = this.element.querySelectorAll('a');
     this.tabs = new Tabs(this.element);
-    this.selectedSpy = this.sinon.spy();
-    this.tabs.on('selected', this.selectedSpy);
   });
 
   suite('test click link 1', function() {
     setup(function() {
       this.links[0].click();
     });
-    test('no event emitted (already selected)', function() {
-      assert.equal(this.selectedSpy.callCount, 0);
-    });
   });
   suite('test click link 2', function() {
     setup(function() {
       this.links[1].click();
     });
-    test('event emitted', function() {
-      assert.equal(this.selectedSpy.callCount, 1);
-      assert.equal(this.selectedSpy.args[0][0].hash, '#link-1');
-    });
     test('moved aria-selected', function() {
       assert.isTrue(this.links[1].parentNode.hasAttribute('aria-selected'));
       assert.equal(this.element.querySelectorAll('[aria-selected]').length, 1);
-    });
-    suite('test click link 2', function() {
-      setup(function() {
-        this.selectedSpy.reset();
-        this.links[1].click();
-      });
-      test('no event emitted (already selected)', function() {
-        assert.equal(this.selectedSpy.callCount, 0);
-      });
     });
   });
 });
