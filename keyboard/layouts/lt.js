edd1fdf761aca1cc2a786e1f4a814555790d64e2
Rimas Kudelis <rq@akl.lt>
2013-12-16 20:53:11 +0200

    Added Lithuanian keyboard layout.

---
+++ keyboard/layouts/lt.js
@@ -0,0 +1,68 @@
+Keyboards.lt = {
+  label: 'Lithuanian',
+  menuLabel: 'Lietuvių',
+  imEngine: 'latin',
+  types: ['text', 'url', 'email'],
+  autoCorrectLanguage: 'lt',
+  alt: {
+    a: 'ą',
+    c: 'č',
+    e: 'ęė€',
+    i: 'į',
+    u: 'ųū',
+    s: 'š$',
+    l: '£',
+    y: '¥',
+    z: 'ž',
+    '.': ',–?!;:„“'
+  },
+  keys: [
+    [
+      { value: 'q' }, { value: 'w' }, { value: 'e' } , { value: 'r' },
+      { value: 't' } , { value: 'y' }, { value: 'u' } , { value: 'i' },
+      { value: 'o' }, { value: 'p' }
+    ], [
+      { value: 'a' }, { value: 's' }, { value: 'd' }, { value: 'f' },
+      { value: 'g' } , { value: 'h' }, { value: 'j' }, { value: 'k' },
+      { value: 'l' },
+      { value: ':', visible: ['url']}, { value: '_', visible: ['email']}
+    ], [
+      { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+      { value: 'z' }, { value: 'x' }, { value: 'c' }, { value: 'v' },
+      { value: 'b' }, { value: 'n' }, { value: 'm' },
+      { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+    ], [
+      { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+    ]
+  ],
+  alternateLayout: {
+    alt: {
+      '"': '„“',
+      '-': '–',
+      '$': '€ £ ¥ R$',
+      '?': '¿',
+      '!': '¡'
+    },
+    keys: [
+      [
+        { value: '1' }, { value: '2' }, { value: '3' } , { value: '4' },
+        { value: '5' } , { value: '6' }, { value: '7' } , { value: '8' },
+        { value: '9' }, { value: '0' }
+      ], [
+        { value: '@', hidden: ['email'] }, { value: '#' }, { value: '$' },
+        { value: '%' }, { value: '&' } , { value: '*' }, { value: '-' },
+        { value: '+' }, { value: '(' }, { value: ')' },
+        { value: '_', visible: ['email'] }
+      ], [
+        { value: 'ALT', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
+        { value: '!' }, { value: '\"' }, { value: "'" }, { value: ':' },
+        { value: ';' }, { value: '/' }, { value: '?' },
+        { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+      ], [
+        { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+        { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+      ]
+    ]
+  }
+};
