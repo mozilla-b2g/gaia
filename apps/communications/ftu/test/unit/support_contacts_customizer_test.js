69614933cd3b88ca85681b83bc4e330363390f8e
Kyle Machulis <kyle@nonpolynomial.com>
2013-12-16 17:08:01 -0800

    Bug 950876: Fix Support Contacts unit test to use Support Contacts customizer

--- apps/communications/ftu/test/unit/support_contacts_customizer_test.js
+++ apps/communications/ftu/test/unit/support_contacts_customizer_test.js
@@ -1,38 +1,26 @@
 'use strict';
 
 requireApp('communications/ftu/js/customizers/customizer.js');
-requireApp('communications/ftu/js/customizers/wallpaper_customizer.js');
 requireApp('communications/ftu/js/customizers/support_contacts_customizer.js');
 requireApp('communications/ftu/test/unit/mock_navigator_moz_settings.js');
 
 suite('Support contacts customizer >', function() {
   var createLockSpy, realSettings;
-  const SUPPORT_CONTACTS_KEYS_VALUES = [
-    {
-      key: 'support.onlinesupport.title',
-      value: 'Mozilla Support'
-    },
-    {
-      key: 'support.onlinesupport.href',
-      value: 'http://test.mozilla.org/support'
-    },
-    {
-      key: 'support.callsupport1.title',
-      value: 'Call Support (Primary)'
-    },
-    {
-      key: 'support.callsupport1.href',
-      value: 'tel:14155550001'
-    },
-    {
-      key: 'support.callsupport2.title',
-      value: 'Call Support (Secondary)'
-    },
-    {
-      key: 'support.callsupport2.href',
-      value: 'tel:14155550002'
-    }
-  ];
+  const SUPPORT_CONTACTS_KEYS_VALUES =
+          {
+            'onlinesupport': {
+              'title': 'Mozilla Support',
+              'href': 'http://test.mozilla.org/support'
+            },
+            'callsupport1': {
+              'title': 'Call Support (Primary)',
+              'href': 'tel:14155550001'
+            },
+            'callsupport2': {
+              'title': 'Call Support (Secondary)',
+              'href': 'tel:14155550002'
+            }
+          };
   suiteSetup(function() {
     realSettings = navigator.mozSettings;
     navigator.mozSettings = MockNavigatorSettings;
@@ -50,7 +38,7 @@ suite('Support contacts customizer >', function() {
   });
 
   test(' set > ', function() {
-    wallpaperCustomizer.set(SUPPORT_CONTACTS_KEYS_VALUES);
+    supportContactsCustomizer.set(SUPPORT_CONTACTS_KEYS_VALUES);
     assert.isTrue(createLockSpy.calledOnce);
   });
 });
