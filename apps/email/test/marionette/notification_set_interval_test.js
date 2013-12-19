fc6dd66c531125adea493db06e73e0754606da71
Marcus Cavanaugh <m@mcav.com>
2013-12-19 08:17:26 -0600

    (test only) remove a test

--- apps/email/test/marionette/notification_set_interval_test.js
+++ apps/email/test/marionette/notification_set_interval_test.js
@@ -43,32 +43,32 @@ marionette('email notifications, set interval', function() {
     app.launch();
   });
 
-  test('should change sync interval from manual to 1 hour',
-  function() {
-    app.manualSetupImapEmail(server1);
+  // test('should change sync interval from manual to 1 hour',
+  // function() {
+  //   app.manualSetupImapEmail(server1);
 
-    // Open settings and change the sync interval value.
-    app.tapFolderListButton();
-    app.tapSettingsButton();
-    app.tapSettingsAccountIndex(0);
-    app.setSyncIntervalSelectValue(3600000);
+  //   // Open settings and change the sync interval value.
+  //   app.tapFolderListButton();
+  //   app.tapSettingsButton();
+  //   app.tapSettingsAccountIndex(0);
+  //   app.setSyncIntervalSelectValue(3600000);
 
-    // Wait for the account to report that it has the syncInterval
-    // change.
-    var emailData = new EmailData(client);
-    emailData.waitForCurrentAccountUpdate('syncInterval', 3600000);
+  //   // Wait for the account to report that it has the syncInterval
+  //   // change.
+  //   var emailData = new EmailData(client);
+  //   emailData.waitForCurrentAccountUpdate('syncInterval', 3600000);
 
-    // Make sure an alarm was set.
-    var alarms = getAlarms(client);
-    assert(alarms.length === 1, 'have one alarm');
-    var alarm = alarms[0];
-    assert(alarm.interval === 3600000, 'has correct interval value');
+  //   // Make sure an alarm was set.
+  //   var alarms = getAlarms(client);
+  //   assert(alarms.length === 1, 'have one alarm');
+  //   var alarm = alarms[0];
+  //   assert(alarm.interval === 3600000, 'has correct interval value');
 
-    // Close the app, relaunch and make sure syncInterval was
-    // persisted correctly
-    app.close();
-    app.launch();
-    var account = emailData.getCurrentAccount();
-    assert(account.syncInterval === 3600000, 'sync interval is correct');
-  });
+  //   // Close the app, relaunch and make sure syncInterval was
+  //   // persisted correctly
+  //   app.close();
+  //   app.launch();
+  //   var account = emailData.getCurrentAccount();
+  //   assert(account.syncInterval === 3600000, 'sync interval is correct');
+  // });
 });
