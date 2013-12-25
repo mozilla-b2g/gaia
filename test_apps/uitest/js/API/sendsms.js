e2464dd8910b6e50b08f463306f29b0205c451f6
kumarrishav <reactiverishav007@gmail.com>
2013-12-25 02:14:49 -0800

    Bug 944286 identation issue fixed

--- test_apps/uitest/js/API/sendsms.js
+++ test_apps/uitest/js/API/sendsms.js
@@ -2,11 +2,11 @@ var sendSMS = document.querySelector('#send-sms');
 if (sendSMS) {
   sendSMS.onclick = function() {
     var sms = new MozActivity({
-        name: 'new', // Possible compose-sms in future versions
-        data: {
-          type: 'websms/sms',
-          number: '+46777888999'
-        }
+      name: 'new', // Possible compose-sms in future versions
+      data: {
+        type: 'websms/sms',
+        number: '+46777888999'
+      }
     });
   };
 }
