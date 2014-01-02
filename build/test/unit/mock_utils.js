f5ffc91cffe63c089d33862f2744cb5d5de11bcd
cctuan <georgeiscoming@gmail.com>
2014-01-02 14:35:54 +0800

    Bug 955940 - [Gaia][Build] Unit Test for install-gaia.js

--- build/test/unit/mock_utils.js
+++ build/test/unit/mock_utils.js
@@ -1,20 +1,25 @@
 'use strict';
 
-exports = {
-	ls: function () {
-	},
-	getDistributionFileContent: function () {
-	},
-	getFile: function () {
-	},
-	writeContent: function () {
-	},
-	getGaia: function () {
-	},
-	gaiaManifestURL: function () {
-	},
-	getJSON: function () {
-	},
-	gaiaOriginURL: function () {
-	}
+var hasRunCommands = {};
+
+exports.joinPath = function() {
+	var args = Array.prototype.slice.call(arguments);
+		return args.join('/');
+	};
+exports.Commander = function(type) {
+	hasRunCommands[type] = [];
+	this.run = function(cmds) {
+		hasRunCommands[type].push(cmds.join(' '));
+	};
+	this.initPath = function() {
+	};
+};
+
+exports.hasRunCommands = hasRunCommands;
+
+exports.psParser = function(content) {
+	return content;
+};
+
+exports.getEnvPath = function() {
 };
