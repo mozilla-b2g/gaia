a9d5938c3b6b8ac58f98510c7e85b4a9c446c4d1
millermedeiros <miller@millermedeiros.com>
2014-01-07 19:26:32 -0200

    Bug 951423 - [B2G][Calendar][Day View]Header displays truncated if date exceeds eighteen characters.

---
+++ apps/calendar/test/marionette/day_view_test.js
@@ -0,0 +1,34 @@
+var Calendar = require('./calendar'),
+    Marionette = require('marionette-client');
+    assert = require('assert');
+
+marionette('day view', function() {
+  var app;
+  var client = marionette.client();
+
+  setup(function() {
+    app = new Calendar(client);
+    app.launch();
+
+    // Hide the hint
+    app.findElement('hintSwipeToNavigate').click();
+
+    // Go to day view
+    app.findElement('dayButton').click();
+  });
+
+  test('header copy should not overflow', function() {
+    var header = app.findElement('monthYearHeader');
+    var wid = header.scriptWith(function(el) {
+      return {
+        content: el.scrollWidth,
+        container: el.getBoundingClientRect().width
+      };
+    });
+    // we need to check the widths are != 0 since element might be hidden
+    assert.ok(wid.content, 'content width is not valid');
+    assert.ok(wid.container, 'container width is not valid');
+    assert.equal(wid.content, wid.container,
+      'content is bigger than container');
+  });
+});
