afa895b68a1d2e4a62aee374cad246c387fe1976
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2014-01-07 19:34:12 +0100

    4 seconds limit for sms sounds
    this increases the limit for sms ringtones up to 4 seconds.

--- apps/system/js/notifications.js
+++ apps/system/js/notifications.js
@@ -438,7 +438,7 @@ var NotificationScreen = {
           ringtonePlayer.pause();
           ringtonePlayer.removeAttribute('src');
           ringtonePlayer.load();
-        }, 2000);
+        }, 4000);
       }
 
       if (this.vibrates) {
