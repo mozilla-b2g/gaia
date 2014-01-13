ce1df1cc305adba92cb26640b52f169645574e7b
Fernando Jiménez <ferjmoreno@gmail.com>
2014-01-13 19:18:14 +0100

    Bug 959227 - Revert UITest changes introduced in bug 929388. r=zac

--- test_apps/uitest/js/API/identity.js
+++ test_apps/uitest/js/API/identity.js
@@ -13,10 +13,6 @@ function getIssuerName() {
   return document.getElementById('issuer-name').value.trim();
 }
 
-function getWantIssuerName() {
-  return document.getElementById('wantIssuer-name').value.trim();
-}
-
 function unpackAssertion(assertion) {
   var parts = assertion.split('.');
   return {
@@ -58,13 +54,11 @@ function IdentityTests() {
   };
 
   this._setupCallbacks = function id__setupCallbacks() {
+    if (this._running) return;
     var self = this;
-    var wantIssuer = getWantIssuerName();
     try {
       navigator.mozId.watch({
         loggedInUser: null,
-
-        wantIssuer: wantIssuer,
 
         onlogin: function(assertion) {
           var unpacked = JSON.stringify(unpackAssertion(assertion), null, 2);
@@ -78,22 +72,12 @@ function IdentityTests() {
 
         onready: function() {
           self.recordEvent('ready', 'ready');
-          self._enableButtons();
         }
+
       });
     } catch (err) {
       this.recordEvent('Error: ' + err, 'error');
     }
-  };
-
-  this._enableButtons = function id__enableButtons() {
-    ['t-request', 't-request-withOnCancel',
-     't-request-allowUnverified', 't-request-forceIssuer',
-     't-request-forceIssuer-allowUnverified',
-     't-request-wantIssuer',
-     't-logout'].forEach(function(selector) {
-      document.getElementById(selector).disabled = false;
-    });
   };
 
   /**
@@ -103,16 +87,14 @@ function IdentityTests() {
     if (this._running) return;
     var self = this;
     var testElementHandlers = {
-      't-watch': function() {
-        self._setupCallbacks();
-       },
       't-request': function() {
         navigator.mozId.request();
       },
 
       't-request-withOnCancel': function() {
-        navigator.mozId.request({
-          oncancel: function() { self.recordEvent('cancel', 'cancel') }
+        navigator.mozId.request({oncancel: function() {
+                                           self.recordEvent('cancel', 'cancel');
+                                         }
         });
       },
 
@@ -135,12 +117,6 @@ function IdentityTests() {
         });
       },
 
-      't-request-wantIssuer': function() {
-        navigator.mozId.request({
-          wantIssuer: getWantIssuerName()
-        });
-      },
-
       't-logout': function() {
         navigator.mozId.logout();
       }
@@ -153,6 +129,7 @@ function IdentityTests() {
   };
 
   this.init = function id_init() {
+    this._setupCallbacks();
     this._bindEvents();
     this._running = true;
 
