c21ce927b949f6ceec814e5d4e9fe7e068233241
borjasalguero <fbsc@tid.es>
2014-01-13 17:52:32 +0000

    Bug 941655 - [Download Manager] Update time strings within download list r=kaze, arcturus

--- apps/settings/test/unit/mock_l10n.js
+++ apps/settings/test/unit/mock_l10n.js
@@ -1,18 +1,22 @@
+/* exported MockL10n*/
+
 'use strict';
 
 var MockL10n = {
   get: function get(key, params) {
     return key;
   },
-  localize: function localize() {},
+  localize: function localize(elem, l10nKey, params) {
+    elem.textContent = JSON.stringify(params);
+  },
   ready: function(callback) {
     callback();
   },
-  DateTimeFormat: function() {}
-};
-
-MockL10n.DateTimeFormat.prototype = {
-  localeFormat: function mockLocaleFormat(time, strFormat) {
-    return '' + time;
+  DateTimeFormat: function(date) {
+    return {
+      fromNow: function(date) {
+        return Date.now();
+      }
+    };
   }
 };
