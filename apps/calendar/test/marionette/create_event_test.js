df488f06c15503f3a9996aca3438a1177b1929cb
millermedeiros <miller@millermedeiros.com>
2014-01-14 23:16:29 -0200

    Bug 916411 - [fugu][Buri][Calendar]The event display abnormally when title/where/notes is long

--- apps/calendar/test/marionette/create_event_test.js
+++ apps/calendar/test/marionette/create_event_test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Calendar = require('./calendar'),
     assert = require('chai').assert;
 
@@ -7,12 +9,19 @@ marionette('creating an event', function() {
   var app;
   var client = marionette.client();
 
+  // we always use today as base day to make test simpler, we also
+  // set the hours/minutes so it always shows up at first hours of event list
+  // (avoids conflicts with click events)
   var startDate = new Date(), endDate = new Date();
-  startDate.setDate(1);
+  startDate.setHours(2);
+  startDate.setMinutes(0);
+  startDate.setSeconds(0);
+  startDate.setMilliseconds(0);
   endDate.setTime(startDate.getTime() + 60 * 60 * 1000 /* one hour */);
-  var event = {
-    title: 'Puppy Bowl',
-    location: 'Animal Planet',
+  var sourceData = {
+    title: 'Puppy Bowl dogefortlongtextfotestloremipsumdolorsitamet',
+    location: 'Animal Planet reallylongwordthatshouldnotoverflowbecausewewrap',
+    description: 'lorem ipsum dolor sit amet maecennas ullamcor',
     startDate: startDate,
     endDate: endDate
   };
@@ -20,24 +29,52 @@ marionette('creating an event', function() {
   setup(function() {
     app = new Calendar(client);
     app.launch({ hideSwipeHint: true });
+
+    app.createEvent(sourceData);
+
+    // Wait until we return to the base, month view.
+    app.waitForMonthView();
   });
 
   suite('vanilla event', function() {
-    setup(function() {
-      app.createEvent(event);
-
-      // Wait until we return to the base, month view.
-      client.waitFor(function() {
-        return app.isMonthViewActive();
-      });
-    });
-
     test('should show event in month view', function() {
       var event = app.waitForElement('monthViewDayEvent');
       var title = app.waitForChild(event, 'monthViewDayEventName');
       var location = app.waitForChild(event, 'monthViewDayEventLocation');
-      assert.equal(title.text(), 'Puppy Bowl');
-      assert.equal(location.text(), 'Animal Planet');
+      assert.equal(title.text(), sourceData.title);
+      assert.equal(location.text(), sourceData.location);
     });
   });
+
+  suite('view event', function() {
+
+    setup(function() {
+      // FIXME: temporary hack for keyboard while Bug 965131 is fixed
+      app.waitForKeyboardHide();
+      // we change to week view because some months spans through 6 rows which
+      // makes the click event on "monthViewDayEvent" trigger the wrong link
+      app.waitForElement('weekButton').click();
+      app.waitForWeekView();
+      app.waitForElement('weekViewEvent').click();
+    });
+
+    test('should display the created event in read-only view', function() {
+      var actual = app.getViewEventEvent();
+      assert.deepEqual(actual, {
+        calendar: 'Offline calendar',
+        title: sourceData.title,
+        location: sourceData.location,
+        description: sourceData.description
+      }, 'event data should match');
+    });
+
+    test('should not overflow title, location and description',
+      function() {
+        app.checkOverflow('viewEventViewTitle', 'title');
+        app.checkOverflow('viewEventViewLocation', 'location');
+        app.checkOverflow('viewEventViewDescription', 'description');
+      });
+
+  });
+
 });
