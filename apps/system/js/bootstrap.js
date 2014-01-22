c6a3f7c448636899d4e7df8786719166e462b584
Mason Chang <mchang@mozilla.com>
2014-01-21 16:38:10 -0800

    Bug 960325 - Move Homescreen Background into Homescreen App. r=alive, crdlc

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -110,18 +110,6 @@ window.addEventListener('localized', function onlocalized() {
   document.documentElement.dir = navigator.mozL10n.language.direction;
 });
 
-var wallpaperURL = new SettingsURL();
-
-// Define the default background to use for all homescreens
-SettingsListener.observe(
-  'wallpaper.image',
-  'resources/images/backgrounds/default.png',
-  function setWallpaper(value) {
-    document.getElementById('screen').style.backgroundImage =
-      'url(' + wallpaperURL.set(value) + ')';
-  }
-);
-
 // Use a setting in order to be "called" by settings app
 navigator.mozSettings.addObserver(
   'clear.remote-windows.data',
