78d3ce21d188fa734233ff6bdbbf935b1d8d2379
Kevin Grandon <kevingrandon@yahoo.com>
2014-01-24 12:49:00 -0800

    User testing updates - card/task manager work.

--- apps/system/js/rocketbar.js
+++ apps/system/js/rocketbar.js
@@ -66,9 +66,6 @@ var Rocketbar = {
 
   handleEvent: function(e) {
     switch (e.type) {
-      case 'cardchange':
-        this.searchInput.value = e.detail.title;
-        return;
       case 'cardviewclosedhome':
         // Stop listeneing for cardviewclosed if we pressed the home button
         window.removeEventListener('cardviewclosed', this);
