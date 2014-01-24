2ef3673ee9535624da923fc8007e89d4563a1bf7
Hubert Figuière <hub@figuiere.net>
2014-01-24 15:21:31 -0500

    Bug 963695 - Blacklist search (rocket bar) as it is not a standalone app.

--- tests/performance/perf.js
+++ tests/performance/perf.js
@@ -15,6 +15,7 @@ const excludedApps = [
   'fl', 'pdfjs', 'setringtone', // XXX activities
   'template', // XXX not a real thing.
   'homescreen', // we can't "launch" it
+  'search', // new rocketbar isn't standalone
   'system', // reboots the phone
   'system/camera' // copy of the camera app
 ];
