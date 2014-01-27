8a9de961e221d77b434743f36ca045a2ed5fe0b2
Alive Kuo <alegnadise@gmail.com>
2014-01-27 18:58:06 +0800

    Bug 949358 - Orientation management for callscreen + lockscreen + active app
    Conflicts:
    	apps/system/js/orientation_manager.js
    	apps/system/test/unit/orientation_manager_test.js

--- apps/system/js/sleep_menu.js
+++ apps/system/js/sleep_menu.js
@@ -151,6 +151,9 @@ var SleepMenu = {
   },
 
   hide: function lm_hide() {
+    if (!this.elements.overlay.classList.contains('visible')) {
+      return;
+    }
     this.elements.overlay.classList.remove('visible');
     // Reset the orientation for the currently running app
     var currentApp = WindowManager.getDisplayedApp();
