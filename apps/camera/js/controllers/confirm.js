497662c5281ec5fbe9a831f71c428344cf47d1b0
Diego Marcos <diego.marcos@gmail.com>
2014-01-31 00:52:51 -0800

    Bug 961620 - Pick activity sends the first picture taken instead of the last one

--- apps/camera/js/controllers/confirm.js
+++ apps/camera/js/controllers/confirm.js
@@ -31,6 +31,7 @@ function ConfirmController(app) {
   this.activity = app.activity;
   this.container = app.el;
   this.app = app;
+  this.camera = app.camera;
 
   // Allow these dependencies
   // to be injected if need be.
@@ -39,16 +40,30 @@ function ConfirmController(app) {
 
   bindAll(this);
   this.bindEvents();
+
   debug('initialized');
 }
+
+ConfirmController.prototype.renderView = function() {
+  if (this.confirmView) {
+    this.confirmView.show();
+    return;
+  }
+  this.confirmView = new this.ConfirmView();
+  this.confirmView.hide();
+  this.confirmView.render().appendTo(this.container);
+  this.confirmView.on('click:select', this.onSelectMedia);
+  this.confirmView.on('click:retake', this.confirmView.hide);
+  this.camera.resumePreview();
+};
 
 /**
  * Bind callbacks to required events.
  *
  */
 ConfirmController.prototype.bindEvents = function() {
-  this.app.on('newimage', this.onNewImage);
-  this.app.on('newvideo', this.onNewVideo);
+  this.app.on('newimage', this.onNewMedia);
+  this.app.on('newvideo', this.onNewMedia);
 };
 
 /**
@@ -56,86 +71,50 @@ ConfirmController.prototype.bindEvents = function() {
  * will present the user with a
  * confirm overlay where they can
  * decide to 'select' or 'retake'
- * the photo.
+ * the photo or video
  *
  * @param  {Object} data
  *
  */
-ConfirmController.prototype.onNewImage = function(data) {
+ConfirmController.prototype.onNewMedia = function(newMedia) {
   if (!this.activity.active) { return; }
 
-  var confirm = new this.ConfirmView();
-  var activity = this.activity;
-  var camera = this.camera;
-  var blob = data.blob;
-
-  confirm
-    .render()
-    .appendTo(this.container)
-    .setupMediaFrame()
-    .on('click:select', onSelectClick)
-    .on('click:retake', onRetakeClick);
-
-  this.prepareBlob(blob, confirm.showImage);
-
-  function onSelectClick() {
-    var width = activity.data.width;
-    var height = activity.data.height;
-    var needsResizing = width || height;
-
-    if (!needsResizing) {
-      post(blob);
-      return;
-    }
-
-    resizeImage({
-      blob: blob,
-      width: width,
-      height: height
-    }, post);
-  }
-
-  function onRetakeClick() {
-    confirm.destroy();
-    camera.resumePreview();
-  }
-
-  function post(blob) {
-    activity.postResult({
-      type: 'image/jpeg',
-      blob: blob
-    });
+  this.newMedia = newMedia;
+  this.renderView();
+  if (newMedia.isVideo) { // Is video
+    this.confirmView.showVideo(newMedia);
+  } else { // Is Image
+    this.prepareBlob(this.newMedia.blob, this.confirmView.showImage);
   }
 };
 
-ConfirmController.prototype.onNewVideo = function(video) {
-  if (!this.activity.active) { return; }
-
-  var ConfirmView = this.ConfirmView;
-  var confirm = new ConfirmView();
+ConfirmController.prototype.onSelectMedia = function() {
+  var needsResizing;
   var activity = this.activity;
-  var camera = this.camera;
+  var media = {
+    blob: this.newMedia.blob
+  };
 
-  confirm
-    .render()
-    .appendTo(this.container)
-    .setupMediaFrame()
-    .showVideo(video)
-    .on('click:select', onSelectClick)
-    .on('click:retake', onRetakeClick);
-
-  function onSelectClick() {
-    activity.postResult({
-      type: 'video/3gpp',
-      blob: video.blob,
-      poster: video.poster.blob
-    });
+  if (this.newMedia.isVideo) { // Is Video
+    media.type = 'video/3gpp';
+    media.poster = this.newMedia.poster.blob;
+  } else { // Is Image
+    media.type = 'image/jpeg';
+    needsResizing = this.newMedia.width || this.newMedia.height;
+    if (needsResizing) {
+      resizeImage({
+        blob: this.newMedia.blob,
+        width: this.newMedia.width,
+        height: this.newMedia.height
+      }, function(newBlob) {
+        media.blob = newBlob;
+        activity.postResult(media);
+      });
+      return;
+    }
   }
+  activity.postResult(media);
 
-  function onRetakeClick() {
-    camera.resumePreview();
-    confirm.destroy();
-  }
 };
 
 });
