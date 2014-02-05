467ef8c9145d9a57d35b0619db541d23b522b958
Germán Toro del Valle <gtorodelvalle@gmail.com>
2014-02-05 15:33:38 +0100

    Merge pull request #15950 from gtorodelvalle/contacts-967540-invalid-dates-in-gmail
    Bug 967540 - [Contacts] Exporting to SD card contacts imported from Gmail with invalid dates never ends
    (cherry picked from commit 95cea779da64742c1f46da1a628c8c7a21604d8e)
    
    Conflicts:
    	build/jshint/xfail.list

--- apps/communications/gmail/test/unit/gmail_connector_test.js
+++ apps/communications/gmail/test/unit/gmail_connector_test.js
@@ -1,3 +1,6 @@
+/* globals GmailConnector, MockGoogleEntry, MockGoogleEntryInvalidDate,
+    MockGoogleEntryNoName, MockGoogleGroups, MockGoogleListing, MockRest */
+
 'use strict';
 
 requireApp('communications/gmail/js/gmail_connector.js');
@@ -6,14 +9,13 @@ requireApp('communications/gmail/test/unit/mock_listing.js');
 requireApp('communications/gmail/test/unit/mock_rest.js');
 requireApp('communications/gmail/test/unit/mock_groups.js');
 
-if (!this.Rest) {
-  this.Rest = null;
+if (!window.Rest) {
+  window.Rest = null;
 }
 
 suite('Gmail Connector', function() {
 
   var subject,
-      listing,
       END_POINT = 'https://www.google.com/m8/feeds/contacts/' +
         'default/full/?max-results=10000',
       GROUP_ID = 'https://www.google.com/m8/feeds/groups/' +
@@ -64,21 +66,34 @@ suite('Gmail Connector', function() {
 
       assert.isNotNull(result);
       assert.equal(result.uid,
-        'http://www.google.com/m8/feeds/contacts/mepartoconmigo%40gmail.com/base/2fc27a388c2bd974');
+                   'http://www.google.com/m8/feeds/contacts/' +
+                   'mepartoconmigo%40gmail.com/base/2fc27a388c2bd974');
       assert.deepEqual(result.givenName, ['My']);
       assert.deepEqual(result.familyName, ['Contact']);
       assert.equal(result.email1, 'juan@palomo.es');
     });
 
     test('Parse for display without name', function() {
-      var contact = subject.gContactToJson(MockGoogleEntryWoName);
+      var contact = subject.gContactToJson(MockGoogleEntryNoName);
       var result = subject.adaptDataForShowing(contact);
 
       assert.isNotNull(result);
       assert.equal(result.uid,
-        'http://www.google.com/m8/feeds/contacts/mepartoconmigo%40gmail.com/base/2fc27a388c2bd974');
+                   'http://www.google.com/m8/feeds/contacts/' +
+                   'mepartoconmigo%40gmail.com/base/2fc27a388c2bd974');
       assert.deepEqual(result.givenName, '+1555111333444222');
       assert.equal(result.email1, 'juan@palomo.es');
+    });
+
+    test('Parse for display with invalid birthday date', function() {
+      var contact = subject.gContactToJson(MockGoogleEntryInvalidDate);
+      var result = subject.adaptDataForShowing(contact);
+
+      assert.isNotNull(result);
+      assert.equal(result.uid,
+                   'http://www.google.com/m8/feeds/contacts/' +
+                   'mepartoconmigo%40gmail.com/base/2fc27a388c2bd974');
+      assert.isUndefined(result.bday);
     });
 
     test('Parse for saving', function() {
@@ -144,7 +159,7 @@ suite('Gmail Connector', function() {
     });
 
     test('Parse for saving without name', function() {
-      var contact = subject.gContactToJson(MockGoogleEntryWoName);
+      var contact = subject.gContactToJson(MockGoogleEntryNoName);
       var result = subject.adaptDataForSaving(contact);
 
       assert.isNotNull(result);
@@ -199,6 +214,67 @@ suite('Gmail Connector', function() {
         'mepartoconmigo%40gmail.com/base/2fc27a388c2bd974',
         result.url[0].value);
 
+    });
+
+    test('Parse for saving with invalid birthday date', function() {
+      var contact = subject.gContactToJson(MockGoogleEntryInvalidDate);
+      var result = subject.adaptDataForSaving(contact);
+
+      assert.isNotNull(result);
+      assert.isNotNull(result.givenName);
+      assert.length(result.givenName, 1);
+      assert.equal(result.givenName[0], 'My');
+
+      assert.isNotNull(result.familyName);
+      assert.length(result.familyName, 1);
+      assert.equal(result.familyName[0], 'Contact');
+
+      assert.isNotNull(result.email);
+      assert.length(result.email, 2);
+      assert.equal('juan@palomo.es', result.email[0].value);
+      assert.equal('home', result.email[0].type);
+      assert.equal('workemail@email.com', result.email[1].value);
+      assert.equal('work', result.email[1].type);
+
+      assert.isNotNull(result.adr);
+      assert.length(result.adr, 1);
+      assert.equal('This is the Street', result.adr[0].streetAddress);
+      assert.equal('The City', result.adr[0].locality);
+      assert.equal('The State', result.adr[0].region);
+      assert.equal('The Zip', result.adr[0].postalCode);
+      assert.equal('The Country', result.adr[0].countryName);
+
+      assert.isNotNull(result.tel);
+      assert.length(result.tel, 1);
+      assert.equal('+1555111333444222', result.tel[0].value);
+      assert.equal('work', result.tel[0].type);
+
+      assert.isNotNull(result.org);
+      assert.length(result.org, 1);
+      assert.equal('The Company', result.org[0]);
+
+      assert.isNotNull(result.jobTitle);
+      assert.length(result.jobTitle, 1);
+      assert.equal('Title in Company', result.jobTitle[0]);
+
+      assert.isUndefined(result.bday);
+
+      assert.isNotNull(result.note);
+      assert.length(result.note, 1);
+      assert.equal('This is a Note', result.note[0]);
+
+      assert.isNotNull(result.category);
+      assert.length(result.category, 1);
+      assert.equal('gmail', result.category[0]);
+
+      assert.isNotNull(result.url);
+      assert.length(result.url, 1);
+      assert.length(result.url[0].type, 1);
+      assert.equal('source', result.url[0].type[0]);
+      assert.equal('urn:service:gmail:uid:' +
+        'http://www.google.com/m8/feeds/contacts/' +
+        'mepartoconmigo%40gmail.com/base/2fc27a388c2bd974',
+        result.url[0].value);
     });
   });
 });
