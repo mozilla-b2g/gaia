0512175c1492f70d42c9bb1a5ccc30bfd73cab69
Timothy Guan-tin Chien <timdream@gmail.com>
2014-02-05 21:55:50 +0800

    Bug 968187 - Localize manifests copied from build_stage

--- build/copy-build-stage-manifest.js
+++ build/copy-build-stage-manifest.js
@@ -14,9 +14,18 @@ function execute(options) {
     var manifest = app.buildDirectoryFile.clone();
     manifest.append(WEBAPP_FILENAME);
     if (manifest.exists()) {
-      var webappTargetDir = webappsBaseDir.clone();
-      webappTargetDir.append(app.domain);
-      manifest.copyTo(webappTargetDir, WEBAPP_FILENAME);
+      if (gaia.l10nManager) {
+        var manifestContent = gaia.l10nManager.localizeManifest(app);
+        var targetManifest = webappsBaseDir.clone();
+        targetManifest.append(app.domain);
+        targetManifest.append(WEBAPP_FILENAME)
+
+        utils.writeContent(targetManifest, JSON.stringify(manifestContent));
+      } else {
+        var webappTargetDir = webappsBaseDir.clone();
+        webappTargetDir.append(app.domain);
+        manifest.copyTo(webappTargetDir, WEBAPP_FILENAME);
+      }
     }
   })
 }
