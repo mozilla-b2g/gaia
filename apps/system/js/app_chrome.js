cf027f024f5acf9042bda67323c7b48b8b8148ac
Jan Jongboom <janjongboom@gmail.com>
2014-02-06 09:58:58 +0100

    yolo

--- apps/system/js/app_chrome.js
+++ apps/system/js/app_chrome.js
@@ -27,6 +27,8 @@
     if (this.app.config.chrome && this.app.config.chrome.rocketbar) {
       this.app.element.classList.add('rocketbar');
     }
+
+    this.lastTop = 0;
   };
 
   AppChrome.prototype.__proto__ = window.BaseUI.prototype;
@@ -40,9 +42,20 @@
   AppChrome.prototype.hidingNavigation = false;
 
   AppChrome.prototype.view = function an_view() {
+    var ctx = '<div class="context-menu">' +
+        '<div class="item reload">Reload</div>' +
+        '<div class="item new">New Window</div>' +
+        '<div class="item homescreen">Add to Home Screen</div>' +
+        '<div class="item share">Share</div>' +
+        '<div class="item forward">Forward</div>' +
+        '<div class="item settings">Settings</div>' +
+        '<div class="close">Close</div>' +
+      '</div>';
+
     return '<div class="chrome" id="' +
             this.CLASS_NAME + this.instanceID + '">' +
             '<header class="progress"></header>' +
+            ctx +
             '<footer class="navigation visible">' +
               '<menu type="buttonbar">' +
                 '<button type="button" class="back-button"' +
@@ -68,6 +81,7 @@
     this.reloadButton = this.element.querySelector('.reload-button');
     this.forwardButton = this.element.querySelector('.forward-button');
     this.backButton = this.element.querySelector('.back-button');
+    this.closeButton = this.element.querySelector('.close-button');
   };
 
   AppChrome.prototype.handleEvent = function ac_handleEvent(evt) {
@@ -144,6 +158,10 @@
       case this.bookmarkButton:
         this.addBookmark();
         break;
+
+      case this.closeButton:
+        this.toggleContextMenu();
+        break;
     }
   };
 
@@ -152,6 +170,7 @@
     this.forwardButton.addEventListener('click', this);
     this.backButton.addEventListener('click', this);
     this.bookmarkButton.addEventListener('click', this);
+    this.closeButton.addEventListener('click', this);
     this.app.element.addEventListener('mozbrowserlocationchange', this);
     this.app.element.addEventListener('mozbrowserasyncscroll', this);
     this.app.element.addEventListener('_loading', this);
@@ -162,6 +181,19 @@
     this.app.element.addEventListener('_withoutkeyboard', this);
     this.app.element.addEventListener('_homegesture-enabled', this);
     this.app.element.addEventListener('_homegesture-disabled', this);
+
+    var app = this.app;
+    var ctx = this.app.frame.querySelector('.context-menu');
+
+    // Always hide when click on any of the items
+    ctx.onclick = this.toggleContextMenu.bind(this);
+
+    ctx.querySelector('.reload').onclick = app.reload.bind(app);
+    ctx.querySelector('.share').onclick = alert.bind(window, 'Share');
+    ctx.querySelector('.new').onclick = alert.bind(window, 'New window');
+    ctx.querySelector('.forward').onclick = app.forward.bind(app);
+    ctx.querySelector('.homescreen').onclick = this.addBookmark.bind(this);
+    ctx.querySelector('.settings').onclick = alert.bind(window, 'Settings');
   };
 
   AppChrome.prototype._unregisterEvents = function ac__unregisterEvents() {
@@ -169,6 +201,7 @@
     this.forwardButton.removeEventListener('click', this);
     this.backButton.removeEventListener('click', this);
     this.bookmarkButton.removeEventListener('click', this);
+    this.closeButton.removeEventListener('click', this);
     if (!this.app)
       return;
     this.app.element.removeEventListener('mozbrowserlocationchange', this);
@@ -237,21 +270,29 @@
           this.backButton.dataset.disabled = true;
         }
       }.bind(this));
+
+      this.app.frame.classList.remove('context-menu');
     };
 
   AppChrome.prototype.handleScroll = function ac_scroll(evt) {
     if (!this.app)
       return;
 
-    if (evt.detail.top > this.lastTop) {
+    if (this.app.frame.classList.contains('has-context-menu'))
+      return;
+
+    // Scrolling threshold before we do anything with the navigation bar
+    var threshold = 20;
+
+    if (evt.detail.top - threshold > this.lastTop) {
       // scrolling down
       this.app.frame.classList.remove('navigation');
+      this.lastTop = evt.detail.top;
     }
-    else if (evt.detail.top < this.lastTop) {
+    else if (evt.detail.top + threshold < this.lastTop) {
       this.app.frame.classList.add('navigation');
+      this.lastTop = evt.detail.top;
     }
-
-    this.lastTop = evt.detail.top;
   };
 
   AppChrome.prototype.addBookmark = function ac_addBookmark() {
@@ -319,5 +360,27 @@
     }
 
     ModalDialog.selectOne(data, selected);
+  };
+
+  AppChrome.prototype.toggleContextMenu = function() {
+    var self = this;
+
+    if (!self.app) return;
+
+    self.app.frame.classList.add('no-animate');
+    setTimeout(function() {
+      self.app.frame.classList.remove('no-animate');
+    });
+
+    // closing menu? show navigation
+    if (self.app.frame.classList.contains('has-context-menu')) {
+      self.app.frame.classList.add('navigation');
+    }
+    // opening? hide it.
+    else {
+      self.app.frame.classList.remove('navigation');
+    }
+
+    self.app.frame.classList.toggle('has-context-menu');
   };
 }(this));
