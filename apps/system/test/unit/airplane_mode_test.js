bb89c1fcac6ecf4102ae87d56dd37e708994182e
tzhuang <tzhuang@mozilla.com>
2014-02-06 15:25:06 +0800

    Bug 938080 - set 'xyz'.suspended to false if user toggle 'xyz'.enabled on manually

--- apps/system/test/unit/airplane_mode_test.js
+++ apps/system/test/unit/airplane_mode_test.js
@@ -354,11 +354,125 @@ suite('system/airplane_mode.js', function() {
     });
   });
 
+  suite('AirplaneModeServiceHelper', function() {
+    test('init and _initSetting should set initial values of _settings',
+      function() {
+        AirplaneMode._serviceHelper.init();
+        // SettingsListener is a mock here and would never observe any changes.
+        // We need to trigger callbacks manually to make initSettings work
+        servicesIterator(function(key) {
+          SettingsListener.mTriggerCallback(key + '.enabled', false);
+          SettingsListener.mTriggerCallback(key + '.suspended', false);
+        });
+        // check all values in _settings
+        servicesIterator(function(key) {
+          assert.ok(getSettingOnServiceHelper(key + '.enabled') === false);
+          assert.ok(getSettingOnServiceHelper(key + '.suspended') === false);
+        });
+    });
+
+    suite('_suspend should work as expected when services are enabled',
+      function() {
+        setup(function() {
+          AirplaneMode._serviceHelper.init();
+          setAllSettingsOnServiceHelper({enabled: true, suspended: false});
+        });
+        test('turn on airplane mode, thus all ".enabled" should be false ' +
+          'and all ".suspended" should be true', function() {
+            // enable airplane mode, thus suspend all services
+            servicesIterator(function(key) {
+              AirplaneMode._serviceHelper._suspend(key);
+            });
+            servicesIterator(function(key) {
+              assert.ok(
+                MockNavigatorSettings.mSettings[key + '.enabled'] === false);
+              assert.ok(
+                MockNavigatorSettings.mSettings[key + '.suspended'] === true);
+            });
+        });
+    });
+
+    suite('_unsuspend should work as expected when airplane mode is on',
+      function() {
+        setup(function() {
+          this.sinon.spy(AirplaneMode._serviceHelper, '_unsuspend');
+          AirplaneMode._serviceHelper.init();
+          setAllSettingsOnServiceHelper({enabled: true, suspended: false});
+          // enable airplane mode, thus suspend all services
+          servicesIterator(function(key) {
+            AirplaneMode._serviceHelper._suspend(key);
+          });
+        });
+        teardown(function() {
+          AirplaneMode._serviceHelper._unsuspend.restore();
+        });
+        // expand the tests to all services
+        servicesIterator(function(key) {
+          test('turn on ' + key + ', "' + key + '.suspended" should be false ' +
+            'and _unsuspend should be called', function() {
+              var sset = JSON.parse('{"' + key + '.enabled": true}');
+              sinon.assert.notCalled(AirplaneMode._serviceHelper._unsuspend);
+              // toggle service (identified by key) on
+              MockNavigatorSettings.createLock().set(sset);
+              assert.ok(
+                MockNavigatorSettings.mSettings[key + '.enabled'] === true);
+              assert.ok(
+                MockNavigatorSettings.mSettings[key + '.suspended'] === false);
+              assert.ok(getSettingOnServiceHelper(key + '.enabled') === true);
+              assert.ok(
+                getSettingOnServiceHelper(key + '.suspended') === false);
+              sinon.assert.called(AirplaneMode._serviceHelper._unsuspend);
+          });
+        });
+      });
+
+    suite('_restore should work as expected', function() {
+      setup(function() {
+        AirplaneMode._serviceHelper.init();
+        // turn on all services
+        setAllSettingsOnServiceHelper({enabled: true, suspended: false});
+        // enable airplane mode, thus suspend all services
+        servicesIterator(function(key) {
+          AirplaneMode._serviceHelper._suspend(key);
+        });
+      });
+      test('turn on all services, then turn on airplane mode, ' +
+        'and turn off airplane mode. All ".suspended" and ".enabled" ' +
+        'should be just the same', function() {
+          // disable airplane mode, thus _restore all services
+          servicesIterator(function(key) {
+            AirplaneMode._serviceHelper._restore(key);
+          });
+          // all '.suspended' should be false, and all '.enabled' should be true
+          servicesIterator(function(key) {
+            assert.ok(
+              getSettingOnServiceHelper(key + '.enabled') === true);
+            assert.ok(
+              getSettingOnServiceHelper(key + '.suspended') === false);
+          });
+      });
+    });
+  });
 
   // test helpers
 
+  function servicesIterator(callback) {
+    ['ril.data', 'bluetooth', 'wifi', 'geolocation', 'nfc'].forEach(callback);
+  }
+
   function setSettingOnServiceHelper(key, value) {
     AirplaneMode._serviceHelper._settings[key] = value;
+  }
+
+  function setAllSettingsOnServiceHelper(values) {
+    servicesIterator(function(key) {
+      setSettingOnServiceHelper(key + '.enabled', values.enabled);
+      setSettingOnServiceHelper(key + '.suspended', values.suspended);
+    });
+  }
+
+  function getSettingOnServiceHelper(key) {
+    return AirplaneMode._serviceHelper._settings[key];
   }
 
   function setConnection(connIndex, status) {
