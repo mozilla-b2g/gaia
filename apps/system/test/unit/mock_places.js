4b15569db08fc7d55ebe86143cd660817de5a8aa
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2014-02-09 23:46:51 +0100

    Bug 952912 - Check for update is on even for NOFTU builds.

---
+++ apps/system/test/unit/mock_places.js
@@ -0,0 +1,16 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+/*exported MockPlaces*/
+
+'use strict';
+
+var MockPlaces = (function() {
+
+  function init() {
+  }
+
+  return {
+    init: init
+  };
+})();
