2c890bbb9fdcc81f7df5be07e57448fe15259e05
Hubert Figuière <hub@figuiere.net>
2014-02-12 23:16:44 -0500

    Bug 972166 - MemInfo was not safe to call twice. r=julienw

--- tests/performance/meminfo.js
+++ tests/performance/meminfo.js
@@ -20,84 +20,82 @@ function isNotEmpty(str) {
 
 var MemInfo = {
 
-  _meminfo: [],
-
-  /* keys for fields */
-  _keys: [],
-
-  _currentLineNumber: 0,
-  /* state in parser */
-  _state: 'none', // 'header', 'ps', 'done'
-
-  _parsePsLine: function MemInfo_parsePsLine(line, keys) {
-    var fields = line.split(' ').filter(isNotEmpty);
-    if (fields.length < keys.length) {
-      console.error('Not enough fields given the number of keys.');
-      return null;
-    }
-
-    var numKeys = keys.length;
-    var last = fields.length - 1;
-    var idx = last;
-    var ps = {};
-    while(Object.keys(ps).length < numKeys) {
-      ps[keys[numKeys - 1 - (last - idx)]] = fields[idx];
-      idx--;
-    }
-
-    // now fix the process name as we have some with spaces in it
-    // oh the joy of parsing text output.
-    if(idx >= 0) {
-      fields.splice(idx + 1, fields.length);
-      var s = '';
-      fields.forEach(function (e) {
-        s += (s.length ? ' ' : '') + e;
-      });
-      ps[keys[0]] = s + ' ' + ps[keys[0]];
-    }
-    return ps;
-  },
-
-  _processLine: function(line) {
-    this._currentLineNumber++;
-    switch(this._state) {
-
-    case 'none':
-      var regex = /bytes/;
-      var r = line.match(regex);
-      if(r) {
-        this._state = 'header';
-      }
-      return;
-
-    case 'header':
-      this._keys = line.split(' ').filter(isNotEmpty);
-      this._state = 'ps';
-      break;
-
-    case 'ps':
-      if (line == '') {
-        // an empty line means we are done with ps.
-        // if we need more info change the state to something else.
-        this._state = 'done';
-        return;
-      }
-
-      var ps = this._parsePsLine(line, this._keys);
-      if(ps != null) {
-        this._meminfo.push(ps);
-      }
-      break;
-
-    case 'done':
-      return;
-
-    default:
-      break;
-    }
-  },
-
   meminfo: function MemInfo_meminfo() {
+
+    var _meminfo = [];
+
+    /* keys for fields */
+    var _keys = [];
+
+    /* state in parser */
+    var _state = 'none'; // 'header', 'ps', 'done'
+
+    function _parsePsLine(line, keys) {
+      var fields = line.split(' ').filter(isNotEmpty);
+      if (fields.length < keys.length) {
+        console.error('Not enough fields given the number of keys.');
+        return null;
+      }
+
+      var numKeys = keys.length;
+      var last = fields.length - 1;
+      var idx = last;
+      var ps = {};
+      while(Object.keys(ps).length < numKeys) {
+        ps[keys[numKeys - 1 - (last - idx)]] = fields[idx];
+        idx--;
+      }
+
+      // now fix the process name as we have some with spaces in it
+      // oh the joy of parsing text output.
+      if(idx >= 0) {
+        fields.splice(idx + 1, fields.length);
+        var s = '';
+        fields.forEach(function (e) {
+          s += (s.length ? ' ' : '') + e;
+        });
+        ps[keys[0]] = s + ' ' + ps[keys[0]];
+      }
+      return ps;
+    }
+
+    function _processLine(line) {
+      switch(_state) {
+
+      case 'none':
+        var regex = /bytes/;
+        var r = line.match(regex);
+        if(r) {
+          _state = 'header';
+        }
+        return;
+
+      case 'header':
+        _keys = line.split(' ').filter(isNotEmpty);
+        _state = 'ps';
+        break;
+
+      case 'ps':
+        if (line == '') {
+          // an empty line means we are done with ps.
+          // if we need more info change the state to something else.
+          _state = 'done';
+          return;
+        }
+
+        var ps = _parsePsLine(line, _keys);
+        if(ps != null) {
+          _meminfo.push(ps);
+        }
+        break;
+
+      case 'done':
+        return;
+
+      default:
+        break;
+      }
+    }
 
     var output = b2ginfo().stdout;
 
@@ -108,12 +106,11 @@ var MemInfo = {
       if (j == -1) {
         j = output.length;
       }
-      this._processLine(output.substr(i, j - i));
+      _processLine(output.substr(i, j - i));
       // skip 2 (CRLF)
       i = j + 2;
     }
-
-    return this._meminfo;
+    return _meminfo;
   }
 
 };
