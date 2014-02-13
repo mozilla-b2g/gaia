999808c18d2fc95f14d65b925f25237e78436f56
Timothy Guan-tin Chien <timdream@gmail.com>
2014-02-13 13:52:44 +0800

    Revert "Merge pull request #15808 from pmclanahan/send-language-to-basket-964342"
    This reverts commit 22239db3cac45568b07a84a12a15a3a5b31cabc3.

--- apps/communications/ftu/js/basket-client.js
+++ apps/communications/ftu/js/basket-client.js
@@ -9,22 +9,6 @@ var Basket = {
   callback: null,
   xhr: null,
   itemId: 'newsletter_email',
-  // https://github.com/mozilla/basket-client/blob/master/basket/errors.py
-  errors: {
-    NETWORK_FAILURE: 1,
-    INVALID_EMAIL: 2,
-    UNKNOWN_EMAIL: 3,
-    UNKNOWN_TOKEN: 4,
-    USAGE_ERROR: 5,
-    EMAIL_PROVIDER_AUTH_FAILURE: 6,
-    AUTH_ERROR: 7,
-    SSL_REQUIRED: 8,
-    INVALID_NEWSLETTER: 9,
-    INVALID_LANGUAGE: 10,
-    EMAIL_NOT_CHANGED: 11,
-    CHANGE_REQUEST_NOT_FOUND: 12,
-    UNKNOWN_ERROR: 99
-  },
 
   responseHandler: function b_responseHandler() {
     if (this.xhr.readyState === 4) {
@@ -64,12 +48,7 @@ var Basket = {
                               'application/x-www-form-urlencoded');
     this.xhr.setRequestHeader('Content-length', params.length);
     this.xhr.setRequestHeader('Connection', 'close');
-    this.getLanguage(function do_send(language) {
-      if (language) {
-        params += '&lang=' + language;
-      }
-      this.xhr.send(params);
-    });
+    this.xhr.send(params);
   },
 
   store: function b_store(email, callback) {
@@ -77,26 +56,6 @@ var Basket = {
       if (callback)
         callback();
     });
-  },
-
-  getLanguage: function b_getLanguage(callback) {
-    var settings = window.navigator.mozSettings;
-    if (!settings || !settings.createLock) {
-      callback();
-      return;
-    }
-
-    var s_name = 'language.current';
-    var req = settings.createLock().get(s_name);
-
-    req.onsuccess = function _onsuccess() {
-      callback(req.result[s_name]);
-    };
-
-    req.onerror = function _onerror() {
-      console.error('Error getting setting: ' + s_name);
-      callback();
-    };
   }
 
 };
