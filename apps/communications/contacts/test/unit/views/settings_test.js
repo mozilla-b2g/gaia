e65e0211a0cd46968af750eb44bed4e4711864bf
crdlc <crdlc@tid.es>
2014-02-14 10:35:19 +0100

    Bug 959414 - [B2G][Contacts] Cancelling out of editing or Updating a contact after importing displays broken UI

--- apps/communications/contacts/test/unit/views/settings_test.js
+++ apps/communications/contacts/test/unit/views/settings_test.js
@@ -237,7 +237,7 @@ suite('Contacts settings', function() {
       function(done) {
         var observer = new MutationObserver(function(record) {
           observer.disconnect();
-          assert.isTrue(record[0].target.classList.contains('visible'));
+          assert.isTrue(record[0].target.classList.contains('opening'));
           done();
         });
         observer.observe(document.getElementById('statusMsg'), {
