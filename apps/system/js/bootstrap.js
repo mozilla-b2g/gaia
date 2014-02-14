9a4fe922cc70e898d2a63c859e4e1e7e34d75fbb
EragonJ <eragonj@eragonj.me>
2014-02-14 16:04:43 +0800

    Bug 971565 - [System2] Instantiable Title
    - update test
    - update bootstrap.js

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -63,6 +63,8 @@ window.addEventListener('load', function startup() {
   window.devtoolsView = new DevtoolsView();
   window.ttlView = new TTLView();
 
+  window.title = new Title();
+
   // We need to be sure to get the focus in order to wake up the screen
   // if the phone goes to sleep before any user interaction.
   // Apparently it works because no other window has the focus at this point.
