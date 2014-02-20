1db7a4857faec427799de0827964128b445e80ea
EragonJ <eragonj@eragonj.me>
2014-02-20 18:23:12 +0800

    Bug 974828 - [Settings][dsds] Outgoing data confirmation is wrong

--- apps/settings/test/unit/simcard_manager_test.js
+++ apps/settings/test/unit/simcard_manager_test.js
@@ -1,5 +1,5 @@
 /* global mocha, MockL10n, MockTemplate, MockSimUIModel,
-   SimUIModel, MockSimSettingsHelper, SimCardManager,
+   SimUIModel, MockSimSettingsHelper, SimCardManager, SimSettingsHelper,
    MockNavigatorMozIccManager, MockNavigatorMozMobileConnections,
    MockMobileOperator, MockNavigatorSettings, MockAirplaneModeHelper, test,
    requireApp, suite, suiteTeardown, suiteSetup, setup, assert, sinon */
@@ -21,7 +21,8 @@ requireApp('settings/test/unit/mock_template.js');
 requireApp('settings/test/unit/mock_simcard_manager_simcard_helper.js');
 
 mocha.globals(['Template', 'SimUIModel', 'SimCardManager',
-  'SimSettingsHelper', 'MobileOperator', 'localize', 'AirplaneModeHelper']);
+  'SimSettingsHelper', 'MobileOperator', 'localize', 'AirplaneModeHelper',
+  'confirm']);
 
 suite('SimCardManager > ', function() {
   var realL10n;
@@ -123,6 +124,83 @@ suite('SimCardManager > ', function() {
     stubById.restore();
   });
 
+  suite('addOutgoingDataSelectEvent > ', function() {
+    var select;
+
+    function triggerEventOnSelect(evtName) {
+      if (select.addEventListener.withArgs(evtName)) {
+        var cb = select.addEventListener.withArgs(evtName).args[0][1];
+        cb.call(select);
+      }
+    }
+
+    setup(function() {
+      select = document.createElement('select');
+
+      [0, 1].forEach(function(index) {
+        var option = document.createElement('option');
+        option.text = index;
+        option.value = index;
+        if (index === 0) {
+          option.selected = true;
+        }
+        select.add(option);
+      });
+
+      SimCardManager.simManagerOutgoingDataSelect = select;
+      this.sinon.stub(select, 'addEventListener');
+      this.sinon.stub(SimSettingsHelper, 'setServiceOnCard');
+
+      SimCardManager.addOutgoingDataSelectEvent();
+    });
+
+    suite('if there is no change in option', function() {
+      setup(function() {
+        this.sinon.stub(window, 'confirm', function() {
+          return false;
+        });
+
+        triggerEventOnSelect('focus');
+        select.selectedIndex = 0;
+        triggerEventOnSelect('blur');
+      });
+      test('nothing happened', function() {
+        assert.isFalse(SimSettingsHelper.setServiceOnCard.called);
+      });
+    });
+
+    suite('if option is changed, but users don\'t confirm it', function() {
+      setup(function() {
+        this.sinon.stub(window, 'confirm', function() {
+          return false;
+        });
+
+        triggerEventOnSelect('focus');
+        select.selectedIndex = 1;
+        triggerEventOnSelect('blur');
+      });
+      test('we would set the select back to original value', function() {
+        assert.equal(select.selectedIndex, 0);
+        assert.isFalse(SimSettingsHelper.setServiceOnCard.called);
+      });
+    });
+
+    suite('if option is chagned, and users confirm it', function() {
+      setup(function() {
+        this.sinon.stub(window, 'confirm', function() {
+          return true;
+        });
+
+        triggerEventOnSelect('focus');
+        select.selectedIndex = 1;
+        triggerEventOnSelect('blur');
+      });
+      test('we would set cardIndex on mozSettings', function() {
+        assert.equal(select.selectedIndex, 1);
+        assert.isTrue(SimSettingsHelper.setServiceOnCard.called);
+      });
+    });
+  });
 
   // add test below
   suite('init > ', function() {
@@ -135,6 +213,8 @@ suite('SimCardManager > ', function() {
       this.sinon.stub(SimCardManager, 'addCardStateChangeEventOnIccs');
       this.sinon.stub(SimCardManager, 'addAirplaneModeChangeEvent');
       this.sinon.stub(SimCardManager, 'addVoiceChangeEventOnConns');
+      this.sinon.stub(SimCardManager, 'addOutgoingDataSelectEvent');
+
       SimCardManager.init();
     });
 
@@ -143,14 +223,10 @@ suite('SimCardManager > ', function() {
         SimCardManager.simManagerOutgoingCallSelect;
       var outgoingMessages =
         SimCardManager.simManagerOutgoingMessagesSelect;
-      var outgoingData =
-        SimCardManager.simManagerOutgoingDataSelect;
 
       assert.equal(outgoingCall.addEventListener.lastCall.args[0],
         'change');
       assert.equal(outgoingMessages.addEventListener.lastCall.args[0],
-        'change');
-      assert.equal(outgoingData.addEventListener.lastCall.args[0],
         'change');
     });
 
