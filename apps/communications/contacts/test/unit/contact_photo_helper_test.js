3b762ced7106c097ced465b6df68da2b35582667
Jose M. Cantera <jmcanterafonseca@gmail.com>
2014-02-20 10:05:56 +0100

    Bug 974646 - [B2G][Contacts] Linking a Facebook contact to existing contact shows local image on contacts list, but Facebook picture in contact details

--- apps/communications/contacts/test/unit/contact_photo_helper_test.js
+++ apps/communications/contacts/test/unit/contact_photo_helper_test.js
@@ -6,9 +6,11 @@ require('/shared/js/contact_photo_helper.js');
 
 suite('shared/ContactPhotoHelper >', function() {
   var contactWithoutPhoto, contactWithString, contactWithoutThumbnail,
-      contactWithThumbnail, cntactWithExtra;
+      contactWithThumbnail, cntactWithExtra, cntactFbLinkedOnePhoto,
+      cntactFbLinkedTwoPhotos, cntactFbLinkedAllPhotos;
 
-  var dataURL, fullResolution, thumbnail, extra;
+  var dataURL, fullResolution, thumbnail, extra,
+      thumbnailFb, fullResolutionFb;
   var subject;
 
   setup(function() {
@@ -16,6 +18,9 @@ suite('shared/ContactPhotoHelper >', function() {
     fullResolution = new Blob(['1234567890'], {type: 'image/png'});
     thumbnail = new Blob(['123'], {type: 'image/png'});
     extra = new Blob(['12345'], {type: 'image/png'});
+
+    fullResolutionFb = new Blob(['9876543210'], {type: 'image/png'});
+    thumbnailFb = new Blob(['987'], {type: 'image/png'});
 
     contactWithoutPhoto = {
       photo: []
@@ -35,6 +40,26 @@ suite('shared/ContactPhotoHelper >', function() {
 
     cntactWithExtra = {
       photo: [thumbnail, extra, fullResolution]
+    };
+
+    // Legacy contact (linked / imported by a previous OS version)
+    // with one photo
+    cntactFbLinkedOnePhoto = {
+      photo: [fullResolutionFb],
+      category: ['fb_linked']
+    };
+
+    // Legacy contact (linked by a previous OS version)
+    // with two photos (local and Facebook)
+    cntactFbLinkedTwoPhotos = {
+      photo: [fullResolution, fullResolutionFb],
+      category: ['fb_linked']
+    };
+
+    // New contact
+    cntactFbLinkedAllPhotos = {
+      category: ['fb_linked'],
+      photo: [fullResolution, thumbnail, fullResolutionFb, thumbnailFb]
     };
 
     subject = ContactPhotoHelper;
@@ -69,6 +94,26 @@ suite('shared/ContactPhotoHelper >', function() {
     });
   });
 
+  suite('getThumbnail > FB Linked >', function() {
+    test('should return photo[0] for a legacy linked contact with one photo',
+      function() {
+        assert.equal(subject.getThumbnail(cntactFbLinkedOnePhoto),
+                     fullResolutionFb);
+    });
+
+    test('should return photo[0] for a legacy linked contact with two photos',
+      function() {
+        assert.equal(subject.getThumbnail(cntactFbLinkedTwoPhotos),
+                     fullResolution);
+    });
+
+    test('should return local photo when the linked contact has both photos',
+      function() {
+        assert.equal(subject.getThumbnail(cntactFbLinkedAllPhotos),
+                     thumbnail);
+    });
+  });
+
   suite('getFullResolution >', function() {
     test('should return null with the contact has no photo', function() {
       assert.isNull(subject.getFullResolution(contactWithoutPhoto));
@@ -94,6 +139,26 @@ suite('shared/ContactPhotoHelper >', function() {
     test('should return the biggest blob if the contact multiple photos',
     function() {
       assert.equal(subject.getFullResolution(cntactWithExtra),
+                   fullResolution);
+    });
+  });
+
+  suite('getFullResolution > FB Linked >', function() {
+    test('should return photo[0] for a legacy linked contact with one photo',
+      function() {
+        assert.equal(subject.getFullResolution(cntactFbLinkedOnePhoto),
+                     fullResolutionFb);
+    });
+
+    test('should return photo[0] for a legacy linked contact with two photos',
+      function() {
+        assert.equal(subject.getFullResolution(cntactFbLinkedTwoPhotos),
+                     fullResolution);
+    });
+
+    test('should return the local photo if the contact has both photos',
+      function() {
+        assert.equal(subject.getFullResolution(cntactFbLinkedAllPhotos),
                    fullResolution);
     });
   });
