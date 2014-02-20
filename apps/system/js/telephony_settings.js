16cbc8dc6d56fff9a8a5401a6286e105cc2af571
Arthur Chen <crh0716@gmail.com>
2014-02-20 17:40:16 +0800

    Bug 974253 - Set preferred network type on startup based on the user preference

--- apps/system/js/telephony_settings.js
+++ apps/system/js/telephony_settings.js
@@ -44,4 +44,68 @@
       };
     });
   });
+
+  // Initialize preferred network type
+  function _setDefaultPreferredNetworkType(conn, preferredNetworkType) {
+    var doSet = function() {
+      var setReq = conn.setPreferredNetworkType(preferredNetworkType);
+      setReq.onerror = function set_vpm_error() {
+        console.error('Error setting preferred network type: ' +
+          preferredNetworkType);
+      };
+    };
+    if (conn.radioState === 'enabled') {
+      doSet();
+    } else {
+      conn.addEventListener('radiostatechange', function onchange() {
+        if (conn.radioState === 'enabled') {
+          conn.removeEventListener('radiostatechange', onchange);
+          doSet();
+        }
+      });
+    }
+  }
+
+  /**
+   * Returns an array specifying the default preferred network types of all
+   * mobile connections.
+   */
+  function _getDefaultPreferredNetworkTypes() {
+    return mobileConnections.map(function(conn) {
+      return _getDefaultPreferredNetworkType(conn.supportedNetworkTypes);
+    });
+  }
+
+  /**
+   * Returns the default preferred network types based on the hardware
+   * supported network types.
+   */
+  function _getDefaultPreferredNetworkType(hwSupportedTypes) {
+    return ['lte', 'wcdma', 'gsm', 'cdma', 'evdo'].filter(function(type) {
+      return (hwSupportedTypes.indexOf(type) !== -1);
+    }).join('/');
+  }
+
+  /**
+   * If the default value is null, we should use the option that makes the
+   * device able to connect all supported netwrok types.
+   */
+  var preferredNetworkTypeHelper =
+    SettingsHelper('ril.radio.preferredNetworkType');
+  preferredNetworkTypeHelper.get(function got_pnt(values) {
+    if (!values) {
+      values = _getDefaultPreferredNetworkTypes();
+      preferredNetworkTypeHelper.set(values);
+    } else if (typeof values == 'string') {
+      // do the migration
+      var tempDefault = _getDefaultPreferredNetworkTypes();
+      tempDefault[0] = values;
+      values = tempDefault;
+      preferredNetworkTypeHelper.set(values);
+    }
+
+    mobileConnections.forEach(function pnt_iterator(conn, index) {
+      _setDefaultPreferredNetworkType(conn, values[index]);
+    });
+  });
 })();
