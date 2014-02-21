780c77a7fb1d6a94d52dbfd1086a0bf7dc5bc3ee
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2014-02-20 19:20:29 -0500

    Merge pull request #16253 from pmclanahan/send-language-to-basket-964342
    Bug 964342: Send language setting to basket.
    (cherry picked from commit ab77e4689a361752365ab94a966de6ac9fbc89bd)

--- apps/communications/ftu/js/basket-client.js
+++ apps/communications/ftu/js/basket-client.js
@@ -9,6 +9,22 @@ var Basket = {
   callback: null,
   xhr: null,
   itemId: 'newsletter_email',
+  // https://github.com/mozilla/basket-client/blob/master/basket/errors.py
+  errors: {
+    NETWORK_FAILURE: 1,
+    INVALID_EMAIL: 2,
+    UNKNOWN_EMAIL: 3,
+    UNKNOWN_TOKEN: 4,
+    USAGE_ERROR: 5,
+    EMAIL_PROVIDER_AUTH_FAILURE: 6,
+    AUTH_ERROR: 7,
+    SSL_REQUIRED: 8,
+    INVALID_NEWSLETTER: 9,
+    INVALID_LANGUAGE: 10,
+    EMAIL_NOT_CHANGED: 11,
+    CHANGE_REQUEST_NOT_FOUND: 12,
+    UNKNOWN_ERROR: 99
+  },
 
   responseHandler: function b_responseHandler() {
     if (this.xhr.readyState === 4) {
@@ -28,6 +44,10 @@ var Basket = {
     }
   },
 
+  timeoutHandler: function b_timeoutHandler() {
+    this.callback('Timeout');
+  },
+
   /**
    * Send data to Mozilla Basket.
    *
@@ -38,17 +58,25 @@ var Basket = {
    *                            in the error case.
    */
   send: function b_send(email, callback) {
+    var self = this;
     var params = 'email=' + encodeURIComponent(email) +
-                 '&newsletters=' + this.newsletterId;
-    this.callback = callback;
-    this.xhr = new XMLHttpRequest({mozSystem: true});
-    this.xhr.onreadystatechange = this.responseHandler.bind(this);
-    this.xhr.open('POST', this.basketUrl, true);
-    this.xhr.setRequestHeader('Content-type',
+                 '&newsletters=' + self.newsletterId;
+    self.callback = callback;
+    self.xhr = new XMLHttpRequest({mozSystem: true});
+    self.xhr.onreadystatechange = self.responseHandler.bind(self);
+    self.xhr.open('POST', self.basketUrl, true);
+    self.xhr.setRequestHeader('Content-type',
                               'application/x-www-form-urlencoded');
-    this.xhr.setRequestHeader('Content-length', params.length);
-    this.xhr.setRequestHeader('Connection', 'close');
-    this.xhr.send(params);
+    self.xhr.setRequestHeader('Connection', 'close');
+    self.xhr.timeout = 5000;  // 5 seconds
+    self.xhr.ontimeout = self.timeoutHandler.bind(self);
+    self.getLanguage(function do_send(language) {
+      if (language) {
+        params += '&lang=' + language;
+      }
+      self.xhr.setRequestHeader('Content-length', params.length);
+      self.xhr.send(params);
+    });
   },
 
   store: function b_store(email, callback) {
@@ -56,6 +84,26 @@ var Basket = {
       if (callback)
         callback();
     });
+  },
+
+  getLanguage: function b_getLanguage(callback) {
+    var settings = window.navigator.mozSettings;
+    if (!settings || !settings.createLock) {
+      callback();
+      return;
+    }
+
+    var s_name = 'language.current';
+    var req = settings.createLock().get(s_name);
+
+    req.onsuccess = function _onsuccess() {
+      callback(req.result[s_name]);
+    };
+
+    req.onerror = function _onerror() {
+      console.error('Error getting setting: ' + s_name);
+      callback();
+    };
   }
 
 };
