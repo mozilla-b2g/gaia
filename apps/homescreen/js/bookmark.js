f4c1a2d4cab7d4d4f807f3a0c6842b8cd89821c2
crdlc <crdlc@tid.es>
2014-02-24 10:33:05 +0100

    Bug 975834 - homescreen bookmarks do not work for URLs ending in a file

--- apps/homescreen/js/bookmark.js
+++ apps/homescreen/js/bookmark.js
@@ -3,14 +3,19 @@
 var Bookmark = function Bookmark(params) {
   GridItem.call(this, params);
 
-  var url = params.bookmarkURL.trim();
-  this.url = this.origin = this.bookmarkURL = url.endsWith('/') ? url :
-                                                                  url + '/';
+  this.url = this.origin = params.bookmarkURL.trim();
+  // The bookmarkURL is used for indexing bookmarks in the homescreen. "It's not
+  // a real URL", just used for indexing in homescreen (bug #976955)
+  this.bookmarkURL = this.generateIndex(this.url);
   this.type = GridItemsFactory.TYPE.BOOKMARK;
 };
 
 Bookmark.prototype = {
   __proto__: GridItem.prototype,
+
+  generateIndex: function bookmark_generateIndex(url) {
+    return 'bookmark:' + url;
+  },
 
   launch: function bookmark_launch() {
     var features = this.getFeatures();
