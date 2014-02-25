646b1110028229094de9e802f22c95f027184e70
Miller Medeiros <miller@millermedeiros.com>
2014-02-25 16:25:03 -0300

    Bug 972666 - [Calendar] lines over events longer than 2 hours

--- apps/calendar/test/marionette/calendar.js
+++ apps/calendar/test/marionette/calendar.js
@@ -52,6 +52,7 @@ Calendar.Selector = Object.freeze({
   eventListSection: '#event-list',
   hintSwipeToNavigate: '#hint-swipe-to-navigate',
   modifyEventView: '#modify-event-view',
+  monthDayViewEvents: '#months-day-view .day-events',
   monthEventList: '#event-list div.events > section',
   monthEventTitle: 'h5',
   monthViewDayEvent: '#event-list .event',
@@ -60,6 +61,7 @@ Calendar.Selector = Object.freeze({
   monthViewpresent: '#month-view li.present',
   monthViewselected: '#month-view li.selected',
   monthYearHeader: '#current-month-year',
+  dayViewEvent: '#day-view .active .event',
   todayTabItem: '#today',
   toolbarAddAccountButton: '#settings a[href="/select-preset/"]',
   toolbarButton: '#time-header button.settings',
@@ -77,6 +79,7 @@ Calendar.Selector = Object.freeze({
   viewEventViewTitleContent: '#event-view .title .content',
   viewEventViewDescription: '#event-view .description',
   viewEventViewDescriptionContent: '#event-view .description .content',
+  viewEventViewCancelButton: '#event-view button.cancel',
   weekButton: '#view-selector .week a',
   weekViewEvent: '#week-view .event'
 });
@@ -135,6 +138,22 @@ Calendar.prototype = {
 
   waitForWeekView: function() {
     this.client.waitFor(this.isWeekViewActive.bind(this));
+  },
+
+  waitForDayView: function() {
+    this.client.waitFor(this.isDayViewActive.bind(this));
+  },
+
+  waitForViewEventView: function() {
+    this.client.waitFor(this.isViewEventViewActive.bind(this));
+  },
+
+  waitForEditEventView: function() {
+    this.client.waitFor(this.isEditEventViewActive.bind(this));
+  },
+
+  waitForAddEventView: function() {
+    this.client.waitFor(this.isAddEventViewActive.bind(this));
   },
 
   // TODO: extract this logic into the marionette-helper repository since this
@@ -248,6 +267,8 @@ Calendar.prototype = {
    *   (string) location - event location
    *   (Date) startDate - when event starts
    *   (Date) endDate - when event ends
+   *   (Number) startHour - shortcut for setting the startDate
+   *   (Number) duration - shortcut for setting the endDate based on startDate
    * @return {Event} Created event.
    */
   createEvent: function(opts) {
@@ -265,16 +286,37 @@ Calendar.prototype = {
     titleInput.sendKeys(opts.title);
     locationInput.sendKeys(opts.location);
     descriptionInput.sendKeys(opts.description);
+
+    var startDate = opts.startDate;
+    var endDate = opts.endDate;
+    var startHour = opts.startHour || 0;
+
+    if (!startDate) {
+      startDate = new Date();
+      startDate.setHours(startHour);
+      startDate.setMinutes(0);
+      startDate.setSeconds(0);
+      startDate.setMilliseconds(0);
+    }
+
+    if (!endDate) {
+      // duration should always be bigger than 0, defaults to 1h
+      var duration = opts.duration || 1;
+      endDate = new Date(startDate.getTime() + (60 * duration * 60 * 1000));
+    }
+
     var form = this.waitForElement('editEventForm');
     this.client.forms.fill(form, {
-      startDate: opts.startDate,
-      startTime: opts.startDate,
-      endDate: opts.endDate,
-      endTime: opts.endDate
+      startDate: startDate,
+      startTime: startDate,
+      endDate: endDate,
+      endTime: endDate
     });
 
     // Save event.
     this.waitForElement('editEventSaveButton').click();
+
+    this.waitForKeyboardHide();
 
     // TODO(gareth): Sort out the dates and times here.
     return {
@@ -356,6 +398,20 @@ Calendar.prototype = {
    */
   isViewEventViewActive: function() {
     return this.isViewActive('event/show');
+  },
+
+  /**
+   * @return {boolean} Whether or not the add event view is active.
+   */
+  isAddEventViewActive: function() {
+    return this.isViewActive('event/add');
+  },
+
+  /**
+   * @return {boolean} Whether or not the edit event view is active.
+   */
+  isEditEventViewActive: function() {
+    return this.isViewActive('event/edit');
   },
 
   /**
--- apps/calendar/test/marionette/day_view_test.js
+++ apps/calendar/test/marionette/day_view_test.js
@@ -1,5 +1,6 @@
+'use strict';
+
 var Calendar = require('./calendar'),
-    Marionette = require('marionette-client'),
     assert = require('chai').assert;
 
 marionette('day view', function() {
@@ -11,7 +12,7 @@ marionette('day view', function() {
     app.launch({ hideSwipeHint: true });
     // Go to day view
     app.waitForElement('dayButton').click();
-    client.waitFor(app.isDayViewActive.bind(app));
+    app.waitForDayView();
   });
 
   test('header copy should not overflow', function() {
@@ -20,4 +21,40 @@ marionette('day view', function() {
     // 20 chars is a "safe" limit if font-family is Fira Sans
     assert.operator(header.text().length, '<', 21);
   });
+
+  suite('events longer than 2h', function() {
+    setup(function() {
+      app.createEvent({
+        title: 'Lorem Ipsum',
+        location: 'Dolor Amet',
+        startHour: 0,
+        duration: 3
+      });
+      app.waitForDayView();
+    });
+
+    test('click after first hour', function() {
+      // click will happen at middle of element and middle is after first hour,
+      // so this should be enough to trigger the event details (Bug 972666)
+      client.findElement('#day-view .active .day-events .hour-2').click();
+
+      app.waitForViewEventView();
+
+      var title = app.findElement('viewEventViewTitle');
+
+      assert.equal(
+        title.text(),
+        'Lorem Ipsum',
+        'title should match'
+      );
+    });
+
+    test('click after event end', function() {
+      // click will happen at middle of element so this should be enough to
+      // trigger the create event (since .hour-3 is after event duration)
+      client.findElement('#day-view .active .day-events .hour-3').click();
+      assert.ok(app.isAddEventViewActive(), 'should go to add event view');
+    });
+  });
+
 });
