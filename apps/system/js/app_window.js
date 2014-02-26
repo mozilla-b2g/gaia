870a91032c9d6bd84713618df0e80f0b50962488
Alive Kuo <alegnadise@gmail.com>
2014-02-26 16:45:29 +0800

    Bug 970028 - Nicer activity opener animation

--- apps/system/js/app_window.js
+++ apps/system/js/app_window.js
@@ -646,7 +646,8 @@
         var caller = this.activityCaller;
         this.activityCaller.activityCallee = null;
         this.activityCaller = null;
-        caller.requestOpen();
+        caller.open('in-from-left');
+        this.close('out-to-right');
       }
     };
 
