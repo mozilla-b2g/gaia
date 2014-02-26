a716fd315a14946ace589daaf43e5c79189666ef
Ben Tian <btian@mozilla.com>
2014-02-26 10:49:20 +0800

    Revert "Bug 972142 - Set camera preview style with CSS pixel values."
    This reverts commit 03404b44494fcaece0631a5effab771431a72e33.

--- apps/camera/js/camera.js
+++ apps/camera/js/camera.js
@@ -1120,12 +1120,10 @@ var Camera = {
     transform += 'rotate(' + camera.sensorAngle + 'deg)';
     style.transform = transform;
 
-    style.left =
-      ((screenWidth - previewWidth) / 2 / window.devicePixelRatio) + 'px';
-    style.top =
-      ((screenHeight - previewHeight) / 2 / window.devicePixelRatio) + 'px';
-    style.width = previewWidth / window.devicePixelRatio + 'px';
-    style.height = previewHeight / window.devicePixelRatio + 'px';
+    style.left = ((screenWidth - previewWidth) / 2) + 'px';
+    style.top = ((screenHeight - previewHeight) / 2) + 'px';
+    style.width = previewWidth + 'px';
+    style.height = previewHeight + 'px';
   },
 
   recordingStateChanged: function(msg) {
