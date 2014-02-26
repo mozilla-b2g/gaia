4e767a7cf0dddfeafc5b463e175fc67b90e18b33
Wilson Page <wilsonpage@me.com>
2014-02-26 10:26:38 +0900

    Bug 976875 - [Camera] Camera app launch with black screen in Settings/SMS apps

--- apps/camera/js/controllers/activity.js
+++ apps/camera/js/controllers/activity.js
@@ -5,6 +5,8 @@ define(function(require, exports, module) {
  * Dependencies
  */
 
+var lessThanBytes = require('lib/picture-sizes/less-than-bytes');
+var closestToSize = require('lib/picture-sizes/closest-to-size');
 var debug = require('debug')('controller:activity');
 var bindAll = require('lib/bind-all');
 
@@ -21,14 +23,19 @@ module.exports.ActivityController = ActivityController;
  * @param {App} app
  */
 function ActivityController(app) {
-  if (app.activity.active) {
-    bindAll(this);
-    this.activity = app.activity;
-    this.settings = app.settings;
-    this.app = app;
-    this.configure();
-    this.bindEvents();
-  }
+  if (!app.activity.active) { return; }
+
+  bindAll(this);
+  this.activity = app.activity;
+  this.settings = app.settings;
+  this.app = app;
+
+  // Allow these methods to be overridden
+  this.closestToSize = app.closestToSize || closestToSize;
+  this.lessThanBytes = app.lessThanBytes || lessThanBytes;
+
+  this.configure();
+  this.bindEvents();
 }
 
 ActivityController.prototype.configure = function() {
@@ -41,78 +48,34 @@ ActivityController.prototype.configureMode = function() {
 };
 
 ActivityController.prototype.bindEvents = function() {
-  this.settings.pictureSizes().on('optionsreset', this.configurePictureSize);
-  this.settings.recorderProfiles().on('optionsreset', this.configureVideoSize);
+  this.settings.pictureSizes.on('optionsreset', this.configurePictureSize);
+  this.settings.recorderProfiles.on('optionsreset', this.configureVideoSize);
 };
 
 ActivityController.prototype.configurePictureSize = function(options) {
-  var maxFileSize = this.activity.data.maxFileSizeBytes;
-  var setting = this.settings.pictureSizes();
   var data = this.activity.data;
+  var maxFileSize = data.maxFileSizeBytes;
+  var setting = this.settings.pictureSizes;
 
   if (maxFileSize) {
-    options = filterBytesLessThan(3000000, options);
+    options = this.lessThanBytes(maxFileSize, options);
     setting.set('options', options);
   } else if (data.width || data.height) {
-    options = [pickBySize(options, data)];
-    debug('picked picture size ', JSON.stringify(options));
+    options = [this.closestToSize(data, options)];
     setting.set('options', options);
+    debug('picked picture size', options);
   }
 };
 
 ActivityController.prototype.configureVideoSize = function(options) {
   var maxFileSize = this.activity.data.maxFileSizeBytes;
-  var setting = this.settings.recorderProfiles();
+  var setting = this.settings.recorderProfiles;
 
   if (maxFileSize) {
     options = [getLowResVideoSize(options)];
     setting.set('options', options);
   }
 };
-
-/**
- * Utils
- */
-
-function filterBytesLessThan(bytes, sizes) {
-  return sizes.filter(function(option) {
-    var size = option.value;
-    var mp = size.width * size.height;
-    return mp <= bytes;
-  });
-}
-
-function pickBySize(options, target) {
-  debug('picking closest picture size');
-
-  var width = target.width || 0;
-  var height = target.height || 0;
-
-  return options.reduce(function(result, option) {
-    var resultSize = result && result.value;
-    var size = option.value;
-
-    var largerThanTarget =
-      size.width >= width &&
-      size.height >= height;
-
-    // If we don't yet have a result and this option
-    // is larger than the target dimensions, use it.
-    if (!result) { return largerThanTarget ? option : null; }
-
-    // If it's not larger than the target,
-    // this option isn't going to be appropriate.
-    if (!largerThanTarget) { return result; }
-
-    var smallerThanCurrent =
-      size.width <= resultSize.width &&
-      size.height <= resultSize.height;
-
-    // If the option is larger than the target, yet
-    // smaller is size than the current choice use it!
-    return smallerThanCurrent ? option : result;
-  }, null);
-}
 
 function getLowResVideoSize(options) {
   var hash = {};
