499a3c2fd34525e6290712e3f19df29901485c0b
Zac Campbell <zcampbell@mozilla.com>
2014-02-27 16:07:41 +0000

    Bug 977640 - Bump up timeout in test_sms_notification.py a=testonly r=me

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/messages/test_sms_notification.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/messages/test_sms_notification.py
@@ -27,8 +27,8 @@ class TestSmsNotification(GaiaTestCase):
 
         # Send a SMS to the device
         self.data_layer.send_sms(self.testvars['carrier']['phone_number'], _text_message_content)
-        # We will wait upto 120 seconds for the SMS to arrive due to network latency
-        system.wait_for_notification_toaster_displayed(timeout=120)
+        # We will wait upto 300 seconds for the SMS to arrive due to network latency
+        system.wait_for_notification_toaster_displayed(timeout=300)
         system.wait_for_notification_toaster_not_displayed()
 
         self.assertTrue(self.apps.running_apps[1].name == "Messages")
