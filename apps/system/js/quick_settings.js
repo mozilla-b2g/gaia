49fb28df5c36a59c28d1e8a9c60fc7a189f82491
KWierso <kwierso@gmail.com>
2014-02-28 16:26:50 -0800

    Revert "Merge pull request #16739 from mcjimenez/bug976951_v2" for gaia-unit test bustage
    This reverts commit 61391cd5b77e5085dd1362eee7014c56fefbbdfb, reversing
    changes made to 26fa8fe02e656655bc9b89e0c5e74f135965e799.

--- apps/system/js/quick_settings.js
+++ apps/system/js/quick_settings.js
@@ -12,9 +12,6 @@ var QuickSettings = {
 
   init: function qs_init() {
     var settings = window.navigator.mozSettings;
-    if (!settings) {
-      return;
-    }
 
     // XXX: check bug-926169
     // this is used to keep all tests passing while introducing multi-sim APIs
@@ -22,21 +19,8 @@ var QuickSettings = {
       window.navigator.mozMobileConnections &&
         window.navigator.mozMobileConnections[0];
 
-    (function initNetworkSprite() {
-      var networkTypeSetting =
-        SettingsHelper('operatorResources.data.icon', {});
-
-      networkTypeSetting.get(function gotNS(networkTypeValues) {
-        if (!networkTypeValues) {
-          return;
-        }
-        var sprite = networkTypeValues['data_sprite'];
-        if (sprite) {
-          document.getElementById('quick-settings-data').style.backgroundImage =
-            'url("' + sprite + '")';
-        }
-      });
-    })();
+    if (!settings)
+      return;
 
     this.getAllElements();
 
