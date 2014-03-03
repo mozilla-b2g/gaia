242e477761643a440e6b244d291fb9d0ce204993
Cristian Rodriguez <crdlc@tid.es>
2014-03-03 16:11:24 +0100

    Merge pull request #16794 from crdlc/bug-978739
    Bug 978739 - After rebooting users are not able to delete/open bookmarks from...

--- apps/homescreen/js/bookmark.js
+++ apps/homescreen/js/bookmark.js
@@ -3,7 +3,7 @@
 var Bookmark = function Bookmark(params) {
   GridItem.call(this, params);
 
-  this.url = this.origin = params.bookmarkURL.trim();
+  this.url = this.origin = this.sanitizeURL(params.bookmarkURL);
   // The bookmarkURL is used for indexing bookmarks in the homescreen. "It's not
   // a real URL", just used for indexing in homescreen (bug #976955)
   this.bookmarkURL = this.generateIndex(this.url);
@@ -13,8 +13,17 @@ var Bookmark = function Bookmark(params) {
 Bookmark.prototype = {
   __proto__: GridItem.prototype,
 
+  _INDEX_PREFIX: 'bookmark:',
+
+  sanitizeURL: function bookmark_sanitizeURL(url) {
+    url = url.trim();
+    var prefix = this._INDEX_PREFIX;
+    return url.startsWith(prefix) ? url.substr(prefix.length) : url;
+  },
+
   generateIndex: function bookmark_generateIndex(url) {
-    return 'bookmark:' + url;
+    var prefix = this._INDEX_PREFIX;
+    return url.startsWith(prefix) ? url : prefix + url;
   },
 
   launch: function bookmark_launch() {
