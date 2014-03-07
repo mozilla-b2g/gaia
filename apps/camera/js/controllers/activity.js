7a8b68b82bd1ba9988d4d68000ca573877eab4bc
Diego Marcos <diego.marcos@gmail.com>
2014-03-07 13:32:25 -0800

    Bug 980599 - [Camera] Picture and video resolution selection (cherry picked from commit 7ca934f21b7e2ea027b7bb3ceb36d37c74072b5b)

--- apps/camera/js/controllers/activity.js
+++ apps/camera/js/controllers/activity.js
@@ -5,6 +5,8 @@ define(function(require, exports, module) {
  * Dependencies
  */
 
+var lessThanPixels = require('lib/picture-sizes/less-than-pixels');
+var closestToSize = require('lib/picture-sizes/closest-to-size');
 var debug = require('debug')('controller:activity');
 var bindAll = require('lib/bind-all');
 
@@ -15,102 +17,65 @@ var bindAll = require('lib/bind-all');
 module.exports = function(app) { return new ActivityController(app); };
 module.exports.ActivityController = ActivityController;
 
+/**
+ * Initialize new `ActivityController`
+ *
+ * @param {App} app
+ */
 function ActivityController(app) {
+  if (!app.activity.active) { return; }
+
   bindAll(this);
   this.activity = app.activity;
   this.settings = app.settings;
   this.app = app;
+
+  // Allow these methods to be overridden
+  this.closestToSize = app.closestToSize || closestToSize;
+  this.lessThanPixels = app.lessThanPixels || lessThanPixels;
+
   this.configure();
   this.bindEvents();
 }
 
 ActivityController.prototype.configure = function() {
-  if (!this.activity.active) { return; }
+  this.configureMode();
+};
+
+ActivityController.prototype.configureMode = function() {
   var values = this.activity.data.modes;
-  this.settings.mode.configureOptions(values);
+  this.settings.mode.resetOptions(values);
 };
 
 ActivityController.prototype.bindEvents = function() {
-  this.app.on('settings:beforeconfigured', this.configureMediaSizes);
+  this.settings.pictureSizes.on('optionsreset', this.configurePictureSize);
+  this.settings.recorderProfiles.on('optionsreset', this.configureVideoSize);
 };
 
-ActivityController.prototype.configureMediaSizes = function() {
-  debug('configure media sizes');
-  var activity = this.app.activity;
-  if (activity.active) {
-    this.configurePictureSize(activity.data);
-    this.configureVideoSize(activity.data);
-  }
-};
-
-ActivityController.prototype.configurePictureSize = function(data) {
-  var setting = this.app.settings.pictureSizes;
+ActivityController.prototype.configurePictureSize = function(options) {
+  var data = this.activity.data;
   var maxFileSize = data.maxFileSizeBytes;
-  var options = setting.get('options');
+  var setting = this.settings.pictureSizes;
 
   if (maxFileSize) {
-    options = getPictureSizesSmallerThan(options, maxFileSize);
-    setting.configureOptions(options);
-    return;
-  }
-
-  if (data.width || data.height) {
-    options = [pickBySize(options, data)];
-    debug('picked picture size ', JSON.stringify(options));
-    setting.configureOptions(options);
+    options = this.lessThanPixels(maxFileSize, options);
+    setting.set('options', options);
+  } else if (data.width || data.height) {
+    options = [this.closestToSize(data, options)];
+    setting.set('options', options);
+    debug('picked picture size', options);
   }
 };
 
-ActivityController.prototype.configureVideoSize = function(data) {
-  var setting = this.app.settings.recorderProfiles;
-  var maxFileSize = data.maxFileSizeBytes;
-  var options = setting.get('options');
+ActivityController.prototype.configureVideoSize = function(options) {
+  var maxFileSize = this.activity.data.maxFileSizeBytes;
+  var setting = this.settings.recorderProfiles;
 
   if (maxFileSize) {
     options = [getLowResVideoSize(options)];
-    setting.configureOptions(options);
+    setting.set('options', options);
   }
 };
-
-function getPictureSizesSmallerThan(options, bytes) {
-  return options.filter(function(option) {
-    var size = option.value;
-    var mp = size.width * size.height;
-    return mp <= bytes;
-  });
-}
-
-function pickBySize(options, target) {
-  debug('picking closest picture size');
-
-  var width = target.width || 0;
-  var height = target.height || 0;
-
-  return options.reduce(function(result, option) {
-    var resultSize = result && result.value;
-    var size = option.value;
-
-    var largerThanTarget =
-      size.width >= width &&
-      size.height >= height;
-
-    // If we don't yet have a result and this option
-    // is larger than the target dimensions, use it.
-    if (!result) { return largerThanTarget ? option : null; }
-
-    // If it's not larger than the target,
-    // this option isn't going to be appropriate.
-    if (!largerThanTarget) { return result; }
-
-    var smallerThanCurrent =
-      size.width <= resultSize.width &&
-      size.height <= resultSize.height;
-
-    // If the option is larger than the target, yet
-    // smaller is size than the current choice use it!
-    return smallerThanCurrent ? option : result;
-  }, null);
-}
 
 function getLowResVideoSize(options) {
   var hash = {};
