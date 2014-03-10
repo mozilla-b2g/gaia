7cfdb56f7a8342e40465ec5a08e71779ea430fb5
DouglasSherk <github@sherk.me>
2014-03-10 18:03:42 -0400

    Bug 980208 - [DSDS][Dialer] Display default SIM and long tap indicator on call button. r=Rik

--- apps/communications/dialer/js/call_button.js
+++ apps/communications/dialer/js/call_button.js
@@ -1,23 +1,47 @@
-/* globals LazyLoader, SimPicker, SimSettingsHelper */
+/* globals LazyLoader, SettingsListener, SimPicker */
 /* exported CallButton */
 
 'use strict';
 
-var CallButton = {
-  _phoneNumberGetter: null,
-  _imports: ['/shared/js/sim_picker.js',
-             '/shared/js/sim_settings_helper.js'],
+// Keep this in sync with SimSettingsHelper.
+const ALWAYS_ASK_OPTION_VALUE = '-1';
 
-  init: function cb_init(button, phoneNumberGetter, callCallback) {
+var CallButton = {
+
+  _phoneNumberGetter: null,
+  _callCallback: null,
+  _settingsKey: null,
+
+  init: function cb_init(button, phoneNumberGetter, callCallback, settingsKey) {
     this._phoneNumberGetter = phoneNumberGetter;
     this._callCallback = callCallback;
+    this._settingsKey = settingsKey;
 
     button.addEventListener('click', this._click.bind(this));
 
     if (window.navigator.mozMobileConnections &&
         window.navigator.mozMobileConnections.length > 1) {
       button.addEventListener('contextmenu', this._contextmenu.bind(this));
+
+      var self = this;
+      LazyLoader.load(['/shared/js/settings_listener.js'], function() {
+        self._simIndication = button.querySelector('.js-sim-indication');
+        SettingsListener.observe(settingsKey, 0, self._updateUI.bind(self));
+      });
     }
+  },
+
+  _getCardIndex: function cb_getCardIndex(callback) {
+    var settingsKey = this._settingsKey;
+    var settings = navigator.mozSettings;
+    var getReq = settings.createLock().get(settingsKey);
+    var done = function done() {
+      callback(getReq.result[settingsKey]);
+    };
+    getReq.onsuccess = done;
+    getReq.onerror = function() {
+      console.error('Failed to retrieve ', settingsKey);
+    };
   },
 
   _click: function cb_click(event) {
@@ -36,17 +60,35 @@ var CallButton = {
     }
 
     var self = this;
-    LazyLoader.load(this._imports, function() {
-      SimSettingsHelper.getCardIndexFrom('outgoingCall', function(cardIndex) {
-        // The user has requested that we ask them every time for this key,
-        // so we prompt them to pick a SIM even when they only click.
-        if (cardIndex == SimSettingsHelper.ALWAYS_ASK_OPTION_VALUE) {
+    self._getCardIndex(function(cardIndex) {
+      // The user has requested that we ask them every time for this key,
+      // so we prompt them to pick a SIM even when they only click.
+      if (cardIndex == ALWAYS_ASK_OPTION_VALUE) {
+        LazyLoader.load(['/shared/js/sim_picker.js'], function() {
           SimPicker.show(cardIndex, phoneNumber, self.makeCall.bind(self));
-        } else {
-          self.makeCall();
-        }
-      });
+        });
+      } else {
+        self.makeCall();
+      }
     });
+  },
+
+  _updateUI: function cb_updateUI(cardIndex) {
+    if (cardIndex >= 0 &&
+        window.navigator.mozMobileConnections &&
+        window.navigator.mozMobileConnections.length > 1) {
+      if (this._simIndication) {
+        var self = this;
+        navigator.mozL10n.ready(function() {
+          navigator.mozL10n.localize(self._simIndication,
+                                     'sim-picker-button', {n: cardIndex+1});
+        });
+      }
+
+      document.body.classList.add('has-preferred-sim');
+    } else {
+      document.body.classList.remove('has-preferred-sim');
+    }
   },
 
   _contextmenu: function cb_contextmenu(event) {
@@ -65,8 +107,8 @@ var CallButton = {
     }
 
     var self = this;
-    LazyLoader.load(this._imports, function() {
-      SimSettingsHelper.getCardIndexFrom('outgoingCall', function(cardIndex) {
+    self._getCardIndex(function(cardIndex) {
+      LazyLoader.load(['/shared/js/sim_picker.js'], function() {
         SimPicker.show(cardIndex, phoneNumber, self.makeCall.bind(self));
       });
     });
@@ -78,15 +120,13 @@ var CallButton = {
       return;
     }
 
-    var self = this;
-    LazyLoader.load(['/shared/js/sim_settings_helper.js'], function() {
-      if (cardIndex !== undefined) {
+    if (cardIndex !== undefined) {
+      this._callCallback(phoneNumber, cardIndex);
+    } else {
+      var self = this;
+      this._getCardIndex(function(cardIndex) {
         self._callCallback(phoneNumber, cardIndex);
-      } else {
-        SimSettingsHelper.getCardIndexFrom('outgoingCall', function(cardIndex) {
-          self._callCallback(phoneNumber, cardIndex);
-        });
-      }
-    });
+      });
+    }
   },
 };
