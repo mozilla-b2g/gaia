8fac15e93d866e2aa89947b931d72b28d9102fbc
ashish.singh <ashish.singh@lge.com>
2014-03-12 01:32:52 +0900

    [Camera] ISo implementation

--- apps/camera/js/controllers/camera.js
+++ apps/camera/js/controllers/camera.js
@@ -106,6 +106,7 @@ CameraController.prototype.onSettingsConfigured = function() {
   var recorderProfile = settings.recorderProfiles.selected('key');
   var pictureSize = settings.pictureSizes.selected('data');
   this.setFlashMode();
+  this.setISO();
   this.camera
     .setRecorderProfile(recorderProfile)
     .setPictureSize(pictureSize)
@@ -247,6 +248,11 @@ CameraController.prototype.onBlur = function() {
   }
 
   debug('torn down');
+};
+
+CameraController.prototype.setISO = function() {
+  if (this.settings.isoModes.get('disabled')) { return; }
+  this.camera.setISOMode(this.settings.isoModes.selected('key'));
 };
 
 });
