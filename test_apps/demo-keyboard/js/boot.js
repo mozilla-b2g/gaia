dc0569bdec8f12e753fd00431d604f3d9b8b1e0b
Rudy Lu <me@rudy.lu>
2014-03-12 19:43:49 +0800

    Bug 979764 - [keyboard refactor] resizeTo would be invoked more than once when keyboard relaunch

--- test_apps/demo-keyboard/js/boot.js
+++ test_apps/demo-keyboard/js/boot.js
@@ -1,4 +1,6 @@
 'use strict';
 
-window.app = new KeyboardApp();
-window.app.start();
+var app = window.app = new KeyboardApp();
+// Wait until load event, or the app itself will get resize event twice,
+// which may intervene the UI update logic.
+window.addEventListener('load', app.start.bind(app));
