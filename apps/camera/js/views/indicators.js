d7a3febf120e48d1c54cf85171306eb758b27987
Wilson Page <wilsonpage@me.com>
2014-03-13 18:57:43 +0000

    Bug 976852 - [Camera] Indicator Icons On camera Preview for Important camera settings

---
+++ apps/camera/js/views/indicators.js
@@ -0,0 +1,34 @@
+define(function(require, exports, module) {
+'use strict';
+
+/**
+ * Dependencies
+ */
+
+var View = require('vendor/view');
+
+/**
+ * Exports
+ */
+
+module.exports = View.extend({
+  name: 'indicators',
+  tag: 'ul',
+
+  initialize: function() {
+    this.render();
+  },
+
+  render: function() {
+    this.el.innerHTML = this.template();
+  },
+
+  template: function() {
+    return '<li class="indicator_timer icon-timer rotates"></li>' +
+    '<li class="indicator_geolocation icon-geo-location rotates"></li>' +
+    '<li class="indicator_hdr icon-hdr rotates"></li>' +
+    '<li class="indicator_battery icon rotates"></li>';
+  }
+});
+
+});
