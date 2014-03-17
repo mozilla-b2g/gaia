2ea2aab306bd1c941719160cdcb49ee9d755dc17
Etienne Segonzac <etienne@segonzac.info>
2014-03-17 11:07:08 +0100

    Merge pull request #17129 from etiennesegonzac/bug-981468-callgroup-keypad
    Bug 981468 - Fixing JS erros occuring while using the keypad during a conference call. r=ferjm

--- apps/communications/dialer/js/dialer.js
+++ apps/communications/dialer/js/dialer.js
@@ -439,6 +439,7 @@ var NavbarManager = {
     LazyLoader.load(['/shared/js/async_storage.js',
                      '/shared/js/notification_helper.js',
                      '/shared/js/simple_phone_matcher.js',
+                     '/shared/js/contact_photo_helper.js',
                      '/dialer/js/contacts.js',
                      '/dialer/js/call_log.js',
                      '/dialer/style/call_log.css'], function rs_loaded() {
@@ -477,10 +478,8 @@ var NavbarManager = {
       case '#call-log-view':
         checkContactsTab();
         this.ensureResources(function() {
-          LazyLoader.load(['/shared/js/contact_photo_helper.js'], function() {
-            recent.classList.add('toolbar-option-selected');
-            CallLog.init();
-          });
+          recent.classList.add('toolbar-option-selected');
+          CallLog.init();
         });
         break;
       case '#contacts-view':
