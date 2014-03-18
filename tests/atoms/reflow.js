6b73e2727f329537bb0dac1e6013a22cb7d1b804
Etienne Segonzac <etienne@segonzac.info>
2014-03-18 19:20:57 +0100

    Bug 982196 - Upgrading the ReflowHelper to use the new devtools tooling. r=gaye,janx

--- tests/atoms/reflow.js
+++ tests/atoms/reflow.js
@@ -7,96 +7,32 @@ const Cu = Components.utils;
     return;
   }
 
-  let DebuggerCli = Cu.import('resource://gre/modules/devtools/dbg-client.jsm',
-                              {}).DebuggerClient;
-
-  let reflowWatcher = {
+  exports.MozReflowAtom = {
     count: null,
-
-    _client: null,
-    _webappsActor: null,
-    _appActor: null,
+    _trackingManifest: null,
 
     init: function rw_init() {
-      if (!DebuggerServer.initialized) {
-        RemoteDebugger.start();
-      }
-
-      this._client = new DebuggerCli(DebuggerServer.connectPipe());
-
-      this._client.connect((type, traits) => {
-        this._client.listTabs((res) => {
-          this._webappsActor = res.webappsActor;
-        });
-      });
-
-      this.reflowListener = this.reflowListener.bind(this);
-      this._client.addListener('reflowActivity', this.reflowListener);
+      exports.addEventListener('developer-hud-update', this);
     },
 
-    trackApp: function rw_trackApp(manifestURL, done) {
-      if (!this._webappsActor) {
-        return done && done();
-      };
-
-      this._client.request({
-        to: this._webappsActor,
-        type: 'getAppActor',
-        manifestURL: manifestURL
-      }, (res) => {
-        if (res.error) {
-          return done && done();
-        }
-
-        this._appActor = res.actor;
-
-        this._client.request({
-          to: this._appActor.consoleActor,
-          type: 'startListeners',
-          listeners: ['ReflowActivity']
-        }, (res) => {
-          this.count = 0;
-          done && done();
-        });
-      });
-    },
-
-    untrackApp: function rw_untrackApp(done) {
-      if (!this._appActor) {
-        return done && done();
-      };
-
-      this._client.request({
-        to: this._appActor.consoleActor,
-        type: 'stopListeners',
-        listeners: ['ReflowActivity']
-      }, (res) => {
-        this.count = 0;
-        done && done();
-      });
-    },
-
-    reflowListener: function rw_reflowListener(type, packet) {
-      if (packet.type !== 'reflowActivity') {
+    handleEvent: function rw_handleEvent(evt) {
+      if (evt.detail.metric !== 'reflows' ||
+          evt.detail.manifest !== this._trackingManifest) {
         return;
       }
       this.count++;
-    }
-  };
-
-  reflowWatcher.init();
-
-  exports.MozReflowAtom = {
-    startTracking: function(manifestURL, done) {
-      reflowWatcher.trackApp(manifestURL, done);
     },
 
-    stopTracking: function(done) {
-      reflowWatcher.untrackApp(done);
+    startTracking: function(manifestURL) {
+      this._trackingManifest = manifestURL;
+      this.count = 0;
     },
 
-    getCount: function() {
-      return reflowWatcher.count;
-    }
+    stopTracking: function() {
+      this.count = 0;
+      this._trackingManifest = null;
+    },
   };
+
+  exports.MozReflowAtom.init();
 })(window);
