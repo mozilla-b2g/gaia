4ab2166a1d8820ff4ca36bc99646ce1269888ec9
Francisco Jordano <arcturus@ardeenelinfierno.com>
2014-03-19 13:50:26 +0100

    Merge pull request #17288 from arcturus/bug-983777
    Bug 983777 - [B2G][Contacts] Tapping the Favorite button on an imported facebook contact will break the Edit Contact button

--- apps/communications/contacts/test/marionette/details_test.js
+++ apps/communications/contacts/test/marionette/details_test.js
@@ -2,6 +2,7 @@
 
 var Contacts = require('./lib/contacts');
 var assert = require('assert');
+var fs = require('fs');
 
 marionette('Contacts > Details', function() {
   var client = marionette.client(Contacts.config);
@@ -90,6 +91,60 @@ marionette('Contacts > Details', function() {
 
     assert.equal(formHeader.text(), expectedResult);
     assert.equal(formContactName.getAttribute('value'), clickedContactName);
+  });
+
+  test('Favorite FB contact and edit it', function() {
+    client.importScript(fs.readFileSync(__dirname +
+                                          '/data/facebook_contact_data.js',
+                                          'utf8'));
+    var saveFBContact = function() {
+      var fb = window.wrappedJSObject.fb,
+          data = window.wrappedJSObject.data;
+
+      var fbContact = new fb.Contact();
+      fbContact.setData(data.fbContactData);
+
+      var savingFBContact = fbContact.save();
+
+      savingFBContact.onsuccess = function() {
+        marionetteScriptFinished(data.fbContactData);
+      };
+
+      savingFBContact.onerror = function() {
+        marionetteScriptFinished();
+      };
+    };
+
+    var fbContactData;
+    client.executeAsyncScript(saveFBContact, function(err, val) {
+      fbContactData = val;
+    });
+
+    client.waitFor(function() {
+      return fbContactData;
+    });
+
+    client.helper.waitForElement(selectors.listContactFirstText)
+      .click();
+
+    subject.waitSlideLeft('details');
+
+    // Check we loaded the FB contact
+    var telNode = client.helper.waitForElement(selectors.detailsTelButtonFirst);
+    assert.equal(telNode.text(), '+34666666666');
+
+    // It's not a favorite
+    var nameNode = client.helper.waitForElement(selectors.detailsContactName);
+    assert.equal(nameNode.getAttribute('class').indexOf('favorite'), -1);
+
+    // Click on favorite
+    client.helper.waitForElement(selectors.detailsFavoriteButton).click();
+    nameNode = client.helper.waitForElement(selectors.detailsContactName);
+    assert.notEqual(nameNode.getAttribute('class').indexOf('favorite'), -1);
+
+    // Click on edit and go to the edit form
+    client.helper.waitForElement(selectors.detailsEditContact).click();
+    subject.waitForFormShown();
   });
 
 });
