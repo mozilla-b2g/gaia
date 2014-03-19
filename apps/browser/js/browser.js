bebf5f27a332fcfc880619380067bbfdc845a281
Dale Harvey <dale@arandomurl.com>
2014-03-19 16:27:48 -0500

    Bug 983760 - Dont show crash screen if tab is not currently active. r=daleharvey

--- apps/browser/js/browser.js
+++ apps/browser/js/browser.js
@@ -583,7 +583,8 @@ var Browser = {
   handleCrashedTab: function browser_handleCrashedTab(tab) {
     // No need to show the crash screen for background tabs,
     // they will be revived when selected
-    if (tab.id === this.currentTab.id && !document.hidden) {
+    if (tab.id === this.currentTab.id && !document.hidden &&
+        this.currentTab.url != null) {
       this.showCrashScreen();
     }
     tab.loading = false;
