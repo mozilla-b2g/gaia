de58fcb0263a5fe450003f941de3630fc3d595a8
gasolin <gasolin@gmail.com>
2014-03-19 09:33:27 +0800

    Bug 985341 - Fix uitest-privileged for test_video_capture_prompt failure

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/system/test_privileged_app_video_capture_prompt.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/system/test_privileged_app_video_capture_prompt.py
@@ -7,27 +7,27 @@ from gaiatest.apps.ui_tests_privileged.app import UiTestsPivileged
 from gaiatest.apps.homescreen.regions.permission_dialog import PermissionDialog
 
 
-class TestPrivilegedAppAudioCapturePrompt(GaiaTestCase):
+class TestPrivilegedAppVideoCapturePrompt(GaiaTestCase):
 
     def setUp(self):
         GaiaTestCase.setUp(self)
 
-    def test_audio_capture_prompt(self):
+    def test_video_capture_prompt(self):
         uiTestsPrivileged = UiTestsPivileged(self.marionette)
         uiTestsPrivileged.launch()
 
         user_media = uiTestsPrivileged.tap_get_user_media_option()
         user_media.switch_to_frame()
-        user_media.tap_audio1_button()
+        user_media.tap_video1_button()
 
         permission = PermissionDialog(self.marionette)
         self.marionette.switch_to_default_content()
         permission.wait_for_permission_dialog_displayed()
 
         self.assertEqual(permission.permission_dialog_message,
-                         u'Would you like to share your microphone with UI tests - Privileged App?')
+                         u'Would you like to share your camera with UI tests - Privileged App?')
 
         permission.tap_to_confirm_permission()
 
-        current_permission = self.apps.get_permission('UI tests - Privileged App', 'audio-capture')
+        current_permission = self.apps.get_permission('UI tests - Privileged App', 'video-capture')
         self.assertEqual(current_permission, 'prompt')
