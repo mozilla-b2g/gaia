49492a1ba84a9e0e7dc7ec8e09cb11337484c17a
Kevin Grandon <kevingrandon@yahoo.com>
2014-03-21 15:54:56 -0700

    Revert "intermittent cost_control tests"
    This reverts commit 570751acd66f0179eccc25f189834932bd95abd6.

--- tests/python/gaia-ui-tests/gaiatest/apps/cost_control/regions/ftu_step1.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/cost_control/regions/ftu_step1.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import time
 from marionette.by import By
 from gaiatest.apps.base import Base
 from gaiatest.apps.cost_control.regions.ftu_step2 import FTUStep2
@@ -19,7 +18,5 @@ class FTUStep1(Base):
 
     def tap_next(self):
         self.wait_for_element_displayed(*self._next_button_locator)
-        # TODO sometimes we may tap before the click handlers are ready
-        time.sleep(1)
         self.marionette.find_element(*self._next_button_locator).tap()
         return FTUStep2(self.marionette)
