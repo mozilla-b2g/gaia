d24e2372b16b84f666903335abfa85141a2a8fcb
Sam Foster <sfoster@mozilla.com>
2014-03-21 09:47:00 -0700

    Bug 972216 - Use app icon instead of screenshot in cards view preview. r=alive,auswerk
    cherry-picked from 7ffb9e1ccf0af4d944f8562a0267896cf1c75806
    
    Conflicts:
    
    	apps/system/js/cards_view.js

--- apps/system/js/cards_view.js
+++ apps/system/js/cards_view.js
@@ -9,7 +9,9 @@
 
 var CardsView = (function() {
   //display icon of an app on top of app's card
-  var DISPLAY_APP_ICON = false;
+  var DISPLAY_APP_ICON = true;
+  var DISPLAY_APP_SCREENSHOT = false;
+
   // if 'true' user can close the app
   // by dragging it upwards
   var MANUAL_CLOSING = true;
@@ -43,21 +45,29 @@ var CardsView = (function() {
    * @param{String} the position of the app in our cache
    */
   function getIconURI(position) {
-    var icons = stack[position].manifest.icons;
-    if (!icons) {
+    var app = stack[position];
+    var icons = app.manifest && app.manifest.icons;
+    var iconPath;
+
+    if (icons) {
+      var sizes = Object.keys(icons).map(function parse(str) {
+        return parseInt(str, 10);
+      });
+
+      sizes.sort(function(x, y) { return y - x; });
+
+      var index = sizes[(HVGA) ? sizes.length - 1 : 0];
+      iconPath = icons[index];
+    } else {
+      iconPath = app.icon;
+    }
+
+    if (!iconPath) {
       return null;
     }
 
-    var sizes = Object.keys(icons).map(function parse(str) {
-      return parseInt(str, 10);
-    });
-
-    sizes.sort(function(x, y) { return y - x; });
-
-    var index = sizes[(HVGA) ? sizes.length - 1 : 0];
-    var iconPath = icons[index];
-
     if (iconPath.indexOf('data:') !== 0) {
+      var origin = app.manifestURL.split('/').slice(0,3).join('/');
       iconPath = origin + iconPath;
     }
 
@@ -226,19 +236,24 @@ var CardsView = (function() {
       //display app icon on the tab
       if (DISPLAY_APP_ICON) {
         var iconURI = getIconURI(position);
+        var appIcon;
         if (iconURI) {
-          var appIcon = document.createElement('img');
-          appIcon.classList.add('appIcon');
+          appIcon = document.createElement('img');
           appIcon.src = iconURI;
-          card.appendChild(appIcon);
+        } else {
+          appIcon = document.createElement('span');
         }
+        appIcon.classList.add('appIcon');
+        card.appendChild(appIcon);
+        card.classList.add('appIconPreview');
       }
 
       var title = document.createElement('h1');
       title.textContent = app.name;
       card.appendChild(title);
 
-      var frameForScreenshot = app.iframe;
+      // only take the frame reference if we need to
+      var frameForScreenshot = DISPLAY_APP_SCREENSHOT && app.iframe;
 
       var origin = stack[position].origin;
       if (PopupManager.getPopupFromOrigin(origin)) {
@@ -261,7 +276,7 @@ var CardsView = (function() {
 
       if (TrustedUIManager.hasTrustedUI(origin)) {
         var popupFrame = TrustedUIManager.getDialogFromOrigin(origin);
-        frameForScreenshot = popupFrame.frame;
+        frameForScreenshot = DISPLAY_APP_SCREENSHOT && popupFrame.frame;
         var header = document.createElement('section');
         header.setAttribute('role', 'region');
         header.classList.add('skin-organic');
@@ -297,8 +312,14 @@ var CardsView = (function() {
             degree == 270) {
           isLandscape = true;
         }
+
         // Rotate screenshotView if needed
         screenshotView.classList.add('rotate-' + degree);
+
+        if (!DISPLAY_APP_SCREENSHOT) {
+          return;
+        }
+
         if (isLandscape) {
           // We must exchange width and height if it's landscape mode
           var width = card.clientHeight;
