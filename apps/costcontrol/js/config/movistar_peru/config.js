40691fb38d1242f6b92ea9c29f5624b1b8081913
mai <mri@tid.es>
2014-03-22 09:22:24 +0100

    Bug 986473 -Modify cost control parameters

--- apps/costcontrol/js/config/movistar_peru/config.js
+++ apps/costcontrol/js/config/movistar_peru/config.js
@@ -2,14 +2,15 @@
 
 var MovistarPeruConfig = {
   provider: 'Movistar',
-  is_free: true,
+  is_free: false,
   credit: { currency: 'S/.' },
   balance: {
     destination: '600',
     text: 'S',
     senders: ['515'],
     regexp: '([0-9,]+)\\.([0-9]{2})',
-    zero_regexp: '(Usted no tiene saldo en sus cuentas)'
+    zero_regexp: '(Usted no tiene saldo en sus cuentas)',
+    minimum_delay: 3 * 60 * 60 * 1000 // 3h
   },
   default_low_limit_threshold: 1
 };
