d84161f34888f858c8d03e7c5188d5a02a3d15cb
Diego Marcos <diego.marcos@gmail.com>
2014-03-23 00:13:45 +0000

    Bug 985012 - [Camera][Madai] Control buttons are disabled when we change from Video mode to camera mode and take a picture (cherry picked from commit c688805f329b3bc99a7a55d331cbc20d2e1d5442)

--- apps/camera/js/controllers/controls.js
+++ apps/camera/js/controllers/controls.js
@@ -36,12 +36,11 @@ ControlsController.prototype.bindEvents = function() {
   this.app.on('newthumbnail', this.onNewThumbnail);
   this.app.on('camera:ready', this.controls.enable);
   this.app.on('camera:busy', this.controls.disable);
-  this.app.on('camera:loading', this.controls.disable);
   this.app.on('change:recording', this.controls.setter('recording'));
   this.app.on('camera:timeupdate', this.controls.setVideoTimer);
   this.controls.on('click:capture', this.app.firer('capture'));
   this.controls.on('click:gallery', this.onGalleryButtonClick);
-  this.controls.on('click:switch', this.app.settings.mode.next);
+  this.controls.on('click:switch', this.onSwitchButtonClick);
   this.controls.on('click:cancel', this.onCancelButtonClick);
   this.app.on('timer:started', this.controls.disable);
   this.app.on('timer:cleared', this.controls.enable);
@@ -84,6 +83,11 @@ ControlsController.prototype.onTimerStarted = function(image) {
 ControlsController.prototype.onTimerEnd = function(image) {
   this.controls.set('capture-active', false);
   this.enableButtons();
+};
+
+ControlsController.prototype.onSwitchButtonClick = function() {
+  this.controls.disable();
+  this.app.settings.mode.next();
 };
 
 /**
