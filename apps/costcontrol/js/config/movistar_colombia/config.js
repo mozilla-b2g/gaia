55e9c12be944af0a0c51fab89f09501a4918ec24
gitmai <mri@tid.es>
2014-03-24 10:19:37 +0100

    Merge pull request #17474 from gitmai/bug-986473-CC-modify-parameters
    Bug 986473 -Modify cost control parameters
    r=salva

--- apps/costcontrol/js/config/movistar_colombia/config.js
+++ apps/costcontrol/js/config/movistar_colombia/config.js
@@ -2,14 +2,15 @@
 
 var MovistarColombiaConfig = {
   provider: 'Movistar',
-  is_free: true,
-  is_roaming_free: true,
+  is_free: false,
+  is_roaming_free: false,
   credit: { currency: '$' },
   balance: {
     destination: '611',
     text: 'SALDO,A',
     senders: ['612', '611'],
-    regexp: 'Saldo total:\\$([0-9,]+)\\.([0-9]{2})'
+    regexp: 'Saldo total:\\$([0-9,]+)\\.([0-9]{2})',
+    minimum_delay: 3 * 60 * 60 * 1000 // 3h
   },
   topup: {
     ussd_destination: '*611#'
