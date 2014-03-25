80af23f8c74d9d2e9388d8ed3c204040b5c528ec
Arthur Chen <crh0716@gmail.com>
2014-03-25 14:51:00 +0800

    Revert "Bug 974253 - Set preferred network type on startup based on the user preference"
    This reverts commit c6931e59efef682aa42ad09cab3d843669d5f8f7.

--- apps/settings/js/carrier.js
+++ apps/settings/js/carrier.js
@@ -272,28 +272,17 @@ var CarrierSettings = (function(window, document, undefined) {
       return;
 
     var alertDialog = document.getElementById('preferredNetworkTypeAlert');
-    var message = document.getElementById('preferredNetworkTypeAlertMessage');
     var continueButton = alertDialog.querySelector('button');
     continueButton.addEventListener('click', function onClickHandler() {
       alertDialog.hidden = true;
       getSupportedNetworkInfo(_mobileConnection, cs_updateNetworkTypeSelector);
     });
 
-    var preferredNetworkTypeHelper =
-      SettingsHelper('ril.radio.preferredNetworkType');
-
     var selector = document.getElementById('preferredNetworkType');
-    selector.addEventListener('blur', function evenHandler() {
-      var targetIndex = DsdsSettings.getIccCardIndexForCellAndDataSettings();
+    selector.addEventListener('change', function evenHandler() {
       var type = selector.value;
       var request = _mobileConnection.setPreferredNetworkType(type);
-
-      request.onsuccess = function onSuccessHandler() {
-        preferredNetworkTypeHelper.get(function gotPNT(values) {
-          values[targetIndex] = type;
-          preferredNetworkTypeHelper.set(values);
-        });
-      };
+      var message = document.getElementById('preferredNetworkTypeAlertMessage');
       request.onerror = function onErrorHandler() {
         message.textContent = _('preferredNetworkTypeAlertErrorMessage');
         alertDialog.hidden = false;
@@ -310,17 +299,16 @@ var CarrierSettings = (function(window, document, undefined) {
       return;
     }
 
-    var selector = document.getElementById('preferredNetworkType');
-    // Clean up all option before updating again.
-    while (selector.hasChildNodes()) {
-      selector.removeChild(selector.lastChild);
-    }
-
     var request = _mobileConnection.getPreferredNetworkType();
     request.onsuccess = function onSuccessHandler() {
       var supportedNetworkTypes = supportedNetworkTypeResult.networkTypes;
       var networkType = request.result;
       if (networkType) {
+        var selector = document.getElementById('preferredNetworkType');
+        // Clean up all option before updating again.
+        while (selector.hasChildNodes()) {
+          selector.removeChild(selector.lastChild);
+        }
         supportedNetworkTypes.forEach(function(type) {
           var option = document.createElement('option');
           option.value = type;
--- apps/system/test/unit/telephony_settings_test.js
+++ apps/system/test/unit/telephony_settings_test.js
@@ -21,12 +21,7 @@ suite('system/TelephonySettings', function() {
 
   var fakeConnections = [{
     setVoicePrivacyMode: function() {},
-    setRoamingPreference: function() {},
-    setPreferredNetworkType: function() {},
-    supportedNetworkTypes: ['gsm', 'wcdma', 'cdma', 'evdo'],
-    addEventListener: function() {},
-    removeEventListener: function() {},
-    radioState: 'enabled'
+    setRoamingPreference: function() {}
   }];
 
   setup(function() {
@@ -67,24 +62,18 @@ suite('system/TelephonySettings', function() {
       subject = new TelephonySettings();
       var privacyStub = this.sinon.stub(subject, 'initVoicePrivacy');
       var roamingStub = this.sinon.stub(subject, 'initRoaming');
-      var preferredStub = this.sinon.stub(subject, 'initPreferredNetworkType');
       subject.start();
       assert.ok(privacyStub.calledOnce);
       assert.ok(roamingStub.calledOnce);
-      assert.ok(preferredStub.calledOnce);
     });
   });
 
   suite('initVoicePrivacy', function() {
-    var stub, subject;
+    var stub;
     setup(function() {
       navigator.mozMobileConnections = fakeConnections;
       stub = this.sinon.stub(fakeConnections[0], 'setVoicePrivacyMode')
         .returns(reqResponse);
-
-      subject = new TelephonySettings();
-      this.sinon.stub(subject, 'initRoaming');
-      this.sinon.stub(subject, 'initPreferredNetworkType');
     });
 
     teardown(function() {
@@ -92,11 +81,17 @@ suite('system/TelephonySettings', function() {
     });
 
     test('setVoicePrivacyMode default value', function() {
+      subject = new TelephonySettings();
+      this.sinon.stub(subject, 'initRoaming');
+
       subject.start();
       assert.ok(stub.calledWith(false));
     });
 
     test('setVoicePrivacyMode from settings', function() {
+      subject = new TelephonySettings();
+      this.sinon.stub(subject, 'initRoaming');
+
       MockSettingsHelper.instances['ril.voicePrivacy.enabled'] =
         {value: ['custom-value']};
       subject.start();
@@ -105,15 +100,11 @@ suite('system/TelephonySettings', function() {
   });
 
   suite('initRoaming', function() {
-    var stub, subject;
+    var stub;
     setup(function() {
       navigator.mozMobileConnections = fakeConnections;
       stub = this.sinon.stub(fakeConnections[0], 'setRoamingPreference')
         .returns(reqResponse);
-
-      subject = new TelephonySettings();
-      this.sinon.stub(subject, 'initVoicePrivacy');
-      this.sinon.stub(subject, 'initPreferredNetworkType');
     });
 
     teardown(function() {
@@ -121,88 +112,21 @@ suite('system/TelephonySettings', function() {
     });
 
     test('setRoamingPreference default value', function() {
+      subject = new TelephonySettings();
+      this.sinon.stub(subject, 'initVoicePrivacy');
+
       subject.start();
       assert.ok(stub.calledWith('any'));
     });
 
     test('setRoamingPreference from settings', function() {
+      subject = new TelephonySettings();
+      this.sinon.stub(subject, 'initVoicePrivacy');
+
       MockSettingsHelper.instances['ril.roaming.preference'] =
         {value: ['custom-value2']};
       subject.start();
       assert.ok(stub.calledWith('custom-value2'));
-    });
-  });
-
-  suite('initPreferredNetworkType', function() {
-    var stub, subject;
-    setup(function() {
-      navigator.mozMobileConnections = fakeConnections;
-      stub = this.sinon.stub(fakeConnections[0], 'setPreferredNetworkType')
-        .returns(reqResponse);
-
-      subject = new TelephonySettings();
-      this.sinon.stub(subject, 'initVoicePrivacy');
-      this.sinon.stub(subject, 'initRoaming');
-    });
-
-    teardown(function() {
-      stub.restore();
-    });
-
-    test('setPreferredNetworkType default value', function() {
-      subject.start();
-      assert.ok(stub.calledWith('wcdma/gsm/cdma/evdo'));
-    });
-
-    test('setPreferredNetworkType from settings', function() {
-      MockSettingsHelper.instances['ril.radio.preferredNetworkType'] =
-        {value: ['custom-value3']};
-      subject.start();
-      assert.ok(stub.calledWith('custom-value3'));
-    });
-
-    test('setPreferredNetworkType is not called when radio state is not ' +
-      'enabled', function() {
-        var fakeConnection = fakeConnections[0];
-        var originalRadioState = fakeConnection.radioState;
-
-        fakeConnection.radioState = 'disabled';
-        subject.start();
-        assert.ok(stub.notCalled);
-        fakeConnection.radioState = originalRadioState;
-    });
-
-    test('setPreferredNetworkType is called when radio state becomes enabled',
-      function() {
-        var fakeConnection = fakeConnections[0];
-        var originalRadioState = fakeConnection.radioState;
-        var callbacks = {
-          'radiostatechange': []
-        };
-        sinon.stub(fakeConnection, 'addEventListener',
-          function(event, callback) {
-            if (callbacks[event]) {
-              callbacks[event].push(callback);
-            }
-        });
-        sinon.spy(fakeConnection, 'removeEventListener');
-
-        fakeConnection.radioState = 'disabled';
-        subject.start();
-        fakeConnection.radioState = 'enabled';
-        console.log(callbacks.radiostatechange.length);
-        callbacks.radiostatechange.forEach(function(callback) {
-          callback();
-        });
-
-        assert.ok(stub.calledOnce);
-        // Ensure that the event listener is removed correctly
-        sinon.assert.calledWith(fakeConnection.removeEventListener,
-          'radiostatechange', callbacks.radiostatechange[0]);
-
-        fakeConnection.addEventListener.restore();
-        fakeConnection.removeEventListener.restore();
-        fakeConnection.radioState = originalRadioState;
     });
   });
 });
