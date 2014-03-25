4cd9e27a09434c583008d27034d2a0e13d6cdf59
Ghislain 'Aus' Lacroix <aus@mozilla.com>
2014-03-25 15:34:29 -0700

    bug 981764
    * Add 'cardviewbeforeshow' and 'cardviewbeforeclose' events to cards view.
    * Added handling of 'cardviewbeforeshow' and 'cardviewbeforeclose' to HomescreenLauncher
      so that it will now fadeOut the homescreen before the card view is shown and fadeIn the
      homescreen before the card view is closed.
    * Added tests for new homescreen launcher behavior.
    
    r=alive

--- apps/system/test/unit/homescreen_launcher_test.js
+++ apps/system/test/unit/homescreen_launcher_test.js
@@ -155,5 +155,23 @@ suite('system/HomescreenLauncher', function() {
       assert.isTrue(stubFadeOut.called);
       stubFadeOut.restore();
     });
+
+    test('cards view before shown, then closed', function() {
+      MockSettingsListener.mCallbacks['homescreen.manifestURL']('first.home');
+      homescreen = window.homescreenLauncher.getHomescreen();
+      var stubFadeOut = this.sinon.stub(homescreen, 'fadeOut');
+      window.homescreenLauncher.handleEvent({
+        type: 'cardviewbeforeshow'
+      });
+      assert.isTrue(stubFadeOut.called);
+      stubFadeOut.restore();
+
+      var stubFadeIn = this.sinon.stub(homescreen, 'fadeIn');
+      window.homescreenLauncher.handleEvent({
+        type: 'cardviewbeforeclose'
+      });
+      assert.isTrue(stubFadeIn.called);
+      stubFadeIn.restore();
+    });
   });
 });
