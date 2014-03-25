577a883aa60c0f5309b4554505d60ff04db583e9
Eitan Isaacson <eitan@monotonous.org>
2014-03-25 15:48:03 -0700

    Bug 990235 - [Clock] Localize alarm panel.

--- apps/clock/js/panels/alarm/main.js
+++ apps/clock/js/panels/alarm/main.js
@@ -5,6 +5,7 @@ var Panel = require('panel');
 var ClockView = require('panels/alarm/clock_view');
 var AlarmList = require('panels/alarm/alarm_list');
 var ActiveAlarm = require('panels/alarm/active_alarm');
+var mozL10n = require('l10n');
 var html = require('text!panels/alarm/panel.html');
 
 function AlarmPanel() {
@@ -14,6 +15,7 @@ function AlarmPanel() {
   ClockView.init();
   AlarmList.init();
   ActiveAlarm.singleton().init();
+  mozL10n.translate(this.element);
 }
 
 AlarmPanel.prototype = Object.create(Panel.prototype);
