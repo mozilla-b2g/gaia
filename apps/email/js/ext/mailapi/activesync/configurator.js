a85174bf128ffafe3a226747294c092114e3275f
Marcus Cavanaugh <m@mcav.com>
2014-03-26 15:12:07 -0700

    Bug 805501 - Support different username / password / credentials for SMTP server from IMAP server. r=asuth

--- apps/email/js/ext/mailapi/activesync/configurator.js
+++ apps/email/js/ext/mailapi/activesync/configurator.js
@@ -2710,6 +2710,13 @@ ActiveSyncAccount.prototype = {
       this.conn.connect(function(error) {
         if (error) {
           this._reportErrorIfNecessary(error);
+          // If the error was HTTP 401 (bad user/pass), report it as
+          // bad-user-or-pass so that account logic like
+          // _cmd_clearAccountProblems knows whether or not to report
+          // the error as user-serviceable.
+          if (this._isBadUserOrPassError(error) && !failString) {
+            failString = 'bad-user-or-pass';
+          }
           errback(failString || 'unknown');
           return;
         }
@@ -2720,6 +2727,12 @@ ActiveSyncAccount.prototype = {
     }
   },
 
+  _isBadUserOrPassError: function(error) {
+    return (error &&
+            error instanceof $asproto.HttpError &&
+            error.status === 401);
+  },
+
   /**
    * Reports the error to the user if necessary.
    */
@@ -2728,9 +2741,10 @@ ActiveSyncAccount.prototype = {
       return;
     }
 
-    if (error instanceof $asproto.HttpError && error.status === 401) {
+    if (this._isBadUserOrPassError(error)) {
       // prompt the user to try a different password
-      this.universe.__reportAccountProblem(this, 'bad-user-or-pass');
+      this.universe.__reportAccountProblem(
+        this, 'bad-user-or-pass', 'incoming');
     }
   },
 
