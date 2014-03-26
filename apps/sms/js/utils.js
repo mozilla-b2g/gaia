bb1a0c234c4354b0df6d8d61e78cd7eb5b670461
Steve Chung <schung@mozilla.com>
2014-03-26 16:25:59 +0800

    Bug 983631 - [Messages] After deleting a message thread the notification persists forever.
    * Add closeNotificationsForThread api and make it return as promise
    * Apply julien's promise-based test case

--- apps/sms/js/utils.js
+++ apps/sms/js/utils.js
@@ -1,7 +1,7 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- /
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
-/* globals ContactPhotoHelper */
+/* globals ContactPhotoHelper, Notification, Threads*/
 
 (function(exports) {
   'use strict';
@@ -542,6 +542,31 @@
           window.URL.revokeObjectURL(this.src);
         };
       });
+    },
+
+    /*
+      Helper function for removing notifications. It will fetch the notification
+      using the current threadId or the parameter if provided, and close them
+       from the returned list.
+    */
+    closeNotificationsForThread: function ut_closeNotificationsForThread(tid) {
+      var threadId = tid ? tid : Threads.currentId;
+      if (!threadId) {
+        return;
+      }
+
+      var targetTag = 'threadId:' + threadId;
+
+      return Notification.get({tag: targetTag})
+        .then(
+          function onSuccess(notifications) {
+            for (var i = 0; i < notifications.length; i++) {
+              notifications[i].close();
+            }
+          }
+        ).catch(function onError(reason) {
+          console.error('Notification.get(tag: ' + targetTag + '): ', reason);
+        });
     }
   };
 
