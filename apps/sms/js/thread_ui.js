344e5ffb9262e5f9fb4686b3d71fe30fe4ba331a
sasikala.paruchuri <sasikala.paruchuri@lge.com>
2014-03-26 17:47:44 +0530

    Bug 984841 - [Messages] The cursor and the keyboard are not shown when New message screen is opened r=julien
    Updated review comments

--- apps/sms/js/thread_ui.js
+++ apps/sms/js/thread_ui.js
@@ -333,6 +333,7 @@ var ThreadUI = global.ThreadUI = {
    * visible.
    */
   onBeforeEnter: function thui_onBeforeEnter() {
+    Recipients.View.isFocusable = true;
     if (!this.multiSimActionButton) {
       // handles the various actions on the send button and encapsulates the
       // DSDS specific behavior
