2e66c41db44f12d49ef13053386c312464252d1e
Justin Imel <justinmichaelimel@gmail.com>
2014-03-27 03:29:40 -0400

    Integrated app send features into home screen
    Rough draft home screen with send features added.  Only the send side
    is implemented now and no back end is in place yet…

--- apps/homescreen/js/request.js
+++ apps/homescreen/js/request.js
@@ -3,7 +3,7 @@
 
 var ConfirmDialog = (function() {
 
-  var dialog, titleElem, messageElem, cancelButton, confirmButton;
+  var dialog, titleElem, messageElem, cancelButton, cancelButton2, cancelButton3, confirmButton;
 
   var _ = navigator.mozL10n.get;
 
@@ -11,9 +11,11 @@ var ConfirmDialog = (function() {
     dialog = document.getElementById('confirm-dialog');
 
     titleElem = document.getElementById('confirm-dialog-title');
-    messageElem = document.getElementById('confirm-dialog-message');
+    //messageElem = document.getElementById('confirm-dialog-message');
 
     cancelButton = document.getElementById('confirm-dialog-cancel-button');
+	cancelButton2 = document.getElementById('confirm-dialog-cancel-button2');
+	cancelButton3 = document.getElementById('confirm-dialog-cancel-button3');
     confirmButton = document.getElementById('confirm-dialog-confirm-button');
   }
 
@@ -26,14 +28,22 @@ var ConfirmDialog = (function() {
     },
 
     show: function dialog_show(title, msg, cancel, confirm) {
+	  
+	  //Make sure send app to dialog is hidden at first.
+	  document.getElementById('app_manage').style.display='block';
+	  document.getElementById('send_manage').style.display='none';
+		
+	  //Slice "remove" or "delete" from the dialog title
+	  title = title.slice(7);
+	
       titleElem.textContent = title;
-      messageElem.textContent = msg;
+      //messageElem.textContent = msg;
 
       cancelButton.textContent = cancel.title;
       confirmButton.textContent = confirm.title;
 
-      cancelButton.className = '';
-      confirmButton.className = '';
+      //cancelButton.className = '';
+      //confirmButton.className = '';
 
       if (cancel.applyClass) {
         cancelButton.classList.add(cancel.applyClass);
@@ -42,7 +52,7 @@ var ConfirmDialog = (function() {
         confirmButton.classList.add(confirm.applyClass);
       }
 
-      cancelButton.onclick = confirmButton.onclick = clickHandler;
+      cancelButton.onclick = cancelButton2.onclick = cancelButton3.onclick = confirmButton.onclick = clickHandler;
 
       function clickHandler(evt) {
         evt.target === confirmButton ? confirm.callback() : cancel.callback();
