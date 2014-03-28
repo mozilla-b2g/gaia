aa3bae6e8b8a37dc1bf5b13dc54b5841f93be5e4
jrburke <jrburke@gmail.com>
2014-03-27 17:27:34 -0700

    Bug 989104 - [email] allow for longer module load timeouts for background loads

--- apps/email/js/mail_app.js
+++ apps/email/js/mail_app.js
@@ -9,6 +9,13 @@
 // their own config.
 if (typeof TestUrlResolver === 'undefined') {
   requirejs.config({
+    // waitSeconds is set to the default here; the build step rewrites
+    // it to 0 in build/email.build.js so that we never timeout waiting
+    // for modules in production. This is important when the device is
+    // under super-low-memory stress, as it may take a while for the
+    // device to get around to loading things email for background tasks
+    // like periodic sync.
+    waitSeconds: 7,
     baseUrl: 'js',
     paths: {
       l10nbase: '../shared/js/l10n',
