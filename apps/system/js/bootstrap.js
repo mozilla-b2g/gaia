c555cb7b8847f72e7c9f813812e53a2606ae98cf
John Hu <johu@mozilla.com>
2014-04-01 10:03:39 -0600

    Bug 971554 - [System2] Instantiable SoundManager, r=alive

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -78,6 +78,7 @@ window.addEventListener('load', function startup() {
   window.layoutManager = new LayoutManager().start();
   window.remoteDebugger = new RemoteDebugger();
   window.softwareButtonManager = new SoftwareButtonManager().start();
+  window.soundManager = new SoundManager().start();
 
   window.telephonySettings = new TelephonySettings();
   window.telephonySettings.start();
