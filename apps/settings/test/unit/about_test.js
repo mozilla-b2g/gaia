6c50349f41d40ba175ea0fc0c2c2cbd739ba7170
Arthur Chen <crh0716@gmail.com>
2014-04-02 18:02:37 +0800

    Bug 975343 - Show multiple phone numbers and iccids on dual sim devices

--- apps/settings/test/unit/about_test.js
+++ apps/settings/test/unit/about_test.js
@@ -1,3 +1,7 @@
+/* global MockL10n, MockNavigatorMozMobileConnections,
+          MockNavigatorMozIccManager, MockNavigatorSettings, MocksHelper,
+          About */
+
 'use strict';
 
 requireApp('settings/test/unit/mock_l10n.js');
@@ -5,9 +9,14 @@ requireApp('settings/test/unit/mock_navigator_settings.js');
 requireApp('settings/test/unit/mock_settings.js');
 requireApp('settings/test/unit/mocks_helper.js');
 requireApp('settings/js/about.js');
-requireApp('../../shared/js/screen_layout.js');
+require('/shared/test/unit/mocks/mock_navigator_moz_mobile_connections.js');
+require('/shared/test/unit/mocks/mock_navigator_moz_icc_manager.js');
 
-var mocksForAbout = ['Settings'];
+var mocksForAbout = [
+  'Settings',
+  'NavigatorMozMobileConnections',
+  'NavigatorMozIccManager'
+];
 
 mocksForAbout.forEach(function(mockName) {
   if (! window[mockName]) {
@@ -15,12 +24,8 @@ mocksForAbout.forEach(function(mockName) {
   }
 });
 
-if (!window.getMobileConnection) {
-  window.getMobileConnection = null;
-}
-
 suite('about >', function() {
-  var realL10n, realNavigatorSettings, realGetMobileConnection;
+  var realL10n, realNavigatorSettings, realMobileConnections, realIccManager;
   var updateStatusNode, systemStatus, generalInfo;
   var mocksHelper;
 
@@ -28,10 +33,10 @@ suite('about >', function() {
     realL10n = navigator.mozL10n;
     navigator.mozL10n = MockL10n;
 
-    realGetMobileConnection = window.getMobileConnection;
-    window.getMobileConnection = function() {
-      return null;
-    };
+    realIccManager = navigator.mozIccManager;
+    navigator.mozIccManager = MockNavigatorMozIccManager;
+    realMobileConnections = navigator.mozMobileConnections;
+    navigator.mozMobileConnections = MockNavigatorMozMobileConnections;
     realNavigatorSettings = navigator.mozSettings;
     navigator.mozSettings = MockNavigatorSettings;
 
@@ -40,13 +45,19 @@ suite('about >', function() {
   });
 
   suiteTeardown(function() {
+    MockNavigatorMozIccManager.mTeardown();
+    MockNavigatorMozIccManager.addIcc('12345', {
+      'cardState': 'ready'
+    });
+
     navigator.mozL10n = realL10n;
     realL10n = null;
-
     navigator.mozSettings = realNavigatorSettings;
     realNavigatorSettings = null;
-    window.getMobileConnection = realGetMobileConnection;
-    realGetMobileConnection = null;
+    navigator.mozMobileConnections = realMobileConnections;
+    realMobileConnections = null;
+    navigator.mozIccManager = realIccManager;
+    realIccManager = null;
     mocksHelper.suiteTeardown();
   });
 
@@ -56,6 +67,9 @@ suite('about >', function() {
     var updateNodes =
       '<section id="root" role="region"></section>' +
       '<ul>' +
+        '<li id="deviceinfo-phone-num">' +
+          '<small id="deviceInfo-msisdns"></small>' +
+        '</li>' +
         '<li>' +
           '<label>' +
             '<button id="check-update-now">Check Now</button>' +
@@ -126,7 +140,9 @@ suite('about >', function() {
 
     suite('getting response for system update >', function() {
       suite('successful >', function() {
+        var handlerToBeRemoved = null;
         setup(function() {
+          handlerToBeRemoved = geckoUpdateHandlers[0];
           MockNavigatorSettings.mTriggerObservers(geckoUpdateSetting, {
             settingValue: 'check-complete'
           });
@@ -144,7 +160,7 @@ suite('about >', function() {
           var removedObservers = MockNavigatorSettings.mRemovedObservers;
           var removedObserver = removedObservers[geckoUpdateSetting][0];
           assert.isFunction(removedObserver);
-          assert.equal(removedObserver, geckoUpdateHandlers[0]);
+          assert.equal(removedObserver, handlerToBeRemoved);
         });
       });
 
@@ -170,22 +186,23 @@ suite('about >', function() {
         assert.equal(systemStatus.textContent, 'retry-when-online');
       });
 
-      test('check-error', function() {
-        var errors = ['check-error-http-200', 'check-error-http-403',
-                      'check-error-http-404', 'check-error-http-500',
-                      'check-error-2152398878'];
-        for (var i = 0; i < errors.length; i++) {
-          MockNavigatorSettings.mTriggerObservers(geckoUpdateSetting, {
-            settingValue: errors[i]
+      ['check-error-http-200', 'check-error-http-403', 'check-error-http-404',
+       'check-error-http-500', 'check-error-2152398878']
+        .forEach(function(error) {
+          test(error, function() {
+            MockNavigatorSettings.mTriggerObservers(geckoUpdateSetting, {
+              settingValue: error
+            });
+            assert.equal(systemStatus.textContent, error);
           });
-          assert.equal(systemStatus.textContent, errors[i]);
-        }
       });
     });
 
     suite('getting response for app update >', function() {
       suite('successful >', function() {
+        var handlerToBeRemoved = null;
         setup(function() {
+          handlerToBeRemoved = appsUpdateHandlers[0];
           MockNavigatorSettings.mTriggerObservers(appsUpdateSetting, {
             settingValue: 'check-complete'
           });
@@ -203,7 +220,7 @@ suite('about >', function() {
           var removedObservers = MockNavigatorSettings.mRemovedObservers;
           var removedObserver = removedObservers[appsUpdateSetting][0];
           assert.isFunction(removedObserver);
-          assert.equal(removedObserver, appsUpdateHandlers[0]);
+          assert.equal(removedObserver, handlerToBeRemoved);
         });
       });
     });
@@ -259,6 +276,115 @@ suite('about >', function() {
             assert.equal(systemStatus.textContent.length, 0);
           });
         });
+      });
+    });
+  });
+
+  suite('loadHardwareInfo >', function() {
+    var deviceInfoPhoneNum;
+    var iccIds = ['12345', '22345'];
+    var sandbox = sinon.sandbox.create();
+
+    setup(function() {
+      deviceInfoPhoneNum = document.getElementById('deviceinfo-phone-num');
+      // Remove all default iccids
+      MockNavigatorMozIccManager.iccIds.forEach(function(iccId) {
+        MockNavigatorMozIccManager.removeIcc(iccId);
+      });
+      MockNavigatorMozIccManager.addIcc(iccIds[0]);
+      MockNavigatorMozMobileConnections[0].iccId = iccIds[0];
+    });
+
+    teardown(function() {
+      MockNavigatorMozIccManager.mTeardown();
+      sandbox.restore();
+    });
+
+    suite('single sim', function() {
+      test('the list item should be hidden when without iccinfo',
+        function() {
+          MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo = null;
+          About.loadHardwareInfo();
+          assert.isTrue(deviceInfoPhoneNum.hidden);
+      });
+
+      test('should show unknown phone number when no msisdn and mdn',
+        function() {
+          MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo = {};
+          sandbox.spy(MockL10n, 'localize');
+          About.loadHardwareInfo();
+          var span =
+            deviceInfoPhoneNum.querySelector('#deviceInfo-msisdns span');
+          sinon.assert.calledWith(MockL10n.localize, span,
+            'unknown-phoneNumber');
+      });
+
+      test('should show correct value when with mdn', function() {
+        MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo =
+          { mdn: 'mdn' };
+        About.loadHardwareInfo();
+        var span = deviceInfoPhoneNum.querySelector('#deviceInfo-msisdns span');
+        assert.equal(span.textContent, 'mdn');
+      });
+
+      test('should show correct value when with msisdn', function() {
+        MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo =
+          { msisdn: 'msisdn' };
+        About.loadHardwareInfo();
+        var span = deviceInfoPhoneNum.querySelector('#deviceInfo-msisdns span');
+        assert.equal(span.textContent, 'msisdn');
+      });
+    });
+
+    suite('multiple sim', function() {
+      setup(function() {
+        MockNavigatorMozIccManager.addIcc(iccIds[1]);
+        MockNavigatorMozMobileConnections.mAddMobileConnection();
+        MockNavigatorMozMobileConnections[1].iccId = iccIds[1];
+        MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo =
+          { mdn: 'mdn1' };
+        MockNavigatorMozIccManager.getIccById(iccIds[1]).iccInfo =
+          { mdn: 'mdn2' };
+      });
+
+      test('the list item should be hidden when without iccinfo',
+        function() {
+          MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo = null;
+          MockNavigatorMozIccManager.getIccById(iccIds[1]).iccInfo = null;
+          About.loadHardwareInfo();
+          assert.isTrue(deviceInfoPhoneNum.hidden);
+      });
+
+      test('should show the list item when there are iccinfos', function() {
+        MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo = null;
+        MockNavigatorMozIccManager.getIccById(iccIds[1]).iccInfo = {};
+        About.loadHardwareInfo();
+        assert.isFalse(deviceInfoPhoneNum.hidden);
+      });
+
+      test('should show unknown phone number when no msisdn and mdn',
+        function() {
+          MockNavigatorMozIccManager.getIccById(iccIds[0]).iccInfo = {};
+          MockNavigatorMozIccManager.getIccById(iccIds[1]).iccInfo = {};
+          sandbox.spy(MockL10n, 'localize');
+          About.loadHardwareInfo();
+          var spans =
+            deviceInfoPhoneNum.querySelectorAll('#deviceInfo-msisdns span');
+
+          assert.deepEqual(MockL10n.localize.args[0], [
+            spans[0], 'unknown-phoneNumber-sim', { index: 1 }
+          ]);
+          assert.deepEqual(MockL10n.localize.args[1], [
+            spans[1], 'unknown-phoneNumber-sim', { index: 2 }
+          ]);
+      });
+
+      test('should show correct sim indicator', function() {
+        About.loadHardwareInfo();
+        var spans =
+          deviceInfoPhoneNum.querySelectorAll('#deviceInfo-msisdns span');
+        assert.equal(spans[0].textContent, 'SIM 1: mdn1');
+        assert.equal(spans[1].textContent, 'SIM 2: mdn2');
       });
     });
   });
