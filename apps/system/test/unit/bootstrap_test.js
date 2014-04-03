f12773c4f39292d466d73d8af2943a1895a36030
gasolin <gasolin@gmail.com>
2014-04-03 16:06:47 +0800

    Revert "Merge pull request #17683 from gasolin/issue--971538"
    This reverts commit 4a2e2032589de749583a3674cb2a433b5a8e8361, reversing
    changes made to fb0aec1ef13f915a9739fc889df88ceda07c7b53.

--- apps/system/test/unit/bootstrap_test.js
+++ apps/system/test/unit/bootstrap_test.js
@@ -19,7 +19,6 @@ requireApp('system/test/unit/mock_home_gesture.js');
 requireApp('system/test/unit/mock_homescreen_launcher.js');
 requireApp('system/test/unit/mock_l10n.js');
 requireApp('system/test/unit/mock_media_recording.js');
-requireApp('system/test/unit/mock_permission_manager.js');
 requireApp('system/test/unit/mock_places.js');
 requireApp('system/test/unit/mock_remote_debugger.js');
 requireApp('system/test/unit/mock_rocketbar.js');
@@ -74,8 +73,7 @@ mocha.globals([
   'ActivityWindowFactory',
   'visibilityManager',
   'applications',
-  'layoutManager',
-  'permissionManager'
+  'layoutManager'
 ]);
 
 var mocksForBootstrap = new MocksHelper([
@@ -87,7 +85,6 @@ var mocksForBootstrap = new MocksHelper([
   'HomeGesture',
   'HomescreenLauncher',
   'MediaRecording',
-  'PermissionManager',
   'Places',
   'RemoteDebugger',
   'Rocketbar',
