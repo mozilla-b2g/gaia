6dbabfd4121c06a7de6ad2902a060bfb6fbae838
Hubert Figuière <hub@figuiere.net>
2014-04-03 15:16:32 +0800

    Change the atom. STILL BROKEN

--- tests/performance/performance_helper_atom.js
+++ tests/performance/performance_helper_atom.js
@@ -1,92 +1,121 @@
-'use strict';
+Components.utils.import('resource://gre/modules/Services.jsm');
 
-(function(window) {
-  var PERF_EVENT_NAME = 'x-moz-perf';
-  var PERF_FLAG_NAME = 'mozPerfHasListener';
-  var perfMeasurements;
-  var hasRegistered;
 
-  var DEBUG = true;
+function _registerListener(document)
+{
+  try {
+    let window = document.defaultView.wrappedJSObject;
 
-  function debug() {
-    if (DEBUG) {
-      var msg = Array.slice(arguments).join(' ');
-      console.log(
-        'PerformanceHelperAtom: ' + msg + ' (' + Date.now() / 1000 + ')'
-      );
-    }
-  }
+    var PERF_EVENT_NAME = 'x-moz-perf';
+    var PERF_FLAG_NAME = 'mozPerfHasListener';
+    var perfMeasurements;
+    var hasRegistered;
 
-  // FIXME: we have to find why this gets imported several times for one test
-  if ('PerformanceHelperAtom' in window) {
-    debug('FIXME We got called twice');
-    return;
-  }
+    var DEBUG = true;
 
-  var finalEventName;
-  var finishCallback;
-
-  function handlePerfEvent(e) {
-    var evtName = e.detail.name;
-
-    debug('got', evtName);
-
-    perfMeasurements[evtName] = window.performance.now();
-    checkFinish();
-  }
-
-  function checkFinish(name, cb) {
-    if (name) {
-      finalEventName = name;
-    }
-    if (cb) {
-      finishCallback = cb;
+    function debug() {
+      if (DEBUG) {
+        var msg = Array.slice(arguments).join(' ');
+        dump(
+          'PerformanceHelperAtom: ' + msg + ' (' + Date.now() / 1000 + ')'
+        );
+      }
     }
 
-    debug('waiting for ', finalEventName);
-
-    if (finishCallback && finalEventName &&
-        finalEventName in perfMeasurements) {
-      finishCallback();
-      finishCallback = null;
+    // FIXME: we have to find why this gets imported several times for one test
+    if ('PerformanceHelperAtom' in window) {
+      debug('FIXME We got called twice');
+      return;
     }
-  }
 
-  window.PerformanceHelperAtom = {
-    getMeasurements: function() {
-      return perfMeasurements;
-    },
+    var finalEventName;
+    var finishCallback;
 
-    init: function() {
-      if (hasRegistered) {
-        return;
+    function handlePerfEvent(e) {
+      var evtName = e.detail.name;
+
+      debug('got', evtName);
+
+      perfMeasurements[evtName] = window.performance.now();
+      checkFinish();
+    }
+
+    function checkFinish(name, cb) {
+      if (name) {
+        finalEventName = name;
+      }
+      if (cb) {
+        finishCallback = cb;
       }
 
-      debug('registering');
-      perfMeasurements = Object.create(null);
-      perfMeasurements.start = window.performance.now();
+      debug('waiting for ', finalEventName);
 
-      window.addEventListener(PERF_EVENT_NAME, handlePerfEvent);
-      window[PERF_FLAG_NAME] = hasRegistered = true;
-
-      debug('end registering');
-    },
-
-    unregister: function() {
-      // note: we're not reseting mozPerfHasListener to false here because at
-      // this point we basically don't mind if the app keeps sending perf
-      // events.
-      window.removeEventListener(PERF_EVENT_NAME, handlePerfEvent);
-      hasRegistered = false;
-
-      marionetteScriptFinished();
-    },
-
-    waitForEvent: function(name, cb) {
-      checkFinish(name, cb);
+      if (finishCallback && finalEventName &&
+          finalEventName in perfMeasurements) {
+        finishCallback();
+        finishCallback = null;
+      }
     }
-  };
 
-  window.PerformanceHelperAtom.init();
-})(window.wrappedJSObject);
+    window.__defineGetter__('PerformanceHelperAtom', function() {
+      return {
+        getMeasurements: function() {
+          return perfMeasurements;
+        },
+
+        init: function() {
+          if (hasRegistered) {
+            return;
+          }
+
+          debug('registering');
+          perfMeasurements = Object.create(null);
+          perfMeasurements.start = window.performance.now();
+
+          window.addEventListener(PERF_EVENT_NAME, handlePerfEvent);
+          window[PERF_FLAG_NAME] = hasRegistered = true;
+
+          debug('end registering');
+        },
+
+        unregister: function() {
+          // note: we're not reseting mozPerfHasListener to false here because at
+          // this point we basically don't mind if the app keeps sending perf
+          // events.
+          window.removeEventListener(PERF_EVENT_NAME, handlePerfEvent);
+          hasRegistered = false;
+
+          marionetteScriptFinished();
+        },
+
+        waitForEvent: function(name, cb) {
+          checkFinish(name, cb);
+        }
+      };
+    });
+
+    window.PerformanceHelperAtom.init();
+  } catch (e) {
+    dump('ERR: ' + e.toString() + '\n');
+  }
+}
+
+
+try {
+  if (document) {
+    _registerListener(document);
+  } else {
+    Services.obs.addObserver(function(document) {
+      if (!document || !document.location)
+        return;
+
+      var location = document.location.href;
+      dump('\n\n' + location + '\n\n');
+
+      _registerListener(document);
+    }, 'document-element-inserted', false);
+  }
+} catch (e) {
+  dump('ERR: ' + e.toString() + '\n');
+}
 
