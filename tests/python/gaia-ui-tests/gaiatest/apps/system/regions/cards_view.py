1124ffc11d02b42227a943ba8f441bfe4cdae05d
Bob Silverberg <bob.silverberg@gmail.com>
2014-04-07 14:02:34 -0400

    Revert "Revert "Merge pull request #17988 from bobsilverberg/cards_view_v1.3""
    This reverts commit f43898e5c9488ae8614c71d2fe9b7c205726c471.

--- tests/python/gaia-ui-tests/gaiatest/apps/system/regions/cards_view.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/system/regions/cards_view.py
@@ -2,16 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import time
 from marionette.by import By
+from marionette.marionette import Actions
+
 from gaiatest.apps.base import Base
 
 
 class CardsView(Base):
 
     # Home/Cards view locators
-    _cards_view_locator = (By.CSS_SELECTOR, '#cards-view.active')
-    _no_apps_locator = (By.CSS_SELECTOR, '.no-recent-apps')
+    _cards_view_locator = (By.CSS_SELECTOR, '#screen.cards-view:not(.edges)')
     # Check that the origin contains the current app name, origin is in the format:
     # app://clock.gaiamobile.org
     _apps_cards_locator = (By.CSS_SELECTOR, '#cards-view li[data-origin*="%s"]')
@@ -23,23 +23,14 @@ class CardsView(Base):
     def _close_button_locator(self, app):
         return (self._close_buttons_locator[0], self._close_buttons_locator[1] % app.lower())
 
-    def open_cards_view(self):
-        # Hold the home button to open cards view
-        self.marionette.execute_script("window.wrappedJSObject.dispatchEvent(new Event('holdhome'));")
-        time.sleep(1)
-        self.wait_for_cards_view()
-
-    def exit_cards_view(self):
-        # Touch the home button to exit cards view
-        self.marionette.execute_script("window.wrappedJSObject.dispatchEvent(new Event('home'));")
-        self.wait_for_cards_view_not_displayed()
-
     @property
     def is_cards_view_displayed(self):
         return self.is_element_displayed(*self._cards_view_locator)
 
     def is_app_displayed(self, app):
-        return self.marionette.find_element(*self._app_card_locator(app)).is_displayed()
+        card = self.marionette.find_element(*self._app_card_locator(app))
+        # card is displayed and not in transition
+        return card.is_displayed() and 'transition' not in card.get_attribute('style')
 
     def is_app_present(self, app):
         return self.is_element_present(*self._app_card_locator(app))
@@ -48,7 +39,9 @@ class CardsView(Base):
         return self.marionette.find_element(*self._app_card_locator(app)).tap()
 
     def close_app(self, app):
-        return self.marionette.find_element(*self._close_button_locator(app)).tap()
+        self.wait_for_condition(lambda m: self.is_app_displayed(app))
+        self.marionette.find_element(*self._close_button_locator(app)).tap()
+        self.wait_for_element_not_present(*self._app_card_locator(app))
 
     def wait_for_cards_view(self):
         self.wait_for_element_displayed(*self._cards_view_locator)
@@ -56,6 +49,12 @@ class CardsView(Base):
     def wait_for_cards_view_not_displayed(self):
         self.wait_for_element_not_displayed(*self._cards_view_locator)
 
-    @property
-    def no_recent_apps_message(self):
-        return self.marionette.find_element(*self._no_apps_locator).text
+    def swipe_to_next_app(self):
+        current_frame = self.apps.displayed_app.frame
+
+        start_x_position = current_frame.size['width']
+        start_y_position = current_frame.size['height'] // 2
+
+        # swipe backward to get next app card
+        Actions(self.marionette).flick(
+            current_frame, start_x_position, start_y_position, 0, start_y_position).perform()
