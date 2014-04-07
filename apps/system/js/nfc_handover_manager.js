05ad31eff9d5b8000c2745eb01921201f2756546
Arno Puder <arno@puder.org>
2014-04-07 16:12:32 -0700

    Bug 988254 - Proper initialization in Handover Manager if BT is already enabled at boot

--- apps/system/js/nfc_handover_manager.js
+++ apps/system/js/nfc_handover_manager.js
@@ -83,6 +83,17 @@ var NfcHandoverManager = {
 
   init: function init() {
     var self = this;
+
+    if (this.bluetooth.enabled) {
+      this.debug('Bluetooth already enabled on boot');
+      var req = this.bluetooth.getDefaultAdapter();
+      req.onsuccess = function bt_getAdapterSuccess() {
+        self.defaultAdapter = req.result;
+        self.debug('MAC address: ' + self.defaultAdapter.address);
+        self.debug('MAC name: ' + self.defaultAdapter.name);
+      };
+    }
+
     window.addEventListener('bluetooth-adapter-added', function() {
       self.debug('bluetooth-adapter-added');
       var req = self.bluetooth.getDefaultAdapter();
