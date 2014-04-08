e95af95c74ebd7cf3411eef0df360cee3854bda0
Aleh Zasypkin <azasypkin@mozilla.com>
2014-04-08 17:22:22 +0200

    Bug 946497 - [B2G][SMS][MMS] Thumbnail previews for cropped images in MMS message appear stretched causing not all of the image to be visible.

--- apps/sms/js/attachment.js
+++ apps/sms/js/attachment.js
@@ -103,7 +103,8 @@
         canvas.width = Math.round(img.width / ratio);
         canvas.height = Math.round(img.height / ratio);
         var context = canvas.getContext('2d', { willReadFrequently: true });
-        context.drawImage(img, 0, 0, width, height);
+        // Using canvas width and height with correct image proportions
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
         var data = canvas.toDataURL(type);
 
         callback({
