8aeae8fc65448c4da47169d61518d443571334be
mpizza <mmm198219@gmail.com>
2014-04-11 15:53:24 +0800

    Bug 995093 - Add a marionette test to check Keyboard app could show up correctly.

---
+++ apps/keyboard/test/marionette/launch_test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var KeyboardTests = require('./lib/keyboard_tests'),
+    assert = require('assert');
+
+var KEYBOARD_ORIGIN = 'app://keyboard.gaiamobile.org';
+
+marionette('Keyboard APP', function() {
+  var apps = {},
+      keyboardtests = null,
+      client = null;
+
+  apps[KeyboardTests.ORIGIN] = __dirname + '/keyboardtests';
+  
+  client = marionette.client({
+    apps: apps,
+    prefs: {
+      'focusmanager.testmode': true
+    }
+  });
+
+  setup(function() {
+    keyboardtests = new KeyboardTests(client);
+    keyboardtests.launch();
+    keyboardtests.textInput.click();
+  });
+
+  test('should show lowercase layout', function() {
+    // switch to System app
+    client.switchToFrame();
+    client.apps.switchToApp(KEYBOARD_ORIGIN);
+    // XXX: Workaround
+    // To get the #keyboard element to instead of the body element.
+    // The value of `client.findElement('body').displayed()` could not be true
+    // when the keyboard app is show up in the screen currently.
+    // Please refer to http://bugzil.la/995865.
+    var keyboard = client.findElement('#keyboard');
+
+    client.waitFor(function() {
+      if (keyboard.displayed()) {
+        assert.ok(true);
+        return true;
+      }
+    });
+  });
+});
