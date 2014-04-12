b1fba95fcbad1af7ba3aee466bf65429fe49fedf
David Flanagan <dflanagan@mozilla.com>
2014-04-12 00:46:51 -0700

    Merge pull request #18252 from justindarc/bug991396
    Bug 991396 - [camera][madai] delete dialogue button should be red and sa... r=djf

--- apps/camera/js/config/require.js
+++ apps/camera/js/config/require.js
@@ -11,6 +11,7 @@ require.config({
     'VideoPlayer': '../shared/js/media/video_player',
     'MediaFrame': '../shared/js/media/media_frame',
     'BlobView': '../shared/js/blobview',
+    'CustomDialog': '../shared/js/custom_dialog',
     'debug': 'vendor/debug'
   },
   shim: {
@@ -40,6 +41,9 @@ require.config({
     },
     'GestureDetector': {
       exports: 'GestureDetector'
+    },
+    'CustomDialog': {
+      exports: 'CustomDialog'
     }
   }
 });
