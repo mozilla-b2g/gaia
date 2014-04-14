daaf52d9a7165b09e3b777d8738d81abdded5e7e
pychen <mmm198219@gmail.com>
2014-04-14 18:31:14 +0800

    add 5secs for screenshot

--- apps/keyboard/test/marionette/launch_test.js
+++ apps/keyboard/test/marionette/launch_test.js
@@ -34,7 +34,8 @@ marionette('Keyboard APP', function() {
   test('should show lowercase layout', function() {
     // switch to System app
     client.switchToFrame();
-    console.log(client.screenshot());
+    client.helper.wait(5000);
+    console.log('data:image/png;base64,' + client.screenshot());
     client.apps.switchToApp(KEYBOARD_ORIGIN);
     // XXX: Workaround to get the #keyboard element to instead of the body element.
     // The value of `client.findElement('body').displayed()` could not be true
