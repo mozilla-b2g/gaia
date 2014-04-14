10f4ab85c71e7357e03c864b2d5fb501e5c7720e
Rudy Lu <me@rudy.lu>
2014-04-14 20:38:14 +0800

    add dump log for keyboard manager

--- apps/system/js/keyboard_manager.js
+++ apps/system/js/keyboard_manager.js
@@ -85,7 +85,7 @@ var KeyboardManager = {
 
   focusChangeTimeout: 0,
   switchChangeTimeout: 0,
-  _onDebug: false,
+  _onDebug: true,
   _debug: function km_debug(msg) {
     if (this._onDebug)
       console.log('[Keyboard Manager] ' + msg);
