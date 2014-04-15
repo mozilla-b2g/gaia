403f40d31361fb49b1c6dd00d6ad788795b80205
Rudy Lu <me@rudy.lu>
2014-04-15 11:55:00 +0800

    add continuous clicking

--- apps/keyboard/test/marionette/launch_test.js
+++ apps/keyboard/test/marionette/launch_test.js
@@ -20,6 +20,45 @@ marionette('Keyboard APP', function() {
     }
   });
 
+  function checkKeyboardAvailable() {
+    console.log('checkKeyboardAvailable');
+
+    // switch to System app
+    client.switchToFrame();
+    //console.log(client.screenshot());
+
+    // Wait for the keyboard pop up and switch to it
+    client.waitFor(function() {
+      var keyboards = client.findElement('#keyboards');
+
+      var classes = keyboards.getAttribute('class');
+      var transitionIn = keyboards.getAttribute('data-transition-in');
+
+      console.log('keyboards class; ' + classes, ' - transition-in: ' +
+                  transitionIn);
+
+      return ( classes.indexOf('hide') == -1 ) &&  transitionIn !== 'true';
+    });
+
+    //client.helper.wait(5000);
+    //console.log('data:image/png;base64,' + client.screenshot());
+    client.apps.switchToApp(KEYBOARD_ORIGIN);
+    // XXX: Workaround to get the #keyboard element to instead of the body
+    // element.
+    // The value of `client.findElement('body').displayed()` could not be true
+    // when the keyboard app is show up in the screen currently.
+    // Please refer to http://bugzil.la/995865.
+    var keyboard = client.findElement('#keyboard');
+    if (keyboard.displayed()) {
+      console.log('keyboard found');
+      return true;
+    } else {
+
+      console.log('keyboard  not found');
+      urlBar.click();
+    }
+  }
+
   setup(function() {
     // keyboardtests = new KeyboardTests(client);
     // keyboardtests.launch();
@@ -28,13 +67,19 @@ marionette('Keyboard APP', function() {
     client.apps.switchToApp(browser_ORIGIN);
     client.helper.waitForElement('#url-input');
     urlBar = client.findElement('#url-input');
+
     urlBar.click();
+    client.waitFor(function () {
+      return checkKeyboardAvailable(urlBar);
+    });
   });
 
   test('should show lowercase layout', function() {
+
+    /*
     // switch to System app
     client.switchToFrame();
-    console.log(client.screenshot());
+    //console.log(client.screenshot());
 
     // Wait for the keyboard pop up and switch to it
     client.waitFor(function() {
@@ -48,7 +93,7 @@ marionette('Keyboard APP', function() {
       return ( classes.indexOf('hide') == -1 ) &&  transitionIn !== 'true';
     });
 
-    client.helper.wait(5000);
+    //client.helper.wait(5000);
     console.log('data:image/png;base64,' + client.screenshot());
     client.apps.switchToApp(KEYBOARD_ORIGIN);
     // XXX: Workaround to get the #keyboard element to instead of the body
@@ -64,5 +109,11 @@ marionette('Keyboard APP', function() {
         return true;
       }
     });
+    */
+
+    var keyboard = client.findElement('#keyboard');
+    if (keyboard.displayed()) {
+      assert.ok(true);
+    }
   });
 });
