9ce210db58c0ef93e736cbd926f390fc7b0d7cd9
Zac <zcampbell@mozilla.com>
2014-04-17 17:22:52 +0100

    Bug 997673 - Small refactor of Python cost control app tests

--- tests/python/gaia-ui-tests/gaiatest/apps/cost_control/regions/ftu_step1.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/cost_control/regions/ftu_step1.py
@@ -10,12 +10,11 @@ from gaiatest.apps.cost_control.regions.ftu_step2 import FTUStep2
 
 class FTUStep1(Base):
 
-    _welcome_title_locator = (By.CSS_SELECTOR, 'h1[data-l10n-id="fte-welcome-title"]')
     _next_button_locator = (By.CSS_SELECTOR, '#step-1 span[data-l10n-id="next"]')
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
-        self.wait_for_element_displayed(*self._welcome_title_locator)
+        self.wait_for_element_displayed(*self._next_button_locator)
 
     def tap_next(self):
         self.wait_for_element_displayed(*self._next_button_locator)
