2de1daf33fde2a9c6991d18430058a6a8d3582a7
gaye <gaye@mozilla.com>
2014-04-21 17:20:25 -0400

    Bug 951069 - Month view visual refresh

--- apps/calendar/test/marionette/lib/views/month.js
+++ apps/calendar/test/marionette/lib/views/month.js
@@ -18,5 +18,31 @@ Month.prototype = {
 
   get days() {
     return this.findElements('.day');
+  },
+
+  get weekdayHeaders() {
+    return this
+      .findElements('.month.active #month-days li')
+      .map(function(li) {
+        return li.text();
+      });
+  },
+
+  get daySquares() {
+    return this.findElements('.month.active > ol li');
+  },
+
+  get todaySquare() {
+    var daySquares = this.daySquares;
+    // TODO(gareth): Array.prototype.find...
+    for (var i = 0; i < daySquares.length; i++) {
+      var square = daySquares[i];
+      var className = square.getAttribute('className');
+      if (className.indexOf('present') !== -1) {
+        return square;
+      }
+    }
+
+    return null;
   }
 };
