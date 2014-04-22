cd4ca52c254e2aba9c91f4c6cc80c6f7d171b21f
Russ Nicoletti <rnicoletti@mozilla.com>
2014-04-22 14:32:14 -0700

    Bug 979636 - Unit tests for video app -- showPlayer unit tests

---
+++ shared/test/unit/mocks/mock_video_stats.js
@@ -0,0 +1,21 @@
+/*exported VideoStats */
+
+'use strict';
+
+var VideoStats = (function() {
+
+  function start(v) {
+  }
+
+  function stop() {
+  }
+
+  function print() {
+  }
+  
+  return {
+    start: start,
+    stop: stop,
+    dump: print
+  };
+})();
