b919a0ace7b5bea69197eb9c81100ae7528e0edb
Jared Hirsch <ohai@6a68.net>
2014-04-23 19:44:08 -0700

    Bug 988080 - Update copy for FxA FTE post-signup screen r=arcturus

--- apps/communications/ftu/js/ui.js
+++ apps/communications/ftu/js/ui.js
@@ -380,7 +380,7 @@ var UIManager = {
         fxaDescription.innerHTML = '';
         navigator.mozL10n.localize(
           fxaDescription,
-          'fxa-logged',
+          'fxa-email-sent',
           {
             email: response.email
           }
