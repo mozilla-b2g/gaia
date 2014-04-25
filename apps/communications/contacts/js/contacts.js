c5329c04efefb174ab905626e0bb5d63ccf34499
Jose M. Cantera <jmcanterafonseca@gmail.com>
2014-04-25 10:46:48 +0200

    Bug 1003725 - [FTU-separation] Move import.html and related resources to shared/pages/import

--- apps/communications/contacts/js/contacts.js
+++ apps/communications/contacts/js/contacts.js
@@ -26,8 +26,12 @@ var SCALE_RATIO = window.innerWidth / 320;
 var Contacts = (function() {
   var SHARED = 'shared';
   var SHARED_PATH = '/' + SHARED + '/' + 'js';
+
   var SHARED_UTILS = 'sharedUtilities';
   var SHARED_UTILS_PATH = SHARED_PATH + '/contacts/import/utilities';
+
+  var SHARED_CONTACTS = 'sharedContacts';
+  var SHARED_CONTACTS_PATH = SHARED_PATH + '/' + 'contacts';
 
   var navigation = new navigationStack('view-contacts-list');
 
@@ -671,7 +675,7 @@ var Contacts = (function() {
       contacts.List.initSearch(function onInit() {
         contacts.Search.enterSearchMode(evt);
       });
-    });
+    }, SHARED_CONTACTS);
   };
 
   var initEventListeners = function initEventListener() {
@@ -904,6 +908,9 @@ var Contacts = (function() {
         case SHARED_UTILS:
           finalPath = SHARED_UTILS_PATH;
           break;
+        case SHARED_CONTACTS:
+          finalPath = SHARED_CONTACTS_PATH;
+          break;
         default:
           finalPath = 'js' + '/' + type;
       }
@@ -936,8 +943,8 @@ var Contacts = (function() {
    * @param {String} view name.
    * @param {Function} callback.
    */
-  function loadView(view, callback) {
-    load('views', view, callback);
+  function loadView(view, callback, type) {
+    load('views', view, callback, type);
   }
 
   /**
@@ -986,6 +993,9 @@ var Contacts = (function() {
     },
     get SHARED_UTILITIES() {
       return SHARED_UTILS;
+    },
+    get SHARED_CONTACTS() {
+      return SHARED_CONTACTS;
     }
   };
 })();
