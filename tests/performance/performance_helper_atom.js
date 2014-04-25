c7a5da8a394e93a6d8d9821aff06b48dd918c3da
Hubert Figuière <hub@figuiere.net>
2014-04-24 22:09:50 -0400

    Attempt... fail

--- tests/performance/performance_helper_atom.js
+++ tests/performance/performance_helper_atom.js
@@ -20,6 +20,7 @@ function _registerListener(document) {
       }
     }
 
+    dump('OMFG setting up');
     // FIXME: we have to find why this gets imported several times for one test
     if ('PerformanceHelperAtom' in window) {
       debug('FIXME We got called twice');
@@ -55,69 +56,79 @@ function _registerListener(document) {
       }
     }
 
-    window.__defineGetter__('PerformanceHelperAtom', function() {
-      return {
-        getMeasurements: function() {
-          return perfMeasurements;
-        },
+    var atom = {
+      getMeasurements: function() {
+        dump('OMFG getMeasurements');
+        return perfMeasurements;
+      },
 
-        init: function() {
-          if (hasRegistered) {
-            return;
-          }
-
-          debug('registering');
-          perfMeasurements = Object.create(null);
-          perfMeasurements.start = window.performance.now();
-
-          window.addEventListener(PERF_EVENT_NAME, handlePerfEvent);
-          window[PERF_FLAG_NAME] = hasRegistered = true;
-
-          debug('end registering');
-        },
-
-        unregister: function() {
-          // note: we're not reseting mozPerfHasListener to false here because at
-          // this point we basically don't mind if the app keeps sending perf
-          // events.
-          window.removeEventListener(PERF_EVENT_NAME, handlePerfEvent);
-          hasRegistered = false;
-
-          marionetteScriptFinished();
-        },
-
-        waitForEvent: function(name, cb) {
-          checkFinish(name, cb);
+      init: function() {
+        if (hasRegistered) {
+          return;
         }
-      };
+        dump('OMFG init');
+        debug('registering');
+        perfMeasurements = Object.create(null);
+        perfMeasurements.start = window.performance.now();
+
+        window.addEventListener(PERF_EVENT_NAME, handlePerfEvent);
+        window[PERF_FLAG_NAME] = hasRegistered = true;
+
+        debug('end registering');
+      },
+
+      unregister: function() {
+        dump('OMFG unregister');
+        // note: we're not reseting mozPerfHasListener to false here because at
+        // this point we basically don't mind if the app keeps sending perf
+        // events.
+        window.removeEventListener(PERF_EVENT_NAME, handlePerfEvent);
+        hasRegistered = false;
+
+        marionetteScriptFinished();
+      },
+
+      waitForEvent: function(name, cb) {
+        dump('OMFG waitForEvent');
+        checkFinish(name, cb);
+      }
+    };
+
+
+    window.__defineGetter__('PerformanceHelperAtom', function() {
+      try {
+        dump('OMFG returning atom ' + JSON.stringify(Object.keys(atom)));
+        for (var i in atom) {
+          dump('OMFG its ' + i + ' member is ' + JSON.stringify(atom[i]));
+        }
+      } catch (e) {
+        dump('OMFG ERR4: ' + e.toString() + '\n');
+      }
+      return atom;
     });
 
     window.PerformanceHelperAtom.init();
   } catch (e) {
-    dump('ERR: ' + e.toString() + '\n');
+    dump('OMFG ERR2: ' + e.toString() + '\n');
   }
 }
 
+dump('OMFG let\'s start');
 
 try {
-  if (document) {
-
-    _registerListener(document);
-
-  } else {
-
+    dump('OMFG setting up 1');
     Services.obs.addObserver(function(document) {
       if (!document || !document.location)
         return;
 
+      dump('OMFG setting up 2');
       var location = document.location.href;
-      dump('\n\n' + location + '\n\n');
+      dump('OMFG "' + location + '"');
 
       _registerListener(document);
     }, 'document-element-inserted', false);
 
-  }
 } catch (e) {
-  dump('ERR: ' + e.toString() + '\n');
+  dump('OMFG ERR1: ' + e.toString() + '\n');
 }
 
