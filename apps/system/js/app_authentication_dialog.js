9a53a9d2056df72063f2d5378202b1f76fc14cf2
Kevin Grandon <kevingrandon@yahoo.com>
2014-04-27 14:36:59 -0700

    Bug 1002247 - [System2] AppAuthenticationDialog fixes r=alive

--- apps/system/js/app_authentication_dialog.js
+++ apps/system/js/app_authentication_dialog.js
@@ -1,6 +1,7 @@
 'use strict';
+/* global System */
 
-(function(window) {
+(function(exports) {
   var _id = 0;
 
   /**
@@ -11,7 +12,7 @@
    *                        where this dialog should popup.
    * @extends BaseUI
    */
-  window.AppAuthenticationDialog = function AppAuthenticationDialog(app) {
+  function AppAuthenticationDialog(app) {
     this.app = app;
     this._event = null;
     this.containerElement = app.element;
@@ -19,7 +20,7 @@
     this._injected = false;
     this.app.element.addEventListener('mozbrowserusernameandpasswordrequired',
       this);
-  };
+  }
 
   AppAuthenticationDialog.prototype.__proto__ = window.BaseUI.prototype;
 
@@ -27,18 +28,33 @@
 
   AppAuthenticationDialog.prototype.EVENT_PREFIX = 'authdialog';
 
-  // Used for element id access.
-  // e.g., 'authentication-dialog-alert-ok'
+  /**
+   * Used for element id access.
+   * e.g., 'authentication-dialog-alert-ok'
+   * @type {String}
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.ELEMENT_PREFIX = 'authentication-dialog-';
 
-  // DOM
+  /**
+   * Maps to DOM elements.
+   * @type {Object}
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.elements = null;
 
-  // Store the authentication event.
-  // Note: Only one event one time.
+  /**
+   * The current authentication event.
+   * Note: Only one event one time.
+   * @type {Event}
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype._event = null;
 
-  // Get all elements when inited.
+  /**
+   * Get all elements when inited.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype._fetchElements =
     function aad__fetchElements() {
     this.element = document.getElementById(this.CLASS_NAME + this.instanceID);
@@ -62,6 +78,10 @@
     }, this);
   };
 
+  /**
+   * Generates markup for the dialog.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.view = function aad_view() {
     return '<div class="authentication-dialog" ' +
             'id="' + this.CLASS_NAME + this.instanceID + '">' +
@@ -95,6 +115,10 @@
         '</div>';
   };
 
+  /**
+   * General event handler interface.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.handleEvent = function(evt) {
     System.debug(' AAD>> got event: ' + evt.type);
     evt.preventDefault();
@@ -106,14 +130,22 @@
     this._injected = true;
   };
 
+  /**
+   * Registers click events.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype._registerEvents =
     function aad__registerEvents() {
-      this.elements['httpAuthenticationOk'].
+      this.elements.httpAuthenticationOk.
         addEventListener('click', this.confirmHandler.bind(this));
-      this.elements['httpAuthenticationCancel'].
+      this.elements.httpAuthenticationCancel.
         addEventListener('click', this.cancelHandler.bind(this));
     };
 
+  /**
+   * Unregisters authentication events from the app element.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype._unregisterEvents =
     function aad__unregisterEvents() {
       this.app.element.removeEventListener(
@@ -121,6 +153,10 @@
         this);
     };
 
+  /**
+   * Shows the authentication dialog.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.show = function aad_show() {
     var evt = this._event;
     var elements = this.elements;
@@ -133,6 +169,10 @@
     elements.httpPasswordInput.value = '';
   };
 
+  /**
+   * Hides the authentication dialog.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.hide = function aad_hide() {
     this.elements.httpUsernameInput.blur();
     this.elements.httpPasswordInput.blur();
@@ -141,6 +181,10 @@
     System.debug(' AAD>> hided');
   };
 
+  /**
+   * Called when the user confirms the authentication dialog.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.confirmHandler =
     function aad_confirmHandler() {
       var elements = this.elements;
@@ -151,12 +195,18 @@
       this._event = null;
     };
 
+  /**
+   * Called when the user cancels the authentication dialog.
+   * @memberof AppAuthenticationDialog.prototype
+   */
   AppAuthenticationDialog.prototype.cancelHandler =
     function aad_cancelHandler() {
       var evt = this._event;
-      var elements = this.elements;
       this.hide();
       evt.detail.cancel();
       this._event = null;
     };
-}(this));
+
+  exports.AppAuthenticationDialog = AppAuthenticationDialog;
+
+}(window));
