cc1f1a807d0e3ce0437310a28e71501d720b4b3d
Zac <zcampbell@mozilla.com>
2014-04-28 11:04:32 +0100

    Revert "Bug 943897 - Code timeout into contacts_settings.tap_import_from_sim()"
    This reverts commit d17ff0ce155fabec56117be7f0f865feea929703.

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/contacts/test_import_contacts_from_sim.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/contacts/test_import_contacts_from_sim.py
@@ -15,18 +15,16 @@ class TestImportContactsFromSIM(GaiaTestCase):
 
         """
 
-        number_of_sim_contacts = len(self.data_layer.sim_contacts)
-
-        self.assertGreater(number_of_sim_contacts, 0, "There is no SIM contacts on SIM card.")
+        self.assertGreater(len(self.data_layer.sim_contacts), 0, "There is no SIM contacts on SIM card.")
         contacts_app = Contacts(self.marionette)
         contacts_app.launch()
 
         # import contacts from SIM
         contacts_settings = contacts_app.tap_settings()
         contacts_settings.tap_import_contacts()
-        contacts_settings.tap_import_from_sim(number_of_sim_contacts)
+        contacts_settings.tap_import_from_sim()
         contacts_settings.tap_back_from_import_contacts()
         contacts_settings.tap_done()
 
         # all the contacts in the SIM should be imported
-        self.assertEqual(number_of_sim_contacts, len(self.data_layer.sim_contacts))
+        self.assertEqual(len(contacts_app.contacts), len(self.data_layer.sim_contacts))
