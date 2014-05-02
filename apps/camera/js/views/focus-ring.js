5d0bf0683cdeace8e8fbbb016528caba4227bdbd
ashish.singh <ashish.singh@lge.com>
2014-05-02 16:46:31 +0530

    Focus Configuration changes

--- apps/camera/js/views/focus-ring.js
+++ apps/camera/js/views/focus-ring.js
@@ -6,7 +6,6 @@ define(function(require, exports, module) {
  */
 
 var View = require('vendor/view');
-var find = require('lib/find');
 
 /**
  * Exports
@@ -21,7 +20,7 @@ module.exports = View.extend({
   },
   render: function() {
     this.el.innerHTML = this.template();
-    this.els.focus = find('.js-focus', this.el);
+    this.els.ring = this.find('.js-ring');
   },
 
   setState: function(state) {
@@ -35,7 +34,7 @@ module.exports = View.extend({
 
   changePosition: function(x, y) {
     this.setDefaultValues();
-    this.els.focus.style.transform = 'translate('+x+'px, '+y+'px)';
+    this.els.ring.style.transform = 'translate('+x+'px, '+y+'px)';
   },
 
   /* 
@@ -45,11 +44,11 @@ module.exports = View.extend({
    * when it will come back we need to set it again to the default position
    */
   setDefaultValues: function() {
-    this.els.focus.style.transform = 'translate(-50%, -50%)';
+    this.els.ring.style.transform = 'translate(-50%, -50%)';
   },
 
   template: function() {
-    return '<div class="focusring icon-focus-locking js-focus"></div>';
+    return '<div class="focusring icon-focus-locking js-ring"></div>';
   },
 
 });
