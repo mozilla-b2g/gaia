4382e22654ca39987f5ec1c62b0e1a2f6fae5c2f
Arthur Chen <crh0716@gmail.com>
2014-05-05 23:23:54 +0800

    Bug 968694 - Add marionette tests for the root panel

---
+++ apps/settings/test/marionette/mocks/mock_navigator_moz_mobile_connections_multi.js
@@ -0,0 +1,21 @@
+/*global Components, Services */
+'use strict';
+
+Components.utils.import('resource://gre/modules/Services.jsm');
+
+Services.obs.addObserver(function(document) {
+  if (!document || !document.location) {
+    return;
+  }
+
+  var window = document.defaultView.wrappedJSObject;
+  var _mobileConnections = [{}, {}];
+
+  Object.defineProperty(window.navigator, 'mozMobileConnections', {
+    configurable: false,
+    get: function() {
+      return _mobileConnections;
+    }
+  });
+
+}, 'document-element-inserted', false);
