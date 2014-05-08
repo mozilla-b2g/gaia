a217b8b70faff9d6975898da80fc5f05763a922b
Evan Xd <itoyxd@gmail.com>
2014-05-08 18:09:28 +0800

    Bug 963394 - Visual refresh for event detail page

--- apps/calendar/test/marionette/create_event_test.js
+++ apps/calendar/test/marionette/create_event_test.js
@@ -1,10 +1,9 @@
 'use strict';
 
 var Calendar = require('./lib/calendar'),
-    assert = require('chai').assert,
-    format = require('util').format;
+    assert = require('chai').assert;
 
-marionette('creating an event', function() {
+marionette('creating events', function() {
   var app;
   var client = marionette.client({
     prefs: {
@@ -17,67 +16,177 @@ marionette('creating an event', function() {
     }
   });
 
-  var title = 'Puppy Bowl dogefortlongtextfotestloremipsumdolorsitamet',
-      description = 'lorem ipsum dolor sit amet maecennas ullamcor',
-      location = 'Animal Planet ' +
-                 'reallylongwordthatshouldnotoverflowbecausewewrap',
-      date = new Date(),
-      startHour = 2,
-      duration = 1;
+  var startDate = new Date('Sep 08 1991 12:34:56'),
+      startDateNextHour = new Date(startDate),
+      startDateNextDay = new Date(startDate),
+      scenarios;
 
-  setup(function() {
-    app = new Calendar(client);
-    app.launch({ hideSwipeHint: true });
-    app.createEvent({
-      title: title,
-      description: description,
-      location: location,
-      startHour: startHour,
-      duration: duration
-    });
+  startDateNextHour.setHours(startDate.getHours() + 1);
+  startDateNextDay.setDate(startDate.getDate() + 1);
 
-    app.month.waitForDisplay();
-  });
+  scenarios = [
+    {
+      name: 'one day',
+      allDay: false,
+      startDate: startDate,
+      endDate: startDateNextHour,
+      reminders: ['5 minutes before'],
+      verifyMonthView: function() {
+        var event = app.monthDay.events[0];
+        assert.equal(event.startTime, '12:34 PM', 'start time');
+        assert.equal(event.endTime, '1:34 PM', 'end time');
+      },
+      verifyReadEventView: function() {
+        var readEvent = app.readEvent;
+        assert.equal(
+          readEvent.durationTime,
+          'Sunday, September 08, 1991\n' +
+          'from 12:34 PM to 1:34 PM',
+          'duration time'
+        );
+        assert.equal(
+          readEvent.reminders[0].text(),
+          '5 minutes before',
+          'reminder'
+        );
+      }
+    },
+    {
+      name: 'multiple day',
+      allDay: false,
+      startDate: startDate,
+      endDate: startDateNextDay,
+      reminders: ['5 minutes before'],
+      verifyMonthView: function() {
+        var event = app.monthDay.events[0];
+        assert.equal(event.startTime, '12:34 PM', 'start time');
+        assert.equal(event.endTime, '12:34 PM', 'end time');
+      },
+      verifyReadEventView: function() {
+        var readEvent = app.readEvent;
+        assert.equal(
+          readEvent.durationTime,
+          'From 12:34 PM Sunday, September 08, 1991\n' +
+          'to 12:34 PM Monday, September 09, 1991',
+          'duration time'
+        );
+        assert.equal(
+          readEvent.reminders[0].text(),
+          '5 minutes before',
+          'reminder'
+        );
+      }
+    },
+    {
+      name: 'one all day',
+      allDay: true,
+      startDate: startDate,
+      endDate: startDateNextHour,
+      reminders: ['On day of event'],
+      verifyMonthView: function() {
+        var event = app.monthDay.events[0];
+        assert.equal(event.allDay, 'All Day', 'event hour');
+      },
+      verifyReadEventView: function() {
+        var readEvent = app.readEvent;
+        assert.equal(
+          readEvent.durationTime,
+          'All day\n' +
+          'Sunday, September 08, 1991',
+          'duration time'
+        );
+        assert.equal(
+          readEvent.reminders[0].text(),
+          'On day of event',
+          'reminder'
+        );
+      }
+    },
+    {
+      name: 'multiple all day',
+      allDay: true,
+      startDate: startDate,
+      endDate: startDateNextDay,
+      reminders: ['On day of event'],
+      verifyMonthView: function() {
+        var event = app.monthDay.events[0];
+        assert.equal(event.allDay, 'All Day', 'event hour');
+      },
+      verifyReadEventView: function() {
+        var readEvent = app.readEvent;
+        assert.equal(
+          readEvent.durationTime,
+          'All day from Sunday, September 08, 1991\n' +
+          'to Monday, September 09, 1991',
+          'duration time'
+        );
+        assert.equal(
+          readEvent.reminders[0].text(),
+          'On day of event',
+          'reminder'
+        );
+      }
+    }
+  ];
 
-  test('should display the created event in months day', function() {
-    var monthDay = app.monthDay;
-    var event = monthDay.events[0];
-    assert.equal(event.title, title);
-    assert.equal(event.address, location);
-    assert.equal(event.startTime, '2:00 AM');
-    assert.equal(event.endTime, '3:00 AM');
-  });
+  scenarios.forEach(function(scenario) {
+    suite('creating ' + scenario.name + ' event', function() {
+      setup(function() {
+        // we use default title/description/location to avoid duplication but
+        // allow overriding if needed
+        scenario.title = scenario.title ||
+          'Puppy Bowl dogefortlongtextfotestloremipsumdolorsitamet';
+        scenario.description = scenario.description ||
+          'lorem ipsum dolor sit amet maecennas ullamcor';
+        scenario.location = scenario.location ||
+          'Animal Planet reallylongwordthatshouldnotoverflowbecausewewrap';
 
-  suite('opening event in read view', function() {
-    setup(function() {
-      var event = app.monthDay.events[0];
+        app = new Calendar(client);
+        app.launch({ hideSwipeHint: true });
+        app.createEvent(scenario);
 
-      // Scroll so that the first one is in view and click it.
-      app.monthDay.scrollToEvent(event);
-      event.click();
+        app.month.waitForDisplay();
+      });
 
-      // Wait until the read view is displayed.
-      app.readEvent.waitForDisplay();
-    });
+      test('should display the created event in months day', function() {
+        var event = app.monthDay.events[0];
+        assert.equal(event.title, scenario.title, 'event title');
+        assert.equal(event.address, scenario.location, 'event location');
+        scenario.verifyMonthView();
+      });
 
-    test('should display the created event in read view', function() {
-      var readEvent = app.readEvent;
-      assert.equal(readEvent.title, title);
-      assert.equal(readEvent.description, description);
-      assert.equal(readEvent.location, location);
-      assert.equal(readEvent.calendar, 'Offline calendar');
-      assert.equal(readEvent.startDate, app.formatDate(date));
-      assert.equal(readEvent.startTime, format('%d:00 AM', startHour));
-      assert.equal(readEvent.endDate, app.formatDate(date));
-      assert.equal(readEvent.endTime, format('%d:00 AM', startHour + duration));
-    });
+      suite('opening event in read view', function() {
+        setup(function() {
+          var event = app.monthDay.events[0];
+          // Scroll so that the first one is in view and click it.
+          app.monthDay.scrollToEvent(event);
+          event.click();
+          // Wait until the read view is displayed.
+          app.readEvent.waitForDisplay();
+        });
 
-    test('should not overflow title, location, or description',
-      function() {
-      var readEvent = app.readEvent;
-      app.checkOverflow(readEvent.titleContainer, 'title');
-      app.checkOverflow(readEvent.descriptionContainer, 'description');
-      app.checkOverflow(readEvent.locationContainer, 'location');
+        test('should display the created event in read view', function() {
+          var readEvent = app.readEvent;
+          assert.equal(readEvent.title, scenario.title, 'event title');
+          assert.equal(
+            readEvent.description,
+            scenario.description,
+            'event description'
+          );
+          assert.equal(readEvent.location, scenario.location, 'location');
+          assert.equal(readEvent.calendar, 'Offline calendar');
+          assert.equal(readEvent.calendarColor, 'rgb(249, 124, 23)');
+          scenario.verifyReadEventView();
+        });
+
+        test('should not overflow title, location, or description',
+          function() {
+          var readEvent = app.readEvent;
+          app.checkOverflow(readEvent.titleContainer, 'title');
+          app.checkOverflow(readEvent.descriptionContainer, 'description');
+          app.checkOverflow(readEvent.locationContainer, 'location');
+        });
+      });
     });
   });
 });
