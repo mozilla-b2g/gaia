3bc774295ad3b1c2d57ebc048d77781c1efb6a5a
Dominic Kuo <dkuo@mozilla.com>
2014-05-12 14:56:16 +0800

    Also swap the cover innerHTML of the PlayerView

--- apps/music/js/visibility.js
+++ apps/music/js/visibility.js
@@ -3,6 +3,10 @@
 'use strict';
 
 var Visibility = {
+  get _isPlayerLoaded() {
+    return (typeof PlayerView !== 'undefined');
+  },
+
   init: function() {
     this._views = [TilesView, ListView, SubListView];
     this._views.forEach(function(view) {
@@ -21,11 +25,20 @@ var Visibility = {
   },
 
   _swapInnerHTML: function() {
+    if (this._isPlayerLoaded) {
+      if (this._views.indexOf(PlayerView) === -1) {
+        PlayerView._innerHTML = '';
+        this._views.push(PlayerView);
+      }
+    }
+
     this._views.forEach(function(view) {
-      var innerHTML = view._innerHTML;
-      view._innerHTML = view.anchor.innerHTML;
-      view.anchor.innerHTML = innerHTML;
-    });
+      var anchor = (this._isPlayerLoaded && view === PlayerView) ?
+        view.cover : view.anchor;
+      var tempHTML = view._innerHTML;
+      view._innerHTML = anchor.innerHTML;
+      anchor.innerHTML = tempHTML;
+    }.bind(this));
   }
 };
 
