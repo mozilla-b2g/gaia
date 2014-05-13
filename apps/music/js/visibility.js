8c866c927d9dcf8871476f556a1c5bf234ff0f5f
Dominic Kuo <dkuo@mozilla.com>
2014-05-13 12:37:26 +0800

    Also swap the innerHTML in SearchView

--- apps/music/js/visibility.js
+++ apps/music/js/visibility.js
@@ -1,4 +1,4 @@
-/* global TilesView, ListView, SubListView */
+/* global TilesView, ListView, SubListView, SearchView */
 
 'use strict';
 
@@ -8,7 +8,8 @@ var Visibility = {
   },
 
   init: function() {
-    this._views = [TilesView, ListView, SubListView];
+    // Push the PlayerView to views later because it's lazy loaded.
+    this._views = [TilesView, ListView, SubListView, SearchView];
     this._views.forEach(function(view) {
       view._innerHTML = '';
     });
@@ -19,22 +20,29 @@ var Visibility = {
   },
 
   _visibilityHandler: function(event) {
+    // Does hiding the body helps to trigger the memory gc in gecko?
     document.body.hidden = document.hidden;
-
+    // Swap the anchors' innerHTML to temp strings when it's invisible, then
+    // switch back when they are visible again.
     this._swapInnerHTML();
   },
 
   _swapInnerHTML: function() {
-    if (this._isPlayerLoaded) {
-      if (this._views.indexOf(PlayerView) === -1) {
-        PlayerView._innerHTML = '';
-        this._views.push(PlayerView);
-      }
+    if (this._isPlayerLoaded && this._views.indexOf(PlayerView) === -1) {
+      PlayerView._innerHTML = '';
+      this._views.push(PlayerView);
     }
 
     this._views.forEach(function(view) {
-      var anchor = (this._isPlayerLoaded && view === PlayerView) ?
-        view.cover : view.anchor;
+      // Check the views and point the anchor to the right parent.
+      if (this._isPlayerLoaded && view === PlayerView) {
+        anchor = view.cover;
+      } else if (view === SearchView) {
+        anchor = view.view;
+      } else {
+        anchor = view.anchor;
+      }
+
       var tempHTML = view._innerHTML;
       view._innerHTML = anchor.innerHTML;
       anchor.innerHTML = tempHTML;
