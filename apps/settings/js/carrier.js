4048e674c6053dc51f6ef4c91bdc558e0884d903
Arthur Chen <crh0716@gmail.com>
2014-05-15 15:48:01 +0800

    Bug 1010394 - Refresh the items when entering the carrier panel

--- apps/settings/js/carrier.js
+++ apps/settings/js/carrier.js
@@ -132,6 +132,8 @@ var CarrierSettings = (function(window, document, undefined) {
 
         var currentHash = e.detail.current;
         if (currentHash === '#carrier') {
+          cs_updateNetworkTypeLimitedItemsVisibility(
+            _mobileConnection.voice && _mobileConnection.voice.type);
           // Show carrier name.
           cs_showCarrierName();
           cs_disabeEnableDataCallCheckbox();
