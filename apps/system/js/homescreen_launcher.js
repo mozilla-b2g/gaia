87421aa3321bb33d5f0a1a70fd8f9223c2c8f435
Etienne Segonzac <etienne@segonzac.info>
2014-05-19 19:27:31 +0200

    Bug 1012544 - Always resize the homescreen if loaded when a software-button-change occurs.

--- apps/system/js/homescreen_launcher.js
+++ apps/system/js/homescreen_launcher.js
@@ -133,6 +133,8 @@
       window.addEventListener('shrinking-stop', this);
       window.addEventListener('homescreenopening', this);
       window.addEventListener('homescreenclosing', this);
+      window.addEventListener('software-button-enabled', this);
+      window.addEventListener('software-button-disabled', this);
       return this;
     },
 
@@ -161,6 +163,8 @@
       window.removeEventListener('shrinking-stop', this);
       window.removeEventListener('homescreenopening', this);
       window.removeEventListener('homescreenclosing', this);
+      window.removeEventListener('software-button-enabled', this);
+      window.removeEventListener('software-button-disabled', this);
       this._started = false;
     },
 
@@ -220,6 +224,10 @@
           break;
         case 'homescreenclosing':
           this._screen.classList.remove('on-homescreen');
+          break;
+        case 'software-button-enabled':
+        case 'software-button-disabled':
+          this.getHomescreen().resize();
           break;
       }
     },
