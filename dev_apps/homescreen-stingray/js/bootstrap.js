425a73a716c8ab2fb165ef1b87cb7b9e9f5ce6ac
Luke Chang <lchang@mozilla.com>
2014-05-21 13:32:09 +0800

    Bug 1013036 - [Stingray] Static elements on homescreen

--- dev_apps/homescreen-stingray/js/bootstrap.js
+++ dev_apps/homescreen-stingray/js/bootstrap.js
@@ -2,5 +2,32 @@
 'use strict';
 
 window.addEventListener('load', function() {
-  new Homescreen().init();
+  function loadDemoVideo(filename, callback) {
+    var storage = navigator.getDeviceStorage('videos');
+    var request = storage.get('/sdcard/' + filename);
+
+    request.onsuccess = function() {
+      callback(URL.createObjectURL(request.result));
+    };
+
+    request.onerror = function() {
+      callback();
+    };
+  }
+
+  function startHomescreen(demoVideoURL) {
+    new Homescreen({ demoVideo: demoVideoURL }).start();
+  }
+
+  var fileList = ['demo.mp4', 'demo.webm'];
+
+  loadDemoVideo(fileList.shift(), function handleLoaded(url) {
+    if (url) {
+      startHomescreen(url);
+    } else if (fileList.length) {
+      loadDemoVideo(fileList.shift(), handleLoaded);
+    } else {
+      startHomescreen();
+    }
+  });
 });
