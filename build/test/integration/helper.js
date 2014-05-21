17eadc4cd939e2c8c3a569d503acba598ed1d67c
Martin Thomson <martin.thomson@gmail.com>
2014-05-21 05:01:53 -0700

    Bug 1002545 - Switching to b2g desktop download

--- build/test/integration/helper.js
+++ build/test/integration/helper.js
@@ -102,6 +102,18 @@ function exec(command, callback) {
   exec(command, options, callback);
 }
 
+function emptyJsonFile(filePath) {
+  var content = fs.readFileSync(filePath);
+  fs.unlinkSync(filePath);
+  fs.writeFileSync(filePath, '{}');
+
+  var restoreFunc = function() {
+    fs.writeFileSync(filePath, content);
+  };
+
+  return restoreFunc;
+}
+
 exports.getPrefsSandbox = getPrefsSandbox;
 exports.checkError = checkError;
 exports.checkSettings = checkSettings;
@@ -110,4 +122,5 @@ exports.checkWebappsScheme = checkWebappsScheme;
 exports.checkFileInZip = checkFileInZip;
 exports.checkFileContentInZip = checkFileContentInZip;
 exports.checkFileContentByPathInZip = checkFileContentByPathInZip;
+exports.emptyJsonFile = emptyJsonFile;
 exports.exec = exec;
