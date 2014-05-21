2c7455a92d106efffc6d8fa463a54d73559d60af
Amir Nissim <amirnissim@gmail.com>
2014-05-21 13:41:53 +0300

    Bug 1013842 - Create gaia-menu Web Component [r=kgrandon]

---
+++ shared/elements/gaia_menu/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+window.GaiaMenu = (function(win) {
+  // Extend from HTMLElement prototype
+  var proto = Object.create(HTMLElement.prototype);
+
+  // Allow baseurl to be overridden (used for demo page)
+  var baseurl = window.GaiaMenuBaseurl ||
+    'shared/elements/gaia_menu/';
+
+  proto.createdCallback = function () {
+    var shadow = this.createShadowRoot();
+    this._template = template.content.cloneNode(true);
+    this._styleHack();
+
+    var cancelButton = this._template.querySelector('.gaia-menu-cancel');
+
+    cancelButton.addEventListener('click', function () {
+      this.dispatchEvent(new CustomEvent('gaiamenu-cancel'));
+    }.bind(this));
+
+    shadow.appendChild(this._template);
+  };
+
+  proto._styleHack = function() {
+    var style = this._template.querySelector('style');
+    this.appendChild(style.cloneNode(true));
+  };
+
+  var stylesheet = baseurl + 'style.css';
+  var template = document.createElement('template');
+
+  template.innerHTML =
+    '<style scoped>' +
+      '@import url(' + stylesheet + ');' +
+    '</style>' +
+    '<form role="dialog" data-type="action">' +
+      '<content select="header"></content>' +
+      '<menu>' +
+        '<content select="button"></content>' +
+        '<button data-l10n="cancel" class="gaia-menu-cancel">Cancel</button>' +
+      '</menu>' +
+    '</form>';
+
+  // Register and return the constructor
+  return document.registerElement('gaia-menu', { prototype: proto });
+
+})(window);
