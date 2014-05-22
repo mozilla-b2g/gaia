a890511a4e702ef5c52b87955b7c17001cc1db1e
Marcus Cavanaugh <m@mcav.com>
2014-05-21 17:26:25 -0700

    Bug 921050 - Send email in the background.

--- apps/email/js/mail_common.js
+++ apps/email/js/mail_common.js
@@ -6,10 +6,10 @@
 'use strict';
 define(function(require, exports) {
 
-var Cards, Toaster,
+var Cards,
+    Toaster = require('./toaster'),
     evt = require('evt'),
     mozL10n = require('l10n!'),
-    toasterNode = require('tmpl!./cards/toaster.html'),
     confirmDialogTemplateNode = require('tmpl!./cards/confirm_dialog.html'),
     ValueSelector = require('value_selector');
 
@@ -209,7 +209,7 @@ Cards = {
     this._containerNode = document.getElementById('cardContainer');
     this._cardsNode = document.getElementById('cards');
 
-    this._containerNode.appendChild(toasterNode);
+    Toaster.init(this._containerNode);
 
     this._containerNode.addEventListener('click',
                                          this._onMaybeIntercept.bind(this),
@@ -241,7 +241,7 @@ Cards = {
 
     // Find the card containing the event target.
     var cardNode = event.target;
-    for (cardNode = event.target; cardNode; cardNode = cardNode.parentNode) {
+    for (cardNode = event.target; cardNode; cardNode = cardNode.parentElement) {
       if (cardNode.classList.contains('card'))
         break;
     }
@@ -909,13 +909,6 @@ Cards = {
         addClass(endNode, 'anim-fade');
       }
 
-      // Popup toaster that pended for previous card view.
-      var pendingToaster = Toaster.pendingStack.slice(-1)[0];
-      if (pendingToaster) {
-        pendingToaster();
-        Toaster.pendingStack.pop();
-      }
-
       // If any action to do at the end of transition trigger now.
       if (this._afterTransitionAction) {
         var afterTransitionAction = this._afterTransitionAction;
@@ -1014,166 +1007,6 @@ Cards = {
     if (this._cardStack.length)
       throw new Error('There are ' + this._cardStack.length + ' cards but' +
                       ' there should be ZERO');
-  }
-};
-
-/**
- * Central tracker of poptart messages; specifically, ongoing message sends,
- * failed sends, and recently performed undoable mutations.
- */
-Toaster = {
-  get body() {
-    delete this.body;
-    return this.body =
-           document.querySelector('section[role="status"]');
-  },
-  get text() {
-    delete this.text;
-    return this.text =
-           document.querySelector('section[role="status"] p');
-  },
-  get undoBtn() {
-    delete this.undoBtn;
-    return this.undoBtn =
-           document.querySelector('.toaster-banner-undo');
-  },
-  get retryBtn() {
-    delete this.retryBtn;
-    return this.retryBtn =
-           document.querySelector('.toaster-banner-retry');
-  },
-
-  undoableOp: null,
-  retryCallback: null,
-
-  /**
-   * Toaster timeout setting.
-   */
-  _timeout: 5000,
-  /**
-   * Toaster fadeout animation event handling.
-   */
-  _animationHandler: function() {
-    this.body.addEventListener('transitionend', this, false);
-    this.body.classList.add('fadeout');
-  },
-  /**
-   * The list of cards that want to hear about what's up with the toaster.  For
-   * now this will just be the message-list, but it might also be the
-   * message-search card as well.  If it ends up being more, then we probably
-   * want to rejigger things so we can just overlay stuff on most cards...
-   */
-  _listeners: [],
-
-  pendingStack: [],
-
-  /**
-   * Tell toaster listeners about a mutation we just made.
-   *
-   * @param {Object} undoableOp undoable operation.
-   * @param {Boolean} pending
-   *   If true, indicates that we should wait to display this banner until we
-   *   transition to the next card.  This is appropriate for things like
-   *   deleting the message that is displayed on the current card (and which
-   *   will be imminently closed).
-   */
-  logMutation: function(undoableOp, pending) {
-    if (pending) {
-      this.pendingStack.push(this.show.bind(this, 'undo', undoableOp));
-    } else {
-      this.show('undo', undoableOp);
-    }
-  },
-
-  /**
-   * Something failed that it makes sense to let the user explicitly trigger
-   * a retry of!  For example, failure to synchronize.
-   */
-  logRetryable: function(retryStringId, retryCallback) {
-    this.show('retry', retryStringId, retryCallback);
-  },
-
-  handleEvent: function(evt) {
-    switch (evt.type) {
-      case 'click' :
-        var classList = evt.target.classList;
-        if (classList.contains('toaster-banner-undo')) {
-          this.undoableOp.undo();
-          this.hide();
-        } else if (classList.contains('toaster-banner-retry')) {
-          if (this.retryCallback)
-            this.retryCallback();
-          this.hide();
-        } else if (classList.contains('toaster-cancel-btn')) {
-          this.hide();
-        }
-        break;
-      case 'transitionend' :
-        this.hide();
-        break;
-    }
-  },
-
-  show: function(type, operation, callback) {
-    // Close previous toaster before showing the new one.
-    if (!this.body.classList.contains('collapsed')) {
-      this.hide();
-    }
-
-    var text, textId, showUndo = false;
-    if (type === 'undo') {
-      this.undoableOp = operation;
-      // There is no need to show toaster if affected message count < 1
-      if (!this.undoableOp || this.undoableOp.affectedCount < 1) {
-        return;
-      }
-      textId = 'toaster-message-' + this.undoableOp.operation;
-      text = mozL10n.get(textId, { n: this.undoableOp.affectedCount });
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=804916
-      // Remove undo email move/delete UI for V1.
-      showUndo = (this.undoableOp.operation !== 'move' &&
-                  this.undoableOp.operation !== 'delete');
-    } else if (type === 'retry') {
-      textId = 'toaster-retryable-' + operation;
-      text = mozL10n.get(textId);
-      this.retryCallback = callback;
-    // XXX I assume this is for debug purposes?
-    } else if (type === 'text') {
-      text = operation;
-    }
-
-    if (type === 'undo' && showUndo)
-      this.undoBtn.classList.remove('collapsed');
-    else
-      this.undoBtn.classList.add('collapsed');
-    if (type === 'retry')
-      this.retryBtn.classList.remove('collapsed');
-    else
-      this.retryBtn.classList.add('collapsed');
-
-    this.body.title = type;
-    this.text.textContent = text;
-    this.body.addEventListener('click', this, false);
-    this.body.classList.remove('collapsed');
-    this.fadeTimeout = window.setTimeout(this._animationHandler.bind(this),
-                                         this._timeout);
-  },
-
-  isShowing: function() {
-    return !this.body.classList.contains('collapsed');
-  },
-
-  hide: function() {
-    this.body.classList.add('collapsed');
-    this.body.classList.remove('fadeout');
-    window.clearTimeout(this.fadeTimeout);
-    this.fadeTimeout = null;
-    this.body.removeEventListener('click', this);
-    this.body.removeEventListener('transitionend', this);
-
-    // Clear operations:
-    this.undoableOp = null;
-    this.retryCallback = null;
   }
 };
 
@@ -1450,7 +1283,6 @@ function mimeToClass(mimeType) {
 }
 
 exports.Cards = Cards;
-exports.Toaster = Toaster;
 exports.ConfirmDialog = ConfirmDialog;
 exports.FormNavigation = FormNavigation;
 exports.prettyDate = prettyDate;
