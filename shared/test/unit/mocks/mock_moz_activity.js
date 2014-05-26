e1d028cb91ad1f05ddb6fcfff411369e21074280
cctuan <georgeiscoming@gmail.com>
2014-05-26 15:49:46 +0800

    Bug 973443 - [settings] refactor Display panel with AMD pattern

--- shared/test/unit/mocks/mock_moz_activity.js
+++ shared/test/unit/mocks/mock_moz_activity.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var MockMozActivity = function(info) {
+/* exported MockMozActivity */
 
+var MockMozActivity = function(info) {
   MockMozActivity.calls.push(info);
   MockMozActivity.instances.push(this);
 
@@ -11,20 +12,40 @@ var MockMozActivity = function(info) {
 
   var name = info.name;
   var data = info.data;
-
   return {
-    set onsuccess(cb) { setTimeout(cb, 50); },
-    set onerror(cb) {},
+    set onsuccess(cb) {
+      MockMozActivity.successCallback = cb;
+      setTimeout(cb, 50);
+    },
+    set onerror(cb) {
+      MockMozActivity.errorCallback = cb;
+    },
     name: name,
-    data: data
+    data: data,
+    get result() {
+      return MockMozActivity.successResult;
+    }
   };
-
 };
 
 // create an array to hold any calls to this function to test them
 MockMozActivity.mSetup = function() {
   MockMozActivity.calls = [];
   MockMozActivity.instances = [];
+  MockMozActivity.successResult = null;
+};
+
+// setup result data
+MockMozActivity.mSetupResult = function(data) {
+  MockMozActivity.successResult = data;
+};
+
+MockMozActivity.mTriggerOnSuccess = function() {
+  MockMozActivity.successCallback();
+};
+
+MockMozActivity.mTriggerOnError = function() {
+  MockMozActivity.errorCallback();
 };
 
 // destroy the array so it resets every time
