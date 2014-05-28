46161165a8bb10c615e8774b5759cd46a15b45bf
Timothy Guan-tin Chien <timdream@gmail.com>
2014-05-28 15:38:45 +0800

    Bug 1015643 - (Part II) Implement LayoutManager

--- apps/keyboard/js/keyboard/layout_manager.js
+++ apps/keyboard/js/keyboard/layout_manager.js
@@ -181,9 +181,60 @@ LayoutLoader.prototype.initLayout = function(layoutName) {
   Keyboards[layoutName] = null;
 };
 
+/**
+ * LayoutManager do one and only simply job: Allow you to switch currentLayout,
+ * tell you when it is ready, and give you access to it.
+ */
+var LayoutManager = function() {
+  this.currentLayout = null;
+};
+
+LayoutManager.prototype.start = function() {
+  this.loader = new LayoutLoader();
+  this.loader.start();
+
+  this._switchStateId = 0;
+};
+
+/*
+ * Switch switchCurrentLayout() will switch the current method to the
+ * desired layout.
+ *
+ * This method returns a promise and it resolves when the layout is ready.
+ *
+ */
+LayoutManager.prototype.switchCurrentLayout = function(layoutName) {
+  var switchStateId = ++this._switchStateId;
+
+  var loaderPromise = this.loader.getLayoutAsync(layoutName);
+
+  var p = loaderPromise.then(function(layout) {
+    if (switchStateId !== this._switchStateId) {
+      console.log('LayoutManager: ' +
+        'Promise is resolved after another switchCurrentLayout() call. ' +
+        'Reject the promise instead.');
+
+      return Promise.reject(new Error(
+        'LayoutManager: switchCurrentLayout() is called again before ' +
+        'resolving.'));
+    }
+
+    this.currentLayout = layout;
+
+    // resolve to undefined
+    return;
+  }.bind(this), function(error) {
+    return Promise.reject(error);
+  }.bind(this));
+
+  return p;
+};
+
 // Expose Keyboards object
 exports.Keyboards = Keyboards;
 
 exports.LayoutLoader = LayoutLoader;
+exports.LayoutManager = LayoutManager;
+
 
 })(window);
--- apps/keyboard/test/unit/keyboard/layout_manager_test.js
+++ apps/keyboard/test/unit/keyboard/layout_manager_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global LayoutLoader */
+/* global LayoutLoader, LayoutManager */
 
 require('/js/keyboard/layout_manager.js');
 
@@ -154,6 +154,143 @@ suite('LayoutLoader', function() {
         loader.getLayout('foo'), expectedFooLayout, 'foo loaded');
 
       done();
+    }, function() {
+      assert.isTrue(false, 'should not reject');
+
+      done();
+    });
+  });
+});
+
+suite('LayoutManager', function() {
+  var realKeyboards;
+
+  var expectedFooLayout = {
+    keys: [
+      [
+        { value: 'foo' }
+      ]
+    ]
+  };
+
+  suiteSetup(function() {
+    realKeyboards = window.Keyboards;
+  });
+
+  suiteTeardown(function() {
+    window.Keyboards = realKeyboards;
+  });
+
+  test('start', function() {
+    window.Keyboards = {};
+
+    var manager = new LayoutManager();
+    manager.start();
+
+    assert.equal(manager.currentLayout, null, 'started with no layout.');
+  });
+
+  test('switchCurrentLayout', function(done) {
+    window.Keyboards = {};
+
+    var manager = new LayoutManager();
+    manager.start();
+    manager.loader.SOURCE_DIR = './fake-layouts/';
+
+    var p = manager.switchCurrentLayout('foo');
+    p.then(function() {
+      assert.isTrue(true, 'resolved');
+      var layout = manager.loader.getLayout('foo');
+      assert.deepEqual(layout, expectedFooLayout, 'foo loaded');
+      assert.equal(manager.currentLayout, layout, 'currentLayout is set');
+
+      done();
+    }, function() {
+      assert.isTrue(false, 'should not reject');
+
+      done();
+    });
+  });
+
+  test('switchCurrentLayout (failed loader)', function(done) {
+    window.Keyboards = {};
+
+    var manager = new LayoutManager();
+    manager.start();
+    manager.loader.SOURCE_DIR = './fake-layouts/';
+
+    var p = manager.switchCurrentLayout('bar');
+    p.then(function() {
+      assert.isTrue(false, 'should not resolve');
+
+      done();
+    }, function() {
+      assert.isTrue(true, 'rejected');
+
+      done();
+    });
+  });
+
+  test('switchCurrentLayout (twice)', function(done) {
+    window.Keyboards = {};
+
+    var manager = new LayoutManager();
+    manager.start();
+    manager.loader.SOURCE_DIR = './fake-layouts/';
+
+    var p1 = manager.switchCurrentLayout('foo');
+    var p2 = manager.switchCurrentLayout('foo');
+    p1.then(function() {
+      assert.isTrue(false, 'should not resolve');
+    }, function() {
+      assert.isTrue(true, 'rejected');
+    });
+
+    p2.then(function() {
+      assert.isTrue(true, 'resolved');
+      var layout = manager.loader.getLayout('foo');
+      assert.deepEqual(layout, expectedFooLayout, 'foo loaded');
+      assert.equal(manager.currentLayout, layout, 'currentLayout is set');
+
+      done();
+    }, function() {
+      assert.isTrue(false, 'should not reject');
+
+      done();
+    });
+  });
+
+  test('switchCurrentLayout (reload after loaded)', function(done) {
+    window.Keyboards = {
+      'numberLayout': realKeyboards.numberLayout
+    };
+
+    var manager = new LayoutManager();
+    manager.start();
+    manager.loader.SOURCE_DIR = './fake-layouts/';
+
+    var p = manager.switchCurrentLayout('foo');
+    p.then(function() {
+      assert.isTrue(true, 'resolved');
+      var layout = manager.loader.getLayout('foo');
+      assert.deepEqual(layout, expectedFooLayout, 'foo loaded');
+      assert.equal(manager.currentLayout, layout, 'currentLayout is set');
+
+      var p2 = manager.switchCurrentLayout('foo');
+
+      p2.then(function() {
+        assert.isTrue(true, 'resolved');
+        var layout = manager.loader.getLayout('foo');
+        assert.deepEqual(layout, expectedFooLayout, 'foo loaded');
+        assert.equal(manager.currentLayout, layout,
+          'currentLayout is set');
+
+        done();
+      }, function() {
+        assert.isTrue(false, 'should not reject');
+
+        done();
+      });
     }, function() {
       assert.isTrue(false, 'should not reject');
 
