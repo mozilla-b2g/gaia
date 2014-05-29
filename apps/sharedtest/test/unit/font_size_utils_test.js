333d685d194c389d001ab52829ec190108ff894b
Michael Henretty <michael.henretty@gmail.com>
2014-05-28 23:27:46 -0700

    Bug 908300 - use overflow and mutation observer to resize header font

---
+++ apps/sharedtest/test/unit/font_size_utils_test.js
@@ -0,0 +1,459 @@
+/**
+ * Tests for the shared text utils helper
+ */
+
+/* global FontSizeUtils */
+
+'use strict';
+
+require('/shared/js/font_size_utils.js');
+
+suite('shared/js/text_utils.js', function() {
+  const kDefaultFace = 'Arial';
+  const kContainerWidth = 100;
+  const kDefaultSize = 12;
+  const kAllowedSizes = [8, 10, 14];
+  const kStringChar = '#';
+
+  function getMaxFontSizeInfo() {
+    return FontSizeUtils.getMaxFontSizeInfo(text, kAllowedSizes,
+       kDefaultFace, kContainerWidth);
+  }
+
+  function getOverflowCount() {
+    return FontSizeUtils.getOverflowCount(text, kDefaultSize,
+      kDefaultFace, kContainerWidth);
+  }
+
+  var context;
+
+  function getContext() {
+    if (!context) {
+      var canvas = document.createElement('canvas');
+      canvas.setAttribute('moz-opaque', 'true');
+      canvas.setAttribute('width', '1');
+      canvas.setAttribute('height', '1');
+      context = canvas.getContext('2d', { willReadFrequently: true });
+    }
+    return context;
+  }
+
+  function getDefaultContext() {
+    var ctx = getContext();
+    ctx.font = kDefaultSize + 'px ' + kDefaultFace;
+    return ctx;
+  }
+
+  function generateStringForPixels(width, fontSize, fontFace) {
+    fontSize = fontSize || kDefaultSize;
+    fontFace = fontFace || kDefaultFace;
+
+    var ctx = getContext();
+    ctx.font = fontSize + 'px ' + fontFace;
+
+    var str = kStringChar;
+    while (ctx.measureText(str + kStringChar).width < width) {
+      str += kStringChar;
+    }
+
+    return str;
+  }
+
+  var text;
+
+  function setupSmallString(size, face) {
+    text = generateStringForPixels(1, size, face);
+    return text;
+  }
+
+  // string just barely smaller than the container width
+  function setupMediumString(size, face) {
+    text = generateStringForPixels(kContainerWidth, size, face);
+    return text;
+  }
+
+  // string just barely larger than the container width
+  function setupMediumPlusString(size, face) {
+    text = generateStringForPixels(kContainerWidth + 1, size, face) +
+             kStringChar;
+    return text;
+  }
+
+  // way to large to ever fit anywhere
+  function setupLargeString(size, face) {
+    text = generateStringForPixels(kContainerWidth * 10, size, face);
+    return text;
+  }
+
+  function setupHeaderElement() {
+    var header = document.createElement('header');
+    var headerText = document.createElement('h1');
+    header.appendChild(headerText);
+
+    headerText.style.overflow = 'hidden';
+    headerText.style.textOverflow = 'ellipsis';
+    headerText.style.width = kContainerWidth + 'px';
+    // use maximum header fontSize
+    var sizes = FontSizeUtils.getAllowedSizes(headerText);
+    headerText.style.fontSize = sizes[sizes.length - 1] + 'px';
+    return headerText;
+  }
+
+  function setupNonHeaderElement() {
+    var element = document.createElement('h1');
+    element.style.overflow = 'hidden';
+    element.style.textOverflow = 'ellipsis';
+    element.style.width = kContainerWidth + 'px';
+    return element;
+  }
+
+  function getMaxHeaderFontSize() {
+    var sizes = FontSizeUtils.getAllowedSizes(setupHeaderElement());
+    return sizes[sizes.length - 1];
+  }
+
+  function getMinHeaderFontSize() {
+    var sizes = FontSizeUtils.getAllowedSizes(setupHeaderElement());
+    return sizes[0];
+  }
+
+  setup(function() {
+    text = '';
+  });
+
+  teardown(function() {
+    FontSizeUtils.resetCache();
+  });
+
+  /* Global */
+  suite('Global', function() {
+    test('FontSizeUtils exists', function() {
+      assert.ok(FontSizeUtils);
+    });
+  });
+
+  suite('Cache Mechanism', function() {
+    test('Created', function() {
+      FontSizeUtils._getCachedContext(kDefaultSize, kDefaultFace);
+
+      assert.equal(Object.keys(FontSizeUtils._cachedContexts).length, 1);
+    });
+
+    test('Used', function() {
+      var oldContext = FontSizeUtils._getCachedContext(kDefaultSize,
+                                                        kDefaultFace);
+      var newContext = FontSizeUtils._getCachedContext(kDefaultSize,
+                                                        kDefaultFace);
+
+      assert.equal(oldContext, newContext);
+    });
+
+    test('Cleared', function() {
+      FontSizeUtils._getCachedContext(kDefaultSize, kDefaultFace);
+      FontSizeUtils.resetCache();
+
+      assert.equal(Object.keys(FontSizeUtils._cachedContexts).length, 0);
+    });
+
+    test('Created for specified font [size/face]', function() {
+      getMaxFontSizeInfo();
+
+      for (var i = 0; i < kAllowedSizes.length; i++) {
+        assert.ok(FontSizeUtils._getCachedContext(kAllowedSizes[i],
+                                                   kDefaultFace));
+      }
+    });
+  });
+
+  suite('FontSizeUtils.getMaxFontSizeInfo(Small text)', function() {
+    setup(function() {
+      setupSmallString();
+    });
+
+    test('Returns max font size', function() {
+      var infos = getMaxFontSizeInfo();
+      assert.equal(infos.fontSize, kAllowedSizes[kAllowedSizes.length - 1]);
+    });
+
+    test('No overflow', function() {
+      var infos = getMaxFontSizeInfo();
+      assert.isFalse(infos.overflow);
+    });
+  });
+
+  suite('FontSizeUtils.getMaxFontSizeInfo(Medium text)', function() {
+    setup(function() {
+      setupMediumString();
+    });
+
+    test('Returns middle font size', function() {
+      var infos = getMaxFontSizeInfo();
+      assert.equal(infos.fontSize, kAllowedSizes[1]);
+    });
+
+    test('overflow is false', function() {
+      var infos = getMaxFontSizeInfo();
+      assert.isFalse(infos.overflow);
+    });
+  });
+
+  suite('FontSizeUtils.getMaxFontSizeInfo(Large Text)', function() {
+    setup(function() {
+      setupLargeString();
+    });
+
+    test('Returns min font size', function() {
+      var infos = getMaxFontSizeInfo();
+      assert.equal(infos.fontSize, kAllowedSizes[0]);
+    });
+
+    test('Overflow is true', function() {
+      var infos = getMaxFontSizeInfo();
+      assert.isTrue(infos.overflow);
+    });
+  });
+
+
+  suite('FontSizeUtils.getOverflowCount', function() {
+    test('Should be 0 for small text', function() {
+      setupSmallString();
+
+      assert.equal(getOverflowCount(), 0);
+    });
+
+    test('Should be 0 for medium text', function() {
+      setupMediumString();
+
+      assert.equal(getOverflowCount(), 0);
+    });
+
+    test('Should be 1 for medium text plus 1 character', function() {
+      setupMediumPlusString();
+
+      assert.equal(getOverflowCount(), 1);
+    });
+
+    test('Should be overflow width divided by character width', function() {
+      var ctx = getDefaultContext();
+      var charWidth = ctx.measureText(kStringChar).width;
+
+      var string = setupLargeString();
+      var stringWidth = ctx.measureText(string).width;
+
+      // Estimate the amount of character overflow we should have
+      // by measuring overflow width, and then dividing that by
+      // the width of the characters in the string.
+      var overflowWidth = stringWidth - kContainerWidth;
+      var overflowCount = Math.round(overflowWidth / charWidth);
+
+      assert.equal(getOverflowCount(), overflowCount);
+    });
+  });
+
+  suite('FontSizeUtils._getTextChangeObserver', function() {
+    test('Should only ever return 1 mutation observer', function() {
+      var m1 = FontSizeUtils._getTextChangeObserver();
+      var m2 = FontSizeUtils._getTextChangeObserver();
+
+      assert.isTrue(m1 instanceof MutationObserver);
+      assert.equal(m1, m2);
+    });
+  });
+
+  suite('FontSizeUtils.getFontWidth', function() {
+    test('Should measureText correctly for multiple fontSizes', function() {
+      var string = 'arbitrary' + Date.now();
+      var ctx = getContext();
+      // test 2px up to 24px font size measurements
+      for (var fontSize = 2; fontSize < 24; fontSize++) {
+        ctx.font = fontSize + 'px ' + kDefaultFace;
+        assert.equal(ctx.measureText(string).width,
+          FontSizeUtils.getFontWidth(string, fontSize, kDefaultFace));
+      }
+    });
+  });
+
+  suite('FontSizeUtils.getAllowedSizes', function() {
+    test('Should return empty array for non header elements', function() {
+      var el = setupNonHeaderElement();
+      assert.equal(FontSizeUtils.getAllowedSizes(el).length, 0);
+    });
+
+    test('Should return a non-empty array for header elements', function() {
+      var el = setupHeaderElement();
+      var allowedSizes = FontSizeUtils.getAllowedSizes(el);
+      assert.isTrue(allowedSizes.length > 0);
+    });
+  });
+
+  suite('FontSizeUtils.autoResizeElement', function() {
+    test('Should not resize a small header title', function() {
+      var el = setupHeaderElement();
+      var fontSizeBefore = getComputedStyle(el).fontSize;
+
+      el.textContent = setupSmallString(fontSizeBefore);
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should not resize a medium header title', function() {
+      var el = setupHeaderElement();
+      var fontSizeBefore = getComputedStyle(el).fontSize;
+
+      el.textContent = setupMediumString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should resize a barely overflowing header title', function() {
+      var el = setupHeaderElement();
+      var fontSizeBefore = getComputedStyle(el).fontSize;
+
+      el.textContent = setupMediumPlusString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.notEqual(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should resize a barely overflowing header title', function() {
+      var el = setupHeaderElement();
+      var fontSizeBefore = getComputedStyle(el).fontSize;
+
+      el.textContent = setupMediumPlusString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.notEqual(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should resize to minimum a very long header title', function() {
+      var el = setupHeaderElement();
+      var fontSizeBefore = '50px';
+      el.style.fontSize = fontSizeBefore;
+
+      el.textContent = setupLargeString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.notEqual(getMinHeaderFontSize(), getComputedStyle(el).fontSize);
+    });
+  });
+
+  suite('FontSizeUtils.autoResizeElement on non-headers', function() {
+    test('Should not resize a small header title', function() {
+      var el = setupNonHeaderElement();
+      var fontSizeBefore = '50px';
+      el.style.fontSize = fontSizeBefore;
+
+      el.textContent = setupSmallString(fontSizeBefore);
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should not resize a medium header title', function() {
+      var el = setupNonHeaderElement();
+      var fontSizeBefore = '50px';
+      el.style.fontSize = fontSizeBefore;
+
+      el.textContent = setupMediumString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should not resize a barely overflowing header title', function() {
+      var el = setupNonHeaderElement();
+      var fontSizeBefore = '50px';
+      el.style.fontSize = fontSizeBefore;
+
+      el.textContent = setupMediumPlusString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should not resize a barely overflowing header title', function() {
+      var el = setupNonHeaderElement();
+      var fontSizeBefore = '50px';
+      el.style.fontSize = fontSizeBefore;
+
+      el.textContent = setupMediumPlusString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+
+    test('Should not resize to minimum a very long header title', function() {
+      var el = setupNonHeaderElement();
+      var fontSizeBefore = '50px';
+      el.style.fontSize = fontSizeBefore;
+
+      el.textContent = setupLargeString(parseInt(fontSizeBefore));
+      FontSizeUtils.autoResizeElement(el);
+
+      assert.equal(fontSizeBefore, getComputedStyle(el).fontSize);
+    });
+  });
+
+  suite('FontSizeUtils handle overflow events', function() {
+    test('Header overflow should cause autoresize', function(done) {
+      var el = setupHeaderElement();
+      document.body.appendChild(el.parentNode);
+      el.textContent = setupLargeString();
+
+      var stub = sinon.stub(FontSizeUtils, 'autoResizeElement', function() {
+        el.parentNode.removeChild(el);
+        stub.restore();
+        assert.isTrue(stub.calledWith(el));
+        done();
+      });
+    });
+
+    test('Non-header overflow should cause not autoresize', function(done) {
+      var el = setupNonHeaderElement();
+      document.body.appendChild(el);
+      el.textContent = setupLargeString();
+
+      var spy = sinon.spy(FontSizeUtils, 'autoResizeElement');
+      assert.isTrue(spy.notCalled);
+
+      el.addEventListener('overflow', function() {
+        el.parentNode.removeChild(el);
+        spy.restore();
+        assert.isTrue(spy.notCalled);
+        done();
+      });
+    });
+  });
+
+  suite('FontSizeUtils auto resize without Mutation Observer', function() {
+    test('Should auto-resize back up when text changes', function(done) {
+      var el = setupHeaderElement();
+      document.body.appendChild(el.parentNode);
+      el.textContent = setupLargeString();
+
+      // When we get an overflow event, make sure we have auto-resized
+      // to the minimum possible font size for the large string.
+      el.addEventListener('overflow', function onOverflow() {
+        el.removeEventListener('overflow', onOverflow);
+        assert.equal(parseInt(getComputedStyle(el).fontSize),
+                     getMinHeaderFontSize());
+
+        // Now set the smallest string possible, and make sure we have
+        // auto-resized back to the maximum possible font size.
+        el.textContent = setupSmallString();
+        el.addEventListener('underflow', function onUnderflow() {
+          el.removeEventListener('underflow', onUnderflow);
+          assert.equal(parseInt(getComputedStyle(el).fontSize),
+                       getMaxHeaderFontSize());
+
+          // Clean up.
+          el.parentNode.removeChild(el);
+          done();
+        });
+      });
+    });
+  });
+});
+
---
+++ shared/js/font_size_utils.js
@@ -0,0 +1,242 @@
+(function(exports) {
+  'use strict';
+
+  /**
+   * Allowable font sizes for header elements.
+   */
+  var HEADER_SIZES = [
+    16, 17, 18, 19, 20, 21, 22, 23
+  ];
+
+  /**
+   * Utility functions for measuring and manipulating font sizes
+   */
+  var FontSizeUtils = {
+
+    /**
+     * Keep a cache of canvas contexts with a given font.
+     * We do this because it is faster to create new canvases
+     * than to re-set the font on existing contexts repeatedly.
+     */
+    _cachedContexts: {},
+
+    /**
+     * Grab or create a cached canvas context for a given fontSize/family pair.
+     *
+     * @param {Integer} fontSize The font size of the canvas we want.
+     * @param {String} fontFamily The font family of the canvas we want.
+     * @return {CanvasRenderingContext2D} A context with the specified font.
+     */
+    _getCachedContext: function(fontSize, fontFamily) {
+      var cache = this._cachedContexts;
+      var ctx = cache[fontSize] ? cache[fontSize][fontFamily] : null;
+
+      if (!ctx) {
+        var canvas = document.createElement('canvas');
+        canvas.setAttribute('moz-opaque', 'true');
+        canvas.setAttribute('width', '1');
+        canvas.setAttribute('height', '1');
+
+        ctx = canvas.getContext('2d', { willReadFrequently: true });
+        ctx.font = fontSize + 'px ' + fontFamily;
+
+        // Populate the contexts cache.
+        if (!cache[fontSize]) {
+          cache[fontSize] = {};
+        }
+        cache[fontSize][fontFamily] = ctx;
+      }
+
+      return ctx;
+    },
+
+    /**
+     * Clear any current canvas contexts from the cache.
+     */
+    resetCache: function() {
+      this._cachedContexts = {};
+    },
+
+    /**
+     * Use a single observer for all text changes we are interested in.
+     */
+    _textChangeObserver: null,
+
+    /**
+     * Auto resize all text changes.
+     * @param {Array} mutations A MutationRecord list.
+     */
+    _handleTextChanges: function(mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        this.autoResizeElement(mutations[i].target);
+      }
+    },
+
+    /**
+     * Singleton-like interface for getting our text change observer.
+     * By reusing the observer, we make sure we only ever attach a
+     * single observer to any given element we are interested in.
+     */
+    _getTextChangeObserver: function() {
+      if (!this._textChangeObserver) {
+        this._textChangeObserver = new MutationObserver(
+          this._handleTextChanges.bind(this));
+      }
+      return this._textChangeObserver;
+    },
+
+    /**
+     * Perform auto-resize when textContent changes on element.
+     *
+     * @param {HTMLElement} element The element to observer for changes
+     */
+    _resizeOnTextChange: function(element) {
+      var observer = this._getTextChangeObserver();
+      // Listen for any changes in the child nodes of the header.
+      observer.observe(element, { childList: true });
+    },
+
+    /**
+     * Get the width of a string in pixels, given its fontSize and fontFamily.
+     *
+     * @param {String} string The string we are measuring.
+     * @param {Integer} fontSize The size of the font to measure against.
+     * @param {String} fontFamily The font family to measure against.
+     * @return {Integer} The pixel width of the string with the given font.
+     */
+    getFontWidth: function(string, fontSize, fontFamily) {
+      var ctx = this._getCachedContext(fontSize, fontFamily);
+      return ctx.measureText(string).width;
+    },
+
+    /**
+     * Get the maximum allowable fontSize for a string such that it will
+     * not overflow past a maximum width.
+     *
+     * @param {String} string The string for which to check max font size.
+     * @param {Array} allowedSizes A list of fontSizes allowed.
+     * @param {String} fontFamily The font family of the string we're measuring.
+     * @param {Integer} maxWidth The maximum number of pixels before overflow.
+     * @return {Object} Dict containing max fontSize and overflow flag.
+     */
+    getMaxFontSizeInfo: function(string, allowedSizes, fontFamily, maxWidth) {
+      var fontSize;
+      var resultWidth;
+      var i = allowedSizes.length - 1;
+
+      do {
+        fontSize = allowedSizes[i];
+        resultWidth = this.getFontWidth(string, fontSize, fontFamily);
+        i--;
+      } while (resultWidth > maxWidth && i >= 0);
+
+      return { fontSize: fontSize, overflow: resultWidth > maxWidth };
+    },
+
+    /**
+     * Get the amount of characters truncated from overflow ellipses.
+     *
+     * @param {String} string The string for which to check max font size.
+     * @param {Integer} fontSize The font size of the string we are measuring.
+     * @param {String} fontFamily The font family of the string we're measuring.
+     * @param {Integer} maxWidth The maximum number of pixels before overflow.
+     */
+    getOverflowCount: function(string, fontSize, fontFamily, maxWidth) {
+      var substring;
+      var resultWidth;
+      var overflowCount = -1;
+
+      do {
+        overflowCount++;
+        substring = string.substr(0, string.length - overflowCount);
+        resultWidth = this.getFontWidth(substring, fontSize, fontFamily);
+      } while (substring.length > 0 && resultWidth > maxWidth);
+
+      return overflowCount;
+    },
+
+    /**
+     * Get an array of allowed font sizes for an element
+     *
+     * @param {HTMLElement} element The element to get allowed sizes for.
+     * @return {Array} An array containing pizels values of allowed sizes.
+     */
+    getAllowedSizes: function(element) {
+      if (element.tagName === 'H1' && element.parentNode &&
+          element.parentNode.tagName === 'HEADER') {
+        return HEADER_SIZES;
+      }
+      // No allowed sizes for this element, so return empty array.
+      return [];
+    },
+
+    /**
+     * Perform auto-resize logic for an element.
+     *
+     * @param {HTMLElement} element The element to perform auto-resize on.
+     */
+    autoResizeElement: function(element) {
+      var allowedSizes = this.getAllowedSizes(element);
+      if (allowedSizes.length === 0) {
+        return;
+      }
+      var style = window.getComputedStyle(element);
+      var info = this.getMaxFontSizeInfo(element.textContent, allowedSizes,
+                                         style.fontFamily,
+                                         parseInt(style.width));
+      element.style.fontSize = info.fontSize + 'px';
+
+      // If we have resized to less than our max fontSize for this
+      // container, we need to listen for textContent changes so that
+      // we can auto resize to a larger fontSize if space allows it.
+      if (info.fontSize !== allowedSizes[allowedSizes.length - 1]) {
+        this._resizeOnTextChange(element);
+      }
+    },
+
+    /**
+     * When certain elements overflow, auto resize their fonts
+     *
+     * @param {UIEvent} ext The overflow/underflow event object.
+     */
+    handleTextFlowChange: function (evt) {
+      var element = evt.target;
+      // This check should really be in its own function, but as a
+      // performance optimization we will avoid the overhead of any
+      // additional function call by doing this check inline.
+      if (element.tagName === 'H1' && element.parentNode &&
+          element.parentNode.tagName === 'HEADER') {
+        this.autoResizeElement(evt.target);
+      }
+    },
+
+    /**
+     * Initialize the FontSizeUtils, add overflow handler and perform
+     * auto resize once strings have been localized.
+     */
+    init: function () {
+      // Add overflow listener once document body is ready.
+      if (document.readyState === 'loading') {
+        window.addEventListener('DOMContentLoaded', function() {
+          document.body.addEventListener('overflow',
+            this.handleTextFlowChange.bind(this), true);
+        }.bind(this));
+      } else {
+        document.body.addEventListener('overflow',
+          this.handleTextFlowChange.bind(this), true);
+      }
+
+      // When l10n is ready, resize all currently displayed headers.
+      navigator.mozL10n && navigator.mozL10n.ready(function() {
+        var headers = document.querySelectorAll('header > h1');
+        for (var i = 0; i < headers.length; i++) {
+          this.autoResizeElement(headers[i]);
+        }
+      }.bind(this));
+    }
+  };
+
+  FontSizeUtils.init();
+
+  exports.FontSizeUtils = FontSizeUtils;
+}(this));
