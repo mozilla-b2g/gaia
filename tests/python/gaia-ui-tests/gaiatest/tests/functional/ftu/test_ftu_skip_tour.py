5d67105bc0107ac1813a3a7821c8f83b76145d71
Zac <zcampbell@mozilla.com>
2014-05-30 12:31:54 +0100

    Bug 985558 - Make is_wifi_enabled handle mozWifiManager states
    Conflicts:
    	tests/python/gaia-ui-tests/gaiatest/tests/functional/ftu/test_ftu_skip_tour.py

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/ftu/test_ftu_skip_tour.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/ftu/test_ftu_skip_tour.py
@@ -89,7 +89,8 @@ class TestFtu(GaiaTestCase):
         # launch the First Time User app
         self.app = self.apps.launch('FTU')
 
-        self.wait_for_condition(lambda m: self.data_layer.is_wifi_enabled)
+        # If mozWifiManager is not initialised an exception may be thrown
+        Wait(self.marionette).until(lambda m: self.data_layer.is_wifi_enabled)
 
     def create_language_locator(self, language):
         return (By.CSS_SELECTOR, "#languages ul li input[name='language.current'][value='%s'] ~ p" % language)
