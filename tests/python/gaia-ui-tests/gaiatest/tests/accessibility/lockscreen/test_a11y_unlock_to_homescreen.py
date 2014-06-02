1ccf03cce4133d773694471074eb4855b0eb1dd5
No-Jun <npark@mozilla.com>
2014-06-01 20:06:01 -0400

    reverting changes made to the standard test scripts

--- tests/python/gaia-ui-tests/gaiatest/tests/accessibility/lockscreen/test_a11y_unlock_to_homescreen.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/accessibility/lockscreen/test_a11y_unlock_to_homescreen.py
@@ -6,26 +6,15 @@ from marionette.by import By
 from gaiatest import GaiaTestCase
 from gaiatest.apps.lockscreen.app import LockScreen
 from gaiatest.apps.homescreen.app import Homescreen
-from gaiatest.utils.Imagecompare.imagecompare_util import ImageCompareUtil
-import sys,time
 
 
 class TestLockScreenAccessibility(GaiaTestCase):
 
-    #needed for the imagecapture utility
-
     def setUp(self):
         GaiaTestCase.setUp(self)
-
-        current_module = str(sys.modules[__name__])
-        self.module_name = current_module[current_module.find("'")+1:current_module.find("' from")]
-        self.graphics = ImageCompareUtil(self.marionette,'.')
-
         self.device.lock()
 
-
     def test_a11y_unlock_to_homescreen(self):
-
         lock_screen = LockScreen(self.marionette)
         lockScreen_window = self.marionette.find_element(*lock_screen._lockscreen_window_locator)
         homescreen_container = self.marionette.find_element(By.ID, 'homescreen')
@@ -38,5 +27,5 @@ class TestLockScreenAccessibility(GaiaTestCase):
         self.assertEquals(self.apps.displayed_app.name, homescreen.name)
 
         self.assertTrue(self.accessibility.is_hidden(lockScreen_window))
-        self.assertTrue(self.accessibility.is_visible(homescreen_container))
+        self.assertFalse(self.accessibility.is_hidden(homescreen_container))
 
--- tests/python/gaia-ui-tests/gaiatest/tests/accessibility/phone/test_a11y_phone_select_toolbars.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/accessibility/phone/test_a11y_phone_select_toolbars.py
@@ -6,8 +6,6 @@ from marionette.by import By
 from gaiatest import GaiaTestCase
 from gaiatest.apps.phone.app import Phone
 
-from gaiatest.utils.Imagecompare.imagecompare_util import ImageCompareUtil
-import sys,time
 
 class TestAccessibilityPhoneSelectToolbars(GaiaTestCase):
 
@@ -15,10 +13,6 @@ class TestAccessibilityPhoneSelectToolbars(GaiaTestCase):
         GaiaTestCase.setUp(self)
         self.phone = Phone(self.marionette)
         self.phone.launch()
-
-        current_module = str(sys.modules[__name__])
-        self.module_name = current_module[current_module.find("'")+1:current_module.find("' from")]
-        self.graphics = ImageCompareUtil(self.marionette,'.')
 
     def test_phone_select_toolbars(self):
 
@@ -35,8 +29,6 @@ class TestAccessibilityPhoneSelectToolbars(GaiaTestCase):
         self.assertTrue(self.accessibility.is_visible(self.marionette.find_element(
             *self.phone._call_log_toolbar_locator)))
 
-        self.graphics.invoke_screen_capture(self.marionette.get_active_frame())
-
         # Screen reader activated contacts button click.
         self.phone.a11y_click_contacts()
         self.apps.switch_to_displayed_app()
@@ -51,8 +43,6 @@ class TestAccessibilityPhoneSelectToolbars(GaiaTestCase):
         self.assertTrue(self.accessibility.is_hidden(self.marionette.find_element(
             *self.phone._call_log_toolbar_locator)))
 
-        self.graphics.invoke_screen_capture(self.marionette.get_active_frame())
-
         # Screen reader activated keypad button click.
         self.phone.a11y_click_keypad_toolbar_button()
 
@@ -65,18 +55,3 @@ class TestAccessibilityPhoneSelectToolbars(GaiaTestCase):
         # Call log is hidden from the screen reader.
         self.assertTrue(self.accessibility.is_hidden(self.marionette.find_element(
             *self.phone._call_log_toolbar_locator)))
-
-        self.graphics.invoke_screen_capture(self.marionette.get_active_frame())
-
-    def tearDown(self):
-
-        # In case the assertion fails this will still kill the call
-        # An open call creates problems for future tests
-        if (self.testvars['collect_ref_images'] == 'true'):
-            # collect screenshots and save it as ref images
-            self.graphics.collect_ref_images(self.testvars['screenshot_location'],'.',self.module_name)
-        else:
-            # pull the screenshots off the device and compare.
-            self.graphics.collect_and_compare(self,'.',self.testvars['screenshot_location'] , self.module_name, 5)
-
-        GaiaTestCase.tearDown(self)
