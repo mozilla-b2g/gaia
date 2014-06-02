6a07852a55f999139c6112f093beef23f2fdbb76
Michael Henretty <michael.henretty@gmail.com>
2014-06-01 18:32:20 -0700

    add centering logic to auto-resize

--- shared/js/font_size_utils.js
+++ shared/js/font_size_utils.js
@@ -206,8 +206,28 @@
       // additional function call by doing this check inline.
       if (element.tagName === 'H1' && element.parentNode &&
           element.parentNode.tagName === 'HEADER') {
-        this.autoResizeElement(evt.target);
+        this.reformatHeaderText(element);
       }
+    },
+
+    /**
+     * Resize and reposition the header text based on string length and
+     * container position.
+     *
+     * @param {HTMLElement} header h1 text inside header to reformat.
+     */
+    reformatHeaderText: function(header) {
+      FontSizeUtils.autoResizeElement(header);
+      FontSizeUtils.centerTextToScreen(header);
+    },
+
+    /**
+     * Center an elements text based on screen position rather than container.
+     *
+     * @param {HTMLElement} element The element whose text we want to center.
+     */
+    centerTextToScreen: function(element) {
+      // TODO: fill in centering logic here
     },
 
     /**
