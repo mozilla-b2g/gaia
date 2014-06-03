67abfe0e7479e44972ef3c7819646c2935fa65a1
Kevin Grandon <kevingrandon@yahoo.com>
2014-06-02 22:11:58 -0700

    Bug 1019388 - [Collections] Enable bookmarking for results r=amirn

--- apps/collection/js/contextmenu.js
+++ apps/collection/js/contextmenu.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global MozActivity */
 
 (function(exports) {
 
@@ -6,6 +7,7 @@
     this.menu = document.getElementById('cloud-menu');
     this.grid = document.getElementById('grid');
 
+    this.bookmarkButton = this.menu.querySelector('#bookmark-cloudapp');
     this.pinButton = this.menu.querySelector('#pin-cloudapp');
 
     this.grid.addEventListener('contextmenu', this);
@@ -19,6 +21,25 @@
     this.menu.addEventListener('gaiamenu-cancel', function() {
       this.target = null;
     });
+
+    this.bookmarkButton.addEventListener('click', function bookmark() {
+      var identifier = this.target.dataset.identifier;
+      var icon = this.grid.getIcon(identifier);
+
+      /* jshint nonew: false */
+      new MozActivity({
+        name: 'save-bookmark',
+        data: {
+          type: 'url',
+          url: icon.detail.url,
+          name: icon.name,
+          icon: icon.icon
+        }
+      });
+
+      this.target = null;
+      this.menu.hide();
+    }.bind(this));
 
     this.pinButton.addEventListener('click', function pin() {
       if (this.target) {
