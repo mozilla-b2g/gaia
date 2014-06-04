ece4582ec8652e3a8f3270c51187bbbd58690e7c
Miller Medeiros <miller@millermedeiros.com>
2014-06-04 18:55:05 -0300

    Bug 1014820 - [Calendar] remove the waitForKeyboardHide from marionette tests

--- apps/calendar/test/marionette/caldav_test.js
+++ apps/calendar/test/marionette/caldav_test.js
@@ -10,8 +10,18 @@ var ACCOUNT_USERNAME = 'firefox-os',
     DATE_PATTERN = 'yyyymmdd"T"HHMMssZ';
 
 marionette('configure CalDAV accounts', function() {
-  var client = marionette.client(),
-      serverHelper = new Radicale(),
+  var client = marionette.client({
+    prefs: {
+      // we need to disable the keyboard to avoid intermittent failures on
+      // Travis (transitions might take longer to run and block UI)
+      'dom.mozInputMethod.enabled': false,
+      // Do not require the B2G-desktop app window to have focus (as per the
+      // system window manager) in order for it to do focus-related things.
+      'focusmanager.testmode': true,
+    }
+  });
+
+  var serverHelper = new Radicale(),
       app = null;
 
   setup(function(done) {
--- apps/calendar/test/marionette/today_test.js
+++ apps/calendar/test/marionette/today_test.js
@@ -10,7 +10,14 @@ marionette('today', function() {
   var app;
 
   var client = marionette.client({
-    settings: { 'keyboard.ftu.enabled': false }
+    prefs: {
+      // we need to disable the keyboard to avoid intermittent failures on
+      // Travis (transitions might take longer to run and block UI)
+      'dom.mozInputMethod.enabled': false,
+      // Do not require the B2G-desktop app window to have focus (as per the
+      // system window manager) in order for it to do focus-related things.
+      'focusmanager.testmode': true,
+    }
   });
 
   setup(function() {
