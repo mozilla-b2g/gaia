6ed7f9341db5e80a6ebc07906e891e054ba35a0c
Kotaro Oki <ko-oki@kddi-tech.com>
2014-06-06 19:49:01 +0900

    Bug974864, Pick recipient(email adress) up from contacts app

--- apps/sms/js/thread_ui.js
+++ apps/sms/js/thread_ui.js
@@ -923,15 +923,15 @@ var ThreadUI = global.ThreadUI = {
     var activity = new MozActivity({
       name: 'pick',
       data: {
-        type: 'webcontacts/tel'
+        type: 'webcontacts/select'
       }
     });
 
     activity.onsuccess = (function() {
       if (!activity.result ||
-          !activity.result.tel ||
-          !activity.result.tel.length ||
-          !activity.result.tel[0].value) {
+          !activity.result.select ||
+          !activity.result.select.length ||
+          !activity.result.select[0].value) {
         console.error('The pick activity result is invalid.');
         return;
       }
@@ -939,7 +939,7 @@ var ThreadUI = global.ThreadUI = {
       Recipients.View.isFocusable = true;
 
       var data = Utils.basicContact(
-        activity.result.tel[0].value, activity.result
+        activity.result.select[0].value, activity.result.contact
       );
       data.source = 'contacts';
 
