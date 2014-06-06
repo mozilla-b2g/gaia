cafc2cbaea537917d0cb5882b1bc8e004b4baddd
Raniere Silva <raniere@ime.unicamp.br>
2014-06-06 09:28:58 -0300

    Bug 1021500 - Keep layouts consistent
    - Remove '_' from the second row for input "email"
    - Remove ':' from the second row for input "url"

--- apps/keyboard/js/layouts/hu.js
+++ apps/keyboard/js/layouts/hu.js
@@ -28,8 +28,7 @@ Keyboards.hu = {
     ], [
       { value: 'a' }, { value: 's' }, { value: 'd' }, { value: 'f' },
       { value: 'g' } , { value: 'h' }, { value: 'j' }, { value: 'k' },
-      { value: 'l' }, { value: "'", keyCode: 39, hidden: ['email', 'url'] },
-      { value: ':', visible: ['url']}, { value: '_', visible: ['email']}
+      { value: 'l' }, { value: "'", keyCode: 39 }
     ], [
       { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
       { value: 'y' }, { value: 'x' }, { value: 'c' }, { value: 'v' },
