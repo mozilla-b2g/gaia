56a2ec2702311565c8bc02fdb70c7008c2e839f0
Kamil Leszczuk <kamituel@gmail.com>
2014-06-06 20:39:10 +0200

    Bug 1013845: Fix character encoding when sharing contacts via NFC

--- apps/communications/contacts/test/unit/nfc_test.js
+++ apps/communications/contacts/test/unit/nfc_test.js
@@ -1,17 +1,17 @@
 'use strict';
 
-/* global MockMozNfc */
-/* global contacts */
-/* global MocksHelper */
-/* global Contacts */
-/* global fb */
+/* global MockMozNfc, contacts, MocksHelper, Contacts, fb,
+          MockMozNDEFRecord, mozContact, NfcUtils */
 
+require('/shared/test/unit/mocks/mock_moz_ndefrecord.js');
+require('/shared/test/unit/mocks/mock_lazy_loader.js');
 require('/shared/test/unit/mocks/mock_moz_nfc.js');
+require('/shared/js/contact2vcard.js');
+require('/shared/js/setImmediate.js');
+require('/shared/js/nfc_utils.js');
 requireApp('communications/contacts/test/unit/mock_fb.js');
 requireApp('communications/contacts/test/unit/mock_l10n.js');
 requireApp('communications/contacts/test/unit/mock_navigation.js');
-
-require('/shared/test/unit/mocks/mock_lazy_loader.js');
 
 if (!window.contacts) {
   window.contacts = null;
@@ -24,6 +24,7 @@ mocksHelperForNFC.init();
 
 suite('NFC', function() {
   var realMozNfc;
+  var realMozNDEFRecord;
 
   suiteSetup(function(done) {
     requireApp(
@@ -31,8 +32,11 @@ suite('NFC', function() {
       function() {
         realMozNfc = window.navigator.mozNfc;
         window.navigator.mozNfc = MockMozNfc;
-        mocksHelperForNFC.suiteSetup();
 
+        realMozNDEFRecord = window.MozNDEFRecord;
+        window.MozNDEFRecord = MockMozNDEFRecord;
+
+        mocksHelperForNFC.suiteSetup();
         requireApp('communications/contacts/js/nfc.js', done);
       }
     );
@@ -41,6 +45,11 @@ suite('NFC', function() {
   suiteTeardown(function() {
     mocksHelperForNFC.suiteTeardown();
     window.navigator.mozNfc = realMozNfc;
+    window.MozNDEFRecord = realMozNDEFRecord;
+  });
+
+  teardown(function() {
+    fb.setIsFbContact(false);
   });
 
   test('onpeerready is null on start', function() {
@@ -67,6 +76,41 @@ suite('NFC', function() {
       }
     );
     sinon.assert.called(spy, 'showStatus');
+  });
+
+  // Bug 1013845
+  test('Shares diacritic characters correctly', function(done) {
+    var realSendNDEF = MockMozNfc.MockNFCPeer.sendNDEF;
+
+    var contact = new mozContact({
+      name: ['firefox'],
+      givenName: ['الثعلب النار 火狐狸 firefox']
+    });
+
+    var english = [102, 105, 114, 101, 102, 111, 120];
+    var chinese = [231, 129, 171, 231, 139, 144, 231, 139, 184];
+    var arabic = [216, 167, 217, 132, 216, 171, 216, 185, 217, 132, 216,
+                  168, 32, 216, 167, 217, 132, 217, 134, 216, 167, 216, 177];
+    var space = 32;
+    var name = arabic.concat(space).concat(chinese)
+                .concat(space).concat(english);
+
+    MockMozNfc.MockNFCPeer.sendNDEF = function(records) {
+      MockMozNfc.MockNFCPeer.sendNDEF = realSendNDEF;
+
+      var header = 'BEGIN:VCARD\nVERSION:3.0\nn:;';
+      var payload = records[0].payload;
+
+      var pos = header.length + 2;
+      var actual = payload.subarray(pos, pos + name.length);
+      assert.isTrue(NfcUtils.equalArrays(actual, name));
+
+      done();
+      return {};
+    };
+
+    contacts.NFC.startListening(contact);
+    MockMozNfc.onpeerready({});
   });
 
 });
