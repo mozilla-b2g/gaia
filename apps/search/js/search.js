de913292fd382ba42ec6d24c24a97d326cfe0c6b
Dale Harvey <dale@arandomurl.com>
2014-06-07 04:26:33 +0100

    Bug 1022053 - Add notification teaching how to configure search provider. r=kgrandon

--- apps/search/js/search.js
+++ apps/search/js/search.js
@@ -1,10 +1,10 @@
+/* global Search, SearchDedupe, UrlHelper, SettingsListener, asyncStorage,
+          MozActivity */
+/*jshint nonew: false */
+
 (function() {
 
   'use strict';
-  /* global Search */
-  /* global SearchDedupe */
-  /* global UrlHelper */
-  /* global SettingsListener */
 
   // timeout before notifying providers
   var SEARCH_DELAY = 600;
@@ -27,6 +27,14 @@
     newTabPage: document.getElementById('newtab-page'),
 
     suggestionsEnabled: false,
+
+    /**
+     * Used to display a notice on how to configure the search provider
+     * on first use
+     */
+    suggestionNotice: document.getElementById('suggestions-notice-wrapper'),
+    toShowNotice: false,
+    changeCount: 0,
 
     init: function() {
 
@@ -77,6 +85,14 @@
         this.suggestionsEnabled = enabled;
       }.bind(this));
 
+      this.initConfigureLink();
+      this.initNotice();
+
+      // Fire off a dummy geolocation request so the prompt can be responded
+      // to before the user starts typing
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition();
+      }
     },
 
     /**
@@ -116,6 +132,7 @@
       var input = msg.data.input;
       var providers = this.providers;
 
+      this.maybeShowNotice();
       this.clear();
 
       this.changeTimeout = setTimeout(function doSearch() {
@@ -129,6 +146,48 @@
           }
         }
       }.bind(this), SEARCH_DELAY);
+    },
+
+    /**
+     * Show a notice to the user informaing them of how to configure
+     * search providers, should only be shown once.
+     */
+    initNotice: function() {
+
+      var noticeKey = 'notice-shown';
+      var confirm = document.getElementById('suggestions-notice-confirm');
+
+      confirm.addEventListener('click', function() {
+        this.suggestionNotice.hidden = true;
+        this.toShowNotice = false;
+        asyncStorage.setItem(noticeKey, true);
+      }.bind(this));
+
+      asyncStorage.getItem(noticeKey, function(value) {
+        this.toShowNotice = !value;
+      }.bind(this));
+    },
+
+    maybeShowNotice: function() {
+      if (this.toShowNotice && ++this.changeCount > 2) {
+        this.suggestionNotice.hidden = false;
+      }
+    },
+
+    /**
+     * Open a settings activity so use can configure search providers
+     */
+    initConfigureLink: function() {
+      var confirm = document.getElementById('configure-provider');
+      confirm.addEventListener('click', function() {
+        new MozActivity({
+          name: 'configure',
+          data: {
+            target: 'device',
+            section: 'homescreen'
+          }
+        });
+      });
     },
 
     /**
