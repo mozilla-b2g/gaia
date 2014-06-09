e7d913c2b535bece3cdb90851861e02d92a7d66a
Vivien Nicolas <vnicolas@mozilla.com>
2014-06-09 17:28:33 +0200

    Bug 971008 - Don't cause System App reflow noise in the Developer HUD. r=janx

--- apps/system/test/unit/developer_hud_test.js
+++ apps/system/test/unit/developer_hud_test.js
@@ -36,26 +36,126 @@ suite('developerHUD', function() {
       document.body.innerHTML = '';
     });
 
-    test('show one widget', function() {
+    function getCanvasFor(widgets) {
+      var canvas = document.createElement('canvas');
+      canvas.classList.add('widgets');
+      canvas.width = window.innerWidth;
+      canvas.height = 30;
+      document.body.appendChild(canvas);
+
+      var ctx = canvas.getContext('2d');
+      ctx.font = '18px sans-serif';
+      ctx.textBaseline = 'top';
+
+      ctx.save();
+
+      // Widgets are positioned starting from the right side of the screen.
+      ctx.translate(canvas.width, 0);
+
+      widgets.forEach(function(widget) {
+        // The size of a widget is comprise between 30px and the size of its
+        // content. There is also an additional padding of 5px on each side.
+        var textWidth = ctx.measureText(widget.value).width;
+        var widgetWidth = Math.max(30, textWidth) + (5 * 2);
+
+        // Position the widget relatively to the last position on the left
+        // of the screen.
+        ctx.translate(-widgetWidth, 0);
+
+        // Fill widget background-color.
+        ctx.fillStyle = widget.color;
+        ctx.fillRect(0, 0, widgetWidth, canvas.height);
+
+        // Draw widget text centered both horizontally and vertically.
+        ctx.fillStyle = 'white';
+        ctx.fillText(widget.value,
+                     (widgetWidth - textWidth) / 2,
+                     canvas.height / 4);
+      });
+
+      ctx.restore();
+
+      return canvas;
+    }
+
+    test('show widgets', function() {
       updateMetrics([
         {name: 'bugs', value: 42}
       ]);
       var view = getDeveloperHUD();
       assert.isDefined(view);
-      var widget = view.querySelector('.widget');
-      assert.isDefined(widget);
-      assert.equal(widget.textContent, '42');
+      var widgets = view.querySelector('.widgets');
+      assert.isDefined(widgets);
     });
 
-    test('show two widgets', function() {
-      updateMetrics([
+    test('show simple widgets', function() {
+      var metrics = [
         {name: 'errors', value: 23},
-        {name: 'warnings', value: 16}
-      ]);
+        {name: 'warnings', value: 16},
+        {name: 'security', value: 1},
+        {name: 'reflows', value: 4},
+      ];
+
+      updateMetrics(metrics);
       var view = getDeveloperHUD();
       assert.isDefined(view);
-      var widgets = view.querySelectorAll('.widget');
-      assert.equal(widgets.length, 2);
+
+      var canvas = view.querySelector('canvas');
+      var referenceCanvas = getCanvasFor([
+        { 'color': 'red', 'value': '23'},
+        { 'color': 'orange', 'value': '16'},
+        { 'color': 'black', 'value': '1'},
+        { 'color': 'purple', 'value': '4'},
+      ]);
+      assert.equal(canvas.toDataURL(),
+                   referenceCanvas.toDataURL());
+    });
+
+    test('show ms widgets', function() {
+      var metrics = [
+        {name: 'jank', value: 100},
+      ];
+
+      updateMetrics(metrics);
+      var view = getDeveloperHUD();
+      assert.isDefined(view);
+
+      var canvas = view.querySelector('canvas');
+      var referenceCanvas = getCanvasFor([
+        { 'color': 'cornflowerblue', 'value': '100ms'},
+      ]);
+      assert.equal(canvas.toDataURL(),
+                   referenceCanvas.toDataURL());
+    });
+
+    test('show memory widgets', function() {
+      var prefix = ['','K','M','G','T','P','E','Z','Y'];
+
+      for (var i = 0; i < prefix.length; i++) {
+        var value = 1.1 * Math.pow(1024, i);
+        var metrics = [
+          {'name': 'uss', 'value': value},
+          {'name': 'memory', 'value': value},
+        ];
+
+        updateMetrics(metrics);
+        var view = getDeveloperHUD();
+        assert.isDefined(view);
+
+        var canvas = view.querySelector('canvas');
+
+        while (value > 1024) {
+          value /= 1024;
+        }
+        var referenceValue =
+          (Math.round(value * 100) / 100) + ' ' + prefix[i] + 'B';
+        var referenceCanvas = getCanvasFor([
+          { 'color': 'navy', 'value': referenceValue },
+          { 'color': 'slategrey', 'value': referenceValue },
+        ]);
+        assert.equal(canvas.toDataURL(),
+                     referenceCanvas.toDataURL());
+      }
     });
 
     test('show widgets and tear down', function() {
