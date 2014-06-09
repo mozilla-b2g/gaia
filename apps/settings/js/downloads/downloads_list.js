f0b1c5c9a39d2c0922fed9e01c60d60d5bbaafbf
Guillaume Marty <edo999@gmail.com>
2014-06-09 15:09:42 +0100

    Bug 1022461 - Follow-up to bug 983812, add comments wherever we dispatch

--- apps/settings/js/downloads/downloads_list.js
+++ apps/settings/js/downloads/downloads_list.js
@@ -360,6 +360,9 @@
     isEditMode = true;
     _updateButtonsStatus();
 
+    // We're appending new elements to DOM so to make sure headers are
+    // properly resized and centered, we emmit a lazyload event.
+    // This will be removed when the gaia-header web component lands.
     window.dispatchEvent(new CustomEvent('lazyload', {
       detail: targetHeader
     }));
