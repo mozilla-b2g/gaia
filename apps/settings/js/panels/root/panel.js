9326004377724686b7954e0def42be5b528a48df
gasolin <gasolin@gmail.com>
2014-06-10 17:47:40 +0800

    Bug 998847 - [Settings] refactor storage.js with AMD pattern
    - move dialog into root panel
    - rename id to class
    - fix marionette test
    - un-registered observer when leaving the panel
    - add enabled to observer/unobserve
    - create separated modules for each menu item
    - move usb and media item together
    - fix binding

--- apps/settings/js/panels/root/panel.js
+++ apps/settings/js/panels/root/panel.js
@@ -4,18 +4,38 @@ define(function(require) {
   var SettingsPanel = require('modules/settings_panel');
   var Root = require('panels/root/root');
   var BatteryItem = require('panels/root/battery_item');
+  var StorageUSB = require('panels/root/storage_usb');
+  var StorageApp = require('panels/root/storage_app');
 
   return function ctor_root_panel() {
     var root = Root();
     var batteryItem;
 
+    var storage_usb = StorageUSB();
+    var storage_app = StorageApp();
+
     return SettingsPanel({
       onInit: function rp_onInit(panel) {
+        var storage_elements = {
+          appStorageDesc: panel.querySelector('.application-storage-desc'),
+          mediaStorageDesc: panel.querySelector('.media-storage-desc'),
+          umsEnabledCheckBox: panel.querySelector('.ums-switch-root'),
+          umsEnabledInfoBlock: panel.querySelector('.ums-desc-root'),
+          umsWarningDialog: panel.querySelector('.turn-on-ums-dialog'),
+          umsConfirmButton: panel.querySelector('.ums-confirm-option'),
+          umsCancelButton: panel.querySelector('.ums-cancel-option'),
+          mediaStorageSection: panel.querySelector('.media-storage-section')
+        };
+
         root.init();
+        storage_usb.init(storage_elements);
+        storage_app.init(storage_elements);
         batteryItem = BatteryItem(panel.querySelector('.battery-desc'));
       },
       onBeforeShow: function rp_onBeforeShow() {
         batteryItem.enabled = true;
+        storage_usb.enabled = true;
+        storage_app.enabled = true;
       },
       onShow: function rp_onShow() {
         // XXX: Set data-ready to true to indicate that the first panel is
@@ -26,6 +46,8 @@ define(function(require) {
       },
       onHide: function rp_onHide() {
         batteryItem.enabled = false;
+        storage_usb.enabled = false;
+        storage_app.enabled = false;
       }
     });
   };
