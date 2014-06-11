a9795322c67a3e6c37eaab507e9320d99db6cd6b
crdlc <crdlc@tid.es>
2014-06-11 10:46:42 +0200

    Bug 1022474 - Cannot add an e.me app to the homescreen via wrapper

--- shared/elements/gaia_grid/js/items/bookmark.js
+++ shared/elements/gaia_grid/js/items/bookmark.js
@@ -10,8 +10,9 @@
   /**
    * Represents a single bookmark icon on the homepage.
    */
-  function Bookmark(record) {
+  function Bookmark(record, features) {
     this.detail = record;
+    this.features = features || {};
     this.detail.type = TYPE;
   }
 
@@ -91,7 +92,13 @@
         useAsyncPanZoom: true
       };
 
-      window.open(this.detail.url, '_blank', Object.keys(features)
+      var url = this.detail.url;
+      if (this.features.search) {
+        features.searchName = this.name;
+        features.searchUrl = url;
+      }
+
+      window.open(url, '_blank', Object.keys(features)
         .map(function eachFeature(key) {
         return encodeURIComponent(key) + '=' +
           encodeURIComponent(features[key]);
