0bd6ab8a32bd961682e9753ff2ec6e1a6eb15ce0
David Flanagan <dflanagan@mozilla.com>
2014-06-13 17:47:35 -0700

    Bug 1024692 - camera app is killed during zoom in/zoom out (out of memory) on 256 MB target

--- apps/camera/js/app.js
+++ apps/camera/js/app.js
@@ -17,7 +17,6 @@ var bindAll = require('lib/bind-all');
 var model = require('vendor/model');
 var debug = require('debug')('app');
 var HudView = require('views/hud');
-var Pinch = require('lib/pinch');
 var bind = require('lib/bind');
 
 /**
@@ -58,7 +57,8 @@ function App(options) {
   this.settings = options.settings;
   this.camera = options.camera;
   this.activity = {};
-
+  this.sounds = options.sounds;
+  this.Pinch = options.Pinch;
   //
   // If the system app is opening an attention screen (because
   // of an incoming call or an alarm, e.g.) and if we are
@@ -199,10 +199,6 @@ App.prototype.bindEvents = function() {
   bind(this.win, 'localized', this.firer('localized'));
   bind(this.win, 'beforeunload', this.onBeforeUnload);
   bind(this.el, 'click', this.onClick);
-
-  // Pinch
-  this.pinch = new Pinch(this.el);
-  this.pinch.on('pinchchanged', this.firer('pinchchanged'));
 
   debug('events bound');
 };
