11112d7a395f8caa19a7f501eff4e9513e6616f0
Sergi Mansilla <sergi.mansilla@gmail.com>
2014-06-16 12:20:37 +0300

    Bug 1023888 - When exporting contacts via bluetooth and cancelling, an error screen appears

--- apps/communications/contacts/test/unit/export/bt_test.js
+++ apps/communications/contacts/test/unit/export/bt_test.js
@@ -1,12 +1,7 @@
 'use strict';
-/* global ContactsBTExport */
-/* global MockContactToVcarBlob */
-/* global MockgetDeviceStorage */
-/* global MockGetStorageIfAvailable */
-/* global MockGetUnusedFilename */
-/* global MocksHelper */
-/* global MockMozActivity */
-/* global MockMozL10n */
+/* globals ContactsBTExport, MockContactToVcarBlob, MockgetDeviceStorage,
+ MockGetStorageIfAvailable, MockGetStorageIfAvailableError,
+ MockGetUnusedFilename, MocksHelper, MockMozActivity, MockMozL10n */
 
 requireApp('communications/contacts/js/export/bt.js');
 requireApp('communications/contacts/test/unit/mock_mozActivity.js');
@@ -149,10 +144,102 @@ suite('BT export', function() {
     var contacts = [mockContact1, mockContact2];
     subject.setContactsToExport(contacts);
     subject.cancelExport();
-    subject.doExport(function onFinish(error, exported, msg) {
+    subject.doExport(function onFinish(error, exported, isRecoverable) {
       assert.isNull(error);
       assert.equal(0, exported);
+      assert.equal(isRecoverable, false);
       done();
     });
   });
 });
+
+suite('BT export_error', function() {
+  var real_,
+      realMozL10n,
+      realDeviceStorage,
+      realgetUnusedFilename,
+      realContactToVcardBlob,
+      realgetStorageIfAvailable;
+
+  var mockContact1 = {
+        familyName: ['surname1'],
+        givenName: ['name1']
+      },
+      mockContact2 = {
+        familyName: ['surname2'],
+        givenName: ['name2']
+      };
+
+  var subject,
+      mockProgress,
+      mocksHelper = mocksHelperForExportBT;
+
+  suiteSetup(function() {
+    mocksHelper.suiteSetup();
+
+    // Device storage mock
+    realDeviceStorage = navigator.getDeviceStorage;
+    navigator.getDeviceStorage = MockgetDeviceStorage;
+
+    // L10n mock
+    realMozL10n = navigator.mozL10n;
+    navigator.mozL10n = MockMozL10n;
+
+    real_ = window._;
+    window._ = navigator.mozL10n.get;
+
+    //getStorageIfAvailable mock
+    realgetStorageIfAvailable = window.getStorageIfAvailable;
+    window.getStorageIfAvailable = MockGetStorageIfAvailableError;
+
+    //getUnusedFilename mock
+    realgetUnusedFilename = window.getUnusedFilename;
+    window.getUnusedFilename = MockGetUnusedFilename;
+
+    //ContactToVcardBlob mock
+    realContactToVcardBlob = window.ContactToVcardBlob;
+    window.ContactToVcardBlob = MockContactToVcarBlob;
+
+    if (!window.utils) {
+      window.utils = {};
+    }
+    window.utils.overlay = {
+      hideMenu: function() {}
+    };
+
+    mockProgress = function() {};
+  });
+
+  suiteTeardown(function() {
+    mocksHelper.suiteTeardown();
+    navigator.getDeviceStorage = realDeviceStorage;
+    window._ = real_;
+    window.getStorageIfAvailable = realgetStorageIfAvailable;
+    window.getUnusedFilename = realgetUnusedFilename;
+    window.ContactToVcardBlob = realContactToVcardBlob;
+    window.utils = realUtils;
+  });
+
+  setup(function() {
+    mocksHelper.setup();
+    subject = new ContactsBTExport();
+    subject.setProgressStep(mockProgress);
+  });
+
+  teardown(function() {
+    mocksHelper.teardown();
+  });
+
+  test('Calling with error', function(done) {
+    var contacts = [mockContact1, mockContact2];
+    subject.setContactsToExport(contacts);
+    subject.doExport(function onFinish(error, exported, isRecoverable) {
+      assert.isNotNull(error);
+      assert.equal(error.reason, 'noSpace');
+      assert.equal(0, exported);
+      assert.equal(isRecoverable, false);
+      done();
+    });
+  });
+});
+
