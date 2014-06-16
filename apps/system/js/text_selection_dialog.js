28e867ddcd076aff9306f65a959b5cfc233dee4b
cctuan <georgeiscoming@gmail.com>
2014-06-16 14:20:21 +0800

    Bug 1024933 - [Text Selection] Utility bubble's shadow improvement

--- apps/system/js/text_selection_dialog.js
+++ apps/system/js/text_selection_dialog.js
@@ -158,18 +158,26 @@
 
     var numOfSelectOptions = 0;
     var options = ['SelectAll' , 'Paste', 'Cut', 'Copy'];
+
+    // Based on UI spec, we should have dividers ONLY between each select option
+    // So, we use css to put divider in pseudo element and set the last visible
+    // option without it.
+    var lastVisibleOption;
     options.forEach(function(option) {
       if (detail['can' + option]) {
         numOfSelectOptions++;
-        this.elements[option.toLowerCase()].style.display = '';
+        lastVisibleOption = this.elements[option.toLowerCase()];
+        lastVisibleOption.classList.remove('hidden', 'last-option');
       } else {
-        this.elements[option.toLowerCase()].style.display = 'none';
+        this.elements[option.toLowerCase()].classList.add('hidden');
       }
     }, this);
 
     if (numOfSelectOptions === 0) {
       return;
     }
+    // Add last-option class to the last item of options array;
+    lastVisibleOption.classList.add('last-option');
 
     var pos = this.calculateDialogPostion(detail, numOfSelectOptions);
     this.element.style.top = pos.top + 'px';
