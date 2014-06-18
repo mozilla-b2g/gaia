75b7384aca898ce177be17023d2dc4392e9f10dd
Jon Hylands <jhylands@mozilla.com>
2014-06-18 19:07:17 +0200

    Bug 1023877 - Fixed reference workloads to work on flame. r=dhunt

--- test_media/reference-workload/generateImages.sh
+++ test_media/reference-workload/generateImages.sh
@@ -8,11 +8,19 @@ if [ -z "$1" ]; then
 fi
 
 if [ "$1" != "0" ]; then
-  REMOTE_DIR="/sdcard/DCIM/100MZLLA"
-  adb push ${SCRIPT_DIR}/MasterGalleryImage.jpg ${REMOTE_DIR}/IMG_0001.jpg
+#  REMOTE_DIR="/sdcard/DCIM/100MZLLA"
+  REMOTE_DIR=
+  for dir in /sdcard /storage/sdcard0; do
+    if [ -n "$(adb shell "test -d $dir && echo found")" ]; then
+      REMOTE_DIR=$dir
+      break
+    fi
+  done
+
+  adb push ${SCRIPT_DIR}/MasterGalleryImage.jpg ${REMOTE_DIR}/DCIM/100MZLLA/IMG_0001.jpg
 
   for i in `seq -f '%04g' 2 $1` ; do
     FILENAME=IMG_$i.jpg
-    adb shell "cat ${REMOTE_DIR}/IMG_0001.jpg > ${REMOTE_DIR}/${FILENAME}"
+    adb shell "cat ${REMOTE_DIR}/DCIM/100MZLLA/IMG_0001.jpg > ${REMOTE_DIR}/DCIM/100MZLLA/${FILENAME}"
   done
 fi
