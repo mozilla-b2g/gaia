c72b9a0c77e7e48a7af89e12b09c4e2f58128fb1
Kevin Grandon <kevingrandon@yahoo.com>
2014-06-17 22:32:58 -0700

    Merge pull request #20597 from crdlc/bug-1025441
    Bug 1025441 - [Homescreen] Removed bookmarks show up again when restarting the phone

--- apps/verticalhome/js/sources/bookmark.js
+++ apps/verticalhome/js/sources/bookmark.js
@@ -48,7 +48,7 @@
       }
 
       for (i in allAppBookmarks) {
-        this.removeIconFromGrid(allAppBookmarks[i].detail);
+        this.removeIconFromGrid(allAppBookmarks[i].detail.url);
       }
 
       for (i = 0; i < toAdd.length; i++) {
