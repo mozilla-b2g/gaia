56ac177ac72224e58881ef82ceb606a2131acefe
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-06-19 07:57:54 -0300

    Bug 1022992 - Always show Find My Device's enabled state in the root Settings menu.

--- apps/settings/js/panels/root/panel.js
+++ apps/settings/js/panels/root/panel.js
@@ -6,12 +6,14 @@ define(function(require) {
   var NFCItem = require('panels/root/nfc_item');
   var LanguageItem = require('panels/root/language_item');
   var BatteryItem = require('panels/root/battery_item');
+  var FindMyDeviceItem = require('panels/root/findmydevice_item');
 
   return function ctor_root_panel() {
     var root = Root();
     var nfcItem;
     var languageItem;
     var batteryItem;
+    var findMyDeviceItem;
 
     return SettingsPanel({
       onInit: function rp_onInit(panel) {
@@ -19,10 +21,13 @@ define(function(require) {
         nfcItem = NFCItem(panel.querySelector('.nfc-settings'));
         languageItem = LanguageItem(panel.querySelector('.language-desc'));
         batteryItem = BatteryItem(panel.querySelector('.battery-desc'));
+        findMyDeviceItem = FindMyDeviceItem(
+          panel.querySelector('.findmydevice-desc'));
       },
       onBeforeShow: function rp_onBeforeShow() {
         languageItem.enabled = true;
         batteryItem.enabled = true;
+        findMyDeviceItem.enabled = true;
       },
       onShow: function rp_onShow() {
         // XXX: Set data-ready to true to indicate that the first panel is
@@ -34,6 +39,7 @@ define(function(require) {
       onHide: function rp_onHide() {
         languageItem.enabled = false;
         batteryItem.enabled = false;
+        findMyDeviceItem.enabled = false;
       }
     });
   };
