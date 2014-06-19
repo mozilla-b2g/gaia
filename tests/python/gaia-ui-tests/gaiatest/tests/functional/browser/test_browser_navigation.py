e4fc9f183505843eb976ffc46170103ea655c87e
Dave Hunt <dave.hunt@gmail.com>
2014-06-19 15:50:28 +0100

    Use local content for all applicable browser tests.

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_navigation.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_navigation.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette.by import By
+from marionette import By
 
 from gaiatest import GaiaTestCase
 from gaiatest.apps.browser.app import Browser
@@ -10,45 +10,43 @@ from gaiatest.apps.browser.app import Browser
 
 class TestBrowserNavigation(GaiaTestCase):
 
-    _community_link_locator = (By.CSS_SELECTOR, '#community a')
-    _community_history_section_locator = (By.ID, 'history')
-
     def setUp(self):
         GaiaTestCase.setUp(self)
         self.connect_to_network()
 
+        if self.device.is_desktop_b2g or self.data_layer.is_wifi_connected():
+            self.test_data = {
+                'url': self.marionette.absolute_url('xhtmlTest.html'),
+                'title1': 'XHTML Test Page',
+                'link': (By.ID, 'linkId'),
+                'title2': 'We Arrive Here'}
+        else:
+            self.test_data = {
+                'url': 'http://mozqa.com/data/firefox/layout/mozilla.html',
+                'title1': 'Mozilla',
+                'link': (By.CSS_SELECTOR, '#community a'),
+                'title2': 'Mozilla Community'}
+
     def test_browser_back_button(self):
         browser = Browser(self.marionette)
         browser.launch()
-
-        browser.go_to_url('http://mozqa.com/data/firefox/layout/mozilla.html')
+        browser.go_to_url(self.test_data['url'])
 
         browser.switch_to_content()
-        self.verify_home_page()
-
-        community_link = self.marionette.find_element(*self._community_link_locator)
+        self.assertEqual(self.marionette.title, self.test_data['title1'])
+        link = self.marionette.find_element(*self.test_data['link'])
         # TODO: remove the explicit scroll once bug 833370 is fixed
-        self.marionette.execute_script("arguments[0].scrollIntoView(false);", [community_link])
-        community_link.tap()
+        self.marionette.execute_script(
+            'arguments[0].scrollIntoView(false);', [link])
+        link.tap()
+        self.assertEqual(self.marionette.title, self.test_data['title2'])
 
-        self.verify_community_page()
         browser.switch_to_chrome()
         browser.tap_back_button()
-
         browser.switch_to_content()
-        self.verify_home_page()
+        self.assertEqual(self.marionette.title, self.test_data['title1'])
+
         browser.switch_to_chrome()
         browser.tap_forward_button()
-
         browser.switch_to_content()
-        self.verify_community_page()
-
-    def verify_home_page(self):
-        self.wait_for_element_present(*self._community_link_locator)
-        community_link = self.marionette.find_element(*self._community_link_locator)
-        self.assertTrue(community_link.is_displayed(), 'The community link was not visible at mozilla.html.')
-
-    def verify_community_page(self):
-        self.wait_for_element_present(*self._community_history_section_locator)
-        history_section = self.marionette.find_element(*self._community_history_section_locator)
-        self.assertTrue(history_section.is_displayed(), 'The history section was not visible at mozilla_community.html.')
+        self.assertEqual(self.marionette.title, self.test_data['title2'])
--- tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_tabs.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_tabs.py
@@ -2,10 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette import expected
-from marionette import By
-from marionette import Wait
-
 from gaiatest import GaiaTestCase
 from gaiatest.apps.browser.app import Browser
 
@@ -17,16 +13,9 @@ class TestBrowserTabs(GaiaTestCase):
         self.connect_to_network()
 
         if self.device.is_desktop_b2g or self.data_layer.is_wifi_connected():
-            self.test_data = {
-                'url': self.marionette.absolute_url('rectangles.html'),
-                'locator': (By.ID, 'r1'),
-                'text': 'r1'}
+            self.test_url = self.marionette.absolute_url('xhtmlTest.html')
         else:
-            self.test_data = {
-                'url': 'http://mozqa.com/data/firefox/layout/mozilla.html',
-                'locator': (By.ID, 'page-title'),
-                'text': 'We believe that the internet should be public, open '
-                        'and accessible.'}
+            self.test_url = 'http://mozqa.com/data/firefox/layout/mozilla.html'
 
     def test_browser_tabs(self):
         """ Open a new tab.
@@ -45,14 +34,9 @@ class TestBrowserTabs(GaiaTestCase):
 
         # Add a new tab and load a website.
         browser.tap_add_new_tab_button()
-        browser.go_to_url(self.test_data['url'])
-        browser.switch_to_content()
-        element = Wait(self.marionette).until(
-            expected.element_present(*self.test_data['locator']))
-        self.assertEqual(element.text, self.test_data['text'])
+        browser.go_to_url(self.test_url)
 
         # Assert that the new tab has opened.
-        browser.switch_to_chrome()
         self.assertEqual(browser.displayed_tabs_number, 2)
         # Assert that the displayed tabs number is equal with the actual number of opened tabs.
         self.assertEqual(browser.displayed_tabs_number, browser.tabs_count)
