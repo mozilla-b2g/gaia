297c228016891684dd30dc86a5e8062877920f4e
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-06-19 01:18:09 -0400

    Bug 1014481 - Updated integration and unit test for build/preferences.js

--- build/test/integration/build.test.js
+++ build/test/integration/build.test.js
@@ -290,7 +290,7 @@ suite('Build Integration tests', function() {
       // expected values for prefs and user_prefs
       var expectedUserPrefs = {
         'browser.manifestURL': 'app://system.gaiamobile.org/manifest.webapp',
-        'browser.homescreenURL': 'app://system.gaiamobile.org/index.html',
+        'browser.homescreenURL': 'app://system.gaiamobile.org',
         'network.http.max-connections-per-server': 15,
         'dom.mozInputMethod.enabled': true,
         'ril.debugging.enabled': false,
@@ -500,7 +500,7 @@ suite('Build Integration tests', function() {
         'debugger.remote-mode': 'adb-devtools'
       };
       var expectedUserPrefs = {
-        'browser.startup.homepage': 'app://system.gaiamobile.org/index.html',
+        'browser.startup.homepage': 'app://system.gaiamobile.org',
         'startup.homepage_welcome_url': '',
         'browser.shell.checkDefaultBrowser': false,
         'devtools.toolbox.host': 'side',
