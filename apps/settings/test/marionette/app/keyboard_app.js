c1db798ffc2da504ab4fac9c33a10d171c0eccf9
Rudy Lu <me@rudy.lu>
2014-06-20 17:43:45 +0800

    Merge pull request #20548 from RudyLu/settings/Bug1024695-waitFor_keyboard_settings_frame
    Bug 1024695 - Intermittent keyboard_settings_test.js |
    r=evanxd.
    (cherry picked from commit 7b4665c7521a50e52b2053445d2a93c42a1f4ebd)
    a=test-only

--- apps/settings/test/marionette/app/keyboard_app.js
+++ apps/settings/test/marionette/app/keyboard_app.js
@@ -27,6 +27,43 @@ KeyboardApp.prototype = {
     this.client.switchToFrame();
     var settingsFrame = this.client.findElement(
       'iframe[src*="' + KeyboardApp.SETTINGS_LAUNCH_PATH + '"]');
+
+    /**
+     * XXX: Workaround to wait for the frame is ready after switching to the
+     * keyboard app's settings page. We should use
+     * this.client.apps.switchToApp(KeyboardApp.SETTINGS_LAUNCH_PATH)
+     * instead of the logic below after Bug 1027994 is fixed.
+     *
+     * Add swithToApp waiting logic here, see Bug 1003788 for a similar
+     * technique we implemented in marionette-apps plugin.
+     */
+
+    // Wait for the iframe is rendered.
+    this.client.waitFor(function() {
+      var frameClass = settingsFrame.scriptWith(function(el) {
+        return el.parentNode.getAttribute('class');
+      });
+
+      if (frameClass !== null) {
+        return frameClass.indexOf('render') !== -1;
+      } else {
+        return true;
+      }
+    });
+
+    // Wait for the iframe is displayed on screen.
+    this.client.waitFor(function() {
+      var transitionState = settingsFrame.scriptWith(function(el) {
+        return el.parentNode.getAttribute('transition-state');
+      });
+
+      if (transitionState !== null) {
+        return transitionState === 'opened';
+      } else {
+        return settingsFrame.displayed();
+      }
+    });
+
     this.client.switchToFrame(settingsFrame);
   },
 
