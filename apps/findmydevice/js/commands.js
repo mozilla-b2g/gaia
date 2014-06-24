9d2f7bd16a8dc0c74c97c5a40d2f0731f3dfff4b
KWierso <kwierso@gmail.com>
2014-06-23 17:16:25 -0700

    Revert "Merge pull request #20844 from guilherme-pg/bug1000173-tracking-notification" for gaia-unit test failures
    This reverts commit 7bb5daea0f37d4371e9daa08045d45fef3b30344.

--- apps/findmydevice/js/commands.js
+++ apps/findmydevice/js/commands.js
@@ -1,7 +1,6 @@
 /* global SettingsURL */
 /* global SettingsListener */
 /* global DUMP */
-/* global SettingsHelper */
 
 'use strict';
 
@@ -75,7 +74,6 @@ var Commands = {
     // We actually ignore duration if it's nonzero. If zero, stop
     // tracking.
 
-    SettingsHelper('findmydevice.tracking').set(!!duration);
     if (this._trackIntervalId !== null) {
       if (duration === 0) {
         // stop tracking
