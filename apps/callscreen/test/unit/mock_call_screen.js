48a1cc7ee6740480ea08e9bc6462bdc643c65333
DouglasSherk <github@sherk.me>
2014-06-24 21:46:50 -0400

    Bug 1021175 - [Dialer] Fix incorrect wallpaper shown if the user changes it. r=etienne

--- apps/callscreen/test/unit/mock_call_screen.js
+++ apps/callscreen/test/unit/mock_call_screen.js
@@ -29,9 +29,6 @@ var MockCallScreen = {
   setCallerContactImage: function() {
     this.mSetCallerContactImageCalled = true;
   },
-  setEmergencyWallpaper: function() {
-    this.mSetEmergencyWallpaperCalled = true;
-  },
   mute: function() {
     this.mMuteOn = true;
   },
@@ -124,7 +121,6 @@ var MockCallScreen = {
     this.mEnableKeypadCalled = false;
     this.mSyncSpeakerCalled = false;
     this.mSetCallerContactImageCalled = false;
-    this.mSetEmergencyWallpaperCalled = false;
     this.mMuteOn = false;
     this.mSpeakerOn = false;
     this.mLastRenderMode = null;
