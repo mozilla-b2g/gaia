abd59f36443b4caa16008fafc446152efb635596
Punam Dahiya <punamdahiya@yahoo.com>
2014-06-25 02:20:41 -0700

    Bug1028800-[Gallery]- Fix of gallery pick activity is dismissed when invoking app goes to background r=djf

--- apps/gallery/js/gallery.js
+++ apps/gallery/js/gallery.js
@@ -1035,17 +1035,6 @@ function cleanupPick() {
   setView(LAYOUT_MODE.list);
 }
 
-// XXX If the user goes to the homescreen or switches to another app
-// the pick request is implicitly cancelled
-// Remove this code when https://github.com/mozilla-b2g/gaia/issues/2916
-// is fixed and replace it with an onerror handler on the activity to
-// switch out of pickView.
-window.addEventListener('visibilitychange', function() {
-  if (document.hidden && pendingPick)
-    cancelPick();
-});
-
-
 //
 // Event handlers
 //
