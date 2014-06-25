d4b9a8043dc2ddfce69382e656779abebf210bc0
Sam Foster <sam@sam-i-am.com>
2014-06-25 16:19:00 -0700

    Merge pull request #20995 from sfoster/bug-1023179-tutorial-tests
    Bug 1023179  - Fix FTU video unloading/loading, r=fcampo

--- apps/ftu/js/tutorial.js
+++ apps/ftu/js/tutorial.js
@@ -16,6 +16,9 @@
   // Most used DOM elements
   var dom = {};
 
+  // Track initialized state of tutorial
+  var initialized = false;
+
   var MSG_MIGRATE_CON = 'migrate';
   var TXT_MSG = 'migrate';
 
@@ -65,118 +68,181 @@
     }
   }
 
-  function _setStep(value, callback) {
-    // If value is bigger than the max, show finish screen
-    var stepIndex = value - 1;
-    if (stepIndex >= stepsConfig.steps.length) {
-      Tutorial.done();
-      return;
-    }
-    var stepData = stepsConfig.steps[stepIndex];
-    // Set the step
-    dom.tutorial.dataset.step = currentStep;
-
-    // Internationalize
-    navigator.mozL10n.localize(
-      dom.tutorialStepTitle,
-      stepData.l10nKey
-    );
-
-    // Update the image/video
-    var imgElement = dom.tutorialStepMedia.querySelector('img'),
-        videoElement = dom.tutorialStepMedia.querySelector('video');
-
-    function onVideoLoadOrError(evt) {
-      videoElement.removeEventListener('loadeddata', onVideoLoadOrError);
-      videoElement.removeEventListener('error', onVideoLoadOrError);
-      callback(evt);
-    }
-
-    if (stepData.video) {
-      if (typeof callback === 'function') {
-        videoElement.addEventListener('loadeddata', onVideoLoadOrError, false);
-        videoElement.addEventListener('error', onVideoLoadOrError, false);
+  function _loadMedia(mediaElement, src) {
+    var isVideo = (mediaElement.nodeName === 'VIDEO');
+    return new Promise(function(resolve, reject) {
+      function onMediaLoadOrError(evt) {
+        evt.target.removeEventListener('error', onMediaLoadOrError);
+        if (isVideo) {
+          evt.target.removeEventListener('canplay', onMediaLoadOrError);
+        } else {
+          evt.target.removeEventListener('load', onMediaLoadOrError);
+        }
+        // Dont block progress on failure to load media
+        if (evt.type === 'error') {
+          console.log('Failed to load tutorial media: ' + src);
+        }
+        resolve(evt);
       }
-      videoElement.src = stepData.video;
-      videoElement.hidden = false;
-      imgElement.hidden = true;
-    } else {
-      if (typeof callback === 'function') {
-        imgElement.onload = imgElement.onerror = callback;
+      function onVideoUnloaded(evt) {
+        mediaElement.removeEventListener('emptied', onVideoUnloaded);
+        mediaElement.removeEventListener('abort', onVideoUnloaded);
+        mediaElement.addEventListener('canplay', onMediaLoadOrError);
+        mediaElement.src = src;
+        mediaElement.load();
       }
-      imgElement.hidden = false;
-      imgElement.src = stepData.image;
-      videoElement.hidden = true;
-    }
-    _setProgressBarStep(currentStep);
+      if (isVideo) {
+        // must unload video and force load before switching to new source
+        mediaElement.addEventListener('error', onMediaLoadOrError);
+        if (mediaElement.src) {
+          mediaElement.addEventListener('emptied', onVideoUnloaded, false);
+          mediaElement.addEventListener('abort', onVideoUnloaded, false);
+          mediaElement.removeAttribute('src');
+          mediaElement.load();
+        } else {
+          onVideoUnloaded();
+        }
+      } else {
+        mediaElement.addEventListener('load', onMediaLoadOrError, false);
+        mediaElement.addEventListener('error', onMediaLoadOrError, false);
+        mediaElement.src = src;
+      }
+    });
   }
 
   var elementIDs = [
     'tutorial',
     'tutorial-step-title',
     'tutorial-step-media',
+    'tutorial-step-image',
+    'tutorial-step-video',
     'forward-tutorial',
     'back-tutorial',
     'tutorial-progress',
     'tutorial-progress-bar'
   ];
 
-  var initialized = false;
   var Tutorial = {
     // A configuration object.
     config: null,
 
     init: function(stepsKey, onLoaded) {
-      if (initialized) {
-        if (typeof onLoaded == 'function') {
-          this._configPromise.then(onLoaded);
-        }
+      // init is async
+      // need to load config, then load the first step and its assets.
+
+      if (initialized || this._initialization) {
+        // init already underway
         return;
       }
-      notifyHomescreenApp();
-      // Cache DOM elements
-      elementIDs.forEach(function(name) {
-        dom[Utils.camelCase(name)] = document.getElementById(name);
-      }, this);
 
-      // Init in progress
-      initialized = true;
-
-      this.ensureConfig().then(function initWithConfig() {
-        if (!stepsKey) {
-          stepsKey = 'default';
-        }
-        stepsConfig = this.config[stepsKey] || this.config['default'];
-        // Add event listeners
-        dom.forwardTutorial.addEventListener('click', this);
-        dom.backTutorial.addEventListener('click', this);
-
-        // toggle the layout based number of steps and whether we'll show the
-        // progress bar or not
-        if (stepsConfig.steps.length > 3) {
-          dom.tutorial.dataset.progressbar = true;
-          _initProgressBar();
-        } else {
-          delete dom.tutorial.dataset.progressbar;
-        }
-
-        // Set the first step
-        currentStep = 1;
-        _setStep(currentStep, function onFirstResourceLoaded() {
-          if (typeof onLoaded === 'function') {
-            onLoaded();
-          }
+      var initTasks = this._initialization = new Sequence(
+        // config should load or already be loaded.
+        // failure should abort
+        this.ensureConfig.bind(this),
+        this._initWithConfig.bind(this, stepsKey),
+        // setStep should return promise given by _loadMedia
+        function setInitialStep() {
+          return this._setStep();
+        }.bind(this),
+        function showTutorialAndFinishInit() {
           // Show the panel
           dom.tutorial.classList.add('show');
-        });
+          // Custom event that can be used to apply (screen reader) visibility
+          // changes.
+          window.dispatchEvent(new CustomEvent('tutorialinitialized'));
+        }
+      );
+      initTasks.onabort = function() {
+        Tutorial._initialization = null;
+      };
+      initTasks.oncomplete = function(result) {
+        Tutorial._initialization = null;
+        if(typeof onLoaded === 'function') {
+          onLoaded();
+        }
+      };
 
-        // Custom event that can be used to apply (screen reader) visibility
-        // changes.
-        window.dispatchEvent(new CustomEvent('tutorialinitialized'));
-      }.bind(this), function() {
-        console.log('Tutorial.init: config not loaded');
-      });
+      initTasks.nom = '[initTasks]';
+
+      // first time here: cache DOM elements
+      elementIDs.forEach(function(name) {
+        dom[Utils.camelCase(name)] = document.getElementById(name);
+        if (!dom[Utils.camelCase(name)]) {
+          console.log('Cache DOM elements: couldnt cache: ' + name);
+        }
+      }, this);
+
+      // homescreen notification is out of band and neednt block
+      // the other init steps
+      notifyHomescreenApp();
+
+      initTasks.next();
     },
+
+    _initWithConfig: function(stepsKey) {
+      stepsKey = stepsKey || 'default';
+      stepsConfig = this.config[stepsKey] || this.config['default'];
+
+      // Add event listeners
+      dom.forwardTutorial.addEventListener('click', this);
+      dom.backTutorial.addEventListener('click', this);
+
+      // toggle the layout based number of steps and whether we'll show the
+      // progress bar or not
+      if (stepsConfig.steps.length > 3) {
+        dom.tutorial.dataset.progressbar = true;
+        _initProgressBar();
+      } else {
+        delete dom.tutorial.dataset.progressbar;
+      }
+      // Set the first step
+      currentStep = 1;
+    },
+
+    _setStep: function (value) {
+      // If value is bigger than the max, show finish screen
+      value = (typeof value === 'number') ? value : currentStep;
+      var stepIndex = value - 1;
+      if (stepIndex >= stepsConfig.steps.length) {
+        return Promise.resolve().then(function() {
+          Tutorial.done();
+        });
+      }
+
+      var stepData = stepsConfig.steps[stepIndex];
+      if (!stepData) {
+        return Promise.reject('No data for step: ' + value);
+      }
+      // Set the step
+      dom.tutorial.dataset.step = currentStep;
+
+      // Internationalize
+      navigator.mozL10n.localize(
+        dom.tutorialStepTitle,
+        stepData.l10nKey
+      );
+
+      // Update the image/video
+      var imgElement = dom.tutorialStepImage,
+          videoElement = dom.tutorialStepVideo;
+
+      var stepPromise;
+      if (stepData.video) {
+        stepPromise = _loadMedia(videoElement, stepData.video).then(function(){
+          videoElement.play();
+        });
+        videoElement.hidden = false;
+        imgElement.hidden = true;
+      } else {
+        imgElement.hidden = false;
+        stepPromise = _loadMedia(imgElement, stepData.image);
+        imgElement.hidden = false;
+        videoElement.hidden = true;
+      }
+      _setProgressBarStep(currentStep);
+      return stepPromise;
+    },
+
     handleEvent: function(evt) {
       if (evt.type === 'click') {
         switch(evt.target) {
@@ -190,14 +256,16 @@
       }
     },
     next: function() {
-      _setStep(++currentStep);
+      return this._setStep(++currentStep);
     },
     back: function() {
-      _setStep(--currentStep);
+      return this._setStep(--currentStep);
     },
     done: function() {
       FinishScreen.init();
       dom.tutorial.classList.remove('show');
+      dom.tutorialStepVideo.removeAttribute('src');
+      dom.tutorialStepImage.removeAttribute('src');
     },
     loadConfig: function() {
       if (!this._configPromise) {
@@ -213,7 +281,7 @@
         // or specific features for 'small' & 'medium', we should add more
         // logic here.
 
-        currentLayout = ScreenLayout.getCurrentLayout() == 'tiny' ?
+        currentLayout = ScreenLayout.getCurrentLayout() === 'tiny' ?
                               'tiny' : 'large';
 
         var configUrl = '/config/' + currentLayout + '.json';
@@ -250,21 +318,68 @@
       return this.loadConfig();
     },
     reset: function() {
+      if (this._initialization) {
+        this._initialization.abort();
+        this._initialization = null;
+      }
       if (this._configRequest) {
         this._configRequest.abort();
       }
       this._configPromise = null;
-      if (!initialized) {
-        return;
+      currentStep = 1;
+      stepsConfig = this.config = null;
+      if (initialized) {
+        _setProgressBarStep(currentStep);
+        dom.tutorial.classList.remove('show');
       }
-      currentStep = 0;
-      stepsConfig = Tutorial.config = null;
-      initialized = false;
-      _setProgressBarStep(currentStep);
-      dom.tutorial.classList.remove('show');
     }
   };
 
+  // Flow control for a series of steps that may return promises
+  function Sequence() {
+    var sequence = Array.slice(arguments);
+    var aborted = false;
+    sequence.abort = function() {
+      aborted = true;
+      this.length = 0;
+      if (typeof this.onabort === 'function') {
+        this.onabort();
+      }
+    };
+    sequence.complete = function(result) {
+      if(!aborted && typeof this.oncomplete === 'function') {
+        this.oncomplete(result);
+      }
+    };
+    sequence.fail = function(reason) {
+      this.complete(reason);
+    };
+    sequence.next = function(previousTaskResult) {
+      var result, exception;
+      if (aborted) {
+        return;
+      }
+      var task = this.shift();
+      if (task) {
+        try {
+          result = task.apply(null, arguments);
+        } catch(e) {
+          exception = e;
+        }
+        if (exception) {
+          this.fail(exception);
+        } else if (result && typeof result.then === 'function') {
+          result.then(this.next.bind(this), this.fail.bind(this));
+        } else {
+          this.next(result);
+        }
+      } else {
+        this.complete(previousTaskResult);
+      }
+    };
+    return sequence;
+  }
+
   exports.Tutorial = Tutorial;
 
-}(this));
+})(this);
