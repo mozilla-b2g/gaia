6437cff5d8389900358ad0a9c706e1d823a388cc
yu-nagai <yu-nagai@kddi-tech.com>
2014-06-25 15:47:22 +0900

    Bug997547

--- apps/communications/contacts/test/unit/contacts_activities_test.js
+++ apps/communications/contacts/test/unit/contacts_activities_test.js
@@ -289,6 +289,21 @@ suite('Test Activities', function() {
       // Mock returns always the first option from the select
       assert.equal(result.email, contact.email[0].value);
     });
+    test('webcontacts/sms', function() {
+      activity.source.data.type = 'webcontacts/sms';
+      ActivityHandler._currentActivity = activity;
+      ActivityHandler.dataPickHandler(contact);
+      assert.isFalse(ConfirmDialog.showing);
+      assert.equal(result.number, contact.tel[0].value);
+    });
+
+    test('webcontacts/mms', function() {
+      activity.source.data.type = 'webcontacts/mms';
+      ActivityHandler._currentActivity = activity;
+      ActivityHandler.dataPickHandler(contact);
+      assert.isFalse(ConfirmDialog.showing);
+      assert.equal(result.email, contact.email[0].value);
+    });
   });
 
 });
