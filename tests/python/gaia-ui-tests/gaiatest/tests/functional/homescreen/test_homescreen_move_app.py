e89f52998fd2195e24708146b6f8da62bcabe53a
askeing <askeing@gmail.com>
2014-06-25 11:25:04 +0800

    Revert "Revert "Bug 1027144 - Re-enable test_homescreen_move_app.py" for intermittent test failures"
    This reverts commit a0807151b0b28feb59b323a30b1f74dc8801be32.
    
    Conflicts:
    
    	tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/manifest.ini

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/test_homescreen_move_app.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/test_homescreen_move_app.py
@@ -22,13 +22,14 @@ class TestMoveApp(GaiaTestCase):
 
         first_app_before_move = self.homescreen.visible_apps[0].name
 
-        # Activate edit mode
+        # Assert that we are not in edit mode.
         self.assertFalse(self.homescreen.is_edit_mode_active, "Edit mode should not be active")
-        self.homescreen.activate_edit_mode()
-        self.assertTrue(self.homescreen.is_edit_mode_active, "Edit mode should be active")
 
-        # Move first app to position 12
-        self.homescreen.move_app_to_position(0, 3)
+        # Move first app to position 3 (index 2)
+        self.homescreen.move_app_to_position(0, 2)
+
+        # Assert that we are in edit mode.
+        self.assertTrue(self.homescreen.is_edit_mode_active, "Edit mode should be active")
 
         # Exit edit mode
         self.device.touch_home_button()
@@ -37,4 +38,4 @@ class TestMoveApp(GaiaTestCase):
         # Check the app order and that the app on position 12 is the right one
         first_app_after_move = self.homescreen.visible_apps[0].name
         self.assertNotEqual(first_app_before_move, first_app_after_move)
-        self.assertEqual(first_app_before_move, self.homescreen.visible_apps[3].name)
+        self.assertEqual(first_app_before_move, self.homescreen.visible_apps[2].name)
