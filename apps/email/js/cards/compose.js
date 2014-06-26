297a069d987abe220dd4b925d44e52474c6a9670
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2014-06-26 14:48:39 -0400

    Revert "Merge pull request #20828 from jrburke/bug1025727-email-cronsync-failsafe"
    This reverts commit e68f589db4de9355c3ab1896e6f70d63d7d82809.

--- apps/email/js/cards/compose.js
+++ apps/email/js/cards/compose.js
@@ -6,7 +6,7 @@
 
 /*jshint browser: true */
 /*global define, console, MozActivity, alert */
-define(function(require, exports, module) {
+define(function(require) {
 
 var templateNode = require('tmpl!./compose.html'),
     cmpAttachmentItemNode = require('tmpl!./cmp/attachment_item.html'),
@@ -24,8 +24,7 @@ var templateNode = require('tmpl!./compose.html'),
 
     prettyFileSize = common.prettyFileSize,
     Cards = common.Cards,
-    ConfirmDialog = common.ConfirmDialog,
-    dataIdCounter = 0;
+    ConfirmDialog = common.ConfirmDialog;
 
 /**
  * Max composer attachment size is defined as 5120000 bytes.
@@ -161,14 +160,6 @@ function ComposeCard(domNode, mode, args) {
       this.sentAudioEnabled = e.settingValue;
     }).bind(this));
   }
-
-  // Set up unique data IDs for data-sensitive operations that could be in
-  // progress. These IDs are unique per kind of action, not unique per instance
-  // of a kind of action. However, these IDs are just used to know if a hard
-  // shutdown should be delayed a bit, and are unique enough for those purposes.
-  var dataId = module.id + '-' + (dataIdCounter += 1);
-  this._dataIdSaveDraft = dataId + '-saveDraft';
-  this._dataIdSendEmail = dataId + '-sendEmail';
 }
 ComposeCard.prototype = {
 
@@ -389,13 +380,7 @@ ComposeCard.prototype = {
       return;
     }
     this._saveStateToComposer();
-    evt.emit('uiDataOperationStart', this._dataIdSaveDraft);
-    this.composer.saveDraft(function() {
-      evt.emit('uiDataOperationStop', this._dataIdSaveDraft);
-      if (callback) {
-        callback();
-      }
-    }.bind(this));
+    this.composer.saveDraft(callback);
   },
 
   createBubbleNode: function(name, address) {
@@ -812,7 +797,6 @@ ComposeCard.prototype = {
 
     // Initiate the send.
     console.log('compose: initiating send');
-    evt.emit('uiDataOperationStart', this._dataIdSendEmail);
     this.composer.finishCompositionSendMessage(
       function callback(error , badAddress, sentDate) {
         // Card could have been destroyed in the meantime,
@@ -858,7 +842,6 @@ ComposeCard.prototype = {
           self.sentAudio.play();
         }
 
-        evt.emit('uiDataOperationStop', this._dataIdSendEmail);
         activityHandler();
         this._closeCard();
       }.bind(this)
