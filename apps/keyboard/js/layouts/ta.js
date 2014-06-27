896f64a3572b6a6af08a27effb52e4a569d1e0ce
Arun Prakash <arunprakash.pts@gmail.com>
2014-06-27 21:31:48 +0530

    ta-layout

--- apps/keyboard/js/layouts/ta.js
+++ apps/keyboard/js/layouts/ta.js
@@ -1,60 +1,102 @@
 Keyboards.ta = {
   label: 'Tamil',
-  shortLabel: 'Ta',
+  shortLabel: 'த',
   imEngine: 'india',
   menuLabel: 'தமிழ்',
   secondLayout: true,
-  basicLayoutKey: 'அஆஇ',
+  basicLayoutKey: 'த',
   types: ['text', 'url', 'email'],
   keyClassName: 'tamil',
-  upperCase: {
-    'ஒ':'ொ',
-    'ஔ':'ௌ',
-    'ஐ':'ை',
-    'ஶ':'ா',
-    'ஆ':'ீ',
-    'ஈ':'ூ',
-    'ப':'ஊ',
-    'ஹ':'ங',
-    'ஜ':'ஂ',
-    'ஞ':'ௗ',
+width: 11,
 
-    'ஓ':'ோ',
-    'ஏ':'ே',
-    'அ':'்',
-    'இ':'ி',
-    'உ':'ு',
-    'ர':'ற',
-    'க':'&',
-    'த':'ஃ',
-    'ச':'?',
-    'ட':'!',
-
-    'எ':'ெ',
-    'ம':'ண',
-    'ந':'ன',
-    'வ':'ழ',
-    'ல':'ள',
-    'ஸ':'ஷ',
-    'ய':'@'
+upperCase:  {
+'ஆ' : '௧',
+'ஈ' : '௨',
+'ஊ' : '௩', 
+'ஏ' : '௪',
+'ள' : '௫',
+'ற' : '௬',
+'ன' : '௭',
+'ட' : '௮',
+'ண' : '௯',
+'ச' : '௦',
+'ஞ' : '௰',
+   
+'அ' : 'ஃ',
+'இ' : 'ஸ',
+'உ' : 'ஷ',
+'ஐ' : 'ஜ',
+'எ' : 'ஹ',
+'க' : 'க்ஷ',
+'ப' : 'ஸ்ரீ',
+'ம' : 'ஶ',
+'த' : 'ௐ',
+'ந' : '௱',
+'ய' : '௲',
+    
+'ஔ' : '௳',
+'ஓ' : '௴',
+'ஒ' : '௵',
+'வ' : '௶',
+'ங' : '௷',
+'ல' : '௸',
+'ர' : '௹',
+'ழ' : '௺'
+  },
+  alt: {
+'ஆ' : '௧',
+'ஈ' : '௨',
+'ஊ' : '௩', 
+'ஏ' : '௪',
+'ள' : '௫',
+'ற' : '௬',
+'ன' : '௭',
+'ட' : '௮',
+'ண' : '௯',
+'ச' : '௦',
+'ஞ' : '௰',
+   
+'அ' : 'ஃ',
+'இ' : 'ஸ',
+'உ' : 'ஷ',
+'ஐ' : 'ஜ',
+'எ' : 'ஹ',
+'க' : 'க்ஷ',
+'ப' : 'ஸ்ரீ',
+'ம' : 'ஶ',
+'த' : 'ௐ',
+'ந' : '௱',
+'ய' : '௲',
+    
+'ஔ' : '௳',
+'ஓ' : '௴',
+'ஒ' : '௵',
+'வ' : '௶',
+'ங' : '௷',
+'ல' : '௸',
+'ர' : '௹',
+'ழ' : '௺'
   },
   keys: [
     [
-      { value: 'ஒ' }, { value: 'ஔ' }, { value: 'ஐ' }, { value: 'ஶ' },
-      { value: 'ஆ' }, { value: 'ஈ' }, { value: 'ப' }, { value: 'ஹ' },
-      { value: 'ஜ' }, { value: 'ஞ' }
+      { value: 'ஆ' }, { value: 'ஈ' }, { value: 'ஊ' }, 
+      { value: 'ஏ' }, { value: 'ள' }, { value: 'ற' }, { value: 'ன' },
+      { value: 'ட' }, { value: 'ண' }, { value: 'ச' },{ value: 'ஞ' }
     ], [
-      { value: 'ஓ' }, { value: 'ஏ' }, { value: 'அ' }, { value: 'இ' },
-      { value: 'உ' }, { value: 'ர' }, { value: 'க' }, { value: 'த' },
-      { value: 'ச' }, { value: 'ட' }
+      { value: 'அ' }, { value: 'இ' }, { value: 'உ' }, { value: 'ஐ' },
+      { value: 'எ' }, { value: 'க' }, { value: 'ப' }, { value: 'ம' },
+      { value: 'த' }, { value: 'ந' }, { value: 'ய' }
     ], [
       { value: '⇪', ratio: 1.5, keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
-      { value: 'எ' }, { value: 'ம' }, { value: 'ந' }, { value: 'வ' },
-      { value: 'ல' }, { value: 'ஸ' }, { value: 'ய' },
+      { value: 'ஔ' }, { value: 'ஓ' }, { value: 'ஒ' }, { value: 'வ' },
+      { value: 'ங' }, { value: 'ல' }, { value: 'ர' },{ value: 'ழ' },
       { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
     ], [
-      { value: '&nbsp', ratio: 8, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '&nbsp', ratio: 7, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '்' },
       { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
     ]
   ]
+
+
 };
