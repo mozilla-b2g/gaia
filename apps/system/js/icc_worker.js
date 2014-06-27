alivedise/system

Bug 1015833 - [PTCRB][STK]27.22.4.1.1/2 DISPLAY TEXT, normal priority, Unpacked 8 bit data for Text String, screen busy case failed, r=vingtetun

js/icc_worker.js
@@ -227,6 +227,19 @@ var icc_worker = {
   '0x21': function STK_CMD_DISPLAY_TEXT(message) {
     DUMP('STK_CMD_DISPLAY_TEXT:', message.command.options);
     var options = message.command.options;
+
+    // Check if device is idle
+    var activeApp = AppWindowManager.getActiveApp();
+    if (!options.isHighPriority && !activeApp.isHomescreen) {
+      DUMP('Do not display the text because normal priority.');
+      icc.responseSTKCommand(message, {
+        resultCode:
+          icc._iccManager.STK_RESULT_TERMINAL_CRNTLY_UNABLE_TO_PROCESS,
+        additionalInformation: 0x01
+      });
+      return;
+    }
+
     if (options.responseNeeded) {
       icc.responseSTKCommand(message, {
         resultCode: icc._iccManager.STK_RESULT_OK

mozilla-b2g/gaia

Conflicts:
	apps/system/js/icc_worker.js

apps/system/js/icc_worker.js
@@ -217,6 +217,19 @@ var icc_worker = {
   '0x21': function STK_CMD_DISPLAY_TEXT(command, iccManager) {
     DUMP('STK_CMD_DISPLAY_TEXT:', command.options);
     var options = command.options;
+
+    // Check if device is idle
+    var activeApp = AppWindowManager.getActiveApp();
+    if (!options.isHighPriority && !activeApp.isHomescreen) {
+      DUMP('Do not display the text because normal priority.');
+      iccManager.responseSTKCommand({
+        resultCode:
+          iccManager._iccManager.STK_RESULT_TERMINAL_CRNTLY_UNABLE_TO_PROCESS,
+        additionalInformation: 0x01
+      });
+      return;
+    }
+
     if (options.responseNeeded) {
       iccManager.responseSTKCommand({
         resultCode: iccManager._iccManager.STK_RESULT_OK