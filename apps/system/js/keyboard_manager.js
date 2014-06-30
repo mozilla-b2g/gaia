6ba0bc68738f70edc42d496cfede5ea09af334d4
John Lu [:mnjul] <jlu@mozilla.com>
2014-06-30 17:59:10 +0800

    Revert "Merge pull request #20353 from timdream/input-switch-flash"
    This reverts commit c0d1670ba57666210a7e7fd021ca4d9b47ab990c, reversing
    changes made to 56812bb7f5234088d6802361190411cdab796868.

--- apps/system/js/keyboard_manager.js
+++ apps/system/js/keyboard_manager.js
@@ -601,26 +601,19 @@ var KeyboardManager = {
   switchToNext: function km_switchToNext() {
     clearTimeout(this.switchChangeTimeout);
 
+    var self = this;
     var showed = this.showingLayout;
 
     this.switchChangeTimeout = setTimeout(function keyboardSwitchLayout() {
-      if (!this.keyboardLayouts[showed.type]) {
+      if (!self.keyboardLayouts[showed.type]) {
         showed.type = 'text';
       }
-      var length = this.keyboardLayouts[showed.type].length;
+      var length = self.keyboardLayouts[showed.type].length;
       var index = (showed.index + 1) % length;
-      this.keyboardLayouts[showed.type].activeLayout = index;
-
-      var nextLayout = this.keyboardLayouts[showed.type][index];
-      // Only resetShowingKeyboard() if the running layout is not the same app
-      // to prevent flash of black when switching.
-      if (showed.frame.dataset.frameManifestURL !==
-          nextLayout.manifestURL) {
-        this.resetShowingKeyboard();
-      }
-
-      this.setKeyboardToShow(showed.type, index);
-    }.bind(this), SWITCH_CHANGE_DELAY);
+      self.keyboardLayouts[showed.type].activeLayout = index;
+      self.resetShowingKeyboard();
+      self.setKeyboardToShow(showed.type, index);
+    }, SWITCH_CHANGE_DELAY);
   },
 
   // Show the input method menu
