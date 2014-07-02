9633afc864758f140f33f8e9ffa04b6b00d80af7
Alive Kuo <alegnadise@gmail.com>
2014-07-02 17:12:01 +0800

    Bug 1032068 - Maintain id pool of activity chaining instances to keep only one chaining

--- apps/system/test/unit/mock_app_window.js
+++ apps/system/test/unit/mock_app_window.js
@@ -14,7 +14,7 @@
       }
       this.config = config;
     }
-    this.instanceID = _id++;
+    this.instanceID = 'mock-app-' + _id++;
     this.groupID = this.instanceID;
     MockAppWindowHelper.mInstances.push(this);
     MockAppWindowHelper.mLatest = this;
