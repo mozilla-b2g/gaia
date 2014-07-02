a62933eb3c542f22d71c766b2c5099d882610658
Miller Medeiros <miller@millermedeiros.com>
2014-07-02 12:21:46 -0300

    Bug 1030552 - [Calendar] "All day" on month view is not localized for All day events

--- apps/calendar/js/templates/months_day.js
+++ apps/calendar/js/templates/months_day.js
@@ -18,7 +18,8 @@
 
       this.eventTime = function() {
         return this.h('isAllDay') ?
-          '<div class="all-day">All Day</div>' :
+          ('<div class="all-day" data-l10n-id="hour-allday">' +
+          navigator.mozL10n.get('hour-allday') + '</div>') :
           ('<div class="start-time">' + this.h('startTime') + '</div>' +
            '<div class="end-time">' + this.h('endTime') + '</div>');
       };
