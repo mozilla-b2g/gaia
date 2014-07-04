3f4c635106c5364228782d12b1cb76b0c105b971
Anthony Ricaud <anthony@ricaud.me>
2014-07-04 14:44:51 +0200

    Bug 1034279 - USSD messages not displayed if user leaves Dialer while sending r=etienne
    I took the opportunity to re-enable a unit test
    
    (cherry picked from commit 46bad9d7f31cbc79a4551b011529156ba8f1f8c7)
    
    Signed-off-by: Ryan VanderMeulen <rvandermeulen@mozilla.com>

--- apps/communications/dialer/js/mmi.js
+++ apps/communications/dialer/js/mmi.js
@@ -331,7 +331,6 @@ var MmiManager = {
             var iconURL = NotificationHelper.getIconURI(app, 'dialer');
             var clickCB = function(evt) {
               evt.target.close();
-              app.launch('dialer');
               self.handleMMIReceived(message, /* sessionEnded */ true,
                                      cardIndex);
             };
