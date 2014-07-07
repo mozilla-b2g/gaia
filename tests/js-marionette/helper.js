df37225bc64e229efa493dc3ec0f844457375bd6
Hubert Figuière <hub@figuiere.net>
2014-07-07 10:23:08 -0400

    Bug 1027232 - Wait for the homescreen to be ready

--- tests/js-marionette/helper.js
+++ tests/js-marionette/helper.js
@@ -5,8 +5,22 @@ var fs = require('fs');
 var Helper = {
 
   unlockScreen: function(client) {
-    client.executeScript(fs.readFileSync('./tests/atoms/gaia_lock_screen.js') +
-                         'GaiaLockScreen.unlock();\n');
+    // wait that the lockscreen is ready
+    client.waitFor(function () {
+      return client.executeScript(function () {
+        if (!window || !window.wrappedJSObject.lockScreenWindowManager) {
+          return true;
+        }
+
+        var wrappedObject = window.wrappedJSObject;
+        var lockScreen = wrappedObject.lockScreen || wrappedObject.LockScreen;
+
+        return typeof lockScreen.unlock === 'function';
+      });
+    });
+    client.executeScript(
+      fs.readFileSync('./tests/atoms/gaia_lock_screen.js') +
+        'GaiaLockScreen.unlock();\n');
   },
 
   delay: function(client, interval, givenCallback) {
