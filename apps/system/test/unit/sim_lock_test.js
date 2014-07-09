f3e59e430b8a1d0a41ffba5045e57e2d72d58ac1
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-09 16:46:16 -0700

    Bug 1031369 - Show System SIM PIN dialog on upgrade after tutorial

--- apps/system/test/unit/sim_lock_test.js
+++ apps/system/test/unit/sim_lock_test.js
@@ -1,16 +1,19 @@
 /* global SimLock, MockL10n, MocksHelper, SimPinDialog, MockSIMSlotManager */
-/* global preInit */
+/* global preInit, VersionHelper:true */
+
 'use strict';
 
 requireApp('system/shared/test/unit/mocks/mock_simslot_manager.js');
 requireApp('system/test/unit/mock_simcard_dialog.js');
 require('/shared/test/unit/mocks/mock_l10n.js');
 require('/shared/test/unit/mocks/mock_system.js');
+requireApp('system/test/unit/mock_version_helper.js');
 
 var mocksHelperForSimLock = new MocksHelper([
   'SimPinDialog',
   'SIMSlotManager',
-  'System'
+  'System',
+  'VersionHelper'
 ]).init();
 
 suite('SimLock', function() {
@@ -49,18 +52,35 @@ suite('SimLock', function() {
     });
   });
 
-  suite('when we are in ftu', function() {
+  suite('when we are in ftu on first use', function() {
     setup(function() {
       this.sinon.stub(SimPinDialog, 'close');
       this.sinon.stub(SimLock, 'showIfLocked');
-
       SimLock.init();
-      window.dispatchEvent(new window.CustomEvent('ftuopen'));
     });
 
-    test('no simpin dialog would show up', function() {
+    test('no simpin dialog would show up on first run', function() {
+      VersionHelper = window.MockVersionHelper(false);
+      window.dispatchEvent(new window.CustomEvent('ftuopen'));
+      VersionHelper.getVersionInfo();
+      VersionHelper.resolve({ isUpgrade: function () {
+                                return false;
+                              }
+                            });
       assert.isTrue(SimPinDialog.close.called);
     });
+
+    test('simpin dialog would show up on upgrade', function() {
+      VersionHelper = window.MockVersionHelper(true);
+      window.dispatchEvent(new window.CustomEvent('ftuopen'));
+      VersionHelper.getVersionInfo();
+      VersionHelper.resolve({ isUpgrade: function () {
+                                return true;
+                              }
+                            });
+      assert.isFalse(SimPinDialog.close.called);
+    });
+
   });
 
   suite('to test events', function() {
