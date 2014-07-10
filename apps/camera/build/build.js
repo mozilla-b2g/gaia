08a356a30dda21c63585f639a97c181962774c0a
Yuren Ju <yurenju@gmail.com>
2014-07-10 10:34:08 +0800

    Revert "addressed nit"
    This reverts commit 977fa6a83a866dd09f3c1c94718ac4c2c61d92e1.

--- apps/camera/build/build.js
+++ apps/camera/build/build.js
@@ -14,12 +14,16 @@ function optimize(options) {
 function copyUserConfig(options) {
   var targetFile = utils.getFile(options.STAGE_APP_DIR, 'js', 'config',
     'config.js');
-  var [parent, filename] = [targetFile.parent.path, targetFile.leafName];
 
   if (options.GAIA_DISTRIBUTION_DIR) {
     var distConfig = utils.getFile(options.GAIA_DISTRIBUTION_DIR,
       'camera-config.js');
-    utils.copyFileTo(distConfig, parent, filename, true);
+    if (distConfig.exists()) {
+      if (targetFile.exists()) {
+        targetFile.remove(false);
+      }
+      distConfig.copyTo(targetFile.parent, targetFile.leafName);
+    }
   }
 }
 
