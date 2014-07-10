bb5f4ba7372458bb4a446ae6ccfefa8e3f76c345
Timothy Guan-tin Chien <timdream@gmail.com>
2014-07-10 18:03:29 +0800

    Bug 957031 - 100ms delay for keyboard key highlight

--- apps/keyboard/test/unit/keyboard/visual_highlight_manager_test.js
+++ apps/keyboard/test/unit/keyboard/visual_highlight_manager_test.js
@@ -24,6 +24,9 @@ suite('VisualHighlightManager', function() {
 
     manager = new VisualHighlightManager(app);
     manager.start();
+
+    this.sinon.stub(window, 'setTimeout');
+    this.sinon.stub(window, 'clearTimeout');
   });
 
   teardown(function() {
@@ -47,8 +50,50 @@ suite('VisualHighlightManager', function() {
       .calledWith(target, { showUpperCase: true }));
   });
 
+  test('show after hide', function() {
+    manager.hide(target);
+
+    var target2 = {};
+
+    app.isCapitalized.returns(false);
+    manager.show(target2);
+
+    assert.isTrue(window.IMERender.unHighlightKey.calledWith(target),
+      'The first target highlight should be hidden immediately.');
+
+    assert.isTrue(window.IMERender.highlightKey
+      .calledWith(target2, { showUpperCase: false }));
+  });
+
   test('hide', function() {
     manager.hide(target);
+
+    assert.isTrue(window.setTimeout.calledOnce);
+    assert.equal(window.setTimeout.getCall(0).args[1],
+      manager.HIGHTLIGHT_DELAY_MS);
+
+    window.setTimeout.getCall(0).args[0].call(window);
+
+    assert.isTrue(window.IMERender.unHighlightKey.calledWith(target));
+  });
+
+  test('hide twice within HIGHTLIGHT_DELAY_MS', function() {
+    window.setTimeout.returns(200);
+
+    manager.hide(target);
+
+    assert.isTrue(window.setTimeout.calledOnce);
+    assert.equal(window.setTimeout.getCall(0).args[1],
+      manager.HIGHTLIGHT_DELAY_MS);
+
+    window.setTimeout.returns(201);
+
+    manager.hide(target);
+
+    assert.isTrue(window.clearTimeout.calledWith(200));
+    assert.isTrue(window.setTimeout.calledTwice);
+
+    window.setTimeout.getCall(1).args[0].call(window);
 
     assert.isTrue(window.IMERender.unHighlightKey.calledWith(target));
   });
