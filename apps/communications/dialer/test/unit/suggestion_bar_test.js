04f994782d913db53b7ceb5d2e6c51a18e55a815
DouglasSherk <github@sherk.me>
2014-07-15 14:14:13 -0400

    Bug 1035183 - [Dialer] Make matched suggestions not appear when there are more than 50, remove + indicator. r=Rik
    Conflicts:
    	apps/communications/dialer/js/suggestion_bar.js

--- apps/communications/dialer/test/unit/suggestion_bar_test.js
+++ apps/communications/dialer/test/unit/suggestion_bar_test.js
@@ -173,6 +173,9 @@ suite('suggestion Bar', function() {
     this.sinon.stub(MockLazyL10n, 'get', function(callback) {
       callback(mozL10nGet);
     });
+
+    MockContacts.mTearDown();
+    MockFbContacts.mTeardown();
   });
 
   teardown(function() {
@@ -188,6 +191,12 @@ suite('suggestion Bar', function() {
     navigator.mozIccManager = realMozIccManager;
   });
 
+  var cloneMockContactResults = function(count) {
+    MockContacts.mResult = new Array(count);
+    for (var i = 0; i < MockContacts.mResult.length; i++) {
+      MockContacts.mResult[i] = mockResult1[0];
+    }
+  };
 
   suite('Suggestion Bar', function() {
     test('#update suggestions by contact data - 1 data', function() {
@@ -301,6 +310,37 @@ suite('suggestion Bar', function() {
         assert.isFalse(domSuggestionBar.hidden, 'should show suggestionBar');
     });
 
+    test('#update suggestions by contact data - 50 local data - 0 FB data',
+      function() {
+        var enteredNumber = '1234';
+        cloneMockContactResults(50);
+        subject.update(enteredNumber);
+
+        assert.isFalse(domSuggestionBar.hidden, 'should show suggestionBar');
+        assert.equal(domSuggestionBar.getAttribute('aria-hidden'), 'false');
+    });
+
+    test('#update suggestions by contact data - 51 local data - 0 FB data',
+      function() {
+        var enteredNumber = '1234';
+        cloneMockContactResults(51);
+        subject.update(enteredNumber);
+
+        assert.isTrue(domSuggestionBar.hidden, 'should hide suggestionBar');
+        assert.equal(domSuggestionBar.getAttribute('aria-hidden'), 'true');
+    });
+
+    test('#update suggestions by contact data - 50 local data - 1 FB data',
+      function() {
+        var enteredNumber = '1234';
+        cloneMockContactResults(50);
+        MockFbContacts.mResult = mockResultFb.slice(0, 1);
+        subject.update(enteredNumber);
+
+        assert.isTrue(domSuggestionBar.hidden, 'should hide suggestionBar');
+        assert.equal(domSuggestionBar.getAttribute('aria-hidden'), 'true');
+    });
+
     suite('#clear suggestions', function() {
       setup(function() {
         var enteredNumber = '1234';
@@ -353,6 +393,14 @@ suite('suggestion Bar', function() {
   suite('Suggestion List', function() {
     suite('show overlay', function() {
       var suggestions;
+
+      var getSuggestions = function() {
+        suggestions = Array.prototype.filter.call(subject.list.children,
+          function(element) {
+            return element.classList.contains('js-suggestion-item');
+          });
+      };
+
       setup(function() {
         MockContacts.mResult = mockResult2;
         subject.update('1111');
@@ -361,10 +409,7 @@ suite('suggestion Bar', function() {
         this.sinon.spy(LazyLoader, 'load');
 
         subject.showOverlay();
-        suggestions = Array.prototype.filter.call(subject.list.children,
-          function(element) {
-            return element.classList.contains('js-suggestion-item');
-          });
+        getSuggestions();
       });
 
       test('should load the overlay', function() {
@@ -417,6 +462,27 @@ suite('suggestion Bar', function() {
         suggestions.forEach(function(suggestion) {
           assert.equal(suggestion.querySelector('.si__mark').textContent,
                        '1111');
+        });
+      });
+
+      suite('high load', function() {
+        test('should show 50 suggestions', function() {
+          cloneMockContactResults(50);
+          subject.update('1234');
+          subject.showOverlay();
+          getSuggestions();
+
+          assert.equal(suggestions.length, 50);
+        });
+
+        test('should show 50 suggestions with 1 FB contact', function() {
+          cloneMockContactResults(49);
+          MockFbContacts.mResult = mockResultFb.slice(0, 1);
+          subject.update('1234');
+          subject.showOverlay();
+          getSuggestions();
+
+          assert.equal(suggestions.length, 50);
         });
       });
     });
