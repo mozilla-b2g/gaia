c02f9666687de954b408797b633f8b63809bd36d
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-16 00:59:07 -0700

    Merge pull request #21557 from qdot/1031369-tutorial-sim-pin-upgrade
    Bug 1031369 - Show System SIM PIN dialog on upgrade after tutorial
    Conflicts:
    	apps/system/test/unit/sim_lock_test.js

--- apps/system/js/sim_lock.js
+++ apps/system/js/sim_lock.js
@@ -56,7 +56,11 @@ var SimLock = {
   handleEvent: function sl_handleEvent(evt) {
     switch (evt.type) {
       case 'ftuopen':
-        SimPinDialog.close();
+        VersionHelper.getVersionInfo().then(function(info) {
+          if (!info.isUpgrade()) {
+            SimPinDialog.close();
+          }
+        });
         break;
       case 'simpinback':
         var index = evt.detail._currentSlot.index;
@@ -149,8 +153,13 @@ var SimLock = {
       return false;
 
     // FTU has its specific SIM PIN UI
-    if (FtuLauncher.isFtuRunning())
-      return false;
+    if (FtuLauncher.isFtuRunning()) {
+      VersionHelper.getVersionInfo().then(function(info) {
+        if (!info.isUpgrade()) {
+          SimPinDialog.close();
+        }
+      });
+    }
 
     if (this._duringCall) {
       this._showPrevented = true;
