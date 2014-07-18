22753584077b7020f5e16400d8759a3cb55a40e4
Mark Goodwin <mgoodwin@mozilla.com>
2014-07-18 14:21:55 +0100

    Bug 1034570 - Notify the user if initial registration fails r=

--- apps/system/js/findmydevice_launcher.js
+++ apps/system/js/findmydevice_launcher.js
@@ -1,6 +1,8 @@
 /* global DUMP */
 /* global SettingsHelper */
 /* global wakeUpFindMyDevice */
+/* global Notification */
+/* global MozActivity */
 /* global IAC_API_WAKEUP_REASON_ENABLED_CHANGED */
 /* global IAC_API_WAKEUP_REASON_LOGIN */
 /* global IAC_API_WAKEUP_REASON_LOGOUT */
@@ -8,10 +10,45 @@
 
 'use strict';
 
+const FIND_MY_DEVICE_RETRIES = 5;
+const FIND_MY_DEVICE_TAG = 'findmydevice.enable-failed';
+
 navigator.mozSettings.addObserver('findmydevice.enabled', function(event) {
   // make sure Find My Device is registered if it's enabled,
   // and that it notifies the server if disabled
   wakeUpFindMyDevice(IAC_API_WAKEUP_REASON_ENABLED_CHANGED);
+});
+
+navigator.mozSettings.addObserver('findmydevice.retry-count', function(event) {
+  var _ = navigator.mozL10n.get;
+  if (event.settingValue >= FIND_MY_DEVICE_RETRIES) {
+
+    var icon = 'style/find_my_device/images/findMyDevice.png';
+    var title = _('unable-to-connect');
+    var body = _('tap-to-check-settings');
+
+    var notification = new Notification(title,
+      {
+        body:body,
+        icon:icon,
+        tag: FIND_MY_DEVICE_TAG
+      });
+    notification.onclick = function(evt) {
+      var activity = new MozActivity(
+        {
+          name : 'configure',
+          data : {
+            target : 'device',
+            section: 'findmydevice'
+          }
+        });
+      activity.onerror = function (e) {
+        DUMP('There was a problem starting settings: '+e);
+      };
+      SettingsHelper('findmydevice.enabled').set(false);
+      notification.close();
+    };
+  }
 });
 
 navigator.mozSettings.addObserver('geolocation.enabled', function(event) {
@@ -42,3 +79,25 @@ window.addEventListener('mozFxAccountsUnsolChromeEvent', function(event) {
     wakeUpFindMyDevice(IAC_API_WAKEUP_REASON_LOGOUT);
   }
 });
+
+// ensure notifications are cleaned up on startup
+function cleanupOldNotifications() {
+  Notification.get().then(function(notifications) {
+    notifications.forEach(function(notification) {
+      if (!notification) {
+        return;
+      }
+
+      // ignore notification unrelated to Find My Device
+      var tag = notification.tag;
+      if (!tag || !tag.startsWith(FIND_MY_DEVICE_TAG)) {
+        return;
+      }
+
+      // clean up the notification
+      notification.close();
+    });
+  });
+}
+
+cleanupOldNotifications();
