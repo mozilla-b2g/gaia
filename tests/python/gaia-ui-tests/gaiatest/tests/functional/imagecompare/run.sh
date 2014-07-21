c433c9e3ba508f9a9a18595c841238479dc420ab
No-Jun <npark@mozilla.com>
2014-07-21 11:54:19 -0400

    updated run script to save the images elsewhere after the run

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/imagecompare/run.sh
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/imagecompare/run.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-flashlog=$(date +'%Y-%m-%d-%H-%M')_flash.log
-runlog=$(date +'%Y-%m-%d-%H-%M')_run.log
-comparelog=$(date +'%Y-%m-%d-%H-%M')_compare.log
-virtualenvname=demo-gaiatest
-gaiahome=~/GitRepo/gaia/tests/python/gaia-ui-tests
+#!/bin/bash -v
+timestamp=$(date +'%Y-%m-%d-%H-%M')
+flashlog=$timestamp'_flash.log'
+runlog=$timestamp'_run.log'
+comparelog=$timestamp'_compare.log'
+zipfile=$timestamp'_zipped.zip'
+virtualenvname=default
+gaiahome=~/ImgCmpTest/gaia/tests/python/gaia-ui-tests
 username=npark@mozilla.com
 password=A2mw9q01mozilla
 config=b2g-7.json
@@ -14,13 +16,21 @@ cd ~/B2G-flash-tool
 git pull
 ./flash_pvt.py -v central -d hamachi -g -G --eng -u $username -p $password >  $flashlog
 
-# delete the contents in the shots folder
+# zip the contents of the shots folder and save elsewhere
 rm $gaiahome/shots/*
+
+# sleep for 5 minutes, give time until the phone boots up
+sleep 5m
 
 # go to the gaia folder, and setup adb and execute the manifest
 cd $gaiahome
+git pull
 adb forward tcp:2828 tcp:2828
 workon $virtualenvname
 gaiatest --address=localhost:2828 --testvars=gaiatest/$config --restart  --type=b2g gaiatest/tests/functional/imagecompare/manifest.ini > $runlog
 
 
+# copy all data into the shots folders
+mv ~/B2G-flash-tool/$flashlog $gaiahome/shots/
+mv $gaiahome/$runlog $gaiahome/shots/
+zip -r $zipfile $gaiahome/shots/*
