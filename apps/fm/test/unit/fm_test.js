a3eeb5d3f3a2004dd30cfa7f86a3dccfbf2b6803
Justin D'Arcangelo <justindarc@gmail.com>
2014-07-22 17:27:41 -0400

    Bug 1037880 - FM radio should stop sound in speaker mode when I receive an incoming call

--- apps/fm/test/unit/fm_test.js
+++ apps/fm/test/unit/fm_test.js
@@ -22,7 +22,6 @@ suite('FM', function() {
   suite('frequency dialer', function() {
 
     suiteSetup(function() {
-
       tempNode = document.createElement('div');
       tempNode.id = 'test';
       tempNode.innerHTML =
@@ -43,7 +42,6 @@ suite('FM', function() {
 
       document.body.appendChild(tempNode);
       frequencyDialer.init();
-
     });
 
     suiteTeardown(function() {
@@ -275,4 +273,116 @@ suite('FM', function() {
       });
     });
   });
+
+  suite('update radio status based on incoming attention screen status',
+    function() {
+      suiteSetup(function() {
+
+        // Stub AirplaneModeHelper
+        window.AirplaneModeHelper = {
+          addEventListener: sinon.stub(),
+          ready: sinon.stub()
+        };
+
+        // Stub asyncStorage
+        window.asyncStorage = {
+          getItem: sinon.stub(),
+          setItem: sinon.stub()
+        };
+
+        // Stub enableFMRadio
+        window.enableFMRadio = sinon.stub();
+
+        // Stub mozSettings
+        navigator.mozSettings = {
+          addObserver: function(key, callback) {
+            this.callback = callback;
+          }
+        };
+
+        tempNode = document.createElement('div');
+        tempNode.id = 'test';
+        tempNode.innerHTML =
+          '<div id="frequency-bar">' +
+          '  <div id="frequency-display">' +
+          '    <a id="speaker-switch" href="#speaker" ' +
+                'data-speaker-on="false"></a>' +
+          '    <a id="bookmark-button" href="#bookmark"' +
+          '      data-bookmarked="false"></a>' +
+          '    <div id="frequency">0</div>' +
+          '  </div>' +
+          '</div>' +
+          '<div id="dialer-bar">' +
+          '  <div id="dialer-container">' +
+          '    <div id="frequency-indicator"></div>' +
+          '    <div id="frequency-dialer" class="animation-on"></div>' +
+          '  </div>' +
+          '</div>' +
+          '<a id="frequency-op-seekdown" href="#seekdown"></a>' +
+          '<a id="power-switch" href="#power-switch" data-enabled="false" ' +
+            'data-enabling="false"></a>' +
+          '<a id="frequency-op-seekup" href="#seekup"></a>' +
+          '<div id="antenna-warning" hidden="hidden"></div>' +
+          '<div id="airplane-mode-warning" class="warning" hidden>';
+
+        document.body.appendChild(tempNode);
+        init();
+      });
+
+      suiteTeardown(function() {
+        tempNode.parentNode.removeChild(tempNode);
+        tempNode = null;
+      });
+
+      test('disabled powered-on radio for incoming attention screen',
+        function() {
+          mozFMRadio.enabled = true;
+          mozFMRadio.antennaAvailable = true;
+          navigator.mozSettings.callback({
+            settingValue: true
+          });
+
+          assert.equal(mozFMRadio.enabled, false);
+        }
+      );
+
+      test('enabled previously powered-on radio for outgoing attention screen',
+        function() {
+          mozFMRadio.enabled = true;
+          mozFMRadio.antennaAvailable = true;
+          navigator.mozSettings.callback({
+            settingValue: false
+          });
+
+          assert.ok(window.enableFMRadio.called);
+        }
+      );
+
+      test('did nothing for powered-off radio for incoming attention screen',
+        function() {
+          mozFMRadio.enabled = false;
+          mozFMRadio.antennaAvailable = true;
+          navigator.mozSettings.callback({
+            settingValue: true
+          });
+
+          assert.equal(window._previousFMRadioState, false);
+        }
+      );
+
+      test('did nothing for previously powered-off radio for outgoing ' +
+        'attention screen',
+        function() {
+          mozFMRadio.antennaAvailable = true;
+          window._previousFMRadioState = false;
+          window._previousEnablingState = false;
+          navigator.mozSettings.callback({
+            settingValue: false
+          });
+
+          assert.equal(mozFMRadio.enabled, false);
+        }
+      );
+    }
+  );
 });
