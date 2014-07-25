4059b6e06f3abf3afe3e86764ded28c4c9589c35
yu-nagai <ktec>
2014-07-25 13:32:48 +0900

    r4_Bug997547

--- apps/communications/contacts/js/sms_integration.js
+++ apps/communications/contacts/js/sms_integration.js
@@ -4,14 +4,11 @@
 /* jshint nonew: false */
 
 var SmsIntegration = {
-  sendSms: function si_sendSms(number) {
+  _send: function si_integration(data) {
     try {
       new MozActivity({
         name: 'new',
-        data: {
-          type: 'websms/sms',
-          number: number
-        }
+        data: data
       });
     } catch (e) {
       console.log('WebActivities unavailable? : ' + e);
