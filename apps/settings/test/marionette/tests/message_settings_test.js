9d1fc0b3646b1bc3f6e77d2a6add7e60dde63885
gasolin <gasolin@gmail.com>
2014-07-29 22:52:51 +0800

    Bug 1032651 - [Gaia] CMAS on/off toggle in Settings
    - Two toggles, CB and CMAS
    - when CB off, CMAS is also off and disabled
    - When CB on, user can turn on/off the CMAS
    - When CB on, CMAS state will be retained to the state before CB off.
    - add marionette test
    - make CMAS totally count on CBS

--- apps/settings/test/marionette/tests/message_settings_test.js
+++ apps/settings/test/marionette/tests/message_settings_test.js
@@ -21,6 +21,7 @@ marionette('message settings', function() {
       messagePanel.toggleMenuItem('readReport', true);
       messagePanel.toggleMenuItem('wapPush', true);
       messagePanel.toggleMenuItem('cellBroadcast', true);
+      messagePanel.toggleMenuItem('emergencyAlert', true);
 
       // select specific option
       messagePanel.chooseRetrieveMethod('On with roaming');
@@ -31,6 +32,7 @@ marionette('message settings', function() {
       assert.equal(messagePanel.isChecked('readReportCheckbox'), true);
       assert.equal(messagePanel.isChecked('wapPushCheckbox'), true);
       assert.equal(messagePanel.isChecked('cellBroadcastCheckbox'), true);
+      assert.equal(messagePanel.isChecked('emergencyAlertCheckbox'), true);
       assert.equal(
         client.settings.get('ril.sms.requestStatusReport.enabled'), true);
       assert.equal(
@@ -39,9 +41,27 @@ marionette('message settings', function() {
         client.settings.get('wap.push.enabled'), true);
       assert.equal(
         client.settings.get('ril.cellbroadcast.disabled'), false);
+      assert.equal(
+        client.settings.get('cmas.enabled')[0], true);
 
       // we will keep its value as `automatic`
       assert.equal(messagePanel.getSelectedRetrieveMethod(), 'automatic');
+    });
+  });
+
+  suite('check Cell Broadcast and CMAS dependency', function() {
+    setup(function() {
+      messagePanel.initialMessagingSettings();
+      messagePanel.toggleMenuItem('emergencyAlert', true);
+      messagePanel.toggleMenuItem('cellBroadcast', false);
+    });
+
+    test('when CB off, CMAS is also off and disabled', function() {
+      assert.equal(
+        client.settings.get('ril.cellbroadcast.disabled'), true);
+      assert.equal(
+        client.settings.get('cmas.enabled')[0], false);
+      assert.equal(messagePanel.isEmergencyAlertCheckboxEnabled, false);
     });
   });
 });
