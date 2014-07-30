e1694abad4eb63e7843b96334201707eac76341a
Wilson Page <wilsonpage@me.com>
2014-07-30 15:46:09 +0100

    Bug 1044033 - Video Encode: Flicker in the preview can be observed when we change the resolution in front camera

--- apps/camera/js/lib/setting-alias.js
+++ apps/camera/js/lib/setting-alias.js
@@ -6,14 +6,11 @@ define(function(require, exports, module) {
  */
 
 var debug = require('debug')('setting-alias');
-var evt = require('evt');
 
 /**
  * Locals
  */
 
-var off = evt.prototype.off;
-var on = evt.prototype.on;
 var forwardMethods = [
   'filterOptions',
   'resetOptions',
@@ -31,24 +28,83 @@ var forwardMethods = [
 
 module.exports = SettingAlias;
 
-// Mixin emitter
-evt(SettingAlias.prototype);
-
 /**
  * Initialize a new `SettingsAlias`
+ *
+ * SettingsAlias's are kinda confusing,
+ * but they simplify our app code massively
+ * in the long run.
+ *
+ * A `SettingAlias` has the same API as a `Setting`
+ * but behind the scenes provides a single interface
+ * to several `Setting`s. Depending on the state
+ * of the app, the `SettingAlias` will point to
+ * a different settings instance.
+ *
+ * Example:
+ *
+ *    var flashModesPicture = new Setting({ key: 'flashModesPicture' });
+ *    var flashModesVideo = new Setting({ key: 'flashModesVideo' });
+ *    var mode = 'picture';
+ *
+ *    var flashModes = new SettingAlias({
+ *      key: 'flashModes',
+ *      settings: {
+ *        'picture': flashModesPicture,
+ *        'video': flashModesVideo
+ *      },
+ *      get: function() {
+ *        return this.settings[mode];
+ *      }
+ *    });
+ *
+ *    flashModes.get('key'); //=> 'flashModesPicture'
+ *    mode = 'video';
+ *    flashModes.get('key'); //=> 'flashModesVideo'
+ *
+ * This means our app can simply call API on
+ * `flashModes` setting-alias and not worry
+ * about which 'mode' the app is in.
+ *
+ * Options:
+ *
+ *   - `key` The name of the setting-alias
+ *   - `settings` Hash of possible `Setting`s
+ *   - `get` Method that returns the current `Setting`
  *
  * @param {Object} options
  */
 function SettingAlias(options) {
   debug('initialize');
-  this.settings = options.settings;
   this.key = options.key;
-  this.map = options.map || {};
-  this.setting = options.get.bind(this);
+  this.settings = options.settings;
+  this.current = options.get.bind(this);
   forwardMethods.forEach(this.forward, this);
-  this.propagate = this.propagator();
   debug('initialized');
 }
+
+/**
+ * Iterates over each setting.
+ *
+ * @param  {Function} fn
+ * @private
+ */
+SettingAlias.prototype.each = function(fn) {
+  for (var key in this.settings) {
+    fn(this.settings[key]);
+  }
+};
+
+/**
+ * States whether the passed key
+ * is the currently active setting.
+ *
+ * @param  {String}  key
+ * @return {Boolean}
+ */
+SettingAlias.prototype.is = function(key) {
+  return this.get('key') === key;
+};
 
 /**
  * Attaches a method that forwards
@@ -59,65 +115,43 @@ function SettingAlias(options) {
  */
 SettingAlias.prototype.forward = function(method) {
   this[method] = function() {
-    var setting = this.setting();
+    var setting = this.current();
     return setting[method].apply(setting, arguments);
   };
 };
 
 /**
- * Attach a pseudo callback that
- * fires when the same event fires
- * on the currently active setting.
+ * Add an event callback.
+ *
+ * The callback is wrapped in
+ * a function that first checks that
+ * the setting is the active setting
+ * before calling the original callback.
  *
  * @param  {String}   name
  * @param  {Function} fn
  */
 SettingAlias.prototype.on = function(name, fn) {
-  on.call(this, name, fn);
-  for (var key in this.map) {
-    var setting = this.settings[this.map[key]];
-    setting.on(name, this.propagate);
-  }
+  var alias = this;
+  var wrapped = function() {
+    if (!alias.is(this.key)) { return; }
+    fn.apply(this, arguments);
+  };
+
+  this.each(function(setting) { setting.on(name, wrapped); });
+  fn._settingAliasCallback = wrapped;
 };
 
 /**
- * Remove pseudo callback.
+ * Remove an event callback.
  *
  * @param  {String}   name
  * @param  {Function} fn
  */
 SettingAlias.prototype.off = function(name, fn) {
-  off.call(this, name, fn);
-  for (var key in this.map) {
-    var setting = this.settings[this.map[key]];
-    setting.off(name, this.propagate);
-  }
-};
-
-/**
- * Returns a function that when
- * attached as a callback to
- * another event handler, will
- * fire the same event on itself.
- *
- * `this._fireArgs` is the original
- * list of arguments passed when the
- * event was fired.
- *
- * Access to this allows use to re-fire
- * the event on another emitter in exactly
- * the same way it was originally fired.
- *
- * @param  {String}   name
- * @param  {Function} fn
- */
-SettingAlias.prototype.propagator = function() {
-  var alias = this;
-  return function() {
-    if (this.key === alias.get().key) {
-      alias.fire.apply(alias, this._fireArgs);
-    }
-  };
+  var wrapped = fn._settingAliasCallback;
+  this.each(function(setting) { setting.off(name, wrapped); });
+  delete fn._settingAliasCallback;
 };
 
 });
--- apps/camera/test/unit/lib/setting-alias_test.js
+++ apps/camera/test/unit/lib/setting-alias_test.js
@@ -1,4 +1,5 @@
 suite('lib/setting-alias', function() {
+  /*jshint maxlen:false*/
   'use strict';
 
   suiteSetup(function(done) {
@@ -30,29 +31,24 @@ suite('lib/setting-alias', function() {
     this.alias = new this.SettingAlias({
       key: 'my-alias',
       settings: this.settings,
-      map: {
-        'a': 'a',
-        'b': 'b',
-      },
       get: function() {
-        return this.settings[this.map[self.state.value]];
+        return this.settings[self.state.value];
       }
     });
   });
 
   suite('SettingAlias()', function() {
-    test('Should store various options', function() {
+    test('It stores `key` and `settings` options', function() {
       assert.equal(this.alias.key, 'my-alias');
       assert.equal(this.alias.settings, this.settings);
-      assert.equal(this.alias.map.a, 'a');
     });
   });
 
-  suite('SettingAlias#get()', function() {
-    test('Should get the current setting', function() {
-      assert.equal(this.alias.get().key, 'a');
+  suite('SettingAlias#current()', function() {
+    test('It returns the current setting', function() {
+      assert.equal(this.alias.current().key, 'a');
       this.state.value = 'b';
-      assert.equal(this.alias.get().key, 'b');
+      assert.equal(this.alias.current().key, 'b');
     });
   });
 
@@ -62,7 +58,7 @@ suite('lib/setting-alias', function() {
       this.alias.on('event', callback);
       this.settings.a.fire('event');
       this.settings.b.fire('event');
-      assert.ok(callback.calledOnce);
+      sinon.assert.calledOnce(callback);
     });
 
     test('Should pass arguments to the callback', function() {
@@ -74,17 +70,87 @@ suite('lib/setting-alias', function() {
   });
 
   suite('SettingAlias#off()', function() {
-    test('Should be able to remove listeners', function() {
+    test('it calls the callback once', function() {
       var callback = sinon.spy();
 
       this.alias.on('event', callback);
       this.settings.a.fire('event');
-      assert.ok(callback.called);
-      callback.reset();
 
-      this.alias.off('event', callback);
-      this.settings.a.fire('event');
-      assert.ok(!callback.called);
+      sinon.assert.calledOnce(callback);
     });
+
+    test('It only removes the listeners that match the callback given', function() {
+      var callback1 = sinon.spy();
+      var callback2 = sinon.stub();
+
+      this.alias.on('event', callback1);
+      this.alias.on('event', callback2);
+
+      this.settings.a.fire('event');
+
+      sinon.assert.calledOnce(callback1);
+      sinon.assert.calledOnce(callback2);
+
+      callback1.reset();
+      callback2.reset();
+
+      this.alias.off('event', callback1);
+      this.settings.a.fire('event');
+
+      sinon.assert.calledOnce(callback2);
+      sinon.assert.notCalled(callback1);
+    });
+  });
+
+  suite('SettingAlias#get()', function() {
+    test('It calls the underlying Setting\'s `.get()` method', function() {
+      assert.equal(this.alias.get('key'), 'a');
+      this.state.value = 'b';
+      assert.equal(this.alias.get('key'), 'b');
+    });
+  });
+
+  test('It forwards the setting mathod calls onto the current setting', function() {
+    sinon.stub(this.settings.a);
+    sinon.stub(this.settings.b);
+
+    this.alias.filterOptions();
+    this.alias.resetOptions();
+    this.alias.supported();
+    this.alias.selected();
+    this.alias.select();
+    this.alias.next();
+    this.alias.get();
+    this.alias.set();
+
+    sinon.assert.calledOnce(this.settings.a.filterOptions);
+    sinon.assert.calledOnce(this.settings.a.resetOptions);
+    sinon.assert.calledOnce(this.settings.a.supported);
+    sinon.assert.calledOnce(this.settings.a.selected);
+    sinon.assert.calledOnce(this.settings.a.select);
+    sinon.assert.calledOnce(this.settings.a.next);
+    sinon.assert.calledOnce(this.settings.a.get);
+    sinon.assert.calledOnce(this.settings.a.set);
+
+    // Change state
+    this.state.value = 'b';
+
+    this.alias.filterOptions();
+    this.alias.resetOptions();
+    this.alias.supported();
+    this.alias.selected();
+    this.alias.select();
+    this.alias.next();
+    this.alias.get();
+    this.alias.set();
+
+    sinon.assert.calledOnce(this.settings.b.filterOptions);
+    sinon.assert.calledOnce(this.settings.b.resetOptions);
+    sinon.assert.calledOnce(this.settings.b.supported);
+    sinon.assert.calledOnce(this.settings.b.selected);
+    sinon.assert.calledOnce(this.settings.b.select);
+    sinon.assert.calledOnce(this.settings.b.next);
+    sinon.assert.calledOnce(this.settings.b.get);
+    sinon.assert.calledOnce(this.settings.b.set);
   });
 });
