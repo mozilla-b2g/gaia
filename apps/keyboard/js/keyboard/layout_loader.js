35a279f09c0a074b30bd5ac398081b5e5691256b
sudheesh001 <sudheesh1995@outlook.com>
2014-08-01 08:07:06 +0530

    Fixes bug 1036298, UI Consistency for keyboard Alt Key r=rudyl

--- apps/keyboard/js/keyboard/layout_loader.js
+++ apps/keyboard/js/keyboard/layout_loader.js
@@ -25,7 +25,7 @@ Keyboards.alternateLayout = {
       { value: '&' }, { value: '*' }, { value: '-' }, { value: '+' },
       { value: '(' }, { value: ')' }, { value: '_', visible: ['email'] }
     ], [
-      { value: 'ALT', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
+      { value: 'Alt', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
       { value: '!' }, { value: '\"' }, { value: '\'' }, { value: ':' },
       { value: ';' }, { value: '/' }, { value: '?' },
       { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
@@ -47,7 +47,7 @@ Keyboards.symbolLayout = {
       { value: '®' }, { value: '§' }, { value: '<' }, { value: '>' },
       { value: '«' }, { value: '»' }
     ], [
-      { value: 'ALT', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
+      { value: 'Alt', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
       { value: '¥' }, { value: '€' }, { value: '£' }, { value: '$' },
       { value: '¢' }, { value: '\\' }, { value: '=' },
       { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
--- apps/keyboard/js/layouts/hi.js
+++ apps/keyboard/js/layouts/hi.js
@@ -100,7 +100,7 @@ Keyboards.hi = {
         { value: '+' }, { value: '(' }, { value: ')' },
         { value: '_', visible: ['email'] }
       ], [
-        { value: 'ALT', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
+        { value: 'Alt', ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
         { value: '!' }, { value: '\"' }, { value: "'" }, { value: ':' },
         { value: ';' }, { value: '/' }, { value: '?' },
         { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
