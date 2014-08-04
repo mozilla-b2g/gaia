f998a603123f802d5b6db55c7f3c03ecdc82d6fc
arvin zhang <arvin.zhang@spreadtrum.com>
2014-08-04 17:58:20 +0800

    Bug 1046564: Should display normal priority message of STK_CMD_DISPLAY_TEXT command while device is Settings

--- apps/system/js/icc_worker.js
+++ apps/system/js/icc_worker.js
@@ -223,9 +223,10 @@ var icc_worker = {
     DUMP('STK_CMD_DISPLAY_TEXT:', message.command.options);
     var options = message.command.options;
 
-    // Check if device is idle
+    // Check if device is idle or settings
     var activeApp = AppWindowManager.getActiveApp();
-    if (!options.isHighPriority && !activeApp.isHomescreen) {
+    if (!options.isHighPriority && !activeApp.isHomescreen &&
+        activeApp.origin !== 'app://settings.gaiamobile.org') {
       DUMP('Do not display the text because normal priority.');
       icc.responseSTKCommand(message, {
         resultCode:
