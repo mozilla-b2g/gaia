54ec8ac1e57b3d885e2d4afd10a9de79247b9429
arvin zhang <arvin.zhang@spreadtrum.com>
2014-08-07 19:52:38 +0800

    Bug 1046564 - Should display normal priority message of STK_CMD_DISPLAY_TEXT command while device is Settings

--- apps/system/js/icc_worker.js
+++ apps/system/js/icc_worker.js
@@ -228,9 +228,11 @@ var icc_worker = {
     DUMP('STK_CMD_DISPLAY_TEXT:', message.command.options);
     var options = message.command.options;
 
-    // Check if device is idle
+    // Check if device is idle or settings
     var activeApp = AppWindowManager.getActiveApp();
-    if (!options.isHighPriority && !activeApp.isHomescreen) {
+    var settingsOrigin = window.location.origin.replace('system', 'settings');
+    if (!options.isHighPriority && !activeApp.isHomescreen &&
+        activeApp.origin !== settingsOrigin) {
       DUMP('Do not display the text because normal priority.');
       icc.responseSTKCommand(message, {
         resultCode:
