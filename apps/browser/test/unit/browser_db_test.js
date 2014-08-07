f31b2f18385c62a5d6c4e0f1fae047032e4408dc
Swarnava Sengupta <swarnavasengupta@gmail.com>
2014-08-07 23:12:16 +0530

    Bug 1037357 - Adding sumo link on bookmark section of browser

--- apps/browser/test/unit/browser_db_test.js
+++ apps/browser/test/unit/browser_db_test.js
@@ -210,8 +210,9 @@ suite('BrowserDB', function() {
 
         assert.equal(places[2].uri, 'http://mozilla.org/firefoxos');
         assert.equal(places[2].title, 'Firefox OS');
-        
-        assert.equal(places[3].uri, 'http://support.mozilla.org/products/firefox-os');
+
+        assert.equal(places[3].uri, 
+                     'http://support.mozilla.org/products/firefox-os');
         assert.equal(places[3].title, 'Firefox OS Support');
 
         done();
