a677b3a9fc41d9c8cdb00734a60803112aaea517
Yuren Ju <yurenju@gmail.com>
2014-08-12 16:19:09 +0800

    Revert "Bug 1036830 - build failed if camera-config.js in distribution directory doesn't exist r=gduan"
    This reverts commit c9fb6b5a763d744a991aa0b26a22ef988fde8ab7.

--- apps/camera/build/build.js
+++ apps/camera/build/build.js
@@ -19,9 +19,7 @@ function copyUserConfig(options) {
   if (options.GAIA_DISTRIBUTION_DIR) {
     var distConfig = utils.getFile(options.GAIA_DISTRIBUTION_DIR,
       'camera-config.js');
-    if (distConfig.exists()) {
-      utils.copyFileTo(distConfig, parent, filename, true);
-    }
+    utils.copyFileTo(distConfig, parent, filename, true);
   }
 }
 
