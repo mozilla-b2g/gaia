9774c07246d56eb293f6b382738f962be9e685c8
Paco Rampas <pacorampas@gmail.com>
2014-08-14 09:42:10 +0200

    new changes

--- apps/communications/dialer/js/show_filter.js
+++ apps/communications/dialer/js/show_filter.js
@@ -1,32 +1,34 @@
 'use strict';
 
-window.onload = function () {
+window.onload = function() {
 	document.getElementById('option-keypad').click();
 	var scrolable = document.getElementById('call-log-container');
-	scrolable.onscroll = scroll;
-	var ScrollYGlobal = 0;
-	var elFloating = document.getElementById('call-log-filter');
-	elFloating.style.marginTop = "-4rem";
+	var filtersCallLog = document.getElementById('call-log-filter');
+	
+	var globalScroll = scrolable.scrollTop;
+	var scrollStarted = false;
 
-	function scroll () {
+	function scroll() {
 		var yScroll = scrolable.scrollTop;
-		var marginTopEl = parseInt(elFloating.style.marginTop);
-		//console.log('ScrollYGlobal: '+ScrollYGlobal +'// yScroll: '+ 
-		//	yScroll + '// marginTopEl: ' + marginTopEl);
-		if(ScrollYGlobal < yScroll && yScroll > 70) { //scroll down
-			//console.log('scroll down');
-			marginTopEl++;
-			if(marginTopEl >= 0) marginTopEl = 0;
-			//scrolable.scrollTop = marginTopEl*10;
-			elFloating.style.marginTop = marginTopEl+'rem';
-		} else if(ScrollYGlobal > yScroll) { //scroll up
-			//console.log('scroll up');
-			marginTopEl--;
-			if(marginTopEl < -4) marginTopEl = -4;
-			elFloating.style.marginTop = marginTopEl+'rem';
-		} else if(ScrollYGlobal == 0 || yScroll == 0) {
-			elFloating.style.marginTop = '-4rem';
-		} 
-		ScrollYGlobal = yScroll;
+		if(scrollStarted) {
+			filtersCallLog.classList.remove('visible');
+		}
+		if( yScroll > 140) {
+			filtersCallLog.classList.add('after-scroll');
+		}
+		else {
+			filtersCallLog.classList.remove('after-scroll');
+		}
+
+		globalScroll = yScroll;
 	}
-}
+	scrolable.onscroll = scroll;
+
+	scrolable.addEventListener('touchstart', function(){
+		scrollStarted = true;
+	}, false);
+	scrolable.addEventListener('touchend', function(){
+		scrollStarted = false;
+		filtersCallLog.classList.add('visible');}
+	, false);
+}();
