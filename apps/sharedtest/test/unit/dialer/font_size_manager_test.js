9e56536fd60d44a0397dff350deef67323d6ec3b
Tzu-Lin Huang <tzhuang@mozilla.com>
2014-08-15 14:49:40 +0800

    Bug 1047275 - Test ellipsis side on RTL pages for FontSizeManager

--- apps/sharedtest/test/unit/dialer/font_size_manager_test.js
+++ apps/sharedtest/test/unit/dialer/font_size_manager_test.js
@@ -1,8 +1,10 @@
-/* globals FontSizeManager, FontSizeUtils, MocksHelper */
+/* globals FontSizeManager, FontSizeUtils, MocksHelper, MockL10n */
 
 'use strict';
 
 require('/shared/test/unit/mocks/mock_font_size_utils.js');
+require('/shared/test/unit/mocks/mock_l10n.js');
+
 require('/shared/js/dialer/font_size_manager.js');
 
 var mocksHelperForCallScreen = new MocksHelper([
@@ -10,22 +12,21 @@ var mocksHelperForCallScreen = new MocksHelper([
 ]).init();
 
 suite('font size manager', function() {
-
   var ROOT_FONT_SIZE = 10;
+
+  var realMozL10n;
 
   var view;
 
   mocksHelperForCallScreen.attachTestHelpers();
 
-  setup(function() {
-    document.documentElement.style.fontSize = ROOT_FONT_SIZE + 'px';
-    view = document.createElement('div');
-    view.textContent = 'foobar';
-    document.body.appendChild(view);
+  suiteSetup(function() {
+    realMozL10n = navigator.mozL10n;
+    navigator.mozL10n = MockL10n;
   });
 
-  teardown(function() {
-    document.body.innerHTML = '';
+  suiteTeardown(function() {
+    navigator.mozL10n = realMozL10n;
   });
 
   suite('adaptToSpace', function() {
@@ -42,115 +43,168 @@ suite('font size manager', function() {
       test('does nothing for empty inputs, scenario ' + scenario, function() {
         view = document.createElement('input');
         document.body.appendChild(view);
+
         FontSizeManager.adaptToSpace(scenario, view);
+
         assert.equal(view.style.fontSize, '');
       });
 
       test('does nothing for empty elements, scenario ' + scenario, function() {
         view.textContent = '';
+
         FontSizeManager.adaptToSpace(scenario, view);
+
         assert.equal(view.style.fontSize, '');
       });
+
     });
 
-    test('calls FSU with the proper arguments', function() {
-      FontSizeUtils.getMaxFontSizeInfo.returns({fontSize: 10, overflow: false});
-      view.style.fontFamily = 'Arial';
-      view.style.width = '200px';
-      FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view);
-      sinon.assert.calledWith(
-        FontSizeUtils.getMaxFontSizeInfo, view.textContent,
-        [26, 30, 34, 38, 41], view.style.fontFamily, 200);
-    });
+    var viewElements = [
+      {
+        element: 'div',
+        textField: 'textContent'
+      },
+      {
+        element: 'input',
+        textField: 'value'
+      }
+    ];
 
-    test('changes the font size', function() {
-      FontSizeUtils.getMaxFontSizeInfo.returns({fontSize: 42, overflow: false});
-      FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view);
-      assert.equal(view.style.fontSize, '42px');
-    });
+    viewElements.forEach(function(viewElement) {
+      suite('on ' + viewElement.element, function() {
+        var originalInnerHTML;
 
-    test('forces maxfontsize', function() {
-      FontSizeUtils.getMaxFontSizeInfo.returns({fontSize: 10, overflow: false});
-      FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true);
-      sinon.assert.calledWith(
-        FontSizeUtils.getMaxFontSizeInfo, view.textContent, [41]);
-    });
+        var getExpectedEllipsizedText = function(side, direction) {
+          // MockL10n does not invert text when in RTL mode, but FSM will
+          // treat the left side as the end for the purpose of ellipses.
+          return side === 'end' ^ direction === 'ltr' ? '\u2026ar' : 'fo\u2026';
+        };
 
-    test('adds ellipsis', function() {
-      FontSizeUtils.getMaxFontSizeInfo.returns({fontSize: 10, overflow: true});
-      this.sinon.stub(FontSizeUtils, 'getOverflowCount').returns(2);
-      FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true);
-      assert.equal(view.textContent, '\u2026ar');
-    });
+        setup(function() {
+          originalInnerHTML = document.body.innerHTML;
+          document.documentElement.style.fontSize = ROOT_FONT_SIZE + 'px';
+          view = document.createElement(viewElement.element);
+          view[viewElement.textField] = 'foobar';
+          document.body.appendChild(view);
+        });
 
-    test('adds ellipsis to the correct side', function() {
-      FontSizeUtils.getMaxFontSizeInfo.returns({fontSize: 10, overflow: true});
-      this.sinon.stub(FontSizeUtils, 'getOverflowCount').returns(2);
-      FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true, 'end');
-      assert.equal(view.textContent, 'fo\u2026');
-    });
+        teardown(function() {
+          document.body.innerHTML = originalInnerHTML;
+        });
 
-    suite('when view is an input element with value', function() {
-      setup(function() {
-        view = document.createElement('input');
-        view.value = 'foobar';
-        document.body.appendChild(view);
+        test('calls FSU with the proper arguments', function() {
+          FontSizeUtils.getMaxFontSizeInfo.returns({
+            fontSize: 10,
+            overflow: false
+          });
+
+          view.style.fontFamily = 'Arial';
+          view.style.width = '200px';
+
+          FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view);
+
+          sinon.assert.calledWith(FontSizeUtils.getMaxFontSizeInfo,
+                                  view[viewElement.textField],
+                                  [26, 30, 34, 38, 41],
+                                  view.style.fontFamily,
+                                  view.getBoundingClientRect().width);
+        });
+
+        test('changes the font size', function() {
+          FontSizeUtils.getMaxFontSizeInfo.returns({
+            fontSize: 42,
+            overflow: false
+          });
+
+          FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view);
+
+          assert.equal(view.style.fontSize, '42px');
+        });
+
+        test('forces maxfontsize', function() {
+          FontSizeUtils.getMaxFontSizeInfo.returns({
+            fontSize: 10,
+            overflow: false
+          });
+
+          FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true);
+
+          sinon.assert.calledWith(FontSizeUtils.getMaxFontSizeInfo,
+                                  view[viewElement.textField],
+                                  [41]);
+        });
+
+        suite('ellipses in LTR/RTL modes', function() {
+          var l10nLanguageDirection;
+
+          suiteSetup(function() {
+            l10nLanguageDirection = MockL10n.language.direction;
+          });
+
+          suiteTeardown(function() {
+            MockL10n.language.direction = l10nLanguageDirection;
+          });
+
+          ['ltr', 'rtl'].forEach(function(direction) {
+            test('adds ellipsis', function() {
+              MockL10n.language.direction = direction;
+
+              FontSizeUtils.getMaxFontSizeInfo.returns({
+                fontSize: 10,
+                overflow: true
+              });
+              this.sinon.stub(FontSizeUtils, 'getOverflowCount').returns(2);
+
+              FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view,
+                                           true);
+
+              assert.equal(view[viewElement.textField],
+                           getExpectedEllipsizedText('begin', direction));
+            });
+
+            test('adds ellipsis to the correct side', function() {
+              MockL10n.language.direction = direction;
+
+              FontSizeUtils.getMaxFontSizeInfo.returns({
+                fontSize: 10,
+                overflow: true
+              });
+              this.sinon.stub(FontSizeUtils, 'getOverflowCount').returns(2);
+
+              FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true,
+                                           'end');
+
+              assert.equal(view[viewElement.textField],
+                           getExpectedEllipsizedText('end', direction));
+            });
+          });
+        });
       });
-
-      test('calls FSU with the proper arguments', function() {
-        FontSizeUtils.getMaxFontSizeInfo.returns({
-          fontSize: 10, overflow: false});
-        view.style.fontFamily = 'Arial';
-        view.style.width = '200px';
-        document.body.appendChild(view);
-        FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view);
-        sinon.assert.calledWith(
-          FontSizeUtils.getMaxFontSizeInfo, view.value,
-          [26, 30, 34, 38, 41], view.style.fontFamily,
-          view.getBoundingClientRect().width);
-      });
-
-      test('changes the font size', function() {
-        FontSizeUtils.getMaxFontSizeInfo.returns({
-          fontSize: 42, overflow: false});
-        FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view);
-        assert.equal(view.style.fontSize, '42px');
-      });
-
-      test('forces maxfontsize', function() {
-        FontSizeUtils.getMaxFontSizeInfo.returns({
-          fontSize: 10, overflow: false});
-        FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true);
-        sinon.assert.calledWith(
-          FontSizeUtils.getMaxFontSizeInfo, view.value, [41]);
-      });
-
-      test('adds ellipsis', function() {
-        FontSizeUtils.getMaxFontSizeInfo.returns({
-          fontSize: 10, overflow: true});
-        this.sinon.stub(FontSizeUtils, 'getOverflowCount').returns(2);
-        FontSizeManager.adaptToSpace(FontSizeManager.DIAL_PAD, view, true);
-        assert.equal(view.value, '\u2026ar');
-      });
-
-      test('adds ellipsis to the correct side', function() {
-        FontSizeUtils.getMaxFontSizeInfo.returns({
-          fontSize: 10, overflow: true});
-        this.sinon.stub(FontSizeUtils, 'getOverflowCount').returns(2);
-        FontSizeManager.adaptToSpace(
-          FontSizeManager.DIAL_PAD, view, true, 'end');
-        assert.equal(view.value, 'fo\u2026');
-      });
-
     });
   });
 
-  test('ensureFixedBaseline', function() {
-    FontSizeManager.ensureFixedBaseline(FontSizeManager.SINGLE_CALL, view);
-    assert.equal(view.style.lineHeight, '49px');
+  suite('ensureFixedBaseline', function() {
+    var originalInnerHTML;
 
-    view.style.fontSize = '100px';
-    FontSizeManager.ensureFixedBaseline(FontSizeManager.SINGLE_CALL, view);
-    assert.equal(view.style.lineHeight, '4px');
+    setup(function() {
+      originalInnerHTML = document.body.innerHTML;
+      document.documentElement.style.fontSize = ROOT_FONT_SIZE + 'px';
+      view = document.createElement('div');
+      view.textContent = 'foobar';
+      document.body.appendChild(view);
+    });
+
+    teardown(function() {
+      document.body.innerHTML = originalInnerHTML;
+    });
+
+    test('sets correct line heights', function() {
+      FontSizeManager.ensureFixedBaseline(FontSizeManager.SINGLE_CALL, view);
+      assert.equal(view.style.lineHeight, '49px');
+
+      view.style.fontSize = '100px';
+      FontSizeManager.ensureFixedBaseline(FontSizeManager.SINGLE_CALL, view);
+      assert.equal(view.style.lineHeight, '4px');
+    });
   });
 });
