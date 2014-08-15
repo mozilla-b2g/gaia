46ea60a7e1cd028bd5dabde66c835a58000ada61
John Hu <im@john.hu>
2014-08-15 14:47:52 +0800

    Bug 1045523 - customize default window size of responsive web design

---
+++ build/config/tv/custom-prefs.js
@@ -0,0 +1,4 @@
+/*global user_pref*/
+user_pref('devtools.responsiveUI.customWidth', 1366);
+user_pref('devtools.responsiveUI.customHeight', 768);
+user_pref('devtools.responsiveUI.currentPreset', 'custom');
