a60927ebdedc74c0d5f5cc02669997470b55b9a8
Ghislain 'Aus' Lacroix <aus@noiseport.org>
2014-08-18 14:28:03 -0700

    Merge pull request #22945 from nullaus/bug1053509
    bug 1053509 - Use plain js download object when download succeeds. r=crd...
    (cherry picked from commit 385f8b1fc9179441c9d6a6ad16ff8701edfa1faa)

--- apps/system/js/download/download_notification.js
+++ apps/system/js/download/download_notification.js
@@ -57,7 +57,8 @@ DownloadNotification.prototype = {
   _update: function dn_update() {
     this._updateSystemDownloads();
     if (this.download.state === 'finalized') {
-      this._onFinalized();
+      // In theory we should never see this, but, we know that if we were
+      // to see this we want to do nothing.
       return;
     }
     var noNotify = this._wontNotify();
@@ -120,10 +121,6 @@ DownloadNotification.prototype = {
     this._storeDownload(this.download);
   },
 
-  _onFinalized: function dn_onFinalized() {
-    this._close();
-  },
-
   /**
    * This method stores complete downloads to share them with the download list
    * in settings app
@@ -133,16 +130,19 @@ DownloadNotification.prototype = {
   _storeDownload: function dn_storeDownload(download) {
     var req = DownloadStore.add(download);
 
-    req.onsuccess = function _storeDownloadOnSuccess() {
+    req.onsuccess = (function _storeDownloadOnSuccess(request) {
+      // We don't care about any more state changes to the download.
+      download.onstatechange = null;
+      // Update the download object to the datastore representation.
+      this.download = req.result;
+
       var mozDownloadManager = navigator.mozDownloadManager;
       if (mozDownloadManager) {
         // Once we've added the download to our data store we own it so clear
         // all references to it from the Downloads API.
         mozDownloadManager.clearAllDone();
       }
-      console.info('The download', download.id, 'was stored successfully:',
-                    download.url);
-    };
+    }).bind(this);
 
     req.onerror = function _storeDownloadOnError(e) {
       console.error('Exception storing the download', download.id, '(',
