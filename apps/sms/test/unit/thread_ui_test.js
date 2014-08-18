6c366d80c21cd56241883f35a3204dd7a93adef4
Luke Chang <lchang@mozilla.com>
2014-08-18 19:51:17 +0800

    Bug 1052424 - [Messages] Cursor should be in the "to" field by default while forwarding messages
    Conflicts:
    
    	apps/sms/test/unit/thread_ui_test.js

--- apps/sms/test/unit/thread_ui_test.js
+++ apps/sms/test/unit/thread_ui_test.js
@@ -5875,6 +5875,7 @@ suite('thread_ui.js >', function() {
         recipients: []
       });
       this.sinon.spy(Compose, 'fromDraft');
+      this.sinon.stub(Compose, 'focus');
       this.sinon.stub(Drafts, 'delete').returns(Drafts);
       this.sinon.stub(Drafts, 'store').returns(Drafts);
       this.sinon.spy(ThreadUI.recipients, 'add');
@@ -5908,12 +5909,19 @@ suite('thread_ui.js >', function() {
 
       sinon.assert.callOrder(Drafts.delete, Drafts.store);
     });
+
+    test('focus composer', function() {
+      ThreadUI.handleDraft();
+      sinon.assert.called(Compose.focus);
+    });
   });
 
   suite('handleActivity() >', function() {
     setup(function() {
       this.sinon.stub(Compose, 'fromDraft');
       this.sinon.stub(Compose, 'fromMessage');
+      this.sinon.stub(Compose, 'focus');
+      this.sinon.stub(ThreadUI.recipients, 'focus');
     });
 
     test('from activity with unknown contact', function() {
@@ -5959,12 +5967,35 @@ suite('thread_ui.js >', function() {
       assert.equal(ThreadUI.recipients.numbers.length, 0);
       sinon.assert.calledWith(Compose.fromMessage, activity);
     });
+
+    test('focus composer if there is at least one recipient', function() {
+      var activity = {
+        number: '998',
+        contact: null,
+        body: 'test'
+      };
+      ThreadUI.handleActivity(activity);
+      sinon.assert.called(Compose.focus);
+      sinon.assert.notCalled(ThreadUI.recipients.focus);
+    });
+
+    test('focus recipients if there isn\'t any contact or number', function() {
+      var activity = {
+        number: null,
+        contact: null,
+        body: 'Youtube url'
+      };
+      ThreadUI.handleActivity(activity);
+      sinon.assert.notCalled(Compose.focus);
+      sinon.assert.called(ThreadUI.recipients.focus);
+    });
   });
 
   suite('handleForward() >', function() {
     var message;
     setup(function() {
       this.sinon.spy(Compose, 'fromMessage');
+      this.sinon.stub(ThreadUI.recipients, 'focus');
       this.sinon.stub(MessageManager, 'getMessage', function(id) {
         switch (id) {
           case 1:
@@ -6017,6 +6048,15 @@ suite('thread_ui.js >', function() {
       sinon.assert.calledOnce(MessageManager.getMessage);
       sinon.assert.calledWith(MessageManager.getMessage, 3);
       sinon.assert.calledWith(Compose.fromMessage, message);
+    });
+
+    test(' focus recipients', function() {
+      var forward = {
+        messageId: 1
+      };
+      ThreadUI.handleForward(forward);
+      assert.isTrue(Recipients.View.isFocusable);
+      sinon.assert.called(ThreadUI.recipients.focus);
     });
   });
 
@@ -6144,17 +6184,18 @@ suite('thread_ui.js >', function() {
   }
 
   suite('switch to composer >', function() {
-    var transitionArgs = {
-      meta: {
-        next: { panel: 'composer', args: {} },
-        prev: { panel: 'thread-list', args: {} }
-      }
-    };
+    var transitionArgs;
 
     setup(function() {
+      transitionArgs = {
+        meta: {
+          next: { panel: 'composer', args: {} },
+          prev: { panel: 'thread-list', args: {} }
+        }
+      };
+
       this.sinon.stub(Navigation, 'isCurrentPanel').returns(false);
     });
-
 
     suite('beforeEnter()', function() {
       setup(function() {
