0309fdc4da63e27db3caaaf5bbfd16f422d1bb5d
Zac <zcampbell@mozilla.com>
2014-08-19 13:50:40 +0100

    Bug 1049489 - gaiatest: execute all ContactsAPI calls in chrome context

--- tests/python/gaia-ui-tests/gaiatest/gaia_test.py
+++ tests/python/gaia-ui-tests/gaiatest/gaia_test.py
@@ -144,6 +144,12 @@ class GaiaData(object):
         js = os.path.abspath(os.path.join(__file__, os.path.pardir, 'atoms', "gaia_data_layer.js"))
         self.marionette.import_script(js)
 
+        # TODO Bugs 1043562/1049489 To perform ContactsAPI scripts from the chrome context, we need
+        # to import the js file into chrome context too
+        self.marionette.set_context(self.marionette.CONTEXT_CHROME)
+        self.marionette.import_script(js)
+        self.marionette.set_context(self.marionette.CONTEXT_CONTENT)
+
     def set_time(self, date_number):
         self.marionette.set_context(self.marionette.CONTEXT_CHROME)
         self.marionette.execute_script("window.navigator.mozTime.set(%s);" % date_number)
@@ -151,28 +157,37 @@ class GaiaData(object):
 
     @property
     def all_contacts(self):
-        self.marionette.switch_to_frame()
-        return self.marionette.execute_async_script('return GaiaDataLayer.getAllContacts();', special_powers=True)
+        # TODO Bug 1049489 - In future, simplify executing scripts from the chrome context
+        self.marionette.set_context(self.marionette.CONTEXT_CHROME)
+        result = self.marionette.execute_async_script('return GaiaDataLayer.getAllContacts();', special_powers=True)
+        self.marionette.set_context(self.marionette.CONTEXT_CONTENT)
+        return result
 
     @property
     def sim_contacts(self):
-        self.marionette.switch_to_frame()
+        # TODO Bug 1049489 - In future, simplify executing scripts from the chrome context
+        self.marionette.set_context(self.marionette.CONTEXT_CHROME)
         adn_contacts = self.marionette.execute_async_script('return GaiaDataLayer.getSIMContacts("adn");', special_powers=True)
         sdn_contacts = self.marionette.execute_async_script('return GaiaDataLayer.getSIMContacts("sdn");', special_powers=True)
-
+        self.marionette.set_context(self.marionette.CONTEXT_CONTENT)
         return adn_contacts + sdn_contacts
 
     def insert_contact(self, contact):
-        self.marionette.switch_to_frame()
+        # TODO Bug 1049489 - In future, simplify executing scripts from the chrome context
+        self.marionette.set_context(self.marionette.CONTEXT_CHROME)
         mozcontact = contact.create_mozcontact()
         result = self.marionette.execute_async_script('return GaiaDataLayer.insertContact(%s);' % json.dumps(mozcontact), special_powers=True)
         assert result, 'Unable to insert contact %s' % contact
+        self.marionette.set_context(self.marionette.CONTEXT_CONTENT)
+
 
     def remove_all_contacts(self):
-        self.marionette.switch_to_frame()
+        # TODO Bug 1049489 - In future, simplify executing scripts from the chrome context
+        self.marionette.set_context(self.marionette.CONTEXT_CHROME)
         timeout = max(self.marionette.timeout or 60000, 1000 * len(self.all_contacts))
         result = self.marionette.execute_async_script('return GaiaDataLayer.removeAllContacts();', special_powers=True, script_timeout=timeout)
         assert result, 'Unable to remove all contacts'
+        self.marionette.set_context(self.marionette.CONTEXT_CONTENT)
 
     def get_setting(self, name):
         return self.marionette.execute_async_script('return GaiaDataLayer.getSetting("%s")' % name, special_powers=True)
