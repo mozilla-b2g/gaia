7f44b868861c6ad33dc8b081c148365f6390a3eb
Kevin Grandon <kevingrandon@yahoo.com>
2014-08-27 14:50:36 -0700

    Revert "Bug 1059051 - Convert Keyboard-Symbols.woff to Keyboard-Symbols.ttf"
    This reverts commit 3b462c8f348c3ba01325eeb355078f665754ab7b.

--- gaia.mk
+++ gaia.mk
@@ -1,2 +1,2 @@
 PRODUCT_COPY_FILES += gaia/shared/elements/gaia-icons/fonts/gaia-icons.ttf:system/fonts/hidden/gaia-icons.ttf
-PRODUCT_COPY_FILES += gaia/shared/style/keyboard_symbols/Keyboard-Symbols.ttf:system/fonts/hidden/Keyboard-Symbols.ttf
+PRODUCT_COPY_FILES += gaia/shared/style/keyboard_symbols/Keyboard-Symbols.woff:system/fonts/hidden/Keyboard-Symbols.woff
