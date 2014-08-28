51aae73d84fb22faa0d345de241d5b8a8813eb57
gaye <gaye@mozilla.com>
2014-08-28 13:30:58 -0400

    Calendar modularization part 2 r=millermedeiros

--- apps/calendar/js/oauth_window.js
+++ apps/calendar/js/oauth_window.js
@@ -1,174 +1,180 @@
-Calendar.OAuthWindow = (function() {
-  'use strict';
+(function(exports) {
+'use strict';
 
-  /**
-   * Creates a oAuth dialog given a set of parameters.
-   *
-   *    var oauth = new OAuthWindow(
-   *      elementContainer,
-   *      'https://accounts.google.com/o/oauth2/auth',
-   *      {
-   *        response_type: 'code',
-   *        client_id: 'xxx',
-   *        scope: 'https://www.googleapis.com/auth/calendar',
-   *        redirect_uri: 'xxx',
-   *        state: 'foobar',
-   *        access_type: 'offline',
-   *        approval_prompt: 'force'
-   *      }
-   *    );
-   *
-   *    oauth.oncomplete = function(evt) {
-   *      if (evt.detail.code) {
-   *        // success
-   *      }
-   *    };
-   *
-   *    oauth.onabort = function() {
-   *      // oauth was aborted
-   *    };
-   *
-   *
-   */
-  function OAuthWindow(container, server, params) {
-    if (!params.redirect_uri) {
-      throw new Error(
-        'must provide params.redirect_uri so oauth flow can complete'
-      );
-    }
+/**
+ * Module dependencies
+ */
+var QueryString = Calendar.QueryString,
+    View = Calendar.View;
 
-    this.params = {};
-    for (var key in params) {
-      this.params[key] = params[key];
-    }
-
-    this._element = container;
-
-    Calendar.View.call(this);
-    this.target = server + '?' + Calendar.QueryString.stringify(params);
-
-    this._handleUserTriggeredClose =
-      this._handleUserTriggeredClose.bind(this);
+/**
+ * Creates a oAuth dialog given a set of parameters.
+ *
+ *    var oauth = new OAuthWindow(
+ *      elementContainer,
+ *      'https://accounts.google.com/o/oauth2/auth',
+ *      {
+ *        response_type: 'code',
+ *        client_id: 'xxx',
+ *        scope: 'https://www.googleapis.com/auth/calendar',
+ *        redirect_uri: 'xxx',
+ *        state: 'foobar',
+ *        access_type: 'offline',
+ *        approval_prompt: 'force'
+ *      }
+ *    );
+ *
+ *    oauth.oncomplete = function(evt) {
+ *      if (evt.detail.code) {
+ *        // success
+ *      }
+ *    };
+ *
+ *    oauth.onabort = function() {
+ *      // oauth was aborted
+ *    };
+ *
+ *
+ */
+function OAuthWindow(container, server, params) {
+  if (!params.redirect_uri) {
+    throw new Error(
+      'must provide params.redirect_uri so oauth flow can complete'
+    );
   }
 
-  OAuthWindow.prototype = {
-    __proto__: Calendar.View.prototype,
+  this.params = {};
+  for (var key in params) {
+    this.params[key] = params[key];
+  }
 
-    get element() {
-      return this._element;
-    },
+  this._element = container;
 
-    get isOpen() {
-      return !!this.browserFrame;
-    },
+  View.call(this);
+  this.target = server + '?' + QueryString.stringify(params);
 
-    selectors: {
-      browserTitle: 'header > h1',
-      browerCancelButton: 'button.cancel',
-      browserContainer: '.browser-container'
-    },
+  this._handleUserTriggeredClose =
+    this._handleUserTriggeredClose.bind(this);
+}
+exports.OAuthWindow = OAuthWindow;
 
-    get browserContainer() {
-      return this._findElement('browserContainer', this.element);
-    },
+OAuthWindow.prototype = {
+  __proto__: View.prototype,
 
-    get browserTitle() {
-      return this._findElement('browserTitle', this.element);
-    },
+  get element() {
+    return this._element;
+  },
 
-    get browerCancelButton() {
-      return this._findElement('browerCancelButton', this.element);
-    },
+  get isOpen() {
+    return !!this.browserFrame;
+  },
 
-    _handleFinalRedirect: function(url) {
-      this.close();
+  selectors: {
+    browserTitle: 'header > h1',
+    browerCancelButton: 'button.cancel',
+    browserContainer: '.browser-container'
+  },
 
-      if (this.oncomplete) {
-        var params;
+  get browserContainer() {
+    return this._findElement('browserContainer', this.element);
+  },
 
-        // find query string
-        var queryStringIdx = url.indexOf('?');
-        if (queryStringIdx !== -1) {
-          params = Calendar.QueryString.parse(url.slice(queryStringIdx + 1));
-        }
+  get browserTitle() {
+    return this._findElement('browserTitle', this.element);
+  },
 
-        this.oncomplete(params || {});
-      }
-    },
+  get browerCancelButton() {
+    return this._findElement('browerCancelButton', this.element);
+  },
 
-    _handleLocationChange: function(url) {
-      this.browserTitle.textContent = url;
-    },
+  _handleFinalRedirect: function(url) {
+    this.close();
 
-    _handleUserTriggeredClose: function() {
-      // close the oauth flow
-      this.close();
+    if (this.oncomplete) {
+      var params;
 
-      // trigger an event so others can cleanup
-      if (this.onabort) {
-        this.onabort();
-      }
-    },
-
-    handleEvent: function(event) {
-      switch (event.type) {
-        case 'mozbrowserlocationchange':
-          var url = event.detail;
-          if (url.indexOf(this.params.redirect_uri) === 0) {
-            return this._handleFinalRedirect(url);
-          }
-          this._handleLocationChange(url);
-          break;
-      }
-    },
-
-    open: function() {
-      if (this.browserFrame) {
-        throw new Error('attempting to open frame while another is open');
+      // find query string
+      var queryStringIdx = url.indexOf('?');
+      if (queryStringIdx !== -1) {
+        params = QueryString.parse(url.slice(queryStringIdx + 1));
       }
 
-      // add the active class
-      this.element.classList.add(Calendar.View.ACTIVE);
-
-      // handle cancel events
-      this.browerCancelButton.addEventListener(
-        'click', this._handleUserTriggeredClose
-      );
-
-      // setup browser iframe
-      var iframe = this.browserFrame =
-        document.createElement('iframe');
-
-      iframe.setAttribute('mozbrowser', true);
-      iframe.setAttribute('src', this.target);
-
-      this.browserContainer.appendChild(iframe);
-
-      iframe.addEventListener('mozbrowserlocationchange', this);
-    },
-
-    close: function() {
-      if (!this.isOpen) {
-        return;
-      }
-
-      this.browserFrame.removeEventListener(
-        'mozbrowserlocationchange', this
-      );
-
-      this.browerCancelButton.removeEventListener(
-        'click', this._handleUserTriggeredClose
-      );
-
-      this.element.classList.remove(Calendar.View.ACTIVE);
-
-      this.browserFrame.parentNode.removeChild(
-        this.browserFrame
-      );
-
-      this.browserFrame = undefined;
+      this.oncomplete(params || {});
     }
-  };
+  },
 
-  return OAuthWindow;
-}());
+  _handleLocationChange: function(url) {
+    this.browserTitle.textContent = url;
+  },
+
+  _handleUserTriggeredClose: function() {
+    // close the oauth flow
+    this.close();
+
+    // trigger an event so others can cleanup
+    if (this.onabort) {
+      this.onabort();
+    }
+  },
+
+  handleEvent: function(event) {
+    switch (event.type) {
+      case 'mozbrowserlocationchange':
+        var url = event.detail;
+        if (url.indexOf(this.params.redirect_uri) === 0) {
+          return this._handleFinalRedirect(url);
+        }
+        this._handleLocationChange(url);
+        break;
+    }
+  },
+
+  open: function() {
+    if (this.browserFrame) {
+      throw new Error('attempting to open frame while another is open');
+    }
+
+    // add the active class
+    this.element.classList.add(View.ACTIVE);
+
+    // handle cancel events
+    this.browerCancelButton.addEventListener(
+      'click', this._handleUserTriggeredClose
+    );
+
+    // setup browser iframe
+    var iframe = this.browserFrame =
+      document.createElement('iframe');
+
+    iframe.setAttribute('mozbrowser', true);
+    iframe.setAttribute('src', this.target);
+
+    this.browserContainer.appendChild(iframe);
+
+    iframe.addEventListener('mozbrowserlocationchange', this);
+  },
+
+  close: function() {
+    if (!this.isOpen) {
+      return;
+    }
+
+    this.browserFrame.removeEventListener(
+      'mozbrowserlocationchange', this
+    );
+
+    this.browerCancelButton.removeEventListener(
+      'click', this._handleUserTriggeredClose
+    );
+
+    this.element.classList.remove(View.ACTIVE);
+
+    this.browserFrame.parentNode.removeChild(
+      this.browserFrame
+    );
+
+    this.browserFrame = undefined;
+  }
+};
+
+}(Calendar));
