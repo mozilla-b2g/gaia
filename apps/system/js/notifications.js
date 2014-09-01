7fefee893e19ac334269d129b420f529f5c72d44
albertopq <apastor@mozilla.com>
2014-09-01 11:20:48 +0100

    Bug 1060699 - Ambient indicator based on unread notifications
    Fixing unit tests

--- apps/system/js/notifications.js
+++ apps/system/js/notifications.js
@@ -71,6 +71,7 @@ var NotificationScreen = {
     // will hold the count of external contributors to the notification
     // screen
     this.externalNotificationsCount = 0;
+    this.unreadNotifications = 0;
 
     window.addEventListener('utilitytrayshow', this);
     window.addEventListener('lockscreen-appclosed',
@@ -177,6 +178,7 @@ var NotificationScreen = {
 
   hideNotificationIndicator: function ns_hideNotificationIndicator() {
     this.toaster.className = '';
+    this.unreadNotifications = 0;
   },
 
   // TODO: Remove this when we ditch mozNotification (bug 952453)
@@ -388,6 +390,8 @@ var NotificationScreen = {
       (isPriorityNotification) ?
       this.container.querySelector('.priority-notifications') :
       this.container.querySelector('.other-notifications');
+
+    this.unreadNotifications++;
 
     var notificationNode = document.createElement('div');
     notificationNode.classList.add('notification');
@@ -720,11 +724,7 @@ var NotificationScreen = {
   },
 
   updateNotificationIndicator: function ns_updateNotificationIndicator(unread) {
-    var notifCount = this.externalNotificationsCount;
-
-    notifCount += this.container.querySelectorAll('.notification').length;
-
-    var indicatorSize = getIndicatorSize(notifCount);
+    var indicatorSize = getIndicatorSize(this.unreadNotifications);
 
     if (unread) {
       this.toaster.classList.add('unread');
@@ -734,6 +734,7 @@ var NotificationScreen = {
 
   incExternalNotifications: function ns_incExternalNotifications() {
     this.externalNotificationsCount++;
+    this.unreadNotifications++;
     this.updateNotificationIndicator(true);
   },
 
