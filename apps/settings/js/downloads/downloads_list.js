6997d847877164ea4edf08d7a961340769a87e4b
YFdyh000 <yfdyh000@gmail.com>
2014-09-09 21:46:12 +0800

    Bug 1061096 - Fix the edit button is not disabled when downloads list are empty list

--- apps/settings/js/downloads/downloads_list.js
+++ apps/settings/js/downloads/downloads_list.js
@@ -43,7 +43,7 @@
       emptyDownloadsContainer.hidden = true;
     }
 
-    editButton.className = isEmpty ? 'disabled' : '';
+    editButton.disabled = isEmpty ? true : false;
   }
 
   function _newDownload(download) {
