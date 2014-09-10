9d842d62a6fc1449a0f94aecbf0f7e3801f22997
Adrián de la Rosa <hola@adlr.es>
2014-09-10 11:05:11 +0200

    Bug 1060101 - [Settings][Facebook] Removing a Facebook account from Contacts will not finish if Facebook was set up during the FTU / FTE

--- shared/js/contacts/import/import_status_data.js
+++ shared/js/contacts/import/import_status_data.js
@@ -9,85 +9,87 @@
  *
 */
 
-var ImportStatusData = (function ImportStatusData() {
-  var store = null;
-  var DS_NAME = 'Import_Status_Data';
+if (!window.ImportStatusData) {
+  var ImportStatusData = (function() {
+    var store = null;
+    var DS_NAME = 'Import_Status_Data';
 
-  var datastoreLoading = false;
-  var datastoreLoaded = false;
-  var DS_LOADED_EVENT = 'datastore_loaded';
+    var datastoreLoading = false;
+    var datastoreLoaded = false;
+    var DS_LOADED_EVENT = 'datastore_loaded';
 
-  var getDatastore = function getDatastore() {
-    return new Promise(function(resolve, reject) {
-      if (!navigator.getDataStores) {
-        reject({
-          name: 'DatastoreNotEnabled'
-        });
-        return;
-      }
+    var getDatastore = function getDatastore() {
+      return new Promise(function(resolve, reject) {
+        if (!navigator.getDataStores) {
+          reject({
+            name: 'DatastoreNotEnabled'
+          });
+          return;
+        }
 
-      if (datastoreLoaded) {
-        resolve(store);
-        return;
-      }
-
-      if (datastoreLoading) {
-        document.addEventListener(DS_LOADED_EVENT, function loadedHandler() {
-          document.removeEventListener(DS_LOADED_EVENT, loadedHandler);
+        if (datastoreLoaded) {
           resolve(store);
-        });
-      }
+          return;
+        }
 
-      datastoreLoading = true;
-      navigator.getDataStores(DS_NAME).then(function(stores) {
-        store = stores[0];
-      }, reject).then(function() {
-          datastoreLoading = false;
-          datastoreLoaded = true;
-          document.dispatchEvent(new CustomEvent(DS_LOADED_EVENT));
+        if (datastoreLoading) {
+          document.addEventListener(DS_LOADED_EVENT, function loadedHandler() {
+            document.removeEventListener(DS_LOADED_EVENT, loadedHandler);
+            resolve(store);
+          });
+        }
 
-          resolve(store);
-      }, reject);
-    });
-  };
+        datastoreLoading = true;
+        navigator.getDataStores(DS_NAME).then(function(stores) {
+          store = stores[0];
+        }, reject).then(function() {
+            datastoreLoading = false;
+            datastoreLoaded = true;
+            document.dispatchEvent(new CustomEvent(DS_LOADED_EVENT));
 
-  var put = function put(key, obj) {
-    return new Promise(function(resolve, reject) {
-      getDatastore().then(function success(store) {
-        store.put(obj, key).then(resolve, reject);
-      }, reject);
-    });
-  };
+            resolve(store);
+        }, reject);
+      });
+    };
 
-  var remove = function remove(key) {
-    return new Promise(function(resolve, reject) {
-      getDatastore().then(function success(store) {
-        store.remove(key).then(resolve, reject);
-      }, reject);
-    });
-  };
+    var put = function put(key, obj) {
+      return new Promise(function(resolve, reject) {
+        getDatastore().then(function success(store) {
+          store.put(obj, key).then(resolve, reject);
+        }, reject);
+      });
+    };
 
-  var get = function get(key) {
-    return new Promise(function(resolve, reject) {
-      getDatastore().then(function success(store) {
-        store.get(key).then(resolve, reject);
-      }, reject);
-    });
-  };
+    var remove = function remove(key) {
+      return new Promise(function(resolve, reject) {
+        getDatastore().then(function success(store) {
+          store.remove(key).then(resolve, reject);
+        }, reject);
+      });
+    };
 
-  var clear = function clear() {
-    return new Promise(function(resolve, reject) {
-      getDatastore().then(function success(store) {
-        store.clear().then(resolve, reject);
-      }, reject);
-    });
-  };
+    var get = function get(key) {
+      return new Promise(function(resolve, reject) {
+        getDatastore().then(function success(store) {
+          store.get(key).then(resolve, reject);
+        }, reject);
+      });
+    };
 
-  return {
-    put: put,
-    remove: remove,
-    get: get,
-    clear: clear
-  };
+    var clear = function clear() {
+      return new Promise(function(resolve, reject) {
+        getDatastore().then(function success(store) {
+          store.clear().then(resolve, reject);
+        }, reject);
+      });
+    };
 
-})();
+    return {
+      put: put,
+      remove: remove,
+      get: get,
+      clear: clear
+    };
+
+  })();
+}
