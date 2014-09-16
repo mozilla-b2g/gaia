6eef59001dc76083c8f6454d6644ba5fc78497c9
Zac <zcampbell@mozilla.com>
2014-09-16 14:39:56 +0100

    Bug 1060381 - test_gallery_crop_photo is failing - refactor to reference image metadata

--- tests/python/gaia-ui-tests/gaiatest/apps/gallery/app.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/gallery/app.py
@@ -4,6 +4,7 @@
 
 from marionette.by import By
 from gaiatest.apps.base import Base
+from gaiatest.apps.base import PageRegion
 
 import gaiatest.apps.camera.app
 
@@ -32,10 +33,10 @@ class Gallery(Base):
 
     @property
     def gallery_items_number(self):
-        return len(self.marionette.find_elements(*self._gallery_items_locator))
+        return len(self.thumbnails)
 
     def tap_first_gallery_item(self):
-        first_gallery_item = self.marionette.find_elements(*self._gallery_items_locator)[0]
+        first_gallery_item = self.thumbnails[0]
         if self.is_element_displayed(*self._thumbnail_list_view_locator):
             from gaiatest.apps.gallery.regions.fullscreen_image import FullscreenImage as NextView
         else:
@@ -55,6 +56,11 @@ class Gallery(Base):
     def are_gallery_items_displayed(self):
         return self.marionette.find_element(*self._gallery_items_locator).is_displayed()
 
+    @property
+    def thumbnails(self):
+        return [self.Thumbnail(self.marionette, thumbnail, index)
+                for index, thumbnail in enumerate(self.marionette.find_elements(*self._gallery_items_locator))]
+
     def switch_to_camera(self):
         switch_to_camera_button = self.marionette.find_element(*self._switch_to_camera_button_locator)
         switch_to_camera_button.tap()
@@ -62,3 +68,22 @@ class Gallery(Base):
         self.wait_for_condition(lambda m: self.apps.displayed_app.name == camera_app.name)
         self.apps.switch_to_displayed_app()
         return camera_app
+
+    class Thumbnail(PageRegion):
+
+        def __init__(self, marionette, element, index):
+            self.root_element = element
+            # Save the index of the element so we can match it to the js files object
+            self.index = index
+            Base.__init__(self, marionette)
+
+        @property
+        def absolute_image_size(self):
+            '''
+            The dimensions of the image on disk, is drawn from the image's metadata
+            '''
+            return {"height": self.marionette.execute_script('return window.wrappedJSObject.files[%s].metadata.height' % self.index),
+                "width": self.marionette.execute_script('return window.wrappedJSObject.files[%s].metadata.width' % self.index)}
+
+        def tap(self):
+            self.root_element.tap()
