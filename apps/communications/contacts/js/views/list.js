987645cd189790e27ceb49497028ed32e8d00c90
Francisco Jordano <fjordano@mozilla.com>
2014-09-17 17:05:12 +0100

    Bug 1064256 - [Contacts] "TypeError: iceGroup is null" v2.1

--- apps/communications/contacts/js/views/list.js
+++ apps/communications/contacts/js/views/list.js
@@ -319,6 +319,7 @@ contacts.List = (function() {
     };
 
     var complete = function complete() {
+      iceGroup = null;
       initConfiguration(function onInitConfiguration() {
         getContactsByGroup(onError, contacts);
       });
@@ -844,15 +845,17 @@ contacts.List = (function() {
   }
 
   function displayICEIndicator(ids) {
+    if (!iceGroup) {
+      buildICEGroup();
+    }
+
     if (!ids || ids.length === 0) {
       hideICEGroup();
       return;
     }
 
     iceContacts = ids;
-    if (iceGroup === null) {
-      buildICEGroup();
-    }
+    
     showICEGroup();
   }
 
