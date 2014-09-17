c80faf45f2224d027472c4d869ce8b6a9d1a682e
Julien Wajsberg <felash@gmail.com>
2014-09-17 15:55:55 +0200

    Bug 1035133 - [TestAgent] Check that the TestAgent object is here and display an error message otherwise r=yurenju r=rickychien

--- dev_apps/test-agent/agent.js
+++ dev_apps/test-agent/agent.js
@@ -1,43 +1,55 @@
+'use strict';
 (function(window){
+
   var Loader = window.CommonResourceLoader = {},
       domain = document.location.host;
 
   Loader.domain = document.location.protocol + '//' + domain;
   Loader.url = function(url){
     return this.domain + url;
-  }
-
-  Loader.script = function(url, doc){
-    doc = doc || document;
-    doc.write('<script type="application/javascript;version=1.8" src="' + this.url(url) + '"><\/script>');
-    return this;
   };
 
-  Loader.stylesheet = function(url, doc){
-    doc = doc || document;
-    doc.write('<link rel="stylesheet" type="text/css" href="' + this.url(url) + '">');
-    return this;
-    };
+  Loader.script = function(url){
+    var script = document.createElement('script');
+    script.type = 'application/javascript;version=1.8';
+    script.async = false;
+    script.src = this.url(url);
+
+    var promise = new Promise((resolve, reject) => {
+      script.onload = resolve;
+      script.onerror = reject;
+    });
+
+    document.body.appendChild(script);
+    return promise;
+  };
+
+  Loader.stylesheet = function(url){
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = this.url(url);
+
+    var promise = new Promise((resolve, reject) => {
+      link.onload = resolve;
+      link.onerror = reject;
+    });
+
+    document.body.appendChild(link);
+    return promise;
+  };
 }(this));
 
-CommonResourceLoader
-  .stylesheet('/common/vendor/test-agent/test-agent.css')
-  .stylesheet('/common/vendor/mocha/mocha.css');
 
-CommonResourceLoader
-  .script('/common/vendor/mocha/mocha.js')
-  .script('/common/vendor/sinon/sinon.js')
-  .script('/common/vendor/blanket/blanket.js')
-  .script('/common/vendor/test-agent/test-agent.js')
-  .script('/common/test/test_url_resolver.js')
-  .script('/common/test/agent.js');
-
-window.onerror = function() {
-  var args = Array.prototype.slice.call(arguments);
-  var json = JSON.stringify(args);
-  var debug = document.createElement('p');
-  debug.style.color = 'red';
-
-  document.body.appendChild(debug);
-}
+Promise.all([
+  CommonResourceLoader.stylesheet('/common/vendor/test-agent/test-agent.css'),
+  CommonResourceLoader.stylesheet('/common/vendor/mocha/mocha.css'),
+  CommonResourceLoader.script('/common/vendor/mocha/mocha.js'),
+  CommonResourceLoader.script('/common/vendor/sinon/sinon.js'),
+  CommonResourceLoader.script('/common/vendor/blanket/blanket.js'),
+  CommonResourceLoader.script('/common/vendor/test-agent/test-agent.js'),
+  CommonResourceLoader.script('/common/test/test_url_resolver.js'),
+  CommonResourceLoader.script('/common/test/agent.js')
+]).catch(function() {
+  document.getElementById('loading-error-message').hidden = false;
+});
 
