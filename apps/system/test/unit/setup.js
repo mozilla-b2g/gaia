412db00a7c7478743792ac3df1dec630786b4d33
Kevin Grandon <kevingrandon@yahoo.com>
2014-09-18 13:16:50 -0700

    Bug 1038548 - Try to address undefined mozL10n issue

--- apps/system/test/unit/setup.js
+++ apps/system/test/unit/setup.js
@@ -1,3 +1,15 @@
 'use strict';
 
-require('/shared/test/unit/mocks/mocks_helper.js');
+(function() {
+
+  function l10nLink(href) {
+    var resource = document.createElement('link');
+    resource.setAttribute('href', href);
+    resource.setAttribute('rel', 'localization');
+    document.head.appendChild(resource);
+  }
+
+  l10nLink('/locales/system.{locale}.properties');
+  require('/shared/test/unit/mocks/mocks_helper.js');
+
+}());
