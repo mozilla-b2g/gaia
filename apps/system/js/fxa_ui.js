95a51689acd0488b3ba79abe2423cdcc132fff4a
Fernando Jiménez <ferjmoreno@gmail.com>
2014-09-18 14:00:40 +0200

    Bug 1068356 - Authentication screen remains indefinitely when long tapping on the homescreen button in the FxAccount login proccess. r=alive
    Conflicts:
    	apps/system/js/fxa_ui.js
    	apps/system/js/mobileid_dialog.js

--- apps/system/js/fxa_ui.js
+++ apps/system/js/fxa_ui.js
@@ -5,6 +5,7 @@
 var FxAccountsUI = {
   dialog: null,
   panel: null,
+  iframe: null,
   onerrorCb: null,
   onsuccessCb: null,
 
@@ -62,8 +63,11 @@ var FxAccountsUI = {
   },
 
   // Method for reseting the panel.
-  reset: function fxa_ui_reset() {
+  reset: function fxa_ui_reset(reason) {
     this.panel.innerHTML = '';
+    if (reason == 'home' || reason == 'holdhome') {
+      this.onerrorCb && this.onerrorCb('DIALOG_CLOSED_BY_USER');
+    }
     this.onerrorCb = null;
     this.onsuccessCb = null;
   },
