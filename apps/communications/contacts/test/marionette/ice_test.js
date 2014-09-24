ab1c53919f2ccd6fef95ece3bfad2ae812b0b120
crdlc <crdlc@tid.es>
2014-09-24 17:31:59 +0200

    Bug 1071632 - [Contacts] [ICE] ICE Group is not properly refreshed after removing all phone numbers of an ICE Contact

--- apps/communications/contacts/test/marionette/ice_test.js
+++ apps/communications/contacts/test/marionette/ice_test.js
@@ -10,6 +10,24 @@ marionette('Contacts > ICE contacts', function() {
   var selectors;
   var actions = new Actions(client);
 
+  function setFirstContactAsICE() {
+    client.helper.waitForElement(selectors.settingsButton)
+      .click();
+
+    client.helper.waitForElement(selectors.setIceButton)
+      .click();
+
+    var iceSwitch1 = client.helper.waitForElement(selectors.iceSwitch1);
+    subject.clickOn(iceSwitch1);
+
+    var iceButton1 = client.helper.waitForElement(selectors.iceButton1);
+    subject.clickOn(iceButton1);
+
+    var listContactFirstText = 
+      client.helper.waitForElement(selectors.listContactFirstText);
+    subject.clickOn(listContactFirstText);
+  }
+
   setup(function() {
     subject = new Contacts(client);
     subject.launch();
@@ -117,23 +135,6 @@ marionette('Contacts > ICE contacts', function() {
   });
 
   suite('ICE settings', function() {
-    function setFirstContactAsICE() {
-      client.helper.waitForElement(selectors.settingsButton)
-        .click();
-
-      client.helper.waitForElement(selectors.setIceButton)
-        .click();
-
-      var iceSwitch1 = client.helper.waitForElement(selectors.iceSwitch1);
-      subject.clickOn(iceSwitch1);
-
-      var iceButton1 = client.helper.waitForElement(selectors.iceButton1);
-      subject.clickOn(iceButton1);
-
-      var listContactFirstText = 
-        client.helper.waitForElement(selectors.listContactFirstText);
-      subject.clickOn(listContactFirstText);
-    }
 
     suite('Confirm window when deleting numbers from ICE contacts', function() {
       setup(function() {
@@ -261,6 +262,75 @@ marionette('Contacts > ICE contacts', function() {
       assert.equal(confirmText, expectedResult);
 
     });
+  });
+
+  suite('ICE list', function() {
+
+    function getNumberICEContacts() {
+      return client.executeScript(function(selector) {
+        return document.querySelectorAll(selector).length;
+      }, [selectors.iceContact]);
+    }
+
+    test('ICE list empty after removing phone', function() {
+
+      var contact = {
+        givenName: 'Raul Gonzalez Blanco',
+        tel: '123123123'
+      };
+
+      subject.addContact(contact);
+
+      setFirstContactAsICE();
+
+      // Pressing back button to go back to settings
+      var contactsHeader = client.helper.
+                                  waitForElement(selectors.contactListHeader);
+      actions.wait(0.5)
+             .tap(contactsHeader, 10, 10)
+             .perform();
+
+      // Closing settings view
+      var closeSettings = client.helper.waitForElement(selectors.settingsClose);
+      subject.clickOn(closeSettings);
+      
+      // Click on ICE contacts item in the main list
+      var openIce = client.helper.waitForElement(selectors.iceGroupOpen);
+      subject.clickOn(openIce);
+
+      // Go to details for this contact
+      var iceContact = client.helper.waitForElement(selectors.iceContact);
+      subject.clickOn(iceContact);
+
+      // Pressing the button to edit the contact
+      var edit = client.helper.waitForElement(selectors.detailsEditContact);
+      subject.clickOn(edit);
+
+      // Waiting for edit view
+      subject.waitForFormShown();
+
+      // Deleting the first phone number
+      client.findElement(selectors.formDelFirstTel).click();
+
+      // Save the changes when the 'Update' button is enabled
+      var save = client.findElement(selectors.formSave);
+      save.enabled();
+      save.click();
+
+      // The user is aware of his contact will be deleted from ICE Contacts
+      client.helper.waitForElement(selectors.confirmDismiss).click();
+
+      // Go from detail to ICE list
+      var detailsHeader = client.helper.waitForElement(selectors.detailsHeader);
+      actions.wait(0.5)
+             .tap(detailsHeader, 10, 10)
+             .perform();
+
+      client.helper.waitForElement(selectors.iceHeader);
+
+      assert.equal(getNumberICEContacts(), 0);
+    });
+
   });
 
 });
