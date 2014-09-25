99cd64fce6bf5fcbae2cf55b98ee1a7fdd7cc39c
Alive Kuo <alegnadise@gmail.com>
2014-09-25 18:06:37 +0800

    Bug 1072757 - Implement SettingsCore, BaseModule, Service Registration System

---
+++ apps/system/js/core.js
@@ -0,0 +1,54 @@
+/* global BaseModule */
+'use strict';
+
+(function(exports) {
+  /**
+   * This is the bootstrap module of the system app.
+   * It is responsible to instantiate and start the other core modules
+   * and sub systems per API.
+   */
+  var Core = function() {
+  };
+
+  BaseModule.create(Core, {
+    name: 'Core',
+
+    REGISTRY: {
+      'mozSettings': 'SettingsCore'
+    },
+
+    _start: function() {
+      for (var api in this.REGISTRY) {
+        this.debug('Detecting API: ' + api +
+          ' and corresponding module: ' + this.REGISTRY[api]);
+        if (navigator[api]) {
+          this.debug('API: ' + api + ' found, starting the handler.');
+          this.startAPIHandler(api, this.REGISTRY[api]);
+        } else {
+          this.debug('API: ' + api + ' not found, skpping the handler.');
+        }
+      }
+    },
+
+    startAPIHandler: function(api, handler) {
+      BaseModule.lazyLoad([handler]).then(function() {
+        var moduleName = BaseModule.lowerCapital(handler);
+        this[moduleName] =
+          BaseModule.instantiate(handler, navigator[api], this);
+        if (!this[moduleName]) {
+          return;
+        }
+        this[moduleName].start && this[moduleName].start();
+      }.bind(this));
+    },
+
+    _stop: function() {
+      for (var api in this.REGISTRY) {
+        var moduleName =
+            this.REGISTRY[api].charAt(0).toUpperCase() +
+            this.REGISTRY[api].slice(1);
+        this[moduleName] && this[moduleName].stop();
+      }
+    }
+  });
+}(window));
--- apps/system/test/unit/system_test.js
+++ apps/system/test/unit/system_test.js
@@ -4,14 +4,17 @@
 mocha.globals(['System']);
 
 requireApp('system/test/unit/mock_app_window_manager.js');
+require('/shared/test/unit/mocks/mock_lazy_loader.js');
 
 var mocksForSystem = new MocksHelper([
-  'AppWindowManager'
+  'AppWindowManager', 'LazyLoader'
 ]).init();
 
 suite('system/System', function() {
+  var clock;
   mocksForSystem.attachTestHelpers();
   setup(function(done) {
+    clock = this.sinon.useFakeTimers();
     requireApp('system/js/system.js', done);
   });
 
@@ -22,16 +25,111 @@ suite('system/System', function() {
     assert.isTrue(System.isBusyLoading());
   });
 
-  test('Request would fire lock/unlock event', function() {
-    var stubDispatchEvent = this.sinon.stub(window, 'dispatchEvent');
-    System.request('lock');
-    stubDispatchEvent.calledWith(sinon.match(function (evt) {
-      return 'lockscreen-request-lock' === evt.type;
-    }));
+  suite('Services', function() {
+    teardown(function() {
+      System._services.clear();
+      System._providers.clear();
+    });
 
-    System.request('unlock');
-    stubDispatchEvent.calledWith(sinon.match(function (evt) {
-      return 'lockscreen-request-unlock' === evt.type;
-    }));
+    test('Service provider is online.', function(done) {
+      var spy = this.sinon.spy();
+      var fakeLockscreenWindowManager = {
+        lock: spy
+      };
+      System.register('lock', fakeLockscreenWindowManager);
+      System.request('lock').then(function() {
+        assert.isTrue(spy.called);
+        done();
+      });
+    });
+
+    test('Service provider is offline.', function() {
+      System.request('lock');
+      var fakeLockscreenWindowManager = {
+        name: 'fakeLWM',
+        lock: function() {}
+      };
+      var stubLock = this.sinon.stub(fakeLockscreenWindowManager, 'lock');
+      System.register('lock', fakeLockscreenWindowManager);
+      assert.isTrue(stubLock.called);
+    });
+
+    test('Specific service provider is online.', function() {
+      var fakeLockscreenWindowManager = {
+        name: 'fakeLWM',
+        lock: function() {}
+      };
+      System.register('lock', fakeLockscreenWindowManager);
+      var stubLock = this.sinon.stub(fakeLockscreenWindowManager, 'lock');
+      System.request('fakeLWM:lock');
+      assert.isTrue(stubLock.called);
+    });
+
+    test('Specific service provider was ever online.', function() {
+      var fakeLockscreenWindowManager = {
+        name: 'fakeLWM',
+        lock: function() {}
+      };
+      System.register('lock', fakeLockscreenWindowManager);
+      var stubLock = this.sinon.stub(fakeLockscreenWindowManager, 'lock');
+      System.unregister('lock', fakeLockscreenWindowManager);
+      System.request('fakeLWM:lock');
+      assert.isFalse(stubLock.called);
+    });
+
+    test('Specific service provider is offline.', function() {
+      System.request('fakeLWM:lock');
+      var fakeLockscreenWindowManager = {
+        name: 'fakeLWM',
+        lock: function() {}
+      };
+      var stubLock = this.sinon.stub(fakeLockscreenWindowManager, 'lock');
+      System.register('lock', fakeLockscreenWindowManager);
+      assert.isTrue(stubLock.called);
+    });
+
+    test('Passing arguments to specific service provider.', function() {
+      var client = {
+        value: null,
+        observe: function(value) {
+          this.value = value;
+        }
+      };
+      System.request('addObserver', client);
+      var fakeSettingsServer = {
+        name: 'fakeSettingsServer',
+        addObserver: function(client) {
+          this.client = client;
+        }
+      };
+      System.register('addObserver', fakeSettingsServer);
+      fakeSettingsServer.client.observe(2);
+      assert.equal(client.value, 2);
+    });
+
+    test('Chaining promises.', function(done) {
+      var client = {
+        value: 1,
+        read: function(value) {
+          var self = this;
+          System.request('read').then(function(value) {
+            self.value = value;
+            assert.equal(self.value, 2);
+
+            done();
+          });
+        }
+      };
+      var fakeSettingsServer = {
+        name: 'fakeSettingsServer',
+        read: function() {
+          return new Promise(function(resolve) {
+            resolve(2);
+          });
+        }
+      };
+      client.read();
+      System.register('read', fakeSettingsServer);
+    });
   });
 });
