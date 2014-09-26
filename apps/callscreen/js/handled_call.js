74410dda376417e04b8ad7c9c4252d9e7688ffcb
Tzu-Lin Huang <tzhuang@mozilla.com>
2014-09-26 14:52:57 +0800

    Bug 1074601 - Take SIM card 2 into consideration when checking voicemail number
    Conflicts:
    	apps/callscreen/test/unit/handled_call_test.js
    	apps/communications/dialer/test/unit/dialer_test.js
    	build/jshint/xfail.list

--- apps/callscreen/js/handled_call.js
+++ apps/callscreen/js/handled_call.js
@@ -157,7 +157,8 @@ HandledCall.prototype.updateCallNumber = function hc_updateCallNumber() {
     return;
   }
 
-  Voicemail.check(number, function(isVoicemailNumber) {
+  Voicemail.check(number, this.call.serviceId).then(
+  function(isVoicemailNumber) {
     if (isVoicemailNumber) {
       LazyL10n.get(function localized(_) {
         node.textContent = _('voiceMail');
