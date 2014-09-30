ecdf4680f5bf78d00aadf58bdf7375854387a79a
Jan Jongboom <janjongboom@gmail.com>
2014-09-30 17:21:25 +0200

    Bug 1073870 - Autocorrect word does not get cleared when moving the cursor around

--- apps/keyboard/js/imes/latin/latin.js
+++ apps/keyboard/js/imes/latin/latin.js
@@ -708,6 +708,9 @@
       // Mark the auto-correction so the renderer can highlight it
       words[0] = '*' + words[0];
     }
+    else {
+      autoCorrection = null;
+    }
 
     keyboard.sendCandidates(words);
   }
