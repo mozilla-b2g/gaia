e6207091cf045581be27a7f4adc3f66e601ceca8
gaye <gaye@mozilla.com>
2014-09-29 22:12:06 -0400

    Bug 1068236 - Bump test agent to 0.25.0

--- apps/sharedtest/test/unit/l10n/lib/compiler/dos_test.js
+++ apps/sharedtest/test/unit/l10n/lib/compiler/dos_test.js
@@ -1,13 +1,12 @@
-/* global it, before, beforeEach, assert:true, describe, requireApp */
+/* global it, before, beforeEach, assert, describe, requireApp */
 /* jshint -W101 */
 'use strict';
-var compile, assert;
+var compile;
 
 if (typeof navigator !== 'undefined') {
   requireApp('sharedtest/test/unit/l10n/lib/compiler/header.js');
 } else {
   compile = require('./header.js').compile;
-  assert = require('./header.js').assert;
 }
 
 // Bug 803931 - Compiler is vulnerable to the billion laughs attack
