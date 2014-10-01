c218b7228fb7eacf35d59fb54467387e4a38027c
Dale Harvey <dale@arandomurl.com>
2014-10-01 08:40:52 +0200

    Bug 1071057 - Load sim variant top site customisations. r=kgrandon

--- apps/search/test/marionette/browser_test.js
+++ apps/search/test/marionette/browser_test.js
@@ -24,4 +24,33 @@ marionette('Browser test', function() {
       return search.getTopSites().length == 2;
     });
   });
+
+  test('Ensure sim variant preloaded sites exist', function() {
+
+    client.executeScript(function() {
+      var settings = window.wrappedJSObject.navigator.mozSettings;
+      settings.createLock().set({
+        'operatorResources.data.topsites': {
+          'topSites': [{
+            url: 'http://example1.org',
+            title: 'Example1'
+          }, {
+            url: 'http://example2.org',
+            title: 'Example2'
+          }, {
+            url: 'http://example3.org',
+            title: 'Example3'
+          }]
+        }
+      });
+    });
+
+    client.apps.launch(Search.URL);
+    client.apps.switchToApp(Search.URL);
+
+    client.waitFor(function() {
+      return search.getTopSites().length == 3;
+    });
+  });
+
 });
