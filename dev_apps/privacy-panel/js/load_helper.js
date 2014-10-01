45b3d57c9035f92f7cee0d96596562a36bcdd400
marta <marta@martas-MacBook-Air-2.local>
2014-10-01 10:01:44 -0700

    pull request for Privacy Panel

---
+++ dev_apps/privacy-panel/js/load_helper.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function() {
+
+  function LoadHelper() {}
+
+  LoadHelper.prototype = {
+    /**
+     * Change page
+     * @param event
+     */
+    changePage: function(event) {
+      var target, id = this.hash.replace('#', '');
+
+      event.preventDefault();
+
+      if ( ! id) {
+        return;
+      }
+
+      target = document.getElementById(id);
+      showSection(target);
+    },
+
+    /**
+     * Register events for given element
+     * @param elements
+     */
+    registerEvents: function(elements) {
+      var element, links, el;
+
+      for (element of elements) {
+        links = element.querySelectorAll('.pp-link');
+
+        for (el of links) {
+          el.addEventListener('click', this.changePage);
+        }
+      }
+    }
+  };
+
+  /**
+   * Show section
+   * @param element
+   */
+  var showSection = function(element) {
+    var sections = document.querySelectorAll('section');
+
+    for (var section of sections) {
+      section.style.display = 'none';
+    }
+
+    element.style.display = 'block';
+  };
+
+  window.LoadHelper = new LoadHelper();
+}());
