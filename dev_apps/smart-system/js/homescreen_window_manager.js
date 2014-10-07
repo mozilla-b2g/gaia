cf09b65a3bc1c9551f565600549b5b1dafb842a1
John Hu <im@john.hu>
2014-10-07 14:58:39 +0800

    bug-1074040

--- dev_apps/smart-system/js/homescreen_window_manager.js
+++ dev_apps/smart-system/js/homescreen_window_manager.js
@@ -1,18 +1,21 @@
-/* global homescreenLauncher, System */
+/* global homescreenLauncher, System, LazyLoader, LandingAppLauncher */
 
 'use strict';
 (function(exports) {
+  const LANDING_APP_WINDOW_URL = '/js/landing_app_window.js';
+  const LANDING_APP_LAUNCHER_URL = '/js/landing_app_launcher.js';
+
   /**
-   * HomescreenWindowManager manages the show/hide of HomescreenWindow and
+   * TVHomescreenWindowManager manages the show/hide of HomescreenWindow and
    * HomescreenLauncher instances.
    *
-   * @class HomescreenWindowManager
+   * @class TVHomescreenWindowManager
    * @requires HomescreenLauncher
    * @requires System
    */
-  function HomescreenWindowManager() {}
+  function TVHomescreenWindowManager() {}
 
-  HomescreenWindowManager.prototype = {
+  TVHomescreenWindowManager.prototype = {
     DEBUG: false,
     CLASS_NAME: 'HomescreenWindowManager',
 
@@ -22,11 +25,12 @@
      * ready.
      *
      * @access public
-     * @memberOf HomescreenWindowManager.prototype
+     * @memberOf TVHomescreenWindowManager.prototype
      * @type {boolean}
      */
     get ready() {
-      return homescreenLauncher.ready;
+      return homescreenLauncher.ready && this.landingAppLauncher &&
+             this.landingAppLauncher.ready;
     },
 
     debug: function hwm_debug() {
@@ -43,10 +47,20 @@
      * @memberOf HomescreenWindowManager.prototype
      */
     start: function hwm_start() {
+      var self = this;
+      LazyLoader.load([LANDING_APP_WINDOW_URL, LANDING_APP_LAUNCHER_URL],
+        function() {
+          console.log('trying to start landingAppLauncher');
+          self.landingAppLauncher = new window.LandingAppLauncher();
+          self.landingAppLauncher.start();
+        });
       window.addEventListener('appswitching', this);
       window.addEventListener('ftuskip', this);
-      window.addEventListener('open-app', this, true);
-      window.addEventListener('webapps-launch', this, true);
+      window.addEventListener('open-app', this);
+      window.addEventListener('webapps-launch', this);
+      window.addEventListener('appopened', this);
+      window.addEventListener('homescreenopened', this);
+      window.addEventListener('landingappopened', this);
     },
 
     /**
@@ -55,34 +69,106 @@
      * @memberOf HomescreenWindowManager.prototype
      */
     stop: function hwm_stop() {
+      this.landingAppLauncher.stop();
       window.removeEventListener('appswitching', this);
       window.removeEventListener('ftuskip', this);
-      window.removeEventListener('open-app', this, true);
-      window.removeEventListener('webapps-launch', this, true);
+      window.removeEventListener('open-app', this);
+      window.removeEventListener('webapps-launch', this);
+      window.removeEventListener('appopened', this);
     },
 
     handleEvent: function hwm_handleEvent(evt) {
       switch(evt.type) {
         case 'appswitching':
+          this.getHomescreen().showFadeOverlay();
           this.getHomescreen().fadeOut();
           break;
         case 'ftuskip':
           // XXX: There's a race between lockscreenWindow and homescreenWindow.
           // If lockscreenWindow is instantiated before homescreenWindow,
           // we should not display the homescreen here.
-          if (System.locked) {
-            this.getHomescreen().setVisible(false);
-          }
+          this.getHomescreen().setVisible(!System.locked);
           break;
         case 'open-app':
         case 'webapps-launch':
           var detail = evt.detail;
-          if (detail.manifestURL === homescreenLauncher.manifestURL) {
-            this.getHomescreen();
+          console.log('trying to ' + evt.type + ': ' + detail.manifestURL);
+          if (detail.manifestURL === homescreenLauncher.manifestURL ||
+              detail.manifestURL === this.landingAppLauncher.manifestURL) {
+            this.launchHomescreen(evt, detail.manifestURL);
+            evt.stopPropagation();
             evt.stopImmediatePropagation();
+            evt.preventDefault();
+          }
+          break;
+        case 'appopened':
+          var detail = evt.detail;
+          if (detail.isHomescreen) {
+            this._activeHome = ('LandingAppWindow' === detail.CLASS_NAME) ?
+                                 this.landingAppLauncher : homescreenLauncher;
+          } else {
+            // If we don't have _activeHome that means ftu is opened, we need to
+            // make sure landing app is the one after ftu.
+            if (this._activeHome) {
+              this._activeHome.getHomescreen().setVisible(false);
+              this._activeHome.getHomescreen().close('immediate');
+              this._activeHome = null;
+            }
+          }
+          break;
+        case 'homescreenopened':
+          if (exports.homescreenLauncher) {
+            homescreenLauncher.getHomescreen().showFadeOverlay();
+          }
+          if (this.landingAppLauncher) {
+            this.landingAppLauncher.getHomescreen().hideFadeOverlay();
+          }
+          break;
+        case 'landingappopened':
+          if (exports.homescreenLauncher) {
+            homescreenLauncher.getHomescreen().hideFadeOverlay();
+          }
+          if (this.landingAppLauncher) {
+            this.landingAppLauncher.getHomescreen().showFadeOverlay();
           }
           break;
       }
+    },
+
+    publish: function awm_publish(event, detail) {
+      var evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(event, true, false, detail || this);
+
+      this.debug('publish: ' + event);
+      window.dispatchEvent(evt);
+    },
+
+    launchHomescreen: function launchHomescreen(evt, manifestURL) {
+      console.log('trying to launch home: ' + manifestURL);
+      if (!this.landingAppLauncher.hasLandingApp) {
+        // cal getHomescreen to ensure it.
+        this.getHomescreen();
+        return;
+      }
+
+      if (this._activeHome) {
+        console.log('currently in home: ' + this._activeHome.manifestURL);
+        if (this._activeHome.manifestURL !== manifestURL) {
+          console.log('switch to : ' + manifestURL);
+          // in homeA trying to switch to homeB
+          
+          this.publish('home');
+        } else {
+          console.log('ensure self : ' + manifestURL);
+          // cal getHomescreen to ensure it.
+          this.getHomescreen();
+        }
+      } else if (homescreenLauncher.manifestURL === manifestURL) {
+        console.log('app to origin home : ' + manifestURL);
+        // in appX trying to switch to home
+        this.publish('home');
+      }
+      // We don't support appX trying to switch to landing app
     },
 
     /**
@@ -92,10 +178,37 @@
      * @memberOf HomescreenWindowManager.prototype
      */
     getHomescreen: function getHomescreen(isHomeEvent) {
-      if (!exports.homescreenLauncher || !exports.homescreenLauncher.ready) {
+      if (!exports.homescreenLauncher || !exports.homescreenLauncher.ready ||
+          !this.landingAppLauncher || !this.landingAppLauncher.ready) {
         return null;
       }
-      var home  = homescreenLauncher.getHomescreen(true);
+
+      if (this.landingAppLauncher.hasLandingApp) {
+
+        // use landing app launcher as first home launcher
+        if (!this._activeHome) {
+          // If this._activeHome is null, the active app is normal app. We need
+          // to show normal homescreen app.
+          //
+          // If this._activeHome is undefined, the active app is FTU/lockscreen
+          // or other visible but not normal app. We need to show landing app.
+          this._activeHome = this._activeHome === null ? homescreenLauncher :
+                                                        this.landingAppLauncher;
+        } else if (isHomeEvent) {
+          // If press home when one home app active, we need to swap the
+          // launcher.
+          this._activeHome.getHomescreen().setVisible(false);
+          this._activeHome.getHomescreen().close('immediate');
+          this._activeHome = this._activeHome === this.landingAppLauncher ?
+                             homescreenLauncher : this.landingAppLauncher;
+        }
+      } else if (!this._activeHome) {
+        // If we don't have landing app, we need to initialize active home as
+        // homescreen launcher.
+        this._activeHome = homescreenLauncher;
+      }
+
+      var home  = this._activeHome.getHomescreen(true);
       if (isHomeEvent) {
         home.ensure(true);
       }
@@ -103,5 +216,5 @@
     }
   };
 
-  exports.HomescreenWindowManager = HomescreenWindowManager;
+  exports.HomescreenWindowManager = TVHomescreenWindowManager;
 }(window));
---
+++ dev_apps/smart-system/js/landing_app_launcher.js
@@ -0,0 +1,239 @@
+'use strict';
+/* global applications, SettingsListener, LandingAppWindow, TrustedUIManager */
+(function(exports) {
+  /**
+   * LandingAppLauncher is responsible for launching the landing app window
+   * instance and make sure it's a singleton.
+   *
+   * Every extermal modules should use
+   * <code>window.homescreenLauncher.getHomescreen()</code>
+   * to access the homescreen window instance. Since window.homescreenLauncher
+   * should be instantiated and started in bootstrap.js
+   *
+   * @example
+   * var home = HomescreenLauncher.getHomescreen();
+   * home.open(); // Do the open animation.
+   *
+   * @class HomescreenLauncher
+   * @requires module:TrustedUIManager
+   * @requires module:Applications
+   * @requires module:SettingsListener
+   * @requires LandingAppWindow
+   * @requires Applications
+   */
+  var LandingAppLauncher = function() {
+  };
+
+  /**
+   * Fired when homescreen launcher detect 'homescreen.manifestURL' changed
+   * @event HomescreenLauncher#homescreen-changed
+   */
+  /**
+   * Fired when homescreen launcher is done retriving 'homescreen.manifestURL'
+   * @event HomescreenLauncher#homescreen-ready
+   */
+
+
+  LandingAppLauncher.prototype = {
+    _settingsKey: 'landing_app.manifestURL',
+
+    _currentManifestURL: '',
+
+    _instance: undefined,
+
+    _started: false,
+
+    _ready: false,
+
+    /**
+     * Homescreen launcher is ready or not. Homescreen launcher is ready
+     * only when it is done retrieving 'homescreen.manifestURL'
+     * from settings DB.
+     *
+     * @access public
+     * @memberOf HomescreenLauncher.prototype
+     * @type {boolean}
+     */
+    get ready() {
+      return this._ready;
+    },
+
+    get hasLandingApp() {
+      return !!this._currentManifestURL;
+    },
+
+    /**
+     * manifest URL of homescreen.
+     *
+     * @access public
+     * @memberOf HomescreenLauncher.prototype
+     * @type {string}
+     */
+    get manifestURL() {
+      return this._currentManifestURL;
+    },
+
+    _fetchSettings: function hl_fetchSettings() {
+      var that = this;
+      SettingsListener.observe(this._settingsKey, '',
+        // XXX: After landing of bug 976986, we should write a deregister
+        // function of onRetrievingHomescreenManifestURL
+        // see https://bugzilla.mozilla.org/show_bug.cgi?id=976998
+        function onRetrievingHomescreenManifestURL(value) {
+          var previousManifestURL = that._currentManifestURL;
+          that._currentManifestURL = value;
+          if (typeof(that._instance) !== 'undefined') {
+            if (previousManifestURL !== '' &&
+                previousManifestURL !== that._currentManifestURL) {
+              that._instance.kill();
+              that._instance = new LandingAppWindow(value);
+              // Dispatch 'homescreen is changed' event.
+              window.dispatchEvent(new CustomEvent('homescreen-changed'));
+            } else {
+              that._instance.ensure();
+            }
+          }
+          if (!that._currentManifestURL) {
+            console.warn('We enable landing app but without defining it?');
+          }
+          that._ready = true;
+          window.dispatchEvent(new CustomEvent('homescreen-ready'));
+        });
+    },
+
+    _onAppReady: function hl_onAppReady() {
+      window.removeEventListener('applicationready', this._onAppReady);
+      this._fetchSettings();
+    },
+
+    /**
+     * Start process
+     * ![Homescreen launch process](http://i.imgur.com/JZ1ibkc.png)
+     *
+     * @memberOf HomescreenLauncher.prototype
+     */
+    start: function hl_start() {
+      if (this._started) {
+        return;
+      }
+      this._started = true;
+      if (applications.ready) {
+        this._fetchSettings();
+      } else {
+        window.addEventListener('applicationready',
+          this._onAppReady.bind(this));
+      }
+      window.addEventListener('trusteduishow', this);
+      window.addEventListener('trusteduihide', this);
+      window.addEventListener('appopening', this);
+      window.addEventListener('appopened', this);
+      window.addEventListener('keyboardchange', this);
+      window.addEventListener('shrinking-start', this);
+      window.addEventListener('shrinking-stop', this);
+      window.addEventListener('software-button-enabled', this);
+      window.addEventListener('software-button-disabled', this);
+    },
+
+    /**
+     * Stop process
+     *
+     * @memberOf HomescreenLauncher.prototype
+     */
+    stop: function hl_stop() {
+      if (typeof(this._instance) !== 'undefined') {
+        // XXX: After landing of bug 976986, we should move action of
+        // cleaing _instance into a deregister function of
+        // onRetrievingHomescreenManifestURL
+        // see https://bugzilla.mozilla.org/show_bug.cgi?id=976998
+        this._instance.kill();
+        this._instance = undefined;
+      }
+      this._currentManifestURL = '';
+      window.removeEventListener('appopening', this);
+      window.removeEventListener('trusteduihide', this);
+      window.removeEventListener('trusteduishow', this);
+      window.removeEventListener('applicationready', this._onAppReady);
+      window.removeEventListener('shrinking-start', this);
+      window.removeEventListener('shrinking-stop', this);
+      window.removeEventListener('software-button-enabled', this);
+      window.removeEventListener('software-button-disabled', this);
+      this._started = false;
+    },
+
+    /**
+     * General event handler interface.
+     *
+     * @param  {DOMEvent} evt The event.
+     * @type {boolean}
+     */
+    handleEvent: function hl_handleEvent(evt) {
+      switch (evt.type) {
+        case 'trusteduishow':
+          this.getHomescreen(true).toggle(true);
+          this.getHomescreen(true).fadeIn();
+          break;
+        case 'trusteduihide':
+          this.getHomescreen().toggle(false);
+          break;
+        case 'appopening':
+          // Fade out homescreen if the opening app is landscape.
+          if (evt.detail.rotatingDegree === 90 ||
+              evt.detail.rotatingDegree === 270) {
+            this.getHomescreen().fadeOut();
+          }
+          break;
+        case 'appopened':
+          // XXX: Remove the dependency in trustedUI rework.
+          if (!TrustedUIManager.hasTrustedUI(evt.detail.origin)) {
+            this.getHomescreen().fadeOut();
+          }
+          break;
+        case 'keyboardchange':
+          // Fade out the homescreen, so that it won't be seen when showing/
+          // hiding/switching keyboard.
+          this.getHomescreen().fadeOut();
+          break;
+        case 'shrinking-start':
+          // To hide the homescreen overlay while we set the background behind
+          // it due to the shrinking UI.
+          this.getHomescreen().hideFadeOverlay();
+          break;
+        case 'shrinking-stop':
+          // To resume the homescreen after shrinking UI is over.
+          this.getHomescreen().showFadeOverlay();
+          break;
+        case 'software-button-enabled':
+        case 'software-button-disabled':
+          var homescreen = this.getHomescreen();
+          homescreen && homescreen.resize();
+          break;
+      }
+    },
+
+    /**
+     * Get instance of homescreen window singleton
+     *
+     * @memberOf HomescreenLauncher.prototype
+     * @params {Boolean} ensure Ensure the homescreen app is alive.
+     * @returns {LandingAppWindow} Instance of homescreen window singleton, or
+     *                             null if HomescreenLauncher is not ready
+     */
+    getHomescreen: function hl_getAppWindow(ensure) {
+      if (!this.hasLandingApp) {
+        console.warn('LandingAppLauncher: do not have a landing app.');
+        return null;
+      }
+      if (typeof this._instance == 'undefined') {
+        this._instance = new LandingAppWindow(this._currentManifestURL);
+        return this._instance;
+      } else {
+        if (ensure) {
+          this._instance.ensure();
+        }
+        return this._instance;
+      }
+    }
+  };
+
+  exports.LandingAppLauncher = LandingAppLauncher;
+}(window));
