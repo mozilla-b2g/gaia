59987f5ab6a029f4b2ca9d9d456a5f11401b6bf4
Dominic Kuo <dkuo@mozilla.com>
2014-10-07 14:43:58 +0800

    Bug 1056968 - [System] vibrates to notify the user vibration is enabled when volume is 0

--- apps/system/test/unit/sound_manager_test.js
+++ apps/system/test/unit/sound_manager_test.js
@@ -178,7 +178,29 @@ suite('system/sound manager', function() {
         });
       });
 
+      test('volume up to vibrate', function() {
+        var spy = this.sinon.spy(soundManager, 'notifyByVibrating');
+        soundManager.currentChannel = 'notification';
+        soundManager.currentVolume.notification = -1;
+        soundManager.vibrationEnabled = false;
+        window.dispatchEvent(new CustomEvent('volumeup'));
+        checkVolume({
+          'alarm': 5,
+          'bt_sco': 5,
+          'content': 5,
+          'notification': 0,
+          'telephony': 5
+        });
+        assert.equal('MUTE', soundManager.muteState);
+        assert.isTrue(soundManager.vibrationEnabled);
+        assert.isTrue(MockNavigatorSettings.mSettings['vibration.enabled']);
+        var vibrationClassList = document.getElementById('volume').classList;
+        assert.isTrue(vibrationClassList.contains('vibration'));
+        assert.isTrue(spy.calledOnce);
+      });
+
       test('volume down to vibrate', function() {
+        var spy = this.sinon.spy(soundManager, 'notifyByVibrating');
         soundManager.currentChannel = 'notification';
         soundManager.currentVolume.notification = 1;
         soundManager.vibrationEnabled = false;
@@ -195,6 +217,7 @@ suite('system/sound manager', function() {
         assert.isTrue(MockNavigatorSettings.mSettings['vibration.enabled']);
         var vibrationClassList = document.getElementById('volume').classList;
         assert.isTrue(vibrationClassList.contains('vibration'));
+        assert.isTrue(spy.calledOnce);
       });
 
       test('volume down to silent', function() {
