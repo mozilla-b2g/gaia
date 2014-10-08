fbb64429cbcad56773da119bf68ead4c4aac9a67
Eli Perelman <eli@eliperelman.com>
2014-10-08 11:32:21 -0500

    Bug 1079700 - Correcting errors in performance testing

--- tests/performance/startup_events_test.js
+++ tests/performance/startup_events_test.js
@@ -4,7 +4,6 @@ var assert = require('assert');
 
 var App = require('./app');
 var PerformanceHelper = requireGaia('/tests/performance/performance_helper.js');
-var MarionetteHelper = requireGaia('/tests/js-marionette/helper.js');
 
 var appPath = mozTestInfo.appPath;
 
@@ -55,7 +54,8 @@ marionette('startup event test > ' + appPath + ' >', function() {
 
   var client = marionette.client({
     settings: {
-      'ftu.manifestURL': null
+      'ftu.manifestURL': null,
+      'lockscreen.enabled': false
     }
   });
   // Do nothing on script timeout. Bug 987383
@@ -76,14 +76,9 @@ marionette('startup event test > ' + appPath + ' >', function() {
   });
 
   setup(function() {
-    // it affects the first run otherwise
     this.timeout(500000);
     client.setScriptTimeout(50000);
-
-    // inject perf event listener
     PerformanceHelper.injectHelperAtom(client);
-
-    MarionetteHelper.unlockScreen(client);
   });
 
   test('startup >', function() {
@@ -101,18 +96,10 @@ marionette('startup event test > ' + appPath + ' >', function() {
           throw error;
         }
 
-        var epochEnd = PerformanceHelper.getEpochEnd(client);
-        var epochStart = PerformanceHelper.getEpochStart(client);
-        var delta = epochEnd - epochStart;
+        runResults.start = PerformanceHelper.getEpochStart(client);
 
-        // Bug 1045076: Sanity check. If for some reason any handlers
-        // didn't register or we didn't get valid timestamps back, do not
-        // report the values for this run and continue on
-        if (!epochEnd || !epochStart || delta < 1) {
-          return app.close();
-        }
-
-        performanceHelper.reportRunDurations(runResults, null, delta);
+        assert.ok(runResults.start > 0, 'problem capturing start epoch');
+        performanceHelper.reportRunDurations(runResults);
         app.close();
         assert.ok(Object.keys(runResults).length, 'empty results');
       });
