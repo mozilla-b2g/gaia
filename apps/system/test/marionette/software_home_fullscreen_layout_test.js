ad7891518de4b1e51680988e3606313d515194a2
albertopq <apastor@mozilla.com>
2014-10-09 10:05:33 +0200

    Bug 1078911 - SHB and inline activities on fullscreen_layout
    Conflicts:
    	apps/system/style/window_layout.css

--- apps/system/test/marionette/software_home_fullscreen_layout_test.js
+++ apps/system/test/marionette/software_home_fullscreen_layout_test.js
@@ -4,6 +4,8 @@ var assert = require('assert');
 var Home = require(
   '../../../verticalhome/test/marionette/lib/home2');
 var System = require('./lib/system');
+var Actions = require('marionette-client').Actions;
+var appUrl = 'app://fullscreen_layout.gaiamobile.org';
 
 marionette('Software Home Button - Fullscreen Layout', function() {
 
@@ -22,22 +24,21 @@ marionette('Software Home Button - Fullscreen Layout', function() {
         __dirname + '/fullscreen_layout'
     }
   });
-  var home, system;
+  var home, system, actions;
 
   setup(function() {
     home = new Home(client);
     system = new System(client);
+    actions = new Actions(client);
     system.waitForStartup();
     home.waitForLaunch();
+    client.switchToFrame();
+    client.apps.launch(appUrl);
+    client.apps.switchToApp(appUrl);
     client.switchToFrame();
   });
 
   test('Hides after a mozRequestFullscreen request', function() {
-    var appUrl = 'app://fullscreen_layout.gaiamobile.org';
-    client.apps.launch(appUrl);
-    client.apps.switchToApp(appUrl);
-    client.switchToFrame();
-
     assert.ok(system.softwareHomeFullscreenLayout.displayed());
 
     var frame = system.waitForLaunch(appUrl);
@@ -46,18 +47,52 @@ marionette('Software Home Button - Fullscreen Layout', function() {
 
     client.switchToFrame();
 
-    client.waitFor(function(){
+    client.waitFor(function() {
       return !system.softwareHomeFullscreenLayout.displayed();
     });
 
-    client.executeScript(function(){
+    client.executeScript(function() {
       window.wrappedJSObject.document.mozCancelFullScreen();
     });
 
     system.clickSoftwareHomeButton();
-    client.waitFor(function(){
+    client.waitFor(function() {
       return client.findElement(System.Selector.activeHomescreenFrame)
         .displayed();
     });
+  });
+
+  test('Is shown in an inline activity', function() {
+    var frame = system.waitForLaunch(appUrl);
+    client.switchToFrame(frame);
+    client.executeScript(function() {
+      var a = new window.wrappedJSObject.MozActivity({
+        name: 'share',
+        data: {
+          type: 'video/*',
+          number: 1,
+          blobs: ['blobs'],
+          filenames: ['names'],
+          filepaths: ['fullpaths']
+        }
+      });
+      a.onerror = function() {
+        console.log('Activity error');
+      };
+    });
+    client.switchToFrame();
+    var bluetooth = system.getActivityOptionMatching('bluetooth');
+    actions.tap(bluetooth).perform();
+    client.switchToFrame();
+    client.waitFor(function() {
+      return client.findElement('.appWindow.inline-activity').displayed();
+    });
+
+    var bluetoothWindow = client.findElement('.appWindow.inline-activity');
+    var bluetoothHeight = bluetoothWindow.cssProperty('height');
+    var screenHeight = client.findElement('#screen').size().height;
+    var shbSelector = '#software-buttons-fullscreen-layout';
+    var shbHeight = client.findElement(shbSelector).size().height;
+    assert.ok(bluetoothHeight === ((screenHeight - shbHeight) + 'px'));
   });
 });
