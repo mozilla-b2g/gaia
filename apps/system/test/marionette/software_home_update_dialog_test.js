8b573b0f1693784411d721e689772f6c7c098c6c
Michael Henretty <michael.henretty@gmail.com>
2014-10-10 12:42:39 -0700

    Bug 1074123 - Fix update and quick settings dialogs
    Conflicts:
    	apps/system/style/quick_settings/quick_settings.css

---
+++ apps/system/test/marionette/software_home_update_dialog_test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var Home = require(
+  '../../../verticalhome/test/marionette/lib/home2');
+var System = require('./lib/system');
+
+marionette('Software Home Button - Update Dialog', function() {
+
+  var client = marionette.client({
+    prefs: {
+      'focusmanager.testmode': true,
+      'dom.w3c_touch_events.enabled': 1
+    },
+    settings: {
+      'ftu.manifestURL': null,
+      'lockscreen.enabled': false,
+      'software-button.enabled': true
+    }
+  });
+  var home, system;
+
+  setup(function() {
+    home = new Home(client);
+    system = new System(client);
+    system.waitForStartup();
+    home.waitForLaunch();
+    client.switchToFrame();
+  });
+
+  test('Proper layout for updates', function() {
+    client.executeScript(function() {
+      window.wrappedJSObject.UpdateManager.showDownloadPrompt();
+    });
+
+    function rect(el) {
+      return el.getBoundingClientRect();
+    }
+
+    var winHeight = client.findElement('body').size().height;
+    client.waitFor(function() {
+      var dialog = client.findElement('#updates-download-dialog');
+      var dialogRect = dialog.scriptWith(rect);
+      var shbRect = system.softwareButtons.scriptWith(rect);
+
+      return dialogRect.bottom === shbRect.top &&
+        winHeight === (dialogRect.height + shbRect.height);
+    });
+  });
+});
