a2128a4afe5bebf60bc6e2ea8bcde940838430c1
Russ Nicoletti <rnicoletti@mozilla.com>
2014-10-10 09:14:12 -0700

    Bug 1079692 --  [Flame][Woodduck][Free Test][Video] Make video title truncation process more efficient

--- apps/video/js/video_utils.js
+++ apps/video/js/video_utils.js
@@ -93,8 +93,6 @@ var VideoUtils = {
    *             node {HTMLElement}: the element contains the text.
    *             maxLine {int}: optional, maximum line of text. The default
    *                            value is 2.
-   *             ellipsisIndex {int}: optional, the number of left text in the
-   *                                  end. The default value is 3.
    *             ellipsisCharacter {string}: optional, the text of ellipsis
    *                                         char. The default value is '...'.
    *
@@ -104,28 +102,12 @@ var VideoUtils = {
     // options
     var maxLine = options.maxLine || 2;
     var node = options.node;
-    var ellipsisIndex = (options.ellipsisIndex || options.ellipsisIndex === 0) ?
-                        ellipsisIndex = options.ellipsisIndex : 3;
     var ellipsisCharacter = options.ellipsisCharacter || '...';
 
     if (node === null) {
       return oldName;
     }
 
-    // used variables and functions
-    function hitsNewline(oldHeight, newHeight) {
-      return oldHeight !== newHeight;
-    }
-
-    var newName = '';
-    var oldHeight;
-    var newHeight;
-    var baseHeight;
-    var currentLine;
-    var ellipsisAt;
-    var nameBeforeEllipsis = [];
-    var nameBeforeEllipsisString;
-    var nameAfterEllipsis = ellipsisIndex ? oldName.slice(-ellipsisIndex) : '';
     var realVisibility = node.style.visibility;
     var realWordBreak = node.style.wordBreak;
 
@@ -140,17 +122,24 @@ var VideoUtils = {
     node.style.wordBreak = 'break-all';
 
     /*
-     * Get the base height to count the currentLine at first
+     * Get the height of a line of text
      */
     node.textContent = '.';
-    baseHeight = node.clientHeight;
+    var baseHeight = node.clientHeight;
+    var computedStyle = window.getComputedStyle(node, null);
+    var paddingTop =
+      parseInt(computedStyle.getPropertyValue('padding-top'), 10);
+    var paddingBottom =
+      parseInt(computedStyle.getPropertyValue('padding-bottom'), 10);
+    var padding = paddingTop + paddingBottom;
+    var lineHeight = baseHeight - padding;
 
     /*
      * Determine if the title needs truncating
      */
     node.textContent = oldName;
-    var originalTitleHeight = node.clientHeight;
-    var linesForOriginalTitle = (originalTitleHeight / baseHeight);
+    var originalTitleHeight = node.clientHeight - padding;
+    var linesForOriginalTitle = (originalTitleHeight / lineHeight);
     if (linesForOriginalTitle <= maxLine) {
       /*
        * Title does not need truncating. Restore UI and return original title
@@ -160,66 +149,30 @@ var VideoUtils = {
       return oldName;
     }
 
-    node.textContent = '';
+    function titleTooLong(pos) {
+      node.textContent = oldName.slice(0, pos) + ellipsisCharacter;
+      var tempTitleHeight = node.clientHeight - padding;
+      return (tempTitleHeight / lineHeight) > maxLine;
+    }
 
-    var ellipseIndex;
-    oldName.split('').some(function(character, index) {
+    var low = 0;
+    var high = oldName.length - 1;
+    var mid;
+    var ellipsisIndex = 0;
 
-      ellipseIndex = index;
-      nameBeforeEllipsis.push(character);
-      nameBeforeEllipsisString = nameBeforeEllipsis.join('');
-
-      oldHeight = node.clientHeight;
-      node.textContent = nameBeforeEllipsisString +
-          ellipsisCharacter + nameAfterEllipsis;
-      newHeight = node.clientHeight;
-
-      /*
-       * When index is 0, we have to update currentLine according to
-       * the first assignment (it is possible that at first the currentLine
-       * is not 0 if the width of node is too small)
-       */
-      if (index === 0) {
-        currentLine = Math.floor(newHeight / baseHeight);
+    do {
+      mid = Math.floor((low + high) / 2);
+      if (titleTooLong(mid)) {
+        high = mid - 1;
       }
-
-      if (hitsNewline(oldHeight, newHeight) && index !== 0) {
-
-        currentLine += 1;
+      else {
+        ellipsisIndex = mid;
+        low = mid + 1;
       }
+    }
+    while (low <= high);
 
-      if (currentLine > maxLine) {
-        if (index === 0) {
-
-          /*
-           * It means that at first, the whole string is already in
-           * an overflowed situation, you have to avoid this situation.
-           * And we will bypass oldName back to you.
-           *
-           * There are some options for you :
-           *
-           *   1. Check options.ellipsisCharacter
-           *   2. Check options.maxLine
-           *   3. Check node's width (maybe too narrow)
-           */
-          console.error(
-            'Your string is in a overflowed situation, ' +
-            'please check your options');
-        }
-
-        /*
-         * Remove the last character, because it causes the overflow
-         */
-        nameBeforeEllipsis.pop();
-        node.textContent = '';
-
-        newName = nameBeforeEllipsis.join('').slice(0, ellipseIndex);
-        newName += ellipsisCharacter;
-        newName += nameAfterEllipsis;
-
-        return;
-      }
-    });
+    var newName = oldName.slice(0, ellipsisIndex) + ellipsisCharacter;
 
     // restore UI
     node.style.visibility = realVisibility;
