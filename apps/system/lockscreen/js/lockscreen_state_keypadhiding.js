3b99f2d2a05dd78d85779e36a16af0b6d59a9847
Greg Weng <snowmantw@gmail.com>
2014-10-13 11:03:55 +0800

    Bug 1069879 - Only three dots shown when entering passcode

--- apps/system/lockscreen/js/lockscreen_state_keypadhiding.js
+++ apps/system/lockscreen/js/lockscreen_state_keypadhiding.js
@@ -37,6 +37,8 @@
         // XXX: keyboard animation would affect panel, but the target would
         // not be 'this.lockScreen.overlay'.
         if (evt.target.classList.contains('lockscreen-panel')) {
+          this.lockScreen.passcodePad.style.transitionDelay = '';
+          this.lockScreen.passcodeCode.style.transitionDelay = '';
           window.removeEventListener('transitionend', transitionEnd);
           // We must map this native event to avoid listen 'transitionend'
           // in the manager cause racing problem (the event and transferring
@@ -47,6 +49,9 @@
       // Copy from the original switching method.
       this.lockScreen.overlay.classList.remove('no-transition');
       window.addEventListener('transitionend', transitionEnd);
+      // Must delay the animation to let the final dot shows.
+      this.lockScreen.passcodePad.style.transitionDelay = '0.1s';
+      this.lockScreen.passcodeCode.style.transitionDelay = '0.1s';
       // Trigger the transition.
       this.lockScreen.overlay.dataset.panel = 'main';
     });
