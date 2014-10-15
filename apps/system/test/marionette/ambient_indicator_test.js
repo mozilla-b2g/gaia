e5d84cb7f0d2c5b482e79298a0d7e021da1f131b
albertopq <apastor@mozilla.com>
2014-10-15 18:01:50 +0200

    Bug 1073032 - Adding integration tests to the ambient indicator

---
+++ apps/system/test/marionette/ambient_indicator_test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var assert = require('chai').assert;
+var AmbientIndicator = require('./lib/ambient_indicator');
+var UtilityTray = require('./lib/utility_tray');
+var NotificationTest = require('./lib/notification').NotificationTest;
+var System = require('./lib/system');
+
+marionette('Ambient indicator', function() {
+
+  var client = marionette.client({
+    settings: {
+      'ftu.manifestURL': null,
+      'lockscreen.enabled': false
+    }
+  });
+
+  var ambientIndicator = new AmbientIndicator(client);
+  var utilityTray = new UtilityTray(client);
+  var system = new System(client);
+  var details, notification;
+
+  suite('Ambient indicator', function() {
+    setup(function() {
+      details = {
+        tag: 'test tag',
+        title: 'test title',
+        body: 'test body',
+        dir: 'rtl',
+        lang: 'en'
+      };
+      system.waitForStartup();
+    });
+
+    test('is shown when a notification arrives', function() {
+      assert.ok(ambientIndicator.displayed === false);
+      notification = new NotificationTest(client, details);
+      client.waitFor(function() {
+        return ambientIndicator.displayed;
+      });
+    });
+
+    test('gets cleared when opening the tray', function() {
+      assert.ok(ambientIndicator.displayed === false);
+      notification = new NotificationTest(client, details);
+      utilityTray.open();
+      client.waitFor(function() {
+        return !ambientIndicator.displayed;
+      });
+    });
+
+    test('gets cleared when externaly removing the notification', function() {
+      assert.ok(ambientIndicator.displayed === false);
+      notification = new NotificationTest(client, details);
+      client.waitFor(function() {
+        return ambientIndicator.displayed;
+      });
+      notification.close();
+      client.waitFor(function() {
+        return !ambientIndicator.displayed;
+      });
+    });
+  });
+
+});
