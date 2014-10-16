2d2118ddb9ca916eac90f31bf955f4dfaab127de
Martijn Wargers <mwargers@mozilla.com>
2014-10-16 19:41:43 +0200

    Bug 989562 - Write a test to install & launch a packaged app

--- tests/python/gaia-ui-tests/gaiatest/apps/homescreen/regions/confirm_install.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/homescreen/regions/confirm_install.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import time
 from marionette.by import By
 from gaiatest.apps.base import Base
 
@@ -11,5 +12,8 @@ class ConfirmInstall(Base):
         _confirm_install_button_locator = (By.ID, 'app-install-install-button')
 
         def tap_confirm(self):
+            # TODO add a good wait here when Bug 1008961 is resolved
+            time.sleep(1)
+            self.marionette.switch_to_frame()
             self.wait_for_element_displayed(*self._confirm_install_button_locator)
             self.marionette.find_element(*self._confirm_install_button_locator).tap()
