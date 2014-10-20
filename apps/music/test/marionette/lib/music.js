4154ff4fb6dfb1fc066b71cdb822a1d08176afb6
Kevin Grandon <kevingrandon@yahoo.com>
2014-10-20 15:49:41 -0700

    Bug 1085667 - [Music] Try clicking on share button mutliple times r=dkuo

--- apps/music/test/marionette/lib/music.js
+++ apps/music/test/marionette/lib/music.js
@@ -19,7 +19,7 @@ Music.Selector = Object.freeze({
   firstTile: '.tile',
   songsTab: '#tabs-songs',
   albumsTab: '#tabs-albums',
-
+  coverImage: '#player-cover-image',
   viewsList: '#views-list-anchor',
   viewsSublist: '#views-sublist-anchor',
   firstSong: '.list-item',
@@ -199,11 +199,32 @@ Music.prototype = {
   },
 
   shareWith: function(appName) {
-    var shareButton = this.shareButton;
-    this.client.waitFor(function() {
-      return shareButton.displayed();
+
+    // Allow findElement to fail quickly.
+    var quickly = this.client.scope({
+      searchTimeout: 50
     });
-    shareButton.tap();
+
+    var shareMenu;
+    // Wait until the share menu is displayed.
+    // Try to click the cover image followed by the share button in the case
+    // that it hides before we get a chance to click it.
+    this.client.waitFor(function() {
+      this.client.helper.waitForElement(Music.Selector.coverImage).click();
+      this.shareButton.tap();
+
+      this.client.switchToFrame();
+      try {
+        shareMenu = quickly.findElement(Music.Selector.shareMenu);
+      } catch(e) {
+        this.client.apps.switchToApp(this.origin);
+        return false;
+      }
+
+      var isDisplayed = shareMenu.displayed();
+      this.client.apps.switchToApp(this.origin);
+      return isDisplayed;
+    }.bind(this));
 
     var list = this.shareMenu.findElements('button');
     for (var i = 0; i < list.length; i++) {
