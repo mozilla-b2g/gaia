a8e70e18058174f4ab15da9ce30c52f752217af7
Evan Xd <itoyxd@gmail.com>
2014-10-21 21:01:16 +0800

    Bug 1071389 - [Calendar] Drawer /Add event/ Display event should not play scrolling animation in week view

--- apps/calendar/js/views/week.js
+++ apps/calendar/js/views/week.js
@@ -18,9 +18,28 @@ WeekView.prototype = {
   visibleCells: 5,
   _hourFormat: 'week-hour-format',
   _addAmPmClass: true,
+  _previousStartDate: null,
 
   get element() {
     return document.getElementById('week-view');
+  },
+
+  isThereNeedToSwipe: function() {
+    var needToSwipe = true;
+    var currentStartDate = new Date(this._currentTime.timespan.start);
+    console.log('currentStartDate: ' + currentStartDate);
+    console.log('_previousStartDate: ' + this._previousStartDate);
+    if (this._previousStartDate &&
+        Calc.isSameDate(currentStartDate, this._previousStartDate)) {
+      needToSwipe = false;
+    }
+    this._previousStartDate = currentStartDate;
+    return needToSwipe;
+  },
+
+  _onDayChange: function() {
+    MultiDay.prototype._onDayChange.apply(this, arguments);
+    this._previousStartDate = new Date(this._currentTime.timespan.start);
   },
 
   _calcBaseDate: function(date) {
