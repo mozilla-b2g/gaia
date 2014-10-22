523640c5c3d291c71a5034b115c24c064051c3b3
Manuel Casas Barrado <manuel.casasbarrado@telefonica.com>
2014-10-22 13:34:20 +0200

    Bug 1080799 - Connecting to/joining (hidden?) Wi-Fi fails after factory resetting phone/first run, restart required. Patch v2.1

--- apps/ftu/test/unit/mock_wifi_helper.js
+++ apps/ftu/test/unit/mock_wifi_helper.js
@@ -5,5 +5,11 @@ requireApp('ftu/test/unit/mock_navigator_moz_wifi_manager.js');
 var MockWifiHelper = {
   getWifiManager: function() {
     return MockNavigatorMozWifiManager;
+  },
+  getSecurity: function(network) {
+    return network.security;
+  },
+  isConnected: function(network) {
+    return false;
   }
 };
