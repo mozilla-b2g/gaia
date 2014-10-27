e8a04a1af412731aba2b8b3ee38dcac4bb9ca4d0
Julien Wajsberg <felash@gmail.com>
2014-10-27 12:39:40 +0100

    fix internet sharing test

--- apps/system/test/unit/internet_sharing_test.js
+++ apps/system/test/unit/internet_sharing_test.js
@@ -47,6 +47,7 @@ suite('internet sharing > ', function() {
     // Unfortunately, for asyncStorage scoping reasons, we can't simply
     // use 'attachTestHelpers' anywhere in the internet sharing tests.
     mocksForInternetSharing.suiteSetup();
+    mocksForInternetSharing.setup();
 
     realSettings = navigator.mozSettings;
     navigator.mozSettings = MockNavigatorSettings;
@@ -70,6 +71,7 @@ suite('internet sharing > ', function() {
   });
 
   suiteTeardown(function() {
+    mocksForInternetSharing.teardown();
     mocksForInternetSharing.suiteTeardown();
     navigator.mozSettings = realSettings;
     navigator.mozL10n = realL10n;
@@ -105,14 +107,6 @@ suite('internet sharing > ', function() {
   }
   // suite 1 from no sim card to sim card inserted, and re-inserted again.
   suite('from null sim to sim 1 >>', function() {
-
-    suiteSetup(function() {
-      mocksForInternetSharing.setup();
-    });
-
-    suiteTeardown(function() {
-      mocksForInternetSharing.teardown();
-    });
 
     // fresh startup
     test('null sim no settings', function() {
@@ -247,8 +241,6 @@ suite('internet sharing > ', function() {
   // switching test for 2 sim and no sim
   suite('switch between sim1, sim2, and null >>', function() {
     suiteSetup(function() {
-      // we need to keep asyncStorage under this suite.
-      mocksForInternetSharing.setup();
       // setting up:
       // null, pinRequired, pukRequired...(non-ready): usb enabled
       // sim1: wifi hotspot enabled
@@ -266,10 +258,6 @@ suite('internet sharing > ', function() {
       changeSettings(KEY_WIFI_HOTSPOT, false);
     });
 
-    suiteTeardown(function() {
-      // we need to keep asyncStorage under this suite.
-      mocksForInternetSharing.teardown();
-    });
     // test initial state
     test('test asyncStorage', function() {
       var testSet = [
