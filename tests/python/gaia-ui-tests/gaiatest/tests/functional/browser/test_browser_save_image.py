664496d4ddd93c4983a8b90a29816796f4efebb1
Viorela Ioia <viorelaioia@gmail.com>
2014-10-31 15:33:00 +0200

    Bug 1092154 - Fix failure in test_browser_save_image.py

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_save_image.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_save_image.py
@@ -27,6 +27,9 @@ class TestBrowserSaveImage(GaiaTestCase):
         https://moztrap.mozilla.org/manage/case/6889/
         """
 
+        # Check that there are no images on sdcard
+        self.assertEqual(0, len(self.data_layer.sdcard_files('.jpeg')))
+
         search = Search(self.marionette)
         search.launch()
 
@@ -42,9 +45,6 @@ class TestBrowserSaveImage(GaiaTestCase):
             wait(1).\
             perform()
 
-        # Check that there are no images on sdcard before saving
-        self.assertEqual(0, len(self.data_layer.sdcard_files()))
-
         context_menu = ContextMenu(self.marionette)
         context_menu.tap_save_image()
 
@@ -52,4 +52,5 @@ class TestBrowserSaveImage(GaiaTestCase):
         system.wait_for_notification_toaster_displayed()
         system.wait_for_notification_toaster_not_displayed()
 
-        self.assertEqual(1, len(self.data_layer.sdcard_files()))
+        # Check that there is one image on sdcard
+        self.assertEqual(1, len(self.data_layer.sdcard_files('.jpeg')))
