fe2167fa5314c7e71c143a590914cbf3771905a8
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2014-11-03 17:32:29 -0500

    Revert "Merge pull request #25643 from mancas/bug956902"
    This reverts commit 5f3126b10569f4f252b2babfba5442b66605740b, reversing
    changes made to cebd567d0de3755e2bddbc07a7721a7c67ca6eb6.

--- apps/system/js/utility_tray.js
+++ apps/system/js/utility_tray.js
@@ -125,7 +125,7 @@ var UtilityTray = {
         break;
 
       case 'touchstart':
-        if (lockScreen.locked || FtuLauncher.isFtuRunning()) {
+        if (lockScreen.locked) {
           return;
         }
 
