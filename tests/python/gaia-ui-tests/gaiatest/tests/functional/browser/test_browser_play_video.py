4bf9932675e90cbfc99d1aec657266341e0ea4cc
Martijn Wargers <mwargers@mozilla.com>
2014-11-05 17:52:02 +0100

    Bug 1047168 - Modify test_play_youtube to play a video file locally

---
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_play_video.py
@@ -0,0 +1,84 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marionette import Wait
+from gaiatest import GaiaTestCase
+from gaiatest.apps.search.app import Search
+from gaiatest.apps.search.regions.html5_player import HTML5Player
+from gaiatest.apps.homescreen.regions.permission_dialog import PermissionDialog
+
+
+class TestVideo(GaiaTestCase):
+
+    acceptable_delay = 2.0
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+        self.connect_to_local_area_network()
+        self.video_URL = self.marionette.absolute_url('VID_0001.mp4')
+        self.apps.set_permission_by_url(Search.manifest_url, 'geolocation', 'deny')
+
+    def test_play_video(self):
+        """Confirm video playback
+
+        https://moztrap.mozilla.org/manage/case/6073/
+        """
+        search = Search(self.marionette)
+        search.launch()
+        browser = search.go_to_url(self.video_URL)
+        browser.wait_for_page_to_load(180)
+        browser.switch_to_content()
+
+        player = HTML5Player(self.marionette)
+
+        # Make player loop, so it doesn't accidentally stops playing
+        player.set_loop(True)
+
+        # Check that video is playing
+        player.wait_for_video_loaded()
+        self.assertTrue(player.is_video_playing())
+
+        # Tap on the edge of the video to make the controls appear
+        player.invoke_controls()
+        # Pause playback
+        player.tap_pause()
+        stopped_at = player.current_timestamp
+        self.assertFalse(player.is_video_playing())
+
+        # Resume playback
+        player.tap_play()
+        resumed_at = player.current_timestamp
+        self.assertTrue(player.is_video_playing())
+
+        # After tapping the play button, the controls disappear, make them appear again
+        player.invoke_controls()
+
+        # Tap mute button
+        player.tap_mute()
+        player.tap_unmute()
+
+        # Ensure that video resumes to play
+        # from the place where it was paused
+        delay = resumed_at - stopped_at
+        self.assertLessEqual(delay, self.acceptable_delay,
+                             'video resumed to play not from place where it was paused')
+        player.tap_full_screen()
+        permission = PermissionDialog(self.marionette)
+        self.marionette.switch_to_default_content()
+        permission.wait_for_permission_dialog_displayed()
+        permission.tap_to_confirm_permission()
+        browser.switch_to_content()
+
+        # This lines is for some reason necessary, because Marionette on device
+        # lost connection to the element for some reason
+        player = HTML5Player(self.marionette)
+
+        Wait(self.marionette).until(lambda m: player.is_fullscreen)
+
+        # After tapping full screen, the controls disappear, make them appear again
+        player.invoke_controls()
+        Wait(self.marionette).until(lambda m: player.controls_visible)
+        player.tap_full_screen()
+        Wait(self.marionette).until(lambda m: player.is_fullscreen is False)
+        Wait(self.marionette).until(lambda m: player.controls_visible is False)
