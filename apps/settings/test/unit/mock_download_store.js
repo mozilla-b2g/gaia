a363b8ea395ac51c75a878321cf51a8161511e11
Ghislain 'Aus' Lacroix <aus@noiseport.org>
2014-11-13 17:31:02 -0800

    Merge pull request #26125 from nullaus/bug1077595
    Bug1077595
    (cherry picked from commit 906123b6d71bcd9d8db77227ffba7269e6cbaeb9)

--- apps/settings/test/unit/mock_download_store.js
+++ apps/settings/test/unit/mock_download_store.js
@@ -5,6 +5,7 @@
 var MockDownloadStore = {
   error: false,
   downloads: [],
+  listeners: [],
   getAll: function() {
     var request = {
       onsuccess: null
@@ -17,11 +18,32 @@ var MockDownloadStore = {
       };
       request.onsuccess(event);
 
-    }.bind(this), 1000);
+    }.bind(this), 0);
     return request;
   },
   add: function() {
   },
   remove: function() {
+  },
+  addListener: function(listener) {
+    var request = {
+      onsuccess: null
+    };
+
+    if (this.listeners.indexOf(listener) == -1) {
+      this.listeners.push(listener);
+    }
+    setTimeout(function() {
+      var event = { target: { result: {} }};
+      request.onsuccess(event);
+    }.bind(this), 0);
+
+    return request;
+  },
+  removeListener: function(listener) {
+    var index = this.listeners.indexOf(listener);
+    if (index > -1) {
+      this.listeners.splice(index, 1);
+    }
   }
 };
