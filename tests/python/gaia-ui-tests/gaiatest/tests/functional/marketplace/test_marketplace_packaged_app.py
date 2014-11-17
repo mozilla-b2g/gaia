68baa1c2db4ade93d62ba20ce875698ea0dcd20c
Martijn Wargers <mwargers@mozilla.com>
2014-11-18 00:40:37 +0100

    Bug 1094151 - Write a test to install & launch a packaged app from the Marketplace

---
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/marketplace/test_marketplace_packaged_app.py
@@ -0,0 +1,47 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+from marionette import expected
+from marionette import Wait
+from marionette.by import By
+from gaiatest import GaiaTestCase
+from gaiatest.apps.homescreen.app import Homescreen
+from gaiatest.apps.marketplace.app import MarketplaceDev
+from gaiatest.apps.system.app import System
+from gaiatest.apps.homescreen.regions.confirm_install import ConfirmInstall
+
+class TestSearchMarketplaceAndInstallApp(GaiaTestCase):
+    
+    app_search = ':packaged'
+    
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+        self.connect_to_local_area_network()
+    
+    def test_search_and_install_app(self):
+
+        marketplace = MarketplaceDev(self.marionette, 'Marketplace')
+        system = System(self.marionette)
+
+        results = marketplace.search(self.app_search)
+        first_result = results.search_results[0]
+        app_name = first_result.get_app_name()
+        first_result.tap_install_button()
+
+        # Confirm the installation and wait for the app icon to be present
+        confirm_install = ConfirmInstall(self.marionette)
+        confirm_install.tap_confirm()
+
+        # Press Home button
+        self.device.touch_home_button()
+
+        # Check that the icon of the app is on the homescreen
+        homescreen = Homescreen(self.marionette)
+        homescreen.wait_for_app_icon_present(app_name)
+
+        installed_app = homescreen.installed_app(app_name)
+        installed_app.tap_icon()
+
+        Wait(self.marionette).until(lambda m: m.title == app_name)
