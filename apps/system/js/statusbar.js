b5516bece85f6588fe1cabc7738d48c031774f7c
Manuel Casas <manuel.casasbarrado@telefonica.com>
2014-11-18 11:24:10 +0100

    Bug 1097296 - [Window Management] Battery, Signal and time on the status bar will appear behind download update overlay. r=etienne

--- apps/system/js/statusbar.js
+++ apps/system/js/statusbar.js
@@ -302,6 +302,10 @@ var StatusBar = {
     window.addEventListener('homescreenopened', this);
     window.addEventListener('stackchanged', this);
 
+    // Listen to updates dialog
+    window.addEventListener('updatepromptshown', this);
+    window.addEventListener('updateprompthidden', this);
+
     // Track Downloads via the Downloads API.
     var mozDownloadManager = navigator.mozDownloadManager;
     if (mozDownloadManager) {
@@ -584,6 +588,12 @@ var StatusBar = {
         // indicator. If you think this logic needs to change, think really hard
         // about it and then come and ask @nullaus
         this.addSystemDownloadListeners(evt.download);
+        break;
+      case 'updatepromptshown':
+        this.element.classList.remove('light');
+        break;
+      case 'updateprompthidden':
+        this.setAppearance(Service.currentApp);
         break;
     }
   },
