0eefc6aa63fed4527b182e779705f2012209761c
Gabriele Svelto <gsvelto@mozilla.com>
2014-11-18 16:09:49 +0100

    Bug 1092362 - Use the default audio channel when playing audible tones outside of calls r=drs,francisco
    (cherry picked from commit e327daf01fac1e5fbe2ba732c26e3a21db32443a)

--- shared/js/dialer/tone_player.js
+++ shared/js/dialer/tone_player.js
@@ -18,6 +18,7 @@ var TonePlayer = {
   _gainNode: null,
   _playingNodes: [],
   _maybeTrashAudio: null,
+  _initialized: false,
 
   /**
    * Initializes the tone player by specifying which channel will be used to
@@ -48,6 +49,8 @@ var TonePlayer = {
     window.addEventListener('visibilitychange', this._maybeTrashAudio);
     telephony && telephony.addEventListener('callschanged',
                                             this._maybeTrashAudio);
+
+    this._initialized = true;
   },
 
   /**
@@ -61,6 +64,7 @@ var TonePlayer = {
                                                this._maybeTrashAudio);
     window.removeEventListener('visibilitychange', this._maybeTrashAudio);
     this._reset();
+    this._initialized = false;
   },
 
   /**
@@ -75,11 +79,16 @@ var TonePlayer = {
   },
 
   _ensureAudio: function tp_ensureAudio() {
-    if (this._audioContext || !this._channel) {
+    if (this._audioContext || !this._initialized) {
       return;
     }
 
-    this._audioContext = new AudioContext(this._channel);
+    if (this._channel) {
+      this._audioContext = new AudioContext(this._channel);
+    } else {
+      // If no channel was specified stick with the default one.
+      this._audioContext = new AudioContext();
+    }
   },
 
   _trashAudio: function tp_trashAudio() {
