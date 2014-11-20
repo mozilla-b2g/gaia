1f07a87e6091c4e395ec92eb77b4f0d8e4f8ab8d
Alexandre Poirot <poirot.alex@gmail.com>
2014-11-20 14:22:42 +0100

    Bug 1098664 - fallback to manifest app type if metadata.json one is empty. r=gduan

--- build/utils-xpc.js
+++ build/utils-xpc.js
@@ -348,7 +348,8 @@ function getWebapp(app, domain, scheme, port, stageDir) {
   if (metaData.exists()) {
     webapp.pckManifest = readZipManifest(webapp.sourceDirectoryFile);
     webapp.metaData = getJSON(metaData);
-    webapp.appStatus = utils.getAppStatus(webapp.metaData.type || 'web');
+    webapp.appStatus = utils.getAppStatus(webapp.metaData.type ||
+                                          webapp.manifest.type || 'web');
   } else {
     webapp.appStatus = utils.getAppStatus(webapp.manifest.type);
   }
