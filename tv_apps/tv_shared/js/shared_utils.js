356379b629db2ad97c7a8dc03e5f7c36b916b5fa
John Hu <johu@mozilla.com>
2014-11-24 10:35:58 -0600

    Bug 1102142 - [Stingray][home] create circle-button and use it in Home's search button

--- tv_apps/tv_shared/js/shared_utils.js
+++ tv_apps/tv_shared/js/shared_utils.js
@@ -15,6 +15,36 @@
           }
         }
       }
+    },
+
+    // Because the scoped css cannot be override by outer css, we have to create
+    // a cloned function without scope from shared/component_utils.js
+    injectComponentStyle: function su_injectComponentStyle(self, baseUrl) {
+      var style = document.createElement('style');
+      var url = baseUrl + 'style.css';
+
+      style.innerHTML = '@import url(' + url + ');';
+      self.appendChild(style);
+
+      self.style.visibility = 'hidden';
+
+      // Wait for the stylesheet to load before injecting
+      // it into the shadow-dom. This is to work around
+      // bug 1003294, let's review once landed.
+      style.addEventListener('load', function() {
+
+        // Put a clone of the stylesheet into the shadow-dom.
+        // We have to use two <style> nodes, to work around
+        // the lack of `:host` (bug 992245) and `:content`
+        // (bug 992249) selectors. Once we have those we
+        // can do all our styling from a single style-sheet
+        // within the shadow-dom.
+        if (self.shadowRoot) {
+          self.shadowRoot.appendChild(style.cloneNode(true));
+        }
+
+        self.style.visibility = '';
+      });
     }
   };
 
