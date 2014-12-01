9419063d2c64159260543c83c072a95d723d2991
Dave Hunt <dave.hunt@gmail.com>
2014-12-01 15:35:06 +0000

    Bug 1074117 - Use expected conditions and optimise element lookups in messages app object. r=fstrugariu

--- tests/python/gaia-ui-tests/gaiatest/apps/messages/regions/attachment_options.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/messages/regions/attachment_options.py
@@ -2,9 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from marionette import expected
 from marionette import Wait
 from marionette.by import By
-from marionette import expected
 
 from gaiatest.apps.base import Base
 
@@ -18,13 +18,13 @@ class AttachmentOptions(Base):
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
-        Wait(self.marionette).until(lambda m:
-                                    m.find_element(*self._attachment_options_locator).location['y'] == 0)
+        element = self.marionette.find_element(*self._attachment_options_locator)
+        Wait(self.marionette).until(lambda m: element.location['y'] == 0)
 
     def tap_cancel(self):
+        element = self.marionette.find_element(*self._attachment_options_locator)
         self.marionette.find_element(*self._cancel_button_locator).tap()
-        Wait(self.marionette).until(expected.element_not_displayed(
-            Wait(self.marionette).until(expected.element_present(*self._attachment_options_locator))))
+        Wait(self.marionette).until(expected.element_not_displayed(element))
 
     def tap_view_button(self):
         self.marionette.find_element(*self._view_button_locator).tap()
