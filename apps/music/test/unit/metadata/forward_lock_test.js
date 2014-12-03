60fa397621a04ec6f8fbc444ebf9865601ddabfc
Jim <jporter@mozilla.com>
2014-12-03 17:59:08 -0600

    Bug 1106612 - [Music] Cannot play Forward Locked/DRM mp3 files

---
+++ apps/music/test/unit/metadata/forward_lock_test.js
@@ -0,0 +1,63 @@
+/* global fetchBuffer, ForwardLock, parseMetadataBlob, MockLazyLoader,
+   MockGetDeviceStorage */
+'use strict';
+
+require('/test/unit/metadata/utils.js');
+require('/shared/js/omadrm/fl.js');
+require('/js/metadata/forward_lock.js');
+require('/js/metadata/id3v2.js');
+
+suite('forwardlock files', function() {
+
+  var RealLazyLoader, RealGetDeviceStorage, RealForwardLockGetKey;
+
+  var secret = 0xDEADBEEF;
+  function mockGetKey(callback) {
+    callback(secret);
+  }
+
+  setup(function(done) {
+    this.timeout(1000);
+    RealLazyLoader = window.LazyLoader;
+    window.LazyLoader = MockLazyLoader;
+
+    RealGetDeviceStorage = navigator.getDeviceStorage;
+    navigator.getDeviceStorage = MockGetDeviceStorage;
+
+    RealForwardLockGetKey = ForwardLock.getKey;
+    ForwardLock.getKey = mockGetKey;
+
+    require('/js/metadata_scripts.js', function() {
+      done();
+    });
+  });
+
+  teardown(function() {
+    window.LazyLoader = RealLazyLoader;
+    navigator.getDeviceStorage = RealGetDeviceStorage;
+    ForwardLock.getKey = RealForwardLockGetKey;
+  });
+
+  test('forwardlocked id3v2', function(done) {
+    var vendorMetadata = {vendor: 'Songs for Cool Kids, Inc'};
+    fetchBuffer('/test-data/id3v2.4-simple-latin1.mp3').then(function(buffer) {
+      return ForwardLock.lockBuffer(
+        secret, buffer, 'audio/mpeg', vendorMetadata
+      );
+    }).then(parseMetadataBlob).then(function(metadata) {
+      done(function() {
+        assert.strictEqual(metadata.locked, true);
+        assert.strictEqual(metadata.vendor, vendorMetadata.vendor);
+        assert.strictEqual(metadata.tag_format, 'id3v2.4.0');
+        assert.strictEqual(metadata.artist, 'AC/DC');
+        assert.strictEqual(metadata.album, 'Dirty Deeds Done Dirt Cheap');
+        assert.strictEqual(metadata.title, 'Problem Child');
+        assert.strictEqual(metadata.tracknum, 5);
+        assert.strictEqual(metadata.trackcount, 9);
+        assert.strictEqual(metadata.discnum, 1);
+        assert.strictEqual(metadata.disccount, 1);
+      });
+    });
+  });
+
+});
