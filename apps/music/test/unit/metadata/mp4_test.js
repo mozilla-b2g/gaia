b3753dda56f6d350b792c5e0b6462c11cc44ae89
Hubert Figuière <hub@figuiere.net>
2014-12-04 13:21:52 -0800

    Bug 1107593 - Part 1: unit testing of the cover art in m4a files.

--- apps/music/test/unit/metadata/mp4_test.js
+++ apps/music/test/unit/metadata/mp4_test.js
@@ -1,10 +1,10 @@
-/* global parseMetadata, MockLazyLoader, MockGetDeviceStorage */
+/* global parseMetadata, loadPicture, MockLazyLoader, MockGetDeviceStorage */
 'use strict';
 
 require('/test/unit/metadata/utils.js');
 require('/js/metadata/mp4.js');
 
-suite('mp4 tags', function() {
+suite('mp4', function () {
   var RealLazyLoader, RealGetDeviceStorage;
 
   setup(function(done) {
@@ -25,18 +25,34 @@ suite('mp4 tags', function() {
     navigator.getDeviceStorage = RealGetDeviceStorage;
   });
 
-  test('m4a tags', function(done) {
-    parseMetadata('/test-data/aac-tags.m4a').then(function(metadata) {
-      done(function() {
-        assert.strictEqual(metadata.tag_format, 'mp4');
-        assert.strictEqual(metadata.artist, 'The Tragically Hip');
-        assert.strictEqual(metadata.album, 'Trouble At The Henhouse');
-        assert.strictEqual(metadata.title, 'Giftshop');
-        assert.strictEqual(metadata.tracknum, 1);
-        assert.strictEqual(metadata.trackcount, 12);
-        assert.strictEqual(metadata.discnum, 1);
-        assert.strictEqual(metadata.disccount, 1);
+
+  suite('mp4 tags', function() {
+    var expectedPicture;
+    setup(function(done) {
+      loadPicture('/test-data/album-art.jpg', 'image/jpeg', 'embedded').then(
+        function(data) {
+          expectedPicture = data;
+          done();
+        }
+      );
+    });
+
+
+    test('m4a tags', function(done) {
+      parseMetadata('/test-data/aac-tags.m4a').then(function(metadata) {
+        done(function() {
+          assert.strictEqual(metadata.tag_format, 'mp4');
+          assert.strictEqual(metadata.artist, 'The Tragically Hip');
+          assert.strictEqual(metadata.album, 'Trouble At The Henhouse');
+          assert.strictEqual(metadata.title, 'Giftshop');
+          assert.strictEqual(metadata.tracknum, 1);
+          assert.strictEqual(metadata.trackcount, 12);
+          assert.strictEqual(metadata.discnum, 1);
+          assert.strictEqual(metadata.disccount, 1);
+          assert.deepEqual(metadata.picture, expectedPicture);
+        });
       });
     });
   });
+
 });
