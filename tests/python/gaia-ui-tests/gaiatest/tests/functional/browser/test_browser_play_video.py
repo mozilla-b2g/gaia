c0c88674b649ca1f139817f6ba2d479dce8fa4f3
BavarianTomcat <tomcat@mozilla.com>
2014-12-10 16:09:46 +0100

    Revert "Bug 982839 - Write a test to protect against the regression seen in bug 981804 (navigate out of fullscreen video)" for f2 test failures
    This reverts commit 801300deb395acf4a1a61c511a63969e3aac4c83.

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_play_video.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/test_browser_play_video.py
@@ -66,23 +66,3 @@ class TestVideo(GaiaTestCase):
         # Tap mute button
         player.tap_mute()
         player.tap_unmute()
-
-        player.tap_full_screen()
-        permission = PermissionDialog(self.marionette)
-        self.marionette.switch_to_default_content()
-        permission.wait_for_permission_dialog_displayed()
-        permission.tap_to_confirm_permission()
-
-        # The interaction with the permission dialog makes us
-        # loose connection to the player frame, so we need to reconnect
-        browser.switch_to_content()
-        player = HTML5Player(self.marionette)
-
-        Wait(self.marionette).until(lambda m: player.is_fullscreen)
-
-        # After tapping full screen, the controls disappear, make them appear again
-        player.invoke_controls()
-        Wait(self.marionette).until(lambda m: player.controls_visible)
-        player.tap_full_screen()
-        Wait(self.marionette).until(lambda m: player.is_fullscreen is False)
-        Wait(self.marionette).until(lambda m: player.controls_visible is False)
