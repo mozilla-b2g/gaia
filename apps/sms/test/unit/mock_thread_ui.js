91d2168331ff3e9833ce07e069e6d1f22cca89a7
Luke Chang <lchang@mozilla.com>
2014-12-12 19:26:49 +0800

    Bug 1105977 - [FFOS2.0][Woodduck][SMS] The discard function is wrong
    cherry-picked from 5e0a980ff72b83ee4fd23fa8066773d3471e88d4

--- apps/sms/test/unit/mock_thread_ui.js
+++ apps/sms/test/unit/mock_thread_ui.js
@@ -93,6 +93,7 @@ var MockThreadUI = {
   promptContact: function() {},
   prompt: function() {},
   saveDraft: function() {},
+  discardDraft: function() {},
   onCreateContact: function() {},
   isShowMessageErrorCalledTimes: 0,
   showMessageError: function() {
