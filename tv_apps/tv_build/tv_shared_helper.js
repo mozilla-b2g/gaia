89ad08c085933a759e27aae3829db838b706b4f3
John Hu <johu@mozilla.com>
2014-12-15 11:23:04 +0800

    Bug 1111416 - [Stingray] enable webcomponents support in tv_build

--- tv_apps/tv_build/tv_shared_helper.js
+++ tv_apps/tv_build/tv_shared_helper.js
@@ -15,9 +15,15 @@
 
   var CSS_FONT =
      /src: (?:url\()?['"].*tv_shared\/([^\/]+)\/([^'"\s]+)['"](?:\))?.*;$/gm;
+
+  var WEB_COMPONENT_PATH = 'components';
+  var WEB_COMPONENT_EXCLUSIVE_LIST = [
+    /^examples$/gm,// examples folder
+    /^\..+/gm]; // invisible files
       
   exports.TVSharedHelper = {
     execute: function(options) {
+      this.copiedWebComponents = [];
       this.sourceDir = utils.getFile(options.APP_DIR);
       this.stageDir = utils.getFile(options.STAGE_APP_DIR);
       this.tvSharedDir = utils.getFile(options.GAIA_DIR, 'tv_apps',
@@ -57,6 +63,29 @@
       }
     },
 
+    copyWebComponentToStage: function(file, target, component) {
+      if (this.copiedWebComponents.indexOf(file.leafName) > -1) {
+        // web component is already copied.
+        return;
+      }
+
+      var files = utils.ls(file, false);
+      files.forEach((function(wc) {
+        var excluded = WEB_COMPONENT_EXCLUSIVE_LIST.some(function(regex) {
+          return regex.test(wc.leafName);
+        });
+        if (excluded) {
+          // file is in exclusive list, we don't need it.
+          return;
+        }
+
+        var targetFile = target.clone();
+        targetFile.append(wc.leafName);
+        this.copyFileToStage(wc, targetFile);
+      }).bind(this));
+      this.copiedWebComponents.push(file.leafName);
+    },
+
     copyMatchedFilesToStage: function(regex, content) {
       var matches = null;
       var sourceFile = null;
@@ -68,17 +97,24 @@
         targetFile.append('tv_shared');
         sourceFile.append(matches[1]);
         targetFile.append(matches[1]);
-        matches[2].split('/').forEach(function(segment) {
-          sourceFile.append(segment);
-          targetFile.append(segment);
-        });
-        // copy to stage
-        this.copyFileToStage(sourceFile, targetFile);
-        // check other html and css files
-        if (matches[2].endsWith('.html')) {
-          this.analyzeHtml(sourceFile);
-        } else if (matches[2].endsWith('.css')) {
-          this.analyzeCss(sourceFile);
+        var patchs = matches[2].split('/');
+        if (matches[1] === WEB_COMPONENT_PATH) {
+          sourceFile.append(patchs[0]);
+          targetFile.append(patchs[0]);
+          this.copyWebComponentToStage(sourceFile, targetFile, patchs[0]);
+        } else {
+          patchs.forEach(function(segment) {
+            sourceFile.append(segment);
+            targetFile.append(segment);
+          });
+          // copy to stage
+          this.copyFileToStage(sourceFile, targetFile);
+          // check other html and css files
+          if (matches[2].endsWith('.html')) {
+            this.analyzeHtml(sourceFile);
+          } else if (matches[2].endsWith('.css')) {
+            this.analyzeCss(sourceFile);
+          }
         }
       }
     },
