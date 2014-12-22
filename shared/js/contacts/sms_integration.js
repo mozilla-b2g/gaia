1fcba0677b63194d5dbef0efc7cd939781dfab87
Salvador de la Puente <salva@unoyunodiez.com>
2014-12-22 12:45:27 +0100

    Revert "Merge pull request #26240 from lodr/bug-997547-text-to-email-from-contacts"
    This reverts commit 87e91a8c0ff44ff23c49d62c0d8f3827e0bccff9, reversing
    changes made to 17b0cf9691ff70ee02b1254691351d6b63086c03.

--- shared/js/contacts/sms_integration.js
+++ shared/js/contacts/sms_integration.js
@@ -10,7 +10,7 @@ var SmsIntegration = {
         name: 'new',
         data: {
           type: 'websms/sms',
-          target: number
+          number: number
         }
       });
     } catch (e) {
