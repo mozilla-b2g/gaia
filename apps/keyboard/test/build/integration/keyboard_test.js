d41da1e1ac2b52f195a5fe763f4d7054f3171484
Timothy Guan-tin Chien <timdream@gmail.com>
2014-12-22 17:58:54 +0800

    Bug 1112460 - Part IV, Amend the build test to check the content of the zipped files

--- apps/keyboard/test/build/integration/keyboard_test.js
+++ apps/keyboard/test/build/integration/keyboard_test.js
@@ -4,6 +4,7 @@
 
 var assert = require('chai').assert;
 var path = require('path');
+var fs = require('fs');
 var helper = require('helper');
 var AdmZip = require('adm-zip');
 
@@ -11,44 +12,68 @@ suite('Keyboard tests', function() {
   suiteSetup(helper.cleanupWorkspace);
   teardown(helper.cleanupWorkspace);
 
-  test('APP=keyboard make, checking keyboard are available',
-    function(done) {
-      var cmd = 'APP=keyboard make';
-      helper.exec(cmd, function(error, stdout, stderr) {
-        helper.checkError(error, stdout, stderr);
+  test('APP=keyboard make', function(done) {
+    var cmd = 'APP=keyboard make';
+    helper.exec(cmd, function(error, stdout, stderr) {
+      helper.checkError(error, stdout, stderr);
 
-        var keyboard = new AdmZip(path.join(process.cwd(), 'profile',
-        'webapps', 'keyboard.gaiamobile.org', 'application.zip'));
-        var files = keyboard.getEntries();
-        var config = JSON.parse(process.env.BUILD_CONFIG);
-        var defaultLayouts = config.GAIA_KEYBOARD_LAYOUTS.split(',').sort();
-        var layouts = defaultLayouts.map(function(layout) {
-          return 'js/layouts/' + layout + '.js';
-        });
-        var imes = [
-          'js/imes/jshangul/',
-          'js/imes/jspinyin/',
-          'js/imes/latin/'
-        ];
-        var dicts = [
-          'js/imes/latin/dictionaries/de.dict',
-          'js/imes/latin/dictionaries/en_us.dict',
-          'js/imes/latin/dictionaries/es.dict',
-          'js/imes/latin/dictionaries/fr.dict',
-          'js/imes/latin/dictionaries/pl.dict',
-          'js/imes/latin/dictionaries/pt_br.dict'
-        ];
-        var checkList = [].concat(layouts, imes, dicts);
+      var config = JSON.parse(process.env.BUILD_CONFIG);
+      var layoutIds = config.GAIA_KEYBOARD_LAYOUTS.split(',').sort();
+      var zipPath = path.join(process.cwd(), 'profile',
+        'webapps', 'keyboard.gaiamobile.org', 'application.zip');
+      var appDirPath = config.GAIA_DIR + '/apps/keyboard';
 
-        var result = checkList.every(function(check) {
-          return files.some(function(file) {
-            return file.entryName.indexOf(check) !== -1;
-          });
-        });
-
-        assert.isTrue(result, 'necessary files exist in profile folder');
-
-        done();
+      var layouts = layoutIds.map(function(layout) {
+        return 'js/layouts/' + layout + '.js';
       });
+
+      // Unfortunately we can only whilelist files here because we shouldn't
+      // throw error if excluded files are missing (e.g. README).
+      var imes = [
+        'js/imes/jshangul/jshangul.js',
+        'js/imes/jspinyin/empinyin_files.data',
+        'js/imes/jspinyin/empinyin_files.js',
+        'js/imes/jspinyin/jspinyin.js',
+        'js/imes/jspinyin/libpinyin.js',
+        'js/imes/jspinyin/worker.js',
+        'js/imes/latin/latin.js',
+        'js/imes/latin/predictions.js',
+        'js/imes/latin/worker.js'
+      ];
+
+      var dicts = [
+        'js/imes/latin/dictionaries/de.dict',
+        'js/imes/latin/dictionaries/en_us.dict',
+        'js/imes/latin/dictionaries/es.dict',
+        'js/imes/latin/dictionaries/fr.dict',
+        'js/imes/latin/dictionaries/pl.dict',
+        'js/imes/latin/dictionaries/pt_br.dict'
+      ];
+
+      var checkList = [].concat(layouts, imes, dicts);
+      checkList.forEach(function(path) {
+        helper.checkFileInZip(zipPath, path, appDirPath + '/' + path);
+      });
+
+      // Verify inputs entry in manifest
+      var zip = new AdmZip(zipPath);
+      var entry = zip.getEntry('manifest.webapp');
+      var manifest = JSON.parse(zip.readAsText(entry));
+      var inputKeysInManifest = Object.keys(manifest.inputs);
+
+      assert.deepEqual(inputKeysInManifest.sort(),
+                       [].concat(layoutIds, 'number').sort());
+
+      // Verify dictionary config
+      var dictJSON = JSON.parse(fs.readFileSync(
+            appDirPath +
+            '/test/build/integration/resources/' +
+            'default-make-dictionaries.json'));
+
+      helper.checkFileContentInZip(
+        zipPath, 'js/settings/dictionaries.json', dictJSON, true);
+
+      done();
     });
+  });
 });
