f03a0c46ec5153f60cb29b8efb298d8cef0a3dd5
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2014-12-22 10:40:12 -0500

    Revert "Merge pull request #26629 from aosmond/bug1078435"
    This reverts commit 3919250c8884439a9faaddfa18057d8b8c7f6002, reversing
    changes made to 6af3d029bae3a14f400fec0926f0f8ad7b579b4b.

--- apps/camera/js/lib/camera/camera.js
+++ apps/camera/js/lib/camera/camera.js
@@ -497,11 +497,7 @@ Camera.prototype.previewSizes = function() {
  */
 Camera.prototype.previewSize = function() {
   var sizes = this.previewSizes();
-  var resolution;
-  if (this.mode === 'video') {
-    resolution = this.resolution();
-  }
-  var size = CameraUtils.getOptimalPreviewSize(sizes, undefined, resolution);
+  var size = CameraUtils.getOptimalPreviewSize(sizes);
   debug('get optimal previewSize', size);
   return size;
 };
