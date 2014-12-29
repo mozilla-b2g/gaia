7da315708ec42493d3510cac6d00c445d9318218
Alexandre Poirot <poirot.alex@gmail.com>
2014-12-29 18:54:08 +0100

    Bug 1102972 - ensure rebuilding app removed from profile folder. r=rickychien

--- build/build-app.js
+++ build/build-app.js
@@ -5,13 +5,13 @@
 var utils = require('utils');
 
 exports.execute = function(options) {
-  var app = utils.getFile(options.APP_DIR);
-  require(app.leafName + '/build').execute(options);
+  var webapp = utils.getWebapp(options.APP_DIR, options);
+  require(webapp.appDir.leafName + '/build').execute(options, webapp);
 
   // Wait for all app tasks to be done before proceeding.
   utils.processEvents(function () {
     return { wait: false };
   });
 
-  require('post-app').execute(options);
+  require('post-app').execute(options, webapp);
 };
