b11406418d6907c9ac2211a6f109847b4bdcdf06
sudheesh001 <sudheesh1995@outlook.com>
2015-01-01 18:36:56 +0530

    Fixes bug 1005331 jshint fixes for shared/js/media r=djf

--- shared/js/media/jpeg_metadata_parser.js
+++ shared/js/media/jpeg_metadata_parser.js
@@ -1,5 +1,6 @@
 'use strict';
-
+/* global BlobView */
+/* exported parseJPEGMetadata */
 //
 // This file defines a single function that asynchronously reads a
 // JPEG file (or blob) to determine its width and height and find the
@@ -94,8 +95,8 @@ function parseJPEGMetadata(file, metadataCallback, metadataError) {
 
       case 0xE1:  // APP1 segment. Probably holds EXIF metadata
         parseAPP1(data);
-        /* fallthrough */
 
+      /* falls through */
       default:
         // A segment we don't care about, so just go on and read the next one
         if (isLastSegment) {
@@ -205,8 +206,9 @@ function parseJPEGMetadata(file, metadataCallback, metadataError) {
     var ifd0entries = data.getUint16(offset + 10, byteorder);
     var ifd1 = data.getUint32(offset + 12 + 12 * ifd0entries, byteorder);
     // If there is an offset for IFD1, parse that
-    if (ifd1 !== 0)
+    if (ifd1 !== 0) {
       parseIFD(data, ifd1 + 10, byteorder, exif, true);
+    }
 
     return exif;
   }
@@ -217,8 +219,9 @@ function parseJPEGMetadata(file, metadataCallback, metadataError) {
       parseEntry(data, offset + 2 + 12 * i, byteorder, exif);
     }
 
-    if (onlyParseOne)
+    if (onlyParseOne) {
       return;
+    }
 
     var next = data.getUint32(offset + 2 + 12 * numentries, byteorder);
     if (next !== 0 && next < file.size) {
@@ -292,8 +295,9 @@ function parseJPEGMetadata(file, metadataCallback, metadataError) {
     var tagname = tagnames[tag];
 
     // If we don't know about this tag type or already processed it, skip it
-    if (!tagname || exif[tagname])
+    if (!tagname || exif[tagname]) {
       return;
+    }
 
     var type = data.getUint16(offset + 2, byteorder);
     var count = data.getUint32(offset + 4, byteorder);
@@ -305,9 +309,10 @@ function parseJPEGMetadata(file, metadataCallback, metadataError) {
   }
 
   function parseValue(data, offset, type, count, byteorder) {
+    var i;
     if (type === 2) { // ASCII string
       var codes = [];
-      for (var i = 0; i < count - 1; i++) {
+      for (i = 0; i < count - 1; i++) {
         codes[i] = data.getUint8(offset + i);
       }
       return String.fromCharCode.apply(String, codes);
@@ -317,7 +322,7 @@ function parseJPEGMetadata(file, metadataCallback, metadataError) {
       } else {
         var values = [];
         var size = typesize[type];
-        for (var i = 0; i < count; i++) {
+        for (i = 0; i < count; i++) {
           values[i] = parseOneValue(data, offset + size * i, type, byteorder);
         }
         return values;
