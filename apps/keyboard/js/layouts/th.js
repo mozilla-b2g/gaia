276536734709b2f044472ba6fb0dc28e6f75ec1d
Greg Green <gpgreen@gmail.com>
2015-01-01 11:37:04 -0800

    Bug 844717 - Add Thai input method for Gaia. Picked up previous patch and made it work with latest. R=gpgreen@gmail.com

--- apps/keyboard/js/layouts/th.js
+++ apps/keyboard/js/layouts/th.js
@@ -3,81 +3,80 @@ Keyboards.th = {
   shortLabel: 'Th',
   menuLabel: 'ไทย', /*ไทย*/
   secondLayout: true,
-  basicLayoutKey: '\u1000\u1001\u1002', /*ကခဂ*/
-  alternateLayoutKey: 'กขค', /*กขค*/
+  basicLayoutKey: 'กขค', /*กขค*/
   imEngine: 'latin',
   types: ['text', 'url', 'email'],
   width: 11,
   keys: [
-      [
-        { value: 'ๆ' }, { value: 'ภ' }, { value: 'ถ' }, { value: 'ุ' },
-        { value: 'ู' } , { value: 'ึ' }, { value: 'ค' } , { value: 'ต' },
-        { value: 'จ' }, { value: 'ข' }, { value: 'ช' }
-      ], [
-        { value: 'ไ' }, { value: 'ำ' }, { value: 'พ' }, { value: 'ะ' },
-        { value: 'ั' } , { value: 'ี' }, { value: 'ร' }, { value: 'น' },
-        { value: 'ย' }, { value: 'บ' }, { value: 'ล' }
-      ], [
-        { value: 'ฟ' }, { value: 'ห' }, { value: 'ก' }, { value: 'ด' },
-        { value: 'เ' } , { value: '้' }, { value: '่' }, { value: 'า' },
-        { value: 'ส' }, { value: 'ว' }, { value: 'ง' }
-      ], [
-        { value: '⇪', keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
-        { value: 'ป' }, { value: 'แ' }, { value: 'อ' }, { value: 'ิ' },
-        { value: 'ื' }, { value: 'ท' }, { value: 'ม' },
-        { value: 'ใ' }, { value: 'ฝ' },
-        { value: '⌫', keyCode: KeyEvent.DOM_VK_BACK_SPACE }
-      ], [
-        { value: '&nbsp', ratio: 9, keyCode: KeyboardEvent.DOM_VK_SPACE },
-        { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
-      ]
+    [
+      { value: 'ๆ' }, { value: 'ภ' }, { value: 'ถ' }, { value: 'ุ' },
+      { value: 'ู' } , { value: 'ึ' }, { value: 'ค' } , { value: 'ต' },
+      { value: 'จ' }, { value: 'ข' }, { value: 'ช' }
+    ], [
+      { value: 'ไ' }, { value: 'ำ' }, { value: 'พ' }, { value: 'ะ' },
+      { value: 'ั' } , { value: 'ี' }, { value: 'ร' }, { value: 'น' },
+      { value: 'ย' }, { value: 'บ' }, { value: 'ล' }
+    ], [
+      { value: 'ฟ' }, { value: 'ห' }, { value: 'ก' }, { value: 'ด' },
+      { value: 'เ' } , { value: '้' }, { value: '่' }, { value: 'า' },
+      { value: 'ส' }, { value: 'ว' }, { value: 'ง' }
+    ], [
+      { value: '⇪', keyCode: KeyEvent.DOM_VK_CAPS_LOCK },
+      { value: 'ป' }, { value: 'แ' }, { value: 'อ' }, { value: 'ิ' },
+      { value: 'ื' }, { value: 'ท' }, { value: 'ม' },
+      { value: 'ใ' }, { value: 'ฝ' },
+      { value: '⌫', keyCode: KeyEvent.DOM_VK_BACK_SPACE }
+    ], [
+      { value: '&nbsp', ratio: 9, keyCode: KeyboardEvent.DOM_VK_SPACE },
+      { value: '↵', ratio: 2, keyCode: KeyEvent.DOM_VK_RETURN }
+    ]
   ],
   upperCase: {
-      'ๆ': '฿',
-      'ภ': '1',
-      'ถ': '2',
-      'ุ': '3',
-      'ู': '4',
-      'ึ': '5',
-      'ค': '6',
-      'ต': '7',
-      'จ': '8',
-      'ข': '9',
-      'ช': '0',
-      'ไ': 'ๆ',
-      'ำ': 'ฎ',
-      'พ': 'ฑ',
-      'ะ': 'ธ',
-      'ั': 'ํ',
-      'ี': '๊',
-      'ร': 'ณ',
-      'น': 'ฯ',
-      'ย': 'ญ',
-      'บ': 'ฐ',
-      'ล': 'ฅ',
-      'ฟ': 'ฤ',
-      'ห': 'ฆ',
-      'ก': 'ฏ',
-      'ด': 'โ',
-      'เ': 'ฌ',
-      '้': '็',
-      '่': '๋',
-      'า': 'ษ',
-      'ส': 'ศ',
-      'ว': 'ซ',
-      'ง': 'ข',
-      'ป': ')',
-      'แ': 'ฉ',
-      'อ': 'ฮ',
-      'ิ': 'ฺ',
-      'ื': '์',
-      'ท': '?',
-      'ม': 'ฒ',
-      'ใ': 'ฬ',
-      'ฝ': 'ฦ',
-      'ผ': '('
-    },
-    alternateLayout: {
+    'ๆ': '฿',
+    'ภ': '1',
+    'ถ': '2',
+    'ุ': '3',
+    'ู': '4',
+    'ึ': '5',
+    'ค': '6',
+    'ต': '7',
+    'จ': '8',
+    'ข': '9',
+    'ช': '0',
+    'ไ': 'ๆ',
+    'ำ': 'ฎ',
+    'พ': 'ฑ',
+    'ะ': 'ธ',
+    'ั': 'ํ',
+    'ี': '๊',
+    'ร': 'ณ',
+    'น': 'ฯ',
+    'ย': 'ญ',
+    'บ': 'ฐ',
+    'ล': 'ฅ',
+    'ฟ': 'ฤ',
+    'ห': 'ฆ',
+    'ก': 'ฏ',
+    'ด': 'โ',
+    'เ': 'ฌ',
+    '้': '็',
+    '่': '๋',
+    'า': 'ษ',
+    'ส': 'ศ',
+    'ว': 'ซ',
+    'ง': 'ข',
+    'ป': ')',
+    'แ': 'ฉ',
+    'อ': 'ฮ',
+    'ิ': 'ฺ',
+    'ื': '์',
+    'ท': '?',
+    'ม': 'ฒ',
+    'ใ': 'ฬ',
+    'ฝ': 'ฦ',
+    'ผ': '('
+  },
+  alternateLayout: {
       alt: {
         '0': 'º',
         '$': '฿ € £ ¥ R$',
@@ -122,8 +121,7 @@ Keyboards.th = {
         { value: '{' }, { value: '}' }, { value: '_' }, { value: '-' },
         { value: '+' }, { value: '=' }
       ], [
-        { value: 'กขค'/*กขค*/, ratio: 1.5,
-          keyCode: KeyEvent.DOM_VK_ALT },
+        { value: 'กขค'/*กขค*/, ratio: 1.5, keyCode: KeyEvent.DOM_VK_ALT },
         { value: '\\' }, { value: '\'' }, { value: '[' }, { value: ']' },
         { value: '<' }, { value: '>' }, {value: ';' },
         { value: '⌫', ratio: 1.5, keyCode: KeyEvent.DOM_VK_BACK_SPACE }
