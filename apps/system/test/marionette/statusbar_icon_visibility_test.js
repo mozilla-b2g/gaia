9e144e37b5c0f336086dece8c7564e6bb5c30c75
albertopq <albert.pastor@gmail.com>
2015-01-02 11:27:17 +0100

    Bug 1114764 - Statusbar visibility on inline activities

--- apps/system/test/marionette/statusbar_icon_visibility_test.js
+++ apps/system/test/marionette/statusbar_icon_visibility_test.js
@@ -74,6 +74,47 @@ marionette('Statusbar Visibility', function() {
     });
   });
 
+
+  test('Shadow is visible and maximized on inline activities', function() {
+    var BROWSER = 'app://search.gaiamobile.org';
+    var frame = system.waitForLaunch(BROWSER);
+    client.switchToFrame(frame);
+    newShareActivity();
+    var sms = system.getActivityOptionMatching('sms');
+    actions.tap(sms).perform();
+
+    client.waitFor(function() {
+      return system.inlineActivity.displayed();
+    });
+
+    client.waitFor(function() {
+      // The element is rendered with moz-element so we can't use
+      // marionette's .displayed()
+      var maximized = system.inlineActivity.findElement('.titlebar-maximized');
+      var style = maximized.scriptWith(function(element) {
+        return window.getComputedStyle(element);
+      });
+      return (style.visibility == 'visible' && style.opacity == '1');
+    });
+  });
+
+  function newShareActivity() {
+    client.executeScript(function() {
+      var a = new window.wrappedJSObject.MozActivity({
+        name: 'share',
+        data: {
+          type: 'url',
+          url: 'myurl'
+        }
+      });
+      a.onerror = function() {
+        console.log('Activity error');
+      };
+    });
+
+    client.switchToFrame();
+  }
+
   // skipping since nfc.enabled triggers HW change and icon is updated
   // on success. Status bar needs to observe nfc.status setting.
   // This will be fixed and reenabled in Bug 1103874
