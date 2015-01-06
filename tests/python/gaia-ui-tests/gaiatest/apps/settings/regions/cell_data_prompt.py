5262a8c23bf5c42bb34ce515794f1e3e12ae8cbb
Viorela Ioia <viorelaioia@gmail.com>
2015-01-06 11:41:06 +0200

    Bug 1117823 - Update locators in Settings - Cell data page

--- tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/cell_data_prompt.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/cell_data_prompt.py
@@ -8,12 +8,12 @@ from gaiatest.apps.base import Base
 
 class CellDataPrompt(Base):
 
-    _cell_data_prompt_container_locator = (By.CSS_SELECTOR, '#carrier-dc-warning')
-    _cell_data_prompt_turn_on_button_locator = (By.CSS_SELECTOR, '#carrier-dc-warning button[type="submit"]')
+    _cell_data_prompt_container_locator = (By.CSS_SELECTOR, '#settings-confirm-dialog')
+    _cell_data_prompt_turn_on_button_locator = (By.CSS_SELECTOR, '#settings-confirm-dialog button[data-l10n-id="turnOn"]')
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
-        self.wait_for_condition(lambda m: m.find_element(*self._cell_data_prompt_container_locator).location['x'] == 0)
+        self.wait_for_condition(lambda m: 'current' in m.find_element(*self._cell_data_prompt_container_locator).get_attribute('class'))
 
     def turn_on(self):
         container = self.marionette.find_element(*self._cell_data_prompt_container_locator)
