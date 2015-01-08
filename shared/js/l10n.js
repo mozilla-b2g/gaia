50520377e2cd74fb3ffe2220cd4a25b2f90e0bbe
Staś Małolepszy <stas@mozilla.com>
2015-01-08 01:41:13 +0100

    Bug 1115773 - Allow L10n's loadJSON to be sync. r=gandalf

--- shared/js/l10n.js
+++ shared/js/l10n.js
@@ -15,18 +15,33 @@
   /* jshint browser:true */
 
   var io = {
-    load: function load(url, callback, sync) {
+
+    _load: function(type, url, callback, sync) {
       var xhr = new XMLHttpRequest();
+      var needParse;
 
       if (xhr.overrideMimeType) {
-        xhr.overrideMimeType('text/plain');
+        xhr.overrideMimeType(type);
       }
 
       xhr.open('GET', url, !sync);
 
-      xhr.addEventListener('load', function io_load(e) {
+      if (type === 'application/json') {
+        //  Gecko 11.0+ forbids the use of the responseType attribute when
+        //  performing sync requests (NS_ERROR_DOM_INVALID_ACCESS_ERR).
+        //  We'll need to JSON.parse manually.
+        if (sync) {
+          needParse = true;
+        } else {
+          xhr.responseType = 'json';
+        }
+      }
+
+      xhr.addEventListener('load', function io_onload(e) {
         if (e.target.status === 200 || e.target.status === 0) {
-          callback(null, e.target.responseText);
+          // Sinon.JS's FakeXHR doesn't have the response property
+          var res = e.target.response || e.target.responseText;
+          callback(null, needParse ? JSON.parse(res) : res);
         } else {
           callback(new L10nError('Not found: ' + url));
         }
@@ -42,33 +57,14 @@
       }
     },
 
-    loadJSON: function loadJSON(url, callback) {
-      var xhr = new XMLHttpRequest();
+    load: function(url, callback, sync) {
+      return io._load('text/plain', url, callback, sync);
+    },
 
-      if (xhr.overrideMimeType) {
-        xhr.overrideMimeType('application/json');
-      }
-
-      xhr.open('GET', url);
-
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', function io_loadjson(e) {
-        if (e.target.status === 200 || e.target.status === 0) {
-          callback(null, e.target.response);
-        } else {
-          callback(new L10nError('Not found: ' + url));
-        }
-      });
-      xhr.addEventListener('error', callback);
-      xhr.addEventListener('timeout', callback);
-
-      // the app: protocol throws on 404, see https://bugzil.la/827243
-      try {
-        xhr.send(null);
-      } catch (e) {
-        callback(new L10nError('Not found: ' + url));
-      }
+    loadJSON: function(url, callback, sync) {
+      return io._load('application/json', url, callback, sync);
     }
+
   };
 
   function EventEmitter() {}
@@ -1630,7 +1626,7 @@
 
     switch (node.getAttribute('name')) {
       case 'availableLanguages':
-        meta.availableLanguages = 
+        meta.availableLanguages =
           splitAvailableLanguagesString(node.getAttribute('content'));
         break;
       case 'defaultLanguage':
