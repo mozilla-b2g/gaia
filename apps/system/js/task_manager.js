5ee2abafd3846c9454df42db863f21be01892c0e
Sam Foster <sfoster@mozilla.com>
2015-01-08 13:01:32 -0800

    Bug 1118390 - Lock orientation when task manager shows. r=etienne
    Conflicts:
    
    	apps/system/test/unit/task_manager_test.js

--- apps/system/js/task_manager.js
+++ apps/system/js/task_manager.js
@@ -1,5 +1,5 @@
 /* global Card, eventSafety, SettingsListener,
-          Service, homescreenLauncher, StackManager */
+          Service, homescreenLauncher, StackManager, OrientationManager */
 
 (function(exports) {
   'use strict';
@@ -143,6 +143,7 @@
 
     this.publish('cardviewbeforeshow');
 
+    screen.mozLockOrientation(OrientationManager.defaultOrientation);
     this._placeCards();
     this.setActive(true);
 
