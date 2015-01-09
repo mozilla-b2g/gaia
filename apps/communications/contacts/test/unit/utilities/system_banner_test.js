4399464e4af145d92dfcd31747d7bf2b2995f474
Fernando Campo <fernando.campo@telefonica.com>
2015-01-09 12:00:13 +0000

    Bug 1115317 - can't see number of contacts imported

--- apps/communications/contacts/test/unit/utilities/system_banner_test.js
+++ apps/communications/contacts/test/unit/utilities/system_banner_test.js
@@ -1,37 +1,73 @@
-/* global utils, MocksHelper */
+/* global utils, MocksHelper, MockL10n */
 
 'use strict';
 
-document.body.innerHTML = '<div id="statusMsg">' +
-'<p class="statusMsg">Test</p></div>';
+require('/shared/test/unit/mocks/mock_l10n.js');
 require('/shared/test/unit/mocks/mock_lazy_loader.js');
-require('/shared/js/contacts/import/utilities/status.js');
 
 var mocksHelperForActivities = new MocksHelper([
   'LazyLoader'
 ]).init();
 
+function loadHTML() {
+  document.body.innerHTML = '<div id="statusMsg" class="hidden">' +
+                            '  <p class="statusMsg">Test</p>' +
+                            '</div>';
+}
+
 suite('> System Banner Utilities', function() {
-  var subject, clock;
+  var statusDOM,
+      subject;
 
+  var realL10n;
 
+  mocksHelperForActivities.attachTestHelpers();
   suiteSetup(function() {
-    subject = utils.status;
-    mocksHelperForActivities.suiteSetup();
+    realL10n = navigator.mozL10n;
+    navigator.mozL10n = MockL10n;
   });
 
-  setup(function() {
-    clock = sinon.useFakeTimers();
+  setup(function(done) {
+    this.sinon.useFakeTimers();
+    loadHTML();
+    // calling it here to be able to reload the HTML on setup, as status.js
+    // assigns the #statusMsg on load
+    require('/shared/js/contacts/import/utilities/status.js', function () {
+      subject = utils.status;
+      statusDOM = document.querySelector('#statusMsg');
+      done();
+    });
   });
 
   suiteTeardown(function() {
-    clock.restore();
+    navigator.mozL10n = realL10n;
+    subject = null;
+    document.body.innerHTML = '';
   });
 
-  test('Check status msg shown', function(done) {
+  test('Status msg shown when passing proper parameter', function() {
+    var parameter = {
+      id: 'some_id',
+      args: {'optional_args': 'args value'}
+    };
+
+    utils.status.show(parameter);
+    assert.isFalse(statusDOM.classList.contains('hidden'));
+  });
+
+  test('Status not shown when passing wrong parameter', function() {
+    var parameter = 'some string';
+    var errorSpy = this.sinon.spy(console, 'error');
+
+    utils.status.show(parameter);
+    assert.isTrue(statusDOM.classList.contains('hidden'));
+    assert.isTrue(errorSpy.calledWith('Status arguments must be objects'));
+  });
+
+  test('Status msg hidden on time after shown', function(done) {
     var testhide = sinon.spy(subject, 'hide');
-    utils.status.show('system banner shown');
-    clock.tick(5000);
+    utils.status.show({id: 'system_banner_shown'});
+    this.sinon.clock.tick(5000);
     assert.isTrue(testhide.called);
     done();
   });
