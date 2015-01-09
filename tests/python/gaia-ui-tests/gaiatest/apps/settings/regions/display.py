6e21cd8c408f4bf3c7ef36252f22c4e5dabbe5ea
Dave Hunt <dave.hunt@gmail.com>
2015-01-09 11:49:02 +0000

    Bug 1074117 - Use expected conditions and optimise element lookups in settings app object. r=fstrugariu

--- tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/display.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/display.py
@@ -3,8 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 try:
+    from marionette import (expected,
+                            Wait)
     from marionette.by import By
 except:
+    from marionette_driver import (expected,
+                                   Wait)
     from marionette_driver.by import By
 
 from gaiatest.apps.base import Base
@@ -20,8 +24,10 @@ class Display(Base):
 
     @property
     def wallpaper_preview_src(self):
-        self.wait_for_element_displayed(*self._wallpaper_preview_locator)
-        return self.marionette.find_element(*self._wallpaper_preview_locator).get_attribute('src')
+        element = Wait(self.marionette).until(
+            expected.element_present(*self._wallpaper_preview_locator))
+        Wait(self.marionette).until(expected.element_displayed(element))
+        return element.get_attribute('src')
 
     def pick_wallpaper(self):
         self.marionette.find_element(*self._wallpaper_pick_locator).tap()
