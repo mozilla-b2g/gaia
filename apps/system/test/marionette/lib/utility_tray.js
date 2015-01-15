39794d6d1a15bb74bc109f05261ec43e7df50fad
albertopq <albert.pastor@gmail.com>
2015-01-15 13:03:49 +0100

    Bug 1121188 - Allowing pointer events on the attention screen chrome
    Conflicts:
    	apps/system/test/marionette/lib/fakeloopapp.js
    	apps/system/test/marionette/lib/utility_tray.js
    	apps/system/test/marionette/utility_tray_gestures_test.js

--- apps/system/test/marionette/lib/utility_tray.js
+++ apps/system/test/marionette/lib/utility_tray.js
@@ -4,6 +4,15 @@
   var UtilityTray = function(client) {
     this.client = client;
     this.actions = new Actions(client);
+    var width = client.executeScript(function() {
+      return window.innerWidth;
+    });
+    this.halfWidth = width / 2;
+
+    var height = client.executeScript(function() {
+      return window.innerHeight;
+    });
+    this.halfHeight = height / 2;
   };
 
   var Actions = require('marionette-client').Actions;
@@ -62,6 +71,29 @@
         });
         win.UtilityTray.hide();
       });
+    },
+
+    swipeDown: function swipeDown(element) {
+      this.client.waitFor(function() {
+        return element.displayed;
+      });
+
+      // Works better than actions.flick().
+      this.actions
+        .press(element)
+        .moveByOffset(0, this.halfHeight)
+        .release()
+        .perform();
+    },
+
+    swipeUp: function swipeUp(element) {
+      var halfWidth = this.halfWidth;
+      this.client.waitFor(function() {
+        return element.displayed;
+      });
+      this.actions
+        .flick(element, halfWidth, 10, halfWidth, -this.halfHeight, 100)
+        .perform();
     }
 
   };
