e4cd2c0d1ddc8014e974a888df349240eccb202d
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2015-01-15 15:49:16 +0100

    Bug 1097928 - Convert MozPaymentProvider API to WebIDL. r=fabrice

--- apps/system/js/payment.js
+++ apps/system/js/payment.js
@@ -20,9 +20,9 @@ var Payment = {
     // We save the mozChromeEvent identifiers to send replies back from content
     // with this exact value.
     this.chromeEventId = e.detail.id;
-    if (!this.chromeEventId)
+    if (!this.chromeEventId) {
       return;
-
+    }
     this.requestId = e.detail.requestId;
 
     switch (e.detail.type) {
@@ -57,8 +57,9 @@ var Payment = {
         frame.src = kPaymentConfirmationScreen;
         frame.addEventListener('mozbrowserloadend', function addReqs(evt) {
           var frame = evt.target;
-          if (!frame || !requests)
+          if (!frame || !requests) {
             return;
+          }
 
           // TODO: Temp layout until issue #2692 is solved.
           var frameDocument = frame.contentWindow.document;
@@ -90,35 +91,51 @@ var Payment = {
       // Chrome asks Gaia to show the payment flow according to the
       // payment request selected by the user.
       case 'open-payment-flow-dialog':
-        if (!e.detail.uri)
-          return;
-
-        // TODO: For now, known payment providers (BlueVia and Mozilla Market)
-        //       only accepts the JWT by GET, so we just add it to the URI.
-        e.detail.uri += e.detail.jwt;
-
         this.trustedUILayers[this.requestId] = this.chromeEventId;
 
+        // We create the payment iframe but we don't set its src. Instead
+        // we simply return the iframe instance to the platform once it is
+        // inserted in the trusted UI container. At that point the platform
+        // sets the corresponding payment information in the iframe.
         var frame = document.createElement('iframe');
         frame.setAttribute('mozbrowser', 'true');
         frame.classList.add('screen');
-        frame.src = e.detail.uri;
-        frame.addEventListener('mozbrowserloadstart', (function loadStart(evt) {
-          // After creating the new frame containing the payment provider buy
-          // flow, we send it back to chrome so the payment callbacks can be
-          // injected.
+
+        // We place the iframe in the DOM so we can get a DocShell and once
+        // we did that, we give the iframe back to the platform so it can set
+        // the appropriate flags including the iframe source.
+
+        var ontrustedopened = (function(event) {
+          if (!event.detail ||
+              !event.detail.config ||
+              event.detail.config.requestId != this.requestId) {
+            return;
+          }
+          window.removeEventListener('trustedopened', ontrustedopened);
+          // The iframe is already inserted in the trusted UI container at this
+          // point.
           this._dispatchEvent({
             id: this.chromeEventId,
-            frame: evt.target
+            frame: frame
           });
-          frame.removeEventListener('mozbrowserloadstart', loadStart);
-        }).bind(this));
+        }).bind(this);
 
-        // The payment flow is shown within the trusted UI
+        window.addEventListener('trustedopened', ontrustedopened);
         this._openTrustedUI(frame);
         break;
 
       case 'close-payment-flow-dialog':
+        var ontrustedclosed = (function(event) {
+          if (!event.detail ||
+              !event.detail.config ||
+              event.detail.config.requestId != this.requestId) {
+            return;
+          }
+          window.removeEventListener('trustedclosed', ontrustedclosed);
+          this._dispatchEvent({ id: this.chromeEventId });
+        }).bind(this);
+
+        window.addEventListener('trustedclosed', ontrustedclosed);
         window.dispatchEvent(new CustomEvent('killtrusted', {
           detail: {
             requestId: this.requestId,
