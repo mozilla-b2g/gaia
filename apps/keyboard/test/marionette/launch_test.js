6dbcb138bf42f8068738d0b6bc6900aade7a1aba
Rudy Lu <me@rudy.lu>
2015-01-20 11:04:24 +0800

    Bug 1123250 - Re-organize the lib files used for keyboard integration  tests.   - Put system related lib to apps/system and user loader to load these     lib files.

--- apps/keyboard/test/marionette/launch_test.js
+++ apps/keyboard/test/marionette/launch_test.js
@@ -7,7 +7,7 @@ var KeyboardTestApp = require('./lib/keyboard_test_app'),
 marionette('show Keyboard APP', function() {
   var apps = {};
   var keyboardTestApp = null;
-  var system = null;
+  var systemInputMgmt = null;
   var keyboard = null;
   var client = null;
 
@@ -26,7 +26,7 @@ marionette('show Keyboard APP', function() {
 
   setup(function() {
     keyboard =  new Keyboard(client);
-    system = client.loader.getAppClass('keyboard', 'system');
+    systemInputMgmt = client.loader.getAppClass('system', 'input_management');
 
     // create a keyboard test app
     keyboardTestApp = new KeyboardTestApp(client);
@@ -34,8 +34,8 @@ marionette('show Keyboard APP', function() {
     keyboardTestApp.textInput.click();
 
     // Wait for the keyboard pop up and switch to it
-    system.waitForKeyboardFrameDisplayed();
-    system.switchToActiveKeyboardFrame();
+    systemInputMgmt.waitForKeyboardFrameDisplayed();
+    systemInputMgmt.switchToActiveKeyboardFrame();
   });
 
   test('should show lowercase layout', function() {
@@ -78,7 +78,7 @@ marionette('show Keyboard APP', function() {
 
     // Switch back to keyboard
     client.switchToFrame();
-    system.switchToActiveKeyboardFrame();
+    systemInputMgmt.switchToActiveKeyboardFrame();
 
     // Should remain, or switched back to alpha keyboard.
     client.waitFor(function() {
@@ -113,7 +113,7 @@ marionette('show Keyboard APP', function() {
 
     client.helper.wait(3000);
 
-    system.switchToActiveKeyboardFrame();
+    systemInputMgmt.switchToActiveKeyboardFrame();
 
     var keyboardContainer =
       client.findElement('.keyboard-type-container[data-active]');
