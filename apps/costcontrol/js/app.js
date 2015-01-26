f55af211a2a4a3485831faa3e1cb429c7c4b9d48
mai <mri@tid.es>
2015-01-26 13:39:31 +0100

    Bug 1125801 - [Costcontrol] Javascript error on startup

--- apps/costcontrol/js/app.js
+++ apps/costcontrol/js/app.js
@@ -81,6 +81,13 @@ var CostControlApp = (function() {
           if (AirplaneModeHelper.getStatus() === 'enabled') {
             console.warn('The airplaneMode is enabled.');
             fakeState = 'airplaneMode';
+            window.addEventListener('airplaneModeDisabled',
+              function _onAirplanemodeDisabled(evt) {
+                if (evt.detail && evt.detail.serviceId === 'data') {
+                  waitForSIMReady(startApp);
+                }
+              }
+            );
           }
           showNonReadyScreen(fakeState);
         });
@@ -239,13 +246,6 @@ var CostControlApp = (function() {
       loadSettings();
     }
   });
-
-  window.addEventListener('airplaneModeDisabled',
-    function _onAirplanemodeDisabled(evt) {
-      if (evt.detail && evt.detail.serviceId === 'data') {
-        waitForSIMReady(startApp);
-      }
-    });
 
   function setupApp(callback) {
     setupCardHandler();
