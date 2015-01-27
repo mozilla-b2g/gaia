0a9bee1840f3cf40eb5c45b3f57b2ee40847f15f
Brian Birtles <birtles@gmail.com>
2015-01-28 08:26:03 +0900

    Comment out eventSafety and see what happens

--- apps/system/js/app_chrome.js
+++ apps/system/js/app_chrome.js
@@ -522,7 +522,7 @@
       self.element.removeEventListener('transitionend', endBackgroundFade);
     };
     this.element.addEventListener('transitionend', endBackgroundFade);
-    eventSafety(this.element, 'transitionend', endBackgroundFade, 1000);
+    // eventSafety(this.element, 'transitionend', endBackgroundFade, 1000);
 
     window.requestAnimationFrame(function updateAppColor() {
       if (finishedFade || !self.element) {
