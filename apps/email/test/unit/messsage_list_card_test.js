ace893bf20aff7f1251e6a2799d7cdeac6a146da
Yura Zenevich <yzenevich@mozilla.com>
2015-01-27 13:34:20 -0500

    Bug 1068705 - improved accessibility of the edit toolbar of the message list card.

--- apps/email/test/unit/messsage_list_card_test.js
+++ apps/email/test/unit/messsage_list_card_test.js
@@ -8,6 +8,17 @@ requireApp('email/test/config.js');
 suite('message_list', function() {
   var subject, MessageList;
   var mockMessagesSlice = { items: [] };
+  var mockStarredMessage = {
+    isStarred: true
+  };
+  var mockReadMessage = {
+    isRead: true
+  };
+  var mockStarredReadMessage = {
+    isStarred: true,
+    isRead: true
+  };
+  var mockMessage = {};
 
   function testRefreshBtnAccessibility(syncing) {
     assert.equal(subject.refreshBtn.getAttribute('data-l10n-id'),
@@ -70,6 +81,64 @@ suite('message_list', function() {
       testRefreshBtnAccessibility(false);
       subject.toggleOutboxSyncingDisplay(false);
       testRefreshBtnAccessibility(false);
+    });
+  });
+
+  suite('selectedMessagesUpdated', function() {
+    test('no selected messages', function() {
+      subject.selectedMessages = [];
+      subject.selectedMessagesUpdated();
+      assert.equal(subject.starBtn.getAttribute('data-l10n-id'),
+        'message-star-button');
+      assert.equal(subject.readBtn.getAttribute('data-l10n-id'),
+        'message-mark-read-button');
+    });
+
+    test('one read message', function() {
+      subject.selectedMessages = [Object.create(mockReadMessage)];
+      subject.selectedMessagesUpdated();
+      assert.equal(subject.starBtn.getAttribute('data-l10n-id'),
+        'message-star-button');
+      assert.equal(subject.readBtn.getAttribute('data-l10n-id'),
+        'message-mark-unread-button');
+    });
+
+    test('one starred message', function() {
+      subject.selectedMessages = [Object.create(mockStarredMessage)];
+      subject.selectedMessagesUpdated();
+      assert.equal(subject.starBtn.getAttribute('data-l10n-id'),
+        'message-unstar-button');
+      assert.equal(subject.readBtn.getAttribute('data-l10n-id'),
+        'message-mark-read-button');
+    });
+
+    test('one read and starred message', function() {
+      subject.selectedMessages = [Object.create(mockStarredReadMessage)];
+      subject.selectedMessagesUpdated();
+      assert.equal(subject.starBtn.getAttribute('data-l10n-id'),
+        'message-unstar-button');
+      assert.equal(subject.readBtn.getAttribute('data-l10n-id'),
+        'message-mark-unread-button');
+    });
+
+    test('one read and one unread message', function() {
+      subject.selectedMessages = [Object.create(mockReadMessage),
+        Object.create(mockMessage)];
+      subject.selectedMessagesUpdated();
+      assert.equal(subject.starBtn.getAttribute('data-l10n-id'),
+        'message-star-button');
+      assert.equal(subject.readBtn.getAttribute('data-l10n-id'),
+        'message-mark-unread-button');
+    });
+
+    test('one starred and one unstarred message', function() {
+      subject.selectedMessages = [Object.create(mockStarredMessage),
+        Object.create(mockMessage)];
+      subject.selectedMessagesUpdated();
+      assert.equal(subject.starBtn.getAttribute('data-l10n-id'),
+        'message-star-button');
+      assert.equal(subject.readBtn.getAttribute('data-l10n-id'),
+        'message-mark-read-button');
     });
   });
 });
