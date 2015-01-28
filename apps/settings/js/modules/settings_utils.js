8768f2eeb94b638463585c02c3c7c6c720471402
Arthur Chen <crh0716@gmail.com>
2015-01-28 18:54:17 +0800

    Bug 1126708 - Use dialog service for the enter certificate name dialog

--- apps/settings/js/modules/settings_utils.js
+++ apps/settings/js/modules/settings_utils.js
@@ -9,58 +9,9 @@ define(function(require) {
 
   // TODO
   // We need to move utils.js to SettingsUtils step by step
-  var SettingsService = require('modules/settings_service');
   var LazyLoader = require('shared/lazy_loader');
 
   var SettingsUtils = {
-    /*
-     * This method can help you make target panel as a dialog and
-     * you can pass needed options into it.
-     *
-     * @memberOf SettingsUtils
-     * @param {String} dialogID - ID of target panel
-     * @param {Object} userOptions - passed options
-     */
-    openDialog: function su_openDialog(dialogID, userOptions) {
-      if ('#' + dialogID == Settings.currentPanel) {
-        return;
-      }
-
-      var origin = Settings.currentPanel.match(/#(.*)/)[1];
-      var options = userOptions || {};
-      var onSubmit = options.onSubmit;
-      var onReset = options.onReset;
-
-      // Don't bypass these two functions inside panel
-      delete options.onSubmit;
-      delete options.onReset;
-
-      // Load dialog contents and show it.
-      SettingsService.navigate(dialogID, options);
-
-      var dialog = document.getElementById(dialogID);
-      var submit = dialog.querySelector('[type=submit]');
-      if (submit) {
-        submit.onclick = function onsubmit() {
-          // hide dialog box and we would call a callback after navigation
-          SettingsService.navigate(origin, {}, function() {
-            onSubmit.call(dialog);
-          });
-        };
-      }
-
-      var reset = dialog.querySelector('[type=reset]');
-      if (reset) {
-        reset.onclick = function onreset() {
-          if (typeof onReset === 'function') {
-            onReset.call(dialog);
-          }
-          // hide dialog box
-          SettingsService.navigate(origin);
-        };
-      }
-    },
-
     /**
      * We will use LazyLoader to help us load settings' used templates
      * and then return the html to callback
