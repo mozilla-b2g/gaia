e57e9a84ec54b47a451c3d2ab4bd4e8a4c3c09ae
Marshall Culpepper <marshall@mozilla.com>
2015-01-28 12:35:55 -0600

    Bug 1037495: Part 3 - Update FTU Ping to use TelemetryRequest

--- apps/system/js/ftu_ping.js
+++ apps/system/js/ftu_ping.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-/* global MobileOperator, SIMSlotManager, uuid, dump */
+/* global MobileOperator, SIMSlotManager, uuid, dump, TelemetryRequest */
 
 /**
  * A simple ping that is kicked off on first time use
@@ -25,7 +25,8 @@
   const DEFAULT_PING_TIMEOUT = 60 * 1000;
 
   // Used by the telemetry server to help identify the payload format
-  const PING_PAYLOAD_VERSION = 3;
+  const TELEMETRY_VERSION = 1;
+  const TELEMETRY_REASON = 'ftu';
 
   // Settings to observe value changes for while the ping has not been sent
   const OBSERVE_SETTINGS = ['deviceinfo.os',
@@ -97,7 +98,6 @@
     initSettings: function fp_initSettings(callback) {
       this.reset();
 
-      this._pingData.ver = PING_PAYLOAD_VERSION;
       this._pingData.screenHeight = window.screen.height;
       this._pingData.screenWidth = window.screen.width;
       this._pingData.devicePixelRatio = window.devicePixelRatio;
@@ -303,53 +303,32 @@
       }
     },
 
-    generatePingURL: function fp_generatePingURL() {
-      var version = this._pingData['deviceinfo.platform_version'] || 'unknown';
-      var updateChannel = this._pingData['app.update.channel'] || 'unknown';
-      var buildId = this._pingData['deviceinfo.platform_build_id'] || 'unknown';
-
-      var uriParts = [
-        this._pingURL,
-        encodeURIComponent(this._pingData.pingID),
-        'ftu',                             // 'reason'
-        'FirefoxOS',                       // 'appName'
-        encodeURIComponent(version),       // 'appVersion'
-        encodeURIComponent(updateChannel), // 'appUpdateChannel'
-        encodeURIComponent(buildId)        // 'appBuildID'
-      ];
-
-      return uriParts.join('/');
-    },
-
     ping: function fp_ping() {
-      var url = this.generatePingURL();
       this._pingData.pingTime = Date.now();
 
-      if (DEBUG) {
-        this.debug(url);
-        this.debug(JSON.stringify(this._pingData));
-      }
-
-      var xhr = new XMLHttpRequest({ mozSystem: true, mozAnon: true });
-      xhr.timeout = this._pingTimeout;
+      var request = new TelemetryRequest({
+        reason: TELEMETRY_REASON,
+        deviceID: this._pingData.pingID,
+        ver: TELEMETRY_VERSION,
+        url: this._pingURL,
+        appUpdateChannel: this._pingData['app.update.channel'],
+        appVersion: this._pingData['deviceinfo.platform_version'],
+        appBuildID: this._pingData['deviceinfo.platform_build_id']
+      }, this._pingData);
 
       var self = this;
-      xhr.onload = function() {
-        self.pingSuccess(xhr.responseText);
-      };
-
-      xhr.ontimeout = function() {
-        self.pingError('Timed out');
-      };
-
-      xhr.onerror = function() {
-        self.pingError(xhr.statusText);
-      };
-
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('Content-type', 'application/json');
-      xhr.responseType = 'text';
-      xhr.send(JSON.stringify(this._pingData));
+      request.send({
+        timeout: this._pingTimeout,
+        onload: function() {
+          self.pingSuccess(this.responseText);
+        },
+        ontimeout: function() {
+          self.pingError('Timed out');
+        },
+        onerror: function() {
+          self.pingError(this.statusText);
+        }
+      });
     },
 
     pingSuccess: function fp_pingSuccess(result) {
