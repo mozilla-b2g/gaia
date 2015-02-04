3e7c671b6ed6c444e40e2adf2d8ade29485ac96a
Alive Kuo <alegnadise@gmail.com>
2015-02-04 12:09:38 +0800

    Bug 1120541 - Do not maximize titlebar if it is fatal error

--- apps/system/test/unit/app_window_test.js
+++ apps/system/test/unit/app_window_test.js
@@ -1652,8 +1652,10 @@ suite('system/AppWindow', function() {
 
       var chromeEventSpy = this.sinon.stub(AppChrome.prototype, 'handleEvent');
 
+      app1.inError = true;
       app1.element.dispatchEvent(new CustomEvent('_opened'));
 
+      sinon.assert.calledWith(chromeEventSpy, {type: 'mozbrowsererror'});
       sinon.assert.calledWith(chromeEventSpy, {type: 'mozbrowserloadstart'});
       sinon.assert.calledWith(chromeEventSpy, {type: '_loading'});
     });
