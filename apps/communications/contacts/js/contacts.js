cbc34eae82f514cabd3366a721bf6b08a74b4fb0
Gabriele Svelto <gsvelto@mozilla.com>
2015-02-04 15:24:53 +0100

    Bug 1102814 - Use the `notification' channel to play keypad touch tones r=thills

--- apps/communications/contacts/js/contacts.js
+++ apps/communications/contacts/js/contacts.js
@@ -263,12 +263,11 @@ var Contacts = (function() {
 
     window.addEventListener('listRendered', loadDeferredActions);
 
-    /* XXX: Don't specify a default volume control channel as we want to stick
-     * with the default one as a workaround for bug 1092346. Once that bug is
-     * fixed please add back the following line:
-     *
-     * navigator.mozAudioChannelManager.volumeControlChannel = 'notification';
-     */
+    /* Tell the audio channel manager that we want to adjust the "notification"
+     * channel when the user presses the volumeup/volumedown buttons. */
+    if (navigator.mozAudioChannelManager) {
+      navigator.mozAudioChannelManager.volumeControlChannel = 'notification';
+    }
   };
 
   var initContactsList = function initContactsList() {
