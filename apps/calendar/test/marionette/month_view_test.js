fef87cf70c388e5b5bdbfc5b57ef014aa0698613
gaye <gaye@mozilla.com>
2015-02-04 15:12:35 -0500

    Bug 1128483 - Fix month view test that checks for greyed-out days outside current month

--- apps/calendar/test/marionette/month_view_test.js
+++ apps/calendar/test/marionette/month_view_test.js
@@ -88,7 +88,17 @@ marionette('month view', function() {
     }
 
     // Either the first or last day chronologically must be
-    // outside of the current month.
+    // outside of the current month (except for in February!).
+
+    // Swipe to next month if it's February.
+    var monthDay = app.monthDay;
+    var date = monthDay.date;
+    date = date.toLowerCase();
+    if (date.indexOf('feb') !== -1) {
+      // Swipe to another month.
+      app.swipeLeft();
+    }
+
     var month = app.month;
     var daySquares = month.daySquares;
     assert.isTrue(daySquares.some(isOutsideMonthWithGrayText));
