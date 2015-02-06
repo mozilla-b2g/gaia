a0e71e61922bde009a3b6714cbe965021d3279f1
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2015-02-06 14:40:33 -0500

    Revert "Bug 1130011 - Removing superfluous touch event listener on the whole window r=kgrandon"
    This reverts commit 33e864db8d1576eeef537c26af89f7a53b85213a.

--- apps/system/js/bootstrap.js
+++ apps/system/js/bootstrap.js
@@ -242,3 +242,34 @@ window.addEventListener('wallpaperchange', function(evt) {
 
 window.browserSettings = new BrowserSettings();
 window.browserSettings.start();
+
+
+/* === XXX Bug 900512 === */
+// On some devices touching the hardware home button triggers
+// touch events at position 0,0. In order to make sure those does
+// not trigger unexpected behaviors those are captured here.
+function cancelHomeTouchstart(e) {
+  if (e.touches[0].pageX === 0 && e.touches[0].pageY === 0) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+}
+
+function cancelHomeTouchend(e) {
+  if (e.changedTouches[0].pageX === 0 && e.changedTouches[0].pageY === 0) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+}
+
+function cancelHomeClick(e) {
+  if (e.pageX === 0 && e.pageY === 0) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+}
+
+window.addEventListener('touchstart', cancelHomeTouchstart, true);
+window.addEventListener('touchend', cancelHomeTouchend, true);
+window.addEventListener('mousedown', cancelHomeClick, true);
+window.addEventListener('mouseup', cancelHomeClick, true);
