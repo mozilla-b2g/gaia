ff66c83d21ff16b658bd49edd4bad4ff9e40ed21
Dale Harvey <dale@arandomurl.com>
2015-02-07 19:10:29 +0000

    Bug 1126964 - Fix rocketbar open transition and minor layout issues

--- apps/system/js/rocketbar.js
+++ apps/system/js/rocketbar.js
@@ -363,6 +363,7 @@
         this.searchWindow.open();
       }
       this.results.classList.remove('hidden');
+      this.backdrop.classList.add('results-shown');
     },
 
     /**
@@ -376,6 +377,7 @@
       }
 
       this.results.classList.add('hidden');
+      this.backdrop.classList.remove('results-shown');
 
       // Send a message to the search app to clear results
       if (this._port) {
