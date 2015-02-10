b35ac174e13725b4a8179d6ae3ef7649b9acd057
AutomatedTester <dburns@mozilla.com>
2015-02-10 00:21:39 +0000

    Bug 1131356: Update tests to use marionette_driver if marionette module is not available

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/test_homescreen_column_layout.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/test_homescreen_column_layout.py
@@ -2,7 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette import Wait
+try:
+    from marionette import Wait
+except ImportError:
+    from marionette_driver import Wait
+
 from gaiatest import GaiaTestCase
 from gaiatest.apps.homescreen.app import Homescreen
 from gaiatest.apps.settings.app import Settings
