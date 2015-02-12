b950dfd7adc429e30b31b31db184bae6c761fc74
Guillaume Marty <edo999@gmail.com>
2015-02-12 17:03:43 +0900

    Bug 1128618 - [Windows Management] The header area will overlap the notification bar icons when picking a Gallery or Video app attachment for MMS or Email

--- apps/system/test/marionette/fullscreen_app_statusbar_visibility_test.js
+++ apps/system/test/marionette/fullscreen_app_statusbar_visibility_test.js
@@ -22,8 +22,6 @@ marionette('Fullscreen status bar visibility >', function() {
 
   setup(function() {
     system.waitForStartup();
-    assert(system.statusbarMaximizedWrapper.displayed(),
-      'The status bar maximized wrapper is visible');
     system.waitForLaunch(FULLSCREEN_APP);
   });
 
