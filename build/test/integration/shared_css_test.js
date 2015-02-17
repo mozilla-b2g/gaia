25ba1ba681ab1e6a28a9a9f2e9407c45a4d9671d
Kevin Grandon <kevingrandon@yahoo.com>
2015-02-17 15:00:22 +0100

    Bug 1133720 - Remove test test-shared-css app from dev_apps folder

--- build/test/integration/shared_css_test.js
+++ build/test/integration/shared_css_test.js
@@ -6,9 +6,11 @@ suite('shared CSS', function() {
   suiteSetup(helper.cleanupWorkspace);
   teardown(helper.cleanupWorkspace);
 
-  test('make APP=test-shared-css, checking shared css are imported',
+  test('make APP=test-shared-css-app, checking shared css are imported',
     function(done) {
-      helper.exec('make APP=test-shared-css', function(error, stdout, stderr) {
+      var command = 'make APP=test-shared-css-app ' +
+        'GAIA_APP_CONFIG=' + __dirname + '/../fixtures/shared_css.list';
+      helper.exec(command, function(error, stdout, stderr) {
         helper.checkError(error, stdout, stderr);
         done();
       });
