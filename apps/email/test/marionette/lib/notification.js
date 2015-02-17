91b802ac30c8a5ba192fe1a4bf7f3bc057a44c97
jrburke <jrburke@gmail.com>
2015-02-17 13:25:53 -0800

    Bug 1130547 - [email] marionettejs tests that use notification iconURL fail

--- apps/email/test/marionette/lib/notification.js
+++ apps/email/test/marionette/lib/notification.js
@@ -1,7 +1,8 @@
 /*jshint node: true, browser: true */
+'use strict';
 
 var assert = require('assert'),
-    imgSelector = '#desktop-notifications-container [data-notification-id] img';
+    containerSelector = '#desktop-notifications-container .other-notifications';
 
 function Notification(client) {
   this.client = client;
@@ -9,28 +10,88 @@ function Notification(client) {
 module.exports = Notification;
 
 Notification.prototype = {
+  getContainer: function() {
+    return this.client.findElement(containerSelector);
+  },
+
   assertNoNotification: function() {
     this.getContainer().findElement('div', function(error) {
       assert.ok(!!error);
     });
   },
 
-/*
-  tapFirstNotification: function() {
-    // This one does not throw an error, but it also does not end
-    // up triggering the action for the
-    // return this.client.findElement('#notification-toaster').tap();
-  },
-*/
+  /**
+   * Uses Notification.get to get the first notification to show up. This
+   * approach is used instead of something like tapping on the notification,
+   * since the notification UI is normally hidden and it is non-trivial to open,
+   * and the data API really reflects the result of the notification work done
+   * by the email app.
+   */
+  triggerFirstNotification: function() {
+    var client = this.client;
+    return client.executeAsyncScript(function() {
+      function getFirstNotification() {
+        window.Notification.get().then(function(notifications) {
+          if (!notifications) {
+            marionetteScriptFinished(false);
+          } else if (!notifications.length) {
+            // The check was done too fast. Wait for some time to pass
+            // and check again.
+            window.setTimeout(getFirstNotification, 300);
+          } else {
+            var notification = notifications[0],
+                evt = window.wrappedJSObject.require('evt');
 
-  getContainer: function() {
-    return this.client
-          .findElement('#desktop-notifications-container .other-notifications');
+            evt.emit('notification', {
+              clicked: true,
+              imageURL: notification.imageURL,
+              data: notification.data,
+              tag: notification.tag
+            });
+
+            marionetteScriptFinished(true);
+          }
+        }).catch(function(error) {
+          console.error('Notification.get failed with: ' + error);
+          marionetteScriptFinished(false);
+        });
+      }
+
+      getFirstNotification();
+    });
   },
 
-  getFirstIconUrl: function() {
-    var img = this.getContainer().findElement('img');
-    return img.getAttribute('src');
+  /**
+   * Uses Notification.get to get the first notification's data.
+   */
+  getFirstNotificationData: function() {
+    var client = this.client;
+    return client.executeAsyncScript(function() {
+      function getFirstNotification() {
+        window.Notification.get().then(function(notifications) {
+          if (!notifications) {
+            marionetteScriptFinished(false);
+          } else if (!notifications.length) {
+            // The check was done too fast. Wait for some time to pass
+            // and check again.
+            window.setTimeout(getFirstNotification, 300);
+          } else {
+            var notification = notifications[0];
+
+            marionetteScriptFinished({
+              imageURL: notification.imageURL,
+              data: notification.data,
+              tag: notification.tag
+            });
+          }
+        }).catch(function(error) {
+          console.error('Notification.get failed with: ' + error);
+          marionetteScriptFinished(false);
+        });
+      }
+
+      getFirstNotification();
+    });
   }
 };
 
