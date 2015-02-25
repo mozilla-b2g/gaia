9d2edeab456b5c4c16f43e976e4f8a6c3425c03e
Alive Kuo <alegnadise@gmail.com>
2015-02-25 17:31:24 +0800

    fix font size util try

--- apps/sharedtest/test/unit/font_size_utils_test.js
+++ apps/sharedtest/test/unit/font_size_utils_test.js
@@ -686,12 +686,13 @@ suite('shared/js/text_utils.js', function() {
       var el = setupHeaderElement();
       el.textContent = setupLargeString();
 
-      var stub = sinon.stub(FontSizeUtils, '_reformatHeaderText', function() {
-        document.body.removeChild(el.parentNode);
-        stub.restore();
-        assert.isTrue(stub.calledWith(el));
-        done();
-      });
+      var stub = this.sinon.stub(FontSizeUtils, '_reformatHeaderText',
+        function() {
+          document.body.removeChild(el.parentNode);
+          stub.restore();
+          assert.isTrue(stub.calledWith(el));
+          done();
+        });
 
       lazyLoad(el.parentNode);
     });
@@ -700,7 +701,7 @@ suite('shared/js/text_utils.js', function() {
       var el = setupNonHeaderElement();
       el.textContent = setupLargeString();
 
-      var spy = sinon.spy(FontSizeUtils, '_reformatHeaderText');
+      var spy = this.sinon.spy(FontSizeUtils, '_reformatHeaderText');
       assert.isTrue(spy.notCalled);
 
       el.addEventListener('overflow', function() {
