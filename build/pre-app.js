2565acfc0cb039c161a820361b65319c765c8618
Ricky Chien <ricky060709@gmail.com>
2015-03-03 11:03:17 +0800

    Bug 1138773 - Running search-provider.js on node.js

--- build/pre-app.js
+++ build/pre-app.js
@@ -12,20 +12,18 @@ function execute(options) {
 
   nodeHelper.require('contacts-import-services', options);
 
-  nodeHelper.require('./search-provider', options);
+  nodeHelper.require('search-provider', options);
 
-  nodeHelper.require('./keyboard-layouts', options);
+  nodeHelper.require('keyboard-layouts', options);
 
-  nodeHelper.require('./preferences', options);
+  nodeHelper.require('preferences', options);
 
   if (options.BUILD_APP_NAME == '*') {
     nodeHelper.require('settings', options);
   }
 
-  // Copy shared files to stage folders
   nodeHelper.require('webapp-shared', options);
 
-  // Copy common files such as webapps.json
   nodeHelper.require('copy-common-files', options);
 }
 
