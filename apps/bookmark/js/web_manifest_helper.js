d9764b352892c40fd4c37ddef658b5be66da6458
crdlc <crdlc@tid.es>
2015-03-03 10:20:28 +0100

    Bug 1138814 - Error handling installing an app from a web page

--- apps/bookmark/js/web_manifest_helper.js
+++ apps/bookmark/js/web_manifest_helper.js
@@ -7,6 +7,24 @@
 (function WebManifestHelper(exports) {
 
   /**
+   * It specifies whether the phone is in online or offline mode.
+   *
+   * @returns {Promise} A promise that will be fulfilled when online.
+   */
+  var onConnected = function() {
+    if (navigator.onLine) {
+      return Promise.resolve();
+    }
+
+    return new Promise(function(resolve, reject) {
+       window.addEventListener('online', function onConnect() {
+        window.removeEventListener('online', onConnect);
+        resolve();
+      });
+    });
+  };
+
+  /**
    * Get Manifest.
    *
    * @param {String} url Manifest URL
@@ -14,23 +32,25 @@
    */
   var getManifest = function(url) {
     return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest({mozSystem: true});
-      xhr.open('get', url, true);
-      xhr.responseType = 'json';
-      xhr.setRequestHeader('Accept', 'application/manifest+json');
-      xhr.onload = function() {
-        var status = xhr.status;
-        if (status == 200) {
-          resolve(xhr.response);
-        } else {
-          reject(status);
-        }
-      };
-      xhr.onerror = function(e) {
-        console.error('Unable to get web manifest');
-        reject(e.target.status);
-      };
-      xhr.send();
+      onConnected().then(() => {
+        var xhr = new XMLHttpRequest({mozSystem: true});
+        xhr.open('get', url, true);
+        xhr.responseType = 'json';
+        xhr.setRequestHeader('Accept', 'application/manifest+json');
+        xhr.onload = function() {
+          var status = xhr.status;
+          if (status == 200) {
+            resolve(xhr.response);
+          } else {
+            reject(status);
+          }
+        };
+        xhr.onerror = function(e) {
+          console.error('Unable to get web manifest');
+          reject(e.target.status);
+        };
+        xhr.send();
+      });
     });
   };
 
