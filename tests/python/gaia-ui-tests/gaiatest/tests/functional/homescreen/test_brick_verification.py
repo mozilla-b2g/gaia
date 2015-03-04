932ea4f3e3aecda8bf79bc95ae31519e55c51d37
John Dorlus <jdorlus@outlook.com>
2015-03-03 19:23:50 -0500

    Changed test name to Brick Verification

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/test_brick_verification.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/homescreen/test_brick_verification.py
@@ -2,24 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-try:
-    from marionette import Wait
-except:
-    from marionette_driver import Wait
-
 from gaiatest import GaiaTestCase
 from gaiatest.apps.homescreen.app import Homescreen
-from gaiatest.apps.homescreen.regions.confirm_install import ConfirmInstall
-from gaiatest.apps.system.app import System
 
-class TestVerifyHomeScreen(GaiaTestCase):
+
+class TestBrickVerification(GaiaTestCase):
 
     def setUp(self):
         GaiaTestCase.setUp(self)
         self.homescreen = Homescreen(self.marionette)
 
-
-
     def test_verify_homescreen(self):
         self.apps.switch_to_displayed_app()
-        self.assertTrue(self.homescreen.is_homescreen_visible, "You have yourself a brick.")
+        self.assertTrue(self.homescreen.is_homescreen_visible, "The phone seems to be bricked.")
