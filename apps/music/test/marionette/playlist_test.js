c6d1391b271daddbdc882268f55a2b95a9dbb47c
Hubert Figuière <hub@figuiere.net>
2015-03-09 17:57:10 -0400

    Bug 1122084 - Test highest rated playlist

--- apps/music/test/marionette/playlist_test.js
+++ apps/music/test/marionette/playlist_test.js
@@ -5,6 +5,7 @@ var assert = require('assert');
 var Music = require('./lib/music.js');
 var FakeRingtones = require('./lib/fakeringtones.js');
 var FakeControls = require('./lib/fakecontrols.js');
+var PlaylistHelper = require('./lib/playlisthelper.js');
 /*var Statusbar = require('./lib/statusbar.js');*/
 
 marionette('Music player playlist', function() {
@@ -69,16 +70,16 @@ marionette('Music player playlist', function() {
 
       assert.equal(songs.length, 3);
 
-      assert.equal(songs[0].findElement('.list-song-index').text(), '1');
-      assert.equal(songs[0].findElement('.list-song-title').text(),
+      assert.equal(PlaylistHelper.songIndex(songs[0]), '1');
+      assert.equal(PlaylistHelper.songTitle(songs[0]),
                    'Australian citizen is a US traitor');
 
-      assert.equal(songs[1].findElement('.list-song-index').text(), '4');
-      assert.equal(songs[1].findElement('.list-song-title').text(),
+      assert.equal(PlaylistHelper.songIndex(songs[1]), '4');
+      assert.equal(PlaylistHelper.songTitle(songs[1]),
                    'Ich bin ein Berliner');
 
-      assert.equal(songs[2].findElement('.list-song-index').text(), '8');
-      assert.equal(songs[2].findElement('.list-song-title').text(),
+      assert.equal(PlaylistHelper.songIndex(songs[2]), '8');
+      assert.equal(PlaylistHelper.songTitle(songs[2]),
                    'The Ecuadorian Embassy');
     });
 
@@ -99,9 +100,9 @@ marionette('Music player playlist', function() {
       var songs = music.songs;
 
       assert.equal(songs.length, 3);
-      assert.equal(songs[0].findElement('.list-song-index').text(), '1');
-      assert.equal(songs[1].findElement('.list-song-index').text(), '2');
-      assert.equal(songs[2].findElement('.list-song-index').text(), '3');
+      assert.equal(PlaylistHelper.songIndex(songs[0]), '1');
+      assert.equal(PlaylistHelper.songIndex(songs[1]), '2');
+      assert.equal(PlaylistHelper.songIndex(songs[2]), '3');
     });
 
   });
@@ -154,16 +155,95 @@ marionette('Music player playlist', function() {
 
       assert.equal(songs.length, 6);
 
-      assert.equal(songs[0].findElement('.list-song-index').text(), '1.01');
-      assert.equal(songs[0].findElement('.list-song-title').text(),
-                   'Yield to thread');
+      assert.equal(PlaylistHelper.songIndex(songs[0]), '1.01');
+      assert.equal(PlaylistHelper.songTitle(songs[0]), 'Yield to thread');
 
-      assert.equal(songs[2].findElement('.list-song-index').text(), '1.03');
-      assert.equal(songs[2].findElement('.list-song-title').text(),
-                   'Windows BSOD');
+      assert.equal(PlaylistHelper.songIndex(songs[2]), '1.03');
+      assert.equal(PlaylistHelper.songTitle(songs[2]), 'Windows BSOD');
 
-      assert.equal(songs[3].findElement('.list-song-index').text(), '2.01');
-      assert.equal(songs[3].findElement('.list-song-title').text(), 'Crash');
+      assert.equal(PlaylistHelper.songIndex(songs[3]), '2.01');
+      assert.equal(PlaylistHelper.songTitle(songs[3]), 'Crash');
     });
+  });
+
+  suite('Highest rated playlist', function() {
+    setup(function() {
+      client.fileManager.removeAllFiles();
+      client.fileManager.add([
+        {
+          // Track 1.01 "Yield to Thread"
+          type: 'music',
+          filePath: 'apps/music/test-data/playlists/y.ogg'
+        },
+        {
+          // Track 1.02 "XOXO"
+          type: 'music',
+          filePath: 'apps/music/test-data/playlists/x.ogg'
+        },
+        {
+          // Track 1.03 "Windows BSOD"
+          type: 'music',
+          filePath: 'apps/music/test-data/playlists/w.ogg'
+        },
+        {
+          // Track 2.01 "Crash"
+          type: 'music',
+          filePath: 'apps/music/test-data/playlists/c.ogg'
+        },
+        {
+          // Track 2.02 "Break"
+          type: 'music',
+          filePath: 'apps/music/test-data/playlists/b.ogg'
+        },
+        {
+          // Track 2.03 "Abort"
+          type: 'music',
+          filePath: 'apps/music/test-data/playlists/a.ogg'
+        },
+      ]);
+    });
+
+    test('Rating sort order', function() {
+      music.launch();
+      music.waitForFirstTile();
+
+      music.switchToAlbumsView();
+
+      music.selectAlbum('We crash computers');
+
+      music.waitForSongs(function(songs) {
+        return songs.length >= 6;
+      });
+
+      var songs = music.songs;
+      assert.equal(songs.length, 6);
+
+      client.executeScript(function() {
+        var w = window.wrappedJSObject;
+        var songData = w.SubListView.dataSource[3];
+        songData.metadata.rated = 4;
+        w.musicdb.updateMetadata(songData.name, songData.metadata);
+
+        songData = w.SubListView.dataSource[1];
+        songData.metadata.rated = 5;
+        w.musicdb.updateMetadata(songData.name, songData.metadata);
+      });
+
+      music.switchToPlaylistsView();
+
+      music.selectPlaylist('Highest rated');
+
+      music.waitForSongs(function(songs) {
+        return songs.length >= 6;
+      });
+      songs = music.songs;
+
+      assert.equal(PlaylistHelper.songIndex(songs[0]), '1');
+      assert.equal(PlaylistHelper.songTitle(songs[0]), 'XOXO');
+
+      assert.equal(PlaylistHelper.songIndex(songs[1]), '2');
+      assert.equal(PlaylistHelper.songTitle(songs[1]), 'Crash');
+    });
+
   });
 });
