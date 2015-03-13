53ad2ffab0570a536bfaeb3bf1bf1821c4a8c8bd
albertopq <apastor@mozilla.com>
2015-03-13 09:25:44 +0100

    Bug 1119103 - Fixing intermitent test with app_install r=kgrandon

--- apps/search/test/marionette/app_installed_test.js
+++ apps/search/test/marionette/app_installed_test.js
@@ -33,7 +33,9 @@ marionette('Search - Installed Apps Test', function() {
   test('app result after install/uninstall', function() {
     home.waitForLaunch();
     home.focusRocketBar();
-    search.removeGeolocationPermission();
+    client.executeScript(function() {
+      window.wrappedJSObject.permissionManager.discardPermissionRequest();
+    });
     search.triggerFirstRun(rocketbar);
     rocketbar.enterText('Calendar');
     search.goToResults();
