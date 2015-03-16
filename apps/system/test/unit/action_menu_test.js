d0e09d5e6367e558824f9cbf691da99cedf63037
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2015-03-16 13:14:22 -0400

    Revert "Bug 1141496 - Fix missing activities menu transition. r=alive"
    This reverts commit b154b8518511fab7ff9b6cc62cdb6d3fbd1a87ca.

--- apps/system/test/unit/action_menu_test.js
+++ apps/system/test/unit/action_menu_test.js
@@ -6,7 +6,7 @@ requireApp('system/js/action_menu.js');
 require('/shared/test/unit/mocks/mock_l10n.js');
 
 suite('ActionMenu', function() {
-  var activitiesMockup, realL10n, genericActionsMockup, rafStub;
+  var activitiesMockup, realL10n, genericActionsMockup;
   var iconUrl = 'images/icon.png', title = 'Title';
   var screenElement;
 
@@ -48,13 +48,9 @@ suite('ActionMenu', function() {
     navigator.mozL10n = MockL10n;
     loadBodyHTML('/index.html');
     screenElement = document.getElementById('screen');
-
-    rafStub = sinon.stub(window, 'requestAnimationFrame',
-                         function(callback) { callback(); });
   });
 
   suiteTeardown(function() {
-    rafStub.restore();
     navigator.mozL10n = realL10n;
     document.body.innerHTML = '';
   });
@@ -139,7 +135,6 @@ suite('ActionMenu', function() {
       // Hide. Calls stop
       var stub = this.sinon.stub(menu, 'stop');
       menu.hide();
-      getMenu().dispatchEvent(new CustomEvent('transitionend'));
       assert.ok(stub.calledOnce);
       stub.restore();
       menu.stop();
