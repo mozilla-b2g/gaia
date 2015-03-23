3f5a0c7da0ce3792fa18235740bf42773bd79899
hchang <hchang@mozilla.com>
2015-03-23 09:37:58 +0800

    Bug 1146208 - Add performance measurement mark prior to entering wifi panel.

--- apps/settings/js/panels/wifi/panel.js
+++ apps/settings/js/panels/wifi/panel.js
@@ -111,6 +111,9 @@ define(function(require) {
             this._openBadCredentialsDialog(currentNetwork);
           }
         }.bind(this));
+
+        window.performance.mark('wifiListStart');
+        PerformanceTestingHelper.dispatch('start-wifi-list-test');
       },
       onBeforeShow: function() {
         this._wifiSectionVisible = true;
