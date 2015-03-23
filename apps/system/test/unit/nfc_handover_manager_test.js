a37757c74c103c40d4374e0bcf8796390d6fad74
Arno Puder <arno@puder.org>
2015-03-23 21:53:58 +0900

    Bug 1146220 - Turn off BT at sender when denying concurrent file transfers

--- apps/system/test/unit/nfc_handover_manager_test.js
+++ apps/system/test/unit/nfc_handover_manager_test.js
@@ -226,11 +226,16 @@ suite('Nfc Handover Manager Functions', function() {
    */
   suite('File transfer', function() {
     var spySendNDEF;
-
+    var spyBluetoothEnabledObserver;
     var fileRequest;
 
     setup(function() {
       MockBluetooth.enabled = true;
+      spyBluetoothEnabledObserver = sinon.spy(function(event) {
+        MockBluetooth.enabled = event.settingValue;
+      });
+      MockNavigatorSettings.addObserver('bluetooth.enabled',
+                                        spyBluetoothEnabledObserver);
       MockBluetoothTransfer.fileTransferInProgress = false;
       spySendNDEF = this.sinon.spy(MockMozNfc.MockNFCPeer, 'sendNDEF');
 
@@ -249,6 +254,7 @@ suite('Nfc Handover Manager Functions', function() {
 
       spySendNDEF.restore();
       NfcHandoverManager.sendFileQueue = [];
+      MockNavigatorSettings.removeObserver('bluetooth.enabled');
     });
 
     test('"nfc-manager-send-file" results in handover request sent to peer',
@@ -280,7 +286,6 @@ suite('Nfc Handover Manager Functions', function() {
 
     test('Sending aborts when another file is transmitted concurrently',
       function() {
-
       MockBluetoothTransfer.fileTransferInProgress = true;
       var stubShowNotification = this.sinon.stub(NfcHandoverManager,
                                                  '_showTryAgainNotification');
@@ -369,6 +374,7 @@ suite('Nfc Handover Manager Functions', function() {
       function() {
 
       fileRequest.onerror = sinon.stub();
+      NfcHandoverManager.bluetoothAutoEnabled = true;
       NfcHandoverManager.sendFileQueue.push(fileRequest);
 
       var stubShowNotification = this.sinon.stub(NfcHandoverManager,
@@ -383,6 +389,9 @@ suite('Nfc Handover Manager Functions', function() {
       assert.isTrue(fileRequest.onerror.calledOnce);
       assert.isTrue(stubShowNotification.calledOnce,
                     'Notification not shown');
+      assert.equal(spyBluetoothEnabledObserver.callCount, 1);
+      assert.isFalse(spyBluetoothEnabledObserver.getCall(0)
+                                                .args[0].settingValue);
     });
   });
 
