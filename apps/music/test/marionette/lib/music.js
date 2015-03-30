bb08142b4f8fbc64ca81fe208caef9aed9f060f0
Kevin Grandon <kevingrandon@yahoo.com>
2015-03-30 13:43:04 -0700

    Bug 1140154 - [Music] Adjust list-playlist-title selector in disabled test r=hotfix a=testonly

--- apps/music/test/marionette/lib/music.js
+++ apps/music/test/marionette/lib/music.js
@@ -279,7 +279,7 @@ Music.prototype = {
     var list_items = this.listItems;
 
     list_items.filter(function (element) {
-      return element.findElement('span.list-playlist-title', 'css selector')
+      return element.findElement('.list-playlist-title', 'css selector')
         .text() === name;
     })[0].tap();
   },
