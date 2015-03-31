ca32692cd4c1bb98f3878268c3234d84716ee8a9
Michael Henretty <michael.henretty@gmail.com>
2015-03-31 09:27:16 -0700

    Revert "Merge pull request #29179 from gmarty/Bug-1145359-Signal-icons-not-updating-properly-on-status-bar-v2.2"
    This reverts commit b81477116fd8be278e7fb8f7517c70eea9a115f8, reversing
    changes made to 7656d1a101b4c820fddf3e34d5f3101c87739536.

--- apps/system/js/statusbar.js
+++ apps/system/js/statusbar.js
@@ -1110,7 +1110,6 @@ var StatusBar = {
 
         var previousHiddenState = icon.hidden;
         var previousActiveState = icon.dataset.inactive;
-        var previousSearchingState = icon.dataset.searching;
         var previousRoamingHiddenState = roaming.hidden;
 
         if (this.settingValues['ril.radio.disabled']) {
@@ -1168,7 +1167,6 @@ var StatusBar = {
 
         if (previousHiddenState !== icon.hidden ||
           previousActiveState !== icon.dataset.inactive ||
-          previousSearchingState !== icon.dataset.searching ||
           previousRoamingHiddenState !== roaming.hidden) {
           isDirty = true;
         }
