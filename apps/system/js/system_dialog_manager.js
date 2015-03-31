79896a7327dda6f583a32875c0863fd76f93a69f
albertopq <apastor@mozilla.com>
2015-03-31 14:56:38 +0200

    Bug 1149114 - Remove fullscreen class when opening homescreen r=kgrandon

--- apps/system/js/system_dialog_manager.js
+++ apps/system/js/system_dialog_manager.js
@@ -66,8 +66,7 @@
                 'simlockrequestfocus',
                 'home',
                 'holdhome',
-                'homescreeneopened',
-                'appopened']
+                'hierarchytopmostwindowchanged']
     }
   };
 
@@ -160,10 +159,11 @@
     switch (evt.type) {
       // We only care about appWindow's fullscreen state because
       // we are on top of the appWindow.
-      case 'appopened':
-      case 'homescreenopened':
-        this.elements.containerElement.classList.toggle('fullscreen',
-          evt.detail.isFullScreen());
+      case 'hierarchytopmostwindowchanged':
+        var appWindow = evt.detail.getTopMostWindow();
+        var isFullScreen = appWindow && appWindow.isFullScreen();
+        var container = this.elements.containerElement;
+        container.classList.toggle('fullscreen', isFullScreen);
         break;
       case 'system-dialog-requestfocus':
       case 'simlockrequestfocus':
