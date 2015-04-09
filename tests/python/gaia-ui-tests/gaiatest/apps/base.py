b6ef49c483198cd1cefec992bfeaf91c796aabae
albertopq <albert.pastor@gmail.com>
2015-04-09 11:45:36 +0200

    Bug 1148154 - [TESTS] Wait for list displayed before selecting item

--- tests/python/gaia-ui-tests/gaiatest/apps/base.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/base.py
@@ -74,15 +74,14 @@ class Base(object):
             self.marionette.set_search_timeout(self.marionette.timeout or 10000)
 
     def find_select_item(self, match_string):
+        _list_locator = By.XPATH, "//section[contains(@class,'value-selector-container')]"
         _list_item_locator = (
             By.XPATH, "//section[contains(@class,'value-selector-container')]/descendant::li[descendant::span[.='%s']]" %
             match_string)
         # have to go back to top level to get the B2G select box wrapper
         self.marionette.switch_to_frame()
-        # TODO we should find something suitable to wait for, but this goes too
-        # fast against desktop builds causing intermittent failures
-        time.sleep(0.2)
 
+        self.wait_for_element_displayed(*_list_locator)
         li = self.wait_for_element_present(*_list_item_locator)
         # We need to keep this because the Ok button may hang over the element and stop
         # Marionette from scrolling the element entirely into view
