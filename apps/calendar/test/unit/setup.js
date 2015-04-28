fddfd8a93cfc1a9193327b94fbe392b73aa157dd
Kevin Grandon <kevingrandon@yahoo.com>
2015-04-28 15:09:18 -0700

    Revert "Calendar v3.0 initial service setup r=millermedeiros"
    This reverts commit ba3e52f117ce32c3129cff2b283b02bbc0db8c8e.
    
    Reverting the original commit for not containing a bug number. See bug 1159251.

--- apps/calendar/test/unit/setup.js
+++ apps/calendar/test/unit/setup.js
@@ -150,6 +150,33 @@ window.testAgentRuntime.testLoader = function(path) {
   });
 };
 
+window.mochaPromise = function(mochaFn, description, callback) {
+  if (typeof description === 'function') {
+    callback = description;
+    description = null;
+  }
+
+  function execute(done) {
+    var promise;
+    try {
+      promise = callback.call();
+    } catch (error) {
+     return done(error);
+    }
+
+    return promise.then(() => {
+      done();
+    })
+    .catch(done);
+  }
+
+  if (description) {
+    mochaFn(description, execute);
+  } else {
+    mochaFn(execute);
+  }
+};
+
 window.testSupport = window.testSupport || {};
 window.testSupport.calendar = {
   _lastEnvId: 0,
