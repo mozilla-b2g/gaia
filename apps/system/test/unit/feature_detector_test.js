b98664a00273f85ed7e9a83b03241e08cec427fd
Rudy Lu <me@rudy.lu>
2015-04-29 17:19:55 +0800

    Bug 1159618 - Make getDeviceMemory an async call when it has not retrieve the total memory size.

--- apps/system/test/unit/feature_detector_test.js
+++ apps/system/test/unit/feature_detector_test.js
@@ -8,22 +8,27 @@ requireApp('system/js/feature_detector.js');
 
 suite('system/FeatureDetector', function() {
   var subject;
+  var getFeaturePromise;
 
   setup(function() {
-    var getFeaturePromise = new MockPromise();
+    getFeaturePromise = new MockPromise();
     navigator.getFeature = this.sinon.stub().returns(getFeaturePromise);
 
     subject = BaseModule.instantiate('FeatureDetector');
     subject.start();
-
-    getFeaturePromise.mFulfillToValue(768);
   });
 
   teardown(function() {
     subject.stop();
   });
 
+  test('The device memory would be init as undefined', function() {
+    assert.equal(subject.deviceMemory, undefined);
+  });
+
   test('Hardware memory is correctly retrieved', function() {
+    getFeaturePromise.mFulfillToValue(768);
+
     assert.equal(subject.deviceMemory, 768);
     assert.isTrue(navigator.getFeature.calledWith('hardware.memory'));
   });
