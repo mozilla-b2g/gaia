205628158a14782bc977a64e1e721da827dd4891
Sherman Chen <shamen.chens@gmail.com>
2015-04-29 13:34:21 +0800

    Fix notification screen

--- apps/system/js/notification_screen.js
+++ apps/system/js/notification_screen.js
@@ -1,4 +1,5 @@
-/* global Service */
+/* global LazyLoader, MediaPlaybackWidget, Service,
+          SettingsListener, SettingsURL, toneUpgrader */
 'use strict';
 
 
@@ -697,11 +698,12 @@ var NotificationScreen = {
 
   removeLockScreenNotification:
   function ns_removeLockScreenNotification(notificationId) {
+    var lockScreenNotificationNode;
     var notifSelector = '[data-notification-id="' + notificationId + '"]';
     this.lockScreenContainer = this.getLockScreenContainer();
     if (this.lockScreenContainer) {
-      var lockScreenNotificationNode =
-          this.lockScreenContainer.querySelector(notifSelector);
+      lockScreenNotificationNode =
+        this.lockScreenContainer.querySelector(notifSelector);
     }
 
     if (lockScreenNotificationNode) {
@@ -827,7 +829,7 @@ SettingsListener.observe(
 });
 
 SettingsListener.observe('audio.volume.notification', 7, function(value) {
-  NotificationScreen.silent = (value == 0);
+  NotificationScreen.silent = (value === 0);
 });
 
 SettingsListener.observe('vibration.enabled', true, function(value) {
