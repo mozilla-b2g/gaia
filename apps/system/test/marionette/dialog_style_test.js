d107ad479793974b7b599272581ba37ce9c43063
albertopq <albert.pastor@gmail.com>
2015-05-05 10:43:05 +0200

    Bug 1160505 - Fixing ui tests with long alerts

--- apps/system/test/marionette/dialog_style_test.js
+++ apps/system/test/marionette/dialog_style_test.js
@@ -27,7 +27,12 @@ marionette('Dialogs', function() {
   });
 
   test('displaying a long alert', function() {
-    dialog.alert('1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n1\n');
+    var msg = '';
+    for (var i = 0; i < 1000; i++) {
+      msg += 'i';
+    }
+
+    dialog.alert(msg);
     client.switchToFrame();
     var statusbarSize = system.statusbar.size().height;
     assert.ok(dialog.title.location().y === statusbarSize);
