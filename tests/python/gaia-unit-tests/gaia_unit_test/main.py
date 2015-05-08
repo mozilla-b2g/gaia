8f8cbe0a2335a6cab1a0c711ded2da8eaf343352
Sherman Chen <shamen.chens@gmail.com>
2015-05-08 16:21:43 +0800

    Bug 1162887 - Run unit test in tv_apps

--- tests/python/gaia-unit-tests/gaia_unit_test/main.py
+++ tests/python/gaia-unit-tests/gaia_unit_test/main.py
@@ -224,16 +224,17 @@ def cli():
                     sys.exit(1)
 
         # build a list of tests
-        appsdir = os.path.join(os.path.dirname(os.path.abspath(options.profile)), 'apps')
-        for root, dirs, files in os.walk(appsdir):
-            files.sort()
-            for f in files:
-                # only include tests in a 'unit' directory
-                roots = root.split(os.path.sep)
-                if 'unit' in roots:
-                    full_path = os.path.relpath(os.path.join(root, f), appsdir)
-                    if full_path.endswith('_test.js') and full_path not in disabled:
-                        tests.append(full_path)
+        for path in ('apps', 'tv_apps'):
+            appsdir = os.path.join(os.path.dirname(os.path.abspath(options.profile)), path)
+            for root, dirs, files in os.walk(appsdir):
+                files.sort()
+                for f in files:
+                    # only include tests in a 'unit' directory
+                    roots = root.split(os.path.sep)
+                    if 'unit' in roots:
+                        full_path = os.path.relpath(os.path.join(root, f), appsdir)
+                        if full_path.endswith('_test.js') and full_path not in disabled:
+                            tests.append(full_path)
 
     logger_defaults = { 'tbpl': sys.stdout }
     upload_dir = os.environ.get('MOZ_UPLOAD_DIR')
