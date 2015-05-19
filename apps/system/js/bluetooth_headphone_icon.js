b48894d887286da2168018468c1e17b271c54983
albertopq <albert.pastor@gmail.com>
2015-05-19 10:29:55 +0200

    Bug 1168061 - Moving statusbar icons HTML to each icon file

--- apps/system/js/bluetooth_headphone_icon.js
+++ apps/system/js/bluetooth_headphone_icon.js
@@ -10,5 +10,14 @@
   BluetoothHeadphoneIcon.prototype.shouldDisplay = function() {
     return this.manager.isA2DPProfileConnected;
   };
+
+  BluetoothHeadphoneIcon.prototype.view = function view() {
+    return `<div id="statusbar-bluetooth-headphone"
+              class="sb-icon sb-icon-bluetooth-headphone"
+              role="listitem" hidden
+              data-l10n-id="statusbarBluetoothHeadphone">
+            </div>`;
+  };
+
   exports.BluetoothHeadphoneIcon = BluetoothHeadphoneIcon;
 }(window));
