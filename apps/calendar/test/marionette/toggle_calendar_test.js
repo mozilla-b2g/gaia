a34f452a75802dd6455f836657d045caec4ac6e9
gaye <gaye@mozilla.com>
2015-05-27 14:25:44 -0400

    Bug 1095712 - Reenable disabled calendar marionette tests r=gaye

--- apps/calendar/test/marionette/toggle_calendar_test.js
+++ apps/calendar/test/marionette/toggle_calendar_test.js
@@ -102,7 +102,7 @@ marionette('toggle calendar', function() {
         return app.day.events.length === 0;
       });
       assert.ok(
-        app.day.allDay.displayed(),
+        app.day.allDaysHolder.displayed(),
         'all day should be displayed'
       );
     });
