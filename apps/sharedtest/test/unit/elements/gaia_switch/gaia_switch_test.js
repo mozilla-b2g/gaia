4a1da9bdda97a919887eeec7aaafe9690a735cd6
Kevin Grandon <kevingrandon@yahoo.com>
2015-05-29 09:29:24 -0400

    Bug 1169615 - Add change event bubbling when a gaia-switch is changed

--- apps/sharedtest/test/unit/elements/gaia_switch/gaia_switch_test.js
+++ apps/sharedtest/test/unit/elements/gaia_switch/gaia_switch_test.js
@@ -56,4 +56,24 @@ suite('GaiaSwitch', function() {
       stopImmediatePropagation: function() {}
     });
   });
+
+  test('Dispatches change event', function(done) {
+    this.container.innerHTML = `<div id="wrapper"><gaia-switch>
+    </gaia-switch></div>`;
+    var wrapper = this.container.firstElementChild;
+    var element = wrapper.firstElementChild;
+    var input = element.shadowRoot.querySelector('input');
+    assert.equal(input.checked, false);
+
+    wrapper.addEventListener('change', function(e) {
+      assert.equal(e.type, 'change');
+      assert.equal(e.target, element);
+      done();
+    });
+
+    element.handleClick({
+      preventDefault: function() {},
+      stopImmediatePropagation: function() {}
+    });
+  });
 });
