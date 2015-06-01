4bb616be860524f46cdbb965b8753f3d23e5c918
Kevin Grandon <kevingrandon@yahoo.com>
2015-06-01 08:03:23 -0700

    Revert "Bug 1039893 - Add back inadvertently-removed comment."
    This reverts commit df3ade468c535a84c9003afdb4a76c2941ea196a.

--- apps/system/js/screen_manager.js
+++ apps/system/js/screen_manager.js
@@ -421,8 +421,6 @@
         power.screenEnabled = true;
       }
       this.screenEnabled = true;
-
-      // Set the brightness before the screen is on.
       this.setScreenBrightness(this._savedBrightness, instant);
 
       // If we are in a call  or a conference call and there
