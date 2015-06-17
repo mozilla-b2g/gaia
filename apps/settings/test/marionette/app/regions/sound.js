357fbba94a6352e28faac78566e3faac69595c37
Kevin Grandon <kevingrandon@yahoo.com>
2015-06-16 17:07:44 -0700

    Bug 1175353 - [Settings] Convert sound switch to use gaia-switch

--- apps/settings/test/marionette/app/regions/sound.js
+++ apps/settings/test/marionette/app/regions/sound.js
@@ -16,7 +16,7 @@ function SoundPanel(client) {
 module.exports = SoundPanel;
 
 SoundPanel.Selectors = {
-  'vibrateCheckbox': '#sound input[name="vibration.enabled"]',
+  'vibrateCheckbox': '#sound gaia-switch[name="vibration.enabled"]',
   'vibrateLabel': '#sound label.pack-switch',
   'mediaSlider': '#sound .media input',
   'ringerSlider': '#sound .notification input',
