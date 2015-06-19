7252f964a2a1bd662c4831643803ff16ea7539e8
Russ Nicoletti <rnicoletti@mozilla.com>
2015-06-19 10:39:41 -0700

    Simplified advanced telemetry helper, added example in video app

--- shared/js/advanced_telemetry_helper.js
+++ shared/js/advanced_telemetry_helper.js
@@ -26,10 +26,10 @@
  * 
  * where:
  *
- * <context>      is a string indicating the context within the app to which the
- *                telemetry data corresponds
  * <metric name>  is the name of the telemetry metric
+ *                telemetry data corresponds
  * <data>         is the telemetry data being added.
+ * <context>      is a string indicating the context within the app to which the
  * 
  * When used in this form, <context> is optional; the other values are required.
  */  
@@ -39,32 +39,7 @@ var APP_TELEMETRY_LOG_PREFIX = 'telemetry';
 
 var AdvancedTelemetryHelper = (function() {
 
-  function AdvancedTelemetryHelper(app, metricName, context) {
-
-    switch (arguments.length) {
-      case 3: 
-        this.app = this.getApp(app);
-        this.context = context;
-        break;
-      case 2:
-      case 1:
-        this.app = this.getApp(app);
-        this.context = null;
-        break;
-      default:
-        this.app = null;
-        this.context = null;
-        break;
-    }
-  
-    if (this.app && metricName) {
-      this.message = APP_TELEMETRY_LOG_PREFIX + '|' + this.app;
-    
-      this.message += '|' + metricName;
-    }
-    else {
-      this.message = null;
-    }
+  function AdvancedTelemetryHelper() {
   }
   
   AdvancedTelemetryHelper.prototype.getApp = function(app) {
@@ -81,63 +56,23 @@ var AdvancedTelemetryHelper = (function() {
     }
   };
   
-  AdvancedTelemetryHelper.prototype.add = function(telemetry) {
- 
+  AdvancedTelemetryHelper.prototype.add = function(name, value, context) {
+
     var message;
  
-    // If the app/metric name/context were passed to the constructor,
-    // simply add the 'data'
-    if (this.message) {
-      message = this.message;
-      message += '|' + telemetry;
-      if (this.context) {
-        message += '|' + this.context;
+    if (name && value) {
+      message = APP_TELEMETRY_LOG_PREFIX;
+
+      message += '|' + name;
+      message += '|' + value;
+      if (context) {
+        message += '|' + context;
       }
       console.info(message);
     }
     else {
-
-      // If the app/metric name/context were not passed to the constructor,
-      // parse the input for the metric name, data, and context
-      var context;
-      var metricName;
-      var data;
-  
-      // First, ensure we were instantiated with a certified app
-      if (!this.app) {
-        return;
-      }
-
-      // Validate the components of the telemetry data
-      var params = telemetry.split('|');
-      if (params.length >= 2 && params.length <=3) {
-        message = APP_TELEMETRY_LOG_PREFIX + '|' + this.app;
- 
-        // If there are three components in the telemetry input, the first 
-        // is the metricName, the second is the data, the third is the 'context'
-        if (params.length === 3) {
-          metricName = params[0];
-          data = params[1];
-          context = params[2];
-        }
-        else {
-          // If there are two components in the telemetry input, the first 
-          // is the metricName, the second is the data
-          metricName = params[0];
-          data = params[1];
-        }
-  
-        message += '|' + metricName;
-        message += '|' + data;
-        if (context) {
-          message += '|' + context;
-        }
-        console.info(message);
-      }
-      else {
-        console.warn('A telemetry log entry requires three or four parameters');
-      }
-    } 
+      console.warn('A telemetry log entry requires two or three parameters');
+    }
   };
 
   return AdvancedTelemetryHelper;
