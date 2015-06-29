58feea840fd75fd57cddc360852721c0b8f13b53
Arthur Chen <crh0716@gmail.com>
2015-06-29 18:49:59 +0800

    Bug 1178245 - Use new syntax to define the homescreen module

--- apps/settings/js/panels/homescreen/homescreen.js
+++ apps/settings/js/panels/homescreen/homescreen.js
@@ -2,42 +2,48 @@ define(function(require) {
   'use strict';
 
   var VerticalPreferences = require('shared/homescreens/vertical_preferences');
+  var Module = require('modules/base/module');
   var Observable = require('modules/mvvm/observable');
 
-  var Homescreen = Observable({
-    _isUpdating: false,
-    _cachedColsValue: null,
-    cols: null,
-    setCols: function(value) {
-      if (!this._isUpdating) {
-        this._isUpdating = true;
-        VerticalPreferences.put('grid.cols', value).then(() => {
-          this.cols = value;
-          this._isUpdating = false;
-          if (this._cachedColsValue) {
-            var cachedValue = this._cachedColsValue;
-            this._cachedColsValue = null;
-            this.setCols(cachedValue);
-          }
-        });
-      } else {
-        this._cachedColsValue = value;
+  var Homescreen = Module.create(function Homescreen() {
+    this.super(Observable).call(this);
+
+    this._isUpdating = false;
+    this._cachedColsValue = null;
+    // we may update this value somewhere in other apps.
+    VerticalPreferences.addEventListener('updated', (e) => {
+      var prop = e.target;
+      if (prop.name === 'grid.cols') {
+        this._cols = prop.value;
       }
+    });
+    // set the default value
+    VerticalPreferences.get('grid.cols').then((number) => {
+      this._cols = number;
+    });
+  }).extend(Observable);
+
+  Observable.defineObservableProperty(Homescreen.prototype, 'cols', {
+    readonly: true,
+    value: null
+  });
+
+  Homescreen.prototype.setCols = function(value) {
+    if (!this._isUpdating) {
+      this._isUpdating = true;
+      VerticalPreferences.put('grid.cols', value).then(() => {
+        this._cols = value;
+        this._isUpdating = false;
+        if (this._cachedColsValue) {
+          var cachedValue = this._cachedColsValue;
+          this._cachedColsValue = null;
+          this.setCols(cachedValue);
+        }
+      });
+    } else {
+      this._cachedColsValue = value;
     }
-  });
+  };
 
-  // we may update this value somewhere in other apps.
-  VerticalPreferences.addEventListener('updated', function(e) {
-    var prop = e.target;
-    if (prop.name === 'grid.cols') {
-      Homescreen.cols = prop.value;
-    }
-  });
-
-  // set the default value
-  VerticalPreferences.get('grid.cols').then(function(number) {
-    Homescreen.cols = number;
-  });
-
-  return Homescreen;
+  return Homescreen();
 });
