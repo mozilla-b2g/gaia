62e6b9618e3a8b9f92896ae0a54d7f1a6ee715d9
Timothy Guan-tin Chien <timdream@gmail.com>
2015-07-03 08:33:00 +0800

    Bug 1179400 - Part I, Create a Gij ActivityCallerApp lib

---
+++ apps/system/test/marionette/lib/activitycallerapp.js
@@ -0,0 +1,93 @@
+'use strict';
+
+function ActivityCallerApp(client) {
+  this.client = client;
+}
+module.exports = ActivityCallerApp;
+
+ActivityCallerApp.ORIGIN = 'app://activitycaller.gaiamobile.org';
+
+ActivityCallerApp.Selector = Object.freeze({
+  launchactivity: '#launchactivity',
+  testchainactivity: '#testchainactivity',
+  testdefaultactivity: '#testdefaultactivity',
+  closeButton: '#close',
+  inputElement: '#input',
+  dateInputElement: '#dateinput'
+});
+
+ActivityCallerApp.prototype = {
+  get launchactivity() {
+    return this.client.findElement(ActivityCallerApp.Selector.launchactivity);
+  },
+
+  get testchainactivity() {
+    return this.client.findElement(
+      ActivityCallerApp.Selector.testchainactivity);
+  },
+
+  get testdefaultactivity() {
+    return this.client.findElement(
+      ActivityCallerApp.Selector.testdefaultactivity);
+  },
+
+  get closeButton() {
+    return this.client.findElement(ActivityCallerApp.Selector.closeButton);
+  },
+
+  get inputElement() {
+    return this.client.findElement(ActivityCallerApp.Selector.inputElement);
+  },
+
+  get dateInputElement() {
+    return this.client.findElement(ActivityCallerApp.Selector.dateInputElement);
+  },
+
+  switchTo: function() {
+    this.client.switchToFrame();
+    this.client.apps.switchToApp(ActivityCallerApp.ORIGIN);
+  },
+
+  launch: function() {
+    var client = this.client;
+    client.apps.launch(ActivityCallerApp.ORIGIN);
+    client.apps.switchToApp(ActivityCallerApp.ORIGIN);
+    client.helper.waitForElement('body');
+  },
+
+  startActivity: function() {
+    this.switchTo();
+    this.launchactivity.tap();
+    this.client.switchToFrame();
+  },
+
+  startChainActivity: function() {
+    this.switchTo();
+    this.testchainactivity.tap();
+    this.client.switchToFrame();
+  },
+
+  startDefaultActivity: function() {
+    this.switchTo();
+    this.testdefaultactivity.tap();
+    this.client.switchToFrame();
+  },
+
+  close: function() {
+    this.switchTo();
+    this.closeButton.tap();
+    this.client.switchToFrame();
+  },
+
+  focusTextInput: function() {
+    this.switchTo();
+    this.inputElement.tap();
+    this.client.switchToFrame();
+  },
+
+  focusDateInput: function() {
+    this.switchTo();
+    this.dateInputElement.tap();
+    this.client.switchToFrame();
+  }
+};
