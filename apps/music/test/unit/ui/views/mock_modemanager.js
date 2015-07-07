ce99e746e3f8ee4b8731456db6e6308b02d69cb7
Hubert Figuière <hub@figuiere.net>
2015-07-07 14:47:34 -0400

    Bug 1170647 - SearchView unit test.

--- apps/music/test/unit/ui/views/mock_modemanager.js
+++ apps/music/test/unit/ui/views/mock_modemanager.js
@@ -1,11 +1,12 @@
-/* exported MockModeManager, MODE_LIST, MODE_TILES, MODE_PLAYER, MODE_PICKER, 
-   SETTINGS_OPTION_KEY */
+/* exported MockModeManager, MODE_LIST, MODE_TILES, MODE_PLAYER, MODE_PICKER,
+   MODE_SEARCH_FROM_TILES, SETTINGS_OPTION_KEY */
 'use strict';
 
 var MODE_LIST = 0;
 var MODE_TILES = 1;
 var MODE_PLAYER = 2;
 var MODE_PICKER = 3;
+var MODE_SEARCH_FROM_TILES = 5;
 
 var SETTINGS_OPTION_KEY = 'settings_option_key';
 
