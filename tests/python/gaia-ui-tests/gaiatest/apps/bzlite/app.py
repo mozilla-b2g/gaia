66e06219f5f30469b4e00881f9e574d412b54058
mermi <manel.rhaiem92@gmail.com>
2015-07-08 05:05:12 +0200

    change what is noted

--- tests/python/gaia-ui-tests/gaiatest/apps/bzlite/app.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/bzlite/app.py
@@ -1,24 +1,27 @@
 from marionette import Marionette
 from gaiatest.apps.base import Base
+from marionette_driver import By, Wait, expected
 
 class BugzillaLite(Base):
 
-    name = 'Bzlite'
-    _given_username = ('css selector', "#login input[type='email']")
-    _given_password = ('css selector', "#login input[type='password']")
-    _button_login = ('css selector', "#login input[type='submit']")
+    name = 'Bugzilla Lite'
+    _given_username = (By.CSS_SELECTOR, "#login input[type='email']")
+    _given_password = (By.CSS_SELECTOR, "#login input[type='password']")
+    _button_login = (By.CSS_SELECTOR, "#login input[type='submit']")
+    _app_ready_locator = (By.XPATH, "//div[@class='icon']//span[contains(text(),'Bugzilla Lite')]")
     # home frame
     home_frame = marionette.find_element('css selector', 'div.homescreen iframe')
     marionette.switch_to_frame(home_frame)
     #launch
     def launch (self):
-        BugzLite_icon = marionette.find_element('xpath', "//div[@class='icon']//span[contains(text(),'Bugzilla Lite')]")
-        BugzLite_icon.tap()
+        Base.launch(self)
+        Wait(self.marionette).until(expected.element_displayed(
+            Wait(self.marionette).until(expected.element_present(
+                *self!;_app_ready_locator))))
+
     
     #Switch context
-    self.marionette.switch_to_frame()
-    bugzillaLite_frame = marionette.find_element('css selector', "iframe[data-url*='bzlite.com']")
-    self.marionette.switch_to_frame(bugzillaLite_frame)
+    self.app.switch_to_displayed_app()
     # log in
     def login (self,username,passwrd):
         marionette.find_element(*self._given_username).tap()
