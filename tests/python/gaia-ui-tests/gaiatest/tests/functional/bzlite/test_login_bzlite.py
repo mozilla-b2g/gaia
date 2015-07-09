c15c6ffa10983ec636e27d0e98d1887f5072f5bf
mermi <manel.rhaiem92@gmail.com>
2015-07-09 16:41:16 +0200

    made correction from the last notes

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/bzlite/test_login_bzlite.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/bzlite/test_login_bzlite.py
@@ -1,13 +1,11 @@
 from gaiatest import GaiaTestCase
-from gaiatest.apps.bzlite.app import BugzillaLite
+from gaiatest.apps.bzlite.app import Bugzilla_Lite
 
 
 class TestBugzillaLite(GaiaTestCase):
     def test_login_BugzillaLite(self):
-        self.BugzillaLite = BugzillaLite(self.marionette)
-        slef.BugzillaLite.launch()
-        # enter the username and the password
-        self.BugzillaLite.login_bzlite(self.testvars['bugzilla']['user'], self.testvars['bugzilla']['password'])
-        # check the log in 
+        Bugzilla_Lite = Bugzilla_Lite(marionette)
+        Bugzilla_Lite.launch()
+        self.BugzillaLite.login(self.testvars['bugzilla']['user'], self.testvars['bugzilla']['password'])
         self.assertTrue(self.BugzillaLite.is_logged_in)
 
