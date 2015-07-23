5efc4ccbfe6a6c0f4fd16d3946221c45043c531c
Tzu-Lin Huang <tzhuang@mozilla.com>
2015-07-23 11:03:10 +0800

    Bug 1186301 - [Stingray] Scanning and display BT devices in Device Deck

--- shared/test/unit/mocks/mock_navigator_moz_bluetooth_v2.js
+++ shared/test/unit/mocks/mock_navigator_moz_bluetooth_v2.js
@@ -47,7 +47,7 @@
     onscostatuschanged: null,
     onhfpstatuschanged: null,
     ona2dpstatuschanged: null,
-    onrequestmediaplaystatus: null,
+    onrequestmediaplaystatus: null
   };
 
   var mManagerEventListeners = [];
@@ -71,10 +71,10 @@
     });
   }
 
-  function mmb_triggerEventListeners(type) {
+  function mmb_triggerEventListeners(type, eventObj) {
     mManagerEventListeners.forEach(function(eventListener) {
       if (eventListener.type === type) {
-        eventListener.callback();
+        eventListener.callback(eventObj);
       }
     });
   }
