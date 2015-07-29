3e386065b7bd98e888451776bd02c21b201ad44a
gasolin <gasolin@gmail.com>
2015-07-29 14:23:15 +0800

    Bug 1088450 - improve Bluetooth transfer messages, r=tim

--- apps/system/test/unit/bluetooth_transfer_test.js
+++ apps/system/test/unit/bluetooth_transfer_test.js
@@ -239,7 +239,7 @@ suite('system/bluetooth_transfer', function() {
         var evt = {
           detail: sendingFilesSchedule
         };
-        var title = 'transfer-has-started-title';
+        var title = 'transferHasStartedTitle';
         BluetoothTransfer._onFilesSending(evt);
         assert.equal(2, BluetoothTransfer._sendingFilesQueue.length);
         assert.equal(MockNotificationHelper.mTitleL10n, title);
@@ -289,7 +289,7 @@ suite('system/bluetooth_transfer', function() {
           received: false,
           success: true
         };
-        var title = 'transferReport-title';
+        var title = 'transferReportTitle';
         var body = {
           id: 'transferReport-description',
           args: {'numSuccessful' : 1, 'numUnsuccessful' : 1}
@@ -326,7 +326,7 @@ suite('system/bluetooth_transfer', function() {
           );
           assert.equal(
             MockNotificationHelper.mOptions.bodyL10n,
-            'transfer-confirmation-description'
+            'transfer-confirmation-desc'
           );
           assert.equal(
             MockNotificationHelper.mOptions.icon,
@@ -729,11 +729,11 @@ suite('system/bluetooth_transfer', function() {
           'cancelFileTransfer', 'cancelFileTransfer'));
         assert.equal(
           MockService.request.getCall(0).args[3].title,
-          'continueFileTransfer'
+          'no'
         );
         assert.equal(
           MockService.request.getCall(0).args[4].title,
-          'cancel'
+          'yes'
         );
 
         MockService.request.getCall(0).args[3].callback();
