0657a8934e7040330b0c27a2af3ff1f9c6193c5b
Guillaume Marty <edo999@gmail.com>
2015-07-29 14:13:51 +0100

    Bug 1180665 - Support replaceable homescreen

--- apps/settings/js/panels/homescreen/panel.js
+++ apps/settings/js/panels/homescreen/panel.js
@@ -1,37 +1,50 @@
-define(function(require) {
+/**
+ * Manage wallpaper, default homescreen and replaceable homescreens.
+ */
+define(require => {
   'use strict';
 
   var SettingsPanel = require('modules/settings_panel');
-  var Homescreen = require('panels/homescreen/homescreen');
+  var Wallpaper = require('panels/homescreen/wallpaper');
+  var HomescreenName = require('panels/homescreen/homescreen_name');
+
+  var elements = {};
 
   return function ctor_homescreen_panel() {
+    var wallpaper = Wallpaper();
+    var homescreenName = HomescreenName();
+
     return SettingsPanel({
-      onInit: function(panel) {
-        this._elements = {};
-        this._elements.gridSelect =
-          panel.querySelector('[name="grid.layout.cols"]');
+      onInit: function hp_onInit(panel) {
+        elements = {
+          wallpaper: panel.querySelector('.wallpaper'),
+          wallpaperPreview: panel.querySelector('.wallpaper-preview'),
+          currentHomescreen: panel.querySelector('#current-homescreen')
+        };
 
-        this._elements.gridSelect.addEventListener('change', function() {
-          Homescreen.setCols(this.value);
-        });
-
-        this._updateCols(Homescreen.cols);
-
-        Homescreen.observe('cols', (newValue) => {
-          this._updateCols(newValue);
-        });
+        elements.wallpaper.addEventListener('click',
+          wallpaper.selectWallpaper.bind(wallpaper));
       },
-      _updateCols: function(number) {
-        if (!number) {
-          return;
-        }
 
-        var option =
-          this._elements.gridSelect.querySelector('[value="' + number + '"]');
+      onBeforeShow: function hp_onBeforeShow() {
+        this._setWallpaperPreviewSrc(wallpaper.wallpaperSrc);
+        this._setHomescreenName(homescreenName.name);
 
-        if (option) {
-          option.selected = true;
-        }
+        wallpaper.observe('wallpaperSrc', this._setWallpaperPreviewSrc);
+        homescreenName.observe('name', this._setHomescreenName);
+      },
+
+      onBeforeHide: function hp_onBeforeHide() {
+        wallpaper.unobserve('wallpaperSrc');
+        homescreenName.unobserve('name');
+      },
+
+      _setWallpaperPreviewSrc: function hp_setHomescreenName(src) {
+        elements.wallpaperPreview.src = src;
+      },
+
+      _setHomescreenName: function hp_setHomescreenName(name) {
+        elements.currentHomescreen.textContent = name;
       }
     });
   };
---
+++ apps/settings/js/panels/homescreen_list/homescreen_list.js
@@ -0,0 +1,184 @@
+/**
+ * Handle homescreens list panel functionality.
+ */
+define(require => {
+  'use strict';
+
+  var AppsCache = require('modules/apps_cache');
+  var SettingsListener = require('shared/settings_listener');
+  var SettingsService = require('modules/settings_service');
+  var SettingsCache = require('modules/settings_cache');
+  var ManifestHelper = require('shared/manifest_helper');
+
+  var HomescreensList = function ctor_homescreens_list() {
+    this._apps = [];
+    this.elements = {};
+    this.manifestURL = '';
+  };
+
+  HomescreensList.prototype = {
+    /**
+     * initialization.
+     *
+     * @param {Array.<Object>} elements An array of HTML elements.
+     */
+    init: function hl_init(elements) {
+      this.elements = elements;
+      this._renderHomescreens();
+
+      window.addEventListener('applicationinstall',
+        this._renderHomescreens.bind(this));
+      window.addEventListener('applicationuninstall',
+        this._renderHomescreens.bind(this));
+
+      this.elements.homescreensList.addEventListener('click',
+        this._handleListClick.bind(this));
+      this.elements.moreLink.addEventListener('click',
+        this._showMoreHomescreens.bind(this));
+
+      // Refresh the list whenever apps are installed/uninstalled.
+      AppsCache.addEventListener('oninstall',
+        this._renderHomescreens.bind(this));
+      AppsCache.addEventListener('onuninstall',
+        this._renderHomescreens.bind(this));
+
+      SettingsListener.observe('homescreen.manifestURL', '', manifestURL => {
+        this.manifestURL = manifestURL;
+        // There is a race condition, so we need to refresh the list.
+        this.listBuilder();
+      });
+    },
+
+    /**
+     * handle list click event
+     */
+    _handleListClick: function hl_handleListClick(evt) {
+      var target = evt.target;
+      var index = target.dataset.appIndex;
+
+      while (index === undefined) {
+        target = target.parentNode;
+        index = target.dataset.appIndex;
+      }
+
+      var app = this._apps[index];
+      const settings = window.navigator.mozSettings;
+
+      if (evt.target.nodeName === 'GAIA-RADIO') {
+        // Change the current homescreen.
+        settings.createLock().set({
+          'homescreen.manifestURL': app.manifestURL
+        });
+        return;
+      }
+
+      // Otherwise, open the homescreen detail panel.
+      var manifest = new ManifestHelper(app.manifest || app.updateManifest);
+
+      SettingsService.navigate('homescreen-details', {
+        name: manifest.name,
+        author: manifest.developer,
+        version: manifest.version,
+        description: manifest.description,
+        removable: app.removable,
+        app: app
+      });
+    },
+
+    /**
+     * Build list item.
+     *
+     * @param {Object} app   app object.
+     * @param {Number} index link index.
+     */
+    _listItemBuilder: function hl_listItemBuilder(app, index) {
+      var item = document.createElement('li');
+      item.className = 'pack-split';
+      item.dataset.appIndex = index;
+
+      var radioLabel = document.createElement('label');
+      radioLabel.className = 'pack-radio';
+      radioLabel.role = 'presentation';
+
+      var input = document.createElement('gaia-radio');
+      input.name = 'homescreen';
+      input.value = index;
+      input.checked = (app.manifestURL === this.manifestURL);
+      radioLabel.appendChild(input);
+
+      item.appendChild(radioLabel);
+
+      var nameLabel = document.createElement('label');
+      nameLabel.className = 'name';
+
+      var manifest =
+        new ManifestHelper(app.manifest || app.updateManifest);
+      var icon = document.createElement('img');
+      if (manifest.icons && Object.keys(manifest.icons).length) {
+        var key = Object.keys(manifest.icons)[0];
+        var iconURL = manifest.icons[key];
+        if (!(/^(http|https|data):/.test(iconURL))) {
+          iconURL = app.origin + '/' + iconURL;
+        }
+        icon.src = iconURL;
+      } else {
+        icon.src = '../style/images/default.png';
+      }
+      nameLabel.appendChild(icon);
+
+      var span = document.createElement('span');
+      span.textContent = manifest.name;
+      nameLabel.appendChild(span);
+
+      item.appendChild(nameLabel);
+
+      return item;
+    },
+
+    /**
+     * Build homescreens list
+     */
+    listBuilder: function hl_listBuilder() {
+      this.elements.homescreensList.innerHTML = '';
+      var listFragment = document.createDocumentFragment();
+      var item;
+      this._apps.forEach((app, index) => {
+        item = this._listItemBuilder(app, index);
+        listFragment.appendChild(item);
+      });
+      this.elements.homescreensList.appendChild(listFragment);
+    },
+
+    /**
+     * @public
+     */
+    _renderHomescreens: function hl_renderHomescreens() {
+      return AppsCache.apps().then(apps => {
+        this._apps = apps.filter(app => {
+          var manifest =
+            new ManifestHelper(app.manifest || app.updateManifest);
+          return manifest && manifest.role && manifest.role === 'homescreen';
+        });
+
+        this.listBuilder();
+      });
+    },
+
+    _showMoreHomescreens: function hl_showMoreHomescreens() {
+      SettingsCache.getSettings(result => {
+        var version = result['deviceinfo.os'];
+        /* jshint nonew: false */
+        new window.MozActivity({
+          name: 'marketplace-category',
+          data: {
+            slug: 'homescreens',
+            // Marketplace expects major.minor
+            fxos_version: version.split('.').slice(0, 2).join('.')
+          }
+        });
+      });
+    }
+  };
+
+  return () => new HomescreensList();
+});
