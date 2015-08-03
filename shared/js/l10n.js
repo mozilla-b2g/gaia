102f1299e9eafe3760e1deb44d556b5c4f36b5af
Staś Małolepszy <stas@mozilla.com>
2015-08-03 17:06:00 +0200

    Bug 1190038 - Sanitize all HTML elements. r=gandalf

--- shared/js/l10n.js
+++ shared/js/l10n.js
@@ -2020,12 +2020,10 @@
       }
 
       if (isElementAllowed(childElement)) {
-        for (k = 0, attr; (attr = childElement.attributes[k]); k++) {
-          if (!isAttrAllowed(attr, childElement)) {
-            childElement.removeAttribute(attr.name);
-          }
-        }
-        result.appendChild(childElement);
+        const sanitizedChild = childElement.ownerDocument.createElement(
+          childElement.nodeName);
+        overlayElement(sanitizedChild, childElement);
+        result.appendChild(sanitizedChild);
         continue;
       }
 
