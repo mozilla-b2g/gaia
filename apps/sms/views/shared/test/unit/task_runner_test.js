66e36a0e5ce9cfcf067142cff57f9799bbd2049f
Julien Wajsberg <felash@gmail.com>
2015-08-07 22:18:39 +0200

    better task runner

--- apps/sms/views/shared/test/unit/task_runner_test.js
+++ apps/sms/views/shared/test/unit/task_runner_test.js
@@ -3,10 +3,8 @@
 
 require('/views/shared/js/task_runner.js');
 
-suite('SMS App Unit-Test', function() {
-
+suite('Task Runner', function() {
   test('runs operations serially', function(done) {
-
     var taskQueue = new TaskRunner(),
         resolvers = [],
         promises = [],
@@ -37,5 +35,34 @@ suite('SMS App Unit-Test', function() {
         assert.deepEqual(results, [0, 1, 2]);
       }).then(done, done);
     });
+  });
+
+  test('forwards results, gobbles results for next task', function(done) {
+    var taskQueue = new TaskRunner();
+    var task1 = sinon.stub().returns(42);
+    var task2 = sinon.stub();
+    var stub = sinon.stub();
+
+    taskQueue.push(task1).then(stub);
+    taskQueue.push(task2);
+    taskQueue.flush().then(() => {
+      sinon.assert.callOrder(task1, task2);
+      sinon.assert.neverCalledWith(task2, 42);
+      sinon.assert.calledWithExactly(stub, 42);
+    }).then(done, done);
+  });
+
+  test('forwards errors, gobbles errors for next task', function(done) {
+    var taskQueue = new TaskRunner();
+    var task1 = sinon.stub().throws();
+    var task2 = sinon.stub();
+    var stub = sinon.stub();
+
+    taskQueue.push(task1).catch(stub);
+    taskQueue.push(task2);
+    taskQueue.flush().then(() => {
+      sinon.assert.callOrder(task1, task2);
+      sinon.assert.neverCalledWith(task2, sinon.match.instanceOf(Error));
+    }).then(done, done);
   });
 });
