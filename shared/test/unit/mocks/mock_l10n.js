9fbe9b073b9c38c1b318988214a81e745eef346b
Zibi Braniecki <gandalf@mozilla.com>
2015-08-12 09:04:51 -0700

    Bug 1192426 - Add L10nAssert to mock_l10n.js

--- shared/test/unit/mocks/mock_l10n.js
+++ shared/test/unit/mocks/mock_l10n.js
@@ -1,3 +1,5 @@
+/* globals assert */
+
 (function(exports) {
   'use strict';
 
@@ -84,6 +86,45 @@
     localeFormat: stringify,
     fromNow: stringify,
     relativeParts: stringify
+  };
+
+  /**
+   * Helper used to check that an element has been properly localized. This
+   * supports all the patterns described on MDN for localization best
+   * practices:
+   * - If 'expectedL10n' is a string it will check that the node's
+   *   'data-l10n-id' attribute is set and matches that id.
+   * - If 'expectedL10n' is an object then if it as an id and an args field it
+   *   will check if the node's 'data-l10n-id' and 'data-l10n-args' fields
+   *   match them.
+   * - If 'expectedL10n' is an object with a 'raw' field it will check that the
+   *   node doesn't have a 'data-l10n-id' attribute and its 'textContent' field
+   *   matches the'raw' field.
+   * - If 'expectedL10n' is an object with am 'html' field it will check that
+   *   the node doesn't have a 'data-l10n-id' attribute and its 'innerHTML'
+   *   field matches the'html' field.
+   */
+  exports.l10nAssert = function(node, expectedL10n) {
+    if (typeof expectedL10n === 'string') {
+      assert.isTrue(node.hasAttribute('data-l10n-id'));
+      assert.equal(node.getAttribute('data-l10n-id'), expectedL10n);
+    } else if (expectedL10n.raw) {
+      assert.isFalse(node.hasAttribute('data-l10n-id'));
+      assert.equal(node.textContent, expectedL10n.raw);
+    } else if (expectedL10n.html) {
+      assert.isFalse(node.hasAttribute('data-l10n-id'));
+      assert.equal(node.innerHTML, expectedL10n.html);
+    } else if (expectedL10n.id) {
+      assert.isTrue(node.hasAttribute('data-l10n-id'));
+      assert.equal(node.getAttribute('data-l10n-id'), expectedL10n.id); 
+      if (expectedL10n.args) {
+        assert.isTrue(node.hasAttribute('data-l10n-args'));
+        assert.deepEqual(
+          JSON.parse(node.getAttribute('data-l10n-id')),
+          expectedL10n.args
+        );
+      }
+    }
   };
 
   function stringify() {
