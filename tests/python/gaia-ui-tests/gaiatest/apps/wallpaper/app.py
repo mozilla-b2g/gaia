cc69fc25309d4b5468e7b0535cba8dbfd52a48fd
Martijn Wargers <mwargers@mozilla.com>
2015-08-23 23:49:47 +0200

    Bug 1195033 - Add more tap_element_from_system_app for places that need it

--- tests/python/gaia-ui-tests/gaiatest/apps/wallpaper/app.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/wallpaper/app.py
@@ -17,6 +17,7 @@ class Wallpaper(Base):
         Wait(self.marionette).until(
             lambda m: len(m.find_elements(*self._stock_wallpapers_locator)) >= (index + 1),
             message='%d wallpaper(s) not present after timeout' % (index + 1))
-        self.marionette.find_elements(*self._stock_wallpapers_locator)[index].tap()
+        self.tap_element_from_system_app(
+            self.marionette.find_elements(*self._stock_wallpapers_locator)[index])
         Wait(self.marionette).until(lambda m: self.apps.displayed_app.name != self.name)
         self.apps.switch_to_displayed_app()
