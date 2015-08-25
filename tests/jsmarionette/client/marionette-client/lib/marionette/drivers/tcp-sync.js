81905b9973324483fd22e04a2deca966c86b5846
Ghislain 'Aus' Lacroix <aus@mozilla.com>
2015-08-25 12:59:01 -0700

    Bug 1198172 - Reduce logging noise for ease of debugging. r=me

--- tests/jsmarionette/client/marionette-client/lib/marionette/drivers/tcp-sync.js
+++ tests/jsmarionette/client/marionette-client/lib/marionette/drivers/tcp-sync.js
@@ -56,12 +56,13 @@ TcpSync.prototype.waitForSocket = function(options, callback) {
   }
 
   options = options || {};
-  var interval = options.interval || 0;
+  var interval = options.interval || 100;
   var timeout = options.timeout || 30000;
   var socketTimeout = 1000;
 
   var sockit = new sockittome.Sockit();
   var start = Date.now();
+  var lastDebugMessage = '';
   var self = this;
 
   // Use sockittome's built in polling timeout during calls to connect
@@ -82,7 +83,14 @@ TcpSync.prototype.waitForSocket = function(options, callback) {
       }
     }
     catch(e) {
-      debug('exception when probing socket', e.message);
+      // This may seem ridiculous, but we have to keep these errors showing up
+      // but, they often repeat like CRAZY, so, quiet them down by only showing
+      // each exception we encounter once.
+      if (lastDebugMessage != e.message) {
+        lastDebugMessage = e.message;
+        debug('exception when probing socket', lastDebugMessage);
+      }
+
       // Above read _may_ fail so it is important to close the socket...
       sockit.close();
     }
