d26919468a21591d8acc5540e008bd27589af04d
Jim <jporter@mozilla.com>
2015-09-11 13:18:52 -0500

    Bug 1191760 - [music] App doesn't load all music; fix checkPlayability() so it doesn't hang

--- dev_apps/music-nga/js/metadata/core.js
+++ dev_apps/music-nga/js/metadata/core.js
@@ -134,7 +134,7 @@ var AudioMetadata = (function() {
         var url = URL.createObjectURL(blob);
         player.src = url;
 
-        player.onerror = function() {
+        player.onerror = player.onsuspend = function() {
           URL.revokeObjectURL(url);
           player.removeAttribute('src');
           player.load();
