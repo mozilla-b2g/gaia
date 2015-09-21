5dd95cfb9f1d6501ce0e34414596ef3dd9c2f583
BavarianTomcat <tomcat@mozilla.com>
2015-09-21 13:20:23 +0200

    Revert "Fix passcode override bug 1201530" for breaking GU Tests
    This reverts commit 672411d07b85dbeea1b0a496293ffe71f5481bf8.

--- apps/findmydevice/js/commands.js
+++ apps/findmydevice/js/commands.js
@@ -148,7 +148,7 @@ var Commands = {
         settings['lockscreen.lock-message'] = message;
       }
 
-      if (passcode) {
+      if (!this.deviceHasPasscode() && passcode) {
         settings['lockscreen.passcode-lock.code'] = passcode;
       }
 
