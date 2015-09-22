b3585851ca633350d5ab09088a0c79d97eec7fc5
mermi <manel.rhaiem92@gmail.com>
2015-09-22 15:04:02 +0200

    change to a method without parameterized

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/settings/test_settings_sim_manager_functionality_display_option.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/settings/test_settings_sim_manager_functionality_display_option.py
@@ -1,18 +1,15 @@
-from marionette.marionette_test import parameterized
-
 from gaiatest import GaiaTestCase
 from gaiatest.apps.settings.app import Settings
 
 
 class TestFunctionalitySimManager(GaiaTestCase):
 
-    @parameterized('1', 1, 'SIM 2')
-    def test_functionality_sim_manager(self, default_sim_value, default_sim_option):
-
+    def test_functionality_sim_manager(self):
+        
         settings = Settings(self.marionette)
         settings.launch()
         sim_manager_settings = settings.open_sim_manager()
 
-        sim_manager_settings.select_outgoing_calls(default_sim_value)
-        sim_manager_settings.select_data(default_sim_value)
-        sim_manager_settings.select_outgoing_messages(default_sim_option)
+        sim_manager_settings.select_outgoing_calls("-1")
+        sim_manager_settings.select_data("-1")
+        sim_manager_settings.select_outgoing_messages("1")
