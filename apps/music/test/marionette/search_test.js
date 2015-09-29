d618288b6b3b94bae14156b634c084b6758ea9b3
Hubert Figuière <hub@figuiere.net>
2015-09-29 19:09:52 -0400

    Bug 1208083 - Search tests.

---
+++ apps/music/test/marionette/search_test.js
@@ -0,0 +1,191 @@
+/* global require, marionette, setup, suite, test, __dirname */
+'use strict';
+
+var assert = require('assert');
+var Music = require('./lib/music.js');
+var FakeRingtones = require('./lib/fakeringtones.js');
+var FakeControls = require('./lib/fakecontrols.js');
+
+marionette('Music player search', function() {
+  var apps = {};
+  apps[FakeRingtones.DEFAULT_ORIGIN] = __dirname + '/fakeringtones';
+  apps[FakeControls.DEFAULT_ORIGIN] = __dirname + '/fakecontrols';
+
+  var client = marionette.client({
+    profile: {
+      prefs: {
+        'device.storage.enabled': true,
+        'device.storage.testing': true,
+        'device.storage.prompt.testing': true
+      },
+
+      apps: apps
+    }
+  });
+
+  var music;
+
+  setup(function() {
+    music = new Music(client);
+
+    client.fileManager.removeAllFiles();
+    client.fileManager.add([
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/01.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/02.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/03.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/a.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/b.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/c.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/w.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/x.ogg'
+      },
+      {
+        type: 'music',
+        filePath: 'apps/music/test-data/playlists/y.ogg'
+      }
+    ]);
+  });
+
+  function testSearchResults(expectedCount) {
+    client.switchToFrame(music.activeViewFrame);
+
+    var search = client.findElement('music-search');
+    assert.ok(search);
+
+    client.switchToShadowRoot(search);
+
+    var view = client.helper.waitForElement('#results');
+    assert.ok(view);
+
+    // wait for the results to be displayed.
+    // XXX this mostly assume we populate before showing the div.
+    client.waitFor(function() {
+      return view.displayed();
+    });
+
+    // XXX fix me when this is true
+    // since we display the count, just get it.
+    //var count = view.findElement('.search-result-count').text();
+
+    var results = view.findElement('#list');
+    assert.ok(results);
+
+    var resultsList = results.findElements('li');
+    assert.ok(resultsList);
+
+    // detect inconsistency.
+    assert.equal(resultsList.length, expectedCount);
+    // XXX when the count is back
+    // check what we expect.
+    //    assert.equal(count, expectedCount);
+
+    client.switchToShadowRoot();
+
+    var resultsData = client.executeScript(
+      'var parse = ' + music.parseListItemsData.toString() + '\n' +
+      'var search = document.querySelector(\'music-search\');\n' +
+      'var list = search.shadowRoot.getElementById(\'list\');\n' +
+      'var elements = list.querySelectorAll(\'a\');\n' +
+      'return parse(elements);\n'
+    );
+
+    music.switchToMe();
+    return resultsData;
+  }
+
+  suite('Search tests', function () {
+
+    setup(function() {
+      music.launch();
+      music.waitForFirstTile();
+
+      // Here we wait 1.5 seconds for the search input hides completely.
+      // it will re-show after we scroll the target view.
+      client.helper.wait(1500);
+      music.showSearchInput(Music.Selector.tilesView);
+    });
+
+    test('Check simple search results in artists.', function() {
+      try {
+        music.searchTiles('the');
+        // check for the results in "artists"
+        var resultsList = testSearchResults(3);
+
+        assert.equal(resultsList[1].title, 'The NSA');
+        assert.equal(resultsList[1].section, 'artists');
+        // XXX fixme when we have the search highlights
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=1209432
+        // assert.equal(resultsList[1].highlight, 'The');
+        assert.equal(resultsList[2].title, 'The Ecuadorian Embassy');
+        assert.equal(resultsList[2].section, 'songs');
+
+      } catch(e) {
+        assert.ok(false, e.stack);
+      }
+    });
+
+    test.skip('Check empty results', function() {
+      try {
+        music.searchTiles('qwerty');
+
+        var resultsList = testSearchResults(0);
+        assert.equal(resultsList.length, 0);
+      } catch(e) {
+        assert.ok(false, e.stack);
+      }
+    });
+  });
+
+  suite('Search context tests', function () {
+
+    setup(function() {
+      music.launch();
+      music.waitForFirstTile();
+    });
+
+    // Test contextual search
+    // XXX fixme: currently make the app *crash*
+    test.skip('Check the context for artists', function() {
+      try {
+        music.switchToArtistsView();
+
+        client.helper.wait(1500);
+        music.showSearchInput('#list');
+        music.searchArtists('the');
+
+        var resultsList = testSearchResults(2);
+
+        assert.equal(resultsList[1].title, 'The NSA');
+        // XXX fix when we have highlights
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=1209432
+        // assert.equal(resultsList[1].highlight, 'The');
+      } catch(e) {
+        assert.ok(false, e.stack);
+      }
+    });
+
+  });
+});
