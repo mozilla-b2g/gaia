0d95dacab260e857422ea28d39c1c4cf1a05f858
Sam Foster <sfoster@mozilla.com>
2015-09-29 14:33:17 -0700

    Bug 1202571 - [Browser] Can pin the same site multiple times

--- apps/system/test/marionette/pinning_the_web_test.js
+++ apps/system/test/marionette/pinning_the_web_test.js
@@ -5,7 +5,6 @@ var Rocketbar = require('./lib/rocketbar');
 var PinTheWeb = require('./lib/pinning_the_web');
 var assert = require('assert');
 
-
 marionette('Pinning the Web', function() {
 
   var client = marionette.client({
@@ -43,9 +42,73 @@ marionette('Pinning the Web', function() {
     actions = client.loader.getActions();
   });
 
+  test('Pin site', function() {
+    // Count the current number of site icons
+    system.tapHome();
+    client.switchToFrame(system.getHomescreenIframe());
+    var numIcons = 0;
+    client.waitFor(function() {
+      numIcons = home.numIcons;
+      return numIcons > 0;
+    });
+
+    var url = server.url('sample.html');
+    pinTheWeb.openAndPinSiteFromBrowser(url);
+
+    assert(pinTheWeb.chromeIsPinned());
+
+    // Check that browser chrome expands when tapped
+    actions.wait(1).tap(system.appUrlbar).perform();
+    client.waitFor(function() {
+      return !pinTheWeb.chromeIsPinned();
+    });
+
+    // Check that browser chrome focuses on second tap
+    actions.tap(system.appUrlbar).perform();
+    client.waitFor(function() {
+      return rocketbar.backdrop.displayed();
+    });
+    assert(true, 'browser chrome can be manually expanded');
+
+    // Check that icon was added to homescreen
+    system.tapHome();
+    client.switchToFrame(system.getHomescreenIframe());
+    client.waitFor(function() {
+      return home.numIcons == numIcons + 1;
+    });
+  });
+
+  test('Unpin site', function() {
+    var url, lastIconId;
+
+    function lastIconMatches(id) {
+      system.tapHome();
+      client.switchToFrame(system.getHomescreenIframe());
+      client.waitFor(function() {
+        var ids = home.getIconIdentifiers();
+        return id == ids[ids.length - 1];
+      });
+    }
+
+    system.tapHome();
+    client.switchToFrame(system.getHomescreenIframe());
+    client.waitFor(function() {
+      var ids = home.getIconIdentifiers();
+      lastIconId = ids[ids.length - 1];
+      return lastIconId;
+    });
+    client.switchToFrame();
+    url = server.url('sample.html');
+    pinTheWeb.openAndPinSiteFromBrowser(url);
+    lastIconMatches(url);
+
+    pinTheWeb.openAndUnpinSiteFromBrowser(url);
+    lastIconMatches(lastIconId);
+  });
+
   // Skip test since we are disabling pinning door hanger in 2.5
   // See https://bugzilla.mozilla.org/show_bug.cgi?id=1207710
-  test.skip('Pin site', function() {
+  test.skip('Pin site from doorhanger', function() {
     // Count the current number of site icons
     system.tapHome();
     client.switchToFrame(system.getHomescreenIframe());
@@ -78,6 +141,38 @@ marionette('Pinning the Web', function() {
     client.switchToFrame(system.getHomescreenIframe());
     client.waitFor(function() {
       return home.numIcons == numIcons + 1;
+    });
+  });
+
+  suite.skip('Unpin site from doorhanger', function() {
+    var url, lastIconId;
+
+    function lastIconMatches(id) {
+      system.tapHome();
+      client.switchToFrame(system.getHomescreenIframe());
+      client.waitFor(function() {
+        var ids = home.getIconIdentifiers();
+        return id == ids[ids.length - 1];
+      });
+    }
+
+    setup(function() {
+      system.tapHome();
+      client.switchToFrame(system.getHomescreenIframe());
+      client.waitFor(function() {
+        var ids = home.getIconIdentifiers();
+        lastIconId = ids[ids.length - 1];
+        return lastIconId;
+      });
+      client.switchToFrame();
+      url = server.url('sample.html');
+      pinTheWeb.openAndPinSite(url);
+      lastIconMatches(url);
+    });
+
+    test('Unpin site', function() {
+      pinTheWeb.openAndPinSite(url);
+      lastIconMatches(lastIconId);
     });
   });
 
