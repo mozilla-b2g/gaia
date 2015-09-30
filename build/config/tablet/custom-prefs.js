2c9a9c2b3f38c452a312af574cc92ec9da132234
Shih-Chiang Chien <schien@mozilla.com>
2015-09-30 14:55:45 +0800

    Bug 1199592 - customize device type on simulator. r=evelyn,fabrice.

--- build/config/tablet/custom-prefs.js
+++ build/config/tablet/custom-prefs.js
@@ -2,3 +2,4 @@
 user_pref('devtools.responsiveUI.customWidth', 1280);
 user_pref('devtools.responsiveUI.customHeight', 800);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
+user_pref('devtools.useragent.device_type', 'Tablet');
