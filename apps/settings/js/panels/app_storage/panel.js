93420a976e23e555b5fe149fde89c772ad929353
Scott Wu <scottcwwu@gmail.com>
2015-10-13 16:36:00 +0800

    Bug 1208893 - Add warning text in low storage mode to the storage panel, r=gasolin

--- apps/settings/js/panels/app_storage/panel.js
+++ apps/settings/js/panels/app_storage/panel.js
@@ -1,4 +1,4 @@
-/* global DeviceStorageHelper */
+/* global DeviceStorageHelper, MozActivity */
 /**
  * Used to show Storage/App Storage panel
  */
@@ -9,10 +9,14 @@ define(function(require) {
   var AppStorage = require('modules/app_storage');
 
   return function ctor_app_storage_panel() {
+    var _storageStatus = null;
     var _spaceBarElement = null;
     var _totalSpaceText = null;
     var _usedSpaceText = null;
+    var _freeSpaceLabel = null;
     var _freeSpaceText = null;
+    var _lowDiskSpace = null;
+    var _mediaStorage = null;
 
     var _updateUsePercentage = function() {
       _spaceBarElement.value = AppStorage.usedPercentage;
@@ -31,20 +35,42 @@ define(function(require) {
     var _updateFreeSize = function() {
       _refreshText(_freeSpaceText, AppStorage.freeSize);
     };
+    var _updateStorageStatus = function() {
+      var lowDiskSpace = AppStorage.lowDiskSpace;
+      _storageStatus.className =
+        lowDiskSpace ? 'apps-storage-status-low' : 'apps-storage-status';
+      _freeSpaceLabel.setAttribute('data-l10n-id',
+        lowDiskSpace ? 'apps-available-space' : 'apps-free-space');
+    };
 
     var _updateElements = function() {
       _updateUsePercentage();
       _updateTotalSize();
       _updateUsedSize();
       _updateFreeSize();
+      _updateStorageStatus();
+    };
+
+    var _openMediaStorage = function() {
+      return new MozActivity({
+        name: 'configure',
+        data: {
+          target: 'device',
+          section: 'mediaStorage'
+        }
+      });
     };
 
     return SettingsPanel({
       onInit: function(panel) {
+        _storageStatus = panel.querySelector('.apps-storage-status');
         _spaceBarElement = panel.querySelector('.apps-space-bar');
         _totalSpaceText = panel.querySelector('.apps-total-space');
         _usedSpaceText = panel.querySelector('.apps-used-space');
+        _freeSpaceLabel = panel.querySelector('.apps-free-space-label');
         _freeSpaceText = panel.querySelector('.apps-free-space');
+        _lowDiskSpace = panel.querySelector('.apps-low-disk-space');
+        _mediaStorage = panel.querySelector('.media-storage');
       },
 
       onBeforeShow: function() {
@@ -52,9 +78,11 @@ define(function(require) {
         AppStorage.observe('totalSize', _updateTotalSize);
         AppStorage.observe('usedSize', _updateUsedSize);
         AppStorage.observe('freeSize', _updateFreeSize);
+        AppStorage.observe('lowDiskSpace', _updateStorageStatus);
 
         _updateElements();
         AppStorage.updateInfo();
+        _mediaStorage.addEventListener('click', _openMediaStorage);
       },
 
       onHide: function() {
@@ -62,6 +90,7 @@ define(function(require) {
         AppStorage.unobserve('totalSize', _updateTotalSize);
         AppStorage.unobserve('usedSize', _updateUsedSize);
         AppStorage.unobserve('freeSize', _updateFreeSize);
+        AppStorage.unobserve('lowDiskSpace', _updateStorageStatus);
       }
     });
   };
