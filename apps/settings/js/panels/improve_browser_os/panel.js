8f04772c1e781eb2f743de249ddb9efb88f8eeca
Michael Henretty <michael.henretty@gmail.com>
2015-10-17 09:13:07 +0200

    Revert "Merge pull request #32175 from russnicoletti/bug-1208205"
    This reverts commit a5c6932ce72321aefba5b2c8793b00c637c54a32, reversing
    changes made to 7a67098e9150509468df612261ede16341035294.

--- apps/settings/js/panels/improve_browser_os/panel.js
+++ apps/settings/js/panels/improve_browser_os/panel.js
@@ -1,102 +1,37 @@
 define(function(require) {
   'use strict';
 
-  var SettingsPanel = require('modules/settings_panel');
   var SettingsCache = require('modules/settings_cache');
-
-  const METRICLEVELSETTING = 'metrics.selectedMetrics.level';
-  const DOGFOODSETTING = 'debug.performance_data.dogfooding';
-  const SHAREDATASETTING = 'debug.performance_data.shared';
-  const LEVELSMAP = {
-          'Basic': 'Basic',
-          'Enhanced': 'Enhanced',
-          'None': 'None'
-        };
+  var SettingsPanel = require('modules/settings_panel');
 
   return function ctor_improveBrowserOS() {
     return SettingsPanel({
       onInit: function(panel) {
         var appUsage = panel.querySelector('#appUsageByRegion');
-        var metricsBasic = panel.querySelector('#metrics-basic');
-        var metricsEnhanced = panel.querySelector('#metrics-enhanced');
-        var metricsNone = panel.querySelector('#metrics-none');
+        var sharePrefDataToggle =
+          panel.querySelector('#menuItem-sharePerformanceData gaia-checkbox');
 
-        appUsage.addEventListener('click', this);
-        metricsBasic.addEventListener('click', this);
-        metricsEnhanced.addEventListener('click', this);
-        metricsNone.addEventListener('click', this);
+        appUsage.addEventListener('click', this._openAppUsage.bind(this));
 
-        return new Promise((resolve) => {
+        return new Promise(function(resolve) {
           SettingsCache.getSettings((results) => {
-            var metricsLevel = results[METRICLEVELSETTING];
-
-            switch(metricsLevel) {
-              case 'Basic':
-                metricsBasic.checked = true;
-                break;
-              case 'Enhanced':
-                metricsEnhanced.checked = true;
-                break;
-              case 'None':
-                metricsNone.checked = true;
-                break;
-              default:
-                // When upgrading from a release that has only the
-                // 'share data' checkbox, use the 'sharing' status
-                //  to determine the default metric level.
-                if (results[SHAREDATASETTING]) {
-                  metricsBasic.checked = true;
-                  this._setMetricLevel('Basic');
-                } else {
-                  metricsNone.checked = true;
-                  this._setMetricLevel('None');
-                }
-            }
-
-            // Dogfooders should not be able to disable the metrics.
-            if (results[DOGFOODSETTING]) {
-              metricsBasic.setAttribute('disabled', 'true');
-              metricsEnhanced.setAttribute('disabled', 'true');
-              metricsNone.setAttribute('disabled', 'true');
-            } else {
-              metricsBasic.removeAttribute('disabled');
-              metricsEnhanced.removeAttribute('disabled');
-              metricsNone.removeAttribute('disabled');
-            }
+            var dogFooding = !!results['debug.performance_data.dogfooding'];
+            sharePrefDataToggle.disabled = dogFooding;
             resolve();
           });
         });
       },
 
-      handleEvent: function (event) {
-        switch (event.target.id) {
-          case 'appUsageByRegion':
-            var activity = new window.MozActivity({
-              name: 'marketplace-search',
-              data: {
-                type: 'firefox-os-app-stats'
-              }
-            });
-            activity.onerror = function() {
-              console.log('Error opening app stats page in Marketplace');
-            };
-            break;
-
-          case 'metrics-basic':
-          case 'metrics-enhanced':
-          case 'metrics-none':
-            this._setMetricLevel(event.target.value);
-            break;
-        }
-      },
-
-      _setMetricLevel: function(level) {
-        level = LEVELSMAP[level];
-        if (level) {
-          var metricLevel = {};
-          metricLevel[METRICLEVELSETTING] = level;
-          navigator.mozSettings.createLock().set(metricLevel);
-        }
+      _openAppUsage: function() {
+        var activity = new window.MozActivity({
+          name: 'marketplace-search',
+          data: {
+            type: 'firefox-os-app-stats'
+          }
+        });
+        activity.onerror = function() {
+          console.log('Error opening app stats page in Marketplace');
+        };
       }
     });
   };
