2f8a1a6d43b09d4de6aea72567bee1f3ab6d9076
No-Jun Park <npark@mozilla.com>
2015-10-22 00:33:49 -0400

    Bug 1215705 - Imagecompare RTL: increase Improve firefox OS menu coverage

--- tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/improve.py
+++ tests/python/gaia-ui-tests/gaiatest/apps/settings/regions/improve.py
@@ -10,7 +10,10 @@ from gaiatest.apps.base import Base
 class Improve(Base):
 
     _page_locator = (By.ID, 'improveBrowserOS')
+    _feedback_page_locator = (By.ID, 'improveBrowserOS-chooseFeedback')
     _crash_report_info_locator = (By.CSS_SELECTOR, '[data-l10n-id="crashReportInfo"]')
+    _send_feedback_locator = (By.CSS_SELECTOR, '[data-l10n-id="sendMozillaFeedback"]')
+    _feedback_choice_locator = (By.ID, 'feedback-mood')
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
@@ -20,3 +23,14 @@ class Improve(Base):
     @property
     def screen_element(self):
         return self.marionette.find_element(*self._page_locator)
+
+    @property
+    def feedback_screen_element(self):
+        return self.marionette.find_element(*self._feedback_page_locator)
+
+    def tap_send_feedback(self):
+        element = Wait(self.marionette).until(
+            expected.element_present(*self._send_feedback_locator))
+        Wait(self.marionette).until(expected.element_displayed(element))
+        element.tap()
+        Wait(self.marionette).until(expected.element_displayed(*self._feedback_choice_locator))
