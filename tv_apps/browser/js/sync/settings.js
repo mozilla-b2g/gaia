5cd902b37d3769b2c4df26321cecaf06308654c1
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2015-10-22 20:45:20 +0800

    Bug 1217376 - [TV] Ask user for confirmation before signing the user out. r=yifan

--- tv_apps/browser/js/sync/settings.js
+++ tv_apps/browser/js/sync/settings.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* global BrowserDialog */
 /* global LazyLoader */
 /* global Settings */
 /* global SettingsListener */
@@ -270,7 +271,9 @@
     },
 
     disable() {
-      SyncManagerBridge.disable();
+      BrowserDialog.createDialog('signout_confirm').then(() => {
+        SyncManagerBridge.disable();
+      });
     },
 
     sync() {
