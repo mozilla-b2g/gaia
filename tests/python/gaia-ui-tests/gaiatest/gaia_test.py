b7d76ead05fe99295ac3d08935bc3acc94ae36e6
mermi <manel.rhaiem92@gmail.com>
2015-10-24 18:40:31 +0200

    Bug 1212843 - Provide an install method in gaia_test.py and convert uninstall the use manifestURL

--- tests/python/gaia-ui-tests/gaiatest/gaia_test.py
+++ tests/python/gaia-ui-tests/gaiatest/gaia_test.py
@@ -147,13 +147,23 @@ class GaiaApps(object):
         self.marionette.switch_to_frame()
         self.marionette.execute_async_script("GaiaApps.killAll();")
 
-    def uninstall(self, name):
+    def install(self,manifest_url):
+        self._change_state_of_app(manifest_url, 'install')
+
+    def uninstall(self,manifest_url):
+        self._change_state_of_app(manifest_url, 'uninstall')
+
+    def install_package(self, manifest_url):
+        self._change_state_of_app(manifest_url, 'installPackage')
+
+    def _change_state_of_app(self, manifest_url, desired_action):
         self.marionette.switch_to_frame()
         data_layer = GaiaData(self.marionette)
-        data_layer.set_bool_pref('dom.mozApps.auto_confirm_uninstall', True)
-        result = self.marionette.execute_async_script('GaiaApps.uninstallWithName("%s")' % name)
-        assert (result is True), 'Failed to uninstall app: %s' % result
-        data_layer.set_bool_pref('dom.mozApps.auto_confirm_uninstall', False)
+        preference_action = desired_action.rstrip('Package')
+        data_layer.set_bool_pref('dom.mozApps.auto_confirm_{}'.format(preference_action), True)
+        result = self.marionette.execute_async_script('GaiaApps.{}("{}");'.format(desired_action, manifest_url))
+        assert (result is True), 'Failed to {} app: {}'.format(desired_action, manifest_url)
+        data_layer.set_bool_pref('dom.mozApps.auto_confirm_{}'.format(preference_action), False)
 
     @property
     def installed_apps(self):
