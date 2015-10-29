f3a5729a6f5ef8a69030defb7a8ab90ea6d2b7a1
Sean Lee <selee@mozilla.com>
2015-10-29 17:36:47 +0800

    Bug 1217710.

--- tv_apps/browser/js/bookmarkStore.js
+++ tv_apps/browser/js/bookmarkStore.js
@@ -9,6 +9,11 @@
    * @type {String}
    */
   const SYNC_BOOKMARK_ROOT_FOLDER_ID = 'places';
+  /**
+   * The mobile folder id of synced bookmark data.
+   * @type {String}
+   */
+  const SYNC_BOOKMARK_MOBILE_FOLDER_ID = 'mobile';
 //ENDIF_FIREFOX_SYNC
 
   // TODO: for the live time bookmark data loading task.
@@ -18,6 +23,38 @@
     cache: [],
 
     currentFolder : null,
+
+//IFDEF_FIREFOX_SYNC
+    syncBookmarkRootFolderCache: {
+      id: SYNC_BOOKMARK_ROOT_FOLDER_ID,
+      title: '',
+      type: 'folder',
+      readOnly: true
+    },
+
+    syncBookmarkMobileFolderCache: {
+      id: SYNC_BOOKMARK_MOBILE_FOLDER_ID,
+      title: '',
+      type: 'folder',
+      readOnly: true
+    },
+//ENDIF_FIREFOX_SYNC
+
+    init: function () {
+//IFDEF_FIREFOX_SYNC
+      var l10nPromises = [];
+      l10nPromises
+        .push(navigator.mozL10n.formatValue('fxsync-desktop-bookmarks'));
+      l10nPromises
+        .push(navigator.mozL10n.formatValue('fxsync-mobile-bookmarks'));
+      Promise.all(l10nPromises).then(
+        ([l10nFxsyncDesktopBookmarks, l10nFxsyncMobileBookmarks]) => {
+          this.syncBookmarkRootFolderCache.title = l10nFxsyncDesktopBookmarks;
+          this.syncBookmarkMobileFolderCache.title = l10nFxsyncMobileBookmarks;
+        }
+      );
+//ENDIF_FIREFOX_SYNC
+    },
 
     reset: function(cb) {
       this.cache = [];
@@ -92,6 +129,11 @@
             resolve();
           });
         } else {
+//IFNDEF_FIREFOX_SYNC
+        resolve();
+//ENDIF_FIREFOX_SYNC
+
+//IFDEF_FIREFOX_SYNC
           if(!this.currentFolder) {
             // XXX: Ideally we should use Promise.all for
             // SyncBrowserDB.getBookmark and BrowserDB.getBookmarks.
@@ -99,33 +141,50 @@
             // return Promise.
             SyncBrowserDB.getBookmark({
               parentid: SYNC_BOOKMARK_ROOT_FOLDER_ID
-            }, syncBookmark => {
+            }, syncPlacesBookmark => {
               // We query remote bookmarks by parentid with
               // SYNC_BOOKMARK_ROOT_FOLDER_ID for checking if the indexedDB has
               // remote bookmark data. If there is any remote data, we create a
               // folder item to show the synced bookmark.
-              if(syncBookmark.length > 0) {
-                this.cache.push({
-                  id: SYNC_BOOKMARK_ROOT_FOLDER_ID,
-                  // XXX we need UX's suggestion to have a folder name for root
-                  // folder and don't forget to handle l10n case.
-                  title: 'Synced Bookmarks',
-                  type: 'folder',
-                  readOnly: true
+              if(syncPlacesBookmark.length > 0) {
+                this.cache.push(this.syncBookmarkRootFolderCache);
+
+                // If there is mobile folder and also has children in it,
+                // create a folder item to show the synced mobile bookmark
+                SyncBrowserDB.getBookmark({
+                  parentid: SYNC_BOOKMARK_MOBILE_FOLDER_ID
+                }, syncMobileBookmark => {
+                  if(syncMobileBookmark.length > 0) {
+                    this.cache.push(this.syncBookmarkMobileFolderCache);
+                  }
+                  // get bookmark data from origin indexdDB
+                  BrowserDB.getBookmarks(bookmarks => {
+                    this.cache = this.cache.concat(bookmarks);
+                    resolve();
+                  });
+                });
+              } else {
+                // get bookmark data from origin indexdDB
+                BrowserDB.getBookmarks(bookmarks => {
+                  this.cache = this.cache.concat(bookmarks);
+                  resolve();
                 });
               }
-
-              // get bookmark data from origin indexdDB
-              BrowserDB.getBookmarks(bookmarks => {
-                this.cache = this.cache.concat(bookmarks);
-                resolve();
-              });
             });
           } else {
             SyncBrowserDB.getBookmark({
               parentid: this.currentFolder
-            }, bookmarks => {
-              bookmarks.forEach((bookmark, i) => {
+            }, syncBookmark => {
+              var length = syncBookmark.length,
+                  bookmark = null;
+              for(var i=0; i<length; i++) {
+                bookmark = syncBookmark[i];
+                //hide mobile folder in sync bookmark folder
+                if(this.currentFolder === SYNC_BOOKMARK_ROOT_FOLDER_ID &&
+                  bookmark.id === SYNC_BOOKMARK_MOBILE_FOLDER_ID) {
+                  continue;
+                }
+
                 // XXX: Now we only support two types: folder and bookmark.
                 // And the data from firefox sync can't be modified.
                 if(bookmark.type === 'folder' && bookmark.title) {
@@ -136,10 +195,11 @@
                   bookmark.readOnly = true;
                   this.cache.push(bookmark);
                 }
-              });
+              }
               resolve();
             });
           }
+//ENDIF_FIREFOX_SYNC
         }
       });
     }
