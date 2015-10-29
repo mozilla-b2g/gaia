94ad60669ede8f76a39ee65f9453825af1b5e555
Jim <jporter@mozilla.com>
2015-10-28 21:08:03 -0500

    Bug 1063467 - [Ringtone]Multiple ringtone list appneded

--- apps/ringtones/js/pick.js
+++ apps/ringtones/js/pick.js
@@ -1,49 +1,18 @@
 /* global NullRingtone, Promise, ToneList, TonePlayer */
 'use strict';
 
-navigator.mozSetMessageHandler('activity', function(activity) {
+(function() {
   var tonePlayer = new TonePlayer();
+  var header = document.getElementById('header');
+  var setButton = document.getElementById('set');
+  var allTones = {};
 
-  var toneTypes = activity.source.data.type;
-  var allowNone = activity.source.data.allowNone;
-  var currentToneID = activity.source.data.currentToneID;
-
-  var allToneTypes = window.builtInRingtones.toneTypes;
-  if (!Array.isArray(toneTypes)) {
-    toneTypes = [toneTypes];
-  }
-  toneTypes = toneTypes.filter(function(x) {
-    return allToneTypes.indexOf(x) !== -1;
-  });
-
-  // Put any unasked-for types in another array so we can list them at the end.
-  var otherToneTypes = allToneTypes.filter(function(x) {
-    return toneTypes.indexOf(x) === -1;
-  });
-
-  document.getElementById('header').addEventListener('action', function() {
+  header.addEventListener('action', function() {
     tonePlayer.stop();
-    activity.postError('cancelled');
   });
 
-  document.getElementById('set').addEventListener('click', function() {
+  setButton.addEventListener('click', function() {
     tonePlayer.stop();
-    tonePlayer.isValid(function(valid) {
-      if (!valid) {
-        // The tone couldn't be played. Just act like the user canceled.
-        activity.postError('cancelled');
-      }
-
-      var selectedTone = tonePlayer.currentTone;
-      selectedTone.getBlob().then(function(blob) {
-        activity.postResult({
-          name: selectedTone.name,
-          l10nID: selectedTone.l10nID,
-          id: selectedTone.id,
-          blob: blob
-        });
-      });
-    });
   });
 
   function PickerToneList(...args) {
@@ -53,64 +22,107 @@ navigator.mozSetMessageHandler('activity', function(activity) {
   PickerToneList.prototype.constructor = PickerToneList;
   PickerToneList.prototype.makeItem = function(tone) {
     var item = ToneList.prototype.makeItem.call(this, tone);
+    allTones[tone.id] = item;
 
     var input = item.querySelector('gaia-radio');
-    input.checked = (tone.id === currentToneID);
     input.addEventListener('click', function() {
       tonePlayer.setTone(tone);
-      document.getElementById('set').disabled = false;
+      setButton.disabled = false;
     });
 
     return item;
   };
 
   navigator.mozL10n.once(function() {
-    var promises = [];
     var listParent = document.getElementById('list-parent');
+    var toneLists = {};
+    var promises = [];
 
-    function addToneLists(toneType, includeNone) {
+    var noneList = new PickerToneList('section-title-none', listParent);
+    noneList.add(new NullRingtone());
+
+    window.builtInRingtones.toneTypes.forEach(function(toneType) {
       var builtInList = new PickerToneList(
         'section-title-builtin-' + toneType, listParent
       );
-      promises.push(window.builtInRingtones.list(toneType)
-                          .then(function(tones) {
-        builtInList.add(tones);
-
-        // Add the empty ringtone to the first list if it's allowed. This is a
-        // bit strange, since the empty ringtone doesn't *really* belong in any
-        // of our categories, so as a compromise, we just put it first. In
-        // practice, this works out ok, since consumers generally set allowNone
-        // to true when they want an alert tone, and the empty ringtone is
-        // sort of an alert tone.
-        if (includeNone) {
-          builtInList.add(new NullRingtone());
-        }
-      }));
+      promises.push(window.builtInRingtones.list(toneType).then(
+        (tones) => builtInList.add(tones)
+      ));
 
       var customList = new PickerToneList(
         'section-title-custom-' + toneType, listParent
       );
-      promises.push(window.customRingtones.list(toneType).then(function(tones) {
-        customList.add(tones);
-      }));
-      promises.push(window.sdCardRingtones.list(toneType).then(function(tones) {
-        customList.add(tones);
-      }));
-    }
+      promises.push(window.customRingtones.list(toneType).then(
+        (tones) => customList.add(tones)
+      ));
+      promises.push(window.sdCardRingtones.list(toneType).then(
+        (tones) => customList.add(tones)
+      ));
 
-    // Add the asked-for built-in tones.
-    toneTypes.forEach(function(toneType, i) {
-      addToneLists(toneType, i === 0 && allowNone);
+      toneLists[toneType] = {builtIn: builtInList, custom: customList};
     });
 
-    // Add the unasked-for built-in tones.
-    otherToneTypes.forEach(function(toneType) {
-      addToneLists(toneType);
-    });
-
-    Promise.all(promises).then(function(listsData) {
+    Promise.all(promises).then(function() {
       // This just notifies the tests that we're finished building our lists.
       document.querySelector('body').dataset.ready = true;
     });
+
+    navigator.mozSetMessageHandler('activity', function(activity) {
+
+      var requestedTypes = activity.source.data.type;
+      if (!Array.isArray(requestedTypes)) {
+        requestedTypes = [requestedTypes];
+      }
+
+      // Put the requested tone types first, followed by all the rest.
+      var allToneTypes = window.builtInRingtones.toneTypes;
+      var toneTypes = [
+        ...requestedTypes.filter((x) => allToneTypes.includes(x)),
+        ...allToneTypes.filter((x) => !requestedTypes.includes(x))
+      ];
+
+      // Rearrange the lists so they're in the expected order.
+      toneTypes.forEach((toneType) => {
+        listParent.appendChild(toneLists[toneType].builtIn.element);
+        listParent.appendChild(toneLists[toneType].custom.element);
+      });
+
+      // Show/hide the "None" ringtone.
+      noneList.element.hidden = !activity.source.data.allowNone;
+
+      // Find the item corresponding to the current ringtone, and select it.
+      Promise.all(promises).then(() => {
+        var currentItem = allTones[activity.source.data.currentToneID];
+        if (currentItem) {
+          currentItem.querySelector('gaia-radio').checked = true;
+        }
+      });
+
+      header.addEventListener('action', function() {
+        activity.postError('cancelled');
+      });
+
+      setButton.addEventListener('click', function() {
+        tonePlayer.isValid(function(valid) {
+          if (!valid) {
+            // The tone couldn't be played. Just act like the user canceled.
+            activity.postError('cancelled');
+            return;
+          }
+
+          var selectedTone = tonePlayer.currentTone;
+          selectedTone.getBlob().then(function(blob) {
+            activity.postResult({
+              name: selectedTone.name,
+              l10nID: selectedTone.l10nID,
+              id: selectedTone.id,
+              blob: blob
+            });
+          });
+        });
+      });
+
+    });
+
   });
-});
+})();
