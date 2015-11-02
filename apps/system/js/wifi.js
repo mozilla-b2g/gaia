b6d4138315b883bba2b9bfab9a672bc55caf8149
Timothy Guan-tin Chien <timdream@gmail.com>
2015-11-02 18:17:27 +0800

    Bug 1174405 - Check the condition again when wifi-off alarm triggers

--- apps/system/js/wifi.js
+++ apps/system/js/wifi.js
@@ -6,17 +6,25 @@
   var Wifi = {
     name: 'Wifi',
 
+    // Sync with mozSettings wifi.enabled key with an observer.
+    // Note that there is a timing difference with _enabled and enabled.
     wifiEnabled: true,
 
+    // True if we have disabled the wifi so we would remember to quietly turn
+    // it back on.
+    // (TODO: this should be named wifiDisabledByTimeout)
     wifiDisabledByWakelock: false,
 
     // Without an wifi wake lock, wait for screenOffTimeout milliseconds
     // to turn wifi off after the conditions are met.
     // If it's set to 0, wifi will never be turn off.
+    // This is in sync with mozSettings wifi.screen_off_timeout.
     screenOffTimeout: 0,
 
-    // When wifiSleepMode is true, Wi-Fi will be automatically turned off
-    // during sleep to save battery power.
+    // When wifiSleepMode is true and the screenOffTimeout is not zero,
+    // Wi-Fi will be automatically turned off during sleep to save battery
+    // power.
+    // This is in sync with mozSettings wifi.sleepMode.
     wifiSleepMode: false,
 
     // if Wifi is enabled but disconnected, try to scan for networks every
@@ -25,11 +33,19 @@
 
     _scanTimer: null,
 
+    // Sync with the hardware (mozWifiManager) state by listening to the
+    // two events.
     _enabled: false,
 
     get enabled() {
       return this._enabled;
     },
+
+    // true if there is an alarm or if there is an alarm being set.
+    _hasAlarm: false,
+
+    // keep the id returned by mozAlarms API
+    _alarmId: null,
 
     start: function() {
       if (!window.navigator.mozSettings) {
@@ -169,8 +185,59 @@
       this.maybeToggleWifi();
     },
 
-    // Check the status of screen, wifi wake lock and power source
-    // and turn on/off wifi accordingly
+    /**
+     * Check the status of screen, wifi wake lock, and power source
+     * and turn on/off wifi accordingly.
+     *
+     * You can think of this as a gigantic state machine where the input
+     * is the states and the output is triggering the timer to turn wifi off,
+     * or turning the wifi back on (if it is turned off by us).
+     *
+     * |--------------------------------|---------------------|
+     * | Conditions                     |                     |
+     * |--------|-----------|-----------| End state           |
+     * | Screen | Power     | Wake lock |                     |
+     * |--------|-----------|-----------|---------------------|
+     * | off    | unplugged | not hold  | set timer to off (1)|
+     * |--------|-----------|-----------|---------------------|
+     * | off    | unplugged | hold      | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (2)           |
+     * |--------|-----------|-----------|---------------------|
+     * | off    | plugged   | not hold  | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (3)           |
+     * |--------|-----------|-----------|---------------------|
+     * | off    | plugged   | hold      | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (4)           |
+     * |--------|-----------|-----------|---------------------|
+     * | on     | unplugged | not hold  | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (5)           |
+     * |--------|-----------|-----------|---------------------|
+     * | on     | unplugged | hold      | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (6)           |
+     * |--------|-----------|-----------|---------------------|
+     * | on     | plugged   | not hold  | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (7)           |
+     * |--------|-----------|-----------|---------------------|
+     * | on     | plugged   | hold      | turn back on        |
+     * |        |           |           | if turned off by    |
+     * |        |           |           | timer (8)           |
+     * |--------|-----------|-----------|---------------------|
+     *
+     * The rest of the code are housekeepers making sure we keep an CPU lock
+     * during the operations, and try not to set the timer twice etc.
+     *
+     * It is also assumed setting mozSettings w/ the same value would be a
+     * no-op with no side effects so we don't really protect that.
+     *
+     * As the entry point to of the state machine, it is very important to
+     * keep this function making the right decisions at all times.
+     */
     maybeToggleWifi: function wifi_maybeToggleWifi() {
       // Keep CPU awake to ensure the flow is done.
       var cpuLock = navigator.requestWakeLock('cpu');
@@ -208,15 +275,8 @@
       var lock = SettingsListener.getSettingsLock();
       // Let's quietly turn off wifi if there is no wake lock and
       // the screen is off and we are not on a power source.
-      if (!Service.query('screenEnabled') && !battery.charging) {
-        if (!this.wifiEnabled && this._wakeLockManager.isHeld) {
-          lock.set({ 'wifi.enabled': true });
-          window.addEventListener('wifi-enabled', function() {
-            releaseCpuLock();
-          });
-          return;
-        }
-
+      if (!Service.query('screenEnabled') && !battery.charging &&
+          !this._wakeLockManager.isHeld) {
         // We still need to turn of wifi even if there is no Alarm API
         if (!navigator.mozAlarms) {
           console.warn('Turning off wifi without sleep timer because' +
@@ -233,28 +293,39 @@
         // When user wants to allow wifi off then start with a timer,
         // only turn off wifi till timeout.
         if (this.wifiSleepMode === true) {
+          // maybeToggleWifi state machine state #1
+
+          // We should not try to set more than one alarms.
+          if (this._hasAlarm) {
+            releaseCpuLock();
+            return;
+          }
+
+          this._hasAlarm = true;
           var date = new Date(Date.now() + this.screenOffTimeout);
-          var self = this;
           var req = navigator.mozAlarms.add(date, 'ignoreTimezone', 'wifi-off');
           req.onsuccess = function wifi_offAlarmSet() {
-            self._alarmId = req.result;
+            this._alarmId = req.result;
             releaseCpuLock();
-          };
+          }.bind(this);
           req.onerror = function wifi_offAlarmSetFailed() {
             console.warn('Fail to set wifi sleep timer on Alarm API. ' +
               'Turn off wifi immediately.');
-            self.sleep();
+            this._hasAlarm = false;
+            this.sleep();
             releaseCpuLock();
-          };
-        }
-        else {
+          }.bind(this);
+        } else {
+          releaseCpuLock();
           return;
         }
       }
       // ... and quietly turn it back on or cancel the timer otherwise
       else {
+        // maybeToggleWifi state machine state #2 to #8
         if (this._alarmId) {
           navigator.mozAlarms.remove(this._alarmId);
+          this._hasAlarm = false;
           this._alarmId = null;
         }
 
@@ -340,14 +411,28 @@
       }
 
       this._systemMessageHandlerRegistered = true;
-      var self = this;
       navigator.mozSetMessageHandler('alarm', function gotAlarm(message) {
         if (message.data !== 'wifi-off') {
           return;
         }
 
-        self.sleep();
-      });
+        this._hasAlarm = false;
+        this._alarmId = null;
+
+        // Wifi shall not sleep if the condition to turn this off has changed.
+        // (Alarm should have been removed when condition changes, but for
+        // reason unknown to us it still triggers.)
+        var battery = window.navigator.battery;
+        if (Service.query('screenEnabled') ||
+            battery.charging ||
+            this._wakeLockManager.isHeld) {
+          console.error(
+            'Wifi: mozAlarms should be cancelled but still triggers.');
+          return;
+        }
+
+        this.sleep();
+      }.bind(this));
     }
   };
   exports.Wifi = Wifi;
