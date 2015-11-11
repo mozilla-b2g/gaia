6ab68d4d362d4d61048075a1906a36e3d80bd8dc
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2015-11-11 19:19:07 +0800

    Bug 1220584 - [TV] FTE for Casting video and website to TV. r=rexboy, a=jocheng

---
+++ tv_apps/fling-tutorial/js/offline_content/index.js
@@ -0,0 +1,24 @@
+/* global FTEWizard
+*/
+'use strict';
+(function(exports) {
+
+  var tutorialContainer = document.body;
+
+  var tutorial = new FTEWizard('castingTutorial');
+  tutorial.init({
+    container: tutorialContainer,
+    pageClass: 'slide',
+    buttonsClass: 'slide-button',
+    launchEveryTime: true,
+    propagateKeyEvent: true,
+    onfinish: function () {
+      // Rewrite the location to trigger the mozbrowserlocationchange event
+      // to let the outer app know user finishes this tutorial
+      window.location.href = window.location.href + '#finished';
+    }.bind(tutorial)
+  });
+
+  exports.tutorial = tutorial;
+
+})(window);
