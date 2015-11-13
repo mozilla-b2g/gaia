9b95e5a2a1ec55490e3cc3b528ccd1b155262c96
Punam Dahiya <punamdahiya@yahoo.com>
2015-11-13 14:22:09 -0800

    Bug 1219657 - [Gallery] Implement delete multiple images Gij test

--- apps/gallery/test/marionette/lib/gallery.js
+++ apps/gallery/test/marionette/lib/gallery.js
@@ -27,10 +27,12 @@ Gallery.Selector = Object.freeze({
   thumbnailsSelectButton: '#thumbnails-select-button',
   thumbnailsDeleteButton: '#thumbnails-delete-button',
   confirmButton: '#confirm-ok',
+  confirmCancelButton: '#confirm-cancel',
   overlayView: '#overlay',
   overlayTitle: '#overlay-title',
   overlayText: '#overlay-text',
-  overlayCameraButton: '#overlay-camera-button'
+  overlayCameraButton: '#overlay-camera-button',
+  thumbnailsNumberSelected: '#thumbnails-number-selected'
 });
 
 Gallery.prototype = {
@@ -120,7 +122,30 @@ Gallery.prototype = {
    */
   get confirmButton() {
     return this.client.helper.waitForElement(Gallery.Selector.confirmButton);
-  }, 
+  },
+
+  /**
+   * @return {Marionette.Element} Element to click to cancel the delete dialog.
+   */
+  get confirmCancelButton() {
+    return this.client.helper.waitForElement(
+      Gallery.Selector.confirmCancelButton);
+  },
+
+  /**
+   * @return {Marionette.Element} Element to display number of selected items
+   */
+  get thumbnailsNumberSelected() {
+    return this.client.helper.waitForElement(
+      Gallery.Selector.thumbnailsNumberSelected);
+  },
+
+  switchToSelectView: function() {
+    this.thumbnailsSelectButton.click();
+    this.client.waitFor(function() {
+      return this.thumbnailsNumberSelected.displayed();
+    }.bind(this));
+  },
 
   tapFirstThumbnail: function() {
     this.thumbnail.click();
@@ -130,12 +155,17 @@ Gallery.prototype = {
     this.thumbnails[n].click();
   },
 
+  isThumbnailSelected: function(n) {
+    this.client.waitFor(function() {
+      return this.thumbnails[n].cssProperty('outline') != null;
+    }.bind(this));
+  },
+
   /**
    * Start the Gallery, save the client for future ops, and wait for the
    * Gallery to finish an initial render.
    */
   launch: function(nomedia) {
-
     this.client.apps.launch(Gallery.ORIGIN);
     this.client.apps.switchToApp(Gallery.ORIGIN);
     // Wait for the document body to know we're really 'launched'.
