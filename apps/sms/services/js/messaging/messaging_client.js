98c7c768f653554fddc7642af3792b32c5473af3
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-11-15 03:30:48 +0530

    Bug 1223462 - [Messages][Docs] Improve JSDoc comments in services/js/message_manager.js . r=azasypkin

--- apps/sms/services/js/messaging/messaging_client.js
+++ apps/sms/services/js/messaging/messaging_client.js
@@ -16,7 +16,6 @@
 
   /**
    * Reference to active bridge client instance.
-   *
    * @type {Client}
    */
   var client;
@@ -51,18 +50,71 @@
       });
     },
 
+    /**
+     * Sends an SMS based on the specified parameter object.
+     * @param {Object} options Options object that contains :-
+     * - recipients (string or array of string): contains the list of
+     *   recipients for this message
+     * - content (string): the message's body
+     * - serviceId (optional long or string): the SIM serviceId we use to send
+     *   the message
+     * - onsuccess (optional function): will be called when one SMS has been
+     *   sent successfully, with the request's result as argument. Can be called
+     *   several times.
+     * - onerror (optional function): will be called when one SMS transmission
+     *   failed, with the error object as argument. Can be called several times.
+     * - oncomplete (optional function): will be called when all messages have
+     *   been sent. Its argument will have the following properties:
+     *   + hasError (boolean): whether we had at least one error
+     *   + return (array): each item is an object with the following properties:
+     *     . success (boolean): whether this is a success or an error
+     *     . result (request's result): the request's result object
+     *     . recipient (string): the recipient used for this transmission.
+     * @return {Promise}
+     */
     sendSMS(options) {
       return client.method('sendSMS', options, appInstanceId);
     },
 
+    /**
+     * Sends an MMS based on the specified parameter object.
+     * @param {Object} options Options object is the options field with the
+     * following properties:
+     * - recipients (string or array of string): recipients for this message.
+     * - subject (optional string): subject for this message.
+     * - content (array of SMIL slides): this is the content for the message
+     *   (see ConversationView for more information).
+     * - serviceId (optional long or string): the SIM that should be used for
+     *   sending this message. If this is not the current default configuration
+     *   for sending MMS, then we'll first switch the configuration to this
+     *   serviceId, and only then send the message. That means that the
+     *   "sending" event will come quite late in this case.
+     * - onsuccess (optional func): called only once, even for several
+     *   recipients, when the message is successfully sent.
+     * - onerror (optional func): called only once if there is an error.
+     * @return {Promise}
+     */
     sendMMS(options) {
       return client.method('sendMMS', options, appInstanceId);
     },
 
+    /**
+     * Resends message with the given opts.
+     * Takes a formatted message in case you happen to have one.
+     * @param {Object} message The message object containing all the information
+     * depending on the type of the message which can be 'sms' or 'mms'.
+     * @return {Promise}
+     */
     resendMessage(message) {
       return client.method('resendMessage', message, appInstanceId);
     },
 
+    /**
+     * Gets the MMS object having a given id.
+     * @param {number} id The id of the MMS we wish to get.
+     * @return {Promise<MmsMessage>} A Promise object that handles the success
+     * or error of the operation.
+     */
     retrieveMMS(id) {
       return client.method('retrieveMMS', id, appInstanceId);
     }
