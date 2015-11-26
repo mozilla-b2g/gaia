aa357738f78277497fc125a7dc3464c175186e20
gasolin <gasolin@gmail.com>
2015-11-26 16:38:24 +0800

    Bug 1228252 - move openDialog from util.js to panel_utils, r=yzen

--- apps/settings/test/unit/mock_settings_utils.js
+++ apps/settings/test/unit/mock_settings_utils.js
@@ -2,7 +2,6 @@ define(function() {
   'use strict';
 
   var MockSettingsUtils = {
-    openDialog: function(dialogID, userOptions) {},
     loadTemplate: function(templatePanelId, callback) {
       callback();
     }
