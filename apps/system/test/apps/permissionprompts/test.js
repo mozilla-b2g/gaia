ab49c274d29d3f5c403925a7f54e76c2a4920c43
Michael Henretty <michael.henretty@gmail.com>
2015-12-04 14:39:04 +0100

    Bug 1219698 - Add marionette js test for privileged app device storage permission prompts

--- apps/system/test/apps/permissionprompts/test.js
+++ apps/system/test/apps/permissionprompts/test.js
@@ -1,10 +1,22 @@
 'use strict';
 
+function getDeviceStorageFile(type) {
+  var storage = navigator.getDeviceStorage(type);
+  storage.get('fake.fak');
+}
+
 window.addEventListener('load', function() {
   // Save a contact to prompt for contacts permission.
   document.getElementById('contacts').onclick = function() {
     var person = new window.mozContact();
     navigator.mozContacts.save(person);
   };
+
+  var storageTypes = ['sdcard', 'music', 'pictures', 'videos'];
+  storageTypes.forEach(function(type) {
+    var el = document.getElementById(type);
+    el.onclick = getDeviceStorageFile.bind(this, type);
+  });
+
   document.querySelector('body').classList.add('loaded');
 });
