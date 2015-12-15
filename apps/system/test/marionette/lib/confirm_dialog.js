0e0fbaac68118c5780ea6961114baab0b1b358e6
albertopq <albert.pastor@gmail.com>
2015-12-15 17:17:35 +0100

    Bug 1142439 - Using client helper for waiting the dialog button

--- apps/system/test/marionette/lib/confirm_dialog.js
+++ apps/system/test/marionette/lib/confirm_dialog.js
@@ -17,8 +17,12 @@ ConfirmDialog.prototype = {
   @param {String} type of dialog.
   */
   getElement: function(type) {
-    var selector = 'gaia-confirm[data-type="' + type + '"]';
+    var selector = this.getDialogSelector(type);
     return this.client.helper.waitForElement(selector);
+  },
+
+  getDialogSelector: function(type) {
+    return 'gaia-confirm[data-type="' + type + '"]';
   },
 
   /**
@@ -28,32 +32,12 @@ ConfirmDialog.prototype = {
   @param {String} selector of the button. Defaults to .confirm.
   */
   confirm: function(type, button) {
-    var dialog = this.getElement(type);
-    var confirmButton;
-
+    var dialogSelector = this.getDialogSelector(type);
+    var confirmSelector = dialogSelector + ' .confirm';
+    var confirmButton = this.client.helper.waitForElement(confirmSelector);
     this.client.waitFor(function() {
-      confirmButton = dialog.findElement(button || '.confirm');
-      return confirmButton && confirmButton.displayed();
+      return confirmButton.displayed();
     });
-
-    // XXX: Hack to use faster polling
-    var quickly = this.client.scope({ searchTimeout: 50 });
-    confirmButton.client = quickly;
-
-    // tricky logic to ensure the dialog has been removed and clicked
-    this.client.waitFor(function() {
-      try {
-        // click the dialog to dismiss it
-        confirmButton.click();
-        // ensure it is either hidden or hits the stale element ref
-        return !confirmButton.displayed();
-      } catch (e) {
-        if (e.type === 'StaleElementReference') {
-          // element was successfully removed
-          return true;
-        }
-        throw e;
-      }
-    });
+    confirmButton.click();
   }
 };
