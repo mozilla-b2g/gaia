860c6a2bdc7bdaf609b7dc5c1cdd7899636e2bf9
albertopq <albert.pastor@gmail.com>
2015-12-16 12:22:46 +0100

    Bug 1219628 - Test Browser Save Image

--- apps/system/test/marionette/browser_context_menu_test.js
+++ apps/system/test/marionette/browser_context_menu_test.js
@@ -39,11 +39,36 @@ marionette('Browser Context Menu', function() {
     rocketbar.goToURL(url);
     system.shareLink();
 
-
     system.getActivityOptionMatching('sms').tap();
     var smsFrame = messagesApp.waitForAppToAppear();
     client.switchToFrame(smsFrame);
 
     assert.equal(messagesApp.Composer.messageInput.text(), url);
+  });
+
+  test('save image', function() {
+    var url = server.url('sample_image.html');
+    rocketbar.goToURL(url);
+    system.gotoBrowser(url);
+
+    var image = client.helper.waitForElement('img');
+    actions.longPress(image, 0.5).perform(function() {
+      system.saveImage();
+
+      client.switchToFrame();
+      client.waitFor(function() {
+        return system.toaster.displayed();
+      });
+
+      client.waitFor(function() {
+        return !system.toaster.displayed();
+      });
+
+      // This does not work on desktop. See:
+      // tests/python/gaia-ui-tests/gaiatest/tests/functional/browser/manifest.ini#L22
+      // and Bug 1091622
+
+      // assert(system.getSdCardFilesMatching('.png').length === 1);
+    });
   });
 });
