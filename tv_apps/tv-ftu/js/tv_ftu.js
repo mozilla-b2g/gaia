d9177aad28c7adc38bba5e58f4c35ea75d62c1f4
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2015-12-21 15:49:15 +0800

    Bug 1232584 - [TV] FTE for usage metrics specs update for the new legal approved terms

--- tv_apps/tv-ftu/js/tv_ftu.js
+++ tv_apps/tv-ftu/js/tv_ftu.js
@@ -1,4 +1,5 @@
-/* global evt, SettingsListener, KeyNavigationAdapter, SimpleKeyNavigation */
+/* global evt, SettingsListener, KeyNavigationAdapter, SimpleKeyNavigation
+*/
 (function (exports) {
 'use strict';
 
@@ -11,35 +12,92 @@ window.addEventListener('load', function() {
       this.APP_USAGE_ENABLE_VALUE = true;
       this.APP_USAGE_DISABLE_VALUE = false;
 
-      this._noButton =
-          document.getElementById('app-usage-no-button');
-      this._yesButton =
-          document.getElementById('app-usage-yes-button');
-      this._terms =
-          document.getElementById('app-usage-terms');
-      this._termsSection =
-          document.getElementById('app-usage-terms-section');
-      this._arrowIcon =
-          document.getElementById('app-usage-arrow-icon');
+      this._currentPage = null;
+      this._currentContentSection = null;
+      this._currentContent = null;
+      this._currentArrowIcon = null;
+      this._currentFocused = null;
+      this._currentNavigationType = null;
 
       this.keyNavAdapter = new KeyNavigationAdapter();
       this.keyNavAdapter.init();
       this.keyNavAdapter.on('move', this.onKeyMove.bind(this));
+      this.keyNavAdapter.on('back-keyup', this.onBackKeyUp.bind(this));
       this.keyNavAdapter.on('enter-keyup', this.onEnterKeyUp.bind(this));
 
-      this.buttonNav = new SimpleKeyNavigation();
-      this.buttonNav.start(
-        [this._noButton, this._yesButton],
-        SimpleKeyNavigation.DIRECTION.HORIZONTAL
-      );
-      this.buttonNav.on('focusChanged', (elem) => {
-        this.fire('focus', elem);
-      });
-      this.buttonNav.pause();
+      this.keyNav = new SimpleKeyNavigation();
+      this.keyNav.start([], SimpleKeyNavigation.DIRECTION.HORIZONTAL);
+      this.keyNav.on('focusChanged', elem => this.fire('focus', elem));
 
-      this._termsSection.focus();
-      this._currentFocused = this._termsSection;
+      // Special treatment for translation with links
+      var links = document.querySelectorAll(
+        '#app-usage-page p[data-l10n-id=fxos-is-free] > a');
+      links[0].setAttribute('tabindex', -1);
+      links[0].setAttribute('id', 'fxos-privacy-notice-link');
+      links[1].setAttribute('tabindex', -1);
+      links[1].setAttribute('id', 'moz-privacy-policy-link');
+
       this.on('focus', this.onFocus.bind(this));
+      this.goToPage('app-usage-page');
+    },
+
+    goToPage: function (id) {
+      if (this._currentPage) {
+        this._currentPage.classList.add('hidden');
+      }
+      this._currentPage = document.getElementById(id);
+      this._currentArrowIcon = this._currentPage.querySelector('.arrow-icon');
+      this._currentContent = this._currentPage.querySelector('.page-content');
+      this._currentContentSection =
+        this._currentPage.querySelector('.content-section');
+      this._currentPage.classList.remove('hidden');
+
+      this._currentNavigationType = null;
+      this.changeNavigationType('buttons');
+      if (this.getContentUnscrolledHeight() > 0) {
+        this.changeNavigationType('unscrolled_content');
+      }
+    },
+
+    getContentUnscrolledHeight: function () {
+      var unscrolledHeight = this._currentContent.clientHeight -
+                             this._currentContentSection.scrollTop -
+                             this._currentContentSection.clientHeight;
+      return unscrolledHeight;
+    },
+
+    changeNavigationType: function (target) {
+      if (this._currentNavigationType !== target) {
+
+        this._currentNavigationType = target;
+        this._currentArrowIcon.classList.add('hidden');
+        this.keyNav.pause();
+
+        switch (target) {
+          case 'unscrolled_content':
+            this._currentContentSection.focus();
+            this._currentArrowIcon.classList.remove('hidden');
+            this.fire('focus', this._currentContentSection);
+          break;
+
+          case 'links':
+            this.keyNav.resume();
+            this.keyNav.updateList(Array.prototype.slice.call(
+              this._currentPage.querySelectorAll('a')));
+          break;
+
+          case 'buttons':
+            this.keyNav.resume();
+            this.keyNav.updateList(Array.prototype.slice.call(
+              this._currentPage.querySelectorAll('.page-button')));
+            var defaultBtn =
+              this._currentPage.querySelector('.page-default-button');
+            if (defaultBtn) {
+              this.keyNav.focusOn(defaultBtn);
+            }
+          break;
+        }
+      }
     },
 
     onFocus: function (elem) {
@@ -47,26 +105,36 @@ window.addEventListener('load', function() {
     },
 
     onKeyMove: function (key) {
-      switch (key) {
-        case 'up':
-          if (this._currentFocused !== this._termsSection) {
-            this.buttonNav.blur();
-            this.buttonNav.pause();
-            this._termsSection.focus();
-            this.fire('focus', this._termsSection);
-            this._arrowIcon.classList.remove('hidden');
+
+      switch (this._currentNavigationType) {
+        case 'unscrolled_content':
+          if (key === 'down' && this.getContentUnscrolledHeight() <= 0) {
+            this.changeNavigationType('buttons');
           }
         break;
 
-        case 'down':
-          if (this._currentFocused === this._termsSection) {
-            var unscrolledHeight = this._terms.clientHeight -
-                                   this._termsSection.scrollTop -
-                                   this._termsSection.clientHeight;
-            if (unscrolledHeight <= 0) {
-              this.buttonNav.resume();
-              this.buttonNav.focusOn(this._yesButton);
-              this._arrowIcon.classList.add('hidden');
+        case 'links':
+          // this.keyNav only handles
+          // the left/right key (horizontal key navigation)
+          // so we need extra handling of the up/down key here
+          if ((key === 'down' || key === 'right') &&
+            'moz-privacy-policy-link' === this._currentFocused.id) {
+            // If focus on the last link, moz-privacy-policy-link,
+            // then change navigation target to buttons
+            this.changeNavigationType('buttons');
+          } else if (key === 'up') {
+            this.keyNav.movePrevious();
+          } else if (key === 'down') {
+            this.keyNav.moveNext();
+          }
+        break;
+
+        case 'buttons':
+          if (key === 'up') {
+            if (this._currentContentSection.scrollTop > 0) {
+              this.changeNavigationType('unscrolled_content');
+            } else if (this._currentPage.querySelectorAll('a').length > 0) {
+              this.changeNavigationType('links');
             }
           }
         break;
@@ -74,11 +142,11 @@ window.addEventListener('load', function() {
     },
 
     onEnterKeyUp: function () {
-      switch (this._currentFocused) {
-        case this._yesButton:
-        case this._noButton:
+      switch (this._currentFocused.id) {
+        case 'app-usage-page-no-button':
+        case 'app-usage-page-yes-button':
           var setting = {};
-          if (this._currentFocused === this._yesButton) {
+          if (this._currentFocused.id === 'app-usage-page-yes-button') {
             setting[this.APP_USAGE_ENABLE_KEY] = this.APP_USAGE_ENABLE_VALUE;
           } else {
             setting[this.APP_USAGE_ENABLE_KEY] = this.APP_USAGE_DISABLE_VALUE;
@@ -86,6 +154,36 @@ window.addEventListener('load', function() {
           SettingsListener.getSettingsLock().set(setting);
           window.close();
         break;
+
+        case 'moz-privacy-policy-link':
+        case 'fxos-privacy-notice-link':
+          var privacySection =
+            document.querySelector('#privacy-policy-page > .content-section');
+          var mozPrivacy =
+            document.querySelector('#moz-privacy-policy-content');
+          var fxosPrivacy =
+            document.querySelector('#fxos-privacy-notice-content');
+
+          if (this._currentFocused.id === 'moz-privacy-policy-link') {
+            mozPrivacy.style.display = 'block';
+            fxosPrivacy.style.display = 'none';
+          } else {
+            mozPrivacy.style.display = 'none';
+            fxosPrivacy.style.display = 'block';
+          }
+          privacySection.scrollTo(0, 0);
+          this.goToPage('privacy-policy-page');
+        break;
+
+        case 'open-app-usage-page-button':
+          this.goToPage('app-usage-page');
+        break;
+      }
+    },
+
+    onBackKeyUp: function () {
+      if (this._currentPage.id !== 'app-usage-page') {
+        this.goToPage('app-usage-page');
       }
     }
   });
