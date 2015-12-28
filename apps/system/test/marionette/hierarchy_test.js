6c7863394c6000226275f35d0c38b28a72e751f9
Gareth Aye <gareth.aye@gmail.com>
2015-12-28 11:47:55 -0800

    Bug 1234601 - Error while trying to run make test-integration-test: TypeError: Cannot read property 'getAppClass' of undefined r=auswerk

--- apps/system/test/marionette/hierarchy_test.js
+++ apps/system/test/marionette/hierarchy_test.js
@@ -70,7 +70,7 @@
 
     var system;
     var utilityTray;
-    var rocketbar = new Rocketbar(client);
+    var rocketbar;
     var fxASystemDialog = new FxASystemDialog(client);
     var lockscreen = new Lockscreen();
     lockscreen.start(client);
@@ -80,6 +80,7 @@
 
     suite('Test aria-hidden and top most UI', function() {
       setup(function() {
+        rocketbar = new Rocketbar(client);
         utilityTray = new UtilityTray(client);
         system = client.loader.getAppClass('system');
         system.waitForFullyLoaded();
