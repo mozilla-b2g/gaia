058a3c125d2b82940d267bea4c47fdd1f87ec892
UberMarklar <mbryant@mozilla.com>
2015-12-29 10:33:20 -0800

    Bug 1231998 - test_settings_airplane_mode.py : TimeoutException: TimeoutException: Timed out after 40.1 seconds

--- tests/python/gaia-ui-tests/gaiatest/tests/functional/settings/test_settings_airplane_mode.py
+++ tests/python/gaia-ui-tests/gaiatest/tests/functional/settings/test_settings_airplane_mode.py
@@ -45,10 +45,7 @@ class TestAirplaneMode(GaiaTestCase):
         settings.disable_airplane_mode()
 
         # Wait for wifi to be connected, because this takes the longest to connect after airplane mode is switched off
-        self.wait_for_condition(lambda s: 'Connected to ' + self.testvars['wifi']['ssid'] in settings.wifi_menu_item_description, timeout=40)
-
-        # check Wifi is enabled
-        self.assertTrue(self.data_layer.is_wifi_connected(self.testvars['wifi']), "WiFi was not connected after switching off Airplane mode")
+        settings.wait_until_wifi_is_connected_to(self.testvars['wifi']['ssid'])
 
         # check that Cell Data is enabled
         self.assertTrue(self.data_layer.get_setting('ril.data.enabled'), "Cell data was not connected after switching off Airplane mode")
