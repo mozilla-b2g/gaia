edd03b00f36bb2e4c8fe01ab8e82c0de1c349909
Michael Henretty <michael.henretty@gmail.com>
2015-12-31 15:27:48 +0100

    Bug 1236029 - Override url port banning for integration test web server

--- shared/test/integration/profile.js
+++ shared/test/integration/profile.js
@@ -1,6 +1,17 @@
 /**
  * @fileoverview this is where all the global profile overrides live.
  */
+'use strict';
+
+// Firefox bans loading urls on certain ports even through localhost,
+// but we run the integration web server on a random local port so we need
+// to disable this security.
+// See http://www-archive.mozilla.org/projects/netlib/PortBanning.html
+var mozBannedPorts =
+  [1,7,9,11,13,15,17,19,20,21,22,23,25,37,42,43,53,77,79,87,95,101,102,103,
+   104,109,110,111,113,115,117,119,123,135,139,143,179,389,465,512,513,514,
+   515,526,530,531,532,540,556,563,587,601,636,993,995,2049,4045,6000];
+
 module.exports = {
   settings: {
     'search.suggestions.enabled': false,
@@ -19,7 +30,8 @@ module.exports = {
     'app.update.url.override': '',
     'browser.newtabpage.directory.source': '',
     'browser.newtabpage.directory.ping': '',
-    'webapps.update.enabled': false
+    'webapps.update.enabled': false,
+    'network.security.ports.banned.override': mozBannedPorts.join(',')
   }
   // apps: {}
 };
