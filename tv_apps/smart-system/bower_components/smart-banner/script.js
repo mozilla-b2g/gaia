81759f0547e47277ab89cffd43c93df8914f450c
Ricky Chien <ricky060709@gmail.com>
2015-12-31 18:58:00 +0800

    Bug 1235128 - [TV][2.5] System banner supports icon and title

--- tv_apps/smart-system/bower_components/smart-banner/script.js
+++ tv_apps/smart-system/bower_components/smart-banner/script.js
@@ -79,14 +79,22 @@ window.SmartBanner = (function(win) {
         break;
       case 'animationend':
         this.classList.remove('flying');
-        this.open();
+        this.classList.remove('bouncing');
+
+        if (evt.animationName === 'bouncingAnimation') {
+          this.init();
+          this.fireEvent('hidden');
+        } else {
+          this.open();
+        }
         break;
     }
   };
 
   proto.open = function() {
-    // no operation during flying and hiding
+    // no operation during flying, bouncing and hiding
     if (this.classList.contains('flying') ||
+        this.classList.contains('bouncing') ||
         this.classList.contains('hiding')) {
       return;
     }
@@ -96,13 +104,13 @@ window.SmartBanner = (function(win) {
     this.classList.remove('closed');
     this.classList.remove('closing');
     this.classList.add('opening');
-    // change to opening
     this.style.width = this.calculateChildWidth() + 'px';
   };
 
   proto.close = function() {
-    // no operation during flying and hiding
+    // no operation during flying, bouncing and hiding
     if (this.classList.contains('flying') ||
+        this.classList.contains('bouncing') ||
         this.classList.contains('hiding')) {
       return;
     }
@@ -119,9 +127,20 @@ window.SmartBanner = (function(win) {
     this.classList.add('hiding');
   };
 
-  proto.flyOpen = function() {
+  proto.flying = function() {
+    this.classList.add('flying');
+  };
+
+  proto.bouncing = function() {
+    this.classList.remove('closed');
+    this.style.width = 'auto';
+    this.classList.add('bouncing');
+  };
+
+  proto.show = function(options) {
+    var animation = (options && options.animation) || 'flying';
     if (this.classList.contains('closed')) {
-      this.classList.add('flying');
+      this[animation] && this[animation]();
     }
   };
 
