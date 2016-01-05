ccbc79f2daf7e3735e571b8592525bf1bc6e029f
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2016-01-05 18:07:25 +0800

    3rd try

--- tv_apps/smart-system/test/marionette/app_modal_dialog_test.js
+++ tv_apps/smart-system/test/marionette/app_modal_dialog_test.js
@@ -3,7 +3,7 @@
 var assert = require('chai').assert;
 var AppModalDialog = require('./lib/app_modal_dialog');
 
-// 2nd try
+// 3rd try
 marionette('Test Modal Dialog Events', function() {
 
   var APP_NAME = 'modaldialogapp';
