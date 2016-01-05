d57fcf7b3e41fbb04544cf68b291b7803de73ba7
Tilman Kamp <tilman@dreamandmatter.com>
2016-01-05 17:30:42 +0100

    Testing bug 1117125

--- apps/system/test/marionette/global_overlay_window_test.js
+++ apps/system/test/marionette/global_overlay_window_test.js
@@ -34,15 +34,21 @@ marionette('GlobalOverlayWindow', function() {
   test('The global overlay window should be opened',
   function() {
     fakeGlobalOverlay.launch();
+    console.log ("launched");
     fakeGlobalOverlay.waitForTitleShown(true);
+    console.log ("title shown");
     client.switchToFrame();
+    console.log ("switched to frame");
 
     var globalOverlayWindow =
       client.helper.waitForElement('.globalOverlayWindow.active');
+    console.log ("waited for global overlay element");
 
     client.waitFor(function() {
+      console.log ("testing... global overlay window displayed?");
       return globalOverlayWindow.displayed();
     });
+
     assert(globalOverlayWindow.displayed(),
            'The global overlay window is now visible');
   });
