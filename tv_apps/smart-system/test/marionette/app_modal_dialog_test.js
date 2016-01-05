029255a9642ebc415db00555af974ccc89e55fb3
Fischer.json <foxbrush@Fischerjsons-MacBook-Pro.local>
2016-01-05 15:27:19 +0800

    3rd try

--- tv_apps/smart-system/test/marionette/app_modal_dialog_test.js
+++ tv_apps/smart-system/test/marionette/app_modal_dialog_test.js
@@ -13,7 +13,7 @@ var Keys = {
 var assert = require('chai').assert;
 var AppModalDialog = require('./lib/app_modal_dialog');
 
-// 2nd try
+// 3rd try
 marionette('Test Modal Dialog Events', function() {
 
   var opts = {
