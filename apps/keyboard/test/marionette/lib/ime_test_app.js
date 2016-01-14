973dd7c95e3ff987963af77b88af82049a6e55eb
Ray Lin <ralin@mozilla.com>
2016-01-14 15:25:31 +0800

    Bug 1233533 - Intermittent test fail on Keyboard App. r=timdream

--- apps/keyboard/test/marionette/lib/ime_test_app.js
+++ apps/keyboard/test/marionette/lib/ime_test_app.js
@@ -3,8 +3,10 @@
 /*
  * A helper module for the testing 3rd-party IME app.
  */
+var Base = require('./base');
+
 function ImeTestApp(client) {
-  this.client = client.scope({ searchTimeout: 20000 });
+  Base.call(this, client, ImeTestApp.ORIGIN);
 }
 
 module.exports = ImeTestApp;
@@ -12,12 +14,35 @@ module.exports = ImeTestApp;
 ImeTestApp.ORIGIN = 'app://imetestapp.gaiamobile.org';
 ImeTestApp.MANIFEST_URL = 'app://imetestapp.gaiamobile.org/manifest.webapp';
 
-ImeTestApp.Selector = Object.freeze({
-  sendKeyButton: '#sendKey'
+ImeTestApp.Selectors = Object.freeze({
+  sendKeyButton: '#sendKey',
+  switchLayoutButton: '#switchLayout'
 });
 
 ImeTestApp.prototype = {
+  __proto__: Base.prototype,
+
   get sendKeyButton() {
-    return this.client.helper.waitForElement(ImeTestApp.Selector.sendKeyButton);
+    return this.client.helper.waitForElement(
+      ImeTestApp.Selectors.sendKeyButton);
+  },
+
+  get switchLayoutButton() {
+    return this.client.helper.waitForElement(
+      ImeTestApp.Selectors.switchLayoutButton);
+  },
+
+  switchTo: function() {
+    var systemInputMgmt = this.client.loader.getAppClass(
+      'system', 'input_management');
+    var imeKeyboard;
+
+    systemInputMgmt.waitForKeyboardFrameDisplayed();
+    systemInputMgmt.switchToActiveKeyboardFrame();
+
+    imeKeyboard = this.client.helper.waitForElement('#keyboard.ime-test-app');
+    this.client.waitFor(() => {
+      return imeKeyboard.displayed();
+    });
   }
 };
